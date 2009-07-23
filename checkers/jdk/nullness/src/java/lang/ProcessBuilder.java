0c515dc13b9357c7dc671c1cc3ccd93a372fe37f
Michael Ernst <mernst@cs.washington.edu>
2009-07-23 18:42:42 -0400

    Change array to vararg.

--- checkers/jdk/nullness/src/java/lang/ProcessBuilder.java
+++ checkers/jdk/nullness/src/java/lang/ProcessBuilder.java
@@ -6,9 +6,9 @@ import checkers.nullness.quals.*;
 
 public final class ProcessBuilder{
   public ProcessBuilder(java.util.List<java.lang.String> a1) { throw new RuntimeException("skeleton method"); }
-  public ProcessBuilder(java.lang.String[] a1) { throw new RuntimeException("skeleton method"); }
+  public ProcessBuilder(java.lang.String... a1) { throw new RuntimeException("skeleton method"); }
   public java.lang.ProcessBuilder command(java.util.List<java.lang.String> a1) { throw new RuntimeException("skeleton method"); }
-  public java.lang.ProcessBuilder command(java.lang.String[] a1) { throw new RuntimeException("skeleton method"); }
+  public java.lang.ProcessBuilder command(java.lang.String... a1) { throw new RuntimeException("skeleton method"); }
   public java.util.List<java.lang.String> command() { throw new RuntimeException("skeleton method"); }
   public java.util.Map<java.lang.String, java.lang.String> environment() { throw new RuntimeException("skeleton method"); }
   public @Nullable java.io.File directory() { throw new RuntimeException("skeleton method"); }
