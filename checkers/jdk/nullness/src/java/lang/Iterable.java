a8150863ea2f59b2bfe36f95a8582bd6029a77de
Michael Ernst <mernst@cs.washington.edu>
2009-07-28 09:39:10 -0700

    Permit Iterable type param to be @Nullable

--- checkers/jdk/nullness/src/java/lang/Iterable.java
+++ checkers/jdk/nullness/src/java/lang/Iterable.java
@@ -4,6 +4,6 @@ import checkers.nullness.quals.*;
 
 @checkers.quals.DefaultQualifier("checkers.nullness.quals.NonNull")
 
-public abstract interface Iterable<T> {
+public abstract interface Iterable<T extends @Nullable Object> {
   public abstract java.util.Iterator<T> iterator();
 }
