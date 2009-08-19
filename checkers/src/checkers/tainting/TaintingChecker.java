ac6a133a4c356bbaf94370df50491b70a2fb06fa
mali <devnull@localhost>
2009-08-19 15:38:12 -0400

    improvements to tainting javadoc

--- checkers/src/checkers/tainting/TaintingChecker.java
+++ checkers/src/checkers/tainting/TaintingChecker.java
@@ -4,5 +4,12 @@ import checkers.basetype.BaseTypeChecker;
 import checkers.quals.TypeQualifiers;
 import checkers.quals.Unqualified;
 
+/**
+ * A type-checker plug-in for the Tainting type system qualifier that finds
+ * (and verifies the absence of) trust bugs.
+ *
+ * It verifies that only verified values are trusted and that user-input
+ * is sanitized before use.
+ */
 @TypeQualifiers({Untainted.class, Unqualified.class})
 public class TaintingChecker extends BaseTypeChecker {}
