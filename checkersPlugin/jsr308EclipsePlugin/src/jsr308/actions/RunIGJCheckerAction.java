48f89c8979d0bbe66c73c918b2d14a819d4c60c7
Michael Ernst <mernst@cs.washington.edu>
2009-09-27 20:58:28 -0700

    Convert tabs to spaces

---
+++ checkersPlugin/jsr308EclipsePlugin/src/jsr308/actions/RunIGJCheckerAction.java
@@ -0,0 +1,9 @@
+package jsr308.actions;
+
+import checkers.igj.*;
+
+public class RunIGJCheckerAction extends RunCheckerAction{
+    public RunIGJCheckerAction(){
+        super(IGJChecker.class);
+    }
+}
