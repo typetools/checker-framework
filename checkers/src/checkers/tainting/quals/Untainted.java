fe2ec17ee5af9dce6e03b243cc83863f474967fe
Mahmood Ali <msaeed43@gmail.com>
2009-10-04 00:33:58 -0400

    correct tainting checker hierarchy update demo script expected files

--- checkers/src/checkers/tainting/quals/Untainted.java
+++ checkers/src/checkers/tainting/quals/Untainted.java
@@ -7,6 +7,6 @@ import static com.sun.source.tree.Tree.Kind.STRING_LITERAL;
  * Denotes a reference that is untainted, i.e. can be trusted.
  */
 @TypeQualifier
-@SubtypeOf(Unqualified.class)
+@SubtypeOf(Tainted.class)
 @ImplicitFor(trees = { STRING_LITERAL })
 public @interface Untainted { }
