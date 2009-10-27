7ae02dbeb8ec11c2e8207916bc8d803950786b4e
Michael Ernst <mernst@cs.washington.edu>
2009-10-27 08:34:29 -0700

    Tweak Linear checker Javadoc

--- checkers/src/checkers/linear/LinearChecker.java
+++ checkers/src/checkers/linear/LinearChecker.java
@@ -4,12 +4,10 @@ import checkers.basetype.BaseTypeChecker;
 import checkers.linear.quals.*;
 import checkers.quals.TypeQualifiers;
 
-//  * A typechecker plug-in for the Rawness type system qualifier that finds (and
-//* verifies the absence of) null-pointer errors.
-
 /**
- * A typechecker plug-in for the Linear type system qualifier that finds (and
- * verifies the absence of) usage
+ * A typechecker plug-in for the Linear type system.  A @Linear reference
+ * may be used only one time.  After that, it is "used up" and of type
+ * @Unusable, and any further use is a compile-time error.
  */
 @TypeQualifiers({Normal.class, Linear.class, Unusable.class})
 public class LinearChecker extends BaseTypeChecker { }
