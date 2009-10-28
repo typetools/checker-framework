b128cc43e4ce8cc6354e90b581fc3d4eccf1ff1e
Mahmood Ali <mahmood@notnoop.com>
2009-10-28 01:45:29 -0400

    add keylookup checker

---
+++ checkers/src/checkers/localizing/quals/LocalizableKey.java
@@ -0,0 +1,19 @@
+package checkers.localizing.quals;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import checkers.quals.SubtypeOf;
+import checkers.quals.TypeQualifier;
+import checkers.quals.Unqualified;
+
+/**
+ * Indicates that the {@Code String} type has been localized and
+ * formatted for the target output locale
+ */
+@TypeQualifier
+@SubtypeOf(Unqualified.class)
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LocalizableKey { }
