051a3492703ac1bc02cced011a0d81e47a774479
Mahmood Ali <msaeed43@gmail.com>
2009-11-01 19:10:51 -0800

    correct javadoc tags

--- checkers/src/checkers/i18n/quals/LocalizableKey.java
+++ checkers/src/checkers/i18n/quals/LocalizableKey.java
@@ -9,7 +9,7 @@ import checkers.quals.TypeQualifier;
 import checkers.quals.Unqualified;
 
 /**
- * Indicates that the {@Code String} type has been localized and
+ * Indicates that the {@code String} type has been localized and
  * formatted for the target output locale
  */
 @TypeQualifier
--- checkers/src/checkers/linear/LinearChecker.java
+++ checkers/src/checkers/linear/LinearChecker.java
@@ -5,9 +5,9 @@ import checkers.linear.quals.*;
 import checkers.quals.TypeQualifiers;
 
 /**
- * A typechecker plug-in for the Linear type system.  A @Linear reference
- * may be used only one time.  After that, it is "used up" and of type
- * @Unusable, and any further use is a compile-time error.
+ * A typechecker plug-in for the Linear type system.  A {@code @Linear}
+ * reference may be used only one time.  After that, it is "used up" and
+ * of type {@code @Unusable}, and any further use is a compile-time error.
  */
 @TypeQualifiers({Normal.class, Linear.class, Unusable.class})
 public class LinearChecker extends BaseTypeChecker { }
