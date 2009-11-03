629237f62bd6a3a24536a7fc8d36d3af2d33806e
Mahmood Ali <mahmood@notnoop.com>
2009-11-03 10:28:15 -0500

    remove protected class skeletons (hendering compilation)

--- checkers/jdk/nullness/src/java/lang/ThreadDeath.java
+++ checkers/jdk/nullness/src/java/lang/ThreadDeath.java
@@ -4,6 +4,6 @@ import checkers.nullness.quals.*;
 
 @checkers.quals.DefaultQualifier("checkers.nullness.quals.NonNull")
 
-public class ThreadDeath{
+public class ThreadDeath extends Error{
   public ThreadDeath() { throw new RuntimeException("skeleton method"); }
 }
