608dbe8dca8178568d5f4c16c49d97785c52730c
Michael Ernst <mernst@cs.washington.edu>
2009-11-21 16:33:12 -0800

    Fix bug:  change PolyNull to PolyRaw

--- checkers/jdk/nullness/src/java/lang/Throwable.java
+++ checkers/jdk/nullness/src/java/lang/Throwable.java
@@ -13,7 +13,7 @@ public class Throwable implements java.io.Serializable{
   public @Nullable java.lang.String getMessage() { throw new RuntimeException("skeleton method"); }
   public @Nullable java.lang.String getLocalizedMessage() { throw new RuntimeException("skeleton method"); }
   public @Nullable java.lang.Throwable getCause() { throw new RuntimeException("skeleton method"); }
-  public synchronized @PolyNull java.lang.Throwable initCause(@Nullable java.lang.Throwable a1) @PolyNull { throw new RuntimeException("skeleton method"); }
+  public synchronized @PolyRaw java.lang.Throwable initCause(@Nullable java.lang.Throwable a1) @PolyRaw { throw new RuntimeException("skeleton method"); }
   public java.lang.String toString() { throw new RuntimeException("skeleton method"); }
   public void printStackTrace() { throw new RuntimeException("skeleton method"); }
   public void printStackTrace(java.io.PrintStream a1) { throw new RuntimeException("skeleton method"); }
