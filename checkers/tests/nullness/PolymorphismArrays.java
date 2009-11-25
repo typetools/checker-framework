2909e61e7b2eeffb2fe676449e4e1c7994cdf7ff
Michael Ernst <mernst@cs.washington.edu>
2009-11-24 21:58:54 -0800

    Add failing test

---
+++ checkers/tests/nullness/PolymorphismArrays.java
@@ -0,0 +1,29 @@
+import checkers.nullness.quals.*;
+
+public class PolymorphismArrays {
+
+    public static boolean @PolyNull [] slice(boolean @PolyNull [] seq, int start, int end) {
+        if (seq == null) { return null; }
+        return new boolean[] { };
+    }
+
+    public static boolean @PolyNull [] slice(boolean @PolyNull [] seq, long start, int end) {
+        return slice(seq, (int)start, end);
+    }
+
+    public static @PolyNull String [] intern(@PolyNull String[] a) {
+        return a;
+    }
+
+    // from OneOfStringSequence.java
+    private String[][] elts;
+    public PolymorphismArrays clone() {
+        PolymorphismArrays result = new PolymorphismArrays();
+        result.elts = elts.clone();
+        for (int i=0; i < elts.length; i++) {
+            result.elts[i] = intern(elts[i].clone());
+        }
+        return result;
+  }
+
+}
