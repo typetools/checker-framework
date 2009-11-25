4699cf764d421c8811a8f7e3c7e179ac25a93147
Michael Ernst <mernst@cs.washington.edu>
2009-11-24 21:58:11 -0800

    Improve documentation.

--- checkers/src/checkers/nullness/quals/LazyNonNull.java
+++ checkers/src/checkers/nullness/quals/LazyNonNull.java
@@ -27,7 +27,8 @@ import checkers.quals.TypeQualifier;
  * external method calls that have access to the given field.
  * <p>
  *
- * Note that final fields are treated as LazyNonNull by default.
+ * Note that LazyNonNull is a field declaration annotation, not a type annotation.
+ * Final fields are treated as LazyNonNull by default.
  * <p>
  *
  * This annotation is associated with the {@link NullnessChecker}.
