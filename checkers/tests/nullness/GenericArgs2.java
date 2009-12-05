525c95b42ad056bf2c496b19b336010a1412c6a0
notnoop <devnull@localhost>
2009-12-04 22:39:26 -0500

    remove an out file

--- checkers/tests/nullness/GenericArgs2.java
+++ checkers/tests/nullness/GenericArgs2.java
@@ -8,10 +8,15 @@ class Cell<T extends @Nullable Object> {
 
 class GenericArgs2 {
   static <F extends Object> void test1(Cell<F> collection) {
+    //:: (type.incompatible)
     collection.add(null); // should fail
   }
   static <F extends @Nullable Object> void test2(Cell<F> collection) {
+    //:: (type.incompatible)
     collection.add(null); // should fail
+  }
+  static void test3(Cell<@Nullable Object> collection) {
+    collection.add(null); // valid
   }
   // No "<F super Object>" version of the above, as that is illegal in Java.
 }
