c4356bc87623eaac1ab4ba141627e7926f8d002b
Michael Ernst <mernst@cs.washington.edu>
2009-12-09 10:36:11 -0800

    Add failing test of @Unused and initializers

---
+++ checkers/tests/basic/UnusedTypes.java
@@ -0,0 +1,22 @@
+import checkers.quals.*;
+import checkers.nullness.quals.*;
+
+import checkers.util.test.SuperQual;
+import checkers.util.test.SubQual;
+
+public class UnusedTypes {
+
+  @TypeQualifier
+  @SubtypeOf({})
+  public @interface Prototype {}
+
+  /*@Unused(when=Prototype.class)*/
+  public Object ppt;
+
+  protected @Prototype UnusedTypes() {
+    // It should be legal to initialize an unused field to null in the
+    // constructor.
+    this.ppt = null;
+  }
+
+}
