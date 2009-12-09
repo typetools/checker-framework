d7ba2397c2602591b5075d56ab111ce5e1b2a47d
Michael Ernst <mernst@cs.washington.edu>
2009-12-09 12:45:20 -0800

    Rename method.

--- checkers/tests/basic/ThrowCatch.java
+++ checkers/tests/basic/ThrowCatch.java
@@ -4,7 +4,7 @@ import checkers.util.test.Critical;
 
 abstract class ThrowCatch {
 
-  void throwsUnencrypted() throws Exception {
+  void throwsNoncritical() throws Exception {
     throw new Exception();
   }
 
@@ -14,12 +14,12 @@ abstract class ThrowCatch {
 
   void catches() {
     try {
-      throwsUnencrypted();
+      throwsNoncritical();
     } catch (Exception e) {
     }
 
     try {
-      throwsUnencrypted();
+      throwsNoncritical();
     //:: (type.incompatible)
     } catch (@Critical Exception e) {
     }
