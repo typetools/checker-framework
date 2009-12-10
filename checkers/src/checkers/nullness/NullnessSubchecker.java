ddcb21c6212067138e13989baf147e9ad7ff6f2d
notnoop <notnoop@localhost>
2009-12-10 18:52:14 -0500

    add check for uninitialized fields

--- checkers/src/checkers/nullness/NullnessSubchecker.java
+++ checkers/src/checkers/nullness/NullnessSubchecker.java
@@ -15,5 +15,5 @@ import checkers.source.*;
  * @checker.framework.manual #nullness-checker Nullness Checker
  */
 @TypeQualifiers({ Nullable.class, LazyNonNull.class, NonNull.class, PolyNull.class })
-@SupportedLintOptions({"nulltest"})
+@SupportedLintOptions({"nulltest", "uninitialized"})
 public class NullnessSubchecker extends BaseTypeChecker {}
