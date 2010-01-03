7f244577080a1c8e2f8513db433a2e013d1d9a3d
Michael Ernst <mernst@cs.washington.edu>
2010-01-02 23:08:33 -0800

    Annotate class, trying to mark all uses of Void as @NonNull.

--- checkers/jdk/nullness/src/java/lang/Void.java
+++ checkers/jdk/nullness/src/java/lang/Void.java
@@ -4,7 +4,7 @@ import checkers.nullness.quals.*;
 
 @checkers.quals.DefaultQualifier("checkers.nullness.quals.NonNull")
 
-public final class Void{
+public final @NonNull class Void {
   protected Void() {}
   public final static java.lang.Class<java.lang.Void> TYPE = Void.class;
 }
