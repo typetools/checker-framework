6ed6c9d6c7f9415b6f6e28f429829472d5e3f269
Michael Ernst <mernst@cs.washington.edu>
2010-01-03 09:05:07 -0800

    Failing test:  String treated as interned even though it is constructed from +.

--- checkers/tests/interning/FlowInterning.java
+++ checkers/tests/interning/FlowInterning.java
@@ -10,4 +10,40 @@ public class FlowInterning {
             : (a.equals(b)));
   }
 
+  public String[] parse (String args) {
+
+    // Split the args string on whitespace boundaries accounting for quoted
+    // strings.
+    args = args.trim();
+    List<String> arg_list = new ArrayList<String>();
+    String arg = "";
+    char active_quote = 0;
+    for (int ii = 0; ii < args.length(); ii++) {
+      char ch = args.charAt (ii);
+      if ((ch == '\'') || (ch == '"')) {
+        arg+= ch;
+        ii++;
+        while ((ii < args.length()) && (args.charAt(ii) != ch))
+          arg += args.charAt(ii++);
+        arg += ch;
+      } else if (Character.isWhitespace (ch)) {
+        // System.out.printf ("adding argument '%s'%n", arg);
+        arg_list.add (arg);
+        arg = "";
+        while ((ii < args.length()) && Character.isWhitespace(args.charAt(ii)))
+          ii++;
+        if (ii < args.length())
+          ii--;
+      } else { // must be part of current argument
+        arg += ch;
+      }
+    }
+    // Interning checker should NOT suggest == here.
+    if (!arg.equals (""))
+      arg_list.add (arg);
+
+    String[] argsArray = arg_list.toArray (new String[arg_list.size()]);
+    return argsArray;
+  }
+
 }
