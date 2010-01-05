35d03d2222224962f8abd4197f1a7344a10a9456
notnoop <notnoop@localhost>
2010-01-05 11:28:05 -0500

    move test about asserting nullness of parameters to a separate test mark it as skip, as it is registered under Issue 47

--- checkers/tests/nullness/ConditionalNullness.java
+++ checkers/tests/nullness/ConditionalNullness.java
@@ -63,19 +63,4 @@ public class ConditionalNullness {
         field.toString();       // error
     }
 
-    /** True iff both sequences are non-null and have the same length. */
-    @AssertNonNullIfTrue({"seq1", "seq2"})
-    /* pure */ public static boolean sameLength(boolean @Nullable [] seq1, boolean @Nullable [] seq2) {
-        return ((seq1 != null)
-                && (seq2 != null)
-                && seq1.length == seq2.length);
-    }
-
-    /* pure */ public static boolean pairwiseEqual(boolean @Nullable [] seq3, boolean @Nullable [] seq4) {
-        if (! sameLength(seq3, seq4)) { return false; }
-        boolean b1 = seq3[0];
-        boolean b2 = seq4[0];
-        return true;
-    }
-
 }
