3385e4f3084bf3cdba0dd4932260d868a3cc83e1
notnoop <devnull@localhost>
2010-01-12 12:35:04 -0500

    add some docs to MapGetHeuristics

--- checkers/src/checkers/nullness/MapGetHeuristics.java
+++ checkers/src/checkers/nullness/MapGetHeuristics.java
@@ -18,6 +18,40 @@ import com.sun.source.util.TreePath;
 
 /**
  * Utilities class for handling {@code Map.get()} invocations.
+ *
+ * The heuristics cover the following cases:
+ *
+ * <ol>
+ * <li value="1">Within the true condition of a map.containsKey() if statement:
+ * <pre><code>if (map.containsKey(key)) { Object v = map.get(key); }</code></pre>
+ * </li>
+ *
+ * <li value="2">Within an enhanced-for loop of the map.keySet():
+ * <pre><code>for (Object key: map.keySet()) { Object v = map.get(key); }</code></pre>
+ * </li>
+ *
+ * <li value="3">Preceded by an assertion of contains or nullness get check:
+ * <pre><code>assert map.containsKey(key);
+ * Object v = map.get(key);</code></pre>
+ *
+ * Or
+ *
+ * <pre><code>assert map.get(key) != null;
+ * Object v = map.get(key);</code></pre>
+ *
+ * <li value="4">Preceded by an check of contains or nullness if
+ * test that throws an exception, in the first line:
+ *
+ * <pre><code>if (!map.contains(key)) throw new Exception();
+ * Object v = map.get(key);
+ * </code></pre>
+ *
+ * <li value="5">Preceded by a put-if-absent pattern convention:
+ *
+ * <pre><code>if (!map.contains(key)) map.put(key, DEFAULT_VALUE);
+ * Object v = map.get(key);</code></pre>
+ *
+ * </ol>
  */
 /*package-scope*/ class MapGetHeuristics {
 
@@ -42,6 +76,18 @@ import com.sun.source.util.TreePath;
         mapContains = getMethod("java.util.Map", "containsKey", 1);
     }
 
+    public void handle(MethodInvocationTree tree, AnnotatedExecutableType method) {
+        if (isMapGetInvocation(tree)) {
+            AnnotatedTypeMirror type = method.getReturnType();
+            type.clearAnnotations();
+            if (!isSuppressable((MethodInvocationTree)tree)) {
+                type.addAnnotation(factory.NULLABLE);
+            } else {
+                type.addAnnotation(factory.NONNULL);
+            }
+        }
+    }
+
     /**
      * @return true if the tree is a method invocation to a method overriding
      *        {@code Map.get(K)}
@@ -63,6 +109,9 @@ import com.sun.source.util.TreePath;
                 && isKeyInMap((IdentifierTree)tree.getArguments().get(0), (VariableElement)elt));
     }
 
+    /**
+     * Case 1: get() is within true clause of map.containsKey()
+     */
     private boolean checkForContains(Element key, VariableElement map, TreePath path) {
         for (TreePath tp = path; tp != null; tp = tp.getParentPath()) {
             Tree.Kind kind = tp.getLeaf().getKind();
@@ -77,48 +126,9 @@ import com.sun.source.util.TreePath;
         return false;
     }
 
-    private boolean isInvocationOfContains(Element key, VariableElement map, Tree tree) {
-        if (TreeUtils.skipParens(tree) instanceof MethodInvocationTree) {
-            MethodInvocationTree invok = (MethodInvocationTree)TreeUtils.skipParens(tree);
-            if (isMethod(invok, mapContains)) {
-                Element containsArgument = InternalUtils.symbol(invok.getArguments().get(0));
-                if (key.equals(containsArgument) && map.equals(getSite(invok)))
-                    return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean isInvocationOfPut(Element key, VariableElement map, Tree tree) {
-        if (TreeUtils.skipParens(tree) instanceof MethodInvocationTree) {
-            MethodInvocationTree invok = (MethodInvocationTree)TreeUtils.skipParens(tree);
-            if (isMethod(invok, mapPut)) {
-                Element containsArgument = InternalUtils.symbol(invok.getArguments().get(0));
-                if (key.equals(containsArgument) && map.equals(getSite(invok)))
-                    return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean isCheckOfGet(Element key, VariableElement map, Tree tree) {
-        tree = TreeUtils.skipParens(tree);
-        if (tree.getKind() != Tree.Kind.NOT_EQUAL_TO
-            || ((BinaryTree)tree).getRightOperand().getKind() != Tree.Kind.NULL_LITERAL)
-            return false;
-
-        Tree right = TreeUtils.skipParens(((BinaryTree)tree).getLeftOperand());
-        if (right instanceof MethodInvocationTree) {
-            MethodInvocationTree invok = (MethodInvocationTree)right;
-            if (isMethod(invok, mapGet)) {
-                Element containsArgument = InternalUtils.symbol(invok.getArguments().get(0));
-                if (key.equals(containsArgument) && map.equals(getSite(invok)))
-                    return true;
-            }
-        }
-        return false;
-    }
-
+    /**
+     * Case 2: get() is within enhanced for-loop over the keys
+     */
     private boolean checkForEnhanced(Element key, VariableElement map, TreePath path) {
         for (TreePath tp = path; tp != null; tp = tp.getParentPath()) {
             if (tp.getLeaf().getKind() != Tree.Kind.ENHANCED_FOR_LOOP)
@@ -135,6 +145,9 @@ import com.sun.source.util.TreePath;
         return false;
     }
 
+    /**
+     * Case 3: get() is preceded with an assert
+     */
     private boolean checkForAsserts(Element key, VariableElement map, TreePath path) {
         StatementTree stmt = TreeUtils.enclosingOfClass(path, StatementTree.class);
         if (stmt == null)
@@ -160,24 +173,9 @@ import com.sun.source.util.TreePath;
         return false;
     }
 
-    private boolean isNotContained(Element key, VariableElement map, ExpressionTree tree) {
-        tree = TreeUtils.skipParens(tree);
-        return (tree.getKind() == Tree.Kind.LOGICAL_COMPLEMENT
-                && isInvocationOfContains(key, map, ((UnaryTree)tree).getExpression()));
-    }
-
-    private StatementTree firstStatement(StatementTree tree) {
-        StatementTree first = tree;
-        while (first.getKind() == Tree.Kind.BLOCK) {
-            List<? extends StatementTree> trees = ((BlockTree)first).getStatements();
-            if (trees.isEmpty())
-                return null;
-            else
-                first = trees.iterator().next();
-        }
-        return first;
-    }
-
+    /**
+     * Case 4: get() is preceded with explicit assertion
+     */
     private boolean checkForIfExceptions(Element key, VariableElement map, TreePath path) {
         StatementTree stmt = TreeUtils.enclosingOfClass(path, StatementTree.class);
         if (stmt == null)
@@ -207,6 +205,9 @@ import com.sun.source.util.TreePath;
         return false;
     }
 
+    /**
+     * Case 5: get() is preceded by put-if-abset pattern
+     */
     private boolean checkForIfThenPut(Element key, VariableElement map, TreePath path) {
         StatementTree stmt = TreeUtils.enclosingOfClass(path, StatementTree.class);
         if (stmt == null)
@@ -238,29 +239,23 @@ import com.sun.source.util.TreePath;
         return false;
     }
 
-    private boolean isKeyInMap(IdentifierTree tree, VariableElement map) {
-        TreePath path = factory.getPath(tree);
-        Element key = TreeUtils.elementFromUse(tree);
+    /**
+     * Checks for the supported patterns, and determines if we can
+     * infer that the queried key exists in the map
+     *
+     * @param keyTree  the argument passed to {@code Map.get()}
+     * @param map   the symbol of map
+     * @return  true if key is in the map
+     */
+    private boolean isKeyInMap(IdentifierTree keyTree, VariableElement map) {
+        TreePath path = factory.getPath(keyTree);
+        Element key = TreeUtils.elementFromUse(keyTree);
 
-        // Check for contains
-        if (checkForContains(key, map, path)) {
-            return true;
-        }
-
-        // Check for enhanced for loop
-        if (checkForEnhanced(key, map, path)) {
-            return true;
-        }
-
-        if (checkForAsserts(key, map, path)) {
-            return true;
-        }
-
-        if (checkForIfExceptions(key, map, path)) {
-            return true;
-        }
-
-        if (checkForIfThenPut(key, map, path)) {
+        if (checkForContains(key, map, path)
+            || checkForEnhanced(key, map, path)
+            || checkForAsserts(key, map, path)
+            || checkForIfExceptions(key, map, path)
+            || checkForIfThenPut(key, map, path)) {
             return true;
         }
 
@@ -297,15 +292,63 @@ import com.sun.source.util.TreePath;
         throw new RuntimeException("Shouldn't be here!");
     }
 
-    public void handle(MethodInvocationTree tree, AnnotatedExecutableType method) {
-        if (isMapGetInvocation(tree)) {
-            AnnotatedTypeMirror type = method.getReturnType();
-            type.clearAnnotations();
-            if (!isSuppressable((MethodInvocationTree)tree)) {
-                type.addAnnotation(factory.NULLABLE);
-            } else {
-                type.addAnnotation(factory.NONNULL);
+    private boolean isInvocationOfContains(Element key, VariableElement map, Tree tree) {
+        if (TreeUtils.skipParens(tree) instanceof MethodInvocationTree) {
+            MethodInvocationTree invok = (MethodInvocationTree)TreeUtils.skipParens(tree);
+            if (isMethod(invok, mapContains)) {
+                Element containsArgument = InternalUtils.symbol(invok.getArguments().get(0));
+                if (key.equals(containsArgument) && map.equals(getSite(invok)))
+                    return true;
             }
         }
+        return false;
+    }
+
+    private boolean isInvocationOfPut(Element key, VariableElement map, Tree tree) {
+        if (TreeUtils.skipParens(tree) instanceof MethodInvocationTree) {
+            MethodInvocationTree invok = (MethodInvocationTree)TreeUtils.skipParens(tree);
+            if (isMethod(invok, mapPut)) {
+                Element containsArgument = InternalUtils.symbol(invok.getArguments().get(0));
+                if (key.equals(containsArgument) && map.equals(getSite(invok)))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isNotContained(Element key, VariableElement map, ExpressionTree tree) {
+        tree = TreeUtils.skipParens(tree);
+        return (tree.getKind() == Tree.Kind.LOGICAL_COMPLEMENT
+                && isInvocationOfContains(key, map, ((UnaryTree)tree).getExpression()));
+    }
+
+    private StatementTree firstStatement(StatementTree tree) {
+        StatementTree first = tree;
+        while (first.getKind() == Tree.Kind.BLOCK) {
+            List<? extends StatementTree> trees = ((BlockTree)first).getStatements();
+            if (trees.isEmpty())
+                return null;
+            else
+                first = trees.iterator().next();
+        }
+        return first;
+    }
+
+    private boolean isCheckOfGet(Element key, VariableElement map, Tree tree) {
+        tree = TreeUtils.skipParens(tree);
+        if (tree.getKind() != Tree.Kind.NOT_EQUAL_TO
+            || ((BinaryTree)tree).getRightOperand().getKind() != Tree.Kind.NULL_LITERAL)
+            return false;
+
+        Tree right = TreeUtils.skipParens(((BinaryTree)tree).getLeftOperand());
+        if (right instanceof MethodInvocationTree) {
+            MethodInvocationTree invok = (MethodInvocationTree)right;
+            if (isMethod(invok, mapGet)) {
+                Element containsArgument = InternalUtils.symbol(invok.getArguments().get(0));
+                if (key.equals(containsArgument) && map.equals(getSite(invok)))
+                    return true;
+            }
+        }
+        return false;
     }
 }
