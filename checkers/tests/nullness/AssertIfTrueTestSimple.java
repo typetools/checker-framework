dca55e1d284f8ecde9e3195d9896614515ef576b
Michael Ernst <mernst@cs.washington.edu>
2010-01-15 14:49:18 -0800

    Add failing test

---
+++ checkers/tests/nullness/AssertIfTrueTestSimple.java
@@ -0,0 +1,23 @@
+import checkers.nullness.quals.*;
+
+public class AssertIfTrueTestSimple {
+
+  protected int /*@Nullable*/ [] values;
+
+  /*@AssertNonNullIfTrue("values")*/
+  /*@Pure*/
+  public boolean repNulled() {
+    return values == null;
+  }
+
+  public void addAll(AssertIfTrueTestSimple s) {
+    if (repNulled())
+      return;
+    @NonNull Object x = values;
+
+    if (s.repNulled()) {
+      @NonNull Object y = values;
+    }
+  }
+
+}
