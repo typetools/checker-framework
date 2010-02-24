3d5bfe82db760995b6dcb7e5b05b511d55e0bd63
notnoop <notnoop@localhost>
2010-02-24 13:31:03 -0500

    skip some failing javari tests for now

--- checkers/tests/javari/RoClass.java
+++ checkers/tests/javari/RoClass.java
@@ -1,5 +1,8 @@
 import checkers.javari.quals.*;
 
+/*
+ * @skip-test   -  return value of readonly constructors is readonly
+ */
 @ReadOnly class RoClass {
 
     public RoClass() @ReadOnly {} // readonly constructor
