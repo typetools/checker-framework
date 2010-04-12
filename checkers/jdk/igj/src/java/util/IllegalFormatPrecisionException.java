6b7520654efc54b2a4afd3d36b47e9f9cec8f072
notnoop <notnoop@localhost>
2010-04-12 07:39:23 -0700

    revert an accidental change

---
+++ checkers/jdk/igj/src/java/util/IllegalFormatPrecisionException.java
@@ -0,0 +1,8 @@
+package java.util;
+
+public class IllegalFormatPrecisionException extends IllegalFormatException {
+    private static final long serialVersionUID = 0L;
+  public IllegalFormatPrecisionException(int a1) { throw new RuntimeException("skeleton method"); }
+  public int getPrecision() { throw new RuntimeException("skeleton method"); }
+  public String getMessage() { throw new RuntimeException("skeleton method"); }
+}
---
+++ checkers/src/checkers/nullness/quals/Pure.java
@@ -0,0 +1,18 @@
+package checkers.nullness.quals;
+
+import java.lang.annotation.*;
+
+/**
+ * Indicates that if the method is a pure method, so calling it
+ * with the same parameters would yield the same results.
+ *
+ * The method should not have any visible side-effect, and
+ * non-visible side-effects (e.g. caching) are possible.
+ *
+ * TODO: Consider moving Pure to core quals instead of nullness.
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Pure {
+}
