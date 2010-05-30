fce6efbf979a05a79eaa3df39496da68f24bc3e6
Michael Ernst <mernst@cs.washington.edu>
2010-05-29 21:15:58 -0700

    Remove incorrect annotation, and document why.

--- checkers/jdk/nullness/src/java/io/File.java
+++ checkers/jdk/nullness/src/java/io/File.java
@@ -28,7 +28,9 @@ public class File implements Serializable, Comparable<File> {
   public boolean canRead() { throw new RuntimeException("skeleton method"); }
   public boolean canWrite() { throw new RuntimeException("skeleton method"); }
   public boolean exists() { throw new RuntimeException("skeleton method"); }
-  @AssertNonNullIfTrue({"list()","list(FilenameFilter)","listFiles()","listFiles(FilenameFilter)","listFiles(FileFilter)"})
+  // This @AssertNonNullIfTrue is not true, since the list methods also
+  // return true in the case of an IO error (instead of throwing IOException).
+  // @AssertNonNullIfTrue({"list()","list(FilenameFilter)","listFiles()","listFiles(FilenameFilter)","listFiles(FileFilter)"})
   public boolean isDirectory() { throw new RuntimeException("skeleton method"); }
   public boolean isFile() { throw new RuntimeException("skeleton method"); }
   public boolean isHidden() { throw new RuntimeException("skeleton method"); }
