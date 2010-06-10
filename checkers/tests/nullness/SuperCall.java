c8ca85ce423c13427037120c605ee294c7da0807
Michael Ernst <mernst@cs.washington.edu>
2010-06-10 10:00:00 -0700

    Boolean.FALSE and TRUE are interned.

---
+++ checkers/tests/nullness/SuperCall.java
@@ -0,0 +1,16 @@
+import checkers.nullness.quals.*;
+
+public class SuperCall {
+
+  public static class A {
+    public A(@NonNull Object arg) { }
+  }
+
+  public static class B extends A {
+    public B(@Nullable Object arg) {
+      //:: (type.incompatible)
+      super(arg);
+    }
+  }
+
+}
