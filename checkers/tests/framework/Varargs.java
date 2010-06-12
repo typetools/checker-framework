3e8e53b3a0b5093eb26ec5a7acf1868664e5eb04
notnoop <notnoop@localhost>
2010-06-12 12:58:45 -0400

    First cleaning up of expected test results

--- checkers/tests/framework/Varargs.java
+++ checkers/tests/framework/Varargs.java
@@ -5,19 +5,19 @@ public class Varargs {
     public void testVarargsInvocation() {
         @Odd String s = null;
         aVarargsMethod(s);
-        //:: (type.incompatible)
+        //:: (argument.type.incompatible)
         aVarargsMethod(s, "");
         aVarargsMethod(s, s);
 
         moreVarargs(new @Odd String[1]);
-        //:: (type.incompatible)
+        //:: (argument.type.incompatible)
         moreVarargs(new String @Odd [1]);
         moreVarargs(new @Odd String(), new @Odd String());
-        //:: (type.incompatible)
+        //:: (argument.type.incompatible)
         moreVarargs(new String(), new @Odd String());
-        //:: (type.incompatible)
+        //:: (argument.type.incompatible)
         moreVarargs(new String(),
-                    //:: (type.incompatible)
+                    //:: (argument.type.incompatible)
                 new String());
     }
 
