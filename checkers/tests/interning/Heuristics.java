814f3e5c72aa511673003965409b0231f5ac4278
Michael Ernst <mernst@cs.washington.edu>
2010-07-09 15:53:06 -0700

    Add (disabled) optimizeEqualsClient test

--- checkers/tests/interning/Heuristics.java
+++ checkers/tests/interning/Heuristics.java
@@ -14,9 +14,20 @@ public class Heuristics {
 
   @Override
   public boolean equals(Object o) {
-      // Using == is OK if it's the first statement in the equals method
-      // and it compares "this" against the argument.
-      if (this == o) return true;
-      return false;
+    // Using == is OK if it's the first statement in the equals method
+    // and it compares "this" against the argument.
+    if (this == o) return true;
+    return false;
+  }
+
+  public boolean optimizeEqualsClient(Object a, Object b) {
+    // Using == is OK if it's the left-hand side of an || whose right-hand
+    // side is a call to equals with the same arguments.
+    // Some people like to check reference equality themselves first.
+    // This is a premature optimization, but it exists nonetheless.
+
+    // TO DO: Remove this "//::" suppression, because the checker should not issue a warning.
+    //:: (not.interned)
+    return (a == b || a.equals(b));
   }
 }
