a541a304ccdd8e0a6f7944627f1a4273d1b478c8
Werner Dietl <wdietl@gmail.com>
2010-08-04 11:35:47 -0700

    Enable inference and more annotations for the Swing tests.

--- checkers/tests/src/tests/FenumSwingTest.java
+++ checkers/tests/src/tests/FenumSwingTest.java
@@ -8,7 +8,7 @@ import org.junit.runners.Parameterized.Parameters;
 public class FenumSwingTest extends ParameterizedCheckerTest {
 
     public FenumSwingTest(File testFile) {
-        super(testFile, "checkers.fenum.FenumChecker", "fenum", "-Anomsgtext", "-Aquals=checkers.fenum.quals.SwingBoxOrientation");
+        super(testFile, "checkers.fenum.FenumChecker", "fenum", "-Anomsgtext", "-Alint=flowinference", "-Aquals=checkers.fenum.quals.SwingVerticalOrientation,checkers.fenum.quals.SwingHorizontalOrientation,checkers.fenum.quals.SwingBoxOrientation,checkers.fenum.quals.SwingCompassDirection,checkers.fenum.quals.SwingElementOrientation,checkers.fenum.quals.SwingTextOrientation");
         // TODO: check all qualifiers
     }
 
