3677b975b80e9e9ed55798d0a099a1e0b11a5b3b
Werner Dietl <wdietl@gmail.com>
2010-08-29 15:46:17 -0700

    Getting it to really skip.

--- checkers/tests/fenumswing/IdentityArrayList.java
+++ checkers/tests/fenumswing/IdentityArrayList.java
@@ -1,11 +1,12 @@
 import java.util.Arrays;
 
 /**
- * This test case violates an assertion in the compiler.
- * It does not depend on the fenum checker, it breaks for any checker.
- * 
  * @skip-test
  */
+/*
+* This test case violates an assertion in the compiler.
+* It does not depend on the fenum checker, it breaks for any checker.
+*/
 public class IdentityArrayList {
     public <T> T[] toArray(T[] a) {
     	return (T[]) Arrays.copyOf(null, 0, a.getClass());
