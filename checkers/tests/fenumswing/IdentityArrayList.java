28aa376553a2808628cfae5bc34bec9dc6d0fb10
Werner Dietl <wdietl@gmail.com>
2010-08-29 15:41:19 -0700

    Updated documentation.

--- checkers/tests/fenumswing/IdentityArrayList.java
+++ checkers/tests/fenumswing/IdentityArrayList.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
 
 /**
+ * This test case violates an assertion in the compiler.
+ * It does not depend on the fenum checker, it breaks for any checker.
+ * 
  * @skip-test
  */
 public class IdentityArrayList {
