ae91f76632304028833bb76566541edcccc29ebe
Michael Ernst <mernst@cs.washington.edu>
2010-08-30 14:30:40 -0700

    Add commented failing test case for @NonNullOnEntry

--- checkers/tests/nullness/StaticFields.java
+++ checkers/tests/nullness/StaticFields.java
@@ -36,4 +36,23 @@ class StaticFields {
         otherNullable.toString();
     }
 
+
+    @Nullable Object field1;
+    @Nullable Object field2;
+ 
+    @NonNullOnEntry("field1")
+        void method1() {
+        field1.toString();        // OK, field1 is known to be non-null
+        //:: (dereference.of.nullable)
+        field2.toString();        // error, might throw NullPointerException
+   }
+ 
+    void method2() {
+        field1 = new Object();
+        method1();                // OK, satisfies method precondition
+        field1 = null;
+        // XXX TODO FIXME: //:: precondition.not.satisfied
+        method1();                // error, does not satisfy method precondition
+    }
+
 }
