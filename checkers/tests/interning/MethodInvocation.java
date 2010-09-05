577346bade4533930ebf65327bd1e9107fbc89b0
Werner Dietl <wdietl@gmail.com>
2010-09-05 16:46:21 -0700

    Merge with 3c4753dcfeee03be661a7b3b8d9a440bbc7c04d9

--- checkers/tests/interning/MethodInvocation.java
+++ checkers/tests/interning/MethodInvocation.java
@@ -35,19 +35,20 @@ public class MethodInvocation {
         nonInterned.internedMethod();   // should emit error
     }
 
-  // Now, test method parameters
-  void internedCharacterParameter(@Interned Character a) {
-  }
+    // Now, test method parameters
+    void internedCharacterParameter(@Interned Character a) {
+    }
 
-  void internedCharacterParametersClient() {
-    // TODO: auto-boxing from char to Character //:: (argument.type.incompatible)
-    internedCharacterParameter('ä');
-    // TODO: auto-boxing from char to Character //:: (argument.type.incompatible)
-    internedCharacterParameter('a');
-    //:: (argument.type.incompatible)
-    internedCharacterParameter(new Character('a'));
-    //:: (argument.type.incompatible)
-    internedCharacterParameter(Character.valueOf('a'));
-  }
+    // See http://code.google.com/p/checker-framework/issues/detail?id=84
+    void internedCharacterParametersClient() {
+        // TODO: auto-boxing from char to Character //:: (argument.type.incompatible)
+        internedCharacterParameter('\u00E4'); // lowercase a with umlaut
+        // TODO: auto-boxing from char to Character //:: (argument.type.incompatible)
+        internedCharacterParameter('a');
+        //:: (argument.type.incompatible)
+        internedCharacterParameter(new Character('a'));
+        //:: (argument.type.incompatible)
+        internedCharacterParameter(Character.valueOf('a'));
+    }
 
 }
