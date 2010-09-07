48db93fa475ff444402ab6700b5bc36e2c4f7a78
Steph Dietzel <sdietzel@cs.washington.edu>
2010-09-07 10:35:29 -0700

    Automated merge with https://checker-framework.googlecode.com/hg/

--- checkers/tests/interning/MethodInvocation.java
+++ checkers/tests/interning/MethodInvocation.java
@@ -35,4 +35,20 @@ public class MethodInvocation {
         nonInterned.internedMethod();   // should emit error
     }
 
+    // Now, test method parameters
+    void internedCharacterParameter(@Interned Character a) {
+    }
+
+    // See http://code.google.com/p/checker-framework/issues/detail?id=84
+    void internedCharacterParametersClient() {
+        // TODO: auto-boxing from char to Character //:: (argument.type.incompatible)
+        internedCharacterParameter('\u00E4'); // lowercase a with umlaut
+        // TODO: auto-boxing from char to Character //:: (argument.type.incompatible)
+        internedCharacterParameter('a');
+        //:: (argument.type.incompatible)
+        internedCharacterParameter(new Character('a'));
+        //:: (argument.type.incompatible)
+        internedCharacterParameter(Character.valueOf('a'));
+    }
+
 }
