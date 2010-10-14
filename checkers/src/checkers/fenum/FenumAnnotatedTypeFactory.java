71ff8525007ffdd814442d490a7a0383eb05c667
Michael Ernst <mernst@cs.washington.edu>
2010-10-13 21:05:52 -0700

    Convert tabs to spaces

--- checkers/src/checkers/fenum/FenumAnnotatedTypeFactory.java
+++ checkers/src/checkers/fenum/FenumAnnotatedTypeFactory.java
@@ -6,15 +6,15 @@ import checkers.types.BasicAnnotatedTypeFactory;
 
 
 public class FenumAnnotatedTypeFactory extends
-		BasicAnnotatedTypeFactory<FenumChecker> {
+        BasicAnnotatedTypeFactory<FenumChecker> {
 
-	public FenumAnnotatedTypeFactory(FenumChecker checker,
-			CompilationUnitTree root) {
-		// Disable flow checker, as it changes a field access that has the type
-		// @Fenum("A") into an access @Fenum, ignoring the arguments!
-		// This happens in BasicAnnotatedTypeFactory.annotateImplicit, where
-		// all annotations are removed and the inferred annotation is added.
-		// Inference apparently does not handle arguments yet.
-		super(checker, root, false);
-	}
+    public FenumAnnotatedTypeFactory(FenumChecker checker,
+            CompilationUnitTree root) {
+        // Disable flow checker, as it changes a field access that has the type
+        // @Fenum("A") into an access @Fenum, ignoring the arguments!
+        // This happens in BasicAnnotatedTypeFactory.annotateImplicit, where
+        // all annotations are removed and the inferred annotation is added.
+        // Inference apparently does not handle arguments yet.
+        super(checker, root, false);
+    }
 }
