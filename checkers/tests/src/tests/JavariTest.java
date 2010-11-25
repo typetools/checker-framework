3825d18094e4dee8dbb63b43bcdeb5de6b6a857e
Michael Ernst <mernst@cs.washington.edu>
2010-11-24 21:26:43 -0800

    Use -Anocheckjdk command-line argument when testing

--- checkers/tests/src/tests/JavariTest.java
+++ checkers/tests/src/tests/JavariTest.java
@@ -11,7 +11,7 @@ import java.util.*;
 public class JavariTest extends ParameterizedCheckerTest {
 
     public JavariTest(File testFile) {
-        super(testFile, "checkers.javari.JavariChecker", "javari", "-Anomsgtext");
+        super(testFile, "checkers.javari.JavariChecker", "javari", "-Anomsgtext", "-Anocheckjdk");
     }
 
     @Parameters
