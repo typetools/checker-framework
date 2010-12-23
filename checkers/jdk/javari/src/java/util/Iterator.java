2db662acb52aeaa66b46a519b16005a200a49d02
Michael Ernst <mernst@cs.washington.edu>
2010-12-23 15:02:50 -0800

    Reference from Iterator in the annotated JDK, to the manual section about it

--- checkers/jdk/javari/src/java/util/Iterator.java
+++ checkers/jdk/javari/src/java/util/Iterator.java
@@ -3,6 +3,8 @@ import checkers.javari.quals.*;
 
 public interface Iterator<E> {
     boolean hasNext() @ReadOnly;
+    // For a justification of this annotation, see section
+    // "Iterators and their abstract state" in the Checker Framework manual.
     E next() @ReadOnly;
     void remove();
 }
