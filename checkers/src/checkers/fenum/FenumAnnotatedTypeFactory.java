2d2ff796d334c4413d4795fa197b2084c7d12063
wdietl <devnull@localhost>
2010-12-27 16:18:08 -0800

    The type of a compound assignment is the LUB of the two types.

--- checkers/src/checkers/fenum/FenumAnnotatedTypeFactory.java
+++ checkers/src/checkers/fenum/FenumAnnotatedTypeFactory.java
@@ -1,17 +1,21 @@
 package checkers.fenum;
 
 import java.util.Collections;
+import java.util.Set;
 
 import javax.lang.model.element.AnnotationMirror;
-
 import com.sun.source.tree.CompilationUnitTree;
+import com.sun.source.tree.CompoundAssignmentTree;
+import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.Tree;
 
+import checkers.basetype.BaseTypeChecker;
 import checkers.fenum.quals.FenumTop;
 import checkers.fenum.quals.FenumUnqualified;
 import checkers.quals.DefaultLocation;
 import checkers.types.AnnotatedTypeMirror;
 import checkers.types.BasicAnnotatedTypeFactory;
+import checkers.types.TreeAnnotator;
 
 
 public class FenumAnnotatedTypeFactory extends
@@ -61,4 +65,36 @@ public class FenumAnnotatedTypeFactory extends
 			// completer.visit(type);
 		}
 	}
+    
+    @Override
+    protected TreeAnnotator createTreeAnnotator(FenumChecker checker) {
+        return new FenumTreeAnnotator(checker);
+    }
+
+    /**
+     * A class for adding annotations based on tree
+     */
+    private class FenumTreeAnnotator  extends TreeAnnotator {
+
+        FenumTreeAnnotator(BaseTypeChecker checker) {
+            super(checker, FenumAnnotatedTypeFactory.this);
+        }
+        
+        @Override
+        public Void visitCompoundAssignment(CompoundAssignmentTree node, AnnotatedTypeMirror type) {
+        	/*
+        	 * For some reason we did not get a default qualifier applied to the tree.
+        	 * The default qualifier would be the wrong qualifier in some cases, but better than nothing.
+        	 * The implementation below looks for the least upper bound of both sides and returns it.
+        	 */
+        	ExpressionTree var = node.getVariable();
+            ExpressionTree expr = node.getExpression();
+            AnnotatedTypeMirror varType = getAnnotatedType(var);
+            AnnotatedTypeMirror exprType = getAnnotatedType(expr);
+            Set<AnnotationMirror> lub = qualHierarchy.leastUpperBound(varType.getAnnotations(), exprType.getAnnotations());
+        	type.clearAnnotations();
+            type.addAnnotations(lub);
+        	return super.visitCompoundAssignment(node, type);
+        }
+    }
 }
