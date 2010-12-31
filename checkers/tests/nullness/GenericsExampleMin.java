daf4067e94c78a4b389ace5950c696416b6462a3
Werner Dietl <wdietl@gmail.com>
2010-12-31 02:10:29 -0800

    Change the KeyFor annotation into a type qualifier and add a new subchecker to ensure correct usage. Much work has to be spent on substituting the annotation value.

---
+++ checkers/tests/nullness/GenericsExampleMin.java
@@ -0,0 +1,31 @@
+import checkers.nullness.quals.*;
+
+// This is the example from manual section:
+// "Generics (parametric polymorphism or type polymorphism)"
+// whose source code is ../../manual/advanced-features.tex
+class GenericsExampleMin {
+
+  class MyList1<@Nullable T> {
+    T t;
+    @Nullable T nble;
+    @NonNull T nn;
+
+    T get(int i) { return t; }
+
+    // This method works.
+    // Note that it fails to work if it is moved after m2() in the syntax tree.
+    void m1() {
+        t = this.get(0);
+        nble = this.get(0);
+    }
+
+    // When the assignment to nn is added, the assignments to t and nble also fail, which is unexpected.
+    void m2() {
+    	//:: (assignment.type.incompatible)
+        nn = null;
+        t = this.get(0);
+        nble = this.get(0);
+    }
+    
+  }
+}
