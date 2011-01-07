9eb3e831ceba983b27cb9401a211dd7e108b4be1
Michael Ernst <mernst@cs.washington.edu>
2011-01-06 18:13:28 -0800

    Automated merge with ssh://barb.cs.washington.edu//homes/gws/wmdietl/research/eclipse-workspaces/types-plugins/checker-framework

--- checkers/tests/nullness/AssertAfter2.java
+++ checkers/tests/nullness/AssertAfter2.java
@@ -18,6 +18,74 @@ public class AssertAfter2 {
       @NonNull List<@KeyFor("childMap") T> l = childMap.get(parent);
     }
 
+    public void addEdgeBad1( T parent, T child ) {
+    	//:: (assignment.type.incompatible)
+        @NonNull List<@KeyFor("childMap") T> l = childMap.get(parent);
+    }
+
+    public void addEdgeBad2( T parent, T child ) {
+        addNode(parent);
+        //:: (assignment.type.incompatible)
+        @NonNull List<@KeyFor("childMap") T> l = childMap.get(child);
+    }
+
+    public void addEdgeBad3( T parent, T child ) {
+        addNode(parent);
+        parent = child;
+        //:: (assignment.type.incompatible)
+        @NonNull List<@KeyFor("childMap") T> l = childMap.get(parent);
+      }
+
+    public void addEdgeOK( T parent, T child ) {
+        List<@KeyFor("childMap") T> l = childMap.get(parent);
+    }
   }
 
+  class MultiParam {
+	  MultiParam thing;
+	  
+	  // TODO: doc: spaces important
+	  // TODO: doc: no explicit this!
+	  @AssertNonNullAfter("get(#0, #1, #2)")
+	  void add( Object o1, Object o2, Object o3 ) {
+		  // body omitted, not relevant to test case
+	  }
+
+	  @Nullable Object get( Object o1, Object o2, Object o3 ) {
+		  return null;
+	  }
+	  
+	  Object f1, f2, f3;
+	  
+	  void addGood1() {
+		  thing.add(f1, f2, f3);
+		  @NonNull Object nn = thing.get(f1, f2, f3);
+	  }
+	  
+	  void addBad1() {
+		  //:: (assignment.type.incompatible)
+		  @NonNull Object nn = get(f1, f2, f3);
+	  }
+	  
+	  void addBad2() {
+		  thing.add(f1, f2, f3);
+		  f1 = new Object();
+		  //:: (assignment.type.incompatible)
+		  @NonNull Object nn = thing.get(f1, f2, f3);
+	  }
+
+	  void addBad3() {
+		  thing.add(f1, f2, f3);
+		  f2 = new Object();
+		  //:: (assignment.type.incompatible)
+		  @NonNull Object nn = thing.get(f1, f2, f3);
+	  }
+	  
+	  void addBad4() {
+		  thing.add(f1, f2, f3);
+		  f3 = new Object();
+		  //:: (assignment.type.incompatible)
+		  @NonNull Object nn = thing.get(f1, f2, f3);
+	  }
+  }
 }
