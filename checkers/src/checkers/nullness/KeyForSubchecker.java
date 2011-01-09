86150a810abb035efae20dee2992d9532de6b529
wdietl <wdietl@localhost>
2011-01-08 20:45:26 -0800

    Use the Covariant annotation on type parameters.

--- checkers/src/checkers/nullness/KeyForSubchecker.java
+++ checkers/src/checkers/nullness/KeyForSubchecker.java
@@ -2,6 +2,8 @@ package checkers.nullness;
 
 import java.util.List;
 
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.TypeKind;
 
 import checkers.basetype.BaseTypeChecker;
@@ -16,7 +18,7 @@ import checkers.types.TypeHierarchy;
 /**
  * TODO: doc
  */
-@TypeQualifiers({ KeyFor.class, Unqualified.class, KeyForBottom.class })
+@TypeQualifiers({ KeyFor.class, Unqualified.class, KeyForBottom.class, Covariant.class })
 public class KeyForSubchecker extends BaseTypeChecker {
     protected TypeHierarchy createTypeHierarchy() {
         return new KeyForTypeHierarchy(getQualifierHierarchy());
@@ -31,6 +33,10 @@ public class KeyForSubchecker extends BaseTypeChecker {
 			if (lhs.getAnnotations().isEmpty()) {
 				return true;
 			}
+		}
+		// Otherwise Covariant would cause trouble.
+		if (rhs.getAnnotation(KeyForBottom.class)!=null) {
+			return true;
 		}
 		return super.isSubtype(rhs, lhs);
     }
@@ -49,11 +55,19 @@ public class KeyForSubchecker extends BaseTypeChecker {
 	        if (rhsTypeArgs.isEmpty() || lhsTypeArgs.isEmpty())
 	            return true;
 
-	        boolean kfcovar = rhs.getUnderlyingType().asElement().getAnnotation(KeyForCovariant.class) != null;
+	        TypeElement lhsElem = (TypeElement) lhs.getUnderlyingType().asElement();
+	        TypeElement rhsElem = (TypeElement) lhs.getUnderlyingType().asElement();
+	        AnnotatedDeclaredType lhsDecl = currentATF.fromElement(lhsElem);
+	        AnnotatedDeclaredType rhsDecl = currentATF.fromElement(rhsElem);
+	        List<AnnotatedTypeMirror> lhsTVs = lhsDecl.getTypeArguments();
+	        List<AnnotatedTypeMirror> rhsTVs = rhsDecl.getTypeArguments();
 	        
 	        assert lhsTypeArgs.size() == rhsTypeArgs.size();
 	        for (int i = 0; i < lhsTypeArgs.size(); ++i) {
-	        	if (kfcovar) {
+		        boolean covar = lhsTVs.get(i).getAnnotation(Covariant.class) != null ||
+		        	rhsTVs.get(i).getAnnotation(Covariant.class) != null;
+
+	        	if (covar) {
 	        		if (!KeyForSubchecker.this.isSubtype(rhsTypeArgs.get(i), lhsTypeArgs.get(i)))
 	        			return false;
 	        	} else {
