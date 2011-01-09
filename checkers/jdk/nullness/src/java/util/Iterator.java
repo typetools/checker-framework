f69ac992f6469f7b03a6c0cf1307f1c096e499d8
Michael Ernst <mernst@cs.washington.edu>
2011-01-08 22:01:29 -0800

    @Covariant would be illegal on ListIterator even though it's fine on Iterator.

--- checkers/jdk/nullness/src/java/util/Iterator.java
+++ checkers/jdk/nullness/src/java/util/Iterator.java
@@ -3,6 +3,8 @@ package java.util;
 import checkers.nullness.quals.*;
 
 @checkers.quals.DefaultQualifier("checkers.nullness.quals.NonNull")
+// This @Covariant annotation is sound, but it would not be sound on
+// ListIterator (a subclass of Iterator), which supports a set operation.
 public interface Iterator<@Covariant E extends @Nullable Object> {
   public abstract boolean hasNext();
   public abstract E next();
