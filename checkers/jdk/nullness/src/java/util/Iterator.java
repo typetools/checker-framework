47da7d5e1872918876a7609d985bcdd72af31b0d
wdietl <devnull@localhost>
2011-01-08 20:41:53 -0800

    Change KeyForCovariant into Covariant and move it to the type parameter.

--- checkers/jdk/nullness/src/java/util/Iterator.java
+++ checkers/jdk/nullness/src/java/util/Iterator.java
@@ -3,8 +3,7 @@ package java.util;
 import checkers.nullness.quals.*;
 
 @checkers.quals.DefaultQualifier("checkers.nullness.quals.NonNull")
-@KeyForCovariant
-public interface Iterator<E extends @Nullable Object> {
+public interface Iterator<@Covariant E extends @Nullable Object> {
   public abstract boolean hasNext();
   public abstract E next();
   public abstract void remove();
