b054982968845ed6cd4947264c171970a2129730
wdietl <devnull@localhost>
2011-01-11 14:21:34 -0800

    Change into a class annotation.

--- checkers/src/checkers/nullness/quals/Covariant.java
+++ checkers/src/checkers/nullness/quals/Covariant.java
@@ -2,10 +2,6 @@ package checkers.nullness.quals;
 
 import java.lang.annotation.*;
 
-import checkers.quals.MarkerQualifier;
-import checkers.quals.SubtypeOf;
-import checkers.quals.TypeQualifier;
-import checkers.quals.Unqualified;
 
 /**
  * TODO: doc.
@@ -13,18 +9,15 @@ import checkers.quals.Unqualified;
  * type variable can safely ignore KeyFor annotations.
  * The prime example is Map.Entry<K>.
  * It is not checked whether the annotated class is immutable.
+ * The value is the set of zero-based indices of the type parameters that should be covariant.
  * 
  * TODO: move to a different package?
  *
  */
 
 @Documented
-// TODO: I had a special case in BaseTypeChecker, similar to PolymorphicQualifier, but that didn't work :-(
-@MarkerQualifier
-// TODO: Who checks this annotation??? I don't find it's uses :-(
-@TypeQualifier
-@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
-// TODO: Without this, the checks fail, without any helpful output :-((
-@SubtypeOf( Unqualified.class )
+@Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Covariant {}
+public @interface Covariant {
+	int[] value();
+}
