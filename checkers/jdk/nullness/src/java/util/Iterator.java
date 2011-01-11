54c89d548762823d375e136560ee4c1819d270fa
wdietl <wdietl@localhost>
2011-01-11 14:16:46 -0800

    Use new style of @Covariant annotation.

--- checkers/jdk/nullness/src/java/util/Iterator.java
+++ checkers/jdk/nullness/src/java/util/Iterator.java
@@ -5,7 +5,7 @@ import checkers.nullness.quals.*;
 @checkers.quals.DefaultQualifier("checkers.nullness.quals.NonNull")
 // This @Covariant annotation is sound, but it would not be sound on
 // ListIterator (a subclass of Iterator), which supports a set operation.
-// public interface Iterator<@Covariant E extends @Nullable Object> {
+@Covariant(0)
 public interface Iterator<E extends @Nullable Object> {
   public abstract boolean hasNext();
   public abstract E next();
