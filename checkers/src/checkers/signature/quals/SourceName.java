6c9e461ec2d5c0a7d4f51ae87a35c6f127d34502
Kivanc Muslu <kivanc@cs.washington.edu>
2011-01-13 17:20:59 -0800

    Added documentation for signature quals. Removed unused quals in Signature Checker.

--- checkers/src/checkers/signature/quals/SourceName.java
+++ checkers/src/checkers/signature/quals/SourceName.java
@@ -3,8 +3,21 @@ package checkers.signature.quals;
 import checkers.quals.SubtypeOf;
 import checkers.quals.TypeQualifier;
 
+/**
+ * Since binary names (@BinaryName) and fully qualified names (@FullyQualifiedName) differ only
+ * for inner classes (same for top level classes), checker framework cannot deduce if a fully
+ * qualified name will be used as a fully qualified name or a binary name in the future. 
+ * This is true since all fully qualified names are also valid binary names, where as the other
+ * way around is not true.
+ * Therefore source name (@SourceName) is an annotation that is only to be used by the checker
+ * framework visitors when a fully qualified name (as String literal) is detected. This way 
+ * that name is also permitted to be used a binary name (since it is valid).
+ * Not to be used by the annotator, only used internally.
+ * @author Kivanc Muslu
+ */
 @TypeQualifier
 @SubtypeOf({BinaryName.class,
             FullyQualifiedName.class
            })
+@Target( {} )
 public @interface SourceName {}
