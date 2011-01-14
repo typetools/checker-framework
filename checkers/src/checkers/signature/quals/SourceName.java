1103d437bbbd463d954f5539399f13416b696fb1
Michael Ernst <mernst@cs.washington.edu>
2011-01-13 20:12:47 -0800

    Programmers should not write SourceName

--- checkers/src/checkers/signature/quals/SourceName.java
+++ checkers/src/checkers/signature/quals/SourceName.java
@@ -4,7 +4,6 @@ import checkers.quals.SubtypeOf;
 import checkers.quals.TypeQualifier;
 
 @TypeQualifier
-@SubtypeOf({BinaryName.class,
-            FullyQualifiedName.class
-           })
+@SubtypeOf({BinaryName.class, FullyQualifiedName.class})
+@Target({}) // empty target prevents programmers from writing this in a program
 public @interface SourceName {}
