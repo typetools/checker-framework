67eff13f6e7dd7f86dcfd990cfe6eb1a4e492328
notnoop <devnull@localhost>
2011-01-17 19:45:20 -0500

    enable uninit field testing by default and run uninit tests

--- checkers/tests/src/tests/NullnessTest.java
+++ checkers/tests/src/tests/NullnessTest.java
@@ -11,7 +11,7 @@ import org.junit.runners.Parameterized.Parameters;
 public class NullnessTest extends ParameterizedCheckerTest {
 
     public NullnessTest(File testFile) {
-        super(testFile, "checkers.nullness.NullnessChecker", "nullness", "-Anomsgtext", "-Anocheckjdk");
+        super(testFile, "checkers.nullness.NullnessChecker", "nullness", "-Anomsgtext", "-Anocheckjdk", "-Alint=-uninitialized");
     }
 
     @Parameters
