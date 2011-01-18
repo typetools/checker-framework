4d230d23d94bab5f3ed6cfa39118f6379479cfc8
notnoop <devnull@localhost>
2011-01-18 12:13:49 -0500

    fix the checkNonNull and super fields check

--- checkers/tests/nullness-uninit/Uninit6.java
+++ checkers/tests/nullness-uninit/Uninit6.java
@@ -5,6 +5,6 @@ public class Uninit6 {
   // guarantee of no null pointer errors.
   @LazyNonNull Object f;
   @Nullable Object g;
-  int i;
+  // int i;  // TODO: Discuss the implication of primitive field initialization
   Uninit6() { }
 }
