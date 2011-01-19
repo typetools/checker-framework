abbf4f73fca41b8413abdddd5d2796e7dc866514
Michael Ernst <mernst@cs.washington.edu>
2011-01-18 16:38:47 -0800

    Improve error message

---
+++ checkers/jdk/javari/src/java/lang/CloneNotSupportedException.java
@@ -0,0 +1,13 @@
+package java.lang;
+import checkers.javari.quals.*;
+
+public class CloneNotSupportedException extends Exception {
+    private static final long serialVersionUID = 0L;
+    public CloneNotSupportedException() {
+        throw new RuntimeException("skeleton method");
+    }
+
+    public CloneNotSupportedException(String s) {
+        throw new RuntimeException("skeleton method");
+    }
+}
