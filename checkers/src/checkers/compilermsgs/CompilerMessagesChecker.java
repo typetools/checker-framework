5c15571c8498c6214317c865c2099996408bc5b2
wdietl <devnull@localhost>
2011-01-27 01:11:15 -0800

    Also support the inherited Bottom qualifier.

--- checkers/src/checkers/compilermsgs/CompilerMessagesChecker.java
+++ checkers/src/checkers/compilermsgs/CompilerMessagesChecker.java
@@ -4,6 +4,7 @@ import javax.annotation.processing.SupportedOptions;
 
 import checkers.compilermsgs.quals.CompilerMessageKey;
 import checkers.propkey.PropertyKeyChecker;
+import checkers.propkey.quals.Bottom;
 import checkers.propkey.quals.PropertyKey;
 import checkers.quals.TypeQualifiers;
 import checkers.quals.Unqualified;
@@ -14,7 +15,7 @@ import checkers.quals.Unqualified;
  *
  * @author wmdietl
  */
-@TypeQualifiers( {CompilerMessageKey.class, PropertyKey.class, Unqualified.class} )
+@TypeQualifiers( {CompilerMessageKey.class, PropertyKey.class, Unqualified.class, Bottom.class} )
 @SupportedOptions( {"propfiles", "bundlenames"} )
 public class CompilerMessagesChecker extends PropertyKeyChecker {
 }
