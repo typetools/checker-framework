2bd8581e1748d26effee0d3ec07d92ccc21fa6a9
Michael Ernst <mernst@cs.washington.edu>
2011-02-24 16:32:22 -0800

    Add failing KeyFor test

---
+++ checkers/tests/nullness/KeyFor_DirectionsFinder.java
@@ -0,0 +1,38 @@
+import checkers.nullness.quals.*;
+
+import java.util.*;
+
+class GeoPoint {}
+
+class StreetSegment {}
+
+public class KeyFor_DirectionsFinder {
+
+    public static void buildGraph(List<StreetSegment> segs) {
+        Map<GeoPoint, Set<StreetSegment>> endMap = new 
+            HashMap<GeoPoint, Set<StreetSegment>>();
+        Map</*@KeyFor("endMap")*/GeoPoint, Set<StreetSegment>> beginMap = new 
+            HashMap</*@KeyFor("endMap")*/GeoPoint, Set<StreetSegment>>();
+
+        for(StreetSegment seg : segs) {
+            GeoPoint p1 = new GeoPoint();
+
+            if (!(beginMap.containsKey(p1))) {
+                endMap.put(p1,new HashSet<StreetSegment>());
+                beginMap.put(p1,new HashSet<StreetSegment>());
+            }
+            endMap.get(p1).add(seg);
+            beginMap.get(p1).add(seg);
+        }
+
+        for (/*@KeyFor("endMap")*/ GeoPoint p : beginMap.keySet()) {
+            for (StreetSegment beginSeg : beginMap.get(p)) {
+                for (StreetSegment endSeg : endMap.get(p)) {
+                    // graph.addEdge(endSeg,beginSeg);
+                }
+            }
+        }
+
+    }
+
+}
