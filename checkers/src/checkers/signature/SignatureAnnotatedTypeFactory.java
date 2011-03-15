9fe4d91c93c2e73f1f4f34b39c0978ba73b1ff68
wdietl <wdietl@localhost>
2011-03-14 17:54:57 -0700

    Simplify the Signature Checker by using the ImplicitFor.stringPatterns annotation that is provided by the framework. The only change to the framework is to use the greatest lower bound if multiple patterns match. This should be the generally desirable behavior, but could be adjusted easily. I left the SignatureAnnotatedTypeFactory in, but it is basically empty. I kept it to make future work simpler.

--- checkers/src/checkers/signature/SignatureAnnotatedTypeFactory.java
+++ checkers/src/checkers/signature/SignatureAnnotatedTypeFactory.java
@@ -1,24 +1,21 @@
 package checkers.signature;
 
-import checkers.signature.quals.*;
+import com.sun.source.tree.CompilationUnitTree;
 
-import java.lang.annotation.Annotation;
-import java.util.regex.*;
+import checkers.types.BasicAnnotatedTypeFactory;
 
-import com.sun.source.tree.*;
-
-import checkers.basetype.BaseTypeChecker;
-import checkers.types.*;
 
 // This code is copied from SignatureAnnotatedTypeFactory.
 // The two could be generalized and combined, perhaps.
+// TODO: the above comment is self-referential. What should it mean?
 
 // TODO: Does not yet handle method signature annotations, such as
 // @MethodDescriptor.
 
+
 /**
- * Adds a signature annotation to the type of every tree that is a {@code
- * String} literal.  The particular annotation depends on the literal.
+ * This class is currently not needed, as recent refactorings made the String pattern matching
+ * obsolete. I left it in to make future extension easier.
  */
 public class SignatureAnnotatedTypeFactory extends BasicAnnotatedTypeFactory<SignatureChecker> {
 
@@ -27,6 +24,7 @@ public class SignatureAnnotatedTypeFactory extends BasicAnnotatedTypeFactory<Sig
         super(checker, root);
     }
 
+    /*
     @Override
     public TreeAnnotator createTreeAnnotator(SignatureChecker checker) {
         return new SignatureTreeAnnotator(checker);
@@ -38,44 +36,6 @@ public class SignatureAnnotatedTypeFactory extends BasicAnnotatedTypeFactory<Sig
             super(checker, SignatureAnnotatedTypeFactory.this);
         }
 
-        /**
-         * Case 1: String literal
-         */
-        @Override
-        public Void visitLiteral(LiteralTree tree, AnnotatedTypeMirror type) {
-            if (!type.isAnnotated()
-                && tree.getKind() == Tree.Kind.STRING_LITERAL) {
-                String s = (String)(tree).getValue();
-                type.addAnnotation(bestSignatureAnnotation(s));
-            }
-            return super.visitLiteral(tree, type);
-        }
-
     }
-
-    private static final Pattern fqnPat = Pattern.compile("^[A-Za-z_][A-Za-z_0-9]*(\\.[A-Za-z_][A-Za-z_0-9]*)*(\\[\\])*$");
-    private static final Pattern bnPat = Pattern.compile("^[A-Za-z_][A-Za-z_0-9]*(\\.[A-Za-z_][A-Za-z_0-9]*)*(\\$[A-Za-z_][A-Za-z_0-9]*)?(\\[\\])*$");
-    private static final Pattern fdPat = Pattern.compile("^\\[*([BCDFIJSZ]|L[A-Za-z_][A-Za-z_0-9]*(/[A-Za-z_][A-Za-z_0-9]*)*(\\$[A-Za-z_][A-Za-z_0-9]*)?;)$");
-
-    private static Class<? extends Annotation> bestSignatureAnnotation(String s) {
-        if (fdPat.matcher(s).matches()) {
-            if (bnPat.matcher(s).matches()) {
-                return SignatureBottom.class;
-            } else {
-                return FieldDescriptor.class;
-            }
-        } else {
-            if (bnPat.matcher(s).matches()) {
-                if (fqnPat.matcher(s).matches()) {
-                    // Both a BinaryName and a FullyQualifiedName
-                    return SourceName.class;
-                } else {
-                    return BinaryName.class;
-                }
-            } else {
-                return UnannotatedString.class;
-            }
-        }
-    }
-
+    */
 }
