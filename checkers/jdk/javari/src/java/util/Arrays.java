0383bdc8d473b7866f8c145fd65a0db14156220f
Michael Ernst <mernst@cs.washington.edu>
2011-03-26 02:25:12 -0700

    Suppress a warning

--- checkers/jdk/javari/src/java/util/Arrays.java
+++ checkers/jdk/javari/src/java/util/Arrays.java
@@ -90,7 +90,7 @@ public class Arrays {
     public static double[] copyOfRange(double @ReadOnly [] original, int from, int to) { throw new RuntimeException("skeleton method"); }
     public static boolean[] copyOfRange(boolean @ReadOnly [] original, int from, int to) { throw new RuntimeException("skeleton method"); }
     // In JDK7, should instead be: @SafeVarargs
-    @SuppressWarnings("varargs")
+    @SuppressWarnings({"varargs","unchecked"})
     public static <T> List<T> asList(T... a) { throw new RuntimeException("skeleton method"); }
     public static int hashCode(@ReadOnly long a[]) { throw new RuntimeException("skeleton method"); }
     public static int hashCode(@ReadOnly int a[]) { throw new RuntimeException("skeleton method"); }
