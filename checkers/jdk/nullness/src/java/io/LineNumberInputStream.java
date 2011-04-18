f3f35c766128343ecf57712cd5df8400f9fef971
wdietl <wdietl@gmail.com>
2011-04-18 10:48:06 -0700

    FilterInputStream takes an InputStream as constructor parameter. Add it and adapt the constructors of subclasses.

--- checkers/jdk/nullness/src/java/io/LineNumberInputStream.java
+++ checkers/jdk/nullness/src/java/io/LineNumberInputStream.java
@@ -6,7 +6,7 @@ import checkers.nullness.quals.*;
 
 @Deprecated
 public class LineNumberInputStream extends FilterInputStream {
-  public LineNumberInputStream(InputStream a1) { throw new RuntimeException("skeleton method"); }
+  public LineNumberInputStream(InputStream a1) { super(a1); throw new RuntimeException("skeleton method"); }
   public int read() throws IOException { throw new RuntimeException("skeleton method"); }
   public int read(byte[] a1, int a2, int a3) throws IOException { throw new RuntimeException("skeleton method"); }
   public long skip(long a1) throws IOException { throw new RuntimeException("skeleton method"); }
