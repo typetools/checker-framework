2ef47220820f896aab9cc2d36a1c7b17ac0cfe89
wdietl <wdietl@gmail.com>
2011-05-20 20:45:31 -0700

    Two small fixes to make the Checker Framework work on both Java 6 and Java 7. We need to investigate some more how to support both APIs at the same time.

--- checkers/src/checkers/util/stub/StubParser.java
+++ checkers/src/checkers/util/stub/StubParser.java
@@ -181,18 +181,35 @@ public class StubParser {
             //     throw new Error(String.format("parseType (%s, %s): inconsistent nullness for args and params%n  args = %s%n  params = %s%n", decl, elt, typeArguments, typeParameters));
             // }
             if ((typeParameters == null) && (typeArguments.size() != 0)) {
-                System.out.printf("Dying.  theCompilationUnit=%s%n", theCompilationUnit);
+                // TODO: Class EventListenerProxy in Java 6 does not have type parameters, but in Java 7 does.
+                // To handle both with one specification, we currently ignore the problem.
+                // Investigate what a cleaner solution is, e.g. having a separate Java 7 specification that overrides
+                // the Java 6 specification.
+                // System.out.printf("Dying.  theCompilationUnit=%s%n", theCompilationUnit);
+                System.out.printf(String.format("parseType:  mismatched sizes for params and args%n  decl=%s%n  typeParameters=%s%n  elt=%s (%s)%n  type=%s (%s)%n  typeArguments (size %d)=%s%n  theCompilationUnit=%s%n",
+                                              decl, typeParameters,
+                                              elt, elt.getClass(), type, type.getClass(), typeArguments.size(), typeArguments,
+                                              theCompilationUnit));
                 System.out.flush();
+                /*
                 throw new Error(String.format("parseType:  mismatched sizes for params and args%n  decl=%s%n  typeParameters=%s%n  elt=%s (%s)%n  type=%s (%s)%n  typeArguments (size %d)=%s%n",
                                               decl, typeParameters,
                                               elt, elt.getClass(), type, type.getClass(), typeArguments.size(), typeArguments));
+                 */
             }
             if ((typeParameters != null) && (typeParameters.size() != typeArguments.size())) {
-                System.out.printf("Dying.  theCompilationUnit=%s%n", theCompilationUnit);
+                // TODO: decide how severe this problem really is; see comment above.
+                // System.out.printf("Dying.  theCompilationUnit=%s%n", theCompilationUnit);
+                System.out.printf(String.format("parseType:  mismatched sizes for params and args%n  decl=%s%n  typeParameters (size %d)=%s%n  elt=%s (%s)%n  type=%s (%s)%n  typeArguments (size %d)=%s%n  theCompilationUnit=%s%n",
+                                              decl, typeParameters.size(), typeParameters,
+                                              elt, elt.getClass(), type, type.getClass(), typeArguments.size(), typeArguments,
+                                              theCompilationUnit));
                 System.out.flush();
+                /*
                 throw new Error(String.format("parseType:  mismatched sizes for params and args%n  decl=%s%n  typeParameters (size %d)=%s%n  elt=%s (%s)%n  type=%s (%s)%n  typeArguments (size %d)=%s%n",
                                               decl, typeParameters.size(), typeParameters,
                                               elt, elt.getClass(), type, type.getClass(), typeArguments.size(), typeArguments));
+                */
             }
         }
         annotateParameters(type.getTypeArguments(), decl.getTypeParameters());
