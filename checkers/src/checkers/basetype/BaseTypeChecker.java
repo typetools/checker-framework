d550b0dfff794bf31100558cd55a2b220203e255
wdietl <wdietl@gmail.com>
2011-06-13 18:00:47 -0700

    Documentation fixes.

--- checkers/src/checkers/basetype/BaseTypeChecker.java
+++ checkers/src/checkers/basetype/BaseTypeChecker.java
@@ -41,7 +41,7 @@ import javax.annotation.processing.*;
  * It is a convention that, for a type system Foo, the checker, the visitor,
  * and the annotated type factory are named as  <i>FooChecker</i>,
  * <i>FooVisitor</i>, and <i>FooAnnotatedTypeFactory</i>.  Some factory
- * methods uses this convention to construct the appriopriate classes
+ * methods uses this convention to construct the appropriate classes
  * reflectively.
  *
  * <p>
@@ -79,13 +79,13 @@ import javax.annotation.processing.*;
  */
 public abstract class BaseTypeChecker extends SourceChecker {
 
-    /** To cache the supported type qualifiers */
+    /** To cache the supported type qualifiers. */
     private Set<Class<? extends Annotation>> supportedQuals;
 
-    /** To represent the supported qualifiers and their hierarchy */
+    /** To represent the supported qualifiers and their hierarchy. */
     private QualifierHierarchy qualHierarchy;
 
-    /** To compare annotated types with respect to qualHierarchy */
+    /** To compare annotated types with respect to qualHierarchy. */
     private TypeHierarchy typeHierarchy;
 
     @Override
