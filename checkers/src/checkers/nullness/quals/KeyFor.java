aebfeabef017491e0d587a2ec0e4925b55b76fd2
Michael Ernst <mernst@cs.washington.edu>
2011-06-15 21:14:12 -0700

    Update links to Checker Framework manual

--- checkers/src/checkers/nullness/quals/KeyFor.java
+++ checkers/src/checkers/nullness/quals/KeyFor.java
@@ -35,7 +35,7 @@ import checkers.quals.*;
 public @interface KeyFor {
     /**
      * Java expression(s) that evaluate to a map for which the annotated type is a key.
-     * @see <a href="http://types.cs.washington.edu/checker-framework/#java-expressions-as-arguments">Syntax of Java expressions</a>
+     * @see <a href="http://types.cs.washington.edu/checker-framework/current/checkers-manual.html#java-expressions-as-arguments">Syntax of Java expressions</a>
      */
     public String[] value();
 }
