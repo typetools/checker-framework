e6f76dbeb2ec86d5c0222a135c801674612439cd
Michael Ernst <mernst@cs.washington.edu>
2011-07-12 21:47:26 -0700

    Special-case another variant of Closeable

--- checkers/src/checkers/util/stub/StubParser.java
+++ checkers/src/checkers/util/stub/StubParser.java
@@ -250,7 +250,7 @@ public class StubParser {
                 // TODO: Java 7 added a few AutoCloseable superinterfaces to classes.
                 // We specify those as superinterfaces in the jdk.astub file. Let's ignore
                 // this addition to be compatible with Java 6.
-                assert foundType != null || (superType.toString().equals("AutoCloseable") || superType.toString().equals("java.io.Closeable")) :
+                assert foundType != null || (superType.toString().equals("AutoCloseable") || superType.toString().equals("java.io.Closeable") || superType.toString().equals("Closeable")) :
                     "StubParser: could not find superinterface " + superType + " from type " + type;
                 if (foundType != null) annotate(foundType, superType);
             }
