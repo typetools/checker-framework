cf277a3f6aa14915b910f8f69d048a6159db0f3f
Michael Ernst <mernst@cs.washington.edu>
2011-08-14 15:02:41 -0700

    Add tests for -AskipUses command-line argument

---
+++ checkers/tests/src/tests/NullnessSkipUsesTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import java.io.File;
+import java.util.Collection;
+
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ * JUnit tests for the Nullness checker -- testing -AskipUses command-line argument.
+ */
+public class NullnessSkipUsesTest extends ParameterizedCheckerTest {
+
+    public NullnessSkipUsesTest(File testFile) {
+        super(testFile, "checkers.nullness.NullnessChecker", "nullness", "-Anomsgtext", "-AskipUses=SkipMe");
+    }
+
+    @Parameters
+    public static Collection<Object[]> data() { return testFiles("nullness-skipuses"); }
+
+}
