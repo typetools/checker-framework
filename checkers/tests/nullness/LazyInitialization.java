7ccf25b1301741404e3a62717cfb63231cb402fa
wdietl <wdietl@gmail.com>
2011-08-25 17:12:34 -0700

    Improved test case for bug 107. There seems to be an ordering dependency between class declarations and uses of the fields from that class.

--- checkers/tests/nullness/LazyInitialization.java
+++ checkers/tests/nullness/LazyInitialization.java
@@ -64,16 +64,41 @@ public class LazyInitialization {
             ppt.equality_view.toString();
         }
     }
+
     class PptTopLevel {
         public @LazyNonNull Object equality_view;
     }
 
-    class PptRelation2 {
-        public @LazyNonNull Object equality_view2;
-        public void init_hierarchy_new (PptRelation2 pr, Object eq) {
-            pr.equality_view2 = eq;
-            pr.equality_view2.toString();
+    class PptRelation1b {
+    	// This is the same code as in PptRelation1, but comes after the class
+    	// declaration of PptTopLevel. This works as expected.
+        public void init_hierarchy_new (PptTopLevel ppt, Object eq) {
+            ppt.equality_view = eq;
+            ppt.equality_view.toString();
         }
     }
 
+    class PptRelation2 {
+        public @LazyNonNull Object equality_view2;
+        public void init_hierarchy_new (PptRelation2 pr1, PptRelation2 pr2, Object eq) {
+            //:: error: (dereference.of.nullable)
+            pr1.equality_view2.toString();
+            
+            pr1.equality_view2 = eq;
+            pr1.equality_view2.toString();
+
+            // Flow inference uses the Element instead of something more precise to handle
+            // aliasing correctly.
+            //TODO:: error: (dereference.of.nullable)
+            pr2.equality_view2.toString();
+            //TODO:: error: (dereference.of.nullable)
+            this.equality_view2.toString();
+
+            pr2.equality_view2 = eq;
+            pr2.equality_view2.toString();
+
+            this.equality_view2 = eq;
+            this.equality_view2.toString();
+        }
+    }
 }
