da724c780b40ac0cbbaefaebcc2ea1b4be94519c
Michael Ernst <mernst@cs.washington.edu>
2011-09-14 16:50:38 -0700

    Automated merge with https://checker-framework.googlecode.com/hg

--- release/testcheckers.sh
+++ release/testcheckers.sh
@@ -5,19 +5,44 @@ wget -q http://types.cs.washington.edu/checker-framework/current/checkers.zip
 rm -rf checker-framework/
 unzip -q checkers.zip
 
-echo `which java`
-java -version
-
 export CHECKERS=`pwd`/checker-framework/checkers
+export ORIG_PATH=$PATH
 
-$CHECKERS/binary/javac -version
-java -Xbootclasspath/p:$CHECKERS/binary/jsr308-all.jar \
-       -jar $CHECKERS/binary/jsr308-all.jar -version
 
-$CHECKERS/binary/javac -processor checkers.nullness.NullnessChecker \
-    $CHECKERS/examples/NullnessExample.java 
+function cfruntest() {
+  echo `which java`
+  java -version
 
-java -Xbootclasspath/p:$CHECKERS/binary/jsr308-all.jar \
-    -jar $CHECKERS/binary/jsr308-all.jar \
-    -processor checkers.nullness.NullnessChecker \
-    $CHECKERS/examples/NullnessExample.java 
+  $CHECKERS/binary/javac -version
+  if (($?)); then exit 6; fi
+
+  java -Xbootclasspath/p:$CHECKERS/binary/jsr308-all.jar \
+      -jar $CHECKERS/binary/jsr308-all.jar -version
+  if (($?)); then exit 6; fi
+
+  $CHECKERS/binary/javac -processor checkers.nullness.NullnessChecker \
+      $CHECKERS/examples/NullnessExample.java 
+  if (($?)); then exit 6; fi
+
+  java -Xbootclasspath/p:$CHECKERS/binary/jsr308-all.jar \
+      -jar $CHECKERS/binary/jsr308-all.jar \
+      -processor checkers.nullness.NullnessChecker \
+      $CHECKERS/examples/NullnessExample.java 
+  if (($?)); then exit 6; fi
+}
+
+
+echo "Testing with Java 6:"
+
+export JAVA_HOME=$JAVA6_HOME
+export PATH=$JAVA_HOME/bin:$PATH
+
+cfruntest
+
+
+echo "Testing with Java 7:"
+
+export JAVA_HOME=$JAVA7_HOME
+export PATH=$JAVA_HOME/bin:$ORIG_PATH
+
+cfruntest
