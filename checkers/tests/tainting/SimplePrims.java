605b323bf400bd56336d335b6cad2fb6b3449263
wdietl <wdietl@gmail.com>
2011-09-15 15:15:55 -0700

    Simple test case with primitive types, just to get more primitive type test cases.

---
+++ checkers/tests/tainting/SimplePrims.java
@@ -0,0 +1,49 @@
+import checkers.tainting.quals.*;
+
+class SimplePrims {
+
+    void execute(@Untainted int s) { }
+    void tainted(int s) { }
+
+    void intLiteral() {
+        //:: error: (argument.type.incompatible)
+        execute(5);
+        tainted(6);
+    }
+
+    void intRef(int ref) {
+        //:: error: (argument.type.incompatible)
+        execute(ref);
+        tainted(ref);
+    }
+
+    void untaintedRef(@Untainted int ref) {
+        execute(ref);
+        tainted(ref);
+    }
+
+    void concatenation(@Untainted int s1, int s2) {
+        //:: error: (argument.type.incompatible)
+        execute(s1 + s1);
+        //:: error: (argument.type.incompatible)
+        execute(s1 + 3);
+        
+        //:: error: (argument.type.incompatible)
+        execute(s1 + s2);
+
+        //:: error: (argument.type.incompatible)
+        execute(s2 + s1);
+        //:: error: (argument.type.incompatible)
+        execute(s2 + 4);
+        //:: error: (argument.type.incompatible)
+        execute(s2 + s2);
+
+        tainted(s1 + s1);
+        tainted(s1 + 7);
+        tainted(s1 + s2);
+
+        tainted(s2 + s1);
+        tainted(s2 + 8);
+        tainted(s2 + s2);
+    }
+}
