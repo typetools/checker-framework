65c6c2d7b9d0ad5afe3bc45cad48417abd53fe8d
wdietl <wdietl@gmail.com>
2011-09-27 13:51:57 -0700

    Fix incorrect receiver parameter types.

--- checkers/jdk/igj/src/java/util/AbstractMap.java
+++ checkers/jdk/igj/src/java/util/AbstractMap.java
@@ -30,20 +30,20 @@ public abstract class AbstractMap<K, V> implements @I Map<K, V> {
     public String toString(@ReadOnly SimpleImmutableEntry<K, V> this) { throw new RuntimeException("skeleton method"); }
   }
 
-  protected AbstractMap(@ReadOnly SimpleImmutableEntry<K, V> this) {}
-  public int size(@ReadOnly SimpleImmutableEntry<K, V> this) { throw new RuntimeException("skeleton method"); }
-  public boolean isEmpty(@ReadOnly SimpleImmutableEntry<K, V> this) { throw new RuntimeException("skeleton method"); }
-  public boolean containsValue(@ReadOnly SimpleImmutableEntry<K, V> this, @ReadOnly Object a1) { throw new RuntimeException("skeleton method"); }
-  public boolean containsKey(@ReadOnly SimpleImmutableEntry<K, V> this, @ReadOnly Object a1) { throw new RuntimeException("skeleton method"); }
-  public V get(@ReadOnly SimpleImmutableEntry<K, V> this, Object a1) { throw new RuntimeException("skeleton method"); }
-  public V put(@Mutable SimpleImmutableEntry<K, V> this, K a1, V a2) { throw new RuntimeException("skeleton method"); }
-  public V remove(@Mutable SimpleImmutableEntry<K, V> this, Object a1) { throw new RuntimeException("skeleton method"); }
-  public void putAll(@Mutable SimpleImmutableEntry<K, V> this, @ReadOnly Map<? extends K, ? extends V> a1) { throw new RuntimeException("skeleton method"); }
-  public void clear(@Mutable SimpleImmutableEntry<K, V> this) { throw new RuntimeException("skeleton method"); }
-  public @I Set<K> keySet(@ReadOnly SimpleImmutableEntry<K, V> this) { throw new RuntimeException("skeleton method"); }
-  public @I Collection<V> values(@ReadOnly SimpleImmutableEntry<K, V> this) { throw new RuntimeException("skeleton method"); }
-  public abstract @I Set<@I Map.Entry<K, V>> entrySet(@ReadOnly SimpleImmutableEntry<K, V> this);
-  public boolean equals(@ReadOnly SimpleImmutableEntry<K, V> this, @ReadOnly Object a1) { throw new RuntimeException("skeleton method"); }
-  public int hashCode(@ReadOnly SimpleImmutableEntry<K, V> this) { throw new RuntimeException("skeleton method"); }
-  public String toString(@ReadOnly SimpleImmutableEntry<K, V> this) { throw new RuntimeException("skeleton method"); }
+  protected AbstractMap(@ReadOnly AbstractMap<K, V> this) {}
+  public int size(@ReadOnly AbstractMap<K, V> this) { throw new RuntimeException("skeleton method"); }
+  public boolean isEmpty(@ReadOnly AbstractMap<K, V> this) { throw new RuntimeException("skeleton method"); }
+  public boolean containsValue(@ReadOnly AbstractMap<K, V> this, @ReadOnly Object a1) { throw new RuntimeException("skeleton method"); }
+  public boolean containsKey(@ReadOnly AbstractMap<K, V> this, @ReadOnly Object a1) { throw new RuntimeException("skeleton method"); }
+  public V get(@ReadOnly AbstractMap<K, V> this, Object a1) { throw new RuntimeException("skeleton method"); }
+  public V put(@Mutable AbstractMap<K, V> this, K a1, V a2) { throw new RuntimeException("skeleton method"); }
+  public V remove(@Mutable AbstractMap<K, V> this, Object a1) { throw new RuntimeException("skeleton method"); }
+  public void putAll(@Mutable AbstractMap<K, V> this, @ReadOnly Map<? extends K, ? extends V> a1) { throw new RuntimeException("skeleton method"); }
+  public void clear(@Mutable AbstractMap<K, V> this) { throw new RuntimeException("skeleton method"); }
+  public @I Set<K> keySet(@ReadOnly AbstractMap<K, V> this) { throw new RuntimeException("skeleton method"); }
+  public @I Collection<V> values(@ReadOnly AbstractMap<K, V> this) { throw new RuntimeException("skeleton method"); }
+  public abstract @I Set<@I Map.Entry<K, V>> entrySet(@ReadOnly AbstractMap<K, V> this);
+  public boolean equals(@ReadOnly AbstractMap<K, V> this, @ReadOnly Object a1) { throw new RuntimeException("skeleton method"); }
+  public int hashCode(@ReadOnly AbstractMap<K, V> this) { throw new RuntimeException("skeleton method"); }
+  public String toString(@ReadOnly AbstractMap<K, V> this) { throw new RuntimeException("skeleton method"); }
 }
