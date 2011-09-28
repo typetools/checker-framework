5a1b51a71d781d3b2d3565523c78924a427934d5
wdietl <wdietl@gmail.com>
2011-09-28 15:36:40 -0700

    Initialize the field to ensure a quiet build.

--- checkers/tests/nullness-extra/Bug109_A.java
+++ checkers/tests/nullness-extra/Bug109_A.java
@@ -4,6 +4,6 @@ public class Bug109_A {
     //fix 1: public final int one; {one= "1".length();}
     //fix 2: public final int one = 0 + "1".length();
     
-	int nl;
+	int nl = 5;
 	int two = nl;
 }
