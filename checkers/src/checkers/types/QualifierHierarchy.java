35710049586e6d19d83e6e25b6992235ed8b8e1c
wdietl <wdietl@gmail.com>
2011-10-24 00:06:17 -0700

    Eclipse clean-up of imports.

--- checkers/src/checkers/types/QualifierHierarchy.java
+++ checkers/src/checkers/types/QualifierHierarchy.java
@@ -1,9 +1,7 @@
 package checkers.types;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 import java.util.Set;
 
 import javax.lang.model.element.AnnotationMirror;
