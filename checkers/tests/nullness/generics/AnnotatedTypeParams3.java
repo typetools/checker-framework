9d46997c9ee9909e74320ac63f2a3ac682d3d7fa
wdietl <wdietl@gmail.com>
2011-10-25 14:40:53 -0700

    Improve test cases.

--- checkers/tests/nullness/generics/AnnotatedTypeParams3.java
+++ checkers/tests/nullness/generics/AnnotatedTypeParams3.java
@@ -1,5 +1,6 @@
 import java.lang.reflect.*;
 import java.lang.annotation.Annotation;
+import java.util.List;
 import checkers.nullness.quals.*;
 
 class AnnotatedTypeParams3 {
@@ -32,5 +33,47 @@ class AnnotatedTypeParams3 {
             annotation = null;
         }
         return annotation;
+    }
+
+    <@NonNull T> void test(T p) {
+        Object o = p;
+        @NonNull Object re = o;
+    }
+
+    <T extends @NonNull Object> void test2(T p) {
+        Object o = p;
+        @NonNull Object re = o;
+    }
+
+    // TODO: do we want to infer the type variable annotation on local variable "o"?
+    <T> void test3(@NonNull T p) {
+        T o = p;
+        @NonNull T re = o;
+    }
+
+    void test5a(List<?> p) {
+        p.get(0).toString();
+        Object o = p.get(0);
+        o.toString();
+    }
+
+    void test5b(List<? extends Object> p) {
+        p.get(0).toString();
+        Object o = p.get(0);
+        o.toString();
+    }
+
+    void test5c(List<? extends @NonNull Object> p) {
+        p.get(0).toString();
+        Object o = p.get(0);
+        o.toString();
+    }
+
+    void test5d(List<? extends @Nullable Object> p) {
+        //:: error: (dereference.of.nullable)
+        p.get(0).toString();
+        Object o = p.get(0);
+        //:: error: (dereference.of.nullable)
+        o.toString();
     }
 }
