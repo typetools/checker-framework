ea289ead98dab69642181b5a7a5d78b4034caa16
Eric Spishak <espishak@cs.washington.edu>
2011-10-24 19:23:30 -0700

    Small fixes to manual.

---
+++ checkers/src/checkers/lock/LockChecker.java
@@ -0,0 +1,43 @@
+package checkers.lock;
+
+import javax.lang.model.element.AnnotationMirror;
+
+import checkers.basetype.BaseTypeChecker;
+import checkers.lock.quals.GuardedBy;
+import checkers.quals.Unqualified;
+import checkers.types.QualifierHierarchy;
+import checkers.types.AnnotatedTypeMirror.AnnotatedDeclaredType;
+import checkers.util.AnnotationUtils;
+import checkers.util.GraphQualifierHierarchy;
+
+/**
+ * A typechecker plug-in for the JCIP type system qualifier that finds (and
+ * verifies the absence of) locking and concurrency errors.
+ *
+ * @see GuardedBy
+ */
+public class LockChecker extends BaseTypeChecker {
+
+    @Override
+    protected QualifierHierarchy createQualifierHierarchy() {
+        AnnotationUtils annoFactory = AnnotationUtils.getInstance(env);
+
+        AnnotationMirror guardedBy = annoFactory.fromClass(GuardedBy.class);
+        AnnotationMirror unqualified = annoFactory.fromClass(Unqualified.class);
+
+        GraphQualifierHierarchy.Factory factory =
+            new GraphQualifierHierarchy.Factory(this);
+
+        factory.addQualifier(guardedBy);
+        factory.addQualifier(unqualified);
+        factory.addSubtype(unqualified, guardedBy);
+
+        return factory.build();
+    }
+
+    @Override
+    public boolean isValidUse(AnnotatedDeclaredType declarationType,
+            AnnotatedDeclaredType useType) {
+        return true;
+    }
+}
---
+++ checkers/tests/src/tests/FenumTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import java.io.File;
+import java.util.Collection;
+
+import org.junit.runners.Parameterized.Parameters;
+
+public class FenumTest extends ParameterizedCheckerTest {
+
+    public FenumTest(File testFile) {
+        super(testFile, checkers.fenum.FenumChecker.class.getName(), "fenum",
+                "-Anomsgtext");
+    }
+
+    @Parameters
+    public static Collection<Object[]> data() { return testFiles("fenum", "all-systems"); }
+}
