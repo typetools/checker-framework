d22b5d624714112766a0f71ab2efd950d281dd33
Michael Ernst <mernst@cs.washington.edu>
2011-10-25 06:16:06 -0700

    Automated merge with https://checker-framework.googlecode.com/hg

--- checkers/tests/regex/AnnotatedTypeParams3.java
+++ checkers/tests/regex/AnnotatedTypeParams3.java
@@ -1,11 +1,11 @@
 import java.lang.reflect.*;
 import java.lang.annotation.Annotation;
-import checkers.nullness.quals.*;
+import checkers.regex.quals.Regex;
 
 class AnnotatedTypeParams3 {
-    private <T extends Annotation> @Nullable T
+    private <T extends Annotation> T
             safeGetAnnotation(Field f, Class<T> annotationClass) {
-        @Nullable T annotation;
+        T annotation;
         try {
             // Note how the assignment context influences the inferred type argument.
             // If the type parameter for getAnnotation has @NonNull as upper bound
@@ -16,16 +16,17 @@ class AnnotatedTypeParams3 {
             // has a different default lower bound.
             // As alternative, see safeGetAnnotation2 below, which instead changes
             // the parameter type.
-            annotation = f.getAnnotation( (Class<@NonNull T>) annotationClass);
+            // TODO: Why is the @NonRaw needed?
+            annotation = f.getAnnotation( (Class<T>) annotationClass);
         } catch (Exception e) {
             annotation = null;
         }
         return annotation;
     }
 
-    private <T extends Annotation> @Nullable T
-    safeGetAnnotation2(Field f, Class<@NonNull T> annotationClass) {
-        @Nullable T annotation;
+    private <T extends Annotation> T
+    safeGetAnnotation2(Field f, Class<T> annotationClass) {
+        T annotation;
         try {
             annotation = f.getAnnotation(annotationClass);
         } catch (Exception e) {
@@ -33,4 +34,21 @@ class AnnotatedTypeParams3 {
         }
         return annotation;
     }
+
+    <@Regex T> void test(T p) {
+        Object o = p;
+        @Regex Object re = o;
+    }
+
+    <T extends @Regex Object> void test2(T p) {
+        Object o = p;
+        @Regex Object re = o;
+    }
+
+    // TODO: do we want to infer the type variable annotation on local variable "o"?
+    <T> void test3(@Regex T p) {
+        T o = p;
+        @Regex T re = o;
+    }
+
 }
