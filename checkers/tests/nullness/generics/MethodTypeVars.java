a77f04427b33262243a8bb574c46ed73e1076cfe
wdietl <wdietl@gmail.com>
2011-11-10 01:51:33 -0800

    Method type variable inference fails in two situations; adding explicit type arguments solves the problem. This issue doesn't arise when type-checking plume-lib, so I hope this will be OK.

--- checkers/tests/nullness/generics/MethodTypeVars.java
+++ checkers/tests/nullness/generics/MethodTypeVars.java
@@ -12,14 +12,26 @@ public class MethodTypeVars {
                 
         //:: error: (argument.type.incompatible)
         A.goodMethod(null);
-        A.goodMethod(new Object());
+        A.<Object>goodMethod(new Object());
     }
 }
 
-class A{
+class A {
     public static <T extends @NonNull Object> T badMethod(T t) {
         return (T) new Object();
     }
     
     public static <T extends @NonNull Object> void goodMethod(T t) {}
+}
+
+class B {
+    public <T> void indexOf1(T[] a, /*@Nullable*/ Object elt) {}
+    // This is not valid Java syntax.
+    // public void indexOf2(?[] a, /*@Nullable*/ Object elt) {}
+    
+    void call() {
+        Integer[] arg = new Integer[4];
+        indexOf1(arg, new Integer(5));
+        // indexOf2(arg, new Integer(5));
+    }
 }
