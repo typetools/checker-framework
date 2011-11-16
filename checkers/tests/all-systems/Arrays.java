0109ca2ed0aaa0b23fb3cb969249082c82621c27
wdietl <wdietl@gmail.com>
2011-11-15 17:48:11 -0800

    A test case to ensure that array initializers work. The underlying problem was that String literals didn't receive default annotations, resulting in the unification of the types to fail.

---
+++ checkers/tests/all-systems/Arrays.java
@@ -0,0 +1,7 @@
+class Arrays {
+    public static final String[] RELATIONSHIP_LABELS =
+        { "SJJ", "SJU", "SUJ", "SUU", "DJJ", "DJU", "DUJ", "DUU",
+        "JM", "UM", "MJ", "MU" };
+
+    public static final int[] ia = { 1, 2, 3 };
+}
