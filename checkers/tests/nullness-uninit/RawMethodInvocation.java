f786424b456355ac39c4ce2915de3ef83a3ab0ff
Michael Ernst <mernst@cs.washington.edu>
2011-11-20 16:48:44 -0800

    Improve checking of invocations of non-raw methods from raw contexts

--- checkers/tests/nullness-uninit/RawMethodInvocation.java
+++ checkers/tests/nullness-uninit/RawMethodInvocation.java
@@ -17,26 +17,12 @@ class RawMethodInvocation {
 
   //:: warning: (fields.uninitialized)
   RawMethodInvocation(Byte constructor_inits_b) {
-    // TODO FIXME: This legal call yields a bizarre and incorrect error:
-    // RawMethodInvocation.java:27: error: call to init_b() not allowed on the given receiver.
-    //     init_b();
-    //           ^
-    //   found   : @Nullable RawMethodInvocation
-    //   required: @NonNull RawMethodInvocation
-    //:: error: (method.invocation.invalid)
     init_b();
   }
 
   //:: warning: (fields.uninitialized)
   RawMethodInvocation(byte constructor_inits_b) {
     b = 2;
-    // TODO FIXME: This legal call yields a bizarre and incorrect error:
-    // RawMethodInvocation.java:40: error: call to init_b() not allowed on the given receiver.
-    //     init_b();
-    //           ^
-    //   found   : @Nullable RawMethodInvocation
-    //   required: @NonNull RawMethodInvocation
-    //:: error: (method.invocation.invalid)
     init_b();
   }
 
@@ -53,7 +39,7 @@ class RawMethodInvocation {
 
   RawMethodInvocation(long constructor_escapes_raw) {
     a = 1;
-    //:: error: (method.invocation.invalid)
+    //:: warning: (method.invocation.invalid.rawness)
     nonRawMethod();
     b = 2;
   }
