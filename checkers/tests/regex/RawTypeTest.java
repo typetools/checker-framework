04841860f0ff6737b937748211bddf9ed5307ee2
Eric Spishak <espishak@cs.washington.edu>
2011-11-21 23:22:54 -0800

    Another raw type test case that fails.

--- checkers/tests/regex/RawTypeTest.java
+++ checkers/tests/regex/RawTypeTest.java
@@ -1,5 +1,17 @@
+import java.util.List;
+
 public class RawTypeTest {
-    public void m(ClassLoader cl) throws ClassNotFoundException {
-	Class clazz = cl.loadClass("");
-    }
+  public void m(ClassLoader cl) throws ClassNotFoundException {
+    Class clazz = cl.loadClass("");
+  }
+}
+
+interface I {
+  public void m(List<? extends String> l);
+}
+
+class C implements I {
+  public void m(List l) {
+    
+  }
 }
