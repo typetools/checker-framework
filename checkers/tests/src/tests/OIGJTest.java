3e47e53c10e191188ab9c082bfadc8f305c0cd7b
wdietl <wdietl@gmail.com>
2011-11-27 01:12:11 -0800

    Two simple uses of raw types that should work in all type systems. Javari and OIGJ however use different defaults, which don't work here.

--- checkers/tests/src/tests/OIGJTest.java
+++ checkers/tests/src/tests/OIGJTest.java
@@ -44,6 +44,7 @@ public class OIGJTest extends ParameterizedCheckerTest {
                 o.toString().equals("tests/all-systems/Ternary.java") ||
                 o.toString().equals("tests/all-systems/FieldWithInit.java") ||
                 o.toString().equals("tests/all-systems/TypeVars.java") ||
+                o.toString().equals("tests/all-systems/RawTypes.java") ||
                 o.toString().equals("tests/all-systems/Enums.java");
     }
 }
