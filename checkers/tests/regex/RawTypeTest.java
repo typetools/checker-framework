34a061151b843e3108c3c070cf8b1dda909a6994
Eric Spishak <espishak@cs.washington.edu>
2011-11-29 00:47:40 -0800

    Add some more failing tests to the regex checker to demonstrate more issues with raw types.

--- checkers/tests/regex/RawTypeTest.java
+++ checkers/tests/regex/RawTypeTest.java
@@ -1,7 +1,38 @@
+import java.lang.ref.WeakReference;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.util.List;
+import java.util.Stack;
+
 import checkers.regex.quals.*;
 
 class RawTypeTest {
 
+    public void m1(Class<?> c) {
+        new WeakReference<Class<? extends I2>>(c.asSubclass(I2.class));
+    }
+    
+    public void m2(Class<Object> c) {}
+    
+    public void m3(Class c) {
+        m2(c);
+    }
+  
+    public void m4() {
+        AccessController.doPrivileged(new PrivilegedAction() {
+          public Object run() {
+            return null;
+        }});
+    }
+    
+    public void m5(List list, C4 c) {
+        list.add(c);
+    }
+    
+    public void m6(List list, long l) {
+        list.add(l);
+    }
+    
     class MyList<X extends @Regex String> {
         X f;
     }
@@ -29,4 +60,6 @@ class RawTypeTest {
         //:: error: (override.param.invalid) :: error: (generic.argument.invalid)
         public void m(MyList<String> l) {}
     }
+    
+    class C4 {}
 }
