c43634841543ae5c184e71564dc340a2cff744ac
wdietl <wdietl@gmail.com>
2011-12-10 23:18:50 -0800

    Expand the example, rename type variables to make it understandable.

--- checkers/tests/nullness/generics/MethodTypeVars2.java
+++ checkers/tests/nullness/generics/MethodTypeVars2.java
@@ -1,24 +1,23 @@
 import checkers.nullness.quals.*;
 
 public class MethodTypeVars2 {
-    
+
     class GeoSegment {}
-    class StreetSegment extends GeoSegment {}
-    
-    interface Path<N, P extends Path<N, P>> {}
+    interface Path<N1, P1 extends Path<N1, P1>> {}
 
-    class StreetSegmentPath implements
-    Path<StreetSegment, StreetSegmentPath> {}
-
-    private static <N extends GeoSegment, P extends Path<N, P>>
-    @Nullable Object pathToRoute(Path<N, P> path)
+    private static <N2 extends GeoSegment, P2 extends Path<N2, P2>>
+    @Nullable Object pathToRoute(Path<N2, P2> path)
       { return null; }
-    
+
+    class StreetSegment extends GeoSegment {}
+    class StreetSegmentPath implements
+        Path<StreetSegment, StreetSegmentPath> {}
+
+
     void call(StreetSegmentPath p) {
         Object r = pathToRoute(p);
     }
 
-    /*
     static class WorkingWithOne {
         interface GPath<P extends GPath<P>> {}
 
@@ -27,9 +26,9 @@ public class MethodTypeVars2 {
         private static <P extends GPath<P>>
         @Nullable Object pathToRoute(GPath<P> path)
         { return null; }
-        
+
         void call(GStreetSegmentPath p) {
             Object r = pathToRoute(p);
         }
-    }*/
+    }
 }
