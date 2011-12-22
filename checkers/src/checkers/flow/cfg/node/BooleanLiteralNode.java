bbefe2a0ef9140da40ded32eb47652396d5bda0d
stefanheule <stefanheule@localhost>
2011-12-22 03:19:31 -0800

    Improve the documentation of various nodes.

--- checkers/src/checkers/flow/cfg/node/BooleanLiteralNode.java
+++ checkers/src/checkers/flow/cfg/node/BooleanLiteralNode.java
@@ -1,11 +1,11 @@
 package checkers.flow.cfg.node;
 
-
 import com.sun.source.tree.Tree;
 import com.sun.source.tree.LiteralTree;
 
 /**
- * A node for a boolean literal. For example:
+ * A node for a boolean literal:
+ * 
  * <pre>
  *   <em>true</em>
  *   <em>false</em>
@@ -15,25 +15,24 @@ import com.sun.source.tree.LiteralTree;
  * 
  */
 public class BooleanLiteralNode extends ValueLiteralNode {
-	
+
 	public BooleanLiteralNode(LiteralTree t) {
 		assert t.getKind().equals(Tree.Kind.BOOLEAN_LITERAL);
 		tree = t;
 	}
-	
+
 	public LiteralTree getLiteralTree() {
 		return tree;
 	}
-	
+
 	@Override
 	public Boolean getValue() {
-		return (Boolean)tree.getValue();
+		return (Boolean) tree.getValue();
 	}
 
 	@Override
-	public
-	<R, P> R accept(NodeVisitor<R, P> visitor, P p) {
+	public <R, P> R accept(NodeVisitor<R, P> visitor, P p) {
 		return visitor.visitBooleanLiteral(this, p);
 	}
-	
+
 }
