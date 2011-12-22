35da6fb217101d42ee9188a6d51a105335483508
stefanheule <stefanheule@localhost>
2011-12-22 03:24:35 -0800

    Improve documentation about the relation of LocalVariableNode and VariableDeclarationNode.

--- checkers/src/checkers/flow/cfg/node/VariableDeclarationNode.java
+++ checkers/src/checkers/flow/cfg/node/VariableDeclarationNode.java
@@ -5,27 +5,31 @@ import com.sun.source.tree.VariableTree;
 
 /**
  * A node for a local variable declaration:
+ * 
  * <pre>
- *   <em>modifier</em> <em>type</em>  <em>var</em>;
+ *   <em>modifier</em> <em>type</em> <em>identifier</em>;
  * </pre>
+ * 
+ * Note: Does not have an initializer block, as that will be translated to a
+ * separate {@link AssignmentNode}.
  * 
  * @author Stefan Heule
  * 
  */
 public class VariableDeclarationNode extends Node {
-	
+
 	protected VariableTree tree;
-	
+
 	// TODO: make type and modifier accessible
-	
+
 	public VariableDeclarationNode(VariableTree t) {
 		tree = t;
 	}
-	
+
 	public String getName() {
 		return tree.getName().toString();
 	}
-	
+
 	/**
 	 * Guaranteed to return the same tree as {@link getTree}, but with a more
 	 * specific type.
@@ -40,11 +44,10 @@ public class VariableDeclarationNode extends Node {
 	}
 
 	@Override
-	public
-	<R, P> R accept(NodeVisitor<R, P> visitor, P p) {
+	public <R, P> R accept(NodeVisitor<R, P> visitor, P p) {
 		return visitor.visitVariableDeclaration(this, p);
 	}
-	
+
 	@Override
 	public String toString() {
 		return getName().toString();
