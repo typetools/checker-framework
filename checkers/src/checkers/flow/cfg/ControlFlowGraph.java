2f5e8474a8a4853e18f775999eec422c4416ee4c
stefanheule <stefanheule@localhost>
2012-01-03 19:02:40 -0800

    Introduce abstraction for a control flow graph (instead of just passing around its entry block).

---
+++ checkers/src/checkers/flow/cfg/ControlFlowGraph.java
@@ -0,0 +1,46 @@
+package checkers.flow.cfg;
+
+import java.util.Map;
+
+import checkers.flow.cfg.block.SpecialBlock;
+import checkers.flow.cfg.node.Node;
+
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.Tree;
+
+/**
+ * A control flow graph (CFG for short) of a single method.
+ * 
+ * @author Stefan Heule
+ *
+ */
+public class ControlFlowGraph {
+
+	/** The entry block of the control flow graph. */
+	protected SpecialBlock entryBlock;
+	
+	/** The method this CFG corresponds to. */
+	protected MethodTree tree;
+	
+	/** Map from AST {@link Tree}s to {@link Node}s. */
+	protected Map<Tree, Node> treeLookup;
+
+	public ControlFlowGraph(SpecialBlock entryBlock, MethodTree tree,
+			Map<Tree, Node> treeLookup) {
+		super();
+		this.entryBlock = entryBlock;
+		this.tree = tree;
+		this.treeLookup = treeLookup;
+	}
+	
+	/** @return The entry block of the control flow graph. */
+	public SpecialBlock getEntryBlock() {
+		return entryBlock;
+	}
+
+	/** @return The method this CFG corresponds to. */
+	public MethodTree getTree() {
+		return tree;
+	}
+	
+}
