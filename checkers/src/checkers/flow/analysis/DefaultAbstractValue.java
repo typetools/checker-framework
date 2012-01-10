ded4a8e11d8c0d72763c88661afa405f90a5189e
stefanheule <devnull@localhost>
2012-01-10 01:40:47 -0800

    Finish implementation of DefaultAbstractValue.

--- checkers/src/checkers/flow/analysis/DefaultAbstractValue.java
+++ checkers/src/checkers/flow/analysis/DefaultAbstractValue.java
@@ -1,55 +1,79 @@
 package checkers.flow.analysis;
 
+import java.util.Collections;
+import java.util.Set;
+
 import javax.lang.model.element.AnnotationMirror;
+
+import checkers.flow.util.HashCodeUtils;
+import checkers.types.QualifierHierarchy;
+import checkers.util.AnnotationUtils;
 
 /**
  * An implementation of an abstract value used by the default dataflow analysis.
+ * Contains a set of annotations.
  * 
  * @author Stefan Heule
  * 
  */
 public class DefaultAbstractValue implements AbstractValue {
 
+	/**
+	 * The qualifier hierarchy used to determine the least upper bound of two
+	 * sets of annotations.
+	 */
+	protected QualifierHierarchy qualifierHierarchy;
+
 	/** The annotation corresponding to this abstract value. */
-	// TODO: should be a set of annotations
-	protected AnnotationMirror annotation;
+	protected Set<AnnotationMirror> annotations;
 
-	public DefaultAbstractValue(AnnotationMirror annotation) {
-		this.annotation = annotation;
+	/**
+	 * Create new default abstract value.
+	 * 
+	 * @param annotations
+	 *            The set of annotations.
+	 * @param qualifierHierarchy
+	 *            The qualifier hierarchy used to determine the least upper
+	 *            bound of two sets of annotations.
+	 */
+	public DefaultAbstractValue(Set<AnnotationMirror> annotations,
+			QualifierHierarchy qualifierHierarchy) {
+		this.annotations = annotations;
+		this.qualifierHierarchy = qualifierHierarchy;
 	}
 
-	public AnnotationMirror getAnnotation() {
-		return annotation;
+	/** @return The annotations this abstract value stands for. */
+	public Set<AnnotationMirror> getAnnotations() {
+		return Collections.unmodifiableSet(annotations);
 	}
-	
+
 	@Override
 	public DefaultAbstractValue leastUpperBound(AbstractValue o) {
 		assert o instanceof DefaultAbstractValue;
 		DefaultAbstractValue other = (DefaultAbstractValue) o;
-		// TODO: correct implementation
-		return null;
+		Set<AnnotationMirror> lub = qualifierHierarchy.leastUpperBound(
+				annotations, other.getAnnotations());
+		return new DefaultAbstractValue(lub, qualifierHierarchy);
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (obj == null) {
+		if (obj == null || !(obj instanceof DefaultAbstractValue)) {
 			return false;
 		}
-		if (obj.getClass() != getClass()) {
-			return false;
-		}
-		return ((DefaultAbstractValue) obj).getAnnotation().equals(
-				getAnnotation());
+		DefaultAbstractValue other = (DefaultAbstractValue) obj;
+		return AnnotationUtils
+				.areSame(getAnnotations(), other.getAnnotations());
 	}
 
 	@Override
 	public int hashCode() {
-		return annotation.hashCode();
+		return HashCodeUtils.hash(annotations);
 	}
 
 	@Override
 	public String toString() {
-		return annotation.toString();
+		return annotations.toString();
 	}
 
 }
