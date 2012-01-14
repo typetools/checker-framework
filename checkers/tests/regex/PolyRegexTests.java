7cef8a6ebd318e5f4924bdcef39b45487c561433
Eric Spishak <espishak@cs.washington.edu>
2012-01-13 16:22:24 -0800

    Clean up indentation.

--- checkers/tests/regex/PolyRegexTests.java
+++ checkers/tests/regex/PolyRegexTests.java
@@ -4,25 +4,25 @@ import java.util.regex.Pattern;
 
 public class PolyRegexTests {
 
-	public static @PolyRegex String method(@PolyRegex String s) {
-		return s;
-	}
-	
-	public void testRegex(@Regex String str) {
-		@Regex String s = method(str);
-	}
-	
-	public void testNonRegex(String str) {
-		//:: error: (assignment.type.incompatible)
-		@Regex String s = method(str);	// error
-	}
-	
-	public void testInternRegex(@Regex String str) {
-	  @Regex String s = str.intern();
-	}
-	
-	public void testInternNonRegex(String str) {
-	  //:: error: (assignment.type.incompatible)
-	  @Regex String s = str.intern(); // error
-	}
+  public static @PolyRegex String method(@PolyRegex String s) {
+    return s;
+  }
+
+  public void testRegex(@Regex String str) {
+    @Regex String s = method(str);
+  }
+
+  public void testNonRegex(String str) {
+    //:: error: (assignment.type.incompatible)
+    @Regex String s = method(str); // error
+  }
+
+  public void testInternRegex(@Regex String str) {
+    @Regex String s = str.intern();
+  }
+
+  public void testInternNonRegex(String str) {
+    //:: error: (assignment.type.incompatible)
+    @Regex String s = str.intern(); // error
+  }
 }
