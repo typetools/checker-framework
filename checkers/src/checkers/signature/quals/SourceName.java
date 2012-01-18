b9c40d68bd2e67e62c4140e82fe19115366a1754
Michael Ernst <mernst@cs.washington.edu>
2012-01-17 16:35:32 -0800

    Improve Signature Checker Javadoc: URLs, formatting, descriptions

--- checkers/src/checkers/signature/quals/SourceName.java
+++ checkers/src/checkers/signature/quals/SourceName.java
@@ -7,16 +7,10 @@ import checkers.quals.TypeQualifier;
 import java.lang.annotation.Target;
 
 /**
- * Since binary names (@BinaryName) and fully qualified names (@FullyQualifiedName) differ only
- * for inner classes (same for top level classes), checker framework cannot deduce if a fully
- * qualified name will be used as a fully qualified name or a binary name in the future. 
- * This is true since all fully qualified names are also valid binary names, where as the other
- * way around is not true.
- * Therefore source name (@SourceName) is an annotation that is only to be used by the checker
- * framework visitors when a fully qualified name (as String literal) is detected. This way 
- * that name is also permitted to be used a binary name (since it is valid).
- * Not to be used by the annotator, only used internally.
- * @author Kivanc Muslu
+ * A source name is a string that is a valid fully qualified name and a
+ * valid binary name.
+ * <p>
+ * Not to be used by the programmer, only used internally.
  */
 @TypeQualifier
 @SubtypeOf({BinaryName.class, FullyQualifiedName.class})
