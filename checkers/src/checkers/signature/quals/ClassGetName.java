a2d19dda87b4ce003d67c55d41ea535627857667
Michael Ernst <mernst@cs.washington.edu>
2012-01-18 20:41:29 -0800

    Correct documentation and code: Class.getName's format is not the same as field descriptor, except for arrays whose base type is primitive or in the unnamed package.

--- checkers/src/checkers/signature/quals/ClassGetName.java
+++ checkers/src/checkers/signature/quals/ClassGetName.java
@@ -12,8 +12,8 @@ import checkers.quals.TypeQualifier;
  * {@link Class#forName(String)}, and {@link Class#forName(String, boolean,
  * ClassLoader)} methods.  This format is:
  * <ul>
- *   <li>the {@link BinaryName binary name} for any non-array type</li>
- *   <li>the {@link FieldDescriptor field descriptor} for any array type</li>
+ *   <li>for any non-array type, the {@link BinaryName binary name}
+ *   <li>for any array type, a format like the {@link FieldDescriptor field descriptor}, but using '.' where the field descriptor uses '/'</li>
  * </ul>
  * <p>
  * Examples include
@@ -26,6 +26,6 @@ import checkers.quals.TypeQualifier;
  */
 @TypeQualifier
 @SubtypeOf(UnannotatedString.class)
-@ImplicitFor(stringPatterns="(^[A-Za-z_][A-Za-z_0-9]*(\\.[A-Za-z_][A-Za-z_0-9]*)*(\\$[A-Za-z_][A-Za-z_0-9]*)?$)|^\\[+([BCDFIJSZ]|L[A-Za-z_][A-Za-z_0-9]*(/[A-Za-z_][A-Za-z_0-9]*)*(\\$[A-Za-z_][A-Za-z_0-9]*)?;)$")
+@ImplicitFor(stringPatterns="(^[A-Za-z_][A-Za-z_0-9]*(\\.[A-Za-z_][A-Za-z_0-9]*)*(\\$[A-Za-z_][A-Za-z_0-9]*)?$)|^\\[+([BCDFIJSZ]|L[A-Za-z_][A-Za-z_0-9]*(\\.[A-Za-z_][A-Za-z_0-9]*)*(\\$[A-Za-z_][A-Za-z_0-9]*)?;)$")
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 public @interface ClassGetName {}
