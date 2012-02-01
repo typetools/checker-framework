44f86ca5fbcbd9d4113c72d63823f3be4b1b7e1e
Eric Spishak <espishak@cs.washington.edu>
2012-02-01 00:09:38 -0800

    Improve regex warning suppression comments.

--- checkers/src/checkers/regex/RegexUtil.java
+++ checkers/src/checkers/regex/RegexUtil.java
@@ -37,7 +37,7 @@ public class RegexUtil {
    * Returns true if the argument is a syntactically valid regular
    * expression. 
    */
-  @SuppressWarnings("regex")    // tests whether s is a regex
+  @SuppressWarnings("regex")    // RegexUtil
   /*@Pure*/
   public static boolean isRegex(String s) {
     try {
@@ -53,7 +53,7 @@ public class RegexUtil {
    * expression. Otherwise returns a string describing why the string is
    * not a regex.
    */
-  @SuppressWarnings("regex")    // tests whether s is a regex
+  @SuppressWarnings("regex")    // RegexUtil
   /*@Pure*/
   public static /*@Nullable*/ String regexError(String s) {
     try {
@@ -69,7 +69,7 @@ public class RegexUtil {
    * expression. Otherwise returns a PatternSyntaxException describing
    * why the string is not a regex.
    */
-  @SuppressWarnings("regex")    // tests whether s is a regex
+  @SuppressWarnings("regex")    // RegexUtil
   /*@Pure*/
   public static /*@Nullable*/ PatternSyntaxException regexException(String s) {
     try {
@@ -86,7 +86,7 @@ public class RegexUtil {
    * Once the the Regex Checker supports flow-sensitivity, it should be
    * very rarely needed.
    */
-  @SuppressWarnings("regex")    // suppresses warnings
+  @SuppressWarnings("regex")    // RegexUtil
   /*@Pure*/
   public static /*@Regex*/ String asRegex(String s) {
     try {
