92b47f3eb6134f4e0120de436a12a72ad202208a
stefan <stefan@stefan-mobile.dhcp4.washington.edu>
2012-02-11 14:49:59 -0800

    Change file name character again ("-" is a bad choice because some programs will interpret the name as parameter if it starts with a hyphen).

--- checkers/src/checkers/basetype/BaseTypeVisitor.java
+++ checkers/src/checkers/basetype/BaseTypeVisitor.java
@@ -244,7 +244,7 @@ public class BaseTypeVisitor<Checker extends BaseTypeChecker> extends SourceVisi
             if (options.containsKey("flowdotdir")) {
                 String dotfilename = options.get("flowdotdir") + "/" + node.getName() + ".dot";
                 // make path safe for Windows
-                dotfilename = dotfilename.replace("<", "-").replace(">", "-");
+                dotfilename = dotfilename.replace("<", ".").replace(">", ".");
                 System.err.println("Output to DOT file: " + dotfilename);
                 analysis.outputToDotFile(dotfilename);
             }
