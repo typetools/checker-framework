4bd69ee1af44d220673301c143f8da83ff70467b
charlie garrett <charlie.garrett@gmail.com>
2012-02-12 14:28:54 -0800

    Add many nodes for arithmetic, logical and comparison operations

---
+++ checkers/src/checkers/flow/cfg/node/ExplicitThisNode.java
@@ -0,0 +1,73 @@
+package checkers.flow.cfg.node;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import checkers.flow.util.HashCodeUtils;
+
+import com.sun.source.tree.IdentifierTree;
+import com.sun.source.tree.Tree;
+
+/**
+ * A node for a reference to 'this'.
+ * 
+ * <pre>
+ *   <em>this</em>
+ * </pre>
+ * 
+ * @author Stefan Heule
+ * @author Charlie Garrett
+ * 
+ */
+public class ExplicitThisNode extends Node {
+
+	protected Tree tree;
+
+	public ExplicitThisNode(Tree t) {
+		assert t instanceof IdentifierTree &&
+			((IdentifierTree)t).getName().equals("this");
+		tree = t;
+	}
+
+	@Override
+	public Tree getTree() {
+		return tree;
+	}
+
+	@Override
+	public <R, P> R accept(NodeVisitor<R, P> visitor, P p) {
+		return visitor.visitExplicitThis(this, p);
+	}
+
+	public String getName() {
+		return "this";
+	}
+
+	@Override
+	public String toString() {
+		return getName();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null || !(obj instanceof ExplicitThisNode)) {
+			return false;
+		}
+		return true;
+	}
+	
+	@Override
+	public int hashCode() {
+		return HashCodeUtils.hash(getName());
+	}
+
+	@Override
+	public Collection<Node> getOperands() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public boolean hasResult() {
+		return true;
+	}
+}
--- checkers/src/checkers/flow/cfg/node/SinkNodeVisitor.java
+++ checkers/src/checkers/flow/cfg/node/SinkNodeVisitor.java
@@ -17,10 +17,62 @@ public abstract class SinkNodeVisitor<R, P> implements NodeVisitor<R, P> {
 
 	abstract public R visitNode(Node n, P p);
 
+        // Literals
 	@Override
 	public R visitValueLiteral(ValueLiteralNode n, P p) {
 		return visitNode(n, p);
 	}
+
+	@Override
+	public R visitByteLiteral(ByteLiteralNode n, P p) {
+		return visitValueLiteral(n, p);
+	}
+
+	@Override
+	public R visitShortLiteral(ShortLiteralNode n, P p) {
+		return visitValueLiteral(n, p);
+	}
+
+	@Override
+	public R visitIntegerLiteral(IntegerLiteralNode n, P p) {
+		return visitValueLiteral(n, p);
+	}
+
+	@Override
+	public R visitLongLiteral(LongLiteralNode n, P p) {
+		return visitValueLiteral(n, p);
+	}
+
+	@Override
+	public R visitFloatLiteral(FloatLiteralNode n, P p) {
+		return visitValueLiteral(n, p);
+	}
+
+	@Override
+	public R visitDoubleLiteral(DoubleLiteralNode n, P p) {
+		return visitValueLiteral(n, p);
+	}
+
+	@Override
+	public R visitBooleanLiteral(BooleanLiteralNode n, P p) {
+		return visitValueLiteral(n, p);
+	}
+
+	@Override
+	public R visitCharacterLiteral(CharacterLiteralNode n, P p) {
+		return visitValueLiteral(n, p);
+	}
+
+	@Override
+	public R visitStringLiteral(StringLiteralNode n, P p) {
+		return visitValueLiteral(n, p);
+	}
+
+	@Override
+	public R visitNullLiteral(NullLiteralNode n, P p) {
+		return visitValueLiteral(n, p);
+	}
+
 
 	@Override
 	public R visitAssignment(AssignmentNode n, P p) {
@@ -43,22 +95,210 @@ public abstract class SinkNodeVisitor<R, P> implements NodeVisitor<R, P> {
 	}
 
 	@Override
-	public R visitIntegerLiteral(IntegerLiteralNode n, P p) {
-		return visitValueLiteral(n, p);
-	}
-
-	@Override
 	public R visitImplicitThisLiteral(ImplicitThisLiteralNode n, P p) {
 		return visitNode(n, p);
 	}
 
 	@Override
-	public R visitBooleanLiteral(BooleanLiteralNode n, P p) {
-		return visitValueLiteral(n, p);
+	public R visitExplicitThis(ExplicitThisNode n, P p) {
+		return visitNode(n, p);
 	}
 
 	@Override
+	public R visitSuper(SuperNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitReturn(ReturnNode n, P p) {
+		return visitNode(n, p);
+	};
+
+
+	// Unary operations
+	@Override
+	public R visitNumericalMinus(NumericalMinusNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitNumericalPlus(NumericalPlusNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitBitwiseComplement(BitwiseComplementNode n, P p) {
+		return visitNode(n, p);
+	}
+
+
+	// Binary operations
+	@Override
 	public R visitConditionalOr(ConditionalOrNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitStringConcatenate(StringConcatenateNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitNumericalAddition(NumericalAdditionNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitNumericalSubtraction(NumericalSubtractionNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitNumericalMultiplication(NumericalMultiplicationNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitIntegerDivision(IntegerDivisionNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitFloatingDivision(FloatingDivisionNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitIntegerRemainder(IntegerRemainderNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitFloatingRemainder(FloatingRemainderNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitLeftShift(LeftShiftNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitSignedRightShift(SignedRightShiftNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitUnsignedRightShift(UnsignedRightShiftNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitBitwiseAnd(BitwiseAndNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitBitwiseOr(BitwiseOrNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitBitwiseXor(BitwiseXorNode n, P p) {
+		return visitNode(n, p);
+	}
+
+
+	// Compound assignments
+	@Override
+	public R visitStringConcatenateAssignment(StringConcatenateAssignmentNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitNumericalAdditionAssignment(NumericalAdditionAssignmentNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitNumericalSubtractionAssignment(NumericalSubtractionAssignmentNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitNumericalMultiplicationAssignment(NumericalMultiplicationAssignmentNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitIntegerDivisionAssignment(IntegerDivisionAssignmentNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitFloatingDivisionAssignment(FloatingDivisionAssignmentNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitIntegerRemainderAssignment(IntegerRemainderAssignmentNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitFloatingRemainderAssignment(FloatingRemainderAssignmentNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitLeftShiftAssignment(LeftShiftAssignmentNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitSignedRightShiftAssignment(SignedRightShiftAssignmentNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitUnsignedRightShiftAssignment(UnsignedRightShiftAssignmentNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitBitwiseAndAssignment(BitwiseAndAssignmentNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitBitwiseOrAssignment(BitwiseOrAssignmentNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitBitwiseXorAssignment(BitwiseXorAssignmentNode n, P p) {
+		return visitNode(n, p);
+	}
+
+
+        // Comparison operations
+	@Override
+	public R visitLessThan(LessThanNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitLessThanOrEqual(LessThanOrEqualNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitGreaterThan(GreaterThanNode n, P p) {
+		return visitNode(n, p);
+	}
+
+	@Override
+	public R visitGreaterThanOrEqual(GreaterThanOrEqualNode n, P p) {
 		return visitNode(n, p);
 	}
 
@@ -68,18 +308,8 @@ public abstract class SinkNodeVisitor<R, P> implements NodeVisitor<R, P> {
 	}
 
 	@Override
-	public R visitReturn(ReturnNode n, P p) {
+	public R visitNotEqual(NotEqualNode n, P p) {
 		return visitNode(n, p);
-	};
-
-	@Override
-	public R visitNumericalAddition(NumericalAdditionNode n, P p) {
-		return visitNode(n, p);
-	}
-
-	@Override
-	public R visitStringLiteral(StringLiteralNode n, P p) {
-		return visitValueLiteral(n, p);
 	}
 
 }
