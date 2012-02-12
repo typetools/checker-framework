648946cadd66f006d5e58d76c9000f8ca31befef
stefanheule <stefanheule@localhost>
2012-02-12 13:49:13 -0800

    Improve documentation.

--- checkers/src/checkers/flow/analysis/checkers/CFAnalysis.java
+++ checkers/src/checkers/flow/analysis/checkers/CFAnalysis.java
@@ -12,39 +12,19 @@ import checkers.flow.cfg.CFGDOTVisualizer;
 import checkers.types.AnnotatedTypeFactory;
 import checkers.types.QualifierHierarchy;
 
-
 /**
- * DefaultTypeAnalysis characterizes a kind of abstract value that is computed
- * by the checker framework's default flow-sensitive analysis. It is
- * parameterized by a QualifierHierarchy, defining the relationship among type
- * annotations and an AnnotatedTypeFactory, providing the static type
- * annotations for AST Trees.
- * 
- * The inner class DefaultTypeAnalysis.Value represents a single abstract value
- * computed by the checker framework's default flow-sensitive analysis. A Value
- * is a set of type annotations from the QualifierHierarchy.
- * 
- * The inner class DefaultTypeAnalysis.NodeInfo represents the set of dataflow
- * facts known at a program point, which is a mapping from values, represented
- * by CFG Nodes, to sets of type annotations, represented by
- * DefaultTypeAnalysis.Values.
- * 
- * TODO: Since statically known annotations provided by the AnnotatedTypeFactory
- * are upper bounds, avoid storing NodeInfos explicitly unless they are more
- * precise than the static annotations.
- * 
- * The inner class DefaultTypeAnalysis.Transfer is the transfer function mapping
- * input dataflow facts to output facts. For the default analysis, it merely
- * tracks type annotations through assignments to local variables. Improvements
- * in the precision of type annotations arise from assignments whose RHS has a
- * more precise static type than their LHS.
+ * {@link CFAnalysis} is an extensible dataflow analysis for the Checker
+ * Framework that tracks the annotations using a flow-senstitive analsysis. It
+ * uses an {@link AnnotatedTypeFactory} to provide checker-specific logic how to
+ * combine types (e.g., what is the type of a string concatenation, given the
+ * types of the two operands) and as an abstraction function (e.g., determine
+ * the annotations on literals).
  * 
  * @author Charlie Garrett
  * @author Stefan Heule
  * 
  */
-public class CFAnalysis extends
-		Analysis<CFValue, CFStore, CFTransfer> {
+public class CFAnalysis extends Analysis<CFValue, CFStore, CFTransfer> {
 	/**
 	 * The qualifier hierarchy for which to track annotations.
 	 */
