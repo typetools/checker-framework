67803ed1eed577f2ee130616281c6053275c34c3
stefanheule <stefanheule@localhost>
2012-02-13 00:20:24 -0800

    Start to support tracking types through fields.  Components so far: Internal representation of field accesses, code to update the abstract value at field assignments (including handling of aliasing). Missing: merging stores, updating field information at local variable updates, update information at method calls.

--- checkers/src/checkers/flow/analysis/checkers/CFAbstractStore.java
+++ checkers/src/checkers/flow/analysis/checkers/CFAbstractStore.java
@@ -7,7 +7,14 @@ import java.util.Map.Entry;
 import javax.lang.model.element.Element;
 
 import checkers.flow.analysis.Store;
+import checkers.flow.cfg.node.ExplicitThisNode;
+import checkers.flow.cfg.node.FieldAccessNode;
+import checkers.flow.cfg.node.ImplicitThisLiteralNode;
 import checkers.flow.cfg.node.LocalVariableNode;
+import checkers.flow.cfg.node.Node;
+import checkers.flow.util.HashCodeUtils;
+
+import com.sun.org.apache.bcel.internal.classfile.Unknown;
 
 /**
  * A store for the checker framework analysis tracks the annotations of memory
@@ -22,6 +29,92 @@ import checkers.flow.cfg.node.LocalVariableNode;
 public abstract class CFAbstractStore<V extends CFAbstractValue<V>, S extends CFAbstractStore<V, S>>
 		implements Store<S> {
 
+	public static abstract class Receiver {
+		public boolean containsUnknown() {
+			return contains(new Unkown());
+		}
+
+		/**
+		 * Does {@code other} appear anywhere in this receiver?
+		 */
+		public boolean contains(Receiver other) {
+			return this.equals(other);
+		}
+	}
+
+	public static class FieldAccess extends Receiver {
+		protected Receiver receiver;
+		protected Element field;
+
+		public Receiver getReceiver() {
+			return receiver;
+		}
+
+		public Element getField() {
+			return field;
+		}
+
+		public FieldAccess(Receiver receiver, Element field) {
+			this.receiver = receiver;
+			this.field = field;
+		}
+
+		@Override
+		public boolean contains(Receiver other) {
+			return super.contains(other) || receiver.contains(other);
+		}
+	}
+
+	public static class ThisReference extends Receiver {
+		@Override
+		public boolean equals(Object obj) {
+			return obj != null && obj instanceof ThisReference;
+		}
+
+		@Override
+		public int hashCode() {
+			return HashCodeUtils.hash(0);
+		}
+	}
+
+	public static class Unkown extends Receiver {
+		@Override
+		public boolean equals(Object obj) {
+			return obj != null && obj instanceof Unkown;
+		}
+
+		@Override
+		public int hashCode() {
+			return HashCodeUtils.hash(1);
+		}
+	}
+
+	public static class LocalVariable extends Receiver {
+		protected Element element;
+
+		public LocalVariable(Element element) {
+			this.element = element;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (obj == null || !(obj instanceof LocalVariable)) {
+				return false;
+			}
+			LocalVariable other = (LocalVariable) obj;
+			return other.element.equals(element);
+		}
+
+		@Override
+		public int hashCode() {
+			return HashCodeUtils.hash(element);
+		}
+	}
+
+	// TODO: add pure method calls later
+	public static class PureMethodCall extends Receiver {
+	}
+
 	/**
 	 * The analysis class this store belongs to.
 	 */
@@ -31,19 +124,118 @@ public abstract class CFAbstractStore<V extends CFAbstractValue<V>, S extends CF
 	 * Information collected about local variables, which are identified by the
 	 * corresponding element.
 	 */
-	protected Map<Element, V> localVariableValues;
+	protected final Map<Element, V> localVariableValues;
+
+	/**
+	 * Information collected about fields, using the interal representation
+	 * {@link FieldAccess}.
+	 */
+	protected Map<FieldAccess, V> fieldValues;
 
 	public CFAbstractStore(CFAbstractAnalysis<V, S, ?> analysis) {
 		this.analysis = analysis;
 		localVariableValues = new HashMap<>();
+		fieldValues = new HashMap<>();
 	}
 
 	/** Copy constructor. */
-	protected CFAbstractStore(CFAbstractAnalysis<V, S, ?> analysis,
-			CFAbstractStore<V, S> other) {
-		this.analysis = analysis;
+	protected CFAbstractStore(CFAbstractStore<V, S> other) {
+		this.analysis = other.analysis;
 		localVariableValues = new HashMap<>(other.localVariableValues);
+		fieldValues = new HashMap<>(other.fieldValues);
 	}
+
+	/* --------------------------------------------------------- */
+	/* Handling of fields */
+	/* --------------------------------------------------------- */
+
+	/**
+	 * @return The internal representation (as {@link FieldAccess}) of a
+	 *         {@link FieldAccessNode}. Can contain {@link Unknown} as receiver.
+	 */
+	protected FieldAccess internalReprOf(FieldAccessNode node) {
+		Receiver receiver;
+		Node receiverNode = node.getReceiver();
+		if (receiverNode instanceof FieldAccessNode) {
+			receiver = internalReprOf((FieldAccessNode) receiverNode);
+		} else if (receiverNode instanceof ImplicitThisLiteralNode
+				|| receiverNode instanceof ExplicitThisNode) {
+			receiver = new ThisReference();
+		} else if (receiverNode instanceof LocalVariableNode) {
+			LocalVariableNode lv = (LocalVariableNode) receiverNode;
+			receiver = new LocalVariable(lv.getElement());
+		} else {
+			receiver = new Unkown();
+		}
+		return new FieldAccess(receiver, null);
+	}
+
+	/**
+	 * @return Current abstract value of a field access, or {@code null} if no
+	 *         information is available.
+	 */
+	public/* @Nullable */V getValue(FieldAccessNode n) {
+		FieldAccess fieldAccess = internalReprOf(n);
+		return fieldValues.get(fieldAccess);
+	}
+
+	/**
+	 * Update the information in the store by considering a field assignment
+	 * with target {@code n}, where the right hand side has the abstract value
+	 * {@code val}.
+	 */
+	public void updateForAssignemnt(FieldAccessNode n, V val) {
+		assert val != null;
+		FieldAccess fieldAccess = internalReprOf(n);
+		removeConflicting(fieldAccess, val);
+		if (!fieldAccess.containsUnknown()) {
+			fieldValues.put(fieldAccess, val);
+		}
+	}
+
+	/**
+	 * Remove any information in {@code fieldValues} that might not be true any
+	 * more after the fact that {@code fieldAccess} has abstract value
+	 * {@code val} will be added. This includes the following steps:
+	 * <ol>
+	 * <li value="1">Update the abstract value of other field accesses where the
+	 * field is equal, and the receiver might alias the receiver of
+	 * {@code fieldAccess}. This update will raise the abstract value for such
+	 * field accesses to at least {@code val} (or the old value, if that was
+	 * less precise).</li>
+	 * <li value="2">Remove any abstract values for field accesses where
+	 * {@code fieldAccess} appear anywhere in the receiver.</li>
+	 * </ol>
+	 */
+	protected void removeConflicting(FieldAccess fieldAccess, V val) {
+		Map<FieldAccess, V> newFieldValues = new HashMap<>();
+		for (Entry<FieldAccess, V> e : fieldValues.entrySet()) {
+			FieldAccess otherFieldAccess = e.getKey();
+			V otherVal = e.getValue();
+			// case 2:
+			if (otherFieldAccess.contains(fieldAccess)) {
+				continue; // remove information completely
+			}
+			// case 1:
+			if (fieldAccess.getField().equals(otherFieldAccess.getField())) {
+				if (canAlias(fieldAccess.getReceiver(),
+						otherFieldAccess.getReceiver())) {
+					newFieldValues.put(otherFieldAccess,
+							val.leastUpperBound(otherVal));
+				}
+			}
+		}
+		fieldValues = newFieldValues;
+	}
+
+	protected boolean canAlias(Receiver a, Receiver b) {
+		// TODO more accurate (at least include type information)
+		return true;
+	}
+
+	/* --------------------------------------------------------- */
+	/* Handling of local variables */
+	/* --------------------------------------------------------- */
 
 	/**
 	 * @return Current abstract value of a local variable, or {@code null} if no
@@ -63,17 +255,9 @@ public abstract class CFAbstractStore<V extends CFAbstractValue<V>, S extends CF
 		localVariableValues.put(n.getElement(), val);
 	}
 
-	/**
-	 * Merge in an abstract value of a local variable in the store by taking the
-	 * least upper bound of the previous value and {@code val}. Previous
-	 * information needs to be available.
-	 */
-	public void mergeValue(LocalVariableNode n, V val) {
-		Element el = n.getElement();
-		assert localVariableValues.containsKey(el);
-		V newVal = val.leastUpperBound(localVariableValues.get(el));
-		localVariableValues.put(el, newVal);
-	}
+	/* --------------------------------------------------------- */
+	/* Helper and miscellaneous methods */
+	/* --------------------------------------------------------- */
 
 	@SuppressWarnings("unchecked")
 	@Override
