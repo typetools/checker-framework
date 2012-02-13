82def80389d3f8d3d1606968c487723f98b8c681
stefanheule <stefanheule@localhost>
2012-02-13 04:23:23 -0800

    Documentation.

--- checkers/src/checkers/flow/analysis/checkers/CFStore.java
+++ checkers/src/checkers/flow/analysis/checkers/CFStore.java
@@ -1,5 +1,11 @@
 package checkers.flow.analysis.checkers;
 
+/**
+ * The default store used in the Checker Framework.
+ * 
+ * @author Stefan Heule
+ * 
+ */
 public class CFStore extends CFAbstractStore<CFValue, CFStore> {
 
 	public CFStore(CFAbstractAnalysis<CFValue, CFStore, ?> analysis) {
