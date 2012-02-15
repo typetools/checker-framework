3524149d5710fdfbfaf9fd992e9f405d4b89bef2
Eric Spishak <espishak@cs.washington.edu>
2012-02-15 01:51:16 -0800

    Correctly handle annotations with values in flow.

---
+++ checkers/tests/flow/Values.java
@@ -0,0 +1,52 @@
+import checkers.util.test.*;
+
+public class Values {
+  
+  void test() {
+    Object o = get();
+    Object o1 = get1();
+    Object o2 = get2();
+    foo1(o1);
+    foo2(o2);
+    
+    //:: error: (argument.type.incompatible)
+    foo1(o);
+    //:: error: (argument.type.incompatible)
+    foo2(o1);
+    //:: error: (argument.type.incompatible)
+    foo1(o2);
+    //:: error: (argument.type.incompatible)
+    foo(o2);
+    
+    o1 = o2;
+    foo2(o1);
+    //:: error: (argument.type.incompatible)
+    foo1(o1);
+    
+    o2 = get1();
+    foo1(o2);
+    //:: error: (argument.type.incompatible)
+    foo2(o2);
+  }
+
+  void foo(@Value Object o) {}
+  
+  void foo1(@Value(1) Object o) {}
+  
+  void foo2(@Value(2) Object o) {}
+  
+  @SuppressWarnings("flowtest:return.type.incompatible")
+  @Value Object get() {
+    return null;
+  }
+  
+  @SuppressWarnings("flowtest:return.type.incompatible")
+  @Value(1) Object get1() {
+    return null;
+  }
+  
+  @SuppressWarnings("flowtest:return.type.incompatible")
+  @Value(2) Object get2() {
+    return null;
+  }
+}
