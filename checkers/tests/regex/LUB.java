8a86830a6ee00b354a339d21618f2a90cfe61df5
Eric Spishak <espishak@cs.washington.edu>
2012-02-28 01:23:29 -0800

    Bug fix and tests for NullPointerException in GenKillBits.andlub having to do with annotations with values.

--- checkers/tests/regex/LUB.java
+++ checkers/tests/regex/LUB.java
@@ -23,4 +23,30 @@ public class LUB {
         //:: error: (assignment.type.incompatible)
         @Regex(3) String test2 = s;
     }
+
+    void test3(@Regex(6) String s6, boolean b) {
+        String s;
+        if (b) {
+            s = s6;
+        } else {
+            s = null;
+        }
+        @Regex(6) String test = s;
+
+        //:: error: (assignment.type.incompatible)
+        @Regex(7) String test2 = s;
+    }
+
+    void test4(@Regex(8) String s8, @Regex(9) String s9, boolean b) {
+        String s;
+        if (b) {
+            s = s8;
+        } else {
+            s = s9;
+        }
+        @Regex(8) String test = s;
+
+        //:: error: (assignment.type.incompatible)
+        @Regex(9) String test2 = s;
+    }
 }
