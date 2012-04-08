f10469f53867b7103489f7c65b1fd8b231af3907
stefanheule <stefanheule@localhost>
2012-04-08 21:25:26 +0200

    Tests for methods (pure and non-pure).

--- checkers/tests/flow2/Basic.java
+++ checkers/tests/flow2/Basic.java
@@ -1,5 +1,6 @@
 import checkers.util.test.*;
 import java.util.*;
+import checkers.quals.*;
 
 class Basic {
 
@@ -63,12 +64,13 @@ class Basic {
     }*/
     
     class C {
+        C c;
         String f1, f2, f3;
         @Odd String g1, g2, g3;
     }
     
     // fields
-    void t3(@Odd String p1, String p2, boolean b1, C c1, C c2) {
+    void t5(@Odd String p1, String p2, boolean b1, C c1, C c2) {
         c1.f1 = p1;
         @Odd String l1 = c1.f1;
         c2.f2 = p2; // assignment to f2 does not change knowledge about f1
@@ -77,5 +79,38 @@ class Basic {
         c2.f1 = p2;
         //:: error: (assignment.type.incompatible)
         @Odd String l3 = c1.f1;
+    }
+    
+    // fields
+    void t6(@Odd String p1, String p2, boolean b1, C c1, C c2) {
+        c1.f1 = p1;
+        c1.c.f1 = p1;
+        @Odd String l2 = c1.c.f1;
+        c1.f1 = p2;
+        //:: error: (assignment.type.incompatible)
+        @Odd String l3 = c1.f1;
+        
+        c1.f1 = p1;
+        c1.c.f1 = p1;
+        @Odd String l4 = c1.c.f1;
+        c2.c = c2;
+        //:: error: (assignment.type.incompatible)
+        @Odd String l5 = c1.f1;
+        //:: error: (assignment.type.incompatible)
+        @Odd String l6 = c1.c.f1;
+    }
+    
+    // method calls
+    void nonpure() {}
+    @Pure int pure() { return 1; }
+    void t7(@Odd String p1, String p2, boolean b1, C c1, C c2) {
+        c1.f1 = p1;
+        nonpure();
+        //:: error: (assignment.type.incompatible)
+        @Odd String l1 = c1.f1;
+        
+        c1.f1 = p1;
+        pure();
+        @Odd String l2 = c1.f1;
     }
 }
