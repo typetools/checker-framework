7c52a0f15a63ed4fdea244e7a64903de04619723
wdietl <wdietl@gmail.com>
2012-04-16 09:11:29 -0700

    Report Checker: add new command-line option reportModifiers to search for specific modifiers, e.g. strictfp.

---
+++ checkers/tests/src/tests/ReportModifiersTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import java.io.File;
+import java.util.Collection;
+
+import org.junit.runners.Parameterized.Parameters;
+
+public class ReportModifiersTest extends ParameterizedCheckerTest {
+
+    public ReportModifiersTest(File testFile) {
+        super(testFile, checkers.util.report.ReportChecker.class.getName(), "report",
+                "-Anomsgtext", "-AreportModifiers=native");
+    }
+
+    @Parameters
+    public static Collection<Object[]> data() { return testFiles("reportmodifiers"); }
+}
