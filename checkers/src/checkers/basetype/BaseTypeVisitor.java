694ad43950dce186e723b4ee281af06cbdbe1509
wdietl <wdietl@gmail.com>
2012-04-16 08:41:46 -0700

    SkipDefs and SkipUses should also be respected for checks performed in flow. Refactor how the skip patterns are checked. Refine NullnessFlow to look into skipXXX.

--- checkers/src/checkers/basetype/BaseTypeVisitor.java
+++ checkers/src/checkers/basetype/BaseTypeVisitor.java
@@ -124,7 +124,7 @@ public class BaseTypeVisitor<Checker extends BaseTypeChecker> extends SourceVisi
 
     @Override
     public Void visitClass(ClassTree node, Void p) {
-        if (shouldSkipDefs(node)) {
+        if (checker.shouldSkipDefs(node)) {
             // Not "return super.visitClass(node, p);" because that would
             // recursively call visitors on subtrees; we want to skip the
             // class entirely.
@@ -397,7 +397,7 @@ public class BaseTypeVisitor<Checker extends BaseTypeChecker> extends SourceVisi
      */
     @Override
     public Void visitNewClass(NewClassTree node, Void p) {
-        if (shouldSkipUses(InternalUtils.constructor(node)))
+        if (checker.shouldSkipUses(InternalUtils.constructor(node)))
             return super.visitNewClass(node, p);
 
         Pair<AnnotatedExecutableType, List<AnnotatedTypeMirror>> fromUse = atypeFactory.constructorFromUse(node);
@@ -911,7 +911,7 @@ public class BaseTypeVisitor<Checker extends BaseTypeChecker> extends SourceVisi
             AnnotatedDeclaredType overriddenType,
             Void p) {
 
-        if (shouldSkipUses(overriddenType.getElement())) {
+        if (checker.shouldSkipUses(overriddenType.getElement())) {
             return true;
         }
 
@@ -1131,7 +1131,7 @@ public class BaseTypeVisitor<Checker extends BaseTypeChecker> extends SourceVisi
 
         @Override
         public Void visitDeclared(AnnotatedDeclaredType type, Tree tree) {
-            if (shouldSkipUses(type.getElement()))
+            if (checker.shouldSkipUses(type.getElement()))
                 return super.visitDeclared(type, tree);
 
             {
@@ -1257,7 +1257,7 @@ public class BaseTypeVisitor<Checker extends BaseTypeChecker> extends SourceVisi
 
         @Override
         public Void visitPrimitive(AnnotatedPrimitiveType type, Tree tree) {
-            if (shouldSkipUses(type.getElement()))
+            if (checker.shouldSkipUses(type.getElement()))
                 return super.visitPrimitive(type, tree);
 
             if (!checker.isValidUse(type)) {
@@ -1283,7 +1283,7 @@ public class BaseTypeVisitor<Checker extends BaseTypeChecker> extends SourceVisi
                 return null;
 
             final TypeElement element = (TypeElement) type.getUnderlyingType().asElement();
-            if (shouldSkipUses(element))
+            if (checker.shouldSkipUses(element))
                 return null;
 
             List<AnnotatedTypeVariable> typevars = atypeFactory.typeVariablesFromUse(type, element);
@@ -1397,36 +1397,7 @@ public class BaseTypeVisitor<Checker extends BaseTypeChecker> extends SourceVisi
         // }
 
         Element elm = InternalUtils.symbol(exprTree);
-        return shouldSkipUses(elm);
-    }
-
-    /**
-     * Tests whether the class owner of the passed element is an unannotated
-     * class and matches the pattern specified in the
-     * {@code checker.skipUses} property.
-     *
-     * @param element   an element
-     * @return  true iff the enclosing class of element should be skipped
-     */
-    protected final boolean shouldSkipUses(Element element) {
-        if (element == null)
-            return false;
-        TypeElement typeElement = ElementUtils.enclosingClass(element);
-        String name = typeElement.getQualifiedName().toString();
-        return checker.getShouldSkipUses().matcher(name).find();
-
-    }
-
-    /**
-     * Tests whether the class definition should not be checked because it
-     * matches the {@code checker.skipDefs} property.
-     *
-     * @param node class to potentially skip
-     * @return true if checker should not test node
-     */
-    protected final boolean shouldSkipDefs(ClassTree node) {
-        String qualifiedName = InternalUtils.typeOf(node).toString();
-        return checker.getShouldSkipDefs().matcher(qualifiedName).find();
+        return checker.shouldSkipUses(elm);
     }
 
 
