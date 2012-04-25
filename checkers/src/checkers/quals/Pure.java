69a936b6a71d0573715deb21f1b3294efb911e7c
stefanheule <stefanheule@localhost>
2012-04-26 01:54:01 +0200

    Documentation of @Pure and PurityChecker.

--- checkers/src/checkers/quals/Pure.java
+++ checkers/src/checkers/quals/Pure.java
@@ -8,9 +8,15 @@ import java.lang.annotation.Target;
 
 /**
  * {@code Pure} is a method annotation that indicates the <em>purity</em> of
- * that method. A method is said to be pure if..
- * <p>
- * TODO: formalize notation of purity and
+ * that method. A method is said to be pure if it does not contain any of the
+ * following Java constructs:
+ * <ol>
+ * <li>Assignment to any expression, except for local variables (including
+ * method parameters).
+ * <li>A method invocation of a method that is not pure itself (as indicated by
+ * a {@link Pure} annotation on that methods declaration).
+ * <li>Construction of a new object.
+ * </ol>
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
