c98fbe576948d201f053da453bbca72aa6ee468c
wdietl <wdietl@gmail.com>
2012-04-28 19:53:15 -0700

    Special-case two common patterns for testing whether a string is a regular expression. This class will be removed again and replaced by something more general very soon.

---
+++ checkers/src/checkers/regex/RegexFlow.java
@@ -0,0 +1,81 @@
+package checkers.regex;
+
+import java.util.Set;
+
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.VariableElement;
+
+import com.sun.source.tree.*;
+
+import checkers.basetype.BaseTypeChecker;
+import checkers.flow.DefaultFlow;
+import checkers.flow.DefaultFlowState;
+import checkers.types.AnnotatedTypeFactory;
+import checkers.util.TreeUtils;
+
+public class RegexFlow extends DefaultFlow<DefaultFlowState> {
+
+    public RegexFlow(BaseTypeChecker checker, CompilationUnitTree root,
+            Set<AnnotationMirror> annotations, AnnotatedTypeFactory factory) {
+        super(checker, root, annotations, factory);
+    }
+
+    @Override
+    protected void scanCond(ExpressionTree tree) {
+        if (debug != null) {
+            debug.println("RegexFlow::scanCond: " + tree);
+        }
+
+        super.scanCond(tree);
+
+        tree = TreeUtils.skipParens(tree);
+
+        RegexChecker rec = (RegexChecker) this.checker;
+
+        if (tree.getKind() == Tree.Kind.METHOD_INVOCATION &&
+                isRegex((MethodInvocationTree)tree)) {
+            VariableElement arg = extractArg((MethodInvocationTree)tree);
+            if (arg!=null) {
+                int idx = flowState_whenTrue.vars.indexOf(arg);
+                if (idx >= 0) {
+                    flowState_whenTrue.annos.clearInAll(idx);
+                    flowState_whenTrue.annos.set(rec.REGEX, idx);
+                    flowState_whenFalse.annos.clear(rec.REGEX, idx);
+                }
+            }
+        } else if (tree.getKind() == Tree.Kind.LOGICAL_COMPLEMENT) {
+            tree = ((UnaryTree)tree).getExpression();
+            if (tree.getKind() == Tree.Kind.METHOD_INVOCATION &&
+                    isRegex((MethodInvocationTree)tree)) {
+                VariableElement arg = extractArg((MethodInvocationTree)tree);
+                if (arg!=null) {
+                    int idx = flowState_whenTrue.vars.indexOf(arg);
+                    if (idx >= 0) {
+                        flowState_whenTrue.annos.clear(rec.REGEX, idx);
+                        flowState_whenFalse.annos.clearInAll(idx);
+                        flowState_whenFalse.annos.set(rec.REGEX, idx);
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean isRegex(MethodInvocationTree tree) {
+        ExecutableElement method = TreeUtils.elementFromUse(tree);
+        String sig = method.getEnclosingElement().toString() + "." + method.toString();
+        return sig.equals("checkers.regex.RegexUtil.isRegex(java.lang.String)");
+    }
+
+    private VariableElement extractArg(MethodInvocationTree tree) {
+        ExpressionTree argtree = tree.getArguments().get(0);
+        Element arg = TreeUtils.elementFromUse(argtree);
+        if (arg!=null && arg instanceof VariableElement) {
+            return (VariableElement) arg;
+        } else {
+            return null;
+        }
+    }
+
+}
