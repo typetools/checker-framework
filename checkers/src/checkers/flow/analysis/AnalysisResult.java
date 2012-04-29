06e0d16ebf774dd53e09b18be37649daaaa9be90
stefanheule <stefanheule@localhost>
2012-04-29 15:37:22 +0200

    Remove cache for tree-values. This caused problems when items where cached too early.

--- checkers/src/checkers/flow/analysis/AnalysisResult.java
+++ checkers/src/checkers/flow/analysis/AnalysisResult.java
@@ -23,9 +23,6 @@ public class AnalysisResult<A extends AbstractValue<A>> {
     /** Abstract values of nodes. */
     final protected Map<Node, A> nodeValues;
 
-    /** Abstract values of tree nodes (used as cache). */
-    final protected Map<Tree, A> treeValues;
-    
     /** Map from AST {@link Tree}s to {@link Node}s. */
     final protected Map<Tree, Node> treeLookup;
 
@@ -34,7 +31,6 @@ public class AnalysisResult<A extends AbstractValue<A>> {
      */
     public AnalysisResult(Map<Node, A> nodeValues, Map<Tree, Node> treeLookup) {
         this.nodeValues = new IdentityHashMap<>(nodeValues);
-        treeValues = new IdentityHashMap<>();
         this.treeLookup = new IdentityHashMap<>(treeLookup);
     }
 
@@ -43,7 +39,6 @@ public class AnalysisResult<A extends AbstractValue<A>> {
      */
     public AnalysisResult() {
         nodeValues = new IdentityHashMap<>();
-        treeValues = new IdentityHashMap<>();
         treeLookup = new IdentityHashMap<>();
     }
 
@@ -53,9 +48,6 @@ public class AnalysisResult<A extends AbstractValue<A>> {
     public void combine(AnalysisResult<A> other) {
         for (Entry<Node, A> e : other.nodeValues.entrySet()) {
             nodeValues.put(e.getKey(), e.getValue());
-        }
-        for (Entry<Tree, A> e : other.treeValues.entrySet()) {
-            treeValues.put(e.getKey(), e.getValue());
         }
         for (Entry<Tree, Node> e : other.treeLookup.entrySet()) {
             treeLookup.put(e.getKey(), e.getValue());
@@ -75,11 +67,7 @@ public class AnalysisResult<A extends AbstractValue<A>> {
      *         no information is available.
      */
     public/* @Nullable */A getValue(Tree t) {
-        if (treeValues.containsKey(t)) {
-            return treeValues.get(t);
-        }
         A val = getValue(treeLookup.get(t));
-        treeValues.put(t, val);
         return val;
     }
 }
