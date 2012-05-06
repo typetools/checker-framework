5128aa826da5d9f64357b9dd39931d14bcb2d1c6
stefanheule <stefanheule@localhost>
2012-05-07 00:26:19 +0200

    Add test for System.exit and @TerminatesExecution.

--- checkers/tests/flow2/Termination.java
+++ checkers/tests/flow2/Termination.java
@@ -8,13 +8,22 @@ class Termination {
     @TerminatesExecution
     void exit() {}
     
-    // basic local variable flow sensitivity
-    void t2(@Odd String p1, String p2, boolean b1) {
+    void t1(@Odd String p1, String p2, boolean b1) {
         String l1 = p2;
         if (b1) {
             l1 = p1;
         } else {
             exit();
+        }
+        @Odd String l3 = l1;
+    }
+    
+    void t2(@Odd String p1, String p2, boolean b1) {
+        String l1 = p2;
+        if (b1) {
+            l1 = p1;
+        } else {
+            System.exit(0);
         }
         @Odd String l3 = l1;
     }
