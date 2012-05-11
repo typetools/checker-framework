6d5031b86a332c596172ab1f65e4cd798c587e15
stefanheule <stefanheule@localhost>
2012-05-12 00:11:10 +0200

    Introduce a CompoundAssignemntNode to get better abstractions.

--- checkers/src/checkers/flow/cfg/node/NumericalMultiplicationAssignmentNode.java
+++ checkers/src/checkers/flow/cfg/node/NumericalMultiplicationAssignmentNode.java
@@ -1,11 +1,5 @@
 package checkers.flow.cfg.node;
 
-import java.util.Collection;
-import java.util.LinkedList;
-
-import checkers.flow.util.HashCodeUtils;
-import checkers.util.InternalUtils;
-
 import com.sun.source.tree.Tree;
 import com.sun.source.tree.Tree.Kind;
 
@@ -20,32 +14,12 @@ import com.sun.source.tree.Tree.Kind;
  * @author Charlie Garrett
  * 
  */
-public class NumericalMultiplicationAssignmentNode extends Node {
-
-    protected Tree tree;
-    protected Node left;
-    protected Node right;
+public class NumericalMultiplicationAssignmentNode extends CompoundAssignmentNode {
 
     public NumericalMultiplicationAssignmentNode(Tree tree, Node left,
             Node right) {
+        super(tree, right, right);
         assert tree.getKind() == Kind.MULTIPLY_ASSIGNMENT;
-        this.tree = tree;
-        this.type = InternalUtils.typeOf(tree);
-        this.left = left;
-        this.right = right;
-    }
-
-    public Node getLeftOperand() {
-        return left;
-    }
-
-    public Node getRightOperand() {
-        return right;
-    }
-
-    @Override
-    public Tree getTree() {
-        return tree;
     }
 
     @Override
@@ -57,28 +31,5 @@ public class NumericalMultiplicationAssignmentNode extends Node {
     public String toString() {
         return "(" + getLeftOperand() + " *= " + getRightOperand() + ")";
     }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null
-                || !(obj instanceof NumericalMultiplicationAssignmentNode)) {
-            return false;
-        }
-        NumericalMultiplicationAssignmentNode other = (NumericalMultiplicationAssignmentNode) obj;
-        return getLeftOperand().equals(other.getLeftOperand())
-                && getRightOperand().equals(other.getRightOperand());
-    }
-
-    @Override
-    public int hashCode() {
-        return HashCodeUtils.hash(getLeftOperand(), getRightOperand());
-    }
-
-    @Override
-    public Collection<Node> getOperands() {
-        LinkedList<Node> list = new LinkedList<Node>();
-        list.add(getLeftOperand());
-        list.add(getRightOperand());
-        return list;
-    }
+    
 }
