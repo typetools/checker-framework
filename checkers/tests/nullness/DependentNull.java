36b39550b0a5e7c3daa870d72659d3a74223eec7
Werner Dietl <wdietl@gmail.com>
2012-05-20 16:43:48 -0700

    Adapt to new @Target rules.

--- checkers/tests/nullness/DependentNull.java
+++ checkers/tests/nullness/DependentNull.java
@@ -1,3 +1,5 @@
+import java.lang.annotation.*;
+
 import checkers.nullness.quals.*;
 import checkers.quals.*;
 
@@ -7,6 +9,7 @@ public class DependentNull {
      * NOTE that @Prototype is a SUPERTYPE of an unannotated reference.
      * (Uh, how does the checker know that?  It's important to the checking!)
      */
+    @Target(ElementType.TYPE_USE)
     @interface Prototype {}
 
 
