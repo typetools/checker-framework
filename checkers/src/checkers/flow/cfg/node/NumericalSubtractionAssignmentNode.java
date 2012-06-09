f7cbc2dabca423e4eb7ef202b6a7d909b1fed348
stefanheule <devnull@localhost>
2012-06-09 18:22:33 +0200

    Fix invalid constructor invokation of all CompoundAssignmentNodes.

--- checkers/src/checkers/flow/cfg/node/NumericalSubtractionAssignmentNode.java
+++ checkers/src/checkers/flow/cfg/node/NumericalSubtractionAssignmentNode.java
@@ -17,7 +17,7 @@ import com.sun.source.tree.Tree.Kind;
 public class NumericalSubtractionAssignmentNode extends CompoundAssignmentNode {
 
     public NumericalSubtractionAssignmentNode(Tree tree, Node left, Node right) {
-        super(tree, right, right);
+        super(tree, left, right);
         assert tree.getKind() == Kind.MINUS_ASSIGNMENT;
     }
 
--- checkers/src/checkers/flow/cfg/node/SignedRightShiftAssignmentNode.java
+++ checkers/src/checkers/flow/cfg/node/SignedRightShiftAssignmentNode.java
@@ -17,7 +17,7 @@ import com.sun.source.tree.Tree.Kind;
 public class SignedRightShiftAssignmentNode extends CompoundAssignmentNode {
 
     public SignedRightShiftAssignmentNode(Tree tree, Node left, Node right) {
-        super(tree, right, right);
+        super(tree, left, right);
         assert tree.getKind() == Kind.RIGHT_SHIFT_ASSIGNMENT;
     }
 
