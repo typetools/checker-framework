21e8a3420361d2815d80c8ede82e44c5309d3c24
stefanheule <stefanheule@localhost>
2012-06-29 19:40:40 +0200

    New target AllFlowTests that groups all the unit tests that are supposed to pass in the new dataflow framework. Over time, more test targets can be added, and in the end this class can be removed.

---
+++ checkers/tests/src/tests/AllFlowTests.java
@@ -0,0 +1,10 @@
+package tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({Flow2Test.class, RegexTest.class, I18nTest.class})
+public class AllFlowTests {
+}
