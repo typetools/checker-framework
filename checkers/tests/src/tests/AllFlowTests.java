e026d664e38dbb26e21612b7dada5c7c5e460585
stefanheule <devnull@localhost>
2012-07-04 18:15:35 +0200

    Add the purity suggestion test target to the AllFlowTests target.

--- checkers/tests/src/tests/AllFlowTests.java
+++ checkers/tests/src/tests/AllFlowTests.java
@@ -5,6 +5,6 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({Flow2Test.class, RegexTest.class, I18nTest.class})
+@SuiteClasses({Flow2Test.class, RegexTest.class, I18nTest.class, PuritySuggestionsTest.class})
 public class AllFlowTests {
 }
