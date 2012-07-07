0ecbeef6915eb467820c6917cd710f9a3af58e3d
Werner Dietl <wdietl@gmail.com>
2012-07-07 13:31:28 -0700

    Add missing @Target annotations required in 1.3.

--- checkers/src/checkers/commitment/quals/Committed.java
+++ checkers/src/checkers/commitment/quals/Committed.java
@@ -1,8 +1,10 @@
 package checkers.commitment.quals;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 import checkers.quals.DefaultQualifierInHierarchy;
 import checkers.quals.SubtypeOf;
@@ -10,6 +12,7 @@ import checkers.quals.TypeQualifier;
 
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 @TypeQualifier
 @SubtypeOf(Unclassified.class)
 @DefaultQualifierInHierarchy
