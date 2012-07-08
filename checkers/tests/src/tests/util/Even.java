f5b0aa91b9d93237378373e6482c0e40eb6cb6cb
wdietl <wdietl@gmail.com>
2012-07-08 00:53:20 -0700

    Allow these annotations on type parameters.

--- checkers/tests/src/tests/util/Even.java
+++ checkers/tests/src/tests/util/Even.java
@@ -4,9 +4,6 @@ import java.lang.annotation.Inherited;
 import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
 
-import com.sun.source.tree.Tree;
-
-import checkers.quals.ImplicitFor;
 import checkers.quals.SubtypeOf;
 import checkers.quals.TypeQualifier;
 import checkers.quals.Unqualified;
@@ -14,5 +11,5 @@ import checkers.quals.Unqualified;
 @TypeQualifier
 @Inherited
 @SubtypeOf(Unqualified.class)
-@Target(ElementType.TYPE_USE)
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 public @interface Even {}
