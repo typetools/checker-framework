e3e029412020bce9d70826fc2edc3ceecedad552
stefanheule <stefanheule@localhost>
2012-07-14 22:36:14 +0200

    Formatting only: use spaces in the nonnull tests.

--- checkers/tests/nonnull/MultiConstructorInit.java
+++ checkers/tests/nonnull/MultiConstructorInit.java
@@ -3,23 +3,27 @@ import checkers.commitment.quals.*;
 import static checkers.nonnull.util.NonNullUtils.*;
 
 class MultiConstructorInit {
-	
-	String a;
-	
-	public MultiConstructorInit(boolean t) {
-		a = "";
-	}
-	
-	public MultiConstructorInit() {
-		this(true);
-	}
-	
-	//:: error: (commitment.fields.uninitialized)
-	public MultiConstructorInit(int t) {
-		new MultiConstructorInit();
-	}
-	
-	public static void main(String[] args) {
-		new MultiConstructorInit();
-	}
+    
+    String a;
+    
+    public MultiConstructorInit(boolean t) {
+        a = "";
+    }
+    
+    public MultiConstructorInit() {
+        this(true);
+    }
+    
+    //:: error: (commitment.fields.uninitialized)
+    public MultiConstructorInit(int t) {
+        new MultiConstructorInit();
+    }
+    
+    //:: error: (commitment.fields.uninitialized)
+    public MultiConstructorInit(float t) {
+    }
+    
+    public static void main(String[] args) {
+        new MultiConstructorInit();
+    }
 }
