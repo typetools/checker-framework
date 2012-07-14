539d9e2e42c2c77ff5ac2908cc794ff33f7fae26
stefanheule <devnull@localhost>
2012-07-14 22:36:14 +0200

    Formatting only: use spaces in the nonnull tests.

--- checkers/tests/nonnull/Lazy.java
+++ checkers/tests/nonnull/Lazy.java
@@ -3,31 +3,31 @@ import checkers.commitment.quals.*;
 import checkers.nonnull.quals.*;
 
 public class Lazy {
-	
-	@NonNull String f;
-	@LazyNonNull String g;
-	
-	public Lazy() {
-		f = "";
-		// does not have to initialize g
-	}
-	
-	void test() {
-		g = "";
-		test2(); // retain non-null property across method calls
-		g.toLowerCase();
-	}
-	
-	void test2() {
-	}
-	
-	void test3() {
-		//:: error: (dereference.of.nullable)
-		g.toLowerCase();
-	}
-	
-	void test4() {
-		//:: error: (lazynonnull.null.assignment)
-		g = null;
-	}
+    
+    @NonNull String f;
+    @LazyNonNull String g;
+    
+    public Lazy() {
+        f = "";
+        // does not have to initialize g
+    }
+    
+    void test() {
+        g = "";
+        test2(); // retain non-null property across method calls
+        g.toLowerCase();
+    }
+    
+    void test2() {
+    }
+    
+    void test3() {
+        //:: error: (dereference.of.nullable)
+        g.toLowerCase();
+    }
+    
+    void test4() {
+        //:: error: (lazynonnull.null.assignment)
+        g = null;
+    }
 }
