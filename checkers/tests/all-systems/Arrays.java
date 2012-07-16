b30c617c21e2e311d653ded51f76d90a3973704b
wdietl <wdietl@gmail.com>
2012-07-16 02:02:11 -0700

    Undo previous changes that are no longer necessary.

--- checkers/tests/all-systems/Arrays.java
+++ checkers/tests/all-systems/Arrays.java
@@ -1,17 +1,11 @@
 import checkers.nullness.quals.*;
-import checkers.oigj.quals.*;
-import checkers.signature.quals.*;
-import checkers.i18n.quals.*;
-import checkers.regex.quals.*;
-import checkers.interning.quals.*;
-import checkers.tainting.quals.*;
 
 class Arrays {
-    public static final @Untainted @Interned @Regex(0) @SourceNameForNonArray String[] RELATIONSHIP_LABELS =
+    public static final String[] RELATIONSHIP_LABELS =
         { "SJJ", "SJU", "SUJ", "SUU", "DJJ", "DJU", "DUJ", "DUU",
         "JM", "UM", "MJ", "MU" };
 
-    public static final @Localized int[] ia = { 1, -2, 3 };
+    public static final int[] ia = { 1, -2, 3 };
 
     // Note that "-1.0" is _NOT_ a double literal! It's a unary minus,
     // that needs to be handled correctly.
@@ -23,7 +17,7 @@ class Arrays {
         class VarInfo {}
         VarInfo v1 = null;
         VarInfo v2 = null;
-        @Mutable @Nullable VarInfo[] vis = null;
+        @Nullable VarInfo[] vis = null;
 
         if (v2 == null)
             vis = new @Nullable VarInfo[] {v1};
