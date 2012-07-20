93f3f146a3e0fee89028ff4a86213ce08cc6bee1
wdietl <wdietl@gmail.com>
2012-07-20 01:17:36 -0700

    Adapt test cases to allow covariant array subtyping. Mark all disabled tests using TODOINVARR. This changeset will need to get undone once we switch to invariant arrays.

--- checkers/tests/nullness/generics/GenericsBounds2.java
+++ checkers/tests/nullness/generics/GenericsBounds2.java
@@ -16,14 +16,14 @@ public class GenericsBounds2<X extends @Nullable Object> {
 
         // This error is expected when arrays are invariant.
         // Currently, this error is not raised.
-        //:: error: (assignment.type.incompatible)
+        //TODOINVARR:: error: (assignment.type.incompatible)
         a2 = a1;
         a2[0] = null;
     }
 
     void aaa(@Nullable Object[] p1, @NonNull Object[] p2) {
         // This one is only expected when we switch the default for arrays to be invariant.
-        //:: error: (assignment.type.incompatible)
+        //TODOINVARR:: error: (assignment.type.incompatible)
         p1 = p2;
     }
 }
