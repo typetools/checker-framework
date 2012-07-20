eea788c69af97f605afafd353c5e9d49eab0e2e8
Michael Ernst <mernst@cs.washington.edu>
2012-07-20 08:03:01 -0700

    Fix Javadoc errors

--- checkers/src/checkers/util/QualifierPolymorphism.java
+++ checkers/src/checkers/util/QualifierPolymorphism.java
@@ -23,7 +23,7 @@ import com.sun.source.tree.*;
 /**
  * Implements framework support for type qualifier polymorphism. Checkers that
  * wish to use it should add calls to
- * {@link #annotate(MethodInvocationTree, AnnotatedTypeMirror.AnnotatedExecutableType) annotate(MethodInvocationTree, AnnotatedExecutableType)}
+ * {@link #annotate(MethodInvocationTree, AnnotatedTypeMirror.AnnotatedExecutableType)}, {@link #annotate(MethodInvocationTree, AnnotatedExecutableType)}, and
  * {@link #annotate(Element, AnnotatedTypeMirror)} to the
  * {@link AnnotatedTypeFactory#annotateImplicit(Tree, AnnotatedTypeMirror)} and
  * {@link AnnotatedTypeFactory#annotateImplicit(Tree, AnnotatedTypeMirror)}
