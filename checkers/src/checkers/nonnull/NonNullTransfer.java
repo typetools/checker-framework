bd7f159a6b15f72f0ee60bd93ded4781fb4451ae
stefanheule <stefanheule@localhost>
2012-07-21 15:38:54 +0200

    Correctly infer information about expressions if they are compared against "null" in the non-null transfer function.

--- checkers/src/checkers/nonnull/NonNullTransfer.java
+++ checkers/src/checkers/nonnull/NonNullTransfer.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 
@@ -13,13 +14,18 @@ import checkers.commitment.CommitmentChecker;
 import checkers.commitment.CommitmentStore;
 import checkers.commitment.CommitmentTransfer;
 import checkers.flow.analysis.ConditionalTransferResult;
+import checkers.flow.analysis.FlowExpressions;
+import checkers.flow.analysis.TransferResult;
 import checkers.flow.analysis.FlowExpressions.FieldAccess;
+import checkers.flow.analysis.FlowExpressions.Receiver;
 import checkers.flow.analysis.FlowExpressions.ThisReference;
+import checkers.flow.analysis.checkers.CFAbstractStore;
 import checkers.flow.analysis.checkers.CFValue;
 import checkers.flow.cfg.UnderlyingAST;
 import checkers.flow.cfg.node.LocalVariableNode;
 import checkers.flow.cfg.node.MethodInvocationNode;
 import checkers.flow.cfg.node.Node;
+import checkers.flow.cfg.node.NullLiteralNode;
 import checkers.flow.cfg.node.ThisLiteralNode;
 import checkers.nonnull.quals.NonNull;
 import checkers.types.AnnotatedTypeMirror;
@@ -39,7 +45,7 @@ import com.sun.source.tree.VariableTree;
  * non-null, that field can safely be considered initialized.
  * <li>All non-null fields with an initializer can be considered initialized.
  * </ol>
- * 
+ *
  * @author Stefan Heule
  */
 public class NonNullTransfer extends CommitmentTransfer<NonNullTransfer> {
@@ -112,5 +118,40 @@ public class NonNullTransfer extends CommitmentTransfer<NonNullTransfer> {
                 store.addInitializedField(field.getField());
             }
         }
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>
+     * Furthermore, this method refines the type to {@code NonNull} for the
+     * appropriate branch if an expression is compared to the {@code null}
+     * literal.
+     */
+    @Override
+    protected TransferResult<CFValue, CommitmentStore> strengthenAnnotationOfEqualTo(
+            TransferResult<CFValue, CommitmentStore> res, Node firstNode,
+            Node secondNode, CFValue firstValue, CFValue secondValue,
+            boolean notEqualTo) {
+        res = super.strengthenAnnotationOfEqualTo(res, firstNode, secondNode,
+                firstValue, secondValue, notEqualTo);
+        if (firstNode instanceof NullLiteralNode) {
+            Receiver secondInternal = FlowExpressions.internalReprOf(
+                    analysis.getFactory(), secondNode);
+            if (CFAbstractStore.canInsertReceiver(secondInternal)) {
+                CommitmentStore thenStore = res.getThenStore();
+                CommitmentStore elseStore = res.getElseStore();
+                AnnotationMirror nonNull = analysis.getFactory()
+                        .annotationFromClass(NonNull.class);
+                if (notEqualTo) {
+                    thenStore.insertValue(secondInternal, nonNull);
+                } else {
+                    elseStore.insertValue(secondInternal, nonNull);
+                }
+                return new ConditionalTransferResult<>(res.getResultValue(),
+                        thenStore, elseStore);
+            }
+        }
+        return res;
     }
 }
