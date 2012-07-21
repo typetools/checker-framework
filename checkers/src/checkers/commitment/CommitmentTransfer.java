3c8a32f9100665e8ca8eadb8c6186e5c5c0b16a5
stefanheule <stefanheule@localhost>
2012-07-21 20:56:44 +0200

    Move much of the code in NonNullTranfser to CommitmentTransfer as it was dealing with field initialization (which is something the commitment type system takes care of).

--- checkers/src/checkers/commitment/CommitmentTransfer.java
+++ checkers/src/checkers/commitment/CommitmentTransfer.java
@@ -1,10 +1,19 @@
 package checkers.commitment;
 
-import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.ElementFilter;
 
 import checkers.flow.analysis.ConditionalTransferResult;
 import checkers.flow.analysis.FlowExpressions;
@@ -17,11 +26,20 @@ import checkers.flow.analysis.TransferResult;
 import checkers.flow.analysis.checkers.CFAbstractAnalysis;
 import checkers.flow.analysis.checkers.CFAbstractTransfer;
 import checkers.flow.analysis.checkers.CFAbstractValue;
-import checkers.flow.analysis.checkers.CFValue;
 import checkers.flow.analysis.checkers.CFAbstractValue.InferredAnnotation;
+import checkers.flow.analysis.checkers.CFValue;
+import checkers.flow.cfg.UnderlyingAST;
 import checkers.flow.cfg.node.AssignmentNode;
 import checkers.flow.cfg.node.FieldAccessNode;
+import checkers.flow.cfg.node.LocalVariableNode;
 import checkers.flow.cfg.node.MethodInvocationNode;
+import checkers.flow.cfg.node.Node;
+import checkers.flow.cfg.node.ThisLiteralNode;
+import checkers.types.AnnotatedTypeMirror;
+import checkers.util.TreeUtils;
+
+import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.MethodInvocationTree;
 
 /**
  * A transfer function that extends {@link CFAbstractTransfer} and tracks
@@ -45,6 +63,98 @@ public class CommitmentTransfer<T extends CommitmentTransfer<T>> extends
             CommitmentChecker checker) {
         super(analysis);
         this.checker = checker;
+    }
+
+    @Override
+    public CommitmentStore initialStore(UnderlyingAST underlyingAST,
+            List<LocalVariableNode> parameters) {
+        CommitmentStore result = super.initialStore(underlyingAST, parameters);
+        // Case 3: all non-null fields that have an initializer are part of
+        // 'fieldValues', and can be considered initialized.
+        addInitializedFields(result);
+        return result;
+    }
+
+    /**
+     * Returns the set of fields that can safely be considered initialized after
+     * the method call {@code node}.
+     */
+    protected Set<Element> initializedFieldsAfterCall(
+            MethodInvocationNode node,
+            ConditionalTransferResult<CFValue, CommitmentStore> transferResult) {
+        Set<Element> result = new HashSet<>();
+        MethodInvocationTree tree = node.getTree();
+        ExecutableElement method = TreeUtils.elementFromUse(tree);
+        boolean isConstructor = method.getSimpleName().contentEquals("<init>");
+        Node receiver = node.getTarget().getReceiver();
+        String methodString = tree.getMethodSelect().toString();
+
+        // Case 1: After a call to the constructor of the same class, all
+        // non-null fields are guaranteed to be initialized.
+        if (isConstructor && receiver instanceof ThisLiteralNode
+                && methodString.equals("this")) {
+            ClassTree clazz = TreeUtils.enclosingClass(analysis.getFactory()
+                    .getPath(tree));
+            TypeElement clazzElem = TreeUtils.elementFromDeclaration(clazz);
+            markInvariantFieldsAsInitialized(result, clazzElem);
+        }
+
+        // Case 4: After a call to the constructor of the super class, all
+        // non-null fields of any super class are guaranteed to be initialized.
+        if (isConstructor && receiver instanceof ThisLiteralNode
+                && methodString.equals("super")) {
+            ClassTree clazz = TreeUtils.enclosingClass(analysis.getFactory()
+                    .getPath(tree));
+            TypeElement clazzElem = TreeUtils.elementFromDeclaration(clazz);
+            TypeMirror superClass = clazzElem.getSuperclass();
+
+            while (superClass != null && superClass.getKind() != TypeKind.NONE) {
+                clazzElem = (TypeElement) analysis.getTypes().asElement(
+                        superClass);
+                superClass = clazzElem.getSuperclass();
+                markInvariantFieldsAsInitialized(result, clazzElem);
+            }
+        }
+
+        // Case 2: After a method call that has some postcondition ensuring some
+        // properties about fields, these fields can be known to be initialized.
+        addInitializedFields(transferResult.getThenStore());
+        addInitializedFields(transferResult.getElseStore());
+        return result;
+    }
+
+    /**
+     * Adds all the fields of the class {@code clazzElem} that have the
+     * 'invariant annotation' to the set of initialized fields {@code result}.
+     */
+    protected void markInvariantFieldsAsInitialized(Set<Element> result,
+            TypeElement clazzElem) {
+        List<VariableElement> fields = ElementFilter.fieldsIn(clazzElem
+                .getEnclosedElements());
+        for (VariableElement field : fields) {
+            AnnotatedTypeMirror fieldAnno = analysis.getFactory()
+                    .getAnnotatedType(field);
+            if (fieldAnno.hasAnnotation(checker.getFieldInvariantAnnotation())) {
+                result.add(field);
+            }
+        }
+    }
+
+    /**
+     * For the given store {@code store}, add all fields for which we know some
+     * property (by looking at 'fieldValues' in the store) to the set of
+     * initialized fields.
+     */
+    protected void addInitializedFields(CommitmentStore store) {
+        Map<FieldAccess, CFValue> fieldValues = store.getFieldValues();
+        for (Entry<FieldAccess, CFValue> e : fieldValues.entrySet()) {
+            FieldAccess field = e.getKey();
+            if (field.getReceiver() instanceof ThisReference) {
+                // There is no need to check what CFValue the field has, as any
+                // value means that is has been initialized.
+                store.addInitializedField(field.getField());
+            }
+        }
     }
 
     @Override
@@ -104,17 +214,5 @@ public class CommitmentTransfer<T extends CommitmentTransfer<T>> extends
             }
         }
         return result;
-    }
-
-    /**
-     * Returns the set of fields that can safely be considered initialized after
-     * the method call {@code node}.
-     *
-     * @param result
-     */
-    protected Set<Element> initializedFieldsAfterCall(
-            MethodInvocationNode node,
-            ConditionalTransferResult<CFValue, CommitmentStore> result) {
-        return Collections.emptySet();
     }
 }
--- checkers/src/checkers/nonnull/NonNullTransfer.java
+++ checkers/src/checkers/nonnull/NonNullTransfer.java
@@ -1,42 +1,18 @@
 package checkers.nonnull;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-
 import javax.lang.model.element.AnnotationMirror;
-import javax.lang.model.element.Element;
-import javax.lang.model.element.ExecutableElement;
-import javax.lang.model.element.TypeElement;
-import javax.lang.model.element.VariableElement;
-import javax.lang.model.type.TypeKind;
-import javax.lang.model.type.TypeMirror;
-import javax.lang.model.util.ElementFilter;
 
 import checkers.commitment.CommitmentStore;
 import checkers.commitment.CommitmentTransfer;
 import checkers.flow.analysis.ConditionalTransferResult;
 import checkers.flow.analysis.FlowExpressions;
-import checkers.flow.analysis.FlowExpressions.FieldAccess;
 import checkers.flow.analysis.FlowExpressions.Receiver;
-import checkers.flow.analysis.FlowExpressions.ThisReference;
 import checkers.flow.analysis.TransferResult;
 import checkers.flow.analysis.checkers.CFAbstractStore;
 import checkers.flow.analysis.checkers.CFValue;
-import checkers.flow.cfg.UnderlyingAST;
-import checkers.flow.cfg.node.LocalVariableNode;
-import checkers.flow.cfg.node.MethodInvocationNode;
 import checkers.flow.cfg.node.Node;
 import checkers.flow.cfg.node.NullLiteralNode;
-import checkers.flow.cfg.node.ThisLiteralNode;
 import checkers.nonnull.quals.NonNull;
-import checkers.types.AnnotatedTypeMirror;
-import checkers.util.TreeUtils;
-
-import com.sun.source.tree.ClassTree;
-import com.sun.source.tree.MethodInvocationTree;
 
 /**
  * Transfer function for the non-null type system. Performs the following
@@ -61,96 +37,6 @@ public class NonNullTransfer extends CommitmentTransfer<NonNullTransfer> {
     public NonNullTransfer(NonNullAnalysis analysis, NonNullChecker checker) {
         super(analysis, checker);
         this.analysis = analysis;
-    }
-
-    @Override
-    public CommitmentStore initialStore(UnderlyingAST underlyingAST,
-            List<LocalVariableNode> parameters) {
-        CommitmentStore result = super.initialStore(underlyingAST, parameters);
-        // Case 3: all non-null fields that have an initializer are part of
-        // 'fieldValues', and can be considered initialized.
-        addInitializedFields(result);
-        return result;
-    }
-
-    @Override
-    protected Set<Element> initializedFieldsAfterCall(
-            MethodInvocationNode node,
-            ConditionalTransferResult<CFValue, CommitmentStore> transferResult) {
-        Set<Element> result = new HashSet<>(super.initializedFieldsAfterCall(
-                node, transferResult));
-        MethodInvocationTree tree = node.getTree();
-        ExecutableElement method = TreeUtils.elementFromUse(tree);
-        boolean isConstructor = method.getSimpleName().contentEquals("<init>");
-        Node receiver = node.getTarget().getReceiver();
-        String methodString = tree.getMethodSelect().toString();
-
-        // Case 1: After a call to the constructor of the same class, all
-        // non-null fields are guaranteed to be initialized.
-        if (isConstructor && receiver instanceof ThisLiteralNode
-                && methodString.equals("this")) {
-            ClassTree clazz = TreeUtils.enclosingClass(analysis.getFactory()
-                    .getPath(tree));
-            TypeElement clazzElem = TreeUtils.elementFromDeclaration(clazz);
-            markNonNullFieldsAsInitialized(result, clazzElem);
-        }
-
-        // Case 4: After a call to the constructor of the super class, all
-        // non-null fields of any super class are guaranteed to be initialized.
-        if (isConstructor && receiver instanceof ThisLiteralNode
-                && methodString.equals("super")) {
-            ClassTree clazz = TreeUtils.enclosingClass(analysis.getFactory()
-                    .getPath(tree));
-            TypeElement clazzElem = TreeUtils.elementFromDeclaration(clazz);
-            TypeMirror superClass = clazzElem.getSuperclass();
-
-            while (superClass != null && superClass.getKind() != TypeKind.NONE) {
-                clazzElem = (TypeElement) analysis.getTypes().asElement(
-                        superClass);
-                superClass = clazzElem.getSuperclass();
-                markNonNullFieldsAsInitialized(result, clazzElem);
-            }
-        }
-
-        // Case 2: After a method call that has some postcondition ensuring some
-        // properties about fields, these fields can be known to be initialized.
-        addInitializedFields(transferResult.getThenStore());
-        addInitializedFields(transferResult.getElseStore());
-        return result;
-    }
-
-    /**
-     * Adds all the fields of the class {@code clazzElem} to the set of
-     * initialized fields {@code result}.
-     */
-    protected void markNonNullFieldsAsInitialized(Set<Element> result,
-            TypeElement clazzElem) {
-        List<VariableElement> fields = ElementFilter.fieldsIn(clazzElem
-                .getEnclosedElements());
-        for (VariableElement field : fields) {
-            AnnotatedTypeMirror fieldAnno = analysis.getFactory()
-                    .getAnnotatedType(field);
-            if (fieldAnno.hasAnnotation(NonNull.class)) {
-                result.add(field);
-            }
-        }
-    }
-
-    /**
-     * For the given store {@code store}, add all fields for which we know some
-     * property (by looking at 'fieldValues' in the store) to the set of
-     * initialized fields.
-     */
-    protected void addInitializedFields(CommitmentStore store) {
-        Map<FieldAccess, CFValue> fieldValues = store.getFieldValues();
-        for (Entry<FieldAccess, CFValue> e : fieldValues.entrySet()) {
-            FieldAccess field = e.getKey();
-            if (field.getReceiver() instanceof ThisReference) {
-                // There is no need to check what CFValue the field has, as any
-                // value means that is has been initialized.
-                store.addInitializedField(field.getField());
-            }
-        }
     }
 
     /**
