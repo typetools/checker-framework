c56c61440755761fc2e63936b6c510f2633f3cd9
stefanheule <stefanheule@localhost>
2012-07-22 18:50:27 +0200

    Proper documentation for non-null annotations (@NonNull, @Nullable, @MonoNonNull).

--- checkers/src/checkers/nonnull/quals/NonNull.java
+++ checkers/src/checkers/nonnull/quals/NonNull.java
@@ -8,7 +8,8 @@ import java.lang.annotation.Target;
 
 import javax.lang.model.type.TypeKind;
 
-import checkers.nullness.NullnessChecker;
+import checkers.commitment.CommitmentChecker;
+import checkers.nonnull.NonNullChecker;
 import checkers.quals.DefaultQualifierInHierarchy;
 import checkers.quals.ImplicitFor;
 import checkers.quals.SubtypeOf;
@@ -18,26 +19,29 @@ import checkers.types.AnnotatedTypeMirror.AnnotatedPrimitiveType;
 import com.sun.source.tree.Tree;
 
 /**
- * {@code @NonNull} is a type annotation that indicates that a value is never
- * null.
- * <p>
+ * {@link NonNull} is a type annotation that indicates that an expression is
+ * never {@code null}.
  *
- * When applied to a member field's type, indicates that the field is never null
- * after instantiation (construction) completes. When applied to a static
- * field's type, indicates that the field is never null after the containing
- * class is initialized.
  * <p>
+ * For fields of a class, the {@link NonNull} annotation indicates that this
+ * field is never {@code null}
+ * <em>after the class has been fully initialized</em>. Class initialization is
+ * controlled by the Freedom Before Commitment type system, see
+ * {@link CommitmentChecker} for more details.
  *
+ * <p>
+ * For static fields, the {@link NonNull} annotation indicates that this field
+ * is never {@code null} <em>after the containing class is initialized</em>.
+ *
+ * <p>
  * This annotation is rarely written in source code, because it is the default.
- * No more than one of {@code @NonNull} and {@link Nullable} may be written on a
- * given type.
- * <p>
  *
- * This annotation is associated with the {@link NullnessChecker}.
+ * <p>
+ * This annotation is associated with the {@link NonNullChecker}.
  *
  * @see Nullable
- * @see NullnessChecker
- * @checker.framework.manual #nullness-checker Nullness Checker
+ * @see MonoNonNull
+ * @see NonNullChecker
  */
 @Documented
 @TypeQualifier
