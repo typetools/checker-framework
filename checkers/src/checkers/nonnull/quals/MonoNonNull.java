fb515bf2f9d1e630736e922bc2704108b7f376b0
stefanheule <devnull@localhost>
2012-07-22 18:50:27 +0200

    Proper documentation for non-null annotations (@NonNull, @Nullable, @MonoNonNull).

--- checkers/src/checkers/nonnull/quals/MonoNonNull.java
+++ checkers/src/checkers/nonnull/quals/MonoNonNull.java
@@ -6,48 +6,22 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import javax.lang.model.type.TypeKind;
-
-import com.sun.source.tree.Tree;
-
-import checkers.nullness.NullnessChecker;
-import checkers.quals.DefaultQualifierInHierarchy;
-import checkers.quals.ImplicitFor;
+import checkers.nonnull.NonNullChecker;
 import checkers.quals.MonotonicAnnotation;
 import checkers.quals.SubtypeOf;
 import checkers.quals.TypeQualifier;
-import checkers.types.AnnotatedTypeMirror.AnnotatedPrimitiveType;
 
 /**
- * Indicates that a field is lazily initialized to a non-null value. Once the
- * field becomes non-null, it never becomes {@code null} again. There is no
- * guarantee that the field ever becomes non-null, however.
+ * Indicates that a variable if of a monotonic non-null type; that is, the
+ * variable might start out as {@link Nullable} and will monotonically change to
+ * {@link NonNull}. It is not guaranteed that the variable will ever reach
+ * {@link NonNull}, but if it does, it will stay {@link NonNull}.
+ *
  * <p>
+ * This annotation is associated with the {@link NonNullChecker}.
  *
- * Lazily initialized fields have these two properties:
- * <ol>
- * <li>The field may be assigned only non-null values.</li>
- * <li>The field may be re-assigned as often as desired.</li>
- *
- * </ol>
- *
- * When the field is first read within a method, the field cannot be assumed to
- * be non-null. The benefit of LazyNonNull over Nullable is its different
- * interaction with flow-sensitive type qualifier refinement. After a check of a
- * LazyNonNull field, all subsequent accesses <em>within
- * that method</em> can be assumed to be NonNull, even after arbitrary external
- * method calls that have access to the given field.
- * <p>
- *
- * Note that LazyNonNull is a field declaration annotation, not a type
- * annotation. Final fields are treated as LazyNonNull by default.
- * <p>
- *
- * This annotation is associated with the {@link NullnessChecker}.
- *
- * @see Nullable
- * @see NullnessChecker
- * @checker.framework.manual #nullness-checker Nullness Checker
+ * @see MonotonicAnnotation
+ * @see NonNullChecker
  */
 @Documented
 @TypeQualifier
--- checkers/src/checkers/nonnull/quals/Nullable.java
+++ checkers/src/checkers/nonnull/quals/Nullable.java
@@ -6,7 +6,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import checkers.nullness.NullnessChecker;
+import checkers.nonnull.NonNullChecker;
 import checkers.quals.ImplicitFor;
 import checkers.quals.SubtypeOf;
 import checkers.quals.TypeQualifier;
@@ -14,28 +14,16 @@ import checkers.quals.TypeQualifier;
 import com.sun.source.tree.Tree;
 
 /**
- * {@code @Nullable} is a type annotation that indicates that the value is not
- * known to be non-null (see {@link NonNull}). Another perspective is that if a
- * type is annotated with {@code Nullable}, then it can be legal/expected for a
- * value of that type to be null.
+ * {@link Nullable} is a type annotation that indicates that the value is not
+ * known to be non-null (see {@link NonNull}). Only if an expression has a
+ * {@link Nullable} type it can be assigned {@code null}.
+ *
  * <p>
- * 
- * For example, if a method parameter's type is annotated with {@code @Nullable}
- * , then passing {@code null} as an argument should not by itself cause the
- * method to throw an exception, including a {@code NullPointerException}.
- * Similarly, if a field's type is {@code @Nullable}, then setting it to null
- * should not by itself cause a run-time exception.
- * <p>
- * 
- * No more than one of {@link Nullable} and {@code NonNull} may be written on a
- * given type.
- * <p>
- * 
- * This annotation is associated with the {@link NullnessChecker}.
- * 
+ * This annotation is associated with the {@link NonNullChecker}.
+ *
  * @see NonNull
- * @see NullnessChecker
- * @checker.framework.manual #nullness-checker Nullness Checker
+ * @see MonoNonNull
+ * @see NonNullChecker
  */
 @Documented
 @SubtypeOf({})
