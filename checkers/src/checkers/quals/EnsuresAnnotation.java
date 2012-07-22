d74a9df614bb13c3b1fb1ee9b2775e34d7d360ae
stefanheule <stefanheule@localhost>
2012-07-22 21:10:42 +0200

    Improve javadoc of pre/postcondition annotations.

--- checkers/src/checkers/quals/EnsuresAnnotation.java
+++ checkers/src/checkers/quals/EnsuresAnnotation.java
@@ -7,25 +7,35 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import checkers.flow.util.FlowExpressionParseUtil;
-
 /**
  * A postcondition annotation to indicate that a method ensures certain
- * expressions to have a certain annotation once the method has finished. The
- * expressions for which the annotation must hold after the methods execution
- * are indicated by {@code expression} and are specified using a string. The
- * syntax is specified as part of the documentation of
- * {@link FlowExpressionParseUtil}. The annotation is specified by
- * {@code annotation}.
- * 
+ * expressions to have a certain annotation once the method has successfully
+ * terminated. The expressions for which the annotation must hold after the
+ * methods execution are indicated by {@code expression} and are specified using
+ * a string. The annotation is specified by {@code annotation}.
+ *
  * @author Stefan Heule
- * 
+ * @see <a
+ *      href="http://types.cs.washington.edu/checker-framework/current/checkers-manual.html#java-expressions-as-arguments">Syntax
+ *      of Java expressions</a>
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.METHOD, ElementType.CONSTRUCTOR })
 public @interface EnsuresAnnotation {
+    /**
+     * The Java expressions for which the annotation holds after successful
+     * method termination.
+     *
+     * @see <a
+     *      href="http://types.cs.washington.edu/checker-framework/current/checkers-manual.html#java-expressions-as-arguments">Syntax
+     *      of Java expressions</a>
+     */
     String[] expression();
 
+    /**
+     * The annotation that is guaranteed to hold on successfull termination of
+     * the method.
+     */
     Class<? extends Annotation> annotation();
 }
