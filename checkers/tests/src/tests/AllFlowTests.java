be30c6819c5fee0c3392042f32f57cfa771302b1
stefanheule <devnull@localhost>
2012-07-23 00:44:53 +0200

    Add NonNullTest and NonNull2Test to the AllFlowTests target.

--- checkers/tests/src/tests/AllFlowTests.java
+++ checkers/tests/src/tests/AllFlowTests.java
@@ -7,11 +7,11 @@ import org.junit.runners.Suite.SuiteClasses;
 /**
  * A collection of all test targets that allready work with the new dataflow
  * framework. This class will be removed once all tests pass.
- * 
+ *
  * @author Stefan Heule
  */
 @RunWith(Suite.class)
 @SuiteClasses({ Flow2Test.class, RegexTest.class, I18nTest.class,
-        PuritySuggestionsTest.class, FlowTest.class })
+        PuritySuggestionsTest.class, FlowTest.class, NonNullTest.class, NonNull2Test.class })
 public class AllFlowTests {
 }
