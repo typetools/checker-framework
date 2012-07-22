2f1f34613679a1ee280f850c1b059948b7417868
stefanheule <stefanheule@localhost>
2012-07-22 21:11:25 +0200

    Pre- and postcondition annotations for the nonnull type system.

---
+++ checkers/src/checkers/nonnull/quals/RequiresNonNull.java
@@ -0,0 +1,30 @@
+package checkers.nonnull.quals;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import checkers.quals.PreconditionAnnotation;
+
+/**
+ * A precondition annotation to indicate that a method requires certain
+ * expressions to be {@link NonNull}.
+ *
+ * @author Stefan Heule
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.METHOD, ElementType.CONSTRUCTOR })
+@PreconditionAnnotation(annotation = NonNull.class)
+public @interface RequiresNonNull {
+    /**
+     * The Java expressions which need to be {@link NonNull}.
+     *
+     * @see <a
+     *      href="http://types.cs.washington.edu/checker-framework/current/checkers-manual.html#java-expressions-as-arguments">Syntax
+     *      of Java expressions</a>
+     */
+    String[] value();
+}
