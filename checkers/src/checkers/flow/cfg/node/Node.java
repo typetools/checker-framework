5ea1a11c082689edd113283797bec35f33bdb726
stefanheule <devnull@localhost>
2012-07-24 19:39:39 +0200

    Fix Node.getTransitiveOperands.

--- checkers/src/checkers/flow/cfg/node/Node.java
+++ checkers/src/checkers/flow/cfg/node/Node.java
@@ -13,25 +13,25 @@ import com.sun.source.tree.Tree;
 /**
  * A node in the abstract representation used for Java code inside a basic
  * block.
- * 
+ *
  * <p>
- * 
+ *
  * The following invariants hold:
- * 
+ *
  * <pre>
  * block == null || block instanceof RegularBlock || block instanceof ExceptionBlock
  * block instanceof RegularBlock ==> block.getContents().contains(this)
  * block instanceof ExceptionBlock ==> block.getNode() == this
  * block == null <==> "This object represents a parameter of the method."
  * </pre>
- * 
+ *
  * <pre>
  * type != null
  * tree != null ==> node.getType() == InternalUtils.typeOf(node.getTree())
  * </pre>
- * 
+ *
  * @author Stefan Heule
- * 
+ *
  */
 public abstract class Node {
 
@@ -70,7 +70,7 @@ public abstract class Node {
      * Returns the {@link Tree} in the abstract syntax tree, or
      * <code>null</code> if no corresponding tree exists. For instance, this is
      * the case for an {@link ImplicitThisLiteralNode}.
-     * 
+     *
      * @return The corresponding {@link Tree} or <code>null</code>.
      */
     abstract public/* @Nullable */Tree getTree();
@@ -78,7 +78,7 @@ public abstract class Node {
     /**
      * Returns a {@link TypeMirror} representing the type of a {@link Node} A
      * {@link Node} will always have a type even when it has no {@link Tree}.
-     * 
+     *
      * @return A {@link TypeMirror} representing the type of this {@link Node}.
      */
     public TypeMirror getType() {
@@ -87,7 +87,7 @@ public abstract class Node {
 
     /**
      * Accept method of the visitor pattern
-     * 
+     *
      * @param <R>
      *            Result type of the operation.
      * @param <P>
@@ -123,7 +123,7 @@ public abstract class Node {
      */
     public Collection<Node> getTransitiveOperands() {
         LinkedList<Node> operands = new LinkedList<>(getOperands());
-        LinkedList<Node> transitiveOperands = new LinkedList<>(getOperands());
+        LinkedList<Node> transitiveOperands = new LinkedList<>();
         while (!operands.isEmpty()) {
             Node next = operands.removeFirst();
             operands.addAll(next.getOperands());
