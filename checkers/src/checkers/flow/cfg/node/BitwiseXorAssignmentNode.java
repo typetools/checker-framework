413fcc836d36001210c1725c3735d139df828e78
stefanheule <devnull@localhost>
2012-07-24 19:38:44 +0200

    Slightly improve abstraction of compound assignment nodes. Also improve their hash function.

--- checkers/src/checkers/flow/cfg/node/BitwiseXorAssignmentNode.java
+++ checkers/src/checkers/flow/cfg/node/BitwiseXorAssignmentNode.java
@@ -5,14 +5,14 @@ import com.sun.source.tree.Tree.Kind;
 
 /**
  * A node for the bitwise or logical (single bit) xor compound assignment:
- * 
+ *
  * <pre>
  *   <em>variable</em> ^= <em>expression</em>
  * </pre>
- * 
+ *
  * @author Stefan Heule
  * @author Charlie Garrett
- * 
+ *
  */
 public class BitwiseXorAssignmentNode extends CompoundAssignmentNode {
 
@@ -27,7 +27,7 @@ public class BitwiseXorAssignmentNode extends CompoundAssignmentNode {
     }
 
     @Override
-    public String toString() {
-        return "(" + getLeftOperand() + " ^= " + getRightOperand() + ")";
+    public String getOperator() {
+        return "^";
     }
 }
--- checkers/src/checkers/flow/cfg/node/NumericalAdditionAssignmentNode.java
+++ checkers/src/checkers/flow/cfg/node/NumericalAdditionAssignmentNode.java
@@ -5,14 +5,14 @@ import com.sun.source.tree.Tree.Kind;
 
 /**
  * A node for the numerical addition compound assignment:
- * 
+ *
  * <pre>
  *   <em>variable</em> += <em>expression</em>
  * </pre>
- * 
+ *
  * @author Stefan Heule
  * @author Charlie Garrett
- * 
+ *
  */
 public class NumericalAdditionAssignmentNode extends CompoundAssignmentNode {
 
@@ -27,7 +27,7 @@ public class NumericalAdditionAssignmentNode extends CompoundAssignmentNode {
     }
 
     @Override
-    public String toString() {
-        return "(" + getLeftOperand() + " += " + getRightOperand() + ")";
+    public String getOperator() {
+        return "+";
     }
 }
