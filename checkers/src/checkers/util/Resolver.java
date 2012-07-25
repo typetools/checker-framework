8ae04edbb7a862added29f745f40b676dd229d2a
stefanheule <stefanheule@localhost>
2012-07-25 02:21:16 +0200

    Refactor and improve Resolver; it only includes functionality that is actually used now.

--- checkers/src/checkers/util/Resolver.java
+++ checkers/src/checkers/util/Resolver.java
@@ -1,8 +1,5 @@
 package checkers.util;
 
-import static com.sun.tools.javac.code.Kinds.ERR;
-import static com.sun.tools.javac.code.Kinds.PCK;
-import static com.sun.tools.javac.code.Kinds.TYP;
 import static com.sun.tools.javac.code.Kinds.VAR;
 
 import java.lang.reflect.InvocationTargetException;
@@ -15,11 +12,7 @@ import javax.lang.model.type.TypeMirror;
 import com.sun.source.util.TreePath;
 import com.sun.source.util.Trees;
 import com.sun.tools.javac.api.JavacScope;
-import com.sun.tools.javac.code.Kinds;
 import com.sun.tools.javac.code.Symbol;
-import com.sun.tools.javac.code.Symbol.ClassSymbol;
-import com.sun.tools.javac.code.Symbol.PackageSymbol;
-import com.sun.tools.javac.code.Symbol.TypeSymbol;
 import com.sun.tools.javac.code.Type;
 import com.sun.tools.javac.comp.AttrContext;
 import com.sun.tools.javac.comp.Env;
@@ -31,107 +24,71 @@ import com.sun.tools.javac.util.Name;
 import com.sun.tools.javac.util.Names;
 
 /**
- * A Utility class to find symbols corresponding to string references
+ * A Utility class to find symbols corresponding to string references.
  */
 public class Resolver {
     private final Resolve resolve;
     private final Names names;
     private final Trees trees;
 
-    private final Method FIND_IDENT;
     private final Method FIND_METHOD;
-    private final Method FIND_IDENT_IN_PACKAGE;
-    private final Method FIND_MEMBER_TYPE;
     private final Method FIND_IDENT_IN_TYPE;
 
     public Resolver(ProcessingEnvironment env) {
-        Context context = ((JavacProcessingEnvironment)env).getContext();
+        Context context = ((JavacProcessingEnvironment) env).getContext();
         this.resolve = Resolve.instance(context);
         this.names = Names.instance(context);
         this.trees = Trees.instance(env);
 
         try {
-            this.FIND_IDENT = Resolve.class.getDeclaredMethod(
-                    "findIdent",
-                    Env.class, Name.class, int.class);
-            FIND_IDENT.setAccessible(true);
-            
             this.FIND_METHOD = Resolve.class.getDeclaredMethod("findMethod",
                     Env.class, Type.class, Name.class, List.class, List.class,
                     boolean.class, boolean.class, boolean.class);
             FIND_METHOD.setAccessible(true);
 
-            this.FIND_IDENT_IN_PACKAGE = Resolve.class.getDeclaredMethod(
-                    "findIdentInPackage",
-                    Env.class, TypeSymbol.class, Name.class, int.class);
-            FIND_IDENT_IN_PACKAGE.setAccessible(true);
-
-            this.FIND_MEMBER_TYPE = Resolve.class.getDeclaredMethod(
-                    "findMemberType",
-                    Env.class,
-                    Type.class,
-                    Name.class,
-                    TypeSymbol.class);
-            FIND_MEMBER_TYPE.setAccessible(true);
-
             this.FIND_IDENT_IN_TYPE = Resolve.class.getDeclaredMethod(
-                    "findIdentInType",
-                    Env.class, Type.class, Name.class, int.class);
+                    "findIdentInType", Env.class, Type.class, Name.class,
+                    int.class);
             this.FIND_IDENT_IN_TYPE.setAccessible(true);
         } catch (Exception e) {
-            Error err = new AssertionError("Compiler 'Resolve' class doesn't contain required 'findXXX' method");
+            Error err = new AssertionError(
+                    "Compiler 'Resolve' class doesn't contain required 'find' method");
             err.initCause(e);
             throw err;
         }
     }
 
     /**
-     * Finds the variable referenced in the passed {@code String}.
+     * Finds the field with name {@code name} in a given type.
      *
-     * This method may only operate on variable references, e.g. local
-     * variables, parameters, fields.
-     *
-     * The reference string may be either an single Java identifier (e.g. "field")
-     * or dot-separated identifiers (e.g. "Collections.EMPTY_LIST").
-     *
-     * The method adheres to all the rules of Java's scoping (while also
-     * considering the imports) for name resolution.
-     *
-     * @param reference     the variable reference string
-     * @param path          the tree path to the local scope
-     * @return  the variable reference
-     */
-    public Element findVariable(String reference, TreePath path) {
-        JavacScope scope = (JavacScope) trees.getScope(path);
-        Env<AttrContext> env = scope.getEnv();
-
-        if (!reference.contains(".")) {
-            // Simple variable
-            return wrapInvocation(
-                    FIND_IDENT,
-                    env, names.fromString(reference), Kinds.VAR);
-        } else {
-            int lastDot = reference.lastIndexOf('.');
-            String expr = reference.substring(0, lastDot);
-            String name = reference.substring(lastDot + 1);
-
-            Element site = findType(expr, env);
-            Name ident = names.fromString(name);
-
-            return wrapInvocation(
-                    FIND_IDENT_IN_TYPE,
-                    env, site.asType(), ident, VAR);
-        }
-    }
-    
-    /**
-     * Finds the method element for a given name and list of expected parameter
-     * types.
-     * 
      * <p>
      * The method adheres to all the rules of Java's scoping (while also
      * considering the imports) for name resolution.
-     * 
+     *
+     * @param name
+     *            The name of the field.
+     * @param type
+     *            The type of the receiver (i.e., the type in which to look for
+     *            the field).
+     * @param path
+     *            The tree path to the local scope.
+     * @return The element for the field.
+     */
+    public Element findField(String name, TypeMirror type, TreePath path) {
+        JavacScope scope = (JavacScope) trees.getScope(path);
+        Env<AttrContext> env = scope.getEnv();
+        return wrapInvocation(FIND_IDENT_IN_TYPE, env, type,
+                names.fromString(name), VAR);
+    }
+
+    /**
+     * Finds the method element for a given name and list of expected parameter
+     * types.
+     *
+     * <p>
+     * The method adheres to all the rules of Java's scoping (while also
+     * considering the imports) for name resolution.
+     *
      * @param methodName
      *            Name of the method to find.
      * @param receiverType
@@ -156,38 +113,9 @@ public class Resolver {
                 typeargtypes, allowBoxing, useVarargs, operator);
     }
 
-    private Element findType(String reference, Env<AttrContext> env) {
-        if (!reference.contains(".")) {
-            // Simple variable
-            return wrapInvocation(
-                    FIND_IDENT,
-                    env, names.fromString(reference), Kinds.TYP | Kinds.PCK);
-        } else {
-            int lastDot = reference.lastIndexOf(".");
-            String expr = reference.substring(0, lastDot);
-            String idnt = reference.substring(lastDot + 1);
-
-            Symbol site = (Symbol)findType(expr, env);
-            if (site.kind == ERR)
-                return site;
-            Name name = names.fromString(idnt);
-            if (site.kind == PCK) {
-                env.toplevel.packge = (PackageSymbol)site;
-                return wrapInvocation(
-                        FIND_IDENT_IN_PACKAGE,
-                        env, site, name, TYP | PCK);
-            } else {
-                env.enclClass.sym = (ClassSymbol)site;
-                return wrapInvocation(
-                        FIND_MEMBER_TYPE,
-                        env, site.asType(), name, site);
-            }
-        }
-    }
-
     private Symbol wrapInvocation(Method method, Object... args) {
         try {
-            return (Symbol)method.invoke(resolve, args);
+            return (Symbol) method.invoke(resolve, args);
         } catch (IllegalAccessException e) {
             Error err = new AssertionError("Unexpected Reflection error");
             err.initCause(e);
