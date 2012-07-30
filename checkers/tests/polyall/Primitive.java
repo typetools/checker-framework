b813b25db7c3d5587a3264161329f78b2ff8c62b
wdietl <wdietl@gmail.com>
2012-07-29 19:22:49 -0700

    Fix method naming error in test case.

--- checkers/tests/polyall/Primitive.java
+++ checkers/tests/polyall/Primitive.java
@@ -8,10 +8,10 @@ class Primitive {
     @H1S2 @PolyAll int m(@H1S2 @PolyAll int p) {
         return p;
     }
-    void use(@H1S2 @H2S1 int p) {
+    void use1(@H1S2 @H2S1 int p) {
         @H1S2 @H2S1 int l = m(p);
     }
-    void use(@H1S2 @H2S2 int p) {
+    void use2(@H1S2 @H2S2 int p) {
         //:: error: (assignment.type.incompatible)
         @H1S2 @H2S1 int l = m(p);
     }
