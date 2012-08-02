9e62645c0d7e61d05366ab98195375abd25ed9ee
wdietl <wdietl@gmail.com>
2012-08-01 23:23:47 -0700

    Change DefaultQualifier from String to Class.

--- demos/nonnull-interned-demo/junit/src/java/lang/Object.java
+++ demos/nonnull-interned-demo/junit/src/java/lang/Object.java
@@ -2,7 +2,7 @@ package java.lang;
 
 import checkers.nullness.quals.*;
 
-@checkers.quals.DefaultQualifier("checkers.nullness.quals.NonNull")
+@checkers.quals.DefaultQualifier(checkers.nullness.quals.NonNull.class)
 
 public class Object{
   public Object() { throw new RuntimeException("skeleton method"); }
