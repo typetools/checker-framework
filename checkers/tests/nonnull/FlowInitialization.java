8b715c13ce57a5c7d7061d53494a73d7169de26c
stefanheule <stefanheule@localhost>
2012-08-02 20:51:13 +0200

    Adapt two test to work with rawness and FBC at the same time.

--- checkers/tests/nonnull/FlowInitialization.java
+++ checkers/tests/nonnull/FlowInitialization.java
@@ -45,7 +45,7 @@ public class FlowInitialization {
     }
     
     @EnsuresAnnotation(expression="f", annotation=NonNull.class)
-    public void setField(@Unclassified FlowInitialization this) {
+    public void setField(@Unclassified @Raw FlowInitialization this) {
         f = "";
     }
 }
--- checkers/tests/nonnull/Initializer.java
+++ checkers/tests/nonnull/Initializer.java
@@ -49,12 +49,12 @@ class Initializer {
     }
     
     @EnsuresAnnotation(expression="a", annotation=NonNull.class)
-    public void setField(@Unclassified Initializer this) {
+    public void setField(@Unclassified @Raw Initializer this) {
         a = "";
     }
     
     @EnsuresAnnotationIf(result=true, expression="a", annotation=NonNull.class)
-    public boolean setFieldMaybe(@Unclassified Initializer this) {
+    public boolean setFieldMaybe(@Unclassified @Raw Initializer this) {
         a = "";
         return true;
     }
