1d5d1b36a63d419f37c56c8ab9f5d82e26e8a47f
wmdietl <devnull@localhost>
2012-08-10 04:41:01 -0700

    Enable test case to see how it behaves.

--- checkers/tests/regex/AnnotatedTypeParams3.java
+++ checkers/tests/regex/AnnotatedTypeParams3.java
@@ -5,8 +5,6 @@ import org.junit.Ignore;
 
 import checkers.regex.quals.Regex;
 
-//@skip-test
-//temporarily disabled until generics work properly
 class AnnotatedTypeParams3 {
     private <T extends Annotation> T
             safeGetAnnotation(Field f, Class<T> annotationClass) {
