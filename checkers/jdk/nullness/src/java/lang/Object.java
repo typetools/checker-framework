120c29c302b195d2c8744a0127b4e9a4f3a7eb8e
wmdietl <wmdietl@localhost>
2012-08-10 13:59:27 -0700

    Make all @Raw references also be @Unclassified.

--- checkers/jdk/nullness/src/java/lang/Object.java
+++ checkers/jdk/nullness/src/java/lang/Object.java
@@ -4,16 +4,18 @@ import checkers.quals.Pure;
 import checkers.nullness.quals.Nullable;
 import checkers.nullness.quals.Raw;
 
-@Raw
+import checkers.initialization.quals.Unclassified;
+
+@Unclassified @Raw
 public class Object {
   public Object() { throw new RuntimeException("skeleton method"); }
   public @Pure boolean equals(@Nullable Object a1) { throw new RuntimeException("skeleton method"); }
   public @Pure String toString() { throw new RuntimeException("skeleton method"); }
-  public final void wait(@Raw Object this, long a1, int a2) throws InterruptedException { throw new RuntimeException("skeleton method"); }
-  public final void wait(@Raw Object this) throws InterruptedException { throw new RuntimeException("skeleton method"); }
+  public final void wait(@Unclassified @Raw Object this, long a1, int a2) throws InterruptedException { throw new RuntimeException("skeleton method"); }
+  public final void wait(@Unclassified @Raw Object this) throws InterruptedException { throw new RuntimeException("skeleton method"); }
 
   private static native void registerNatives();
-  public final native @Pure Class<?> getClass(@Raw Object this);
+  public final native @Pure Class<?> getClass(@Unclassified @Raw Object this);
   public native @Pure int hashCode();
   protected native Object clone() throws CloneNotSupportedException;
   public final native void notify();
