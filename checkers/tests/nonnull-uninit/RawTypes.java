4f5e6826d246970d2463461a135a7202c0a54ca6
wmdietl <wmdietl@localhost>
2012-08-10 22:14:37 -0700

    Update @DefaultQualifier to use .class literals.

--- checkers/tests/nonnull-uninit/RawTypes.java
+++ checkers/tests/nonnull-uninit/RawTypes.java
@@ -2,7 +2,8 @@
 
 import checkers.nullness.quals.*;
 import java.util.*;
-@checkers.quals.DefaultQualifier("Nullable")
+
+@checkers.quals.DefaultQualifier(Nullable.class)
 class RawTypes {
 
     class Bad {
