7a2e2d38eac4894b8148d606dd01af2c6a3aa7b3
wdietl <wdietl@gmail.com>
2012-08-21 00:11:36 -0700

    A "general" annotated type factory that supports qualifiers from any type hierarchy. Instead of cryptically including an ATF with null as a few values, use this class.

---
+++ checkers/src/checkers/types/GeneralAnnotatedTypeFactory.java
@@ -0,0 +1,97 @@
+package checkers.types;
+
+import java.util.Collection;
+import java.util.Set;
+
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.Name;
+
+import checkers.source.SourceChecker;
+import com.sun.source.tree.CompilationUnitTree;
+
+/**
+ * A "general" annotated type factory that supports qualifiers from any type hierarchy.
+ * One big limitation is that it does not support annotations coming from a stub files.
+ */
+public class GeneralAnnotatedTypeFactory extends AnnotatedTypeFactory {
+
+    public GeneralAnnotatedTypeFactory(SourceChecker checker, CompilationUnitTree root) {
+        super(checker, new GeneralQualifierHierarchy(), root);
+    }
+
+    /** Return true to support any qualifier.
+      * No handling of aliases.
+      */
+    public boolean isSupportedQualifier(AnnotationMirror a) {
+        return true;
+    }
+
+}
+
+/** A very limited QualifierHierarchy that is used for access to
+  * qualifiers from different type systems.
+  */
+class GeneralQualifierHierarchy extends QualifierHierarchy {
+
+    @Override
+    public Set<AnnotationMirror> getTopAnnotations() {
+        SourceChecker.errorAbort("GeneralQualifierHierarchy:getTopAnnotations() was called! It shouldn't be called.");
+        return null;
+    }
+
+    @Override
+    public AnnotationMirror getTopAnnotation(AnnotationMirror start) {
+        return start;
+    }
+
+    @Override
+    public AnnotationMirror getBottomAnnotation(AnnotationMirror start) {
+        return start;
+    }
+
+    @Override
+    public Set<AnnotationMirror> getBottomAnnotations() {
+        SourceChecker.errorAbort("GeneralQualifierHierarchy:getBottomAnnotations() was called! It shouldn't be called.");
+        return null;
+    }
+
+    @Override
+    public Set<Name> getTypeQualifiers() {
+        SourceChecker.errorAbort("GeneralQualifierHierarchy:getTypeQualifiers() was called! It shouldn't be called.");
+        return null;
+    }
+
+    @Override
+    public boolean isSubtype(AnnotationMirror anno1, AnnotationMirror anno2) {
+        SourceChecker.errorAbort("GeneralQualifierHierarchy:isSubtype() was called! It shouldn't be called.");
+        return false;
+    }
+
+    @Override
+    public boolean isSubtype(Collection<AnnotationMirror> rhs,
+            Collection<AnnotationMirror> lhs) {
+        SourceChecker.errorAbort("GeneralQualifierHierarchy:isSubtyp() was called! It shouldn't be called.")
+        return false;
+    }
+
+    @Override
+    public AnnotationMirror leastUpperBound(AnnotationMirror a1,
+            AnnotationMirror a2) {
+        SourceChecker.errorAbort("GeneralQualifierHierarchy:leastUpperBound() was called! It shouldn't be called."
+        return null;
+    }
+
+    @Override
+    public AnnotationMirror greatestLowerBound(AnnotationMirror a1,
+            AnnotationMirror a2) {
+        SourceChecker.errorAbort("ba9");
+        return null;
+    }
+
+    @Override
+    public AnnotationMirror findCorrespondingAnnotation(
+            AnnotationMirror aliased, Set<AnnotationMirror> annotations) {
+        return null;
+    }
+}
