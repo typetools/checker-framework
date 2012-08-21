633aa8048dd181c9012dc945b588a910b0e3752e
wdietl <wdietl@gmail.com>
2012-08-20 23:34:23 -0700

    Adapt to changed constructor in superclass.

--- checkers/src/checkers/javari/JavariAnnotatedTypeFactory.java
+++ checkers/src/checkers/javari/JavariAnnotatedTypeFactory.java
@@ -80,7 +80,7 @@ public class JavariAnnotatedTypeFactory extends AnnotatedTypeFactory {
      */
     public JavariAnnotatedTypeFactory(JavariChecker checker,
         CompilationUnitTree root) {
-        super(checker, root);
+        super(checker, checker.getQualifierHierarchy(), root);
         this.treePre = new JavariTreePreAnnotator();
         this.typePost = new JavariTypePostAnnotator();
         this.READONLY = checker.READONLY;
