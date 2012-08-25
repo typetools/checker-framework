be4d5688cb600527128558708b26ef28ba51177f
wdietl <wdietl@gmail.com>
2012-08-25 02:50:32 -0700

    Make sure that we actually create non-null arrays in test cases.

--- checkers/tests/nullness/generics/MethodTypeVars.java
+++ checkers/tests/nullness/generics/MethodTypeVars.java
@@ -9,7 +9,7 @@ public class MethodTypeVars {
         //:: error: (type.argument.type.incompatible)
         Object a = A.badMethod(null);
         Object b = A.badMethod(new Object());
-                
+
         //:: error: (argument.type.incompatible)
         A.goodMethod(null);
         A.goodMethod(new Object());
@@ -20,7 +20,7 @@ class A {
     public static <T extends @NonNull Object> T badMethod(T t) {
         return (T) new Object();
     }
-    
+
     public static <T extends @NonNull Object> void goodMethod(T t) {}
 }
 
@@ -28,9 +28,9 @@ class B {
     public <T> void indexOf1(T[] a, /*@Nullable*/ Object elt) {}
     // This is not valid Java syntax.
     // public void indexOf2(?[] a, /*@Nullable*/ Object elt) {}
-    
+
     void call() {
-        Integer[] arg = new Integer[4];
+        Integer[] arg = new Integer[] { 1, 2, 3, 4 };
         indexOf1(arg, new Integer(5));
         // indexOf2(arg, new Integer(5));
     }
