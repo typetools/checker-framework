8682bdb3ac7fa13857c64d03412cb667891a1b83
wdietl <wdietl@gmail.com>
2012-09-24 14:35:29 -0700

    Add comments that should be discussed again later.

--- checkers/tests/nullness/UnexpectedRaw.java
+++ checkers/tests/nullness/UnexpectedRaw.java
@@ -6,17 +6,20 @@ interface Consumer<A extends @Nullable Object> {
 
 class Utils {
 
-  public static <A extends @Nullable Object>
-      Consumer<A> cast(final @Nullable Consumer<? super A> consumer) {
+  public static <B extends @Nullable Object>
+      Consumer<B> cast(final @Nullable Consumer<? super B> consumer) {
           throw new RuntimeException();
   }
 
 
-  public static <A extends @Nullable Object> Consumer<A>
+  public static <C extends @Nullable Object> Consumer<C>
       getConsumer() {
     // null for simplicity, but could be anything
     Consumer<@Nullable Object> nullConsumer = null;
-    Consumer<A> result = Utils.<A>cast(nullConsumer);
+    // C could be @NonNull Object, so argument is incompatible?
+    // Should this fail?
+    //TODO:: error: (argument.type.incompatible)
+    Consumer<C> result = Utils.<C>cast(nullConsumer);
     return result;
   }
 }
