cfc7521bfa2f5ea5424aebb77e38d010d695624a
stefanheule <devnull@localhost>
2012-09-24 02:23:01 +0200

    New variants of LUB/GLB/isSubtype methods that take a type and determine automatically if the *TypeVariable method or the normal method needs to be called.

--- checkers/src/checkers/types/QualifierHierarchy.java
+++ checkers/src/checkers/types/QualifierHierarchy.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Name;
+import javax.lang.model.type.TypeKind;
 
 import checkers.util.AnnotationUtils;
 
@@ -325,6 +326,152 @@ public abstract class QualifierHierarchy {
             }
         }
         return result;
+    }
+
+    /**
+     * Returns true if and only if the given type can have empty annotation sets
+     * (and thus the *TypeVariable methods need to be used).
+     */
+    protected boolean canHaveEmptyAnnotationSet(AnnotatedTypeMirror type) {
+        return type.getKind() == TypeKind.TYPEVAR;
+    }
+
+    /**
+     * Tests whether anno1 is a sub-qualifier of anno2, according to the
+     * type qualifier hierarchy.  This checks only the qualifiers, not the
+     * Java type.
+     *
+     * <p>
+     * This method takes an annotated type to decide if the type variable version of
+     * the method should be invoked, or if the normal version is sufficient (which
+     * provides more strict checks).
+     *
+     * @return true iff anno1 is a sub qualifier of anno2
+     */
+    public boolean isSubtype(AnnotatedTypeMirror type, AnnotationMirror anno1, AnnotationMirror anno2) {
+        if (canHaveEmptyAnnotationSet(type)) {
+            return isSubtypeTypeVariable(anno1, anno2);
+        } else {
+            return isSubtype(anno1, anno2);
+        }
+    }
+
+    /**
+     * Tests whether there is any annotation in lhs that is a super qualifier
+     * of some annotation in rhs.
+     * lhs and rhs contain only the annotations, not the Java type.
+     *
+     * <p>
+     * This method takes an annotated type to decide if the type variable version of
+     * the method should be invoked, or if the normal version is sufficient (which
+     * provides more strict checks).
+     *
+     * @return true iff an annotation in lhs is a super of one in rhs
+     **/
+    public boolean isSubtype(AnnotatedTypeMirror type, Collection<AnnotationMirror> rhs, Collection<AnnotationMirror> lhs) {
+        if (canHaveEmptyAnnotationSet(type)) {
+            return isSubtypeTypeVariable(rhs, lhs);
+        } else {
+            return isSubtype(rhs, lhs);
+        }
+    }
+
+    /**
+     * Returns the  least upper bound for the qualifiers a1 and a2.
+     *
+     * Examples:
+     * For NonNull, leastUpperBound('Nullable', 'NonNull') ==> Nullable
+     * For IGJ,     leastUpperBound('Immutable', 'Mutable') ==> ReadOnly
+     *
+     * The two qualifiers have to be from the same qualifier hierarchy. Otherwise,
+     * null will be returned.
+     *
+     * <p>
+     * This method takes an annotated type to decide if the type variable version of
+     * the method should be invoked, or if the normal version is sufficient (which
+     * provides more strict checks).
+     *
+     * @return  the least restrictive qualifiers for both types
+     */
+    public AnnotationMirror leastUpperBound(AnnotatedTypeMirror type, AnnotationMirror a1, AnnotationMirror a2) {
+        if (canHaveEmptyAnnotationSet(type)) {
+            return leastUpperBoundTypeVariable(a1, a2);
+        } else {
+            return leastUpperBound(a1, a2);
+        }
+    }
+
+    /**
+     * Returns the greatest lower bound for the qualifiers a1 and a2.
+     *
+     * The two qualifiers have to be from the same qualifier hierarchy. Otherwise,
+     * null will be returned.
+     *
+     * <p>
+     * This method takes an annotated type to decide if the type variable version of
+     * the method should be invoked, or if the normal version is sufficient (which
+     * provides more strict checks).
+     *
+     * @param a1 First annotation
+     * @param a2 Second annotation
+     * @return Greatest lower bound of the two annotations
+     */
+    public AnnotationMirror greatestLowerBound(AnnotatedTypeMirror type, AnnotationMirror a1, AnnotationMirror a2) {
+        if (canHaveEmptyAnnotationSet(type)) {
+            return greatestLowerBoundTypeVariable(a1, a2);
+        } else {
+            return greatestLowerBound(a1, a2);
+        }
+    }
+
+    /**
+     * Returns the type qualifiers that are the least upper bound of
+     * the qualifiers in annos1 and annos2.
+     * <p>
+     *
+     * This is necessary for determining the type of a conditional
+     * expression (<tt>?:</tt>), where the type of the expression is the
+     * least upper bound of the true and false clauses.
+     *
+     * <p>
+     * This method takes an annotated type to decide if the type variable version of
+     * the method should be invoked, or if the normal version is sufficient (which
+     * provides more strict checks).
+     *
+     * @return the least upper bound of annos1 and annos2
+     */
+    public Set<AnnotationMirror>
+    leastUpperBounds(AnnotatedTypeMirror type, Collection<AnnotationMirror> annos1, Collection<AnnotationMirror> annos2) {
+        if (canHaveEmptyAnnotationSet(type)) {
+            return leastUpperBoundsTypeVariable(annos1, annos2);
+        } else {
+            return leastUpperBounds(annos1, annos2);
+        }
+    }
+
+    /**
+     * Returns the type qualifiers that are the greatest lower bound of
+     * the qualifiers in annos1 and annos2.
+     *
+     * The two qualifiers have to be from the same qualifier hierarchy. Otherwise,
+     * null will be returned.
+     *
+     * <p>
+     * This method takes an annotated type to decide if the type variable version of
+     * the method should be invoked, or if the normal version is sufficient (which
+     * provides more strict checks).
+     *
+     * @param annos1 First collection of qualifiers
+     * @param annos2 Second collection of qualifiers
+     * @return Greatest lower bound of the two collections of qualifiers
+     */
+    public Set<AnnotationMirror>
+    greatestLowerBounds(AnnotatedTypeMirror type, Collection<AnnotationMirror> annos1, Collection<AnnotationMirror> annos2) {
+        if (canHaveEmptyAnnotationSet(type)) {
+            return greatestLowerBoundsTypeVariable(annos1, annos2);
+        } else {
+            return greatestLowerBounds(annos1, annos2);
+        }
     }
 
     public AnnotationMirror findCorrespondingAnnotation(
