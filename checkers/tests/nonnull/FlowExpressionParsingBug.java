2d817dc26cdf8c598e6326ee92109b51c857d773
stefanheule <devnull@localhost>
2012-09-28 18:06:58 +0200

    New test case for an issue recently fixed.

---
+++ checkers/tests/nonnull/FlowExpressionParsingBug.java
@@ -0,0 +1,15 @@
+import javax.swing.JMenuBar;
+
+import checkers.nonnull.quals.Nullable;
+import checkers.nonnull.quals.RequiresNonNull;
+
+
+public abstract class FlowExpressionParsingBug {
+   
+    protected @Nullable JMenuBar menuBar = null;
+
+    @RequiresNonNull("menuBar")
+    public void addFavorite() {
+        
+    }
+}
