752a2bdcd98abce4bcaaed8209a47dd2659ba37f
jonathangburke <jonathangburke@gmail.com>
2012-09-29 15:38:55 -0700

    Maven plugin no longer uses tabs in code and has improved docs

--- maven-plugin/src/main/java/org/checkersplugin/CheckersMojo.java
+++ maven-plugin/src/main/java/org/checkersplugin/CheckersMojo.java
@@ -37,68 +37,68 @@ import java.io.File;
  * @goal check
  */
 public class CheckersMojo extends AbstractMojo {
-	/**
-	 * PARAMETERS
-	 */
+    /**
+     * PARAMETERS
+     */
 
-	/**
+    /**
      * The list of checkers to pass to the checker compiler
      *
-	 * @parameter
-	 * @required
-	 */
-	private List<String> processors = new ArrayList<String>();
+     * @parameter
+     * @required
+     */
+    private List<String> processors = new ArrayList<String>();
 
-	/**
-	 * A list of inclusion filters for the compiler.
-	 * When CheckersMojo scans the "${compileSourceRoot}" directory for files it will only include those files
+    /**
+     * A list of inclusion filters for the compiler.
+     * When CheckersMojo scans the "${compileSourceRoot}" directory for files it will only include those files
      * that match one of the specified inclusion patterns.  If no patterns are included then
      * PathUtils.DEFAULT_INCLUSION_PATTERN is used
      *
-	 * @parameter
-	 */
-	private Set<String> includes = new HashSet<String>();
+     * @parameter
+     */
+    private Set<String> includes = new HashSet<String>();
 
-	/**
-	 * A list of exclusion filters for the compiler.  When CheckersMojo scans the "${compileSourceRoot}"
+    /**
+     * A list of exclusion filters for the compiler.  When CheckersMojo scans the "${compileSourceRoot}"
      * directory for files it will only include those file that DO NOT match any of the
      * specified exclusion patterns.
-	 *
-	 * @parameter
-	 */
-	private Set<String> excludes = new HashSet<String>();
+     *
+     * @parameter
+     */
+    private Set<String> excludes = new HashSet<String>();
 
-	/**
-	 * Should the build fail on a checker compile error.
-	 *
-	 * @parameter default-value="true"
-	 */
-	private boolean failOnError;
+    /**
+     * Should the build fail on a checker compile error.
+     *
+     * @parameter default-value="true"
+     */
+    private boolean failOnError;
 
-	/**
+    /**
      * The path to the java executable to use, default is "java"
      * This executable is used to call the jsr308 compiler jar
-	 * @parameter
-	 */
-	private String executable;
+     * @parameter
+     */
+    private String executable;
 
-	/**
+    /**
      * Which version of the JSR308 checkers to use
-	 * @parameter default-value="1.0.6"
-	 */
-	private String checkersVersion;
+     * @parameter default-value="1.4.0"
+     */
+    private String checkersVersion;
 
-	/**
+    /**
      * Java runtime parameters added when running the JSR308 compiler jar
-	 * @parameter
-	 */
-	private String javaParams;
+     * @parameter
+     */
+    private String javaParams;
 
-	/**
+    /**
      * Javac params passed to the JSR308 compiler jar
-	 * @parameter
-	 */
-	private String javacParams;
+     * @parameter
+     */
+    private String javacParams;
 
     /**
      * Whether to skip execution
@@ -106,28 +106,28 @@ public class CheckersMojo extends AbstractMojo {
      */
      private boolean skip;
 
-	/**
-	 * DEPENDENCIES
-	 */
-	
-	/**
-	 * The source directories containing the sources to be compiled.
-	 *
-	 * @parameter expression="${project.compileSourceRoots}"
-	 * @required
-	 * @readonly
-	 */
-	private List<String> compileSourceRoots;
+    /**
+     * DEPENDENCIES
+     */
+    
+    /**
+     * The source directories containing the sources to be compiled.
+     *
+     * @parameter expression="${project.compileSourceRoots}"
+     * @required
+     * @readonly
+     */
+    private List<String> compileSourceRoots;
 
-	/**
-	 * The current build session instance. This is used for
-	 * toolchain manager API calls.
-	 *
-	 * @parameter expression="${session}"
-	 * @required
-	 * @readonly
-	 */
-	private MavenSession session;
+    /**
+     * The current build session instance. This is used for
+     * toolchain manager API calls.
+     *
+     * @parameter expression="${session}"
+     * @required
+     * @readonly
+     */
+    private MavenSession session;
 
     /**
      * @parameter expression="${project}"
@@ -137,46 +137,46 @@ public class CheckersMojo extends AbstractMojo {
     private MavenProject project;
 
     /**
-	 * @component
-	 * @required
-	 * @readonly
-	 */
-	private ToolchainManager toolchainManager;
+     * @component
+     * @required
+     * @readonly
+     */
+    private ToolchainManager toolchainManager;
 
-	/**
-	 * @component role="org.apache.maven.artifact.resolver.ArtifactResolver"
-	 * @required
-	 * @readonly
-	 */
-	private ArtifactResolver artifactResolver;
+    /**
+     * @component role="org.apache.maven.artifact.resolver.ArtifactResolver"
+     * @required
+     * @readonly
+     */
+    private ArtifactResolver artifactResolver;
 
-	/**
-	 * @component role="org.apache.maven.artifact.factory.ArtifactFactory"
-	 * @required
-	 * @readonly
-	 */
-	private ArtifactFactory artifactFactory;
+    /**
+     * @component role="org.apache.maven.artifact.factory.ArtifactFactory"
+     * @required
+     * @readonly
+     */
+    private ArtifactFactory artifactFactory;
 
-	/**
-	 * @parameter expression="${localRepository}"
-	 * @required
-	 * @readonly
-	 */
-	private ArtifactRepository localRepository;
+    /**
+     * @parameter expression="${localRepository}"
+     * @required
+     * @readonly
+     */
+    private ArtifactRepository localRepository;
 
-	/**
-	 * @parameter expression="${project.remoteArtifactRepositories}"
-	 * @required
-	 * @readonly
-	 */
-	private List<?> remoteArtifactRepositories;
+    /**
+     * @parameter expression="${project.remoteArtifactRepositories}"
+     * @required
+     * @readonly
+     */
+    private List<?> remoteArtifactRepositories;
 
-	/**
-	 * @parameter expression="${project.compileClasspathElements}"
-	 * @required
-	 * @readonly
-	 */
-	private List<?> classpathElements;
+    /**
+     * @parameter expression="${project.compileClasspathElements}"
+     * @required
+     * @readonly
+     */
+    private List<?> classpathElements;
 
     /**
      * Main control method for the Checker Maven Plugin.  Scans for sources, resolves classpath, and passes these
@@ -184,122 +184,122 @@ public class CheckersMojo extends AbstractMojo {
      * @throws MojoExecutionException
      * @throws MojoFailureException
      */
-	public void execute() throws MojoExecutionException, MojoFailureException {
-		final Log log = getLog();
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        final Log log = getLog();
 
         if (skip) {
-        	log.info("Execution is skipped");
-        	return;
+            log.info("Execution is skipped");
+            return;
         } else if ("pom".equals(project.getPackaging())) {
-        	log.info("Execution is skipped for project with packaging 'pom'");
-        	return;
+            log.info("Execution is skipped for project with packaging 'pom'");
+            return;
         }
 
-		log.info("Running JSR308 checkers version: " + checkersVersion);
-		
-		if (processors.size() == 0) {
-			throw new MojoExecutionException("At least one checker must be specified!");
-		}
+        log.info("Running JSR308 checkers version: " + checkersVersion);
+        
+        if (processors.size() == 0) {
+            throw new MojoExecutionException("At least one checker must be specified!");
+        }
 
-		final String processor = StringUtils.join(processors.iterator(), ",");
+        final String processor = StringUtils.join(processors.iterator(), ",");
 
-		log.info("Running processor(s): " + processor);
+        log.info("Running processor(s): " + processor);
 
-		final List<String> sources = PathUtils.scanForSources(compileSourceRoots, includes, excludes);
-		
-		final String checkersJar = PathUtils.getCheckersJar(checkersVersion, artifactFactory, artifactResolver,
-				remoteArtifactRepositories, localRepository);
+        final List<String> sources = PathUtils.scanForSources(compileSourceRoots, includes, excludes);
+        
+        final String checkersJar = PathUtils.getCheckersJar(checkersVersion, artifactFactory, artifactResolver,
+                remoteArtifactRepositories, localRepository);
 
-		final Commandline cl = new Commandline();
+        final Commandline cl = new Commandline();
 
-		if (StringUtils.isEmpty(executable)) {
-			executable = "java";
-		}
-		cl.setExecutable(PathUtils.getExecutablePath(executable, toolchainManager, session));
+        if (StringUtils.isEmpty(executable)) {
+            executable = "java";
+        }
+        cl.setExecutable(PathUtils.getExecutablePath(executable, toolchainManager, session));
 
-		// Building the arguments
-		final List<String> arguments = new ArrayList<String>();
+        // Building the arguments
+        final List<String> arguments = new ArrayList<String>();
 
-		// Setting the boot class path: prepending the jar with jsr308 compiler
-		arguments.add("-Xbootclasspath/p:" + checkersJar);
-		// Javac currently assumes that assertions are enabled in the launcher
-		arguments.add("-ea:com.sun.tools");
-		// Optionally adding user-specified java parameters
-		if (!StringUtils.isEmpty(javaParams)) {
-			arguments.addAll(Arrays.asList(javaParams.split(" ")));
-		}
-		// Running the compile process - main class of this jar 
-		arguments.add("-jar");
-		arguments.add(checkersJar);
+        // Setting the boot class path: prepending the jar with jsr308 compiler
+        arguments.add("-Xbootclasspath/p:" + checkersJar);
+        // Javac currently assumes that assertions are enabled in the launcher
+        arguments.add("-ea:com.sun.tools");
+        // Optionally adding user-specified java parameters
+        if (!StringUtils.isEmpty(javaParams)) {
+            arguments.addAll(Arrays.asList(javaParams.split(" ")));
+        }
+        // Running the compile process - main class of this jar 
+        arguments.add("-jar");
+        arguments.add(checkersJar);
 
-		// Now the arguments for the jar main class - that is, the compiler
+        // Now the arguments for the jar main class - that is, the compiler
 
-		// Setting the name of the processor
-		arguments.add("-processor");
-		arguments.add(processor);
-		// Running only the annotation processor, without compiling
-		arguments.add("-proc:only");
-		// Setting the classpath
-		arguments.add("-classpath" );
-		arguments.add(StringUtils.join(classpathElements.iterator(), File.pathSeparator));
-		// Setting the source dir path
-		arguments.add("-sourcepath");
-		arguments.add(StringUtils.join(compileSourceRoots.iterator(), File.pathSeparator));
-		// Optionally adding user-specified javac parameters
-		if (!StringUtils.isEmpty(javacParams)) {
-			arguments.addAll(Arrays.asList(javacParams.split(" ")));
-		}
-		// Now the source files
-		arguments.addAll(sources);
+        // Setting the name of the processor
+        arguments.add("-processor");
+        arguments.add(processor);
+        // Running only the annotation processor, without compiling
+        arguments.add("-proc:only");
+        // Setting the classpath
+        arguments.add("-classpath" );
+        arguments.add(StringUtils.join(classpathElements.iterator(), File.pathSeparator));
+        // Setting the source dir path
+        arguments.add("-sourcepath");
+        arguments.add(StringUtils.join(compileSourceRoots.iterator(), File.pathSeparator));
+        // Optionally adding user-specified javac parameters
+        if (!StringUtils.isEmpty(javacParams)) {
+            arguments.addAll(Arrays.asList(javacParams.split(" ")));
+        }
+        // Now the source files
+        arguments.addAll(sources);
 
-		// And executing
-		cl.addArguments(arguments.toArray(new String[arguments.size()]));
+        // And executing
+        cl.addArguments(arguments.toArray(new String[arguments.size()]));
 
-		executeCommandLine(cl, log);
-	}
+        executeCommandLine(cl, log);
+    }
 
-	private void executeCommandLine(final Commandline cl, final Log log) throws MojoExecutionException, MojoFailureException {
-		CommandLineUtils.StringStreamConsumer out = new CommandLineUtils.StringStreamConsumer();
-		CommandLineUtils.StringStreamConsumer err = new CommandLineUtils.StringStreamConsumer();
-		
-		log.debug("command line: " + Arrays.toString(cl.getCommandline()));
-	
-		
-		// Executing the command
-		int exitCode;
-		try {
-			exitCode = CommandLineUtils.executeCommandLine(cl, out, err);
-		} catch (CommandLineException e) {
-			throw new MojoExecutionException("Unable to execute checkers, executable: " + executable +
-					", command line: " + Arrays.toString(cl.getCommandline()), e);
-		}
+    private void executeCommandLine(final Commandline cl, final Log log) throws MojoExecutionException, MojoFailureException {
+        CommandLineUtils.StringStreamConsumer out = new CommandLineUtils.StringStreamConsumer();
+        CommandLineUtils.StringStreamConsumer err = new CommandLineUtils.StringStreamConsumer();
+        
+        log.debug("command line: " + Arrays.toString(cl.getCommandline()));
+    
+        
+        // Executing the command
+        int exitCode;
+        try {
+            exitCode = CommandLineUtils.executeCommandLine(cl, out, err);
+        } catch (CommandLineException e) {
+            throw new MojoExecutionException("Unable to execute checkers, executable: " + executable +
+                    ", command line: " + Arrays.toString(cl.getCommandline()), e);
+        }
 
-		// Parsing the messages from the compiler
-		List<CompilerError> messages;
-		try {
-			messages = JavacCompilerUtil.parseModernStream( new BufferedReader( new StringReader( err.getOutput() ) ) );
-		} catch (IOException e) {
-			throw new MojoExecutionException("Unable to parse messages.", e);
-		}
+        // Parsing the messages from the compiler
+        List<CompilerError> messages;
+        try {
+            messages = JavacCompilerUtil.parseModernStream( new BufferedReader( new StringReader( err.getOutput() ) ) );
+        } catch (IOException e) {
+            throw new MojoExecutionException("Unable to parse messages.", e);
+        }
 
-		// Sanity check - if the exit code is non-zero, there should be some messages
-		if (exitCode != 0 && messages.isEmpty()) {
-			throw new MojoExecutionException("Exit code from the compiler was not zero (" + exitCode +
-					"), but no messages reported. Error stream content: " + err.getOutput() + 
-					" command line: " + Arrays.toString(cl.getCommandline()));
-		}
+        // Sanity check - if the exit code is non-zero, there should be some messages
+        if (exitCode != 0 && messages.isEmpty()) {
+            throw new MojoExecutionException("Exit code from the compiler was not zero (" + exitCode +
+                    "), but no messages reported. Error stream content: " + err.getOutput() + 
+                    " command line: " + Arrays.toString(cl.getCommandline()));
+        }
 
-		if (messages.isEmpty()) {
-			log.info("No errors found by the processor(s).");
-		} else {
-			if (failOnError) {
-				throw new MojoFailureException(CompilationFailureException.longMessage(messages));
-			} else {
+        if (messages.isEmpty()) {
+            log.info("No errors found by the processor(s).");
+        } else {
+            if (failOnError) {
+                throw new MojoFailureException(CompilationFailureException.longMessage(messages));
+            } else {
                 log.info("Run with debug logging in order to view the compiler command line");
-				for (CompilerError compilerError : messages) {
-					log.warn(compilerError.toString());
-				}
-			}
-		}
-	}
+                for (CompilerError compilerError : messages) {
+                    log.warn(compilerError.toString());
+                }
+            }
+        }
+    }
 }
--- maven-plugin/src/main/java/org/checkersplugin/PathUtils.java
+++ maven-plugin/src/main/java/org/checkersplugin/PathUtils.java
@@ -25,92 +25,92 @@ import java.util.Set;
  * @author Adam Warski (adam at warski dot org)
  */
 public class PathUtils {
-	private final static String CHECKERS_GROUPD_ID = "types.checkers";
-	private final static String CHECKERS_ARTIFACT_ID = "jsr308-all";
+    private final static String CHECKERS_GROUPD_ID = "types.checkers";
+    private final static String CHECKERS_ARTIFACT_ID = "jsr308-all";
     private final static String DEFAULT_INCLUSION_PATTERN = "**/*.java";
 
-	/**
-	 * Gets the path to the jsr308-all jar.
-	 * @param checkersVersion Version of checkers to use.
-	 * @param artifactFactory Dependency.
-	 * @param artifactResolver Dependency.
-	 * @param remoteArtifactRepositories Dependency.
-	 * @param localRepository Dependency.
-	 * @return Path to the jsr308-all jar.
-	 * @throws MojoExecutionException
-	 */
-	public static String getCheckersJar(final String checkersVersion, final ArtifactFactory artifactFactory,
-										final ArtifactResolver artifactResolver, final List<?> remoteArtifactRepositories,
-										final ArtifactRepository localRepository) throws MojoExecutionException {
-		final Artifact checkersArtifact;
-		try {
-			checkersArtifact = artifactFactory.createExtensionArtifact(CHECKERS_GROUPD_ID, CHECKERS_ARTIFACT_ID,
-					VersionRange.createFromVersionSpec(checkersVersion));
-		} catch (InvalidVersionSpecificationException e) {
-			throw new MojoExecutionException("Wrong version: " + checkersVersion + " of checkers specifed.", e);
-		}
+    /**
+     * Gets the path to the jsr308-all jar.
+     * @param checkersVersion Version of checkers to use.
+     * @param artifactFactory Dependency.
+     * @param artifactResolver Dependency.
+     * @param remoteArtifactRepositories Dependency.
+     * @param localRepository Dependency.
+     * @return Path to the jsr308-all jar.
+     * @throws MojoExecutionException
+     */
+    public static String getCheckersJar(final String checkersVersion, final ArtifactFactory artifactFactory,
+                                        final ArtifactResolver artifactResolver, final List<?> remoteArtifactRepositories,
+                                        final ArtifactRepository localRepository) throws MojoExecutionException {
+        final Artifact checkersArtifact;
+        try {
+            checkersArtifact = artifactFactory.createExtensionArtifact(CHECKERS_GROUPD_ID, CHECKERS_ARTIFACT_ID,
+                    VersionRange.createFromVersionSpec(checkersVersion));
+        } catch (InvalidVersionSpecificationException e) {
+            throw new MojoExecutionException("Wrong version: " + checkersVersion + " of checkers specifed.", e);
+        }
 
-		try {
-			artifactResolver.resolve(checkersArtifact, remoteArtifactRepositories, localRepository);
-		} catch (ArtifactResolutionException e) {
-			throw new MojoExecutionException("Unable to find version " + checkersVersion + " of checkers.", e);
-		} catch (ArtifactNotFoundException e) {
-			throw new MojoExecutionException("Unable to resolve version " + checkersVersion + " of checkers.", e);
-		}
+        try {
+            artifactResolver.resolve(checkersArtifact, remoteArtifactRepositories, localRepository);
+        } catch (ArtifactResolutionException e) {
+            throw new MojoExecutionException("Unable to find version " + checkersVersion + " of checkers.", e);
+        } catch (ArtifactNotFoundException e) {
+            throw new MojoExecutionException("Unable to resolve version " + checkersVersion + " of checkers.", e);
+        }
 
-		return checkersArtifact.getFile().getPath();
-	}
+        return checkersArtifact.getFile().getPath();
+    }
 
-	/**
-	 * Gets the path to the given executable, looking it up in the toolchain.
-	 * @param executable Name of the executable.
-	 * @param toolchainManager Dependency.
-	 * @param session Dependency.
-	 * @return Path to the executable.
-	 */
-	public static String getExecutablePath(String executable, final ToolchainManager toolchainManager, final MavenSession session) {
-		final File execFile = new File(executable);
-		if (execFile.exists()) {
-			return execFile.getAbsolutePath();
-		} else {
-			final Toolchain tc = toolchainManager.getToolchainFromBuildContext("jdk", session);
+    /**
+     * Gets the path to the given executable, looking it up in the toolchain.
+     * @param executable Name of the executable.
+     * @param toolchainManager Dependency.
+     * @param session Dependency.
+     * @return Path to the executable.
+     */
+    public static String getExecutablePath(String executable, final ToolchainManager toolchainManager, final MavenSession session) {
+        final File execFile = new File(executable);
+        if (execFile.exists()) {
+            return execFile.getAbsolutePath();
+        } else {
+            final Toolchain tc = toolchainManager.getToolchainFromBuildContext("jdk", session);
 
-			if (tc != null) {
-				executable = tc.findTool(executable);
-			}
-		}
+            if (tc != null) {
+                executable = tc.findTool(executable);
+            }
+        }
 
-		return executable;
-	}
+        return executable;
+    }
 
-	/**
-	 * Scans the given compile source roots for sources, taking into account the given includes and excludes.
-	 * @param compileSourceRoots A list of source roots. 
-	 * @param sourceIncludes Includes specification.  Defaults to DEFAULT_INCLUSION_PATTERN if no sourceIncludes are
+    /**
+     * Scans the given compile source roots for sources, taking into account the given includes and excludes.
+     * @param compileSourceRoots A list of source roots. 
+     * @param sourceIncludes Includes specification.  Defaults to DEFAULT_INCLUSION_PATTERN if no sourceIncludes are
      *                       specified
-	 * @param sourceExcludes Excludes specification.
-	 * @return A list of included sources from the given source roots.
-	 */
-	public static List<String> scanForSources(final List<?> compileSourceRoots, final Set<String> sourceIncludes,
+     * @param sourceExcludes Excludes specification.
+     * @return A list of included sources from the given source roots.
+     */
+    public static List<String> scanForSources(final List<?> compileSourceRoots, final Set<String> sourceIncludes,
                                               final Set<String> sourceExcludes) {
-		if (sourceIncludes.isEmpty()) {
-			sourceIncludes.add(DEFAULT_INCLUSION_PATTERN);
-		}
+        if (sourceIncludes.isEmpty()) {
+            sourceIncludes.add(DEFAULT_INCLUSION_PATTERN);
+        }
 
-		final List<String> sources = new ArrayList<String>();
+        final List<String> sources = new ArrayList<String>();
 
-		for (Object compileSourceRoot : compileSourceRoots) {
-			final File compileSourceRootFile = new File(compileSourceRoot.toString());
-			final String[] sourcesFromSourceRoot =
-					scanForSources(compileSourceRootFile, sourceIncludes, sourceExcludes);
+        for (Object compileSourceRoot : compileSourceRoots) {
+            final File compileSourceRootFile = new File(compileSourceRoot.toString());
+            final String[] sourcesFromSourceRoot =
+                    scanForSources(compileSourceRootFile, sourceIncludes, sourceExcludes);
 
-			for (final String sourceFromSourceRoot : sourcesFromSourceRoot) {
-				sources.add(new File(compileSourceRootFile, sourceFromSourceRoot).getAbsolutePath());
-			}
-		}
+            for (final String sourceFromSourceRoot : sourcesFromSourceRoot) {
+                sources.add(new File(compileSourceRootFile, sourceFromSourceRoot).getAbsolutePath());
+            }
+        }
 
-		return sources;
-	}
+        return sources;
+    }
 
     /**
      * Scans a single source dir for sources and includes only the files whose name match the patterns in
@@ -120,19 +120,19 @@ public class PathUtils {
      * @param sourceExcludes Exclude a file if its name matches a pattern in sourceExcludes
      * @return A set of filepath strings
      */
-	private static String[] scanForSources(final File sourceDir, final Set<String> sourceIncludes,
+    private static String[] scanForSources(final File sourceDir, final Set<String> sourceIncludes,
                                            final Set<String> sourceExcludes) {
-		final DirectoryScanner ds = new DirectoryScanner();
-		ds.setFollowSymlinks( true );
-		ds.setBasedir( sourceDir );
+        final DirectoryScanner ds = new DirectoryScanner();
+        ds.setFollowSymlinks( true );
+        ds.setBasedir( sourceDir );
 
-		ds.setIncludes(sourceIncludes.toArray(new String[sourceIncludes.size()]));
-		ds.setExcludes(sourceExcludes.toArray(new String[sourceExcludes.size()]));
+        ds.setIncludes(sourceIncludes.toArray(new String[sourceIncludes.size()]));
+        ds.setExcludes(sourceExcludes.toArray(new String[sourceExcludes.size()]));
 
-		ds.addDefaultExcludes();
+        ds.addDefaultExcludes();
 
-		ds.scan();
+        ds.scan();
 
-		return ds.getIncludedFiles();
-	}
+        return ds.getIncludedFiles();
+    }
 }
