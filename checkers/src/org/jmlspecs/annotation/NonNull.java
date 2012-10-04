3507f4ee637806c9b93ce8ab28b28365cfa6da5f
Michael Ernst <mernst@cs.washington.edu>
2012-10-03 21:03:47 -0700

    Add @Target annotations

--- checkers/src/org/jmlspecs/annotation/NonNull.java
+++ checkers/src/org/jmlspecs/annotation/NonNull.java
@@ -3,9 +3,16 @@ package org.jmlspecs.annotation;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE_PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import checkers.quals.TypeQualifier;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@Target({TYPE_USE, TYPE_PARAMETER})
+@TypeQualifier
 public @interface NonNull {
 
 }
