79128c999b0fc4af90754d6aab75aaadf39d0c8a
stefanheule <stefanheule@localhost>
2012-10-09 12:21:48 +0200

    Update two testcases for rawness checker.

--- checkers/tests/nonnull/PostconditionBug.java
+++ checkers/tests/nonnull/PostconditionBug.java
@@ -1,27 +1,14 @@
-import checkers.initialization.quals.Unclassified;
-import checkers.nonnull.quals.NonNull;
+import checkers.initialization.quals.*;
+import checkers.nonnull.quals.*;
 
 import checkers.nonnull.quals.EnsuresNonNull;
 
 public class PostconditionBug {
     
-    @NonNull String c; 
 
-    public PostconditionBug() {
-        super();
-        b();
-   }
-
-    void a(@Unclassified PostconditionBug this) {
+    void a(@Unclassified @Raw PostconditionBug this) {
          @NonNull String f = "abc";
-         
          //:: error: (assignment.type.incompatible)
          f = null;
      }
-     
-    @EnsuresNonNull("c")
-    void b(@Unclassified PostconditionBug this) {
-        c = "c";
-        a();
-    }
 }
--- checkers/tests/nonnull/ThisLiteral.java
+++ checkers/tests/nonnull/ThisLiteral.java
@@ -5,9 +5,9 @@ public class ThisLiteral {
     public ThisLiteral() {
         new Object() {
             void test() {
-                @Free ThisLiteral l1 = ThisLiteral.this;
+                @Free @Raw ThisLiteral l1 = ThisLiteral.this;
                 //:: error: (assignment.type.incompatible)
-                @Committed ThisLiteral l2 = ThisLiteral.this;
+                @Committed @NonRaw ThisLiteral l2 = ThisLiteral.this;
 
                 //:: error: (method.invocation.invalid)
                 ThisLiteral.this.foo();
