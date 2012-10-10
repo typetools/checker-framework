72c500570978a1f22aa32b74cafd0c4849b30ea7
Eric Spishak <espishak@cs.washington.edu>
2012-10-09 23:09:53 -0700

    Add CheckedPatternSyntaxException static class to RegexUtil.

--- checkers/src/checkers/regex/RegexUtil.java
+++ checkers/src/checkers/regex/RegexUtil.java
@@ -29,6 +29,86 @@ import checkers.regex.quals.*;
  */
 public class RegexUtil {
 
+  /**
+   * A checked version of {@link PatternSyntaxException}.
+   * <p>
+   * This exception is useful when an illegal regex is detected but the
+   * contextual information to report a helpful error message is not available
+   * at the current depth in the call stack. By using a checked
+   * PatternSyntaxException the error must be handled up the call stack where
+   * a better error message can be reported.
+   *
+   * @see PatternSyntaxException
+   */
+  public static class CheckedPatternSyntaxException extends Exception {
+
+    private static final long serialVersionUID = 6266881831979001480L;
+
+    private final PatternSyntaxException pse;
+
+    /**
+     * Constructs a new CheckedPatternSyntaxException equivalent to the
+     * given {@link PatternSyntaxException}.
+     * <p>
+     * Consider calling this constructor with the result of
+     * {@link RegexUtil#regexError}.
+     */
+    public CheckedPatternSyntaxException(PatternSyntaxException pse) {
+      this.pse = pse;
+    }
+
+    /**
+     * Constructs a new CheckedPatternSyntaxException.
+     *
+     * @param desc A description of the error
+     * @param regex The erroneous pattern
+     * @param index The approximate index in the pattern of the error,
+     *              or {@code -1} if the index is not known
+     */
+    public CheckedPatternSyntaxException(String desc, String regex, int index) {
+      this(new PatternSyntaxException(desc, regex, index));
+    }
+
+    /**
+     * Retrieves the description of the error.
+     *
+     * @return The description of the error
+     */
+    public String getDescription() {
+      return pse.getDescription();
+    }
+
+    /**
+     * Retrieves the error index.
+     *
+     * @return The approximate index in the pattern of the error, or {@code -1}
+     *         if the index is not known
+     */
+    public int getIndex() {
+      return pse.getIndex();
+    }
+
+    /**
+     * Returns a multi-line string containing the description of the syntax
+     * error and its index, the erroneous regular-expression pattern, and a
+     * visual indication of the error index within the pattern.
+     *
+     * @return The full detail message
+     */
+    public String getMessage() {
+        return pse.getMessage();
+    }
+
+    /**
+     * Retrieves the erroneous regular-expression pattern.
+     *
+     * @return The erroneous pattern
+     */
+    public String getPattern() {
+      return pse.getPattern();
+    }
+  }
+
   private RegexUtil() {
     throw new AssertionError("Class RegexUtil shouldn't be instantiated");
   }
