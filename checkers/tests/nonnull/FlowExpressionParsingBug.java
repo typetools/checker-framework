5438d22f2e27608a91d872193dd883890dd63cb9
stefanheule <stefanheule@localhost>
2012-10-31 14:58:49 +0100

    Test static field access in flow expressions.

--- checkers/tests/nonnull/FlowExpressionParsingBug.java
+++ checkers/tests/nonnull/FlowExpressionParsingBug.java
@@ -15,4 +15,38 @@ public abstract class FlowExpressionParsingBug {
     @RequiresNonNull("this.menuBar")
     public void addFavorite1() {
     }
+    
+    static @Nullable String i = null;
+
+    @RequiresNonNull("FlowExpressionParsingBug.i")
+    public void a() {
+    }
+    
+    @RequiresNonNull("i")
+    public void b() {
+    }
+    
+    void test1() {
+        //:: error: (contracts.precondition.not.satisfied)
+        a();
+        FlowExpressionParsingBug.i = "";
+        a();
+        
+        //:: error: (contracts.precondition.not.satisfied)
+        a();
+        i = "";
+        a();
+    }
+    
+    void test2() {
+        //:: error: (contracts.precondition.not.satisfied)
+        b();
+        FlowExpressionParsingBug.i = "";
+        b();
+        
+        //:: error: (contracts.precondition.not.satisfied)
+        b();
+        i = "";
+        b();
+    }
 }
