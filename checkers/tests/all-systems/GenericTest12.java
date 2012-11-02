9dbcd2316d487820b5311ba689e2dd84f89e4d33
wdietl <wdietl@gmail.com>
2012-11-02 03:24:00 -0700

    Change test cases to directly use type annotations to allow the use from non-UW compilers, in particular, the type-annotations compiler.

--- checkers/tests/all-systems/GenericTest12.java
+++ checkers/tests/all-systems/GenericTest12.java
@@ -2,9 +2,9 @@ import checkers.nullness.quals.Nullable;
 
 // Test case from Issue 142
 abstract class GenericTest12 {
-  interface Task<V extends /*@Nullable*/ Object> {}
+  interface Task<V extends @Nullable Object> {}
 
-  abstract <V extends /*@Nullable*/ Object> Task<V> create(Runnable runnable, V result);
+  abstract <V extends @Nullable Object> Task<V> create(Runnable runnable, V result);
 
   void submit(Runnable runnable) {
     Task<Void> task = create(runnable, null);
