ec0762caaeeb8c2bec02fa779cac4a5ed50ab288
Werner Dietl <wdietl@gmail.com>
2012-11-15 16:47:26 -0800

    Resolve merge problem.

--- checkers/src/checkers/source/SourceChecker.java
+++ checkers/src/checkers/source/SourceChecker.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
