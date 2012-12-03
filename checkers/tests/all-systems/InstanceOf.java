38f11719c94d5333e28538c082c40111ce872b33
charlie garrett <charlie.garrett@gmail.com>
2012-12-02 22:05:02 -0800

    Fix bug in instanceof transfer function.  Add regression test.

--- checkers/tests/all-systems/InstanceOf.java
+++ checkers/tests/all-systems/InstanceOf.java
@@ -9,13 +9,24 @@ class GETFIELD extends FieldInstruction {}
 class PUTFIELD extends FieldInstruction {}
 
 class InstanceOf {
-    void test_two(FieldInstruction f) {
+    public void emptyGLB(FieldInstruction f) {
         if (f instanceof GETFIELD || f instanceof PUTFIELD) {
             if (f instanceof PUTFIELD) {
+                // During dataflow analysis, we can believe that f is both a
+                // GETFIELD and a PUTFIELD, which yields an empty GLB.  Once
+                // dataflow converges, it will know that f is a PUTFIELD.
                 return;
             }
             return;
         }
+    }
+
+    public boolean assignInstanceOf(Object obj) {
+        // We fixed a bug where the type in an instanceof expression
+        // like Class<?> was stored as the abstract value result of
+        // the expression.
+        boolean is_class = obj instanceof Class<?>;
+        return is_class;
     }
 }
 
