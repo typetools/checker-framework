b4f1e60c52da52468cc98bd41c4fafb16443f0c8
stefanheule <stefanheule@localhost>
2012-12-14 18:41:10 +0100

    Add missing error to test-case.

--- checkers/tests/nonnull/CopyOfArray.java
+++ checkers/tests/nonnull/CopyOfArray.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 
 public class CopyOfArray {
     protected void makeCopy(Object[] args) {
+        //:: error: (assignment.type.incompatible)
         Object[] copy = Arrays.copyOf(args, args.length);
     }
 }
