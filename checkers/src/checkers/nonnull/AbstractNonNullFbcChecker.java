70f1fd26b6e0fef5602461b801cb20e09d49e05c
stefanheule <stefanheule@localhost>
2013-01-07 17:23:25 +0100

    Add PolyAll to set of supported qualifiers for non-null type systems.

--- checkers/src/checkers/nonnull/AbstractNonNullFbcChecker.java
+++ checkers/src/checkers/nonnull/AbstractNonNullFbcChecker.java
@@ -8,12 +8,13 @@ import checkers.nonnull.quals.MonotonicNonNull;
 import checkers.nonnull.quals.NonNull;
 import checkers.nonnull.quals.Nullable;
 import checkers.nonnull.quals.PolyNull;
+import checkers.quals.PolyAll;
 import checkers.quals.TypeQualifiers;
 import checkers.source.SupportedLintOptions;
 
 @TypeQualifiers({ Nullable.class, MonotonicNonNull.class, NonNull.class,
         Free.class, Committed.class, Unclassified.class, FBCBottom.class,
-        PolyNull.class })
+        PolyNull.class, PolyAll.class })
 @SupportedLintOptions({ "strictMonotonicNonNullInit",
 // Temporary option to forbid non-null array component types,
 // which is allowed by default.
