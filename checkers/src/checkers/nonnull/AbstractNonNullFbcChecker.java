66000f5f7afc1a8dfe5619cfb0a73b158e5143b5
stefanheule <stefanheule@localhost>
2013-01-07 15:14:37 +0100

    Add PolyNull qualifier for nonnull checker.

--- checkers/src/checkers/nonnull/AbstractNonNullFbcChecker.java
+++ checkers/src/checkers/nonnull/AbstractNonNullFbcChecker.java
@@ -7,11 +7,13 @@ import checkers.initialization.quals.Unclassified;
 import checkers.nonnull.quals.MonotonicNonNull;
 import checkers.nonnull.quals.NonNull;
 import checkers.nonnull.quals.Nullable;
+import checkers.nonnull.quals.PolyNull;
 import checkers.quals.TypeQualifiers;
 import checkers.source.SupportedLintOptions;
 
 @TypeQualifiers({ Nullable.class, MonotonicNonNull.class, NonNull.class,
-        Free.class, Committed.class, Unclassified.class, FBCBottom.class })
+        Free.class, Committed.class, Unclassified.class, FBCBottom.class,
+        PolyNull.class })
 @SupportedLintOptions({ "strictMonotonicNonNullInit",
 // Temporary option to forbid non-null array component types,
 // which is allowed by default.
