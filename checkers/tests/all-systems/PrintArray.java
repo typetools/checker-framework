1f4a5b2b5735aa2155e63cac037b2b3ec53c7180
stefanheule <stefanheule@localhost>
2013-01-11 13:33:07 +0100

    Ignore a warning about localization in a general test (in all-systems).

--- checkers/tests/all-systems/PrintArray.java
+++ checkers/tests/all-systems/PrintArray.java
@@ -1,5 +1,6 @@
 
 public class PrintArray {
+    @SuppressWarnings("I18n") // the I18n checker correctly issues an error below, but we would like to keep this test in all-systems.
     public static final void print(java.io.PrintStream ps, Object[][] a) {
         if (a == null) {
             ps.println("null");
