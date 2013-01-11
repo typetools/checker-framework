9c0e0b2517319869137fed44db9226dd36cb5f57
stefanheule <stefanheule@localhost>
2013-01-11 21:17:18 +0100

    Add LockTests to AllFlowTests.

--- checkers/tests/src/tests/AllFlowTests.java
+++ checkers/tests/src/tests/AllFlowTests.java
@@ -14,8 +14,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({ AnnotationBuilderTest.class, BasicEncryptedTest.class,
         BasicSuperSubTest.class, FenumTest.class, FenumSwingTest.class,
         FlowTest.class, Flow2Test.class, FrameworkTest.class, I18nTest.class,
-        InterningTest.class, LubGlbTest.class, PolyAllTest.class,
-        PuritySuggestionsTest.class, RegexTest.class,
+        InterningTest.class, LockTest.class, LubGlbTest.class,
+        PolyAllTest.class, PuritySuggestionsTest.class, RegexTest.class,
         ReportModifiersTest.class, ReportTest.class, ReportTreeKindsTest.class,
         SignatureTest.class, TaintingTest.class, TreeParserTest.class,
         UnitsTest.class, NonNullFbcTest.class, NonNullRawnessTest.class })
