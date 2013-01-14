4c16944295ba552621758b1f18664e09e454016d
jonathangburke <jonathangburke@gmail.com>
2013-01-14 15:41:43 -0800

    Update release script/utils and release target(dist) in build.xml

--- release/release.py
+++ release/release.py
@@ -4,9 +4,11 @@
 release.py
 
 Created by Mahmood Ali on 2010-02-25.
+
 Copyright (c) 2010 Jude LLC. All rights reserved.
 """
 
+from xml.dom import minidom
 import sys
 import getopt
 import urllib2
@@ -15,355 +17,183 @@ import subprocess
 import os
 import pwd
 
-DEFAULT_SITE = "http://types.cs.washington.edu/jsr308/"
-# OPENJDK_RELEASE_SITE = 'http://download.java.net/openjdk/jdk7/'
-OPENJDK_RELEASE_SITE = 'http://jdk8.java.net/download.html'
+# See release_utils.py in the same directory as this script
+from release_utils import *
 
-def current_distribution(site=DEFAULT_SITE):
-    """
-    Returns the version of the current release of the checker framework
+#=========================================================================================
+#Global constants used in main
+SCRIPT_PATH=os.path.abspath(__file__)
 
-    """
-    ver_re = re.compile(r"<!-- checkers-version -->(.*),")
-    text = urllib2.urlopen(url=site).read()
-    result = ver_re.search(text)
-    return result.group(1)
-
-def latest_openjdk(site=OPENJDK_RELEASE_SITE):
-    ver_re = re.compile(r"Build b(\d+)")
-    text = urllib2.urlopen(url=site).read()
-    result = ver_re.search(text)
-    return result.group(1)
-
-def increment_version(version):
-    """
-    Returns a recommendation of the next incremental version based on the
-    passed one.
-
-    >>> increment_version('1.0.3')
-    '1.0.4'
-    >>> increment_version('1.0.9')
-    '1.1.0'
-    >>> increment_version('1.1.9')
-    '1.2.0'
-    >>> increment_version('1.3.1')
-    '1.3.2'
-
-    """
-    parts = [int(x) for x in version.split('.')]
-    parts[2] += 1
-    if parts[2] > 9:
-        parts[2] -= 10
-        parts[1] += 1
-    if parts[1] > 9:
-        parts[1] -= 10
-        parts[0] += 1
-    return ".".join([str(x) for x in parts])
-
-def site_copy(ant_args):
-    execute("ant -e -f release.xml %s site-copy" % ant_args)
-
-def site_copy_if_needed(ant_args):
-    if not os.path.exists(DRY_PATH):
-        return site_copy(ant_args)
-    return True
-
-def check_command(command):
-    p = execute(['which', command], halt_if_fail=False)
-    if p:
-        raise AssertionError('command not found: %s' % command)
-
-DEFAULT_PATHS = (
-#    '/homes/gws/mernst/research/invariants/scripts',
-    '/homes/gws/mernst/bin/share',
-#    '/homes/gws/mernst/bin/share-plume',
-    '/homes/gws/mernst/bin/Linux-i686',
-    '/uns/bin',
-    '/homes/gws/mali/local/share/maven/bin',
-    '.',
-)
-
-def append_to_PATH(paths=DEFAULT_PATHS):
-    current_PATH = os.getenv('PATH')
-    new_PATH = current_PATH + ':' + ':'.join(paths)
-    os.environ['PATH'] = new_PATH
-
-REPO_ROOT = os.path.dirname(os.path.dirname(__file__))
-JSR308_LANGTOOLS = os.path.join(REPO_ROOT, '..', 'jsr308-langtools')
-
-# Is PLUME_LIB even necessary?
-PLUME_LIB = os.path.join(os.getenv('HOME'), 'plume-lib')
-
-# Do not include PLUME_LIB in PROJECT_ROOTS, because this script
-# commits to all repositories in PROJECT_ROOTS.
-# TODO: WMD changed this and requires explicit commits; this
-# array is only used to update the projects.
-PROJECT_ROOTS = (
-    REPO_ROOT,                  # Checker Framework
-    JSR308_LANGTOOLS,
-)
-
-def update_projects(paths=PROJECT_ROOTS):
-    for path in paths:
-        execute('hg -R %s pull' % path)
-        execute('hg -R %s update' % path)
-        print("Checking changes")
-        # execute('hg -R %s outgoing' % path)
-
-def commit_and_push(version, path, tag_prefix):
-    execute('hg -R %s commit -m "new release %s"' % (path, version))
-    execute('hg -R %s tag %s%s' % (path, tag_prefix, version))
-    execute('hg -R %s push' % path)
-
-def ensure_group_access(path="/cse/www2/types/"):
-    # Errs for any file not owned by this user.
-    # But, the point is to set group writeability of any *new* files.
-    execute('chmod -f -R g+w %s' % path, halt_if_fail=False)
-
-def file_contains(path, text):
-    f = open(path, 'r')
-    contents = f.read()
-    f.close()
-    return text in contents
-
-def file_prepend(path, text):
-    f = open(path)
-    contents = f.read()
-    f.close()
-
-    f = open(path, 'w')
-    f.write(text)
-    f.write(contents)
-    f.close()
-
-def retrieve_changes(root, prev_version, prefix):
-    return execute(
-            "hg -R %s log -r %s%s:tip --template ' * {desc}\n'" %
-                (root, prefix, prev_version),
-                capture_output=True)
-
-EDITOR = os.getenv('EDITOR')
-if EDITOR == None:
-    raise Exception('EDITOR environment variable is not set')
-CHECKERS_CHANGELOG = os.path.join(REPO_ROOT, 'checkers', 'changelog-checkers.txt')
-
-def edit_checkers_changelog(version, changes="", path=CHECKERS_CHANGELOG):
-    edit = raw_input("Edit the Checker Framework changelog? [Y/n] ")
-    if not (edit == "n"):
-        if not file_contains(path, version):
-            import datetime
-            today = datetime.datetime.now().strftime("%d %b %Y")
-            file_prepend(path,"""Version %s, %s
-
-
-%s
-
-----------------------------------------------------------------------
-""" % (version, today, changes))
-        execute([EDITOR, path])
-
-def changelog_header_checkers(file=CHECKERS_CHANGELOG):
-    return changelog_header(file)
-
-LANGTOOLS_CHANGELOG = os.path.join(JSR308_LANGTOOLS, 'doc', 'changelog-jsr308.txt')
-
-def edit_langtools_changelog(version, changes="", path=LANGTOOLS_CHANGELOG):
-    latest_jdk = latest_openjdk()
-    print("Latest OpenJDK release is b%s" % latest_jdk)
-    edit = raw_input("Edit the JSR308 langtools changelog? [Y/n] ")
-    if not (edit == "n"):
-        if not file_contains(path, version):
-            import datetime
-            today = datetime.datetime.now().strftime("%d %b %Y")
-            file_prepend(path, """Version %s, %s
-
-Base build
-  Updated to OpenJDK langtools build b%s
-
-%s
-
-----------------------------------------------------------------------
-""" % (version, today, latest_jdk, changes))
-        execute([EDITOR, path])
-
-def changelog_header_langtools(file=LANGTOOLS_CHANGELOG):
-    return changelog_header(file)
-
-def make_release(version, ant_args, real=False, sanitycheck=True):
-    command = 'ant -e -f release.xml %s -Drelease.ver=%s %s clean web %s' % (
-        '-Drelease.is.real=true' if real else '',
-        version,
-        ant_args,
-        'sanitycheck' if sanitycheck else '',
-    )
-    print("Actually making the release")
-    return execute(command)
-
-def checklinks(site_url=None):
-    os.putenv('jsr308_www_online', site_url) # set environment var for subshell
-    return execute('make -f %s checklinks' %
-                     os.path.join(JSR308_LANGTOOLS, 'doc', 'Makefile'),
-                   halt_if_fail=False)
-
-MAVEN_GROUP_ID = 'types.checkers'
-MAVEN_REPO = 'file:///cse/www2/types/m2-repo'
-
-def mvn_deploy(name, binary, version, dest_repo=MAVEN_REPO, ):
-    command = """
-    mvn deploy:deploy-file
-        -DartifactId=%s
-        -Dfile=%s
-        -Dversion=%s
-        -Durl=%s
-        -DgroupId=%s
-        -Dpackaging=jar
-        -DgeneratePom=true
-    """ % (name, binary, version, dest_repo, MAVEN_GROUP_ID)
-    return execute(command)
-
-CHECKERS_BINARY = os.path.join(REPO_ROOT, 'checkers', 'binary', 'jsr308-all.jar')
-
-def mvn_deploy_jsr308_all(version, binary=CHECKERS_BINARY, dest_repo=MAVEN_REPO):
-    return mvn_deploy('jsr308-all', binary, version, dest_repo)
-
-
-CHECKERS_QUALS = os.path.join(REPO_ROOT, 'checkers', 'checkers-quals.jar')
-
-def mvn_deploy_quals(version, binary=CHECKERS_QUALS, dest_repo=MAVEN_REPO):
-    return mvn_deploy('checkers-quals', binary, version, dest_repo)
-
-
-def execute(command_args, halt_if_fail=True, capture_output=False):
-    print("Executing: %s" % (command_args))
-    import shlex
-    args = shlex.split(command_args) if isinstance(command_args, str) else command_args
-
-    if capture_output:
-        return subprocess.Popen(args, stdout=subprocess.PIPE).communicate()[0]
-    else:
-        r = subprocess.call(args)
-        if halt_if_fail and r:
-            raise Exception('Error %s while executing %s' % (r, command_args))
-        return r
-
-def changelog_header(filename):
-    f = open(filename, 'r')
-    header = []
-
-    for line in f:
-        if '-------' in line:
-            break
-        header.append(line)
-
-    return ''.join(header)
-
-class Usage(Exception):
-    def __init__(self, msg):
-        self.msg = msg
-
-## Define DRY_RUN_LINK
 # "USER = os.getlogin()" does not work; see http://bugs.python.org/issue584566
 # Another alternative is: USER = os.getenv('USER')
 USER = pwd.getpwuid(os.geteuid())[0]
+
+DEFAULT_SITE = "http://types.cs.washington.edu/jsr308/"
+LOCAL_PATH_TO_SITE ="/cse/www2/types/"
+
+# OPENJDK_RELEASE_SITE = 'http://download.java.net/openjdk/jdk7/'
+OPENJDK_RELEASE_SITE = 'http://jdk8.java.net/download.html'
+
+#TODO: Just build here and move, examine whether or not there are hard-coded paths
+#      then fix them because there should be no reason to build the entire thing twice
+
+#DRY_RUN_PATHS
 DRY_RUN_LINK_HTTP = "http://homes.cs.washington.edu/~%s/jsr308test/jsr308/" % USER
 DRY_PATH = os.path.join(os.environ['HOME'], 'www', 'jsr308test')
 DRY_RUN_LINK_FILE = "file://%s/jsr308/" % DRY_PATH
 DRY_RUN_LINK = DRY_RUN_LINK_HTTP
 
+EMAIL_TO='jsr308-discuss@googlegroups.com, checker-framework-discuss@googlegroups.com'
 
-TO = 'jsr308-discuss@googlegroups.com, checker-framework-discuss@googlegroups.com'
+#The script is at jsr308-release/checker-framework/release/release.py
+RELEASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(SCRIPT_PATH)))
 
-def format_email(version, checkers_header=None, langtools_header=None, to=TO):
-    if checkers_header == None:
-        checkers_header = changelog_header_checkers()
-    if langtools_header == None:
-        langtools_header = changelog_header_langtools()
+CHECKER_FRAMEWORK  = os.path.join(RELEASE_DIR, 'checker-framework')
+CHECKER_BIN_DIR    = os.path.join(CHECKER_FRAMEWORK, 'checkers', 'binary')
+CHECKERS_BINARY    = os.path.join(CHECKER_BIN_DIR, 'checkers.jar'       )
+CHECKERS_QUALS     = os.path.join(CHECKER_BIN_DIR, 'checkers-quals.jar' )
+JAVAC_BINARY       = os.path.join(CHECKER_BIN_DIR, 'javac.jar')
+JDK6_BINARY        = os.path.join(CHECKER_BIN_DIR, 'jdk6.jar' )
+JDK7_BINARY        = os.path.join(CHECKER_BIN_DIR, 'jdk7.jar' )
 
-    template = """
-=================== BEGINING OF EMAIL =====================
+CHECKERS_BINARY_POM    = os.path.join(CHECKER_BIN_DIR, 'poms', 'checkersPom.xml'      )
+CHECKERS_QUALS_POM     = os.path.join(CHECKER_BIN_DIR, 'poms', 'checkersQualsPom.xml' )
+JAVAC_BINARY_POM       = os.path.join(CHECKER_BIN_DIR, 'poms', 'compilerPom.xml'      )
+JDK6_BINARY_POM        = os.path.join(CHECKER_BIN_DIR, 'poms', 'jdk6Pom.xml' )
+JDK7_BINARY_POM        = os.path.join(CHECKER_BIN_DIR, 'poms', 'jdk7Pom.xml' )
 
-To:  %s
-Subject: Release %s of the Checker Framework and Type Annotations compiler
+CHECKERS_CHANGELOG = os.path.join(CHECKER_FRAMEWORK, 'checkers', 'changelog-checkers.txt')
 
-We have released a new version of the Type Annotations (JSR 308) compiler,
-the Checker Framework, and the Eclipse plugin for the Checker Framework.
- * The Type Annotations compiler supports the type annotation syntax that is
-   planned for a future version of the Java language.
- * The Checker Framework lets you create and/or run pluggable type-checkers,
-   in order to detect and prevent bugs in your code.  
- * The Eclipse plugin makes it more convenient to run the Checker Framework.
+JSR308_LANGTOOLS    = os.path.join(RELEASE_DIR, 'jsr308-langtools')
+JSR308_CHANGELOG    = os.path.join(JSR308_LANGTOOLS, 'doc', 'changelog-jsr308.txt')
+JSR308_DOC_MAKEFILE = os.path.join(JSR308_LANGTOOLS, 'doc', 'Makefile')
 
-You can find documentation and download links for these projects at:
-http://types.cs.washington.edu/jsr308/
+PLUME_LIB = os.path.join(RELEASE_DIR, 'plume-lib')
 
-Notable changes include:
-[[ FILL ME HERE ]]
+MAVEN_PLUGIN_DIR = os.path.join(CHECKER_FRAMEWORK, 'maven-plugin')
+MAVEN_PLUGIN_POM = os.path.join(MAVEN_PLUGIN_DIR,  'pom.xml')
+MAVEN_REPO = 'file:///cse/www2/types/m2-repo'
 
-Changes for the Checker Framework
-%s
-Changes for the Type Annotations Compiler
-%s
+#=========================================================================================
+#Set environment variables needed to find specific tools
+#These are paths hardcoded for buffalo.cs.washington.edu
+#If you are running this on a machine besides buffalo then either
+#overwrite these or comment them out and set these from your shell
 
-=================== END OF EMAIL ==========================
-    """ % (to, version, checkers_header, langtools_header,)
-    return template
+#TODO: NEED TO ADD THESE BEFORE MAKING AFU and JSR308, i.e. add making those two to this script
+os.environ['PLUME_LIB'] =  PLUME_LIB
+os.environ['TEXINPUTS'] =  '.:/scratch/secs-jenkins/tools/hevea-1.10/lib/hevea:/usr/share/texmf/tex/latex/hevea/:/homes/gws/mernst/tex/sty:/homes/gws/mernst/tex:..:'
+os.environ['PERLLIB']   =  getAndAppend('PERLLIB', ":")  + "/homes/gws/mernst/bin/src/perl/lib/perl5/site_perl/5.10.0:/homes/gws/jburke/perl_lib"
+os.environ['PERL5LIB']  =  getAndAppend('PERL5LIB', ":") + "/homes/gws/mernst/bin/src/perl/lib/perl5/site_perl/5.10.0:/homes/gws/jburke/perl_lib"
+os.environ['JAVA_HOME'] =  '/scratch/secs-jenkins/java/jdk1.6.0'
+os.environ['JAVA_6_HOME'] =  '/scratch/secs-jenkins/java/jdk1.6.0'
+os.environ['JAVA_7_HOME'] =  '/scratch/secs-jenkins/java/jdk1.7.0'
 
+EDITOR = os.getenv('EDITOR')
+if EDITOR == None:
+    EDITOR = 'emacs'
 
+PATH = os.environ['JAVA_HOME'] + "/projects/uns/F11/bin/"
+PATH = PATH + ":/scratch/secs-jenkins/tools/hevea-1.10/bin/:/projects/uns/F11/bin/"
+PATH = PATH + ":" + PLUME_LIB + "/bin:/homes/gws/mernst/bin/share"
+PATH = PATH + ":/homes/gws/mernst/bin/Linux-i686:/uns/bin:/homes/gws/mali/local/share/maven/bin:."
+os.environ['PATH'] = PATH + ":" + os.environ['PATH']
 
+#Tools that must be on your PATH ( besides common *nix ones like grep )
+TOOLS = [ 'hevea', 'perl', 'java', 'dia', 'latex', 'mvn', 'hg', EDITOR ]
+
+#Consult release_utils.py for method definitions for the steps below
 def main(argv):
-    append_to_PATH()
+    print("PATH " + os.environ['PATH'])
     print("Making a new release of the Checker Framework!")
 
+    print("\nChecking to make sure the following programs are installed:")
+    print(', '.join(TOOLS))
+    print('Note: If you are NOT working on buffalo.cs.washington.edu then you ' +
+          'likely need to change the variables that are set in release.py \n'   +
+          'search for "Set environment variables"')
+    map(check_command, TOOLS)
+
+    #TODO Add check of whether or not there are any differences in the dev folder
+    #TODO If there are, display them and ask if the user wishes to continue
+
+    print("\nUpdating Checker Framework and JSR308 Langtools")
+    # Update repositories
+    update_projects( ( CHECKER_FRAMEWORK, JSR308_LANGTOOLS ) )
+
+    #Ensure the 3 versions of PluginUtils.java are all the same except for the package definition
+    print("\nChecking if PluginUtil.java files are synchronized")
+    checkPluginScript = os.path.join(CHECKER_FRAMEWORK, "release", "checkPluginUtil.sh");
+    execute('sh %s' % (checkPluginScript));
+
     # Infer version
-    curr_version = current_distribution()
-    print("Current release is %s" % curr_version)
+    curr_version = current_distribution(DEFAULT_SITE)
+
+    print("\nCurrent release is %s" % curr_version)
     suggested_version = increment_version(curr_version)
     next_version = raw_input("Suggested next release: (%s): " % suggested_version)
     if not next_version:
         next_version = suggested_version
     print next_version
 
-    # Update repositories
-    update_projects()
+    #update the version of all Maven pom files including the maven plugin
+    print("\nUpdating Maven pom files")
+    updateVersionScript = os.path.join(CHECKER_FRAMEWORK, "checkers", "binary", "poms", "updateAllVersions.sh")
+    execute('sh %s %s' % (updateVersionScript, next_version))
 
-    checkers_changes = retrieve_changes(REPO_ROOT, curr_version, "checkers-")
-    edit_checkers_changelog(version=next_version,changes=checkers_changes)
+    # Retrieve and update changes to the changelogs
+    checkers_changes = retrieve_changes(CHECKER_FRAMEWORK, curr_version, "checkers-")
+    edit_checkers_changelog(next_version, CHECKERS_CHANGELOG, EDITOR, changes=checkers_changes)
+
     langtools_changes = retrieve_changes(JSR308_LANGTOOLS, curr_version, "jsr308-")
-    edit_langtools_changelog(version=next_version,changes=langtools_changes)
+    edit_langtools_changelog(next_version, OPENJDK_RELEASE_SITE, JSR308_CHANGELOG, EDITOR, changes=langtools_changes)
 
     # Making the first release
     ant_args = ""
     for arg in argv[1:]:      # everything but the first element
         ant_args = ant_args + " '" + arg + "'"
-    site_copy_if_needed(ant_args)
+    if not os.path.exists(DRY_PATH):
+        site_copy(ant_args)
     make_release(next_version, ant_args)
-    checklinks(DRY_RUN_LINK)
+    checklinks(JSR308_DOC_MAKEFILE, DRY_RUN_LINK)
 
     print("Pushed to %s" % DRY_RUN_LINK)
     raw_input("Please check the site.  Press ENTER to continue.")
     print("\n\n\n\n\n")
 
     # Making the real release
-    make_release(next_version, ant_args, real=True)
+    make_release(next_version, ant_args, real=False) #TODO: TURN THIS BACK TO TRUE
 
-    # Make Maven release
-    mvn_deploy_jsr308_all(next_version)
-    mvn_deploy_quals(next_version)
+    # Build and then deploy the maven plugin
+    mvn_install(MAVEN_PLUGIN_DIR)
+    mvn_deploy_mvn_plugin(MAVEN_PLUGIN_DIR, MAVEN_PLUGIN_POM, next_version, MAVEN_REPO)
 
-    checklinks(DEFAULT_SITE)
+    # Deploy jsr308 and checker-qual jars to maven repo
+    mvn_deploy( CHECKERS_BINARY, CHECKERS_BINARY_POM, MAVEN_REPO )
+    mvn_deploy( CHECKERS_QUALS,  CHECKERS_QUALS_POM,  MAVEN_REPO )
+    mvn_deploy( JAVAC_BINARY,    JAVAC_BINARY_POM,    MAVEN_REPO )
+    mvn_deploy( JDK6_BINARY,     JDK6_BINARY_POM,     MAVEN_REPO )
+    mvn_deploy( JDK6_BINARY,     JDK7_BINARY_POM,     MAVEN_REPO )
+
+    checklinks(JSR308_DOC_MAKEFILE, DEFAULT_SITE)
 
     print("Pushed to %s" % DEFAULT_SITE)
     raw_input("Please check the site.  DONE?  Press ENTER to continue.")
     print("\n\n\n\n\n")
 
-    commit_and_push(next_version, REPO_ROOT, "checkers-")
-    commit_and_push(next_version, JSR308_LANGTOOLS, "jsr308-")
+    #TODO: Remind the user they must build and deploy the eclipse plugin
 
-    ensure_group_access()
+    #Push the final state (after any changes made during the release) to the repositories
+    #Adding an identifying tag for this release
+    commit_tag_and_push(next_version, CHECKER_FRAMEWORK, "checkers-")    TODO: TEMPORARILY AVOID TAGGING
+    commit_tag_and_push(next_version, JSR308_LANGTOOLS, "jsr308-")
+
+    #Add group write access to all files in the website to ensure they are served
+    ensure_group_access( LOCAL_PATH_TO_SITE )
 
     print("You have just made the release.  Please announce it to the world")
     print("Here is an email template:")
-    print format_email(next_version)
+    print format_email(next_version, EMAIL_TO, JSR308_CHANGELOG, CHECKERS_CHANGELOG)
 
 # The entry point to the Python script.
 if __name__ == "__main__":
