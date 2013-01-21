79e53602921210e0fae113e144d986040ee1c85f
stefanheule <stefanheule@localhost>
2013-01-21 19:00:40 +0100

    Define constant for 'strictmonotonicinit' command line option.

--- checkers/src/checkers/nonnull/AbstractNonNullRawnessChecker.java
+++ checkers/src/checkers/nonnull/AbstractNonNullRawnessChecker.java
@@ -11,13 +11,15 @@ import checkers.quals.TypeQualifiers;
 import checkers.source.SupportedLintOptions;
 
 @TypeQualifiers({ Nullable.class, MonotonicNonNull.class, NonNull.class,
-    NonRaw.class, Raw.class, PolyNull.class, PolyAll.class })
-@SupportedLintOptions({ "strictMonotonicNonNullInit",
- // Temporary option to forbid non-null array component types,
- // which is allowed by default.
- // Forbidding is sound and will eventually be the only possibility.
- // Allowing is unsound but permitted until flow-sensitivity changes are made.
-         "arrays:forbidnonnullcomponents" })
+        NonRaw.class, Raw.class, PolyNull.class, PolyAll.class })
+@SupportedLintOptions({ AbstractNonNullChecker.LINT_STRICTMONOTONICNONNULLINIT,
+        AbstractNonNullChecker.LINT_STRICTNULLCOMPARISON,
+        // Temporary option to forbid non-null array component types,
+        // which is allowed by default.
+        // Forbidding is sound and will eventually be the only possibility.
+        // Allowing is unsound but permitted until flow-sensitivity changes are
+        // made.
+        "arrays:forbidnonnullcomponents" })
 public class AbstractNonNullRawnessChecker extends AbstractNonNullChecker {
 
     public AbstractNonNullRawnessChecker() {
