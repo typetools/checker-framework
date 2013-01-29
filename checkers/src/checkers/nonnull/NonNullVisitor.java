486b389162d2d5c4de79eda15efabb03065ba2f6
stefanheule <stefanheule@localhost>
2013-01-30 00:28:20 +0100

    Use simpler code to check annotations on new expressions in the non-null type system (this was changed some time ago, and is now changed back).

--- checkers/src/checkers/nonnull/NonNullVisitor.java
+++ checkers/src/checkers/nonnull/NonNullVisitor.java
@@ -28,7 +28,6 @@ import checkers.types.AnnotatedTypeMirror.AnnotatedExecutableType;
 import checkers.types.AnnotatedTypeMirror.AnnotatedPrimitiveType;
 import checkers.util.QualifierPolymorphism;
 
-import com.sun.source.tree.AnnotatedTypeTree;
 import com.sun.source.tree.ArrayAccessTree;
 import com.sun.source.tree.AssertTree;
 import com.sun.source.tree.BinaryTree;
@@ -532,30 +531,12 @@ public class NonNullVisitor
     @Override
     public Void visitNewClass(NewClassTree node, Void p) {
         AnnotatedDeclaredType type = atypeFactory.getAnnotatedType(node);
-        ExpressionTree identifier = node.getIdentifier();
-        if (identifier instanceof AnnotatedTypeTree) {
-            AnnotatedTypeTree t = (AnnotatedTypeTree) identifier;
-            for (AnnotationMirror a : atypeFactory.getAnnotatedType(t)
-                    .getAnnotations()) {
-                // is this an annotation of the nonnull checker?
-                boolean nonnullCheckerAnno = AnnotationUtils
-                        .containsSameIgnoringValues(
-                                checker.getNonNullAnnotations(), a);
-                if (nonnullCheckerAnno && !AnnotationUtils.areSame(NONNULL, a)) {
-                    // The type is not non-null => warning
-                    checker.report(
-                            Result.warning("new.class.type.invalid",
-                                    type.getAnnotations()), node);
-                    // Note that other consistency checks are made by isValid.
-                }
-            }
-            if (t.toString().contains("@PolyNull")) {
-                // TODO: this is a hack, but PolyNull gets substituted
-                // afterwards
-                checker.report(
-                        Result.warning("new.class.type.invalid",
-                                type.getAnnotations()), node);
-            }
+        if (!type.hasAnnotation(NONNULL)) {
+            // The type is not non-null => error
+            checker.report(
+                    Result.warning("new.class.type.invalid",
+                            type.getAnnotations()), node);
+            // Note that other consistency checks are made by isValid.
         }
         // TODO: It might be nicer to introduce a framework-level
         // isValidNewClassType or some such.
