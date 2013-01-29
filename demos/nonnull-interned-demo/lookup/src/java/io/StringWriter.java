a03f42f23d6b0d69c45f99df7a982b892047ffae
Werner Dietl <wdietl@gmail.com>
2013-01-28 16:25:47 -0800

    Adapt demos to changed fully-qualified types syntax, mostly by removing all these superfluous fully-qualified types.

--- demos/nonnull-interned-demo/lookup/src/java/io/StringWriter.java
+++ demos/nonnull-interned-demo/lookup/src/java/io/StringWriter.java
@@ -7,13 +7,13 @@ public class StringWriter extends Writer {
   public StringWriter(int a1) { throw new RuntimeException("skeleton method"); }
   public void write(int a1) { throw new RuntimeException("skeleton method"); }
   public void write(@NonNull char[] a1, int a2, int a3) { throw new RuntimeException("skeleton method"); }
-  public void write(@NonNull java.lang.String a1) { throw new RuntimeException("skeleton method"); }
-  public void write(@NonNull java.lang.String a1, int a2, int a3) { throw new RuntimeException("skeleton method"); }
-  public @NonNull java.io.StringWriter append(@Nullable java.lang.CharSequence a1) { throw new RuntimeException("skeleton method"); }
-  public @NonNull java.io.StringWriter append(@Nullable java.lang.CharSequence a1, int a2, int a3) { throw new RuntimeException("skeleton method"); }
-  public @NonNull java.io.StringWriter append(char a1) { throw new RuntimeException("skeleton method"); }
-  public @NonNull java.lang.String toString() { throw new RuntimeException("skeleton method"); }
-  public @NonNull java.lang.StringBuffer getBuffer() { throw new RuntimeException("skeleton method"); }
+  public void write(@NonNull String a1) { throw new RuntimeException("skeleton method"); }
+  public void write(@NonNull String a1, int a2, int a3) { throw new RuntimeException("skeleton method"); }
+  public @NonNull StringWriter append(@Nullable CharSequence a1) { throw new RuntimeException("skeleton method"); }
+  public @NonNull StringWriter append(@Nullable CharSequence a1, int a2, int a3) { throw new RuntimeException("skeleton method"); }
+  public @NonNull StringWriter append(char a1) { throw new RuntimeException("skeleton method"); }
+  public @NonNull String toString() { throw new RuntimeException("skeleton method"); }
+  public @NonNull StringBuffer getBuffer() { throw new RuntimeException("skeleton method"); }
   public void flush() { throw new RuntimeException("skeleton method"); }
   public void close() throws java.io.IOException { throw new RuntimeException("skeleton method"); }
 }
