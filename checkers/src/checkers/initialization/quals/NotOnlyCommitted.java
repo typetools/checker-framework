03ae039974dd029ee8565a956c3663763394ad9d
stefanheule <stefanheule@localhost>
2013-02-24 17:51:37 +0100

    Documentation (javadoc) for NotOnlyCommitted.

--- checkers/src/checkers/initialization/quals/NotOnlyCommitted.java
+++ checkers/src/checkers/initialization/quals/NotOnlyCommitted.java
@@ -6,6 +6,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * A declaration annotation for fields that indicates that the values the given
+ * field can store might not be {@link Committed} (but {@link Free} or
+ * {@link Unclassified} instead). This is necessary to make allow circular
+ * initialization as supported by FBC.
+ *
+ * @author Stefan Heule
+ */
 @Documented
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
