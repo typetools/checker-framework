d6528d69ea01e334292f1746fd6a3c31af983301
charlie garrett <charlie.garrett@gmail.com>
2013-03-05 22:20:37 -0800

    Intern the names of annotation mirrors and classes

--- javacutils/src/javacutils/BasicAnnotationProvider.java
+++ javacutils/src/javacutils/BasicAnnotationProvider.java
@@ -12,13 +12,12 @@ public class BasicAnnotationProvider implements AnnotationProvider {
     @Override
     public AnnotationMirror getDeclAnnotation(Element elt,
             Class<? extends Annotation> anno) {
-        String annoName = anno.getCanonicalName();
         List<? extends AnnotationMirror> annotationMirrors = elt
                 .getAnnotationMirrors();
 
         // Then look at the real annotations.
         for (AnnotationMirror am : annotationMirrors) {
-            if (AnnotationUtils.areSameByName(am, annoName)) {
+            if (AnnotationUtils.areSameByClass(am, anno)) {
                 return am;
             }
         }
