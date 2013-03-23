43e5d469ca8418ebb468ffab5d74abf164e02f2b
stefanheule <stefanheule@localhost>
2013-03-23 16:36:24 -0700

    Delete nullness tests.

--- checkers/tests/nullness/BadCast1.java
+++
@@ -1,6 +0,0 @@
-class BadCast1 {
-  public void m() {
-    //:: error: illegal start of type :: error: not a statement
-    (@NonNull) "";
-  }
-}
--- checkers/tests/nullness/generics/SuperRawness.java
+++
@@ -1,7 +0,0 @@
-import java.util.Set;
-import java.util.Arrays;
-
-class SuperRawness {
-  static <X> void test(Set<X>... args) { test2(Arrays.asList(args)); }
-  static <X> void test2(Iterable<Set<X>> args) {}
-}
