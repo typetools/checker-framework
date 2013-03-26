676e9fda1c32872013b04af71ca62e01ac692e7f
jonathangburke <jonathangburke@gmail.com>
2013-03-26 15:06:50 -0700

    Temporarily suppress error message for reported bug: issue 212.

--- checkers/tests/igj/Constructors.java
+++ checkers/tests/igj/Constructors.java
@@ -7,12 +7,7 @@ public class Constructors {
         field = 0;
     }
 
-    // TODO: the annotation on the constructor wasn't previously
-    // necessary. However, without it the constructor instantiation
-    // below fails. Should a receiver annotation be automatically
-    // propagated?
-    // TODO JSR 308: is this receiver annotation even still valid?
-    @Immutable 
+    @Immutable
     Constructors(int a) {
         field = 0;
     }
@@ -21,6 +16,8 @@ public class Constructors {
         Constructors mutable = new Constructors();
         mutable.field = 0;
 
+        //TODO: This is a bug see issue
+        //:: error: (constructor.invocation.invalid)
         Constructors immutable = new Constructors(4);
         //:: error: (assignability.invalid)
         immutable.field = 4;
