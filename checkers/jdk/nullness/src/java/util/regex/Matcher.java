718b70e60d76fc5098a3237e084340e50b6e9bf4
Michael Ernst <mernst@cs.washington.edu>
2013-04-13 17:20:12 -0700

    @Pure annotations in java.util.regex.Matcher

--- checkers/jdk/nullness/src/java/util/regex/Matcher.java
+++ checkers/jdk/nullness/src/java/util/regex/Matcher.java
@@ -9,12 +9,19 @@ public final class Matcher implements MatchResult {
   public Matcher usePattern(Pattern a1) { throw new RuntimeException("skeleton method"); }
   public Matcher reset() { throw new RuntimeException("skeleton method"); }
   public Matcher reset(CharSequence a1) { throw new RuntimeException("skeleton method"); }
+  @Pure
   public int start() { throw new RuntimeException("skeleton method"); }
+  @Pure
   public int start(int a1) { throw new RuntimeException("skeleton method"); }
+  @Pure
   public int end() { throw new RuntimeException("skeleton method"); }
+  @Pure
   public int end(int a1) { throw new RuntimeException("skeleton method"); }
+  @SideEffectFree
   public String group() { throw new RuntimeException("skeleton method"); }
+  @SideEffectFree
   public @Nullable String group(int a1) { throw new RuntimeException("skeleton method"); }
+  @Pure
   public int groupCount() { throw new RuntimeException("skeleton method"); }
   public boolean matches() { throw new RuntimeException("skeleton method"); }
   public boolean find() { throw new RuntimeException("skeleton method"); }
@@ -26,13 +33,19 @@ public final class Matcher implements MatchResult {
   public String replaceAll(String a1) { throw new RuntimeException("skeleton method"); }
   public String replaceFirst(String a1) { throw new RuntimeException("skeleton method"); }
   public Matcher region(int a1, int a2) { throw new RuntimeException("skeleton method"); }
+  @Pure
   public int regionStart() { throw new RuntimeException("skeleton method"); }
+  @Pure
   public int regionEnd() { throw new RuntimeException("skeleton method"); }
+  @Pure
   public boolean hasTransparentBounds() { throw new RuntimeException("skeleton method"); }
   public Matcher useTransparentBounds(boolean a1) { throw new RuntimeException("skeleton method"); }
+  @Pure
   public boolean hasAnchoringBounds() { throw new RuntimeException("skeleton method"); }
   public Matcher useAnchoringBounds(boolean a1) { throw new RuntimeException("skeleton method"); }
   public String toString() { throw new RuntimeException("skeleton method"); }
+  @Pure
   public boolean hitEnd() { throw new RuntimeException("skeleton method"); }
+  @Pure
   public boolean requireEnd() { throw new RuntimeException("skeleton method"); }
 }
