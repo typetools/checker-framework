d3388012438f29241e0f6ccdd7e90e4889d0a1e1
Michael Ernst <mernst@cs.washington.edu>
2013-04-23 07:41:19 -0700

    Add @SideEffectFree annotations

--- checkers/jdk/nullness/src/java/lang/Throwable.java
+++ checkers/jdk/nullness/src/java/lang/Throwable.java
@@ -1,6 +1,7 @@
 package java.lang;
 
 import dataflow.quals.Pure;
+import dataflow.quals.SideEffectFree;
 
 import checkers.nullness.quals.Nullable;
 import checkers.nullness.quals.Raw;
@@ -8,19 +9,19 @@ import checkers.nullness.quals.Raw;
 
 public class Throwable implements java.io.Serializable{
     private static final long serialVersionUID = 0L;
-  public Throwable() { throw new RuntimeException("skeleton method"); }
-  public Throwable(@Nullable String a1) { throw new RuntimeException("skeleton method"); }
-  public Throwable(@Nullable String a1, @Nullable Throwable a2) { throw new RuntimeException("skeleton method"); }
-  public Throwable(@Nullable Throwable a1) { throw new RuntimeException("skeleton method"); }
+  public @SideEffectFree Throwable() { throw new RuntimeException("skeleton method"); }
+  public @SideEffectFree Throwable(@Nullable String a1) { throw new RuntimeException("skeleton method"); }
+  public @SideEffectFree Throwable(@Nullable String a1, @Nullable Throwable a2) { throw new RuntimeException("skeleton method"); }
+  public @SideEffectFree Throwable(@Nullable Throwable a1) { throw new RuntimeException("skeleton method"); }
   public @Pure @Nullable String getMessage() { throw new RuntimeException("skeleton method"); }
   public @Pure @Nullable String getLocalizedMessage() { throw new RuntimeException("skeleton method"); }
-  public @Nullable Throwable getCause() { throw new RuntimeException("skeleton method"); }
+  public @Pure @Nullable Throwable getCause() { throw new RuntimeException("skeleton method"); }
   // The signature of initCause should use @PolyRaw as follows, but implementing
   // @PolyRaw is a fair amount of work, so don't bother to do so yet.  See
   // https://code.google.com/p/checker-framework/issues/detail?id=216 .
   // public synchronized @PolyRaw Throwable initCause(@PolyRaw Throwable this, @Nullable Throwable a1) { throw new RuntimeException("skeleton method"); }
   public synchronized @Raw Throwable initCause(@Raw Throwable this, @Nullable Throwable a1) { throw new RuntimeException("skeleton method"); }
-  public String toString() { throw new RuntimeException("skeleton method"); }
+  public @SideEffectFree String toString() { throw new RuntimeException("skeleton method"); }
   public void printStackTrace() { throw new RuntimeException("skeleton method"); }
   public void printStackTrace(java.io.PrintStream a1) { throw new RuntimeException("skeleton method"); }
   public void printStackTrace(java.io.PrintWriter a1) { throw new RuntimeException("skeleton method"); }
