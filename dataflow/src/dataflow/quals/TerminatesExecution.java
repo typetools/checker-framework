006e064f8be37f243cf3e699b4c48e206fcf435a
Michael Ernst <mernst@cs.washington.edu>
2013-04-25 13:34:16 -0700

    Make @SideEffectFree and @TerminatesExecution applicable to constructors.

--- dataflow/src/dataflow/quals/TerminatesExecution.java
+++ dataflow/src/dataflow/quals/TerminatesExecution.java
@@ -20,6 +20,6 @@ import java.lang.annotation.Target;
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+@Target({ ElementType.METHOD, ElementType.CONSTRUCTOR })
 public @interface TerminatesExecution {
 }
