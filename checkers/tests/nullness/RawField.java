21f5b36d013ced8d5548c43558ed41167b41ecd7
Michael Ernst <mernst@cs.washington.edu>
2013-05-09 10:27:37 -0700

    Add apparent bug:  FBC initialization checks are too strict.

--- checkers/tests/nullness/RawField.java
+++ checkers/tests/nullness/RawField.java
@@ -27,3 +27,29 @@ class RawField {
         this.a = a;
     }
 }
+
+
+
+class Options {
+  
+    @UnknownInitialization @Raw Object arg;
+
+    public Options (@UnknownInitialization @Raw Object arg) {
+        this.arg = arg;
+    }
+
+    public void parse_or_usage () {
+        // use arg only under the assumption that it is @UnknownInitialization
+    }
+
+}
+
+
+class MultiVersionControl {
+
+  public void parseArgs(@UnknownInitialization @Raw MultiVersionControl this) {
+    Options options = new Options (this);
+    options.parse_or_usage ();
+  }
+
+}
