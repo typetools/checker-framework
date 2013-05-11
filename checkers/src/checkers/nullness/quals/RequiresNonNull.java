1aaf8f2f8ede2c9c4a641516ec5d9f7d18252b61
Michael Ernst <mernst@cs.washington.edu>
2013-05-11 13:08:04 -0700

    Copy documentation from old names to new ones

--- checkers/src/checkers/nullness/quals/RequiresNonNull.java
+++ checkers/src/checkers/nullness/quals/RequiresNonNull.java
@@ -9,10 +9,28 @@ import java.lang.annotation.Target;
 import checkers.quals.PreconditionAnnotation;
 
 /**
- * A precondition annotation to indicate that a method requires certain
- * expressions to be {@link NonNull}.
+ * Indicates a method precondition:  the method expects the specified
+ * expressions to be non-null when the annotated method is invoked.
  *
- * @author Stefan Heule
+ * <p>For example:
+ * <!-- The "&nbsp;" is to hide the at-signs from Javadoc. -->
+ * <pre>
+ * &nbsp;@Nullable Object field1;
+ * &nbsp;@Nullable Object field2;
+ *
+ * &nbsp;@NonNullOnEntry("field1")
+ *  void method1() {
+ *    field1.toString();        // OK, field1 is known to be non-null
+ *    field2.toString();        // error, might throw NullPointerException
+ *  }
+ *
+ *  void method2() {
+ *    field1 = new Object();
+ *    method1();                // OK, satisfies method precondition
+ *    field1 = null;
+ *    method1();                // error, does not satisfy method precondition
+ *  }
+ * </pre>
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
