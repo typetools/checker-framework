c8ab95d7b394fe435fcf610602673d61521a826c
Suzanne Millstein <smillst@cs.washington.edu>
2013-06-04 14:45:44 -0700

    Fix white space and comments

--- checkers/src/checkers/quals/FromByteCode.java
+++ checkers/src/checkers/quals/FromByteCode.java
@@ -11,8 +11,8 @@ import java.lang.annotation.Target;
      *If a method is annotated with this declaration annotation, 
      *then its signature is not written in a stub file and the method 
      *is not declared in source.  This annotation is added in AnnotatedTypeFactory. 
-     *If a method does not have a Tree then it is FromBinary. (If a method is 
-     *already annotated with @FromStubFile, this annotation is not added.)  
+     *(If a method is already annotated with @FromStubFile, 
+     *this annotation is not added.)  
      */
     @Retention(RetentionPolicy.RUNTIME)
     @Target({ElementType.METHOD, ElementType.CONSTRUCTOR,
