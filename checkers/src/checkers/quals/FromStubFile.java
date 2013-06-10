d265d804d5a70cd3a93ea28b79e0764c894bfe0f
Suzanne Millstein <smillst@cs.washington.edu>
2013-06-10 09:24:00 -0700

    Fixed formatting

--- checkers/src/checkers/quals/FromStubFile.java
+++ checkers/src/checkers/quals/FromStubFile.java
@@ -5,14 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
-
-    /**
-     * If a method is annotated with this declaration annotation, then 
-     * its signature was read from a stub file.  It is added by the StubParser 
-     * and is not visible to the end user.  
-     */
-    @Retention(RetentionPolicy.RUNTIME)
-    @Target({ElementType.METHOD, ElementType.CONSTRUCTOR,
-        ElementType.TYPE, ElementType.PACKAGE})
-    public @interface FromStubFile {}
+/**
+ * If a method is annotated with this declaration annotation, then its signature
+ * was read from a stub file. It is added by the StubParser and is not visible
+ * to the end user.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.TYPE,
+        ElementType.PACKAGE })
+public @interface FromStubFile {
+}
