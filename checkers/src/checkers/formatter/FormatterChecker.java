3a86e8d3729ca08a6af7c1e4993d0dbe1443e528
Konstantin Weitz <weitzkon@cs.uw.edu>
2013-06-15 17:10:36 -0700

    Introduced Format String Checker

---
+++ checkers/src/checkers/formatter/FormatterChecker.java
@@ -0,0 +1,32 @@
+package checkers.formatter;
+
+import checkers.basetype.BaseTypeChecker;
+import checkers.formatter.quals.Format;
+import checkers.formatter.quals.FormatBottom;
+import checkers.formatter.quals.InvalidFormat;
+import checkers.quals.TypeQualifiers;
+import checkers.quals.Unqualified;
+import checkers.types.QualifierHierarchy;
+import checkers.util.MultiGraphQualifierHierarchy.MultiGraphFactory;
+
+/**
+ * A type-checker plug-in for the {@link Format} qualifier that finds
+ * syntactically invalid formatter calls.
+ * 
+ * @author Konstantin Weitz
+ */
+@TypeQualifiers({ Unqualified.class, Format.class, FormatBottom.class, InvalidFormat.class })
+public class FormatterChecker extends BaseTypeChecker {
+    public FormatterTreeUtil treeUtil;
+
+    @Override
+    public void initChecker() {
+        super.initChecker();
+        this.treeUtil = new FormatterTreeUtil(this, processingEnv);
+    }
+
+    @Override
+    public QualifierHierarchy createQualifierHierarchy(MultiGraphFactory factory) {
+        return new FormatterQualifierHierarchy(this, processingEnv, factory);
+    }
+}
