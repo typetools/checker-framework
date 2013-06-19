39bf19d3549dc3027df9942e68b49b94090f11c9
Michael Ernst <mernst@cs.washington.edu>
2013-06-19 09:55:02 -0700

    In documentation, @AssertNonNullIfTrue(...) => @EnsuresNonNullIf(..., result=true)

--- checkers/src/checkers/nullness/quals/EnsuresNonNullIf.java
+++ checkers/src/checkers/nullness/quals/EnsuresNonNullIf.java
@@ -18,7 +18,7 @@ import checkers.quals.ConditionalPostconditionAnnotation;
  *
  * <b>Method parameters:</b>
  * A common example is that the <tt>equals</tt> method is annotated as follows:
- * <pre><code>   @AssertNonNullIfTrue("#1")
+ * <pre><code>   @EnsuresNonNullIf(expression="#1", result=true)
  *   public boolean equals(@Nullable Object obj) { ... }</code></pre>
  * because, if <tt>equals</tt> returns true, then the first (#0) argument to
  * <tt>equals</tt> was not null.
@@ -26,32 +26,31 @@ import checkers.quals.ConditionalPostconditionAnnotation;
  *
  * <b>Fields:</b>
  * The value expressions can refer to fields, even private ones.  For example:
- * <pre><code>   @AssertNonNullIfTrue("this.derived")
+ * <pre><code>   @EnsuresNonNullIf(expression="this.derived", result=true)
  *   public boolean isDerived() {
  *     return (this.derived != null);
  *   }</code></pre>
  * As another example, an <tt>Iterator</tt> may cache the next value that
  * will be returned, in which case its <tt>hasNext</tt> method could be
  * annotated as:
- * <pre><code>   @AssertNonNullIfTrue("next_cache")
+ * <pre><code>   @EnsuresNonNullIf(expression="next_cache", result=true)
  *   public boolean hasNext() {
  *     if (next_cache == null) return false;
  *     ...
  *   }</code></pre>
- * An <tt>AssertNonNullIfTrue</tt> annotation that refers to a private field is
+ * An <tt>EnsuresNonNullIf</tt> annotation that refers to a private field is
  * useful for verifying that client code performs needed checks in the right
  * order, even if the client code cannot directly affect the field.
  * <p>
  *
  * <b>Method calls:</b>
- * If {@link File#isDirectory()} returns true, then {@link File#list()} returns
- * non-null, and {@link File#listFiles()} returns non-null.  You
- * can express this relationship as:
- * <pre><code>   @AssertNonNullIfTrue({"list()","listFiles()"})
- *   public boolean isDirectory() { ... }</code></pre>
+ * If {@link Class#isArray()} returns true, then {@link Class#getComponentType()}
+ * returns non-null.  You can express this relationship as:
+ * <pre><code>   @EnsuresNonNullIf(expression="getComponentType()", result=true)
+ *   public native @Pure boolean isArray();</code></pre>
  *
  * @see NonNull
- * @see AssertNonNullIfFalse
+ * @see EnsuresNonNull
  * @see NullnessChecker
  * @checker.framework.manual #nullness-checker Nullness Checker
  */
