def54c96e12106e7754c7f1eaa90e9db2e29b583
Werner Dietl <wdietl@gmail.com>
2013-06-23 11:00:50 -0700

    Fix the supertype of NonRaw to be Raw, not UnknownInitialization.

--- checkers/src/checkers/nullness/quals/NonRaw.java
+++ checkers/src/checkers/nullness/quals/NonRaw.java
@@ -6,7 +6,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import checkers.initialization.quals.UnknownInitialization;
 import checkers.nullness.NullnessRawnessChecker;
 import checkers.quals.DefaultQualifierInHierarchy;
 import checkers.quals.SubtypeOf;
@@ -27,7 +26,7 @@ import checkers.quals.TypeQualifier;
 @Documented
 @TypeQualifier
 @DefaultQualifierInHierarchy
-@SubtypeOf(UnknownInitialization.class)
+@SubtypeOf(Raw.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE_USE, ElementType.TYPE_PARAMETER })
 public @interface NonRaw {
