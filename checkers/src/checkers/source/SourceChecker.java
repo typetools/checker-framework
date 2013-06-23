0f2b94e0b62939342e58dcc5517e0e8e018601a6
Werner Dietl <wdietl@gmail.com>
2013-06-23 10:30:37 -0700

    Fix warnings and failing test.

--- checkers/src/checkers/source/SourceChecker.java
+++ checkers/src/checkers/source/SourceChecker.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryPoolMXBean;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
