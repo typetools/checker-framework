ddb24b78896c98870b7e7ad053ca85ff71ddfc69
Werner Dietl <wdietl@gmail.com>
2013-06-28 18:27:47 +0200

    Use Bottom, not Odd, as type for Void and null.

--- checkers/tests/src/tests/util/Odd.java
+++ checkers/tests/src/tests/util/Odd.java
@@ -4,15 +4,11 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Target;
 
-import checkers.quals.ImplicitFor;
 import checkers.quals.SubtypeOf;
 import checkers.quals.TypeQualifier;
 
-import com.sun.source.tree.Tree;
-
 @TypeQualifier
 @Inherited
-@ImplicitFor ( trees = { Tree.Kind.NULL_LITERAL } )
 @SubtypeOf(MonotonicOdd.class)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 public @interface Odd {}
