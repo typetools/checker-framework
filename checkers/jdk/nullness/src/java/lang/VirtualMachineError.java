2aeb5334bf9a0b433ecaf0c910c778db8bdf7bbb
Michael Ernst <mernst@cs.washington.edu>
2013-08-16 13:28:10 -0700

    Update nullness annotations in JDK

--- checkers/jdk/nullness/src/java/lang/VirtualMachineError.java
+++ checkers/jdk/nullness/src/java/lang/VirtualMachineError.java
@@ -4,6 +4,8 @@ import checkers.nullness.quals.Nullable;
 
 abstract public
 class VirtualMachineError extends Error {
+    private static final long serialVersionUID = 4161983926571568670L;
+
     public VirtualMachineError() {
 	super();
     }
