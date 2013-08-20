44ca35c9d940c69e36ed9c9a4e8fa9ae74da7e4a
Werner Dietl <wdietl@gmail.com>
2013-08-21 01:28:37 +0200

    Make bound explicit to get a NonNull default upper bound.

--- checkers/tests/nullness/VarInfoName.java
+++ checkers/tests/nullness/VarInfoName.java
@@ -1,20 +1,18 @@
 
 public abstract class VarInfoName {
 
-    public abstract <T> T accept(Visitor<T> v);
+    public abstract <T extends Object> T accept(Visitor<T> v);
 
-    public abstract static class Visitor<T> {}
+    public abstract static class Visitor<T extends Object> {}
 
     public abstract static class BooleanAndVisitor
         extends Visitor<Boolean>
     {
         private boolean result;
-        
+
         public BooleanAndVisitor(VarInfoName name) {
             //:: error: (argument.type.incompatible) :: warning: (known.nonnull)
             result = (name.accept(this) != null);
         }
     }
 }
-
-
