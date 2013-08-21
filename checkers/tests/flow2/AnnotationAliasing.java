b68fbca61b52ac435a45af3f46eb39bdd070532b
Werner Dietl <wdietl@gmail.com>
2013-08-21 13:48:55 +0200

    Make org.jmlspecs.annotations.Pure an alias for dataflow.quals.Pure. Update test case to pass. Remove getDeclAnnotationTree method that is unused. If we need it, we need to make it aware of declaration aliases.

--- checkers/tests/flow2/AnnotationAliasing.java
+++ checkers/tests/flow2/AnnotationAliasing.java
@@ -8,31 +8,30 @@ import tests.util.*;
 // Disable the test.  The Checker Framework behaves correctly, but the
 // compiler issues a warning because the test uses a deprecated class
 // (checkers.nullness.quals.Pure), and this warning makes the test fail.
-// @skip-test
 /** Various tests for annotation aliasing. */
 class AnnotationAliasing {
-    
+
     String f1, f2, f3;
-    
+
     @Pure int pure1() { return 1; };
-    @checkers.nullness.quals.Pure int pure2() { return 1; };
-    
+    @org.jmlspecs.annotation.Pure int pure2() { return 1; };
+
     // a method that is not pure (no annotation)
     void nonpure() {
     }
-    
-    //:: error: (pure.not.deterministic.and.sideeffect.free)
+
     @Pure String t1() {
+      //:: error: (purity.not.deterministic.not.sideeffectfree.call)
       nonpure();
       return "";
     }
-    
-    //:: error: (pure.not.deterministic.and.sideeffect.free)
-    @checkers.nullness.quals.Pure String t2() {
+
+    @org.jmlspecs.annotation.Pure String t2() {
+      //:: error: (purity.not.deterministic.not.sideeffectfree.call)
       nonpure();
       return "";
     }
-    
+
     // check aliasing of Pure
     void t1(@Odd String p1, String p2) {
         f1 = p1;
