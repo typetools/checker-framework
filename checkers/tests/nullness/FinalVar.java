f09bba79d10ca80e267a82b816a467cfe9d45523
Michael Ernst <mernst@cs.washington.edu>
2013-08-27 13:25:37 -0700

    Augment FinalVar test case (again, fixing goof in last commit)

--- checkers/tests/nullness/FinalVar.java
+++ checkers/tests/nullness/FinalVar.java
@@ -8,7 +8,7 @@ class FinalVar {
     final @NonNull String iter_view_3 = "Dittos";
     @NonNull String iter_view_4 = "I change but stay non-null";
     iter_view_4 = "new value";
-    @NonNull String iter_view_5 = "I also change but stay non-null";
+    String iter_view_5 = "I also change but stay non-null";
     iter_view_5 = "also new value";
     return new Object() {
       public void useFinalVar() {
