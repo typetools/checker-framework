38c2026b07c2b84bbbbf3f36a7654370dad88d7f
stefanheule <devnull@localhost>
2013-09-20 21:59:36 -0700

    Enable and heavily extend TwoStaticInitBlocks.java test-case.

--- checkers/tests/nullness/TwoStaticInitBlocks.java
+++ checkers/tests/nullness/TwoStaticInitBlocks.java
@@ -1,17 +1,47 @@
 import java.util.regex.*;
 
-// @skip-test BUG, but disabled to avoid breaking the build
+import checkers.nullness.quals.Nullable;
 
+// this is a test-case for initialization that covers multiple initializer blocks, field initializers and a few other things
 class TwoStaticInitBlocks {
-
-  static {
-    System.out.println("First static initializer block, but not the only one.");
+  
+  String f2;
+  String f1 = (f2 = "");
+  
+  {
+    t = "";
+    f1.toString();
+    f2.toString();
   }
 
-  public static final String ws_regexp;
+  final String ws_regexp;
+  String t;
+  String s;
 
-  static {
+  {
     ws_regexp = "hello";
+    t.toString();
+    //:: error: (dereference.of.nullable)
+    s.toString();
   }
 
+}
+
+class B {
+    static String f2;
+    static String f1 = (f2 = "");
+    
+    static {
+      t = "";
+      f1.toString();
+      f2.toString();
+    }
+
+    static final String ws_regexp;
+    static String t;
+
+    static {
+      ws_regexp = "hello";
+      t.toString();
+    }
 }
