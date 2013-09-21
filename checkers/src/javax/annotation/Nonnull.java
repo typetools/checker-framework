fc3a94f7114f6efde4a1c7d8baf85ffd611993d7
Michael Ernst <mernst@cs.washington.edu>
2013-09-20 20:12:53 -0700

    Support newer javax.annotation annotations

--- checkers/src/javax/annotation/Nonnull.java
+++ checkers/src/javax/annotation/Nonnull.java
@@ -2,10 +2,12 @@ package javax.annotation;
 
 import java.lang.annotation.*;
 import checkers.quals.TypeQualifier;
+import javax.annotation.meta.When;
 
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 @TypeQualifier
 public @interface Nonnull {
+    When when() default When.ALWAYS;
 }
