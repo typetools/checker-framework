cc90f1dc7ff2c049686e625b27733100110eccdc
stefanheule <devnull@localhost>
2013-09-21 20:09:07 -0700

    Enable test-case ExtendsArrayList (which is now working).

--- checkers/tests/nullness/ExtendsArrayList.java
+++ checkers/tests/nullness/ExtendsArrayList.java
@@ -1,7 +1,5 @@
 import java.util.*;
 
-// @skip-test failing, but disable to avoid breaking the build
-
 public final class ExtendsArrayList extends ArrayList<String> {
 
   public int removeMany(List<String> toRemove) {
