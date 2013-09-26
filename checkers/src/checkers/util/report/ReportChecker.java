121e024d7c30a550bc7c8fcf8651f16f24a81718
Michael Ernst <mernst@cs.washington.edu>
2013-09-25 20:30:59 -0700

    Javadoc tweaks

--- checkers/src/checkers/util/report/ReportChecker.java
+++ checkers/src/checkers/util/report/ReportChecker.java
@@ -9,11 +9,13 @@ import checkers.types.BasicAnnotatedTypeFactory;
 
 /**
  * The Report Checker for semantic searches.
+ * <p>
  * 
  * See the qualifiers for documentation.
+ * <p>
  *
  * Options:
- * reportTreeKinds: comma separated list of Tree.Kinds that should be reported.
+ * reportTreeKinds: comma-separated list of Tree.Kinds that should be reported.
  *
  */
 @TypeQualifiers({ Unqualified.class })
