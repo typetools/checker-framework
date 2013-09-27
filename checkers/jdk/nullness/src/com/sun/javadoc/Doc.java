42347cfc25d54aec94a6753872cd62f99a82866c
Werner Dietl <wdietl@gmail.com>
2013-09-26 17:26:18 -0700

    Import @Pure annotation.

--- checkers/jdk/nullness/src/com/sun/javadoc/Doc.java
+++ checkers/jdk/nullness/src/com/sun/javadoc/Doc.java
@@ -1,6 +1,7 @@
 package com.sun.javadoc;
 
 import checkers.nullness.quals.Nullable;
+import dataflow.quals.Pure;
 
 public abstract interface Doc extends java.lang.Comparable<java.lang.Object> {
   public abstract java.lang.String commentText();
