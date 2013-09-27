c1ffb8a24b2bc14ddcbdf0014e253593f73778da
stefanheule <stefanheule@localhost>
2013-09-27 14:20:31 -0700

    Correct invalid test-cases with method calls in field initializers.

--- checkers/tests/all-systems/FieldWithInit.java
+++ checkers/tests/all-systems/FieldWithInit.java
@@ -1,7 +1,10 @@
+import checkers.initialization.quals.UnknownInitialization;
+import checkers.nullness.quals.Raw;
+
 class FieldWithInit {
     Object f = foo();
 
-    Object foo() {
+    Object foo(@UnknownInitialization @Raw FieldWithInit this) {
         return new Object();
     }
 }
