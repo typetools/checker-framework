2905a0909f45ebfe2f4a228b8e156408c1f5aa6a
Michael Ernst <mernst@cs.washington.edu>
2013-10-05 20:21:56 -0700

    Improve purity documentation

--- dataflow/src/dataflow/quals/Deterministic.java
+++ dataflow/src/dataflow/quals/Deterministic.java
@@ -17,11 +17,10 @@ import java.lang.annotation.Target;
  * call to a <tt>@Deterministic</tt> method, flow-sensitive type refinement
  * can assume that anything learned about the first invocation is true
  * about subsequent invocations (so long as no non-<tt>@</tt>{@link
- * SideEffectFree} method call intervenes).  For example, it can be
- * determined that the following code never suffers a null pointer
+ * SideEffectFree} method call intervenes).  For example,
+ * the following code never suffers a null pointer
  * exception, so the Nullness Checker need not issue a warning:
- * <pre><code>
-      if (x.myDeterministicMethod() != null) {
+ * <pre><code>      if (x.myDeterministicMethod() != null) {
         x.myDeterministicMethod().hashCode();
       }</code></pre>
  * <p>
@@ -77,6 +76,11 @@ import java.lang.annotation.Target;
  * deterministic, but the Checker Framework may issue false positive
  * warnings, for code that uses one of the forbidden constructs but is
  * deterministic nonetheless.
+ * <p>
+ * 
+ * In fact, the rules are so conservative that checking is currently
+ * disabled by default, but can be enabled via the <tt>-AenablePurity</tt>
+ * command-line option.
  * <p>
  *
  * @checker.framework.manual #type-refinement-purity Side effects, determinism, purity, and flow-sensitive analysis
