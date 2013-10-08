316b1acf104ca6ece506dde245346a307930cc7f
stefanheule <stefanheule@localhost>
2013-10-07 21:15:08 -0700

    Add some items missing from changelog-checkers.txt.

---
+++ checkers/src/checkers/formatter/quals/InvalidFormat.java
@@ -0,0 +1,30 @@
+package checkers.formatter.quals;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+import checkers.quals.SubtypeOf;
+import checkers.quals.TypeQualifier;
+import checkers.quals.Unqualified;
+
+/**
+ * This annotation, attached to a {@link java.lang.String String} type,
+ * indicates that the String may not be passed to
+ * {@link java.util.Formatter#format(String, Object...) Formatter.format}, or
+ * similar functions.
+ * Passing it will lead to the exception message indicated in the annotation's
+ * value.
+ * <p>
+ *
+ * This annotation may not be written in source code; it is an
+ * implementation detail of the checker.
+ *
+ * @checker.framework.manual #formatter-checker Format String Checker
+ * @author Konstantin Weitz
+ */
+@TypeQualifier
+@SubtypeOf(Unqualified.class)
+@Target({ ElementType.TYPE_USE, ElementType.TYPE_PARAMETER })
+public @interface InvalidFormat {
+    String value();
+}
