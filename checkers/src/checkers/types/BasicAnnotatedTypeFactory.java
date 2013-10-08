9dc828dfaec590119ccdf82fb000ad2787d1d891
Werner Dietl <wdietl@gmail.com>
2013-10-08 18:23:17 -0400

    Move many factory methods from the Checkers to the Factory. The AnnotatedTypeFactory class needs some more serious loving.

--- checkers/src/checkers/types/BasicAnnotatedTypeFactory.java
+++ checkers/src/checkers/types/BasicAnnotatedTypeFactory.java
@@ -12,33 +12,30 @@ import java.util.List;
 
 import javax.lang.model.element.VariableElement;
 
-import com.sun.source.tree.CompilationUnitTree;
-
 /**
  * A factory that extends {@link AbstractBasicAnnotatedTypeFactory} to use the
  * default flow-sensitive analysis as provided by {@link CFAnalysis}.
  *
  * @author Stefan Heule
  */
-public class BasicAnnotatedTypeFactory<Checker extends BaseTypeChecker<?>>
-    extends AbstractBasicAnnotatedTypeFactory<Checker, CFValue, CFStore, CFTransfer, CFAnalysis> {
+public class BasicAnnotatedTypeFactory
+    extends AbstractBasicAnnotatedTypeFactory<CFValue, CFStore, CFTransfer, CFAnalysis> {
 
-    public BasicAnnotatedTypeFactory(Checker checker, CompilationUnitTree root,
-            boolean useFlow) {
-        super(checker, root, useFlow);
+    public BasicAnnotatedTypeFactory(BaseTypeChecker checker, boolean useFlow) {
+        super(checker, useFlow);
+
         // Every subclass must call postInit!
         if (this.getClass().equals(BasicAnnotatedTypeFactory.class)) {
             this.postInit();
         }
     }
 
-    public BasicAnnotatedTypeFactory(Checker checker, CompilationUnitTree root) {
-        this(checker, root, FLOW_BY_DEFAULT);
+    public BasicAnnotatedTypeFactory(BaseTypeChecker checker) {
+        this(checker, FLOW_BY_DEFAULT);
     }
 
     @Override
-    protected CFAnalysis createFlowAnalysis(Checker checker,
-            List<Pair<VariableElement, CFValue>> fieldValues) {
-        return new CFAnalysis(this, processingEnv, checker, fieldValues);
+    protected CFAnalysis createFlowAnalysis(List<Pair<VariableElement, CFValue>> fieldValues) {
+        return new CFAnalysis(checker, this, fieldValues);
     }
 }
