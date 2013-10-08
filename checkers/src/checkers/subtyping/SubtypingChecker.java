f0cbde5fca01b1ff68b5dadfea309b04199da78a
Werner Dietl <wdietl@gmail.com>
2013-10-08 18:17:26 -0400

    Move factory method to ATF.

--- checkers/src/checkers/subtyping/SubtypingChecker.java
+++ checkers/src/checkers/subtyping/SubtypingChecker.java
@@ -1,17 +1,14 @@
 package checkers.subtyping;
 
 import checkers.basetype.BaseTypeChecker;
-import checkers.types.BasicAnnotatedTypeFactory;
+import checkers.basetype.BaseTypeVisitor;
 
 import java.lang.annotation.Annotation;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.annotation.processing.SupportedOptions;
-
-import com.sun.source.tree.CompilationUnitTree;
 
 /**
  * A checker for type qualifier systems that only checks subtyping
@@ -32,41 +29,11 @@ import com.sun.source.tree.CompilationUnitTree;
  * @checker.framework.manual #basic-checker Basic Checker
  */
 @SupportedOptions( { "quals" })
-public final class SubtypingChecker extends BaseTypeChecker<BasicAnnotatedTypeFactory<SubtypingChecker>> {
-
-    @Override
-    public BasicAnnotatedTypeFactory<SubtypingChecker> createFactory(CompilationUnitTree root) {
-        return new BasicAnnotatedTypeFactory<SubtypingChecker>(this, root);
-    }
-
-    @Override
-    @SuppressWarnings("unchecked")
-    protected Set<Class<? extends Annotation>> createSupportedTypeQualifiers() {
-
-        String qualNames = getOption("quals");
-        if (qualNames == null) {
-            errorAbort("SubtypingChecker: missing required option: -Aquals");
-        }
-
-        Set<Class<? extends Annotation>> qualSet =
-            new HashSet<Class<? extends Annotation>>();
-        for (String qualName : qualNames.split(",")) {
-            try {
-                final Class<? extends Annotation> q =
-                    (Class<? extends Annotation>)Class.forName(qualName);
-                qualSet.add(q);
-            } catch (ClassNotFoundException e) {
-                errorAbort("SubtypingChecker: could not load class for qualifier: " + qualName + "; ensure that your classpath is correct.");
-            }
-        }
-
-        return Collections.unmodifiableSet(qualSet);
-    }
-
+public final class SubtypingChecker extends BaseTypeChecker {
     @Override
     public Collection<String> getSuppressWarningsKeys() {
         Set<String> swKeys = new HashSet<String>();
-        Set<Class<? extends Annotation>> annos = getSupportedTypeQualifiers();
+        Set<Class<? extends Annotation>> annos = ((BaseTypeVisitor<?>)visitor).getTypeFactory().getSupportedTypeQualifiers();
         if (annos.isEmpty())
             return super.getSuppressWarningsKeys();
 
