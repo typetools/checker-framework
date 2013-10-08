8cb68689368bb8f9cd7ba68ddf48bd50e1a5c002
Werner Dietl <wdietl@gmail.com>
2013-10-08 18:27:24 -0400

    Simple adaptations to framework changes. Small formatting fixes.

--- checkers/tests/test-lubglb/lubglb/quals/D.java
+++ checkers/tests/test-lubglb/lubglb/quals/D.java
@@ -1,13 +1,13 @@
 package lubglb.quals;
 
+import checkers.quals.SubtypeOf;
+import checkers.quals.TypeQualifier;
+
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.lang.annotation.ElementType;
-
-import checkers.quals.SubtypeOf;
-import checkers.quals.TypeQualifier;
 
 @TypeQualifier
 @SubtypeOf({C.class, B.class})
--- checkers/tests/test-polyall/polyall/quals/H2Bot.java
+++ checkers/tests/test-polyall/polyall/quals/H2Bot.java
@@ -1,21 +1,21 @@
 package polyall.quals;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.lang.annotation.ElementType;
-
-import com.sun.source.tree.Tree;
-
 import checkers.quals.ImplicitFor;
 import checkers.quals.SubtypeOf;
 import checkers.quals.TypeQualifier;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.sun.source.tree.Tree;
+
 @TypeQualifier
 @SubtypeOf({H2S1.class, H2S2.class})
+@ImplicitFor(trees={Tree.Kind.NULL_LITERAL})
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
-@ImplicitFor(trees={Tree.Kind.NULL_LITERAL})
 public @interface H2Bot {}
