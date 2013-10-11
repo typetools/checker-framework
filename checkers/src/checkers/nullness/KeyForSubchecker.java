feb1b371a9122d46faaaca0df4d1df8f91131247
Werner Dietl <wdietl@gmail.com>
2013-10-11 00:11:27 -0400

    Put supported type qualifiers on KeyForATF to allow using with a different checker, i.e. the NullnessChecker.

--- checkers/src/checkers/nullness/KeyForSubchecker.java
+++ checkers/src/checkers/nullness/KeyForSubchecker.java
@@ -1,16 +1,11 @@
 package checkers.nullness;
 
 import checkers.basetype.BaseTypeChecker;
-import checkers.nullness.quals.KeyFor;
-import checkers.nullness.quals.KeyForBottom;
-import checkers.quals.TypeQualifiers;
-import checkers.quals.Unqualified;
 
 /**
  * TODO: doc
  * @checker.framework.manual #nullness-checker Nullness Checker
  */
-@TypeQualifiers({ KeyFor.class, Unqualified.class, KeyForBottom.class})
 public class KeyForSubchecker extends BaseTypeChecker {
     /*
     @Override
