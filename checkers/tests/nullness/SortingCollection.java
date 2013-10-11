4d4688e9d0074d9b80ea210e90e6522d4184a37d
Werner Dietl <wdietl@gmail.com>
2013-10-11 01:27:42 -0400

    Automated merge with https://checker-framework.googlecode.com/hg/

--- checkers/tests/nullness/SortingCollection.java
+++ checkers/tests/nullness/SortingCollection.java
@@ -1,4 +1,12 @@
 // @skip-test Crashes the Checker Framework, but skipped to avoid breaking the build
+// 
+// It looks like we are relying on name equality at some point when resolving
+// a type parameter.  If you replace T by E, changing the code to:
+//    static class PollableTreeSet<E> extends TreeSet<E> {
+//    }
+// then the assertion failure goes away.  Evidently this is because
+// the annotated TreeSet.java file uses the type variable E.
+
 
 import java.util.*;
 
