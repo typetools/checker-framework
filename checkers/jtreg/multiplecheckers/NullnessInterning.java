3a3f716cfb3c3d0c09915b81285c6b37b1c22200
Werner Dietl <wdietl@gmail.com>
2013-10-20 22:40:11 -0400

    Output stack traces on errors to make debugging simpler.

--- checkers/jtreg/multiplecheckers/NullnessInterning.java
+++ checkers/jtreg/multiplecheckers/NullnessInterning.java
@@ -2,17 +2,17 @@
  * @test
  * @summary Test that command-line options can be provided to specific checkers
  *
- * @compile/ref=NullnessInterning1.out -XDrawDiagnostics -Anomsgtext -processor checkers.nullness.NullnessChecker,checkers.interning.InterningChecker NullnessInterning.java -Awarns
+ * @compile/ref=NullnessInterning1.out -XDrawDiagnostics -Anomsgtext -processor checkers.nullness.NullnessChecker,checkers.interning.InterningChecker NullnessInterning.java -Awarns -AprintErrorStack
  *
- * @compile/ref=NullnessInterning2.out -AprintErrorStack -XDrawDiagnostics -Anomsgtext -processor checkers.nullness.NullnessChecker,checkers.interning.InterningChecker NullnessInterning.java -Awarns -ANullnessChecker_skipDefs=NullnessInterning
+ * @compile/ref=NullnessInterning2.out -AprintErrorStack -XDrawDiagnostics -Anomsgtext -processor checkers.nullness.NullnessChecker,checkers.interning.InterningChecker NullnessInterning.java -Awarns -AprintErrorStack -ANullnessChecker_skipDefs=NullnessInterning
  *
- * @compile/ref=NullnessInterning2.out -XDrawDiagnostics -Anomsgtext -processor checkers.nullness.NullnessChecker,checkers.interning.InterningChecker NullnessInterning.java -Awarns -Acheckers.nullness.NullnessChecker_skipDefs=NullnessInterning
+ * @compile/ref=NullnessInterning2.out -XDrawDiagnostics -Anomsgtext -processor checkers.nullness.NullnessChecker,checkers.interning.InterningChecker NullnessInterning.java -Awarns -AprintErrorStack -Acheckers.nullness.NullnessChecker_skipDefs=NullnessInterning
  *
- * @compile/ref=NullnessInterning3.out -XDrawDiagnostics -Anomsgtext -processor checkers.nullness.NullnessChecker,checkers.interning.InterningChecker NullnessInterning.java -Awarns -AInterningChecker_skipDefs=NullnessInterning
+ * @compile/ref=NullnessInterning3.out -XDrawDiagnostics -Anomsgtext -processor checkers.nullness.NullnessChecker,checkers.interning.InterningChecker NullnessInterning.java -Awarns -AprintErrorStack -AInterningChecker_skipDefs=NullnessInterning
  *
- * @compile/ref=NullnessInterning3.out -XDrawDiagnostics -Anomsgtext -processor checkers.nullness.NullnessChecker,checkers.interning.InterningChecker NullnessInterning.java -Awarns -Acheckers.interning.InterningChecker_skipDefs=NullnessInterning
+ * @compile/ref=NullnessInterning3.out -XDrawDiagnostics -Anomsgtext -processor checkers.nullness.NullnessChecker,checkers.interning.InterningChecker NullnessInterning.java -Awarns -AprintErrorStack -Acheckers.interning.InterningChecker_skipDefs=NullnessInterning
  *
- * @compile/ref=NullnessInterning4.out -XDrawDiagnostics -Anomsgtext -processor checkers.nullness.NullnessChecker,checkers.interning.InterningChecker NullnessInterning.java -Awarns -Acheckers.basetype.BaseTypeChecker_skipDefs=NullnessInterning
+ * @compile/ref=NullnessInterning4.out -XDrawDiagnostics -Anomsgtext -processor checkers.nullness.NullnessChecker,checkers.interning.InterningChecker NullnessInterning.java -Awarns -AprintErrorStack -Acheckers.basetype.BaseTypeChecker_skipDefs=NullnessInterning
  */
 
 class NullnessInterning {
