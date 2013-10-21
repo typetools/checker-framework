26d81d36580d651ded2a0e489c562140ccb48fe6
Werner Dietl <wdietl@gmail.com>
2013-10-21 01:23:35 -0400

    Improve assertion error output.

--- checkers/src/checkers/types/AnnotatedTypeFactory.java
+++ checkers/src/checkers/types/AnnotatedTypeFactory.java
@@ -1785,7 +1785,7 @@ public class AnnotatedTypeFactory implements AnnotationProvider {
      * @return the path for {@code node} under the current root
      */
     public final TreePath getPath(Tree node) {
-        assert root != null : "root needs to be set when used on trees; factory: " + this.getClass();
+        assert root != null : "AnnotatedTypeFactory.getPath: root needs to be set when used on trees; factory: " + this.getClass();
 
         if (node == null) return null;
 
