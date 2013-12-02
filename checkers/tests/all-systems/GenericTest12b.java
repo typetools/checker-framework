8b921ac8154df20d29e9661b05472ada7475a8d6
Werner Dietl <wdietl@gmail.com>
2013-12-01 22:23:17 -0500

    Allow method call in OIGJ.

--- checkers/tests/all-systems/GenericTest12b.java
+++ checkers/tests/all-systems/GenericTest12b.java
@@ -1,13 +1,14 @@
 import checkers.nullness.quals.Nullable;
 import checkers.nullness.quals.Raw;
 import checkers.initialization.quals.UnderInitialization;
+import checkers.oigj.quals.AssignsFields;
 
 class GenericTest12b {
     class Cell<T1 extends @Nullable Object> {}
 
     class Node<CONTENT extends @Nullable Object> {
         public Node(Cell<CONTENT> userObject) { }
-        void nodecall(@Raw @UnderInitialization Node<CONTENT> this, Cell<CONTENT> userObject) {}
+        void nodecall(@AssignsFields @Raw @UnderInitialization Node<CONTENT> this, Cell<CONTENT> userObject) {}
     }
 
     class RootNode extends Node<Void> {
@@ -16,6 +17,6 @@ class GenericTest12b {
             call(new Cell<Void>());
             nodecall(new Cell<Void>());
         }
-        void call(@Raw @UnderInitialization RootNode this, Cell<Void> userObject) {}
+        void call(@AssignsFields @Raw @UnderInitialization RootNode this, Cell<Void> userObject) {}
     }
 }
