fd83340f287b0a44b9a111fc2abeb0b425dfb144
Werner Dietl <wdietl@gmail.com>
2013-12-11 18:56:46 -0500

    Small javadoc tweaks.

--- checkers/src/checkers/formatter/quals/FormatMethod.java
+++ checkers/src/checkers/formatter/quals/FormatMethod.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 /**
  * If this annotation is attached to a
  * {@link java.util.Formatter#format(String, Object...) Formatter.format} like
- * method, the framework checks that that the parameters passed as var-args are
+ * method, the framework checks that the parameters passed as var-args are
  * compatible with the format string.
  * 
  * @checker_framework_manual #formatter-checker Format String Checker
