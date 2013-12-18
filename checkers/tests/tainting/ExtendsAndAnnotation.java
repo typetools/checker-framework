328b1f40acb188410941f78aceabf162e4318360
Werner Dietl <wdietl@gmail.com>
2013-12-17 22:49:30 -0500

    Separate TypeInvalid from ExtendsAndAnnotation test case. Expand TypeInvalid tests.

--- checkers/tests/tainting/ExtendsAndAnnotation.java
+++ checkers/tests/tainting/ExtendsAndAnnotation.java
@@ -7,29 +7,4 @@ class ExtendsAndAnnotation extends @Tainted Object {
         Object o = new @Untainted ExtendsAndAnnotation();
         o = new @Tainted ExtendsAndAnnotation();
     }
-
-    // Duplication forbidden
-    //:: error: (type.invalid)
-    void bad(@Tainted @Untainted ExtendsAndAnnotation c) {
-        //:: error: (type.invalid)
-        Object o = new @Tainted @Untainted ExtendsAndAnnotation();
-        //:: error: (type.invalid)
-        o = new @Tainted @Untainted ExtendsAndAnnotation();
-        //:: error: (type.invalid)
-        o = o.equals(new @Tainted @Untainted ExtendsAndAnnotation());
-        //:: error: (type.invalid)
-	o = (Object) new @Tainted @Untainted ExtendsAndAnnotation();
-        //:: error: (type.invalid)
-        o = (@Tainted @Untainted ExtendsAndAnnotation) o;
-        //:: error: (type.invalid)
-        o = (new @Tainted @Untainted ExtendsAndAnnotation()) instanceof Object;
-        //:: error: (type.invalid)
-        o = o instanceof @Tainted @Untainted ExtendsAndAnnotation;
-
-        // TODO: for at least one type system we should ensure
-        // that type validation is consistently performed for each
-        // possible tree.
-        // We should also add a jtreg version of this test to
-        // ensure that each error is only output once.
-    }
 }
