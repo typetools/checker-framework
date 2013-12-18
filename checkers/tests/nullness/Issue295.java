6ec87a7700378a67ea2f7ece6ca28db4289b3ccd
Werner Dietl <wdietl@gmail.com>
2013-12-18 14:22:20 -0500

    Use distinct type variable names to make debugging easier.

--- checkers/tests/nullness/Issue295.java
+++ checkers/tests/nullness/Issue295.java
@@ -13,35 +13,38 @@ abstract class Issue295 {
         Box(T value) { this.value = value; }
     }
 
-    <T> void f1(Factory<Box</*@Nullable*/ T>> f) {
-        Box</*@Nullable*/ T> v = load(f);
+    <MT1> void f1(Factory<Box<@Nullable MT1>> f) {
+        Box<@Nullable MT1> v = load(f);
     }
 
-    <T> void f2(Factory<Box</*@Nullable*/ T>> f) {
-        Box<? extends /*@Nullable*/ T> v = load(f);
+    <MT2> void f2(Factory<Box<@Nullable MT2>> f) {
+        Box<? extends @Nullable MT2> v = load(f);
     }
 
-    <T> void f3(Factory<Box<T>> f) {
-        Box<T> v = load(f);
+    <MT3> void f3(Factory<Box<MT3>> f) {
+        Box<MT3> v = load(f);
     }
 
-    <T> void f4(Factory<Box</*@Nullable*/ T>> f) {
-        Box<? extends /*@Nullable*/ T> v = load(f);
+    <MT4> void f4(Factory<Box<@Nullable MT4>> f) {
+        Box<? extends @Nullable MT4> v = load(f);
     }
 
-    <T extends /*@Nullable*/ Object> void f5(Factory<Box<T>> f) {
-        Box<T> v = load(f);
+    <MT5 extends @Nullable Object> void f5(Factory<Box<MT5>> f) {
+        Box<MT5> v = load(f);
     }
 
-    <T extends /*@Nullable*/ Object> void f6(Factory<Box<T>> f) {
-        Box<? extends T> v = load(f);
+    <MT6 extends @Nullable Object> void f6(Factory<Box<MT6>> f) {
+        Box<? extends MT6> v = load(f);
     }
 
-    <T> void f1noquals(Factory<Box<String>> f) {
+    <MT7> void f1noquals(Factory<Box<String>> f) {
         Box<String> v = load(f);
     }
 
-    abstract <T> T load(Factory<T> p);
-    abstract class Factory<T> { abstract T create(); }
+    abstract <MTL> MTL load(Factory<MTL> p);
+
+    abstract class Factory<TF> {
+        abstract TF create();
+    }
 }
 
