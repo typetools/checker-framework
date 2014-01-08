56e0458ec5eeb886e59a74e1aae8a54016a99997
Werner Dietl <wdietl@gmail.com>
2014-01-08 13:33:42 -0500

    Add notes on whether to consider superclasses that have the annotation.

--- checkers/src/checkers/source/SupportedLintOptions.java
+++ checkers/src/checkers/source/SupportedLintOptions.java
@@ -1,14 +1,15 @@
 package checkers.source;
 
 import java.lang.annotation.*;
-import javax.annotation.processing.SupportedOptions;
 
 /**
  * An annotation used to indicate what lint options a checker supports. The
  * {@link SourceChecker#getSupportedLintOptions} method can construct its
  * result from the value of this annotation.
  *
- * @see SupportedOptions
+ * TODO: Are superclasses considered? Should we?
+ *
+ * @see checkers.source.SupportedOptions
  */
 @Inherited
 @Documented
