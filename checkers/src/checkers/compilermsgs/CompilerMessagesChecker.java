98153b3d439118180a20210bd06b277eda650fa8
Werner Dietl <wdietl@gmail.com>
2014-01-21 02:13:12 -0500

    Remove @Unqualified/@Bottom and instead introduce type system specific qualifiers.

--- checkers/src/checkers/compilermsgs/CompilerMessagesChecker.java
+++ checkers/src/checkers/compilermsgs/CompilerMessagesChecker.java
@@ -3,9 +3,9 @@ package checkers.compilermsgs;
 import checkers.compilermsgs.quals.CompilerMessageKey;
 import checkers.propkey.PropertyKeyChecker;
 import checkers.propkey.quals.PropertyKey;
-import checkers.quals.Bottom;
+import checkers.propkey.quals.PropertyKeyBottom;
+import checkers.propkey.quals.UnknownPropertyKey;
 import checkers.quals.TypeQualifiers;
-import checkers.quals.Unqualified;
 
 /**
  * A PropertyKeyChecker for the compiler message keys that are used
@@ -13,5 +13,6 @@ import checkers.quals.Unqualified;
  *
  * @author wmdietl
  */
-@TypeQualifiers( {CompilerMessageKey.class, PropertyKey.class, Unqualified.class, Bottom.class} )
+@TypeQualifiers( {CompilerMessageKey.class, PropertyKey.class,
+    UnknownPropertyKey.class, PropertyKeyBottom.class} )
 public class CompilerMessagesChecker extends PropertyKeyChecker {}
