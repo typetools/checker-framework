a50c6b3ab297403f07c90bb9f4400f5c10008daf
Werner Dietl <wdietl@gmail.com>
2014-01-21 02:13:12 -0500

    Remove @Unqualified/@Bottom and instead introduce type system specific qualifiers.

--- checkers/src/checkers/propkey/quals/PropertyKeyBottom.java
+++ checkers/src/checkers/propkey/quals/PropertyKeyBottom.java
@@ -1,18 +1,19 @@
 package checkers.propkey.quals;
 
 import java.lang.annotation.*;
+
+import checkers.quals.DefaultQualifierInHierarchy;
 import checkers.quals.SubtypeOf;
 import checkers.quals.TypeQualifier;
 
 /**
- * Indicates that the {@code String} type can be used as key in a
- * property file or resource bundle.
+ * The bottom qualifier.
  *
  * @checker_framework_manual #propkey-checker Property File Checker
  */
 @TypeQualifier
-@SubtypeOf(UnknownPropertyKey.class)
+@SubtypeOf(PropertyKey.class)
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
-public @interface PropertyKey {}
+public @interface PropertyKeyBottom {}
