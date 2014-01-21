d15959328703843dd28f5731356f4ab035f0e57c
Werner Dietl <wdietl@gmail.com>
2014-01-21 02:08:21 -0500

    Remove @Unqualified/@Bottom and instead introduce type system specific qualifiers.

--- checkers/src/checkers/units/quals/UnknownUnits.java
+++ checkers/src/checkers/units/quals/UnknownUnits.java
@@ -1,20 +1,22 @@
 package checkers.units.quals;
 
 import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
 
+import checkers.quals.DefaultQualifierInHierarchy;
 import checkers.quals.SubtypeOf;
 import checkers.quals.TypeQualifier;
 
 /**
- * Units of measure for angles.
+ * UnknownUnits is the top type of the type hierarchy.
  */
 @TypeQualifier
-@SubtypeOf(UnknownUnits.class)
+@SubtypeOf({})
+@DefaultQualifierInHierarchy
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
-public @interface Angle {}
+public @interface UnknownUnits {}
