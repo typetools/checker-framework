3bbdcda7cf4dd3ff4bea7e6973bf2d2d2e9b1486
jonathangburke <jonathangburke@gmail.com>
2014-01-23 13:16:47 -0800

    Apply patch from issue #288 ( Parse more parts of the error message ) in the Eclipse plugin

--- eclipse/checker-framework-eclipse-plugin/src/checkers/eclipse/marker/MarkerReporter.java
+++ eclipse/checker-framework-eclipse-plugin/src/checkers/eclipse/marker/MarkerReporter.java
@@ -1,5 +1,8 @@
 package checkers.eclipse.marker;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRunnable;
@@ -29,57 +32,33 @@ public class MarkerReporter implements IWorkspaceRunnable
     private final int startPosition;
     private final int endPosition;
 
-    private final String errorKey;
-    private final int numErrorArguments;
-    private final String errorArguments;
+	private final String errorKey;
+    private final List<String> errorArguments;
 
 
-    public MarkerReporter(IResource resource, int startLine, String message, int startPosition, int endPosition)
-    {
+	public MarkerReporter(IResource resource, int startLine, String errorKey,
+			List<String> errorArguments, String message, int startPosition,
+			int endPosition) {
         this.startLine = startLine;
         this.resource = resource;
         this.startPosition = startPosition;
         this.endPosition = endPosition;
-
-        final String [] details = message.split(DETAIL_SEPARATOR_REGEX);
-        if(details.length < 3) {
-            throw new RuntimeException("Marker reporter expects at least 3 arguments separated by "
-                    + DETAIL_SEPARATOR + ".\n" +
-                    "Message was: " + message + " line was " + startLine);
-        }
-
-        errorKey = details[0].substring(1).substring(0, details[0].length()-2); //strip off ()
-
-        numErrorArguments = Integer.parseInt(details[1].trim());
-
-        if(numErrorArguments > 0) {
-            String errorArgs = details[0];
-
-            for(int i = 1; i < numErrorArguments; i++) {
-                errorArgs += DETAIL_SEPARATOR + details[i+2];
-            }
-            errorArguments = errorArgs;
-        } else {
-            errorArguments = "";
-        }
-
-        this.message = details[details.length - 1];
+        this.errorKey = errorKey;
+        this.errorArguments = errorArguments;
+        this.message = message;
     }
 
 
 
     @Override
-    public void run(IProgressMonitor monitor) throws CoreException
-    {
+    public void run(IProgressMonitor monitor) throws CoreException {
 
-        if (CheckerPlugin.DEBUG)
-        {
+        if (CheckerPlugin.DEBUG) {
             System.out.println("Creating marker for " + resource.getLocation());
         }
 
         IMarker marker = resource.createMarker(NAME);
-        if (CheckerPlugin.DEBUG)
-        {
+        if (CheckerPlugin.DEBUG) {
             System.out.println("Setting attibutes for marker in "
                     + resource.getLocation());
         }
@@ -91,8 +70,14 @@ public class MarkerReporter implements IWorkspaceRunnable
         marker.setAttribute(IMarker.CHAR_END, endPosition);
 
         marker.setAttribute(ERROR_KEY,           errorKey);
-        marker.setAttribute(NUM_ERROR_ARGUMENTS, numErrorArguments);
-        marker.setAttribute(ERROR_ARGUMENTS,     errorArguments);
+        marker.setAttribute(NUM_ERROR_ARGUMENTS, errorArguments.size());
+        Iterator<String> iterator = errorArguments.iterator();
+		int i = 0;
+		while (iterator.hasNext()) {
+			String errorArgument = iterator.next();
+			marker.setAttribute(ERROR_ARGUMENTS + i, errorArgument);
+			++i;
+		}
     }
 
 }
