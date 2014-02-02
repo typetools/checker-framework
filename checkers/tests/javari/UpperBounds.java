9c5eca45c052f50e1bad687d67664ec3029c46e4
Werner Dietl <wdietl@gmail.com>
2014-02-02 15:18:13 -0500

    Adapt test case to explicit vs. implicit upper bound rule.

--- checkers/tests/javari/UpperBounds.java
+++ checkers/tests/javari/UpperBounds.java
@@ -1,16 +1,31 @@
 import checkers.javari.quals.*;
 import java.util.Date;
 
-class UpperBounds<T extends Date> {
-    // check that the upper bound of a class defaults to readonly
+class UpperBoundsImpl<T> {
+    // an implicit upper bound of a class defaults to readonly
     void testAsClass() {
-        UpperBounds<@Mutable Date> t1 = null;
-        UpperBounds<@ReadOnly Date> t2 = null;
+        UpperBoundsImpl<@Mutable Date> t1 = null;
+        UpperBoundsImpl<@ReadOnly Date> t2 = null;
     }
 
     T instance;
     void testAsUse() {
-        UpperBounds<Date> l = new UpperBounds<Date>();
+        UpperBoundsImpl<Date> l = new UpperBoundsImpl<Date>();
+        @Mutable Date d = l.instance;
+    }
+}
+
+class UpperBoundsExpl<T extends Date> {
+    // an explicit upper bound of a class defaults to mutable
+    void testAsClass() {
+        UpperBoundsExpl<@Mutable Date> t1 = null;
+        //:: error: (type.argument.type.incompatible)
+        UpperBoundsExpl<@ReadOnly Date> t2 = null;
+    }
+
+    T instance;
+    void testAsUse() {
+        UpperBoundsExpl<Date> l = new UpperBoundsExpl<Date>();
         @Mutable Date d = l.instance;
     }
 }
