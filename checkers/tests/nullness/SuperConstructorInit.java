304a732111ba275e2227fdf2fcca6e03ec4474b5
Werner Dietl <wdietl@gmail.com>
2014-02-11 18:06:10 -0500

    Remove unused import. Fix formatting.

--- checkers/tests/nullness/SuperConstructorInit.java
+++ checkers/tests/nullness/SuperConstructorInit.java
@@ -1,14 +1,13 @@
 import checkers.nullness.quals.*;
-import static checkers.nullness.NullnessUtils.*;
 
 class SuperConstructorInit {
-    
+
     String a;
-    
+
     public SuperConstructorInit() {
         a = "";
     }
-    
+
     public static class B extends SuperConstructorInit {
         String b;
         //:: error: (initialization.fields.uninitialized)
@@ -17,5 +16,5 @@ class SuperConstructorInit {
             a.toString();
         }
     }
-    
+
 }
