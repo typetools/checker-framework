913f2aba9294f429ab18dde4d54b1cda9c7a95a4
Werner Dietl <wdietl@gmail.com>
2014-02-15 18:58:09 -0500

    GUI Effects Checker contributed by Colin Stebbins Gordon <csgordon@cs.washington.edu>.

---
+++ checkers/tests/guieffects/IGenericTask.java
@@ -0,0 +1,9 @@
+import checkers.guieffects.quals.PolyUI;
+import checkers.guieffects.quals.PolyUIEffect;
+import checkers.guieffects.quals.PolyUIType;
+
+@PolyUIType
+@PolyUI
+public interface IGenericTask {
+    @PolyUIEffect public void doGenericStuff();
+}
