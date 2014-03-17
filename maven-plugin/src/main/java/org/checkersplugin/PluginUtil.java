f6e4fa5a5e965ca17a478e0ba2f6cd0fbfdca6ba
jonathangburke <jonathangburke@gmail.com>
2014-03-17 10:11:53 -0700

    Update maven plugin and example to new project layout (org.checkerframework.mavenplugin)

--- maven-plugin/src/main/java/org/checkersplugin/PluginUtil.java
+++
@@ -1,3 +0,0 @@
-package org.checkersplugin;
-tail: cannot open ‘+2’ for reading: No such file or directory
-tail: cannot open ‘/home/wmdietl/Sync/wmdietl/research/eclipse-workspaces/types/dff-checker-framework/checker-framework/checker/src/org/checkerframework/framework/util/PluginUtil.java’ for reading: No such file or directory
