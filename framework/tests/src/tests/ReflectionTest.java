5bbb9de5c4b1f56ea393baa30d02b7f915be66da
Suzanne Millstein <smillst@cs.washington.edu>
2014-03-25 15:37:28 -0700

    Reflection resolution tests: Added reflection.astub with conservitive annotations invoke and newInstance Had to add an @ReflectBottom qualifier so it code be written in the test code

--- framework/tests/src/tests/ReflectionTest.java
+++ framework/tests/src/tests/ReflectionTest.java
@@ -1,7 +1,9 @@
 package tests;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.checkerframework.framework.test.ParameterizedCheckerTest;
 import org.junit.runners.Parameterized.Parameters;
@@ -9,8 +11,8 @@ import org.junit.runners.Parameterized.Parameters;
 /**
  * Tests the reflection resolution using a toy type system.
  * 
- * @author rjust
- *
+ * @author rjust, smillst
+ * 
  */
 public class ReflectionTest extends ParameterizedCheckerTest {
 
@@ -19,5 +21,19 @@ public class ReflectionTest extends ParameterizedCheckerTest {
     }
 
     @Parameters
-    public static Collection<Object[]> data() { return testFiles("reflection"); }
+    public static Collection<Object[]> data() {
+        return testFiles("reflection");
+    }
+
+    @Override
+    protected void test(File testFile) {
+        final List<String> optionsWithStub = new ArrayList<>(checkerOptions);
+        optionsWithStub.add("-Astubs="+ getFullPath(testFile, "reflection.astub"));
+        test(checkerName, optionsWithStub,testFile);
+    }
+
+    protected String getFullPath(final File javaFile, final String filename) {
+        final String dirname = javaFile.getParentFile().getAbsolutePath();
+        return dirname + System.getProperty("file.separator")+filename;
+    }
 }
