6b8df14a77a63447f81fe0c63c23e80d963a6c18
jonathangburke <jonathangburke@gmail.com>
2014-03-25 17:19:28 -0700

    Merge Javier's doc changes with tip

--- javacutil/src/org/checkerframework/javacutil/Pair.java
+++ javacutil/src/org/checkerframework/javacutil/Pair.java
@@ -1,5 +1,10 @@
 package org.checkerframework.javacutil;
 
+/*>>>
+import org.checkerframework.dataflow.qual.Pure;
+import org.checkerframework.dataflow.qual.SideEffectFree;
+*/
+
 /**
  * Simple pair class for multiple returns.
  *
@@ -18,6 +23,7 @@ public class Pair<V1, V2> {
         return new Pair<V1, V2>(v1, v2);
     }
 
+    /*@SideEffectFree*/
     @Override
     public String toString() {
         return "Pair(" + first + ", " + second + ")";
@@ -25,6 +31,7 @@ public class Pair<V1, V2> {
 
     private int hashCode = -1;
 
+    /*@Pure*/
     @Override
     public int hashCode() {
         if (hashCode == -1) {
