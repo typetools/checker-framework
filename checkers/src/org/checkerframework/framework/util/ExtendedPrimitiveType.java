bce80910ebf5ef2d48093bc67b5053c01112a5df
Stuart Pernsteiner <spernste@cs.washington.edu>
2014-03-27 16:26:42 -0700

    actually merge qualifier framework into new project layout

--- checkers/src/org/checkerframework/framework/util/ExtendedPrimitiveType.java
+++
@@ -1,5 +0,0 @@
-package org.checkerframework.framework.util;
-
-/** {@link ExtendedTypeMirror} variant for {@link javax.lang.model.type.PrimitiveType}. */
-public interface ExtendedPrimitiveType extends ExtendedTypeMirror {
-}
