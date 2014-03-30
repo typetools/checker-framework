42a093bc3ec6f711e3310e02de876bf0c1c103b0
Werner Dietl <wdietl@gmail.com>
2014-03-30 12:40:03 -0400

    Fix Issue 308 by adding dummy attributes to @NotNull. Added a simple test to ensure aliasing continues to work.

--- checker/src/javax/validation/constraints/NotNull.java
+++ checker/src/javax/validation/constraints/NotNull.java
@@ -1,6 +1,10 @@
 package javax.validation.constraints;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 import org.checkerframework.framework.qual.TypeQualifier;
 
@@ -9,4 +13,11 @@ import org.checkerframework.framework.qual.TypeQualifier;
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 @TypeQualifier
 public @interface NotNull {
+    // The following annotation attributes are allowed in source code,
+    // but are ignored by the Nullness Checker.
+
+    Class<?>[]  groups() default {};
+    String      message() default "{javax.validation.constraints.NotNull.message}";
+    // To not depend on the Payload class, let us use a more flexible bound.
+    Class<?>[]  payload() default {};
 }
