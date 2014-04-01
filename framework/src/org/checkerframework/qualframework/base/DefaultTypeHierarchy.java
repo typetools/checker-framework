bc3f52b7c07553dca5686a749592ff109ccb75ea
Stuart Pernsteiner <spernste@cs.washington.edu>
2014-04-01 09:56:21 -0700

    rename arguments of subtyping checks

--- framework/src/org/checkerframework/qualframework/base/DefaultTypeHierarchy.java
+++ framework/src/org/checkerframework/qualframework/base/DefaultTypeHierarchy.java
@@ -24,40 +24,41 @@ public class DefaultTypeHierarchy<Q> implements TypeHierarchy<Q> {
 
 
     @Override
-    public boolean isSubtype(QualifiedTypeMirror<Q> a, QualifiedTypeMirror<Q> b) {
-        return adapter.superIsSubtype(a, b);
+    public boolean isSubtype(QualifiedTypeMirror<Q> subtype, QualifiedTypeMirror<Q> supertype) {
+        return adapter.superIsSubtype(subtype, supertype);
     }
 
     /**
-     * Checks that <code>a</code> is a subtype of <code>b</code>, as an array
+     * Checks that <code>subtype</code> is a subtype of <code>supertype</code>, as an array
      * component type.
      */
-    protected boolean isSubtypeAsArrayComponent(QualifiedTypeMirror<Q> a, QualifiedTypeMirror<Q> b) {
-        return adapter.superIsSubtypeAsArrayComponent(a,b);
+    protected boolean isSubtypeAsArrayComponent(QualifiedTypeMirror<Q> subtype, QualifiedTypeMirror<Q> supertype) {
+        return adapter.superIsSubtypeAsArrayComponent(subtype, supertype);
     }
 
     /**
-     * Checks that <code>a</code> is a subtype of <code>b</code>, as actual
+     * Checks that <code>subtype</code> is a subtype of <code>supertype</code>, as actual
      * type arguments.
      */
-    protected boolean isSubtypeAsTypeArgument(QualifiedTypeMirror<Q> a, QualifiedTypeMirror<Q> b) {
-        return adapter.superIsSubtypeAsTypeArgument(a, b);
+    protected boolean isSubtypeAsTypeArgument(QualifiedTypeMirror<Q> subtype, QualifiedTypeMirror<Q> supertype) {
+        return adapter.superIsSubtypeAsTypeArgument(subtype, supertype);
     }
 
     /**
-     * Checks that <code>a</code> is a subtype of <code>b</code>.
+     * Checks that <code>subtype</code> is a subtype of <code>supertype</code>.
      */
-    final protected boolean isSubtypeImpl(QualifiedTypeMirror<Q> a, QualifiedTypeMirror<Q> b) {
-        return adapter.superIsSubtypeImpl(a, b);
+    final protected boolean isSubtypeImpl(QualifiedTypeMirror<Q> subtype, QualifiedTypeMirror<Q> supertype) {
+        return adapter.superIsSubtypeImpl(subtype, supertype);
     }
 
     /**
-     * Checks that <code>a</code> is a subtype of <code>b</code>, with respect
-     * to type arguments only.   Returns true if any of the provided types is
-     * not a parameterized type.  A parameterized type, rhs, is a subtype of
-     * another, lhs, only if their actual type parameters are invariant. 
+     * Checks that <code>subtype</code> is a subtype of <code>supertype</code>,
+     * with respect to type arguments only.   Returns true if any of the
+     * provided types is not a parameterized type.  A parameterized
+     * type, rhs, is a subtype of another, lhs, only if their actual type
+     * parameters are invariant. 
      */
-    protected boolean isSubtypeTypeArguments(QualifiedDeclaredType<Q> a, QualifiedDeclaredType<Q> b) {
-        return adapter.superIsSubtypeTypeArguments(a, b);
+    protected boolean isSubtypeTypeArguments(QualifiedDeclaredType<Q> subtype, QualifiedDeclaredType<Q> supertype) {
+        return adapter.superIsSubtypeTypeArguments(subtype, supertype);
     }
 }
