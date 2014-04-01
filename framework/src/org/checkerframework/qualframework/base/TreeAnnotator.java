072d3c5f16358b18b4db0f881b695658c40dac33
Stuart Pernsteiner <spernste@cs.washington.edu>
2014-04-01 16:48:57 -0700

    add missing methods to TreeAnnotator and TreeAnnotatorAdapter

--- framework/src/org/checkerframework/qualframework/base/TreeAnnotator.java
+++ framework/src/org/checkerframework/qualframework/base/TreeAnnotator.java
@@ -2,10 +2,8 @@ package org.checkerframework.qualframework.base;
 
 import javax.lang.model.type.TypeMirror;
 
-import com.sun.source.tree.BinaryTree;
-import com.sun.source.tree.LiteralTree;
 import com.sun.source.tree.Tree;
-import com.sun.source.tree.TreeVisitor;
+import com.sun.source.util.SimpleTreeVisitor;
 
 import org.checkerframework.framework.type.AnnotatedTypeMirror;
 
@@ -16,12 +14,8 @@ import org.checkerframework.qualframework.util.ExtendedTypeMirror;
  * {@link DefaultQualifiedTypeFactory} component for computing the qualified
  * type of a {@link Tree}.
  */
-public class TreeAnnotator<Q> {
-    public QualifiedTypeMirror<Q> visitBinary(BinaryTree node, ExtendedTypeMirror type) {
-        return null;
-    }
-
-    public QualifiedTypeMirror<Q> visitLiteral(LiteralTree node, ExtendedTypeMirror type) {
+public class TreeAnnotator<Q> extends SimpleTreeVisitor<QualifiedTypeMirror<Q>, ExtendedTypeMirror> {
+    public QualifiedTypeMirror<Q> defaultAction(Tree node, ExtendedTypeMirror type) {
         return null;
     }
 }
