6fab06590d3cfb6ad6bc965a13441b78e7650b1a
Stuart Pernsteiner <spernste@cs.washington.edu>
2014-04-29 11:26:22 -0700

    remove old files from checkers/

--- checkers/src/org/checkerframework/checkers/tainting/annotations/Untainted.java
+++
@@ -1,7 +0,0 @@
-package org.checkerframework.checkers.tainting.annotations;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
-public @interface Untainted { }
