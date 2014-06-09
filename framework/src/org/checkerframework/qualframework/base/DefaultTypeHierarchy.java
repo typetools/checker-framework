e163b7c9531f3f83063361853c4292713604b6bc
Stuart Pernsteiner <spernste@cs.washington.edu>
2014-06-09 10:52:19 -0700

    fix subtyping checks involving null

--- framework/src/org/checkerframework/qualframework/base/DefaultTypeHierarchy.java
+++ framework/src/org/checkerframework/qualframework/base/DefaultTypeHierarchy.java
@@ -1,5 +1,7 @@
 package org.checkerframework.qualframework.base;
 
+import javax.lang.model.type.TypeKind;
+
 import org.checkerframework.qualframework.base.QualifiedTypeMirror.QualifiedDeclaredType;
 
 /** Default implementation of {@link TypeHierarchy}.  Applies standard Java
@@ -25,6 +27,21 @@ public class DefaultTypeHierarchy<Q> implements TypeHierarchy<Q> {
 
     @Override
     public boolean isSubtype(QualifiedTypeMirror<Q> subtype, QualifiedTypeMirror<Q> supertype) {
+        // Qualifier API doesn't allow qualifiers on NullType, but the
+        // underlying framework does (and expects the annotations to match).
+        // So we have this extra check to make NullType a subtype of all
+        // reference types.
+        if (subtype.getKind() == TypeKind.NULL) {
+            TypeKind superKind = supertype.getKind();
+            switch (supertype.getKind()) {
+                case ARRAY:
+                case DECLARED:
+                case NULL:
+                case TYPEVAR:
+                    return true;
+            }
+        }
+
         return adapter.superIsSubtype(subtype, supertype);
     }
 
