272fa46e5b8393e9be80e4c5644f9507a48a49ff
Stuart Pernsteiner <spernste@cs.washington.edu>
2014-06-10 11:58:56 -0700

    make QTM constructors package-private

--- checker/src/org/checkerframework/checker/tainting/TaintingQualifiedTypeFactory.java
+++ checker/src/org/checkerframework/checker/tainting/TaintingQualifiedTypeFactory.java
@@ -10,6 +10,7 @@ import org.checkerframework.qualframework.base.DefaultQualifiedTypeFactory;
 import org.checkerframework.qualframework.base.QualifierHierarchy;
 import org.checkerframework.qualframework.base.QualifiedTypeMirror;
 import org.checkerframework.qualframework.base.QualifiedTypeMirror.QualifiedDeclaredType;
+import org.checkerframework.qualframework.base.SetQualifierVisitor;
 import org.checkerframework.qualframework.base.TreeAnnotator;
 import org.checkerframework.qualframework.util.ExtendedTypeMirror;
 
@@ -29,12 +30,11 @@ public class TaintingQualifiedTypeFactory extends DefaultQualifiedTypeFactory<Ta
         return new TreeAnnotator<Tainting>() {
             @Override
             public QualifiedTypeMirror<Tainting> visitLiteral(LiteralTree tree, ExtendedTypeMirror type) {
+                QualifiedTypeMirror<Tainting> result = super.visitLiteral(tree, type);
                 if (tree.getKind() == Tree.Kind.STRING_LITERAL) {
-                    return new QualifiedDeclaredType<Tainting>(
-                            type, Tainting.UNTAINTED, new ArrayList<>());
-                } else {
-                    return super.visitLiteral(tree, type);
+                    result = SetQualifierVisitor.apply(result, Tainting.UNTAINTED);
                 }
+                return result;
             }
         };
     }
