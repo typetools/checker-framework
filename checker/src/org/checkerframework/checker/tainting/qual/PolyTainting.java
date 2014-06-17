68a817c7836c3fc1e906670864949326ed69000d
Stuart Pernsteiner <spernste@cs.washington.edu>
2014-06-17 16:13:22 -0700

    add @PolyTainting

---
+++ checker/src/org/checkerframework/checker/tainting/qual/PolyTainting.java
@@ -0,0 +1,10 @@
+package org.checkerframework.checker.tainting.qual;
+
+import java.lang.annotation.*;
+
+import org.checkerframework.checker.tainting.TaintingChecker;
+import org.checkerframework.framework.qual.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+public @interface PolyTainting {}
