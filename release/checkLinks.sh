127983ca3740be474f2c5ccd5b805f1d0b35affc
Michael Ernst <mernst@cs.washington.edu>
2014-07-05 13:44:30 -0700

    Update use of checklink-args.txt

--- release/checkLinks.sh
+++ release/checkLinks.sh
@@ -3,4 +3,4 @@
 export PLUME_BIN=/homes/gws/mernst/bin/share-plume/
 export PERL5LIB=/homes/gws/mernst/bin/src/perl:/homes/gws/mernst/bin/src/perl/share/perl5:/homes/gws/mernst/bin/src/perl/lib64/perl5:/homes/gws/mernst/research/steering/colony-2003/experiment-scripts:/usr/share/perl5/:/homes/gws/jburke/perl_lib 
 
-${PLUME_BIN}/checklink -q -r -e `cat ${PLUME_BIN}/checklink-args.txt` $1
+${PLUME_BIN}/checklink -q -r -e `grep -v '^#' ${PLUME_BIN}/checklink-args.txt` $1
