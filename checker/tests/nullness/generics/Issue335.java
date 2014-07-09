c6d737fe7e85915291b14171ab2adbdecfe61f84
Werner Dietl <wdietl@gmail.com>
2014-07-08 22:03:54 -0400

    Some renamings to make debugging easier. Whitespace fixes.

--- checker/tests/nullness/generics/Issue335.java
+++ checker/tests/nullness/generics/Issue335.java
@@ -1,22 +1,22 @@
 // Test case for Issue 335:
 // https://code.google.com/p/checker-framework/issues/detail?id=335
-//@skip-test
+
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 class Pair<A, B> {
-  public static <A, B> Pair<A, B> of(@Nullable A first, @Nullable B second) {
-    throw new RuntimeException();
-  }
+    static <C, D> Pair<C, D> of(@Nullable C first, @Nullable D second) {
+        throw new RuntimeException();
+    }
 }
 
-class Optional<T> {
-  public static <T> Optional<T> of(T reference) {
-    throw new RuntimeException();
-  }
+class Optional<S> {
+    static <T> Optional<T> of(T reference) {
+        throw new RuntimeException();
+    }
 }
 
 class Issue335 {
-  Optional<Pair<String, String>> m(String one, String two) {
-    return Optional.of(Pair.of(one, two));
-  }
+    Optional<Pair<String, String>> m(String one, String two) {
+        return Optional.of(Pair.of(one, two));
+    }
 }
