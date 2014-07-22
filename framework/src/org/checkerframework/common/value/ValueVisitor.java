af3d3d807af2d99ed463b3642a8563d7a4f5f934
plvines <plvines@localhost>
2014-07-22 15:50:51 -0700

    added error for writing an annotation with too many values. Added test case for this error in Basics.java

--- framework/src/org/checkerframework/common/value/ValueVisitor.java
+++ framework/src/org/checkerframework/common/value/ValueVisitor.java
@@ -3,6 +3,24 @@ package org.checkerframework.common.value;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.common.basetype.BaseTypeVisitor;
 
+import com.sun.source.tree.Tree;
+import javax.lang.model.element.AnnotationMirror;
+import org.checkerframework.framework.source.Result;
+import com.sun.source.tree.AnnotationTree;
+import org.checkerframework.common.value.qual.ArrayLen;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.NewArrayTree;
+import org.checkerframework.common.value.qual.BoolVal;
+import com.sun.source.tree.IdentifierTree;
+import com.sun.source.tree.AssignmentTree;
+import org.checkerframework.common.value.qual.BottomVal;
+import org.checkerframework.common.value.qual.DoubleVal;
+import org.checkerframework.common.value.qual.IntVal;
+import org.checkerframework.common.value.qual.StringVal;
+import org.checkerframework.common.value.qual.UnknownVal;
+
+import org.checkerframework.javacutil.AnnotationUtils;
+
 /**
  * @author plvines
  * 
@@ -18,6 +36,29 @@ public class ValueVisitor extends BaseTypeVisitor<ValueAnnotatedTypeFactory> {
     @Override
     protected ValueAnnotatedTypeFactory createTypeFactory() {
         return new ValueAnnotatedTypeFactory(checker);
+    }
+
+    @Override
+    public Void visitAnnotation(AnnotationTree tree, Void p){
+        
+        AnnotationMirror newAnno = AnnotationUtils.fromName(elements, "org.checkerframework.common.value.qual." + ((IdentifierTree)tree.getAnnotationType()).getName().toString());
+
+        if (newAnno != null){
+            for (AnnotationMirror anno : atypeFactory.constantAnnotations){
+                if (AnnotationUtils.areSameIgnoringValues(newAnno, anno)){
+                    if (tree.getArguments().size() > 0 && tree.getArguments().get(0).getKind() == Tree.Kind.ASSIGNMENT && ((AssignmentTree)tree.getArguments().get(0)).getExpression().getKind() == Tree.Kind.NEW_ARRAY){
+                        int numArgs = ((NewArrayTree)((AssignmentTree)tree.getArguments().get(0)).getExpression()).getInitializers().size();
+                        
+                        if (numArgs > ValueAnnotatedTypeFactory.MAX_VALUES){
+                            checker.report(Result.failure("too.many.values.given", ValueAnnotatedTypeFactory.MAX_VALUES), this.getCurrentPath().getLeaf());
+                            return null;
+                        }
+                    }
+                }
+            }
+        }
+         
+        return super.visitAnnotation(tree, p);
     }
 
 }
