b16c941a87458fb62a2b1bc7269e92d0634ca2ec
Michael Ernst <mernst@cs.washington.edu>
2014-08-07 08:26:27 +0200

    Add tests for single-argument asRegex/isRegex; other small test improvements

--- checker/tests/regex/RegexUtilTest.java
+++ checker/tests/regex/RegexUtilTest.java
@@ -1,18 +1,37 @@
 import org.checkerframework.checker.regex.qual.Regex;
+import org.checkerframework.framework.qual.EnsuresQualifierIf;
 
 public class RegexUtilTest {
-    void packagedRegexUtil(String s) {
+    void fullyQualifiedRegexUtil(String s) {
         if (org.checkerframework.checker.regex.RegexUtil.isRegex(s, 2)) {
             @Regex(2) String s2 = s;
         }
         @Regex(2) String s2 = org.checkerframework.checker.regex.RegexUtil.asRegex(s, 2);
     }
 
-    void noPackageRegexUtil(String s) {
+    void unqualifiedRegexUtil(String s) {
         if (RegexUtil.isRegex(s, 2)) {
             @Regex(2) String s2 = s;
         }
         @Regex(2) String s2 = RegexUtil.asRegex(s, 2);
+    }
+
+    void fullyQualifiedRegexUtilNoParamsArg(String s) {
+        if (org.checkerframework.checker.regex.RegexUtil.isRegex(s)) {
+            @Regex String s2 = s;
+            @Regex(0) String s3 = s;
+        }
+        @Regex String s2 = org.checkerframework.checker.regex.RegexUtil.asRegex(s);
+        @Regex(0) String s3 = org.checkerframework.checker.regex.RegexUtil.asRegex(s);
+    }
+
+    void unqualifiedRegexUtilNoParamsArg(String s) {
+        if (RegexUtil.isRegex(s)) {
+            @Regex String s2 = s;
+            @Regex(0) String s3 = s;
+        }
+        @Regex String s2 = RegexUtil.asRegex(s, 2);
+        @Regex(0) String s3 = RegexUtil.asRegex(s, 2);
     }
 
     void illegalName(String s) {
@@ -32,15 +51,26 @@ public class RegexUtilTest {
         //:: error: (assignment.type.incompatible)
         @Regex(2) String s2 = IllegalNameRegexUtil.asRegex(s, 2);
     }
+
 }
 
 // A dummy RegexUtil class to make sure RegexUtil in no package works.
 class RegexUtil {
-    public static boolean isRegex(String s, int n) {
+    @EnsuresQualifierIf(result=true, expression="#1", qualifier=Regex.class)
+    public static boolean isRegex(final String s, int n) {
         return false;
     }
 
     public static @Regex String asRegex(String s, int n) {
+        return null;
+    }
+
+    @EnsuresQualifierIf(result=true, expression="#1", qualifier=Regex.class)
+    public static boolean isRegex(final String s) {
+        return false;
+    }
+
+    public static @Regex String asRegex(String s) {
         return null;
     }
 }
