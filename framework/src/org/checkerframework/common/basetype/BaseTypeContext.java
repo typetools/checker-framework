dec3a5e79201e47dd8f92771f6b8beb21fa5268f
Werner Dietl <wdietl@gmail.com>
2014-08-15 06:07:17 -0400

    Remove comment that was fixed by recent jsr308-langtools changes. Add the methods with refined return type.

--- framework/src/org/checkerframework/common/basetype/BaseTypeContext.java
+++ framework/src/org/checkerframework/common/basetype/BaseTypeContext.java
@@ -6,16 +6,11 @@ import org.checkerframework.framework.util.CFContext;
 /** An extension of {@link CFContext} that includes {@link
  * BaseTypeChecker}-specific components. */
 public interface BaseTypeContext extends CFContext {
-    // TODO: Using covariant return types in interfaces under `javac -source
-    // 1.8` (the default) generates bridge methods in the interface itself
-    // (instead of generating them only in implementing classes), using Java
-    // 8's new "default method" functionality.  The generated class files are
-    // thus incompatible with the Java 7 runtime.  Compiling with `javac
-    // -source 1.7` doesn't generate these default methods, but I'm not sure
-    // how to modify the build.xml to make that happen.
+    @Override
+    BaseTypeChecker getChecker();
 
-    //BaseTypeChecker getChecker();
-    //BaseTypeVisitor<?> getVisitor();
+    @Override
+    BaseTypeVisitor<?> getVisitor();
 
     GenericAnnotatedTypeFactory<?, ?, ?, ?> getTypeFactory();
 }
