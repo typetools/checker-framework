d5403b34138465c296e87b07b637b09bef53981f
jonathangburke <jonathangburke@gmail.com>
2014-09-10 11:01:44 -0700

    Add text for recursive bounds where one type variable extends another

---
+++ framework/tests/all-systems/GenericExtendsTypeVars.java
@@ -0,0 +1,23 @@
+/*
+ * Sets up recursive bounds where the bounds themselves are type variables.
+ */
+
+interface List<EE> {}
+interface Map<KEY, VALUE> {}
+
+class Tester<EE extends TT, TT extends List<EE>> {
+
+}
+
+class WithWildcard<ZZ extends QQ, QQ extends YY, YY extends Map<QQ, ZZ>> {
+    void context() {
+        ZZ zz = null;
+        QQ qq = null;
+        YY yy = null;
+    }
+}
+
+class Test<KK extends FF, FF extends Map<KK, KK>> {
+    KK kk;
+    FF ff;
+}
