6b0b9cc92916fe7b018f6c2e9c131e94996e577a
jonathangburke <jonathangburke@gmail.com>
2014-10-01 17:14:11 -0700

    Fix makeGroundTargetType and related test

--- checker/tests/nullness/java8/methodref/GroundTargetTypeLub.java
+++ checker/tests/nullness/java8/methodref/GroundTargetTypeLub.java
@@ -5,9 +5,20 @@ interface Supplier <T extends @NonNull Object> {
     T supply();
 }
 
+interface Supplier2<T extends @Nullable Object> {
+    T supply();
+}
+
 class GroundTargetType {
-    //:: error: (methodref.return.invalid) :: error: (type.argument.type.incompatible)
-    Supplier<? extends @Nullable Object> fn = GroundTargetType::myMethod;
 
     static @Nullable Object myMethod() { return null; }
+
+    //:: error: (type.argument.type.incompatible)
+    Supplier<? extends @Nullable Object> fn = GroundTargetType::myMethod;
+    //:: error: (methodref.return.invalid)
+    Supplier<? extends @NonNull Object> fn2 = GroundTargetType::myMethod;
+
+    //Supplier2
+    //:: error: (methodref.return.invalid)
+    Supplier2<? extends @NonNull Object> fn3 = GroundTargetType::myMethod;
 }
