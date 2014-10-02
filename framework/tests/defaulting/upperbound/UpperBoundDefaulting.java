60ae24c430a0d092f0434d9a0bb27976e4eb7508
jonathangburke <jonathangburke@gmail.com>
2014-10-01 17:14:57 -0700

    Update tests for defaulting fixes and checkTypeArgs fixes

--- framework/tests/defaulting/upperbound/UpperBoundDefaulting.java
+++ framework/tests/defaulting/upperbound/UpperBoundDefaulting.java
@@ -18,4 +18,43 @@ public class UpperBoundDefaulting {
         //:: error: (assignment.type.incompatible) :: error: (type.argument.type.incompatible)
         MyArrayList<@UB_BOTTOM ? extends @UB_IMPLICIT Object> eubImplicitToBottom =  new MyArrayList<UAL>();
     }
+
+    public void implicitsWildcard(MyArrayList<?> myArrayList) {
+
+        //should fail because @LB_IMPLICIT is below @UB_TOP
+        //:: error: (type.argument.type.incompatible)
+        @UB_TOP MyArrayList<@UB_BOTTOM ? extends @UB_TOP String> iwLowerBoundIncompatible = myArrayList;
+
+        @UB_TOP MyArrayList<@UB_BOTTOM ? extends @UB_EXPLICIT String> iwLowerBoundCompatible = myArrayList;
+
+        //:: error: (assignment.type.incompatible) :: error: (type.argument.type.incompatible)
+        @UB_TOP MyArrayList<@UB_BOTTOM ? extends @UB_IMPLICIT String> iwLowerBoundStillCompatible = myArrayList;
+
+    }
+
+    public void implicitExtendBoundedWildcard(MyArrayList<? extends String> iebList) {
+
+        //should fail because @LB_IMPLICIT is below @UB_TOP
+        //:: error: (type.argument.type.incompatible)
+        @UB_TOP MyArrayList<@UB_BOTTOM ? extends @UB_TOP String> iebLowerBoundIncompatible = iebList;
+
+        MyArrayList<@UB_BOTTOM ? extends @UB_EXPLICIT Object> eubExplicitToBottom =  iebList;
+
+        //:: error: (assignment.type.incompatible) :: error: (type.argument.type.incompatible)
+        MyArrayList<@UB_BOTTOM ? extends @UB_IMPLICIT Object> eubImplicitToBottom =  iebList;
+    }
+
+    //:: error: (type.argument.type.incompatible) :: error: (type.argument.type.incompatible)
+    public void explicitLowerBoundedWildcard(MyArrayList<? super String> elbList) {
+        //should fail because @LB_EXPLICIT is below @UB_TOP
+        //:: error: (type.argument.type.incompatible)
+        @UB_TOP MyArrayList<@UB_TOP ? super @UB_BOTTOM String> iebLowerBoundIncompatible = elbList;
+
+        //:: error: (type.argument.type.incompatible)
+        @UB_TOP MyArrayList<@UB_IMPLICIT ? super @UB_BOTTOM String> iebLowerBoundStillIncompatible = elbList;
+
+        //:: error: (assignment.type.incompatible)
+        @UB_TOP MyArrayList<@UB_EXPLICIT ? super @UB_BOTTOM String> iebLowerBoundCompatible = elbList;
+
+    }
 }
