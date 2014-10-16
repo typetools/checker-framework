aaaab53cf58861f53d194430a415ce4dc292b359
David McArthur <mcarthur@cs.washington.edu>
2014-10-16 10:01:38 -0700

    Fixed up tests before turning off "Main" qualifier for all types.

--- checker/tests/tainting_qual_poly/ParamSimpleMethod.java
+++ checker/tests/tainting_qual_poly/ParamSimpleMethod.java
@@ -2,15 +2,18 @@
 import org.checkerframework.checker.experimental.tainting_qual_poly.qual.*;
 
 // Test method qual parameters
+@TaintingParam("Main")
+class A { }
+
 abstract class Test {
     @TaintingParam("Main")
-    static void test(@Var("Main") Integer i, @Var("Main") Integer j) { }
+    static void test(@Var("Main") A i, @Var("Main") A j) { }
 
-    abstract @Tainted Integer makeTainted();
-    abstract @Untainted Integer makeUntainted();
+    abstract @Tainted(target="Main") A makeTainted();
+    abstract @Untainted(target="Main") A makeUntainted();
 
-    abstract void takeTainted(@Tainted Integer o);
-    abstract void takeUntainted(@Untainted Integer o);
+    abstract void takeTainted(@Tainted(target="Main") A o);
+    abstract void takeUntainted(@Untainted(target="Main") A o);
 
     void test() {
         test(makeTainted(), makeTainted());
