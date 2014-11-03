0e7925d365343da09aed966778231dcfa9abd71b
David McArthur <mcarthur@cs.washington.edu>
2014-11-03 14:17:53 -0800

    Cleaned up imports for qualifiers.

--- checker/src/org/checkerframework/checker/experimental/tainting_qual_poly/qual/PolyTainting.java
+++ checker/src/org/checkerframework/checker/experimental/tainting_qual_poly/qual/PolyTainting.java
@@ -1,14 +1,16 @@
 package org.checkerframework.checker.experimental.tainting_qual_poly.qual;
 
-import java.lang.annotation.*;
+import org.checkerframework.qualframework.poly.SimpleQualifierParameterAnnotationConverter;
 
-import org.checkerframework.checker.experimental.tainting_qual_poly.TaintingAnnotationConverter;
-import org.checkerframework.checker.experimental.tainting_qual_poly.TaintingChecker;
-import org.checkerframework.framework.qual.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 @Repeatable(MultiPolyTainting.class)
 public @interface PolyTainting {
-    String param() default TaintingAnnotationConverter.PRIMARY_TARGET;
+    String param() default SimpleQualifierParameterAnnotationConverter.PRIMARY_TARGET;
 }
