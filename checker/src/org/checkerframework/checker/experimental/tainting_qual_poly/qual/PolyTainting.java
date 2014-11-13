4d563365d5bb3ee4fe27a4fd7cc357d7ca46cc00
David McArthur <mcarthur@cs.washington.edu>
2014-11-13 15:12:19 -0800

    Reverting some changes to the imports in Tainting type system.

--- checker/src/org/checkerframework/checker/experimental/tainting_qual_poly/qual/PolyTainting.java
+++ checker/src/org/checkerframework/checker/experimental/tainting_qual_poly/qual/PolyTainting.java
@@ -2,11 +2,9 @@ package org.checkerframework.checker.experimental.tainting_qual_poly.qual;
 
 import org.checkerframework.qualframework.poly.SimpleQualifierParameterAnnotationConverter;
 
+import java.lang.annotation.*;
+
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Repeatable;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
--- checker/src/org/checkerframework/checker/experimental/tainting_qual_poly/qual/Var.java
+++ checker/src/org/checkerframework/checker/experimental/tainting_qual_poly/qual/Var.java
@@ -3,11 +3,7 @@ package org.checkerframework.checker.experimental.tainting_qual_poly.qual;
 import org.checkerframework.qualframework.poly.SimpleQualifierParameterAnnotationConverter;
 import org.checkerframework.qualframework.poly.qual.Wildcard;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Repeatable;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
@@ -17,4 +13,3 @@ public @interface Var {
     String param() default SimpleQualifierParameterAnnotationConverter.PRIMARY_TARGET;
     Wildcard wildcard() default Wildcard.NONE;
 }
-
