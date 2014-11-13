628c4ae2f86576dcb89a892ed7a8c59a96494ab1
David McArthur <mcarthur@cs.washington.edu>
2014-11-13 15:12:19 -0800

    Reverting some changes to the imports in Tainting type system.

--- checker/src/org/checkerframework/checker/experimental/tainting_qual_poly/qual/Wild.java
+++ checker/src/org/checkerframework/checker/experimental/tainting_qual_poly/qual/Wild.java
@@ -1,10 +1,6 @@
 package org.checkerframework.checker.experimental.tainting_qual_poly.qual;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Repeatable;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
