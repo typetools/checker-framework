95895c20cb45d40a9d787e30eeb4ae875db74b5a
David McArthur <mcarthur@cs.washington.edu>
2014-11-17 12:48:53 -0800

    clenaup

--- checker/src/org/checkerframework/checker/experimental/regex_qual_poly/RegexTypecheckVisitor.java
+++ checker/src/org/checkerframework/checker/experimental/regex_qual_poly/RegexTypecheckVisitor.java
@@ -78,7 +78,7 @@ public class RegexTypecheckVisitor extends TypecheckVisitorAdapter<QualParams<Re
                 int annoGroups = 0;
                 QualifiedTypeMirror<QualParams<Regex>> receiverType = context.getTypeFactory().getQualifiedType(receiver);
                 Regex regex = receiverType.getQualifier().getPrimary().getMaximum();
-                if (!(regex instanceof Regex.RegexVal) || ((Regex.RegexVal) regex).getCount() < paramGroups) {
+                if (!regex.isRegexVal() || ((Regex.RegexVal) regex).getCount() < paramGroups) {
                     checker.report(Result.failure("group.count.invalid", paramGroups, annoGroups, receiver), group);
                 }
             } else {
