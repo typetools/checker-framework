7a26143dc293e13951e04838ba7d9e7b080a7260
jonathangburke <jonathangburke@gmail.com>
2014-11-21 11:33:25 -0800

    TypeFromTree readability improvements.  Move visitors to their own classes and create an understandable class signature

---
+++ framework/src/org/checkerframework/framework/type/TypeFromClassVisitor.java
@@ -0,0 +1,24 @@
+package org.checkerframework.framework.type;
+
+import com.sun.source.tree.ClassTree;
+import org.checkerframework.javacutil.TreeUtils;
+
+import javax.lang.model.element.TypeElement;
+
+/**
+ * Converts ClassTrees into AnnotatedDeclaredType.
+ * @see org.checkerframework.framework.type.TypeFromTree
+ */
+class TypeFromClassVisitor extends TypeFromTreeVisitor {
+
+    @Override
+    public AnnotatedTypeMirror visitClass(ClassTree node,
+                                          AnnotatedTypeFactory f) {
+        TypeElement elt = TreeUtils.elementFromDeclaration(node);
+        AnnotatedTypeMirror result = f.toAnnotatedType(elt.asType(), true);
+
+        ElementAnnotationApplier.apply(result, elt, f);
+
+        return result;
+    }
+}
