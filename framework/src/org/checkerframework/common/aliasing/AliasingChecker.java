3459252baaba12bebcd96cd50b59d9bae99fd143
Paulo Barros <pbsf@cs.washington.edu>
2014-12-09 19:04:02 -0300

    First implementation of the Aliasing Checker.

---
+++ framework/src/org/checkerframework/common/aliasing/AliasingChecker.java
@@ -0,0 +1,11 @@
+package org.checkerframework.common.aliasing;
+
+import org.checkerframework.common.basetype.BaseTypeChecker;
+
+/**
+ * Aliasing type system -- used to identify expressions that definitely have
+ * no aliases.
+ */
+
+public class AliasingChecker extends BaseTypeChecker {
+}
