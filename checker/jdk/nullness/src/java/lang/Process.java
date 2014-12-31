0aed12236463ba992ad46485b72f9ee6ecb88296
Suzanne Millstein <smillst@cs.washington.edu>
2014-12-31 10:44:19 -0800

    Put the annotation in the correct location for java.lang.Process

--- checker/jdk/nullness/src/java/lang/Process.java
+++ checker/jdk/nullness/src/java/lang/Process.java
@@ -5,9 +5,9 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 
 public abstract class Process{
   public Process() { throw new RuntimeException("skeleton method"); }
-  public abstract @Nullable java.io.OutputStream getOutputStream();
-  public abstract @Nullable java.io.InputStream getInputStream();
-  public abstract @Nullable java.io.InputStream getErrorStream();
+  public abstract java.io. @Nullable  OutputStream getOutputStream();
+  public abstract java.io. @Nullable InputStream getInputStream();
+  public abstract java.io. @Nullable  InputStream getErrorStream();
   public abstract int waitFor() throws InterruptedException;
   public abstract int exitValue();
   public abstract void destroy();
