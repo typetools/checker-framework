daa5a200921c2eef93b8ddc6dfdc5a24e7e522b5
jonathangburke <jonathangburke@gmail.com>
2015-01-20 12:22:53 -0800

    Fix error in which uninitialized type variables were acting as initialized type variables.

--- checker/tests/nullness/generics/Issue314.java
+++ checker/tests/nullness/generics/Issue314.java
@@ -21,7 +21,9 @@ class Issue314 {
         @NonNull S f2;
 
         {
+            //:: error: (assignment.type.incompatible)
             f1 = f2;
+            //:: error: (assignment.type.incompatible)
             f2 = f1;
         }
     }
