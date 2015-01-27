1bfeb445f6b8510bd89af414cca70e4566d3fbf1
David McArthur <mcarthur@cs.washington.edu>
2015-01-26 16:06:31 -0800

    Skipping javari for new testcase.

--- framework/tests/all-systems/Issue392.java
+++ framework/tests/all-systems/Issue392.java
@@ -1,6 +1,7 @@
 // Test case for Issue 392:
 // https://code.google.com/p/checker-framework/issues/detail?id=392
 
+@SuppressWarnings("javari")
 public class Issue392<T> {
 
     public <T> void getFields(T t) {
