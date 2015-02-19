a9acc52f2ca39ce54c2fa0d4d78872320017cb23
Javier Thaine <jthaine@cs.washington.edu>
2015-02-18 18:23:52 -0800

    Map Key Checker canonicalization visitor Ensures that canonicalization of @KeyFor expressions within type arguments is handled recursively.

--- checker/tests/nullness/MissingBoundAnnotations.java
+++ checker/tests/nullness/MissingBoundAnnotations.java
@@ -7,7 +7,7 @@ public final class MissingBoundAnnotations {
   // Test that the upper bound "Comparable<...>" receives an @Unqualified annotation.
   // Otherwise, it fails the check for empty annotations in GraphQualifierHierarchy.
   public static <K extends Comparable<? super K>,V> Collection<@KeyFor("#1") K> sortedKeySet(Map<K,V> m) {
-    ArrayList<@KeyFor("#1") K> theKeys = new ArrayList<@KeyFor("#1") K> (m.keySet());
+    ArrayList<@KeyFor("m") K> theKeys = new ArrayList<@KeyFor("m") K> (m.keySet());
     Collections.sort (theKeys);
     return theKeys;
   }
