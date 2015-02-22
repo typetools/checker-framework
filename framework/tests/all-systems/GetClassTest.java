12b12e529997ac5d010acac70b9e1d1102024c08
jonathangburke <jonathangburke@gmail.com>
2015-02-21 21:35:41 -0800

    Suppress immutability related test errors

--- framework/tests/all-systems/GetClassTest.java
+++ framework/tests/all-systems/GetClassTest.java
@@ -1,6 +1,9 @@
 class GetClassTest {
 
     //See AnntoatedTypeFactory.adaptGetClassReturnTypeToReceiver
+    @SuppressWarnings("javari") //I believe the issue is with Javari's postTreeAnnotator
+                                //normal type systems propagate the annotations from T in the
+                                // declaration of Class<T> but Javari does NOT
 	void context() {
 		Integer i = 4;
 		Class<?> a = i.getClass();
--- framework/tests/all-systems/Issue393.java
+++ framework/tests/all-systems/Issue393.java
@@ -1,6 +1,7 @@
 // Test case for Issue 393
 // http://code.google.com/p/checker-framework/issues/detail?id=393
 
+@SuppressWarnings("oigj") //This may be from improper annotations from the PostTreeAnnotator
 abstract class TypeVarTaintCheck {
 
     void test() {
