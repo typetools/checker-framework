30b8c512888e83fc6496994a50f3126e92444a75
Werner Dietl <wdietl@gmail.com>
2015-02-23 00:43:06 -0500

    Fix Issue 401 by adding yet another set of Android nullness annotations.

---
+++ checker/src/android/annotation/Nullable.java
@@ -0,0 +1,11 @@
+package android.annotation;
+
+import java.lang.annotation.*;
+
+import org.checkerframework.framework.qual.TypeQualifier;
+
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+@TypeQualifier
+public @interface Nullable {
+}
