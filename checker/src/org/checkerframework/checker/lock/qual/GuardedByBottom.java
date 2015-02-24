afd700547ca265eea2b010abf8b0fac0ceaee833
Javier Thaine <jthaine@cs.washington.edu>
2015-02-24 12:06:36 -0800

    Fix Javadoc errors

--- checker/src/org/checkerframework/checker/lock/qual/GuardedByBottom.java
+++ checker/src/org/checkerframework/checker/lock/qual/GuardedByBottom.java
@@ -13,7 +13,7 @@ import org.checkerframework.framework.qual.TypeQualifier;
  * This annotation may not be written in source code; it is an
  * implementation detail of the checker.
  *
- * @checker_framework_manual #lock-checker Lock Checker
+ * @checker_framework.manual #lock-checker Lock Checker
  */
 @TypeQualifier
 @InvisibleQualifier
