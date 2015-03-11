ee8885c39fed9ea15061e71621a3177b1da7c3d5
Suzanne Millstein <smillst@cs.washington.edu>
2015-03-11 12:41:49 -0700

    Fixed static method evaluation.

---
+++ framework/tests/value/StaticExTest.java
@@ -0,0 +1,58 @@
+import org.checkerframework.common.value.qual.BoolVal;
+import org.checkerframework.common.value.qual.DoubleVal;
+import org.checkerframework.common.value.qual.IntVal;
+import org.checkerframework.common.value.qual.StaticallyExecutable;
+import org.checkerframework.common.value.qual.StringVal;
+
+
+
+public class StaticExTest {
+    boolean flag;
+
+    void test1() {
+        String s = "helloworlod";
+        @StringVal({"o","l"}) String subString = flag?"o":"l";
+        @IntVal({5,0,9}) int start = flag?9:flag?5:0;
+        //flag?1:flag?6:
+        @IntVal({-1, 8, 9, 2, 4, 6}) int result = s.indexOf(subString, start);
+    }
+    
+    void test2() {
+        String s = flag?"helloworlod":"lololxxolxxxol";
+        @StringVal({"o","l"}) String subString = flag?"o":"l";
+        @IntVal({0,9}) int start = flag?9:0;
+        //flag?1:flag?6:
+        @IntVal({-1, 0, 1, 2, 4, 9, 12, 13}) int result3 = s.indexOf(subString, start);
+    }
+//
+//    void test3(){
+//        @IntVal({0,1}) int offset = flag? 0 : 1;
+//        char[] data = {'h','e','l','l','o','b','y','e','t','o'};
+//        @IntVal({5,6}) int charCount = flag?5:6;
+//        @StringVal({ "hello", "ellob", "hellob", "elloby" })
+//        String s = new String(data, offset, charCount);
+//        System.out.println(s);
+//    }
+//    void test4(){
+//        @IntVal({0,1})  int offset = flag? 0 : 1;
+//        char[] data1 =  {'h','e','l','l','o','b','y','e','t','o'};
+//        char[] data2 = {'a','b','c','d','e','f','g','h','i','j'};        
+//        char @StringVal({"hellobyeto","abcdefghij"})[] data = flag?data1: data2;
+//        @IntVal({5,6}) int charCount = flag?5:6;
+//        @StringVal({ "hello", "ellob", "hellob", "elloby", "abcde", "bcdef",
+//                "abcdef", "bcdefg" })
+//        String s = new String(data, offset, charCount);
+//        System.out.println(s);
+//    }
+    
+    void test5(){
+        Double a = new Double(0.0);
+        if (flag) {
+            a = 2.0;
+        }
+        @DoubleVal({ 0, 2 }) Double b = a;  
+        @DoubleVal({ 0, 2 }) double b2 = a;        
+
+    }
+   
+}
