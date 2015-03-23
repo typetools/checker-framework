4bb91588de4412f3d83de6c7e9e5e71c71118f8d
Werner Dietl <wdietl@gmail.com>
2015-03-23 19:18:41 -0400

    Don't use annotations-in-comments in test cases.

--- checker/tests/nullness/RefineArray.java
+++ checker/tests/nullness/RefineArray.java
@@ -2,7 +2,7 @@
  
  //TODO: Add as test
  class RefineArray {
-     public static <T> T[] concat(T /*@Nullable*/ [] a, T /*@Nullable*/ [] b) {
+     public static <T> T[] concat(T @Nullable [] a, T @Nullable [] b) {
          if (a == null) {
              if (b != null) {
                  return b;
@@ -16,7 +16,7 @@
                  return a;
              } else {
                  @SuppressWarnings("unchecked")
-                 T[] result = (T[]) new /*@MonotonicNonNull*/ Object[a.length + b.length];
+                 T[] result = (T[]) new @MonotonicNonNull Object[a.length + b.length];
 
                  System.arraycopy(a, 0, result, 0, a.length);
                  System.arraycopy(b, 0, result, a.length, b.length);
