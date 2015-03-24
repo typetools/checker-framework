504905b2a4709b31eeaba22e8f0ff34913481df4
Werner Dietl <wdietl@gmail.com>
2015-03-23 20:31:06 -0400

    Add a mechanism to skip tests if executed with a JDK version lower than Java 8. Use this to make the Issue403 test be skipped on Java 7.

--- framework/src/org/checkerframework/framework/test/TestUtilities.java
+++ framework/src/org/checkerframework/framework/test/TestUtilities.java
@@ -47,7 +47,8 @@ public final class TestUtilities {
         while (in.hasNext()) {
             String nextLine = in.nextLine();
             if (nextLine.contains("@skip-test") ||
-                    (!isJSR308Compiler && nextLine.contains("@non-308-skip-test"))) {
+                    (!isJSR308Compiler && nextLine.contains("@non-308-skip-test")) ||
+                    (!isAtLeast8Jvm && nextLine.contains("@below-java8-jdk-skip-test"))) {
                 in.close();
                 return false;
             }
@@ -62,6 +63,11 @@ public final class TestUtilities {
         OutputStream err = new ByteArrayOutputStream();
         compiler.run(null, null, err, "-version");
         isJSR308Compiler = err.toString().contains("jsr308");
+    }
+
+    public static final boolean isAtLeast8Jvm;
+    static {
+        isAtLeast8Jvm = org.checkerframework.framework.util.PluginUtil.getJreVersion() >= 1.8d;
     }
 
     /**
