ad2c5dfd106a4c06b96da95aa0b48e8d658d8eef
Werner Dietl <wdietl@gmail.com>
2015-03-25 18:36:02 -0400

    Fix Issue 417 by not checking assert statements if we assume they are disabled.

--- checker/tests/nullness-assumeassertions/TestAssumeAssertionsAreDisabled.java
+++ checker/tests/nullness-assumeassertions/TestAssumeAssertionsAreDisabled.java
@@ -2,7 +2,7 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 
 class TestAssumeAssertionsAreDisabled {
 
-  void foo(/*@Nullable*/ String s1, /*@Nullable*/ String s2) {
+  void foo(@Nullable String s1, @Nullable String s2) {
 
     // If assertions are disabled, then this cannot throw a NullPointerException
     assert s2.equals(s1);
