7a3ed78e62082c45cf89187475e0e2a12d1aa7f2
Werner Dietl <wdietl@gmail.com>
2015-03-25 18:36:02 -0400

    Fix Issue 417 by not checking assert statements if we assume they are disabled.

--- checker/tests/src/tests/NullnessAssumeAssertionsAreDisabled.java
+++ checker/tests/src/tests/NullnessAssumeAssertionsAreDisabled.java
@@ -1,14 +1,14 @@
 package tests;
 
+import org.checkerframework.framework.test.ParameterizedCheckerTest;
+
 import java.io.File;
 import java.util.Collection;
 
-import org.checkerframework.checker.nullness.AbstractNullnessChecker;
-import org.checkerframework.framework.test.ParameterizedCheckerTest;
 import org.junit.runners.Parameterized.Parameters;
 
 /**
- * JUnit tests for the Nullness checker (that uses the Freedom Before Commitment
+ * JUnit tests for the Nullness Checker (that uses the Freedom Before Commitment
  * type system for initialization).
  */
 public class NullnessAssumeAssertionsAreDisabled extends ParameterizedCheckerTest {
