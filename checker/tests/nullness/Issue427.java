87f7e974de583fea83efb120708504a457da749f
Michael Ernst <mernst@cs.washington.edu>
2015-04-10 20:39:15 -0300

    Add missing at-sign to assertion message

--- checker/tests/nullness/Issue427.java
+++ checker/tests/nullness/Issue427.java
@@ -10,12 +10,12 @@ import java.util.*;
 public class Issue427 {
 
   public static void assumeAssertionKeyFor1(String var, Map<String, Integer> m) {
-    assert m.containsKey(var) : "AssumeAssertion(keyfor): keys of leaders and timeKilled are the same";
+    assert m.containsKey(var) : "@AssumeAssertion(keyfor): keys of leaders and timeKilled are the same";
     boolean b = (m.get(var) >= 22);
   }
 
   public static void assumeAssertionKeyFor2(String var, Map<String, Integer> m) {
-    assert m.containsKey(var) : "AssumeAssertion(keyfor): keys of leaders and timeKilled are the same";
+    assert m.containsKey(var) : "@AssumeAssertion(keyfor): keys of leaders and timeKilled are the same";
     int x = m.get(var);
   }
 
