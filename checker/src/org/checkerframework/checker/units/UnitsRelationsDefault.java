62c2b9f49f26e2ba4c250c06e2d73aea734d5629
Werner Dietl <wdietl@gmail.com>
2015-04-13 15:47:36 -0400

    Small formatting fixes.

--- checker/src/org/checkerframework/checker/units/UnitsRelationsDefault.java
+++ checker/src/org/checkerframework/checker/units/UnitsRelationsDefault.java
@@ -2,28 +2,29 @@ package org.checkerframework.checker.units;
 
 /*>>>
 import org.checkerframework.checker.nullness.qual.Nullable;
- */
+*/
+
+import org.checkerframework.checker.units.qual.Prefix;
+import org.checkerframework.checker.units.qual.h;
+import org.checkerframework.checker.units.qual.km2;
+import org.checkerframework.checker.units.qual.kmPERh;
+import org.checkerframework.checker.units.qual.m;
+import org.checkerframework.checker.units.qual.m2;
+import org.checkerframework.checker.units.qual.mPERs;
+import org.checkerframework.checker.units.qual.mPERs2;
+import org.checkerframework.checker.units.qual.mm2;
+import org.checkerframework.checker.units.qual.s;
+import org.checkerframework.framework.type.AnnotatedTypeMirror;
+import org.checkerframework.framework.util.AnnotationBuilder;
+import org.checkerframework.javacutil.AnnotationUtils;
+
+import java.util.Map;
 
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.util.Elements;
-import java.util.Map;
-
-import org.checkerframework.checker.units.qual.Prefix;
-import org.checkerframework.checker.units.qual.m;
-import org.checkerframework.checker.units.qual.m2;
-import org.checkerframework.checker.units.qual.mPERs;
-import org.checkerframework.checker.units.qual.mPERs2;
-import org.checkerframework.checker.units.qual.km2;
-import org.checkerframework.checker.units.qual.kmPERh;
-import org.checkerframework.checker.units.qual.mm2;
-import org.checkerframework.checker.units.qual.h;
-import org.checkerframework.checker.units.qual.s;
-import org.checkerframework.framework.type.AnnotatedTypeMirror;
-import org.checkerframework.framework.util.AnnotationBuilder;
-import org.checkerframework.javacutil.AnnotationUtils;
 
 /**
  * Default relations between SI units.
@@ -76,127 +77,106 @@ public class UnitsRelationsDefault implements UnitsRelations {
             Prefix p1prefix = getTypeMirrorPrefix(p1);
             Prefix p2prefix = getTypeMirrorPrefix(p2);
 
-            // prefix of null means we couldn't find or assign any prefixes at all (error state)
-            if(p1prefix == null || p2prefix == null) {
+            if (p1prefix == null || p2prefix == null) {
+                // prefix of null means we couldn't find or assign any prefixes at all (error state)
                 return null;
-            }
-            // km * km
-            else if(p1prefix.equals(p2prefix) && p2prefix.equals(Prefix.kilo)) {
+            } else if (p1prefix.equals(p2prefix) && p2prefix.equals(Prefix.kilo)) {
+                // km * km
                 return km2;
-            }
-            // mm * mm
-            else if(p1prefix.equals(p2prefix) && p2prefix.equals(Prefix.milli)) {
+            } else if (p1prefix.equals(p2prefix) && p2prefix.equals(Prefix.milli)) {
+                // mm * mm
                 return mm2;
-            }
-            // m * m
-            else if(p1prefix.equals(p2prefix) && p2prefix.equals(Prefix.one)) {
+            } else if (p1prefix.equals(p2prefix) && p2prefix.equals(Prefix.one)) {
+                // m * m
                 return m2;
-            }
-            else {
+            } else {
                 return null;
             }
-        }
-        // s * mPERs or mPERs * s => m
-        else if (    AnnotationUtils.containsSame(p1.getAnnotations(), s) &&
+        } else if (AnnotationUtils.containsSame(p1.getAnnotations(), s) &&
                 AnnotationUtils.containsSame(p2.getAnnotations(), mPERs)
                 ||
                 AnnotationUtils.containsSame(p1.getAnnotations(), mPERs) &&
-                AnnotationUtils.containsSame(p2.getAnnotations(), s)    ) {
+                AnnotationUtils.containsSame(p2.getAnnotations(), s)) {
+            // s * mPERs or mPERs * s => m
             return m;
-        }
-        // s * mPERs2 or mPERs2 * s => mPERs
-        else if (    AnnotationUtils.containsSame(p1.getAnnotations(), s) &&
+        } else if (AnnotationUtils.containsSame(p1.getAnnotations(), s) &&
                 AnnotationUtils.containsSame(p2.getAnnotations(), mPERs2)
                 ||
                 AnnotationUtils.containsSame(p1.getAnnotations(), mPERs2) &&
-                AnnotationUtils.containsSame(p2.getAnnotations(), s)    ) {
+                AnnotationUtils.containsSame(p2.getAnnotations(), s)) {
+            // s * mPERs2 or mPERs2 * s => mPERs
             return mPERs;
-        }
-        // h * kmPERh or kmPERh * h => km
-        else if (    AnnotationUtils.containsSame(p1.getAnnotations(), h) &&
+        } else if (AnnotationUtils.containsSame(p1.getAnnotations(), h) &&
                 AnnotationUtils.containsSame(p2.getAnnotations(), kmPERh)
                 ||
                 AnnotationUtils.containsSame(p1.getAnnotations(), kmPERh) &&
                 AnnotationUtils.containsSame(p2.getAnnotations(), h)) {
+            // h * kmPERh or kmPERh * h => km
             return km;
-        }
-        else {
+        } else {
             return null;
         }
     }
 
     @Override
     public /*@Nullable*/ AnnotationMirror division(AnnotatedTypeMirror p1, AnnotatedTypeMirror p2) {
-        // m / s => mPERs
         if (AnnotationUtils.containsSame(p1.getAnnotations(), m) &&
                 AnnotationUtils.containsSame(p2.getAnnotations(), s)) {
+            // m / s => mPERs
             return mPERs;
-        }
-        // km / h => kmPERh
-        else if (AnnotationUtils.containsSame(p1.getAnnotations(), km) &&
+        } else if (AnnotationUtils.containsSame(p1.getAnnotations(), km) &&
                 AnnotationUtils.containsSame(p2.getAnnotations(), h)) {
+            // km / h => kmPERh
             return kmPERh;
-        }
-        // m2 / m => m
-        else if (AnnotationUtils.containsSame(p1.getAnnotations(), m2) &&
+        } else if (AnnotationUtils.containsSame(p1.getAnnotations(), m2) &&
                 AnnotationUtils.containsSame(p2.getAnnotations(), m)) {
+            // m2 / m => m
             return m;
-        }
-        // km2 / km => km
-        else if (AnnotationUtils.containsSame(p1.getAnnotations(), km2) &&
+        } else if (AnnotationUtils.containsSame(p1.getAnnotations(), km2) &&
                 AnnotationUtils.containsSame(p2.getAnnotations(), km)) {
+            // km2 / km => km
             return km;
-        }
-        // mm2 / mm => mm
-        else if (AnnotationUtils.containsSame(p1.getAnnotations(), mm2) &&
+        } else if (AnnotationUtils.containsSame(p1.getAnnotations(), mm2) &&
                 AnnotationUtils.containsSame(p2.getAnnotations(), mm)) {
+            // mm2 / mm => mm
             return mm;
-        }
-        // m / mPERs => s
-        else if (AnnotationUtils.containsSame(p1.getAnnotations(), m) &&
+        } else if (AnnotationUtils.containsSame(p1.getAnnotations(), m) &&
                 AnnotationUtils.containsSame(p2.getAnnotations(), mPERs)) {
+            // m / mPERs => s
             return s;
-        }
-        // km / kmPERh => h
-        else if (AnnotationUtils.containsSame(p1.getAnnotations(), km) &&
+        } else if (AnnotationUtils.containsSame(p1.getAnnotations(), km) &&
                 AnnotationUtils.containsSame(p2.getAnnotations(), kmPERh)) {
+            // km / kmPERh => h
             return h;
-        }
-        // mPERs / s = mPERs2
-        else if (AnnotationUtils.containsSame(p1.getAnnotations(), mPERs) &&
+        } else if (AnnotationUtils.containsSame(p1.getAnnotations(), mPERs) &&
                 AnnotationUtils.containsSame(p2.getAnnotations(), s)) {
+            // mPERs / s = mPERs2
             return mPERs2;
-        }
-        // mPERs / mPERs2 => s  (velocity / acceleration == time)
-        else if (AnnotationUtils.containsSame(p1.getAnnotations(), mPERs) &&
+        } else if (AnnotationUtils.containsSame(p1.getAnnotations(), mPERs) &&
                 AnnotationUtils.containsSame(p2.getAnnotations(), mPERs2)) {
+            // mPERs / mPERs2 => s  (velocity / acceleration == time)
             return s;
-        }
-        else {
+        } else {
             return null;
         }
     }
 
     // helper functions
     // go through each annotation of an annotated type, find the prefix and return it
-    private Prefix getTypeMirrorPrefix(AnnotatedTypeMirror atm)
-    {
-        for(AnnotationMirror mirror : atm.getAnnotations())
-        {
+    private Prefix getTypeMirrorPrefix(AnnotatedTypeMirror atm) {
+        for (AnnotationMirror mirror : atm.getAnnotations()) {
             AnnotationValue annotationValue = getAnnotationMirrorPrefix(mirror);
             // annotation has no element value (ie no SI prefix)
-            if(annotationValue == null) {
+            if (annotationValue == null) {
                 return Prefix.one;
             }
             // if the annotation has a value, then detect the string name of the prefix and return the Prefix
             String prefixString = annotationValue.getValue().toString();
-            if(prefixString.equals(Prefix.kilo.toString())) {
+            if (prefixString.equals(Prefix.kilo.toString())) {
                 return Prefix.kilo;
-            }
-            else if(prefixString.equals(Prefix.milli.toString())) {
+            } else if (prefixString.equals(Prefix.milli.toString())) {
                 return Prefix.milli;
-            }
-            else if(prefixString.equals(Prefix.one.toString())) {
+            } else if (prefixString.equals(Prefix.one.toString())) {
                 return Prefix.one;
             }
         }
@@ -204,8 +184,7 @@ public class UnitsRelationsDefault implements UnitsRelations {
     }
 
     // given an annotation, returns the prefix value (eg kilo) if there is any, otherwise returns null
-    private AnnotationValue getAnnotationMirrorPrefix(AnnotationMirror mirror)
-    {
+    private AnnotationValue getAnnotationMirrorPrefix(AnnotationMirror mirror) {
         Map<? extends ExecutableElement,? extends AnnotationValue> elementValues = mirror.getElementValues();
         for (Map.Entry<? extends ExecutableElement,? extends AnnotationValue> entry : elementValues.entrySet()) {
             if ("value".equals(entry.getKey().getSimpleName().toString())) {
