90b833ccb1f3f86f7ae5438942afc19c0e6b01a7
jonathangburke <jonathangburke@gmail.com>
2015-04-16 16:16:10 -0700

    Update test to reflect manual changes

--- checker/tests/nullness/generics/GenericsExample.java
+++ checker/tests/nullness/generics/GenericsExample.java
@@ -30,11 +30,63 @@ class GenericsExample {
     }
   }
 
+  class MyList1a<@Nullable T extends @Nullable Object> {
+    T t;
+    @Nullable T nble;
+    @NonNull T nn;
+    MyList1a(T t, @Nullable T nble, @NonNull T nn) {
+      this.t = t; this.nble = nble; this.nn = nn;
+    }
+    void add(T arg) { }
+    T get(int i) { return t; }
+    void m() {
+      t = null;
+      t = nble;
+      nble = null;
+      //:: error: (assignment.type.incompatible)
+      nn = null;
+      t = this.get(0);
+      nble = this.get(0);
+      //:: error: (assignment.type.incompatible)
+      nn = this.get(0);
+      this.add(t);
+      this.add(nble);
+      this.add(nn);
+    }
+  }
+
   class MyList2<@NonNull T extends @NonNull Object> {
     T t;
     @Nullable T nble;
     @NonNull T nn;
     MyList2(T t, @Nullable T nble, @NonNull T nn) {
+      this.t = t; this.nble = nble; this.nn = nn;
+    }
+    void add(T arg) { }
+    T get(int i) { return t; }
+    void m() {
+      //:: error: (assignment.type.incompatible)
+      t = null;
+      //:: error: (assignment.type.incompatible)
+      t = nble;
+      nble = null;
+      //:: error: (assignment.type.incompatible)
+      nn = null;
+      t = this.get(0);
+      nble = this.get(0);
+      nn = this.get(0);
+      this.add(t);
+      //:: error: (argument.type.incompatible)
+      this.add(nble);
+      this.add(nn);
+    }
+  }
+
+  class MyList2a<T extends @NonNull Object> { // same as MyList2
+    T t;
+    @Nullable T nble;
+    @NonNull T nn;
+    MyList2a(T t, @Nullable T nble, @NonNull T nn) {
       this.t = t; this.nble = nble; this.nn = nn;
     }
     void add(T arg) { }
@@ -77,33 +129,6 @@ class GenericsExample {
       t = this.get(0);
       nble = this.get(0);
       //:: error: (assignment.type.incompatible)
-      nn = this.get(0);
-      this.add(t);
-      //:: error: (argument.type.incompatible)
-      this.add(nble);
-      this.add(nn);
-    }
-  }
-
-  class MyList4<T extends @NonNull Object> { // same as MyList2
-    T t;
-    @Nullable T nble;
-    @NonNull T nn;
-    MyList4(T t, @Nullable T nble, @NonNull T nn) {
-      this.t = t; this.nble = nble; this.nn = nn;
-    }
-    void add(T arg) { }
-    T get(int i) { return t; }
-    void m() {
-      //:: error: (assignment.type.incompatible)
-      t = null;
-      //:: error: (assignment.type.incompatible)
-      t = nble;
-      nble = null;
-      //:: error: (assignment.type.incompatible)
-      nn = null;
-      t = this.get(0);
-      nble = this.get(0);
       nn = this.get(0);
       this.add(t);
       //:: error: (argument.type.incompatible)
