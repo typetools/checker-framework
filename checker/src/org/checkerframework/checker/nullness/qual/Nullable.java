42274f25c1463539ab79a532b73c78f173ea283c
Werner Dietl <wdietl@gmail.com>
2015-04-23 01:44:39 -0400

    Make @Nullable the default upper bound for all type parameters declared in binary-only code.

--- checker/src/org/checkerframework/checker/nullness/qual/Nullable.java
+++ checker/src/org/checkerframework/checker/nullness/qual/Nullable.java
@@ -37,7 +37,7 @@ import com.sun.source.tree.Tree;
 @ImplicitFor(trees = { Tree.Kind.NULL_LITERAL }, typeNames = { java.lang.Void.class })
 @DefaultFor({ DefaultLocation.LOCAL_VARIABLE, DefaultLocation.RESOURCE_VARIABLE,
             DefaultLocation.IMPLICIT_UPPER_BOUNDS })
-@DefaultForInUntyped({ DefaultLocation.RETURNS })
+@DefaultForInUntyped({ DefaultLocation.RETURNS, DefaultLocation.UPPER_BOUNDS })
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE_USE, ElementType.TYPE_PARAMETER })
