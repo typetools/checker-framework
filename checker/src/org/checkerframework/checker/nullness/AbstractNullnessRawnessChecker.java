96ae9b9ed526c75b044c5ad3718a504688184d28
Werner Dietl <wdietl@gmail.com>
2015-05-27 14:34:11 -0400

    Reduce duplication by moving meta-annotations to the superclass.

--- checker/src/org/checkerframework/checker/nullness/AbstractNullnessRawnessChecker.java
+++ checker/src/org/checkerframework/checker/nullness/AbstractNullnessRawnessChecker.java
@@ -7,26 +7,13 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 import org.checkerframework.checker.nullness.qual.PolyNull;
 import org.checkerframework.checker.nullness.qual.Raw;
 import org.checkerframework.framework.qual.PolyAll;
-import org.checkerframework.framework.qual.StubFiles;
 import org.checkerframework.framework.qual.TypeQualifiers;
-import org.checkerframework.framework.source.SupportedLintOptions;
 
 /**
  * A concrete instantiation of {@link AbstractNullnessChecker} using rawness.
  */
 @TypeQualifiers({ Nullable.class, MonotonicNonNull.class, NonNull.class,
         NonRaw.class, Raw.class, PolyNull.class, PolyAll.class })
-@SupportedLintOptions({
-        AbstractNullnessChecker.LINT_NOINITFORMONOTONICNONNULL,
-        AbstractNullnessChecker.LINT_REDUNDANTNULLCOMPARISON,
-        // Temporary option to forbid non-null array component types,
-        // which is allowed by default.
-        // Forbidding is sound and will eventually be the only possibility.
-        // Allowing is unsound but permitted until flow-sensitivity changes are
-        // made.
-        // See issues 154 and 322.
-        "arrays:forbidnonnullcomponents" })
-@StubFiles("astubs/gnu-getopt.astub")
 public class AbstractNullnessRawnessChecker extends AbstractNullnessChecker {
 
     public AbstractNullnessRawnessChecker() {
