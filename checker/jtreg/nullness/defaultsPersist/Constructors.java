4503185fa62cd710ba205380a63f8bb18b039969
jonathangburke <jonathangburke@gmail.com>
2015-05-28 12:42:12 -0700

    Fix JTReg test with Generics and add a util method to ReferenceInfoUtil

--- checker/jtreg/nullness/defaultsPersist/Constructors.java
+++ checker/jtreg/nullness/defaultsPersist/Constructors.java
@@ -65,9 +65,9 @@ public class Constructors {
     }
 
     @TADescriptions({
-        @TADescription(annotation = "org/checkerframework/checker/nullness/qual/NonNull", type = METHOD_TYPE_PARAMETER, paramIndex=0, boundIndex=0),
-        @TADescription(annotation = "org/checkerframework/checker/initialization/qual/Initialized", type = METHOD_TYPE_PARAMETER, paramIndex=0, boundIndex=0),
-        @TADescription(annotation = "org/checkerframework/checker/nullness/qual/KeyForBottom", type = METHOD_TYPE_PARAMETER, paramIndex=0, boundIndex=0),
+        @TADescription(annotation = "org/checkerframework/checker/nullness/qual/NonNull", type = METHOD_TYPE_PARAMETER, paramIndex=0),
+        @TADescription(annotation = "org/checkerframework/checker/initialization/qual/Initialized", type = METHOD_TYPE_PARAMETER, paramIndex=0),
+        @TADescription(annotation = "org/checkerframework/checker/nullness/qual/KeyForBottom", type = METHOD_TYPE_PARAMETER, paramIndex=0),
 
         @TADescription(annotation = "org/checkerframework/checker/nullness/qual/Nullable", type = METHOD_TYPE_PARAMETER_BOUND, paramIndex=0, boundIndex=0),
         @TADescription(annotation = "org/checkerframework/checker/initialization/qual/Initialized", type = METHOD_TYPE_PARAMETER_BOUND, paramIndex=0, boundIndex=0),
@@ -78,11 +78,11 @@ public class Constructors {
     }
 
     @TADescriptions({
-        @TADescription(annotation = "org/checkerframework/checker/nullness/qual/NonNull", type = METHOD_TYPE_PARAMETER, paramIndex=0, boundIndex=0),
-        @TADescription(annotation = "org/checkerframework/checker/initialization/qual/Initialized", type = METHOD_TYPE_PARAMETER, paramIndex=0, boundIndex=0),
-        @TADescription(annotation = "org/checkerframework/checker/nullness/qual/KeyForBottom", type = METHOD_TYPE_PARAMETER, paramIndex=0, boundIndex=0),
+        @TADescription(annotation = "org/checkerframework/checker/nullness/qual/NonNull", type = METHOD_TYPE_PARAMETER, paramIndex=0),
+        @TADescription(annotation = "org/checkerframework/checker/initialization/qual/Initialized", type = METHOD_TYPE_PARAMETER, paramIndex=0),
+        @TADescription(annotation = "org/checkerframework/checker/nullness/qual/KeyForBottom", type = METHOD_TYPE_PARAMETER, paramIndex=0),
 
-        @TADescription(annotation = "org/checkerframework/checker/nullness/qual/NonNull", type = METHOD_TYPE_PARAMETER_BOUND, paramIndex=0, boundIndex=0),
+        @TADescription(annotation = "org/checkerframework/checker/nullness/qual/NonNull", type = METHOD_TYPE_PARAMETER_BOUND, paramIndex=0,  boundIndex=0),
         @TADescription(annotation = "org/checkerframework/checker/initialization/qual/Initialized", type = METHOD_TYPE_PARAMETER_BOUND, paramIndex=0, boundIndex=0),
         @TADescription(annotation = "org/checkerframework/checker/nullness/qual/UnknownKeyFor", type = METHOD_TYPE_PARAMETER_BOUND, paramIndex=0, boundIndex=0),
     })
@@ -91,9 +91,9 @@ public class Constructors {
     }
 
     @TADescriptions({
-        @TADescription(annotation = "org/checkerframework/checker/nullness/qual/NonNull", type = METHOD_TYPE_PARAMETER, paramIndex=0, boundIndex=1),
-        @TADescription(annotation = "org/checkerframework/checker/initialization/qual/Initialized", type = METHOD_TYPE_PARAMETER, paramIndex=0, boundIndex=1),
-        @TADescription(annotation = "org/checkerframework/checker/nullness/qual/KeyForBottom", type = METHOD_TYPE_PARAMETER, paramIndex=0, boundIndex=1),
+        @TADescription(annotation = "org/checkerframework/checker/nullness/qual/NonNull", type = METHOD_TYPE_PARAMETER, paramIndex=0),
+        @TADescription(annotation = "org/checkerframework/checker/initialization/qual/Initialized", type = METHOD_TYPE_PARAMETER, paramIndex=0),
+        @TADescription(annotation = "org/checkerframework/checker/nullness/qual/KeyForBottom", type = METHOD_TYPE_PARAMETER, paramIndex=0),
 
         @TADescription(annotation = "org/checkerframework/checker/nullness/qual/NonNull", type = METHOD_TYPE_PARAMETER_BOUND, paramIndex=0, boundIndex=1),
         @TADescription(annotation = "org/checkerframework/checker/initialization/qual/Initialized", type = METHOD_TYPE_PARAMETER_BOUND, paramIndex=0, boundIndex=1),
