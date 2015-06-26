f156652e1a137fae5aa60a14a71307e1234e2654
jonathangburke <jonathangburke@gmail.com>
2015-06-25 23:00:17 -0700

    Skip issue 436 unless Java 8 is being use to run the tests

--- framework/tests/all-systems/java8/lambda/Issue436.java
+++ framework/tests/all-systems/java8/lambda/Issue436.java
@@ -2,8 +2,11 @@ import static java.util.Arrays.asList;
 import java.util.function.Supplier;
 import java.util.List;
 
+//@below-java8-jdk-skip-test
 class Issue436 {
     public void makeALongFormConditionalLambdaReturningGenerics(boolean makeAll) {
+        //TypeArgInferenceUtil.assignedTo used to try to use the method return rather than the lambda return
+        //for those return statements below
         Supplier<List<String>> supplier = () -> {
             if (makeAll) {
                 return asList("beer", "peanuts");
