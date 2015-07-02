11832c8441f1b1ad34f982d5978c4670fdd9cf3e
Michael Ernst <mernst@cs.washington.edu>
2015-07-02 20:02:17 -0300

    Improve documentation of polymorphic qualifiers, and support them in example type systems.

---
+++ tutorial/eclipse-projects/encryption-checker/src/myqual/PolyEncrypted.java
@@ -0,0 +1,14 @@
+package myqual;
+
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import org.checkerframework.framework.qual.SubtypeOf;
+import org.checkerframework.framework.qual.TypeQualifier;
+
+/**
+ * Denotes that the representation of an object is encrypted.
+ */
+@TypeQualifier
+@PolymorphicQualifier
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+public @interface PolyEncrypted {}
