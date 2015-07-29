ebfb9289ae9781bfe695276b23eb3c57d50f9887
Werner Dietl <wdietl@gmail.com>
2015-07-28 23:34:50 -0400

    Update travis-ci configuration.

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-cd $TRAVIS_BUILD_DIR/..
+ROOT=$TRAVIS_BUILD_DIR/..
+cd $ROOT
 git clone https://github.com/typetools/annotation-tools.git
 cd annotation-tools/
 ./.travis-build.sh
 # This also builds jsr308-langtools
 
-cd $TRAVIS_BUILD_DIR
+cd $ROOT/checker-framework
 ant clean
 ant dist
 ant javadoc tests
