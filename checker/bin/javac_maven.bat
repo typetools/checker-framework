b962776a86644a7b8d7a49f2852787274768f942
Javier Thaine <jthaine@cs.washington.edu>
2015-11-05 15:07:09 -0800

    Move the HelloGalaxy Maven example to checker/examples/MavenExample, add it to the release, and document it. Deprecate (but don't yet remove) the javac_maven script and javac_maven.bat.

--- checker/bin/javac_maven.bat
+++ checker/bin/javac_maven.bat
@@ -1,3 +1,8 @@
 @echo off
 
 java -jar %~dp0\..\dist\checker.jar -processorpath %~dp0\..\dist\checker.jar @argfile %*
+
+echo.
+echo.
+echo NOTE: javac_maven.bat is deprecated and will be removed in the December 1, 2015 release.
+echo.
