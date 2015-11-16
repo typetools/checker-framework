cbcf32bdb19529ab36587adf21a51eef4567e486
Michael Ernst <mernst@cs.washington.edu>
2015-11-16 04:01:42 -0800

    Test case for issue #511: https://github.com/typetools/checker-framework/issues/511

---
+++ checker/tests/src/tests/NullnessGenericWildcardLibTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import org.checkerframework.framework.test.CheckerFrameworkTest;
+
+import java.io.File;
+
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ * Library for the Nullness checker for issue #511.
+ */
+public class NullnessGenericWildcardLibTest extends CheckerFrameworkTest {
+
+    public NullnessGenericWildcardLibTest(File testFile) {
+        super(testFile,
+                org.checkerframework.checker.nullness.NullnessChecker.class,
+                "nullness",
+                "-Anomsgtext");
+    }
+
+    @Parameters
+    public static String[] getTestDirs() {
+        return new String[]{"nullness-genericwildcardlib"};
+    }
+
+}
