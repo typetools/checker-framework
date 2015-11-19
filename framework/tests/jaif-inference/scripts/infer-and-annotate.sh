db62a05363f7bfd50786d54f3a5117611b227387
Paulo Barros <pbsfcin@gmail.com>
2015-11-19 00:43:29 -0300

    Adds a script to iteratively run whole-program inference.

--- framework/tests/jaif-inference/scripts/infer-and-annotate.sh
+++ framework/tests/jaif-inference/scripts/infer-and-annotate.sh
@@ -7,11 +7,13 @@
 # This script receives as arguments:
 # 1. Processor name
 # 2. Classpath (target project's classpath)
-# 3. Project source folder path
-# 4. Extra processor arguments which will be passed to the checker
+# 3. Extra processor arguments which will be passed to the checker
+# 4. List of paths to .java files in a program
 
 # Example of usage:
-#./infer-and-annotate.sh org.checkerframework.common.value.ValueChecker $PROJECT_CP project_src_folder/ $EXTRA_PROCESSOR_ARGS
+#./infer-and-annotate.sh org.checkerframework.common.value.ValueChecker \
+#   $PROJECT_CP $EXTRA_PROCESSOR_ARGS $PROJECT_DIR/src/my/example/Example1.java \
+#   $PROJECT_DIR/src/my/example/Example2.java
 
 # In case of using this script for Android projects, you must include paths to:
 # android.jar, gen folder, all libs used, source code folder.
@@ -22,16 +24,29 @@ set -e
 
 function run_cf_javac() {
     processor=$1
-    cp=$2
-    source_files_dir=$3
-    extra_args=$4
-    cp=$cp:${CHECKERFRAMEWORK}/../annotation-tools/annotation-file-utilities/annotation-file-utilities.jar
-    for f in $(find $source_files_dir/ -name "*.java" -print0); do
-        files_to_compile="$files_to_compile $f"
-    done
-
+    cp=$2:${CHECKERFRAMEWORK}/../annotation-tools/annotation-file-utilities/annotation-file-utilities.jar
+    extra_args=$3
+    java_files=$4
     # Runs CF's javac
-    ${CHECKERFRAMEWORK}/checker/bin/javac -cp $cp -processor $processor -AuseJaifInference $extra_args $files_to_compile
+    ${CHECKERFRAMEWORK}/checker/bin/javac -cp $cp -processor $processor -AuseJaifInference $extra_args $java_files
+}
+
+function read_input() {
+    # This function separates extra arguments from java files received as arguments.
+    processor=$1
+    cp=$2
+    extra_args=""
+    java_files=""
+    index=2
+    for i in "${@:3}" # Ignores first two arguments (processor and cp).
+    do
+        let "index++"
+        if [[ $i != -* ]]; then
+            java_files="${@:index}"
+            break
+        fi
+        extra_args="$extra_args $i"
+    done
 }
 
 # Main
@@ -40,12 +55,19 @@ if [ "$#" -lt 3 ]; then
     exit 1
 fi
 
-processor=$1
-cp=$2
-source_files_dir=$3
-extra_args=$4
+read_input $@
 
-run_cf_javac $processor $cp $source_files_dir $extra_args
-cur_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-$cur_dir/insert-annotations.sh $source_files_dir build/jaif-files/
+DIFF=$(diff -q prev-jaif build/jaif-files || echo "Files dont exist.")
+while [ "$DIFF" != "" ]
+do
+    echo "once"
+    rm -rf prev-jaif 
+    cp -r build/jaif-files prev-jaif 2>/dev/null || :
+    echo "asd"
+    run_cf_javac $processor $cp $extra_args $java_files
+    insert-annotations-to-source build/jaif-files/* $java_files
+    DIFF=$(diff -q prev-jaif build/jaif-files || echo 'Files dont exist.')
+    echo "end"
+    echo $DIFF
+done
 
