2ab6080219069ad51016adfc5f44e1a05637d53f
Paulo Barros <pbsfcin@gmail.com>
2015-12-03 14:04:20 -0300

    Removes bashisms from .sh script.

--- framework/signature-inference-scripts/infer-and-annotate.sh
+++ framework/signature-inference-scripts/infer-and-annotate.sh
@@ -12,49 +12,63 @@
 # 4. List of paths to .java files in a program.
 
 # Example of usage:
-#./infer-and-annotate.sh org.checkerframework.common.value.ValueChecker \
-#   $PROJECT_CP $EXTRA_PROCESSOR_ARGS $PROJECT_DIR/src/my/example/Example1.java \
-#   $PROJECT_DIR/src/my/example/Example2.java
+# ./infer-and-annotate.sh "LockChecker,NullnessChecker" \
+#     $JSR308/plume-lib/java/plume.jar -AprintErrorStack \
+#     `find $JSR308/plume-lib/java/src/plume/ -name "*.java"`
 
 # In case of using this script for Android projects, you must include paths to:
 # android.jar, gen folder, all libs used, source code folder.
 # The Android project must be built with "ant debug" before running this script.
 
+# Halts the script when a nonzero value is returned from a command.
 set -e
 
 # This function separates extra arguments passed to the checker from java files
 # received as arguments.
-function read_input() {
+# TODO: Handle the following limitation: This function makes the assumption
+# that every argument starts with a hyphen. It means one cannot pass arguments
+# such as -processorpath and -source.
+read_input() {
     processor=$1
     cp=$2:${CHECKERFRAMEWORK}/../annotation-tools/annotation-file-utilities/annotation-file-utilities.jar
     extra_args=""
     java_files=""
-    index=2
-    for i in "${@:3}" # Ignores first two arguments (processor and cp).
+
+    # Ignores first two arguments (processor and cp).
+    shift
+    shift
+
+    for i in "$@"
     do
-        let "index++"
-        if [[ $i != -* ]]; then
-            java_files="${@:index}"
-            break
-        fi
-        extra_args="$extra_args $i"
+        # This function makes the assumption that every extra argument
+        # starts with a hyphen. The rest are .java files.
+        case "$1" in
+            -*)
+                extra_args="$extra_args $1"
+            ;;
+            *)
+                java_files="$java_files $1"
+            ;;
+        esac
+        shift
     done
 }
 
-function infer_and_annotate() {
-    DIFF=firstdiff
+# Iteratively runs the Checker
+infer_and_annotate() {
+    DIFF_JAIF=firstdiff
     rm -rf build/jaif-files
+    mkdir -p build/jaif-files
     # Perform inference and add annotations from .jaif to .java files until
     # prev-jaif has the same contents as build/jaif-files
-    while [ "$DIFF" != "" ]
+    while [ "$DIFF_JAIF" != "" ]
     do
         # Updates prev-jaif folder
         rm -rf prev-jaif
-        cp -r build/jaif-files prev-jaif 2>/dev/null || :
+        cp -r build/jaif-files prev-jaif
 
         # Runs CF's javac
         ${CHECKERFRAMEWORK}/checker/bin/javac -cp $cp -processor $processor -AinferSignatures $extra_args $java_files
-
         # Deletes .unannotated backup files. This is necessary otherwise the
         # insert-annotations-to-source tool will use this file instead of the
         # updated .java one.
@@ -62,23 +76,22 @@ function infer_and_annotate() {
         do
             rm -f "${file}.unannotated"
         done
-
-        # Updates DIFF variable
-        DIFF=$(diff -q prev-jaif build/jaif-files || echo 'Files dont exist.')
-
-        if [ "$DIFF" != "" ]; then
-            # Inserts annotations from .jaif into .java files
-            insert-annotations-to-source -i build/jaif-files/* $java_files
-        fi
+        insert-annotations-to-source -i build/jaif-files/* $java_files
+        # Updates DIFF_JAIF variable.
+        # diff returns exit-value 1 when there are differences between files.
+        # When this happens, this script halts due to the "set -e"
+        # in its header. To avoid this problem, we add the "|| true" below.
+        DIFF_JAIF="$(diff -qr prev-jaif build/jaif-files || true)"
     done
 }
 
 # Main
 if [ "$#" -lt 3 ]; then
-    echo "infer-and-annotate.sh: Expected at least 3 arguments. Received the following arguments: $@. Check the script's documentation."
+    echo "Aborting infer-and-annotate.sh: Expected at least 3 arguments. Check the script's documentation."
+    echo "Received the following arguments: $@."
     exit 1
 fi
 
-read_input $@
+read_input "$@"
 infer_and_annotate
 
