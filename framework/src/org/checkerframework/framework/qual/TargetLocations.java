b9701df2205420b1c28a94208944587b70c0ae98
Paulo Barros <pbsfcin@gmail.com>
2015-12-04 19:28:12 -0300

    Incorporates code review feedback.

--- framework/src/org/checkerframework/framework/qual/TargetLocations.java
+++ framework/src/org/checkerframework/framework/qual/TargetLocations.java
@@ -11,8 +11,9 @@ import java.lang.annotation.Target;
  * qualifier may be written.  When written together with
  * {@code @Target({ElementType.TYPE_USE})}, the given type qualifier may be
  * written only at locations listed in the {@code @TargetLocations(...)}
- * meta-annotation.  The absence of {@code @TargetLocations(...)}
- * means that the qualifier can be written on any type use.
+ * meta-annotation.  {@code @Target({ElementType.TYPE_USE})} together with no
+ * {@code @TargetLocations(...)} means that the qualifier can be written on any
+ * type use.
  * <p>
  *
  * This enables a type system designer to permit a qualifier to be written
@@ -23,11 +24,9 @@ import java.lang.annotation.Target;
  * This meta-annotation is a declarative, coarse-grained approach to enable
  * that. A {@link org.checkerframework.common.basetype.TypeValidator} must be
  * implemented if a more fine-grained control is necessary.
- * <p>
  *
- * TODO: Verify this meta-annotation (step 3 in
- * <a href="https://github.com/typetools/checker-framework/issues/515">Issue #515</a>).
  */
+// TODO: Verify this meta-annotation (step 3 in https://github.com/typetools/checker-framework/issues/515).
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.ANNOTATION_TYPE)
