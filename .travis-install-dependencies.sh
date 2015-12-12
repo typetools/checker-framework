abaf7c9f0c907352ddf8371ce369b373e49c090d
Michael Ernst <mernst@cs.washington.edu>
2015-12-11 16:25:13 -0800

    Remove install step, which is no longer necessary

--- .travis-install-dependencies.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Install jtreg; currently must be from source.
-(cd .. && wget https://adopt-openjdk.ci.cloudbees.com/view/OpenJDK/job/jtreg/lastSuccessfulBuild/artifact/jtreg-4.1-b12.tar.gz && tar xzf jtreg-4.1-b12.tar.gz)
