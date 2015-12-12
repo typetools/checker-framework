e21cf58edad9f6427dd624c675ed37191d748a8e
Michael Ernst <mernst@cs.washington.edu>
2015-12-12 13:11:16 -0800

    Tiny grammar tweak

--- framework/src/org/checkerframework/common/signatureinference/SignatureInferenceScenes.java
+++ framework/src/org/checkerframework/common/signatureinference/SignatureInferenceScenes.java
@@ -107,7 +107,7 @@ public class SignatureInferenceScenes {
 
     /** Set containing Scenes that were modified since the last time all
      * Scenes were written into .jaif files. The String argument of this set
-     * is a path to the .jaif file of the corresponding Scene on the set. It
+     * is a path to the .jaif file of the corresponding Scene in the set. It
      * is obtained by passing a class name as argument to the
      * {@link getJaifPath} method.
      *
