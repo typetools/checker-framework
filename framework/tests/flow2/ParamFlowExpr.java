3c872704f60d63095ef53e7dad1ae2ef5d4e3c49
Michael Ernst <mernst@cs.washington.edu>
2015-12-18 08:11:55 -0800

    Test regarding flowexpr.parameter.not.final

--- framework/tests/flow2/ParamFlowExpr.java
+++ framework/tests/flow2/ParamFlowExpr.java
@@ -17,4 +17,16 @@ class ParamFlowExpr {
     void t2(String p1) {
         p1 = "";
     }
+
+    @RequiresQualifier(expression="#1", qualifier=Odd.class)
+    public static boolean eltsNonNull(Object[] seq1) {
+        if (seq1 == null) { return false; }
+        for (int i = 0 ; i < seq1.length ; i++) {
+            if (seq1[i] == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
