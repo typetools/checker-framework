b4dc26252ff946eec6bf525839a5bc15637dd1ea
Suzanne Millstein <smillst@cs.washington.edu>
2015-12-18 09:32:43 -0800

    A few more places that need to be renamed.

--- framework/src/org/checkerframework/framework/source/SourceChecker.java
+++ framework/src/org/checkerframework/framework/source/SourceChecker.java
@@ -176,14 +176,14 @@ import com.sun.tools.javac.util.Log;
     "concurrentSemantics",
 
     // Whether to use conservative checks for unannotated bytecode; these are configured
-    // by the specific type checker using @Default[Qualifier]ForUnannotatedCode.
+    // by the specific type checker using @Default[QualifierInHierarchy]ForUncheckedCode.
     // They may require more annotations or stub files.
     // "unsafeDefaultsForUnannotatedBytecode",
     // TODO: temporary option to turn on sound behavior.
     "safeDefaultsForUnannotatedBytecode",
 
     // Whether to use conservative checks for unannotated source code; these are configured
-    // by the specific type checker using @Default[Qualifier]ForUnannotatedCode
+    // by the specific type checker using @Default[QualifierInHierarchy]ForUncheckedCode
     // and only apply to source code that is not marked as @AnnotatedFor the checker
     // that is being executed.
     "useSafeDefaultsForUnannotatedSourceCode",
