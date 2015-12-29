82b62626c93256d291703999686fcc27f42c951d
Werner Dietl <wdietl@gmail.com>
2015-12-29 03:50:37 -0500

    Add link to issue, remove comment, update indentation.

--- framework/tests/all-systems/Issue478.java
+++ framework/tests/all-systems/Issue478.java
@@ -1,13 +1,11 @@
+// Test case for Issue 478:
+// https://github.com/typetools/checker-framework/issues/478
+
 import java.io.Serializable;
 import java.util.Comparator;
 
-//@skip-test
-// error: Expected the type of TYPE_CAST tree in assignment context
-//        to be a functional interface.
 class Issue478 {
-  public static Comparator<Object> allTheSame() {
-    // cast fails with any checker but succeeds with no processors
-    return (Comparator<Object> & Serializable) (c1, c2) -> 0;
-  }
+    public static Comparator<Object> allTheSame() {
+        return (Comparator<Object> & Serializable) (c1, c2) -> 0;
+    }
 }
-
