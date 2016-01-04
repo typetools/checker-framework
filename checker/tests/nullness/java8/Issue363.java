7b4a0ddd5fd865f6d93b4e0a7305e8fe33a0b84b
Werner Dietl <wdietl@gmail.com>
2016-01-03 19:13:20 -0500

    Add test case for Issue 363 to ensure it is fixed.

---
+++ checker/tests/nullness/java8/Issue363.java
@@ -0,0 +1,13 @@
+// Test case for Issue 363:
+// https://github.com/typetools/checker-framework/issues/363
+
+class Issue363 {
+    void foo(java.util.OptionalInt value) {
+        value.orElseThrow(() -> new Error());
+    }
+
+    void bar(java.util.OptionalInt value) {
+        java.util.function.Supplier<Error> s = () -> new Error();
+        value.orElseThrow(s);
+    }
+}
