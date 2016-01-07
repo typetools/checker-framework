12149008ebfc24bf6a8a0c9e5182db2e5f4ec2f7
Suzanne Millstein <smillst@cs.washington.edu>
2016-01-07 14:23:56 -0800

    Add test case for changes in PR 550

---
+++ framework/tests/classval/GLBTest.java
@@ -0,0 +1,15 @@
+import org.checkerframework.common.reflection.qual.ClassBound;
+import org.checkerframework.common.reflection.qual.ClassVal;
+import org.checkerframework.common.reflection.qual.ClassValBottom;
+
+class GLBTest<@ClassVal({"A","B"}) T extends Object> {
+    // This code is intented to cover the more complex branchs for the GLB calcuation.
+    // This only triggers the GLB calculation because of a hack in
+    // org.checkerframework.framework.util.AnnotatedTypes.addAnnotationsImpl()
+    // If that code changes, this code may not test GLB anymore
+    // This code does not test correctness. Because no experssion is given the GLB as a type,
+    // it is impossible to test GLB for correctness.
+    GLBTest<@ClassVal({"A","B","C"}) ?> f1 = new GLBTest<@ClassVal({"A","E"}) Object>();
+    GLBTest<@ClassVal({"A","B","C"}) ?> f2 = new GLBTest<@ClassBound({"A","E"}) Object>();
+    GLBTest<@ClassBound({"A","B","C"}) ?> f3 = new GLBTest<@ClassBound({"A","E"}) Object>();
+}
