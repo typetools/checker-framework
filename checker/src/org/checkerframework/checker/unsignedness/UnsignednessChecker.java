4263c780bcf64e2498864a142f626f67249ab84e
cmackie <pullman.cm@gmail.com>
2016-01-14 21:42:22 -0800

    Removed deprecated annotation "TypeQualifiers"

--- checker/src/org/checkerframework/checker/unsignedness/UnsignednessChecker.java
+++ checker/src/org/checkerframework/checker/unsignedness/UnsignednessChecker.java
@@ -2,9 +2,6 @@ package org.checkerframework.checker.unsignedness;
 
 import org.checkerframework.checker.unsignedness.qual.*;
 import org.checkerframework.common.basetype.BaseTypeChecker;
-import org.checkerframework.framework.qual.TypeQualifiers;
 
-@TypeQualifiers( { UnknownSignedness.class, Unsigned.class, 
-    Signed.class, Constant.class, UnsignednessBottom.class } )
 public final class UnsignednessChecker extends BaseTypeChecker {
 }
