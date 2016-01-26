cbbe22bdb2cfca50c5b778346eefb762dc6fc9b5
Suzanne Millstein <smillst@cs.washington.edu>
2016-01-26 13:46:22 -0800

    Remove superfluous @DefaultFor meta-annotation

--- checker/src/org/checkerframework/checker/nullness/qual/NonNull.java
+++ checker/src/org/checkerframework/checker/nullness/qual/NonNull.java
@@ -59,7 +59,7 @@ import com.sun.source.tree.Tree;
         Tree.Kind.DOUBLE_LITERAL, Tree.Kind.FLOAT_LITERAL,
         Tree.Kind.INT_LITERAL, Tree.Kind.LONG_LITERAL, Tree.Kind.STRING_LITERAL })
 @DefaultQualifierInHierarchy
-@DefaultFor({DefaultLocation.EXCEPTION_PARAMETER})
+@DefaultFor({ DefaultLocation.EXCEPTION_PARAMETER })
 @DefaultInUncheckedCodeFor({ DefaultLocation.PARAMETERS, DefaultLocation.LOWER_BOUNDS })
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
