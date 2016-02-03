a70ac5601899ea08f4999346f614a6aeaf02f746
Michael Ernst <mernst@cs.washington.edu>
2016-02-03 15:38:11 -0800

    Add comment about getOutputStream return type

--- checker/jdk/nullness/src/java/lang/Process.java
+++ checker/jdk/nullness/src/java/lang/Process.java
@@ -2,6 +2,9 @@ package java.lang;
 
 public abstract class Process{
   public Process() { throw new RuntimeException("skeleton method"); }
+  // These three methods return @NonNull values despite being documented as
+  // possibly returning a "null stream".  A "null stream" is a non-null
+  // Stream with particular behavior, not a @Nullable Stream reference.
   public abstract java.io.OutputStream getOutputStream();
   public abstract java.io.InputStream getInputStream();
   public abstract java.io.InputStream getErrorStream();
