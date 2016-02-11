bd8bb9031627e376311f0edfd275e7f7499325c4
Michael Ernst <mernst@cs.washington.edu>
2016-02-11 10:38:49 -0800

    Export shell options, such as -x for debugging, to subshells

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -4,6 +4,8 @@ ROOT=$TRAVIS_BUILD_DIR/..
 # Fail the whole script if any command fails
 set -e
 
+export SHELLOPTS
+
 ## Build annotation-tools (Annotation File Utilities)
 (cd $ROOT && (git -C annotation-tools || git clone https://github.com/typetools/annotation-tools.git))
 # This also builds jsr308-langtools
