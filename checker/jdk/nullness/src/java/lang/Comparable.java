3ea72fa7514fb95d20cc8a40b276385a28bb194c
Michael Ernst <mernst@cs.washington.edu>
2016-02-19 19:14:59 -0800

    Cherry-pick 0158725b4068d6bc5ab6e627193ea6b7a9d06281

--- checker/jdk/nullness/src/java/lang/Comparable.java
+++ checker/jdk/nullness/src/java/lang/Comparable.java
@@ -2,8 +2,9 @@ package java.lang;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.dataflow.qual.Pure;
+import org.checkerframework.framework.qual.PolyAll;
 
 public interface Comparable<T extends @NonNull Object> {
   // argument may NOT be null
-  @Pure int compareTo(T a1);
+  @Pure int compareTo(@PolyAll @NonNull T a1);
 }
