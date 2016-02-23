89ec90031200e1d9e06f6dfe64b397c92ff0a256
Suzanne Millstein <smillst@cs.washington.edu>
2016-02-23 10:23:51 -0800

    Formatting and other minor changes

--- checker/tests/nullness/Issue602.java
+++ checker/tests/nullness/Issue602.java
@@ -1,12 +1,11 @@
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.PolyNull;
 
-// Test case for Issue602.java
+// Test case for Issue 602
 // https://github.com/typetools/checker-framework/issues/602
 // @skip-test
-class Loop {
-    @PolyNull
-    String id(@PolyNull String o) {
+class Issue602 {
+    @PolyNull String id(@PolyNull String o) {
         return o;
     }
 
