117d408826c999e159fa528eda9083773751e513
Jianchu <jianchuli0624@gmail.com>
2016-02-24 12:06:40 -0500

    add test case for issue607

---
+++ checker/tests/nullness-extra/issue607/Issue607.java
@@ -0,0 +1,7 @@
+class Issue607 extends Issue607SuperClass {
+    static String simpleString = "a";
+    Issue607() {
+        super(Issue607SuperClass.issue, string -> simpleString);
+    }
+}
+
---
+++ checker/tests/nullness-extra/issue607/Issue607SuperClass.java
@@ -0,0 +1,10 @@
+public class Issue607SuperClass {
+    static Issue607Interface issue;
+    public Issue607SuperClass(Issue607Interface... issue) {
+    }
+}
+
+
+
+
+
