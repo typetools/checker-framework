37be15e775269ad343e31d1216605262673caea0
Michael Ernst <mernst@cs.washington.edu>
2016-02-25 13:41:52 -0800

    Add type qualifier BinaryNameForNonArrayInUnnamedPackage

--- checker/src/org/checkerframework/checker/signature/qual/BinaryNameForNonArrayInUnnamedPackage.java
+++ checker/src/org/checkerframework/checker/signature/qual/BinaryNameForNonArrayInUnnamedPackage.java
@@ -9,11 +9,12 @@ import org.checkerframework.framework.qual.SubtypeOf;
 /**
  * Represents a {@link BinaryName binary name} as defined in the <a
  * href="https://docs.oracle.com/javase/specs/jls/se8/html/jls-13.html#jls-13.1">Java
- * Language Specification, section 13.1</a>, but only for a non-array type.
+ * Language Specification, section 13.1</a>, but only for a non-array type
+ * in the unnamed package (also known as the default package).
  *
  * @checker_framework.manual #signature-checker Signature Checker
  */
-@SubtypeOf({BinaryName.class, ClassGetName.class})
-@ImplicitFor(stringPatterns="^[A-Za-z_][A-Za-z_0-9]*(\\.[A-Za-z_][A-Za-z_0-9]*)*(\\$[A-Za-z_0-9]+)*$")
+@SubtypeOf({BinaryNameForNonArray.class, InternalForm.class})
+@ImplicitFor(stringPatterns="^[A-Za-z_][A-Za-z_0-9]*(\\$[A-Za-z_0-9]+)*$")
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
-public @interface BinaryNameForNonArray {}
+public @interface BinaryNameForNonArrayInUnnamedPackage {}
--- checker/src/org/checkerframework/checker/signature/qual/Identifier.java
+++ checker/src/org/checkerframework/checker/signature/qual/Identifier.java
@@ -11,7 +11,7 @@ import org.checkerframework.framework.qual.SubtypeOf;
  *
  * @checker_framework.manual #signature-checker Signature Checker
  */
-@SubtypeOf({SourceNameForNonArrayNonInner.class, IdentifierOrArray.class})
+@SubtypeOf({SourceNameForNonArrayNonInner.class, BinaryNameForNonArrayInUnnamedPackage.class, IdentifierOrArray.class})
 @ImplicitFor(stringPatterns="^([A-Za-z_][A-Za-z_0-9]*)$")
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 public @interface Identifier {}
