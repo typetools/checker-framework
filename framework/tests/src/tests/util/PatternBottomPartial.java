8d8ee6c672ae62b87e4dd0b864833a9b4d950e58
Suzanne Millstein <smillst@cs.washington.edu>
2016-02-29 10:48:05 -0800

    Adapt to ImplicitFor change

--- framework/tests/src/tests/util/PatternBottomPartial.java
+++ framework/tests/src/tests/util/PatternBottomPartial.java
@@ -1,14 +1,13 @@
 package tests.util;
 
+import org.checkerframework.framework.qual.ImplicitFor;
+import org.checkerframework.framework.qual.LiteralKind;
+import org.checkerframework.framework.qual.SubtypeOf;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
-import org.checkerframework.framework.qual.ImplicitFor;
-import org.checkerframework.framework.qual.SubtypeOf;
-
-import com.sun.source.tree.Tree;
-
 @SubtypeOf({PatternAB.class, PatternBC.class, PatternAC.class})
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
-@ImplicitFor(trees={Tree.Kind.NULL_LITERAL})
+@ImplicitFor(literals = { LiteralKind.NULL })
 public @interface PatternBottomPartial {}
