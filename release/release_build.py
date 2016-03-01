4417da784f4f65f65f4494ff8cbd3323e29c827d
Javier Thaine <jthaine@cs.washington.edu>
2016-02-29 17:10:43 -0800

    Add missing argument to method call

--- release/release_build.py
+++ release/release_build.py
@@ -394,7 +394,7 @@ def main(argv):
                                    AFU_TAG_PREFIXES, AFU_MANUAL, TMP_DIR + "/afu.manual"  )
 
         if projects_to_release[CF_OPT]:
-            build_checker_framework_release(False, jsr308_version, afu_date, "", "", manual_only = True)
+            build_checker_framework_release(False, jsr308_version, afu_version, afu_date, "", "", manual_only = True)
 
             print("")
             print("The built Checker Framework manual (HTML and PDF) can be found at " + CHECKER_MANUAL)
