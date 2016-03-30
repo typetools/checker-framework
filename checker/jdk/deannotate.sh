5b34a943e47d452277e0c1e2578ed4e7041aefa8
Dan Brown <dbro@cs.washington.edu>
2016-03-29 23:09:40 -0700

    Fixes annotation argument regexp.

--- checker/jdk/deannotate.sh
+++ checker/jdk/deannotate.sh
@@ -2,19 +2,22 @@
 
 # strip Checker Framework annotations out of source files
 
-INDIR=$JSR308/annotated-jdk8u-jdk/src/share/classes
-OUTDIR=`pwd`/out
-SRC=`find ${INDIR} -name '*\.java' -print`
+INDIR=$JSR308/annotated-jdk8u-jdk/src/share/classes  # source directory
+OUTDIR=`pwd`/out                                     # output directory
+SRC=`find ${INDIR} -name '*\.java' -print`           # source files
+
 # find checker framework annotations in source directory
 ANNOS=`echo ${SRC} | xargs cat | grep 'org\.checkerframework\.' | sed 's/^.*\(org\.checkerframework\.[^); ]*\).*$/\1/g' | grep -v '\*$' | sort -u`
+QEND='\((\([^()]\|()\)*)\)\?'  # optionally matches annotation arguments
 SCMD=""
 
 # build up in SCMD the sed program that eliminates annotations in ${ANNOS}
 for f in ${ANNOS} ; do
-    QEND='\(({[^}]*})\)?'  # optional end part of annotation
+    # add both qualified and unqualified versions to SCMD
     QUAL=`echo $f | sed 's/\([^.]*\.\)*/\\\\@/'`
     SCMD="s/${QUAL}${QEND}//g;s/\\@$f${QEND}//g;$SCMD"
 done
+
 # run each source file through sed
 for f in `cd ${INDIR} && find * -name '*\.java' -print` ; do
     mkdir -p ${OUTDIR}/`dirname $f`
