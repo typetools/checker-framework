244174c2ee957d9c048a31306a8127a78abc535b
Suzanne Millstein <smillst@cs.washington.edu>
2016-04-15 14:21:04 -0700

    Remove useless utility class

--- framework/src/org/checkerframework/framework/util/ConstructorReturnUtil.java
+++
@@ -1,8 +0,0 @@
-package org.checkerframework.framework.util;
-
-/**
- * Class to hold a utility method needed by TypeFromExpression and BaseTypeVisitor.
- */
-public class ConstructorReturnUtil {
-
-}
