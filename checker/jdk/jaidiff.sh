4c0ab48aca71d599d56f37cc3ceae271daa1239e
Dan Brown <dbro@cs.washington.edu>
2016-04-18 23:03:39 -0700

    minor refactoring

--- checker/jdk/jaidiff.sh
+++ checker/jdk/jaidiff.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#"set difference" for JAIFs
+# "set difference" for JAIFs
 
 # arguments are two JAIFs, in order: "minuend" and "subtrahend"; result
 # is JAIF containing all and only annotations in minuend and not in
@@ -10,7 +10,11 @@
 JSR308="`cd $CHECKERFRAMEWORK/.. && pwd`"   # base directory
 AFU_HOME="${JSR308}/annotation-tools"
 AFU="${AFU_HOME}/annotation-file-utilities"
-CP="${AFU}/lib/plume.jar:${AFU}/bin:${AFU_HOME}/scene-lib/bin:${AFU_HOME}/asmx/bin:${AFU}/annotation-file-utilities.jar"
+AFU_JAR="${AFU}/annotation-file-utilities.jar"
+ASM=${AFU_HOME}/asmx/bin
+#ASM=${AFU}/lib/asm-5.0.jar
+PLUME="${AFU}/lib/plume.jar"
+CP="${AFU}:${AFU}/bin:${AFU_HOME}/scene-lib/bin:${ASM}:${AFU_JAR}"
 
 java -ea -cp ${CP}:${CLASSPATH} annotations.util.SceneOps diff $1 $2
 
--- checker/jdk/jaidiffs.sh
+++ checker/jdk/jaidiffs.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
 
-# jaidiff.sh for directory trees -- work in progress
+# runs jaidiff.sh ("set difference" for JAIFs) on parallel directory trees
 # input directories currently fixed as jdiff/a and jdiff/b, output as jdiff/d
 
 # parameters derived from environment
-# TOOLSJAR derived from JAVA_HOME, rest from JSR308
+# TOOLSJAR derived from JAVA_HOME, rest from CHECKERFRAMEWORK
+JSR308="`cd $CHECKERFRAMEWORK/.. && pwd`"   # base directory
+JAIDIFF=`cd \`dirname $0\` && pwd`/jaidiff.sh
 AFU_HOME="$JSR308/annotation-tools"
 AFU="${AFU_HOME}/annotation-file-utilities"
-WORKDIR="${JSR308}/checker-framework/checker/jdk"
 TOOLSJAR="${JAVA_HOME}/lib/tools.jar"
 LT_BIN="${JSR308}/jsr308-langtools/build/classes"
 CF_BIN="${JSR308}/checker-framework/checker/build"
 CP="${LT_BIN}:${TOOLSJAR}:${CLASSPATH}:${CF_BIN}"
-JAIDIFF=`cd \`dirname $0\` && pwd`/jaidiff.sh
 
-# find classes referring to checkerframework and thus presumably annotated
-# (Q: works as expected if annotations not fully qualified in source?)
-# should calculate once and save...
+# find classfiles referring to checkerframework and thus presumably annotated
 classfiles() {
     for f in `find * -name '*\.class' -print` ; do
         strings "$f" | grep -q checkerframework
@@ -24,7 +22,8 @@ classfiles() {
     done
 }
 
-# extract-annotations on directory tree
+# extracts annotations from classfiles in directory tree and stores them
+# in correspondingly named JAIFs in same directory
 extract() {
     (
     cd "$1"
@@ -34,19 +33,18 @@ extract() {
         D="`dirname $f`"
         B="`basename $f .class`"
         CLASSPATH=${CP} ${AFU}/scripts/extract-annotations "$f"
-        [ $? -ne 0 ] && echo "$f" && return 1
+        [ $? -ne 0 ] && echo "extract annotations failed on $f" && return 1
         mv "$D/*.jaif" "$D/$B.jaif"
     done
     )
     return 0
 }
 
-if [ -r "jdiffs/d" ] ; then
-    extract jdiffs/a
-    [ $? -ne 0 ] && echo "a failed" && exit 1
-    extract jdiffs/b
-    [ $? -ne 0 ] && echo "b failed" && exit 1
-fi
+mkdir -p jdiffs/d
+extract jdiffs/a
+[ $? -ne 0 ] && echo "a failed" && exit 1
+extract jdiffs/b
+[ $? -ne 0 ] && echo "b failed" && exit 1
 
 # run jaidiff.sh on a and b trees, save results to d
 cd jdiffs/a
