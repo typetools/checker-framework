fb247f44ec503347bed6dd1fda249597e6155490
Suzanne Millstein <smillst@cs.washington.edu>
2016-04-22 16:09:29 -0700

    Change the example to not rely on Java 8 libs

--- checker/tests/nullness/Issue672.java
+++ checker/tests/nullness/Issue672.java
@@ -1,18 +1,17 @@
 // Testcase for Issue 672
 // https://github.com/typetools/checker-framework/issues/672
-import java.util.concurrent.CompletionException;
 
-final class Issue672 {
+final class Issue672 extends Throwable {
     final Throwable ex;
 
     Issue672(Throwable x) { ex = x; }
 
     static Issue672 test1(Throwable x) {
-        return new Issue672(x instanceof CompletionException ? x
-                : new CompletionException(x));
+        return new Issue672(x instanceof Exception ? x
+                : new Issue672(x));
     }
     static Issue672 test2(Throwable x) {
-        return test1(x instanceof CompletionException ? x
-                : new CompletionException(x));
+        return test1(x instanceof Exception ? x
+                : new Issue672(x));
     }
 }
