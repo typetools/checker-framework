b3300e176e01594fbe56b1eaeb26ce1461640477
Michael Ernst <mernst@cs.washington.edu>
2016-05-08 08:45:25 -0700

    Reduce git clone depth

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -10,7 +10,7 @@ if [ -d ../annotation-tools ] ; then
     # Older versions of git don't support the -C command-line option
     (cd ../annotation-tools && git pull)
 else
-    (cd .. && git clone https://github.com/typetools/annotation-tools.git)
+    (cd .. && git clone --depth 1 https://github.com/typetools/annotation-tools.git)
 fi
 # This also builds jsr308-langtools
 (cd ../annotation-tools/ && ./.travis-build-without-test.sh)
