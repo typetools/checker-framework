09d644e16a83b7b9e1098d0080a06976fe15fa14
Suzanne Millstein <smillst@cs.washington.edu>
2016-05-13 08:41:24 -0700

    Explain warning supression

--- framework/tests/all-systems/Issue691.java
+++ framework/tests/all-systems/Issue691.java
@@ -3,9 +3,11 @@
 
 interface MyInterface<T> {
 }
-@SuppressWarnings("")
+
 // This code causes greatestLowerBound in the qualifier hierarchy to be executed, which results
-// in a crash if the default implementation isn't correct for a given checker.
+// in a crash if the default implementation isn't correct for a given checker. A checker could
+// issue a valid type checking error for this code, so suppress any warnings.
+@SuppressWarnings("")
 class Issue285<T> implements MyInterface<T> {
     MyInterface<?> mi = new Issue285<>();
 }
