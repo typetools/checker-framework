2b5787efeb3c3dc62b73e33a81e658d767f585a8
Javier Thaine <jthaine@cs.washington.edu>
2016-05-19 12:55:35 -0700

    Add test coverage to ensure whole-program inference respects a @SuppressWarnings("") annotation on a method when inferring annotations on method receivers and parameters.

--- framework/tests/whole-program-inference/non-annotated/OverriddenMethodsTest.java
+++ framework/tests/whole-program-inference/non-annotated/OverriddenMethodsTest.java
@@ -2,6 +2,7 @@ import tests.wholeprograminference.qual.*;
 class Parent {
     public void foo(@Sibling1 Object obj, @Sibling2 Object obj2) {}
     public void bar(@Sibling1 Parent this, @Sibling2 Object obj) {}
+    public void barz(@Sibling1 Parent this, @Sibling2 Object obj) {}
 }
 
 class Child extends Parent {
@@ -19,5 +20,17 @@ class Child extends Parent {
         @Sibling1 Child child = this;
         //:: error: (assignment.type.incompatible)
         @Sibling2 Object o = obj;
+    }
+
+    @SuppressWarnings("")
+    @Override
+    public void barz(Object obj) {}
+
+    public void callbarz(Object obj) {
+        // If the @SuppressWarnings("") on the overridden version of barz above is not
+        // respected, and the annotations on the receiver and parameter of barz are
+        // inferred, then the following call to barz will result in a method.invocation.invalid
+        // and an argument.type.incompatible type checking errors.
+        barz(obj);
     }
 }
