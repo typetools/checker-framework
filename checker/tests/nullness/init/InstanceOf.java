0b05f9553652da09866f1a4f30b90ab582a1000f
Suzanne Millstein <smillst@cs.washington.edu>
2016-05-24 10:09:20 -0700

    Format test case

--- checker/tests/nullness/init/InstanceOf.java
+++ checker/tests/nullness/init/InstanceOf.java
@@ -1,17 +1,25 @@
-import org.checkerframework.checker.nullness.qual.*;
-import org.checkerframework.checker.initialization.qual.*;
+// Test case for pull request 735
+// https://github.com/typetools/checker-framework/pull/735
+
+import org.checkerframework.checker.initialization.UnknownInitialization;
+
 class PptTopLevel {
-   class Ppt {
-     Object method() { return ""; }
-   }
-   class OtherPpt extends Ppt {}
+    class Ppt {
+        Object method() {
+            return "";
+        }
+    }
+
+    class OtherPpt extends Ppt {
+    }
 }
+
 class InstanceOf {
-    void foo( PptTopLevel.@UnknownInitialization(PptTopLevel.class) Ppt ppt) {
+    void foo(PptTopLevel.@UnknownInitialization(PptTopLevel.class) Ppt ppt) {
         ppt.method();
         if (ppt instanceof PptTopLevel.OtherPpt) {
             PptTopLevel.OtherPpt pslice = (PptTopLevel.OtherPpt) ppt;
- String     samp_str = " s" + pslice.method();
+            String samp_str = " s" + pslice.method();
         }
     }
 }
