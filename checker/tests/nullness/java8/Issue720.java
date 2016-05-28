cdf1d0cd278511cccede1b585adc3a91c3f16cba
Werner Dietl <wdietl@gmail.com>
2016-05-28 19:01:15 -0400

    Enable and fix test case.

--- checker/tests/nullness/java8/Issue720.java
+++ checker/tests/nullness/java8/Issue720.java
@@ -1,12 +1,10 @@
 // Test case for issue #720:
 // https://github.com/typetools/checker-framework/issues/720
 
-// @skip-test until the bug is fixed
-
 import java.util.function.IntConsumer;
 
 class Issue720 {
-    static IntConsumer consumer = Test::method;
+    static IntConsumer consumer = Issue720::method;
 
     static int method(int x) {
         return x;
