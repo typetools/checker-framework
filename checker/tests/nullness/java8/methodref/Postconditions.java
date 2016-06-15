2a8488234670b40753aee8c09d0cbb89cc23596b
Suzanne Millstein <smillst@cs.washington.edu>
2016-06-15 11:12:55 -0700

    Add test case of #801

--- checker/tests/nullness/java8/methodref/Postconditions.java
+++ checker/tests/nullness/java8/methodref/Postconditions.java
@@ -1,4 +1,5 @@
-
+// Test case for Issue #801
+// https://github.com/typetools/checker-framework/issues/801
 // @skip-test
 // TODO: Enable post condition override checks for method references
 
@@ -8,14 +9,23 @@ interface AssertFunc {
     @EnsuresNonNullIf(result=true, expression="#1")
     boolean testParam(final @Nullable Object param);
 }
+interface AssertFunc2 {
+    @EnsuresNonNullIf(result=true, expression="#1")
+    boolean testParam(final @Nullable Object param);
+}
 
 class AssertionTest {
     @EnsuresNonNullIf(result=true, expression="#1")
     static boolean override(final @Nullable Object param) {
         return param!=null;
     }
+    static boolean overrideAssertFunc2(final @Nullable Object param) {
+        return param!=null;
+    }
 
     void context() {
         AssertFunc f = AssertionTest::override;
+        //:: error: (methodref.receiver.postcondition)
+        AssertFunc2 f2 = AssertionTest::overrideAssertFunc2;
     }
 }
