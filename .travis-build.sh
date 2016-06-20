83aed4baf7fdf6f0e4cb3f09e1633f3245a6dad2
Suzanne Millstein <smillst@cs.washington.edu>
2016-06-20 15:38:11 -0700

    Try a bit more memory with Travis

--- .travis-build.sh
+++ .travis-build.sh
@@ -21,27 +21,13 @@ ant javadoc-private
 # that Travis truncates the log and terminates the job.
 
 ## Alternative 1 (desired alternative):
-# ant tests-nobuildjdk
+ulimit -s 1082768  # Require a bit of more memory.
+ant tests-nobuildjdk
 ## This should be redundant because it's run by tests-nobuildjdk
-# (cd checker && ant check-compilermsgs check-purity check-tutorial)
 
-## Alternative 2 (because alternative 1 currently crashes);
-## just run tests that Travis doesn't crash on.
-## Eventually, we will remove this alternative from the file.
-# Run framework tests.
-(cd framework && ant all-tests-nojtreg-nobuild)
-# Subset of all-tests
-# Also runs framework jtreg-tests
-(cd checker && ant jtreg-tests)
-(cd checker && ant command-line-tests)
-(cd checker && ant example-tests-nobuildjdk)
-(cd checker && ant check-tutorial)
-# If too many checker tests are run, the tests crash, so run one.
-(cd checker && ant nullness-base-tests regex-qual-tests lock-tests lock-safedefaults-tests)
 
 ## end of alternatives for tests
 
-ulimit -s 1082768  # Demos Require a bit of more memory.
 # It's cheaper to run the demos test here than to trigger the
 # checker-framework-demos job, which has to build the whole Checker Framework.
 (cd checker && ant check-demos)
