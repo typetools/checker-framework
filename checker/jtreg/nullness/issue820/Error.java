8da3a01b0fb936294e9b3ad5564fafc8bd637257
Werner Dietl <wdietl@gmail.com>
2016-07-16 18:54:53 -0400

    Fix Issue #820 by using the same compile policy with and without an error.

--- checker/jtreg/nullness/issue820/Error.java
+++ checker/jtreg/nullness/issue820/Error.java
@@ -1,8 +1,8 @@
 import org.checkerframework.checker.nullness.qual.*;
+
 /*
  * @test
  * @summary Test case for Issue 820 https://github.com/typetools/checker-framework/issues/820
- * @ignore
  *
  * @compile/fail/ref=ErrorAnonymousClass.out -XDrawDiagnostics -Xlint:unchecked -processor org.checkerframework.checker.nullness.NullnessChecker -AprintErrorStack -Anomsgtext Error.java AnonymousClass.java
  * @compile/fail/ref=AnonymousClass.out -XDrawDiagnostics -Xlint:unchecked -processor org.checkerframework.checker.nullness.NullnessChecker -AprintErrorStack -Anomsgtext AnonymousClass.java
