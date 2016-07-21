04810f643a24468f38b07b99c7fa42aec371850d
Martin Kellogg <kelloggm@caribou.cs.washington.edu>
2016-07-21 10:19:03 -0700

    added some documentation for the lower bound checker. Currently its just hanging out at the end of the manual...I'll find somewhere more appropriate for it later

---
+++ checker/src/org/checkerframework/checker/lowerbound/qual/GTENegativeOne.java
@@ -0,0 +1,17 @@
+package org.checkerframework.checker.lowerbound.qual;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+import org.checkerframework.framework.qual.SubtypeOf;
+import org.checkerframework.checker.lowerbound.qual.*;
+
+/**
+ * In the Lower Bound Checker's type system, this type
+ * represents any integer >= -1
+ *
+ * @checker_framework.manual #lowerbound-checker Lower Bound Checker
+ */
+
+@SubtypeOf({LowerBoundUnknown.class})
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+public @interface GTENegativeOne {}
