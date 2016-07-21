7be0e517637b29a4ca822bcaa35bc3a2450bb4b9
Michael Ernst <mernst@cs.washington.edu>
2016-07-21 05:41:35 -0700

    Partial implementation of "downstream" job for Travis

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Optional argument $1 is one of:  junit, nonjunit, misc
+# Optional argument $1 is one of:  junit, nonjunit, misc, downstream
 # If it is omitted, this script does everything.
 
 
@@ -23,9 +23,10 @@ export SHELLOPTS
 # The above command builds the JDK, so there is no need for a subsequent
 # command to rebuild it again.
 
-if [[ "$1" != "junit" && "$1" != "nonjunit" ]]; then
+if [[ "$1" != "junit" && "$1" != "nonjunit" && "$1" != "downstream" ]]; then
   ## jdkany tests: miscellaneous tests that shouldn't depend on JDK version.
-  ## (Maybe they don't even need the full ./.travis-build-without-test.sh .)
+  ## (Maybe they don't even need the full ./.travis-build-without-test.sh ;
+  ## for example they currently don't need the annotated JDK.)
 
   # Code style and formatting
   ant check-style
@@ -36,14 +37,28 @@ if [[ "$1" != "junit" && "$1" != "nonjunit" ]]; then
   make -C checker/manual all
 fi
 
-if [[ "$1" != "nonjunit" && "$1" != "misc" ]]; then
+if [[ "$1" != "nonjunit" && "$1" != "misc" && "$1" != "downstream" ]]; then
   (cd checker && ant junit-tests-nojtreg-nobuild)
 fi
 
-if [[ "$1" != "junit" && "$1" != "misc" ]]; then
+if [[ "$1" != "junit" && "$1" != "misc" && "$1" != "downstream" ]]; then
   (cd checker && ant nonjunit-tests-nojtreg-nobuild jtreg-tests)
 
   # It's cheaper to run the demos test here than to trigger the
   # checker-framework-demos job, which has to build the whole Checker Framework.
   (cd checker && ant check-demos)
+fi
+
+if [[ "$1" != "junit" && "$1" != "nonjunit" && "$1" != "misc" ]]; then
+  ## downstream tests:  projects that depend on the the Checker Framework.
+  ## These are here so they can be run by pull requests.  (Pull requests
+  ## currently don't trigger downstream jobs.)
+
+  # TODO
+  # checker-framework-demos: 15 minutes
+  # checker-framework-inference: 18 minutes
+  # daikon-typecheck: a long time, already split into multiple jobs
+  # plume-lib-typecheck: 30 minutes
+  # sparta: 1 minute, but the command is "true"!
+
 fi
