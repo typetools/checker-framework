efbfc72efb2612dc2b4cec013a4491b98e240876
Suzanne Millstein <smillst@cs.washington.edu>
2016-07-29 13:12:37 -0700

    Should stub parser warnings

--- checker/jtreg/nullness/issue824/Class2.java
+++ checker/jtreg/nullness/issue824/Class2.java
@@ -2,7 +2,7 @@
  * @test
  * @summary Test case for Issue 824 https://github.com/typetools/checker-framework/issues/824
  * @compile -XDrawDiagnostics -Xlint:unchecked ../issue824lib/Class1.java
- * @compile -XDrawDiagnostics -Xlint:unchecked -processor org.checkerframework.checker.nullness.NullnessChecker -AprintErrorStack -Anomsgtext Class2.java -Astubs=Class1.astub
+ * @compile -XDrawDiagnostics -Xlint:unchecked -processor org.checkerframework.checker.nullness.NullnessChecker -AprintErrorStack -Anomsgtext Class2.java -Astubs=Class1.astub -AstubWarnIfNotFound
  * @compile/fail/ref=Class2NoStub.out -XDrawDiagnostics -Xlint:unchecked -processor org.checkerframework.checker.nullness.NullnessChecker -AprintErrorStack -Anomsgtext Class2.java
  */
 
