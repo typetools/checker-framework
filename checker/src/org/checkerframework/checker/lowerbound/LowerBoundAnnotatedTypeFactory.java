c42c2d3ed870c7715f652450ccae3094fd7a126c
Martin Kellogg <kelloggm@cs.washington.edu>
2016-08-11 17:57:38 -0700

    fixed some javadoc comments that I screwed up

--- checker/src/org/checkerframework/checker/lowerbound/LowerBoundAnnotatedTypeFactory.java
+++ checker/src/org/checkerframework/checker/lowerbound/LowerBoundAnnotatedTypeFactory.java
@@ -303,17 +303,17 @@ public class LowerBoundAnnotatedTypeFactory
 
         /**
          *  addAnnotationForPlus handles the following cases:
-         *      lit -2 + pos -> gte-1
-         *      lit -1 + * -> call decrement
-         *      lit 0 + * -> *
-         *      lit 1 + * -> call increment
-         *      lit >= 2 + {gte-1, nn, or pos} -> pos
+         *      lit -2 + pos -&gt gte-1
+         *      lit -1 + * -&gt call decrement
+         *      lit 0 + * -&gt *
+         *      lit 1 + * -&gt call increment
+         *      lit &gt= 2 + {gte-1, nn, or pos} -&gt pos
          *      let all other lits, including sets, fall through:
-         *      pos + pos -> pos
-         *      nn + * -> *
-         *      pos + gte-1 -> nn
-         *      nn + gte-1 -> gte-1
-         *      * + * -> lbu
+         *      pos + pos -&gt pos
+         *      nn + * -&gt *
+         *      pos + gte-1 -&gt nn
+         *      nn + gte-1 -&gt gte-1
+         *      * + * -&gt lbu
          */
         public void addAnnotationForPlus(
                 ExpressionTree leftExpr, ExpressionTree rightExpr, AnnotatedTypeMirror type) {
@@ -377,8 +377,8 @@ public class LowerBoundAnnotatedTypeFactory
 
         /**
          *  addAnnotationForMinus handles the following cases:
-         *      * - lit -> call plus(*, -1 * the value of the lit)
-         *      * - * -> lbu
+         *      * - lit -&gt call plus(*, -1 * the value of the lit)
+         *      * - * -&gt lbu
          */
         public void addAnnotationForMinus(
                 ExpressionTree leftExpr, ExpressionTree rightExpr, AnnotatedTypeMirror type) {
@@ -511,11 +511,11 @@ public class LowerBoundAnnotatedTypeFactory
 
         /**
          *  addAnnotationForDivide handles these cases:
-         *	lit 0 / * -> nn (=0)
-         *      * / lit 1 -> *
-         *      pos / {pos, nn} -> nn (can round to zero)
-         *      * / {pos, nn} -> *
-         *      * / * -> lbu
+         *	lit 0 / * -&gt nn (=0)
+         *      * / lit 1 -&gt *
+         *      pos / {pos, nn} -&gt nn (can round to zero)
+         *      * / {pos, nn} -&gt *
+         *      * / * -&gt lbu
          */
         public void addAnnotationForDivide(
                 ExpressionTree leftExpr, ExpressionTree rightExpr, AnnotatedTypeMirror type) {
