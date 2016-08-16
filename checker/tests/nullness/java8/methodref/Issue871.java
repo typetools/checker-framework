1384844be7a3a7de9ce52e0afea1b46f87c1d6e4
Michael Ernst <mernst@cs.washington.edu>
2016-08-16 12:58:05 -0700

    Test cases for issues 870 and 871; to be reviewed

---
+++ checker/tests/nullness/java8/methodref/Issue871.java
@@ -0,0 +1,13 @@
+// Test case for issue #871: https://github.com/typetools/checker-framework/issues/871
+// @skip-test until it is fixed
+// @below-java8-jdk-skip-test
+
+import java.nio.file.*;
+import java.util.function.Predicate;
+interface Issue871 {
+    default Iterable<Path> a() {
+        return f(Files::isRegularFile);
+    }
+
+    Iterable<Path> f(Predicate<Path> condition) ;
+}
