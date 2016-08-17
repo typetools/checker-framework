910b65d49508e7116c6c547508e05829e9b186a1
Suzanne Millstein <smillst@cs.washington.edu>
2016-08-17 14:38:29 -0700

    Work around for #867
    Removed Value Checker specific work around for #867 and add a more general one.

--- framework/src/org/checkerframework/common/value/ValueAnnotatedTypeFactory.java
+++ framework/src/org/checkerframework/common/value/ValueAnnotatedTypeFactory.java
@@ -129,61 +129,6 @@ public class ValueAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
         return new ValueTransfer(analysis);
     }
 
-    @Override
-    public AnnotatedTypeMirror getAnnotatedType(Tree tree) {
-        if (tree.getKind() == Tree.Kind.POSTFIX_DECREMENT
-                || tree.getKind() == Tree.Kind.POSTFIX_INCREMENT) {
-
-            return getPostFixAnno((UnaryTree) tree, super.getAnnotatedType(tree));
-
-        } else {
-            return super.getAnnotatedType(tree);
-        }
-    }
-
-    private AnnotatedTypeMirror getPostFixAnno(UnaryTree tree, AnnotatedTypeMirror anno) {
-        if (anno.hasAnnotation(DoubleVal.class)) {
-            return postFixDouble(anno, tree.getKind() == Tree.Kind.POSTFIX_INCREMENT);
-        } else if (anno.hasAnnotation(IntVal.class)) {
-            return postFixInt(anno, tree.getKind() == Tree.Kind.POSTFIX_INCREMENT);
-        }
-        return anno;
-    }
-
-    private AnnotatedTypeMirror postFixInt(AnnotatedTypeMirror anno, boolean increment) {
-        List<Long> values = getIntValues(anno.getAnnotation(IntVal.class));
-        List<? extends Number> castedValues =
-                NumberUtils.castNumbers(anno.getUnderlyingType(), values);
-        List<Long> results = new ArrayList<>();
-        for (Number value : castedValues) {
-            NumberMath<?> number = NumberMath.getNumberMath(value);
-            if (increment) {
-                results.add(number.minus(1).longValue());
-            } else {
-                results.add(number.plus(1).longValue());
-            }
-        }
-        anno.replaceAnnotation(createIntValAnnotation(results));
-        return anno;
-    }
-
-    private AnnotatedTypeMirror postFixDouble(AnnotatedTypeMirror anno, boolean increment) {
-        List<Double> values = getDoubleValues(anno.getAnnotation(DoubleVal.class));
-        List<? extends Number> castedValues =
-                NumberUtils.castNumbers(anno.getUnderlyingType(), values);
-        List<Double> results = new ArrayList<>();
-        for (Number value : castedValues) {
-            NumberMath<?> number = NumberMath.getNumberMath(value);
-            if (increment) {
-                results.add(number.minus(1).doubleValue());
-            } else {
-                results.add(number.plus(1).doubleValue());
-            }
-        }
-        anno.replaceAnnotation(createDoubleValAnnotation(results));
-        return anno;
-    }
-
     /**
      * Creates an annotation of the name given with the set of values given.
      * Issues a checker warning and return UNKNOWNVAL if values.size &gt;
