2a20c415dd4833ea9e2c046b3a0b278687363ef4
Suzanne Millstein <smillst@cs.washington.edu>
2016-08-23 08:34:23 -0700

    Simplify testcase

--- framework/tests/all-systems/PolyAllTypeVar.java
+++ framework/tests/all-systems/PolyAllTypeVar.java
@@ -3,17 +3,7 @@ import java.util.Comparator;
 import org.checkerframework.dataflow.qual.Pure;
 import org.checkerframework.framework.qual.PolyAll;
 
-public final class PolyAllTypeVar<T extends Comparable<T>>
-        implements Comparator<T[]>, Serializable {
-    static final long serialVersionUID = 20150812L;
-
-    @Pure
-    @SuppressWarnings(
-            "override.param.invalid") // CF bug: does not permit expanding annotations on array elements with @Poly
-    // The signature on this method is unnecessarily strict because it
-    // requires that the component types be identical.  The signature should
-    // be compare(@PolyAll(1) T[], @PolyAll(2) T[]), but the
-    // @PolyAll qualifier does not yet take an argument.
+public final class PolyAllTypeVar<T extends Comparable<T>> {
     public int compare(@PolyAll T[] a1, @PolyAll T[] a2) {
         // Type systems that don't support dataflow will issue a warning here
         @SuppressWarnings({"guieffect", "units"})
