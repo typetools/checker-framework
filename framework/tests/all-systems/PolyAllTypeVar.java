3014ddae56b0a8b776ad93c5027c0e25ec7c0976
Suzanne Millstein <smillst@cs.washington.edu>
2016-08-23 08:36:47 -0700

    Further simplify

--- framework/tests/all-systems/PolyAllTypeVar.java
+++ framework/tests/all-systems/PolyAllTypeVar.java
@@ -1,13 +1,9 @@
-import java.io.Serializable;
-import java.util.Comparator;
-import org.checkerframework.dataflow.qual.Pure;
 import org.checkerframework.framework.qual.PolyAll;
 
 public final class PolyAllTypeVar<T extends Comparable<T>> {
-    public int compare(@PolyAll T[] a1, @PolyAll T[] a2) {
+    public void method(@PolyAll T[] a1, @PolyAll T[] a2) {
         // Type systems that don't support dataflow will issue a warning here
         @SuppressWarnings({"guieffect", "units"})
         T elt1 = a1[0];
-        return 0;
     }
 }
