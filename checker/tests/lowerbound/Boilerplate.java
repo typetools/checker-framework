30076c32f9c6f21c4ff4b9fa3b96ff9646121a0d
Martin Kellogg <kelloggm@cs.washington.edu>
2016-08-26 11:51:54 -0700

    removed all references to lower bound checker files from the upper bound branch

--- checker/tests/lowerbound/Boilerplate.java
+++
@@ -1,9 +0,0 @@
-import org.checkerframework.checker.lowerbound.qual.*;
-
-public class Boilerplate {
-
-    void test() {
-        //:: error: (assignment.type.incompatible)
-        @Positive int a = -1;
-    }
-}
