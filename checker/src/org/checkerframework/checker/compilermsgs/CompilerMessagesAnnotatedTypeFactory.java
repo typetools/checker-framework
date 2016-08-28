3bd797716b7352661781dae2a84fa38432492472
Werner Dietl <wdietl@gmail.com>
2016-08-27 23:26:11 -0400

    Address Issue 857 by adding additional qualifiers to the I18n and Compiler Message Key Checkers. Overall the code got much nicer.

--- checker/src/org/checkerframework/checker/compilermsgs/CompilerMessagesAnnotatedTypeFactory.java
+++ checker/src/org/checkerframework/checker/compilermsgs/CompilerMessagesAnnotatedTypeFactory.java
@@ -1,17 +1,9 @@
 package org.checkerframework.checker.compilermsgs;
 
-import com.sun.source.tree.Tree;
-import java.lang.annotation.Annotation;
-import java.util.Set;
 import org.checkerframework.checker.compilermsgs.qual.CompilerMessageKey;
 import org.checkerframework.checker.propkey.PropertyKeyAnnotatedTypeFactory;
-import org.checkerframework.checker.propkey.qual.PropertyKey;
-import org.checkerframework.checker.propkey.qual.PropertyKeyBottom;
-import org.checkerframework.checker.propkey.qual.UnknownPropertyKey;
 import org.checkerframework.common.basetype.BaseTypeChecker;
-import org.checkerframework.framework.type.treeannotator.ImplicitsTreeAnnotator;
 import org.checkerframework.framework.type.treeannotator.ListTreeAnnotator;
-import org.checkerframework.framework.type.treeannotator.PropagationTreeAnnotator;
 import org.checkerframework.framework.type.treeannotator.TreeAnnotator;
 
 /**
@@ -26,19 +18,9 @@ public class CompilerMessagesAnnotatedTypeFactory extends PropertyKeyAnnotatedTy
     }
 
     @Override
-    protected Set<Class<? extends Annotation>> createSupportedTypeQualifiers() {
-        return getBundledTypeQualifiersWithoutPolyAll(
-                UnknownPropertyKey.class, PropertyKey.class, PropertyKeyBottom.class);
-    }
-
-    @Override
     public TreeAnnotator createTreeAnnotator() {
-        ImplicitsTreeAnnotator implicitsTreeAnnotator = new ImplicitsTreeAnnotator(this);
-        implicitsTreeAnnotator.addTreeKind(Tree.Kind.NULL_LITERAL, PROPKEY_BOTTOM);
-
         return new ListTreeAnnotator(
-                new PropagationTreeAnnotator(this),
-                implicitsTreeAnnotator,
+                super.createBasicTreeAnnotator(),
                 new KeyLookupTreeAnnotator(this, CompilerMessageKey.class));
     }
 }
--- checker/src/org/checkerframework/checker/compilermsgs/qual/CompilerMessageKeyBottom.java
+++ checker/src/org/checkerframework/checker/compilermsgs/qual/CompilerMessageKeyBottom.java
@@ -5,18 +5,24 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.checkerframework.framework.qual.DefaultFor;
+import org.checkerframework.framework.qual.ImplicitFor;
+import org.checkerframework.framework.qual.LiteralKind;
 import org.checkerframework.framework.qual.SubtypeOf;
+import org.checkerframework.framework.qual.TypeUseLocation;
 
 /**
- * The annotation to distinguish compiler message Strings from
- * normal Strings. The programmer should hardly ever need to use it
- * explicitly.
+ * The bottom qualifier for the Compiler Message Key Checker.
  *
- * @author wmdietl
  * @checker_framework.manual #compilermsgs-checker Compiler Message Key Checker
  */
-@SubtypeOf(UnknownCompilerMessageKey.class)
+@SubtypeOf(CompilerMessageKey.class)
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
-public @interface CompilerMessageKey {}
+@ImplicitFor(
+    typeNames = {java.lang.Void.class},
+    literals = {LiteralKind.NULL}
+)
+@DefaultFor(TypeUseLocation.LOWER_BOUND)
+public @interface CompilerMessageKeyBottom {}
