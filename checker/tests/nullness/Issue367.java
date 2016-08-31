86925e1915321289536592da6e9ef7a6e782d735
Suzanne Millstein <smillst@cs.washington.edu>
2016-08-31 10:16:57 -0700

    Issue #870 was a duplicate of Issue #367
    Issue #870 was fixed in d34886dec67067 so #367 is fixed, too.

--- checker/tests/nullness/Issue367.java
+++ checker/tests/nullness/Issue367.java
@@ -1,6 +1,5 @@
 // Test case for Issue 367:
 // https://github.com/typetools/checker-framework/issues/367
-// @skip-test This requires capture to typecheck correctly
 
 class Test {
     static void test(Iterable<? extends Thread> threads) {
