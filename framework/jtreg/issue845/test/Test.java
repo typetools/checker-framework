dced965e02fc4a54dcc830ffadcc03f9be23021e
Suzanne Millstein <smillst@cs.washington.edu>
2016-09-23 08:32:19 -0700

    Framework jtreg tests should fail on error
    Ignore test case that is currently failing.

--- framework/jtreg/issue845/test/Test.java
+++ framework/jtreg/issue845/test/Test.java
@@ -1,4 +1,5 @@
 /*
+ * @ignore
  * @test
  * @summary Testing that a checker isn't required to be in a package. (Issue 845, https://github.com/typetools/checker-framework/issues/845)  The checker java files have to be in a seperate directory otherwise $CHECKERFRAMEWORK/framework/jtreg/checker/qual directory is on the classpath before the qual directory with the compiled classes.
  *
