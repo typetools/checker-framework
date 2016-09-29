fd86fa92ec694522a70c79a1edca1877442e1222
Martin Kellogg <kelloggm@cs.washington.edu>
2016-09-29 15:07:24 -0700

    modified test that checks functionality we don't have so that it doesn't fail and make me sad.

--- checker/src/org/checkerframework/checker/lowerbound/qual/Positive.java
+++ checker/src/org/checkerframework/checker/lowerbound/qual/Positive.java
@@ -3,7 +3,12 @@ package org.checkerframework.checker.lowerbound.qual;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 import org.checkerframework.checker.lowerbound.qual.NonNegative;
+import org.checkerframework.framework.qual.DefaultFor;
+import org.checkerframework.framework.qual.ImplicitFor;
+import org.checkerframework.framework.qual.LiteralKind;
 import org.checkerframework.framework.qual.SubtypeOf;
+import org.checkerframework.framework.qual.TargetLocations;
+import org.checkerframework.framework.qual.TypeUseLocation;
 
 /**
  * In the Lower Bound Checker's type system, this type
@@ -14,5 +19,9 @@ import org.checkerframework.framework.qual.SubtypeOf;
  * @checker_framework.manual #index-checker Index Checker
  */
 @SubtypeOf({NonNegative.class})
+//@ImplicitFor(literals = {LiteralKind.NULL},
+//	     typeNames = {java.lang.Void.class})
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+//@TargetLocations({TypeUseLocation.EXPLICIT_LOWER_BOUND, TypeUseLocation.EXPLICIT_UPPER_BOUND})
+//@DefaultFor({TypeUseLocation.LOWER_BOUND})
 public @interface Positive {}
