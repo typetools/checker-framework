128ad2fab571aa1acad4986c861b34bf7c7efec7
Joseph Santino <jsantino@attu1.cs.washington.edu>
2016-10-01 12:47:38 -0700

    fix listsupport test

--- checker/tests/lowerbound/ListSupport.java
+++ checker/tests/lowerbound/ListSupport.java
@@ -1,17 +1,19 @@
 import java.util.*;
 
-public class ListSupport {
-	
-	void testGet(){
-		List<Integer> list = new ArrayList<Integer>();
-		int i = -1;
-		int j = 0;
-		
-		// try and use a negative to get, should fail
-		//:: error: (assignment.type.incompatible)
-		list.get(i);
-		
-		//try and us a nonnegative, should work
-		list.get(j)
-	}
+class ListSupport {
+
+    void testGet() {
+
+        List<Integer> list = new ArrayList<Integer>();
+        int i = -1;
+        int j = 0;
+
+        //try and use a negative to get, should fail
+        //:: error: (argument.type.incompatible)
+        Integer m = list.get(i);
+
+        //try and use a nonnegative, should work
+        Integer l = list.get(j);
+    }
 }
+//a comment
