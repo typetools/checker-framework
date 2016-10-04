b774b98ddd2d3c555e5e5e3cc0848f36960a968b
Martin Kellogg <kelloggm@cs.washington.edu>
2016-10-04 10:46:04 -0700

    thought my last commit added this test but apparently not? Whatever, adding it now.

--- checker/tests/lowerbound/LengthOfArrayMinusOne.java
+++ checker/tests/lowerbound/LengthOfArrayMinusOne.java
@@ -1,10 +1,10 @@
 class LengthOfArrayMinusOne {
     void test(int[] arr) {
-	//:: warning: (array.access.unsafe.low)
+        //:: warning: (array.access.unsafe.low)
         int i = arr[arr.length - 1];
 
-	if (arr.length > 0) {
-	    int j = arr[arr.length - 1];
-	}
+        if (arr.length > 0) {
+            int j = arr[arr.length - 1];
+        }
     }
 }
