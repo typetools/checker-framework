906ded17bf6b16f5a7e98a82cf8ab17855364eb3
Martin Kellogg <kelloggm@cs.washington.edu>
2016-10-10 13:51:46 -0700

    formatting

--- checker/tests/lowerbound/HexEncode.java
+++ checker/tests/lowerbound/HexEncode.java
@@ -2,7 +2,7 @@ import java.lang.StringBuffer;
 
 public class HexEncode {
     private static final char[] digits = {
-	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
     };
 
     public static String hexEncode(byte[] bytes) {
