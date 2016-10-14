ea09a0411ebacf993a736467c5005705e8471a98
Werner Dietl <wdietl@gmail.com>
2016-10-14 01:02:38 -0400

    Expand on tests of Opt.

--- checker/tests/nullness/flow/TestOpt.java
+++ checker/tests/nullness/flow/TestOpt.java
@@ -4,9 +4,26 @@ import org.checkerframework.checker.nullness.qual.*;
 /** Test class org.checkerframework.checker.nullness.Opt. */
 // @below-java8-jdk-skip-test
 class TestOpt {
-    void foo(@Nullable Object p) {
+    void foo1(@Nullable Object p) {
         if (Opt.isPresent(p)) {
             p.toString(); // Flow refinement
         }
+    }
+
+    void foo2(@Nullable Object p) {
+        if (!Opt.isPresent(p)) {
+            //:: error: (dereference.of.nullable)
+            p.toString();
+        }
+    }
+
+    void foo3(@Nullable Object p) {
+        Opt.ifPresent(p, x -> System.out.println("Got: " + x));
+    }
+
+    void foo4(@Nullable Object p) {
+        // TODO: we should infer from ifPresent that x is non-null.
+        //:: error: (dereference.of.nullable)
+        Opt.ifPresent(p, x -> System.out.println("Got: " + x.toString()));
     }
 }
