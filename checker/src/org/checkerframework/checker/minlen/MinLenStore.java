df3b965d8dd11b115d86a266719f0a336fa50c22
Joseph Santino <jsantino@quattro.cs.washington.edu>
2016-10-14 12:10:02 -0700

    Make clear and remove transfer ignore MinLenBottom

--- checker/src/org/checkerframework/checker/minlen/MinLenStore.java
+++ checker/src/org/checkerframework/checker/minlen/MinLenStore.java
@@ -70,16 +70,18 @@ public class MinLenStore extends CFAbstractStore<MinLenValue, MinLenStore> {
         MinLenAnnotatedTypeFactory factory = (MinLenAnnotatedTypeFactory) atypeFactory;
         MinLenValue value = this.getValue(rec);
         AnnotatedTypeMirror atm = value.getType();
-        if (isClear) {
-            MinLenValue val =
-                    analysis.createSingleAnnotationValue(factory.createMinLen(0), rec.getType());
-            replace.put(rec, val);
-        } else {
-            int length = MinLenAnnotatedTypeFactory.getMinLenValue(atm.getAnnotation(MinLen.class));
-            MinLenValue val =
-                    analysis.createSingleAnnotationValue(
-                            factory.createMinLen(Math.max(length - 1, 0)), rec.getType());
-            replace.put(rec, val);
+        if (atm.hasAnnotation(MinLen.class)) {
+	        if (isClear) {
+	            MinLenValue val =
+	                    analysis.createSingleAnnotationValue(factory.createMinLen(0), rec.getType());
+	            replace.put(rec, val);
+	        } else {
+	            int length = MinLenAnnotatedTypeFactory.getMinLenValue(atm.getAnnotation(MinLen.class));
+	            MinLenValue val =
+	                    analysis.createSingleAnnotationValue(
+	                            factory.createMinLen(Math.max(length - 1, 0)), rec.getType());
+	            replace.put(rec, val);
+	        }
         }
     }
 
