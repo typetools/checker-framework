dbc8d7a0f83984cfe1f297c840393933e3004ce6
Werner Dietl <wdietl@gmail.com>
2016-10-16 03:13:49 -0400

    Remove trailing whitespace.

--- stubparser/src/org/checkerframework/stubparser/ast/visitor/DumpVisitor.java
+++ stubparser/src/org/checkerframework/stubparser/ast/visitor/DumpVisitor.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2007 Júlio Vilmar Gesser.
- * 
+ *
  * This file is part of Java 1.5 parser and Abstract Syntax Tree.
  *
  * Java 1.5 parser and Abstract Syntax Tree is free software: you can redistribute it and/or modify
