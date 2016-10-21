c5c327934dc60877d019cc37f68dd7f63128f7d7
Joseph Santino <jsantino@sparta.cs.washington.edu>
2016-10-21 16:27:24 -0700

    comment out test for upperbound, since features are not currently under development

--- checker/tests/upperbound/javaAnnotationTest.java
+++ checker/tests/upperbound/javaAnnotationTest.java
@@ -1,15 +1,17 @@
 import java.util.*;
 import org.checkerframework.checker.upperbound.qual.*;
 
-class ListSupport {
+class ViewpointAdaptTest {
+    /*
+        void ListGet(
+                @LessThanLength("list") int index,
+                @LessThanOrEqualToLength("list") int notIndex,
+                List<Integer> list) {
+            ////:: error: (argument.type.incompatible)
+            list.get(index);
 
-    void ListGet(
-            @LessThanLength("list") int index,
-            @LessThanOrEqualToLength("list") int notIndex,
-            List<Integer> list) {
-        list.get(index);
-
-        //:: error: (argument.type.incompatible)
-        list.get(notIndex);
-    }
+            ////:: error: (argument.type.incompatible)
+            list.get(notIndex);
+        }
+    */
 }
