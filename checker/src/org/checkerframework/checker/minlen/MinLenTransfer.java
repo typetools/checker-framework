d8fff3a605c98d68b936dc873acdc307a701fd49
Martin Kellogg <kelloggm@cs.washington.edu>
2016-10-27 16:10:26 -0700

    added support and tests for equal and not equal to in the minlen transfer function. This should fix the failing upper bound test, as well as all outstanding minlen failures.

--- checker/src/org/checkerframework/checker/minlen/MinLenTransfer.java
+++ checker/src/org/checkerframework/checker/minlen/MinLenTransfer.java
@@ -14,10 +14,12 @@ import org.checkerframework.dataflow.analysis.FlowExpressions.Receiver;
 import org.checkerframework.dataflow.analysis.TransferInput;
 import org.checkerframework.dataflow.analysis.TransferResult;
 import org.checkerframework.dataflow.cfg.node.FieldAccessNode;
+import org.checkerframework.dataflow.cfg.node.EqualToNode;
 import org.checkerframework.dataflow.cfg.node.GreaterThanNode;
 import org.checkerframework.dataflow.cfg.node.GreaterThanOrEqualNode;
 import org.checkerframework.dataflow.cfg.node.LessThanNode;
 import org.checkerframework.dataflow.cfg.node.LessThanOrEqualNode;
+import org.checkerframework.dataflow.cfg.node.NotEqualNode;
 import org.checkerframework.dataflow.cfg.node.MethodInvocationNode;
 import org.checkerframework.dataflow.cfg.node.Node;
 import org.checkerframework.framework.flow.CFAbstractTransfer;
@@ -148,6 +150,108 @@ public class MinLenTransfer extends CFAbstractTransfer<MinLenValue, MinLenStore,
         return rfi.newResult;
     }
 
+     @Override
+    public TransferResult<MinLenValue, MinLenStore> visitEqualTo(
+            EqualToNode node, TransferInput<MinLenValue, MinLenStore> in) {
+        TransferResult<MinLenValue, MinLenStore> result = super.visitEqualTo(node, in);
+
+        RefinementInfo rfi =
+                new RefinementInfo(result, in, node.getRightOperand(), node.getLeftOperand());
+
+        // Refine the then branch.
+        refineGTE(rfi.right, rfi.rightType, rfi.left, rfi.leftType, rfi.thenStore);
+        refineGTE(rfi.left, rfi.leftType, rfi.right, rfi.rightType, rfi.thenStore);
+
+	// The else branch should only be refined if a length is being compared
+	// to zero. The following code block implements this special case.
+	// This special case occurs because zero is a hard bound on the bottom
+	// of the array (i.e. no array can be smaller than zero), so in this
+	// case the MinLen of the array is one.
+	refineZeroEquality(rfi.right, rfi.rightType, rfi.left, rfi.leftType, rfi.elseStore);
+	refineZeroEquality(rfi.left, rfi.leftType, rfi.right, rfi.rightType, rfi.elseStore);
+		
+        return rfi.newResult;
+    }
+
+     @Override
+    public TransferResult<MinLenValue, MinLenStore> visitNotEqual(
+            NotEqualNode node, TransferInput<MinLenValue, MinLenStore> in) {
+        TransferResult<MinLenValue, MinLenStore> result = super.visitNotEqual(node, in);
+
+        RefinementInfo rfi =
+                new RefinementInfo(result, in, node.getRightOperand(), node.getLeftOperand());
+
+        // Refine the then branch.
+        refineGTE(rfi.right, rfi.rightType, rfi.left, rfi.leftType, rfi.elseStore);
+        refineGTE(rfi.left, rfi.leftType, rfi.right, rfi.rightType, rfi.elseStore);
+
+	// The then branch should only be refined if a length is being compared
+	// to zero. The following code block implements this special case.
+	// This special case occurs because zero is a hard bound on the bottom
+	// of the array (i.e. no array can be smaller than zero), so in this
+	// case the MinLen of the array is one.
+	refineZeroEquality(rfi.right, rfi.rightType, rfi.left, rfi.leftType, rfi.thenStore);
+	refineZeroEquality(rfi.left, rfi.leftType, rfi.right, rfi.rightType, rfi.thenStore);
+
+	
+        return rfi.newResult;
+    }
+
+    private void refineZeroEquality(Node left, Set<AnnotationMirror> leftType,
+				    Node right, Set<AnnotationMirror> rightType,
+				    MinLenStore store) {
+	FieldAccessNode fi = null;
+        Tree tree = null;
+        Receiver rec = null;
+        Set<AnnotationMirror> type = null;
+        // We only care about length. This will miss an expression which
+        // include an array length (like "a.length + 1"), but that's okay
+        // for now.
+        // FIXME: Joe: List support will be needed here too.
+
+        if (left instanceof FieldAccessNode) {
+            fi = (FieldAccessNode) left;
+            tree = right.getTree();
+            rec = FlowExpressions.internalReprOf(analysis.getTypeFactory(), fi.getReceiver());
+            type = leftType;
+        } else {
+            return;
+        }
+
+        if (fi == null || tree == null || rec == null || type == null) {
+            return;
+        }
+	 if (fi.getFieldName().equals("length")
+                && fi.getReceiver().getType().getKind() == TypeKind.ARRAY) {
+            // At this point, MinLen needs to invoke the constant value checker
+            // to find out if it knows anything about what the length is being
+            // compared to. If so, we can do something.
+
+            int newMinLen = atypeFactory.minLenFromValueType(atypeFactory.valueTypeFromTree(tree));
+
+	    // We must be comparing against zero here; otherwise, we should be using
+	    // refineGTE.
+	    if (newMinLen != 0) {
+		return;
+	    }
+
+            AnnotationMirror anno = AnnotationUtils.getAnnotationByClass(type, MinLen.class);
+            if (!AnnotationUtils.hasElementValue(anno, "value")) {
+                return;
+            }
+
+            Integer currentMinLen =
+                    AnnotationUtils.getElementValue(anno, "value", Integer.class, true);
+
+            if (1 > currentMinLen) {
+                store.insertValue(rec, atypeFactory.createMinLen(1));
+                return;
+            }
+
+            return;
+        }
+    }
+    
     private void refineGT(
             Node left,
             Set<AnnotationMirror> leftType,
@@ -168,11 +272,6 @@ public class MinLenTransfer extends CFAbstractTransfer<MinLenValue, MinLenStore,
             tree = right.getTree();
             rec = FlowExpressions.internalReprOf(analysis.getTypeFactory(), fi.getReceiver());
             type = leftType;
-        } else if (right instanceof FieldAccessNode) {
-            fi = (FieldAccessNode) right;
-            tree = left.getTree();
-            rec = FlowExpressions.internalReprOf(analysis.getTypeFactory(), fi.getReceiver());
-            type = rightType;
         } else {
             return;
         }
