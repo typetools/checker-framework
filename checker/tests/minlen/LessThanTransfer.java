cc99811da3b1e79e7028abd031c14388ccd7a898
Martin Kellogg <kelloggm@cs.washington.edu>
2016-10-27 15:22:03 -0700

    new tests for min len transfer

---
+++ checker/tests/minlen/LessThanTransfer.java
@@ -0,0 +1,15 @@
+import org.checkerframework.checker.minlen.qual.*;
+
+class LessThanTransfer {
+    void lt_check(int[] a) {
+	if (0 < a.length) {
+	    int @MinLen(1) [] b = a;
+	}
+    }
+    void lt_bad_check(int[] a) {
+	if (0 < a.length) {
+	    //:: error: (assignment.type.incompatible)
+	    int @MinLen(2) [] b = a;
+	}
+    }
+}
