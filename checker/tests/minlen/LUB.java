937920016b0eb7757ec1158fe9d9a6e6b3c13b82
Joseph Santino <jsantino@aeb.cs.washington.edu>
2016-11-02 15:16:10 -0700

    Change comparison in LeastUpperBound; add tests for LUB

---
+++ checker/tests/minlen/LUB.java
@@ -0,0 +1,44 @@
+
+import org.checkerframework.checker.minlen.qual.*;
+
+public class LUB {
+
+    public static void MinLen(int @MinLen(10) [] arg, int @MinLen(4) [] arg2) {
+        int[] arr;
+        if (true) {
+            arr = arg;
+        } else {
+            arr = arg2;
+        }
+        //:: error: (assignment.type.incompatible)
+        int @MinLen(10) [] res = arr;
+
+        int @MinLen(4) [] res2 = arr;
+    }
+
+    public static void Bottom(int @MinLenBottom [] arg, int @MinLen(4) [] arg2) {
+        int[] arr;
+        if (true) {
+            arr = arg;
+        } else {
+            arr = arg2;
+        }
+        //:: error: (assignment.type.incompatible)
+        int @MinLen(10) [] res = arr;
+
+        int @MinLenBottom [] res2 = arr;
+    }
+
+    public static void BothBottom(int @MinLenBottom [] arg, int @MinLenBottom [] arg2) {
+        int[] arr;
+        if (true) {
+            arr = arg;
+        } else {
+            arr = arg2;
+        }
+        //:: error: (assignment.type.incompatible)
+        int @MinLen(10) [] res = arr;
+
+        int @MinLenBottom [] res2 = arr;
+    }
+}
