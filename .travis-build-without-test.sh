8597479d024d55979ea4be59a9133e75913cad70
Werner Dietl <wdietl@gmail.com>
2016-11-21 21:20:51 -0500

    Use REPO_SLUG to ensure dependencies stay within repository organization. (#971)

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -5,12 +5,14 @@ set -e
 
 export SHELLOPTS
 
+SLUGOWNER=${TRAVIS_REPO_SLUG%/*}
+
 ## Build annotation-tools (Annotation File Utilities)
 if [ -d ../annotation-tools ] ; then
     # Older versions of git don't support the -C command-line option
     (cd ../annotation-tools && git pull)
 else
-    (cd .. && git clone --depth 1 https://github.com/typetools/annotation-tools.git)
+    (cd .. && git clone --depth 1 https://github.com/${SLUGOWNER}/annotation-tools.git)
 fi
 # This also builds jsr308-langtools
 (cd ../annotation-tools/ && ./.travis-build-without-test.sh)
