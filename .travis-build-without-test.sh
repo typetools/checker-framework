10cd3b839a3e9f86e93ba141704eb9bc1ea25400
Werner Dietl <wdietl@gmail.com>
2016-11-22 23:46:16 -0500

    Ensure that annotation-tools exists within the same repository organization.

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -12,8 +12,14 @@ if [ -d ../annotation-tools ] ; then
     # Older versions of git don't support the -C command-line option
     (cd ../annotation-tools && git pull)
 else
-    (cd .. && git clone --depth 1 https://github.com/${SLUGOWNER}/annotation-tools.git)
+    git ls-remote https://github.com/${SLUGOWNER}/annotation-tools.git &>-
+    if [ "$?" -eq 0 ]; then
+        (cd .. && git clone --depth 1 https://github.com/${SLUGOWNER}/annotation-tools.git)
+    else
+        (cd .. && git clone --depth 1 https://github.com/typetools/annotation-tools.git)
+    fi
 fi
+
 # This also builds jsr308-langtools
 (cd ../annotation-tools/ && ./.travis-build-without-test.sh)
 
