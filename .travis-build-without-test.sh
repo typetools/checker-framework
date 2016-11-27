bed3e77edd478731cb7a59e90253e09c4c4aaf85
Michael Ernst <mernst@cs.washington.edu>
2016-11-27 06:56:14 -0800

    Don't fail script when testing existence of remote repository

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -12,16 +12,19 @@ if [ -d ../annotation-tools ] ; then
     # Older versions of git don't support the -C command-line option
     (cd ../annotation-tools && git pull)
 else
+    set +e
     git ls-remote https://github.com/${SLUGOWNER}/annotation-tools.git &>-
     if [ "$?" -eq 0 ]; then
         (cd .. && git clone --depth 1 https://github.com/${SLUGOWNER}/annotation-tools.git)
     else
         (cd .. && git clone --depth 1 https://github.com/typetools/annotation-tools.git)
     fi
+    set -e
 fi
 
 # This also builds jsr308-langtools
 (cd ../annotation-tools/ && ./.travis-build-without-test.sh)
 
 ## Compile
+echo "running \"ant dist\" for checker-framework"
 ant dist
