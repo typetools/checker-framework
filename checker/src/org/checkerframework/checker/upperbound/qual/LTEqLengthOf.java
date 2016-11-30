c65bc00dc62f37cfb0b9be75d83080d6e423e0f3
Martin Kellogg <kelloggm@cs.washington.edu>
2016-11-30 15:51:55 -0800

    oops forgot to commit the new qual definitions

---
+++ checker/src/org/checkerframework/checker/upperbound/qual/LTEqLengthOf.java
@@ -0,0 +1,18 @@
+package org.checkerframework.checker.upperbound.qual;
+
+import java.lang.annotation.*;
+import org.checkerframework.framework.qual.*;
+
+/**
+ * Indicates that the expression annotated with this type qualifier is less than or equal to the
+ * length of the listed arrays.
+ *
+ * @checker_framework.manual #index-checker Index Checker
+ */
+@SubtypeOf(UpperBoundUnknown.class)
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+public @interface LTEqLengthOf {
+    public String[] value();
+}
