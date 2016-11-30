85be0609889d0e25705eb44d2785092f62d0ff33
Martin Kellogg <kelloggm@cs.washington.edu>
2016-11-30 15:52:47 -0800

    formatting

--- checker/tests/upperbound/Issue20.java
+++ checker/tests/upperbound/Issue20.java
@@ -4,8 +4,7 @@ class Issue20 {
     // An issue with LUB that results in losing information when unifying.
     int[] a, b;
 
-    void test(
-            @LTLengthOf("a") int i, @LTEqLengthOf({"a", "b"}) int j, boolean flag) {
+    void test(@LTLengthOf("a") int i, @LTEqLengthOf({"a", "b"}) int j, boolean flag) {
         @LTEqLengthOf("a") int k = flag ? i : j;
     }
 }
