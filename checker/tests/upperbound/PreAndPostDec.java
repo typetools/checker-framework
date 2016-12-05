5df433345b75990379437a2352b0570a4c4602af
Joe Santino <jsantino@cs.washington.edu>
2016-12-05 12:14:32 -0800

    Test Case for issue #39

---
+++ checker/tests/upperbound/PreAndPostDec.java
@@ -0,0 +1,40 @@
+import org.checkerframework.checker.lowerbound.qual.*;
+import org.checkerframework.checker.minlen.qual.*;
+
+// @skip-test until the bug is fixed
+
+public class PreAndPostDec {
+
+  void pre1(int [] args) {
+    int ii = 0;
+    while ((ii < args.length)) {
+      //:: warning: (array.access.unsafe.high)
+      int m = args[++ii];
+    }
+  }
+
+  void pre2(int [] args) {
+    int ii = 0;
+    while ((ii < args.length)) {
+      ii++;
+      //:: warning: (array.access.unsafe.high)
+      int m = args[ii];
+    }
+  }
+
+  void post1(int [] args) {
+    int ii = 0;
+    while ((ii < args.length)) {
+      int m = args[ii++];
+    }
+  }
+
+  void post2(int [] args) {
+    int ii = 0;
+    while ((ii < args.length)) {
+      int m = args[ii];
+      ii++;
+    }
+  }
+
+}
