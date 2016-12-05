9abfa8f790242828f009d257339b3f31381dfcc1
Joe Santino <jsantino@cs.washington.edu>
2016-12-05 11:43:09 -0800

    Test case for issue #36

---
+++ checker/tests/minlen/equalTo.java
@@ -0,0 +1,12 @@
+import org.checkerframework.checker.minlen.qual.*;
+
+// @skip-test until the bug is fixed
+
+class equalTo {
+
+    public static void equalToMinLen(@MinLen(2) String m, @MinLen(0) String r) {
+        if ( r == m ) {
+		@MinLen(2) String j = r;
+	}
+    }
+}
