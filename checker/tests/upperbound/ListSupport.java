a0f005bbcb2efc876484720cd2066e741af586d1
Joseph Santino <jsantino@semanti.cs.washington.edu>
2016-12-06 13:58:19 -0800

    clean up incomplete Upperbound functionality for merge

--- checker/tests/upperbound/ListSupport.java
+++ checker/tests/upperbound/ListSupport.java
@@ -1,111 +1,102 @@
 import java.util.*;
 import org.checkerframework.checker.upperbound.qual.*;
-
+// @skip-test
 class ListSupport {
-    /*
-        void ListGet(
-                @LTLengthOf("list") int index,
-                @LTEqLengthOf("list") int notIndex,
-                List<Integer> list) {
-            list.get(index);
 
-            ////:: error: (argument.type.incompatible)
-            list.get(notIndex);
-        }
+    void ListGet(
+            @LTLengthOf("list") int index, @LTEqLengthOf("list") int notIndex, List<Integer> list) {
+        list.get(index);
 
-        void ListGetWrongName(@LTLengthOf("arr") int index, List<Integer> list) {
-            ////:: error: (argument.type.imcompatible)
-            list.get(index);
-        }
+        //:: error: (argument.type.incompatible)
+        list.get(notIndex);
+    }
 
-        void indexOf(List<Integer> list) {
-            int index = list.indexOf(0);
+    void ListGetWrongName(@LTLengthOf("arr") int index, List<Integer> list) {
+        //:: error: (argument.type.imcompatible)
+        list.get(index);
+    }
 
-            @LTLengthOf("list") int i = index;
+    void indexOf(List<Integer> list) {
+        int index = list.indexOf(0);
 
-            ////:: error: (assignment.type.incompatible)
-            @UpperBoundBottom int i2 = index;
-        }
+        @LTLengthOf("list") int i = index;
 
-        void lastIndexOf(List<Integer> list) {
-            int index = list.lastIndexOf(0);
+        //:: error: (assignment.type.incompatible)
+        @UpperBoundBottom int i2 = index;
+    }
 
-            @LTLengthOf("list") int i = index;
+    void lastIndexOf(List<Integer> list) {
+        int index = list.lastIndexOf(0);
 
-            ////:: error: (assignment.type.incompatible)
-            @UpperBoundBottom int i2 = index;
-        }
+        @LTLengthOf("list") int i = index;
 
-        void subList(
-                List<Integer> list,
-                @LTLengthOf("list") int index,
-                @LTEqLengthOf("list") int endIndex) {
-            List<Integer> list2 = list.subList(index, endIndex);
+        //:: error: (assignment.type.incompatible)
+        @UpperBoundBottom int i2 = index;
+    }
 
-            //// start index must be strictly lessthanlength
-            ////:: error: (argument.type.incompatible)
-            list2 = list.subList(endIndex, endIndex);
+    void subList(
+            List<Integer> list, @LTLengthOf("list") int index, @LTEqLengthOf("list") int endIndex) {
+        List<Integer> list2 = list.subList(index, endIndex);
 
-            //// edindex must be less than or equal to Length
-            ////:: error: (argument.type.incompatible)
-            list2 = list.subList(index, 28);
-        }
+        // start index must be strictly lessthanlength
+        //:: error: (argument.type.incompatible)
+        list2 = list.subList(endIndex, endIndex);
 
-        void size(List<Integer> list) {
-            int i = list.size();
-            @LTEqLengthOf("list") int k = i;
+        // edindex must be less than or equal to Length
+        //:: error: (argument.type.incompatible)
+        list2 = list.subList(index, 28);
+    }
 
-            ////:: error: (assignment.type.incompatible)
-            @LTLengthOf("list") int m = i;
-        }
+    void size(List<Integer> list) {
+        int i = list.size();
+        @LTEqLengthOf("list") int k = i;
 
-        void remove(
-                List<Integer> list,
-                @LTLengthOf("list") int index,
-                @LTEqLengthOf("list") int endIndex) {
-            int i = index;
-            int j = endIndex;
-            list.remove(0);
+        //:: error: (assignment.type.incompatible)
+        @LTLengthOf("list") int m = i;
+    }
 
-            ////:: error: (assignment.type.incompatible)
-            @LTLengthOf("list") int less = i;
+    void remove(
+            List<Integer> list, @LTLengthOf("list") int index, @LTEqLengthOf("list") int endIndex) {
+        int i = index;
+        int j = endIndex;
+        list.remove(0);
 
-            @LTEqLengthOf("list") int loe = i;
+        //:: error: (assignment.type.incompatible)
+        @LTLengthOf("list") int less = i;
 
-            ////:: error: (assignment.type.incompatible)
-            @LTEqLengthOf("list") int loe2 = j;
-        }
+        @LTEqLengthOf("list") int loe = i;
 
-        void clear(
-                List<Integer> list,
-                @LTLengthOf("list") int index,
-                @LTEqLengthOf("list") int endIndex) {
-            int i = index;
-            int j = endIndex;
-            list.clear();
+        //:: error: (assignment.type.incompatible)
+        @LTEqLengthOf("list") int loe2 = j;
+    }
 
-            ////:: error: (assignment.type.incompatible)
-            @LTLengthOf("list") int less = i;
+    void clear(
+            List<Integer> list, @LTLengthOf("list") int index, @LTEqLengthOf("list") int endIndex) {
+        int i = index;
+        int j = endIndex;
+        list.clear();
 
-            ////:: error: (assignment.type.incompatible)
-            @LTEqLengthOf("list") int loe = i;
+        //:: error: (assignment.type.incompatible)
+        @LTLengthOf("list") int less = i;
 
-            ////:: error: (assignment.type.incompatible)
-            @LTEqLengthOf("list") int loe2 = j;
-        }
+        //:: error: (assignment.type.incompatible)
+        @LTEqLengthOf("list") int loe = i;
 
-        void add(
-                List<Integer> list,
-                @LTEqLengthOf("list") int index,
-                @LTEqLengthOf("arr") int arrIndex) {
-            int i = index;
-            int arri = arrIndex;
+        //:: error: (assignment.type.incompatible)
+        @LTEqLengthOf("list") int loe2 = j;
+    }
 
-            list.add(0);
-            @LTLengthOf("list") int less = i;
+    void add(
+            List<Integer> list,
+            @LTEqLengthOf("list") int index,
+            @LTEqLengthOf("arr") int arrIndex) {
+        int i = index;
+        int arri = arrIndex;
 
-            ////:: error: (assignment.type.incompatible)
-            @LTLengthOf("arr") int arrLess = arri;
-        }
-    */
+        list.add(0);
+        @LTLengthOf("list") int less = i;
+
+        //:: error: (assignment.type.incompatible)
+        @LTLengthOf("arr") int arrLess = arri;
+    }
 }
