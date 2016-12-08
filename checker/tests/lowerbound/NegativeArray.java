043fca5394b8566fb4689ad9a6ff1174b8f6ad95
Martin Kellogg <kelloggm@cs.washington.edu>
2016-12-08 10:49:51 -0800

    added support for warning about arrays initialized with possibly-negative variables. Closes issue 37

--- checker/tests/lowerbound/NegativeArray.java
+++ checker/tests/lowerbound/NegativeArray.java
@@ -1,11 +1,9 @@
 import org.checkerframework.checker.lowerbound.qual.*;
 
-// @skip-test until the bug is fixed
-
 public class NegativeArray {
 
     public static void negativeArray(@GTENegativeOne int len) {
-        //:: error: (array.length.negative)
+        //:: warning: (array.length.negative)
         int[] arr = new int[len];
     }
 }
