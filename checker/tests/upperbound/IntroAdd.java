10454c57e59303683a6e089109c9e2019b0ab9bd
Michael Ernst <mernst@cs.washington.edu>
2016-12-10 15:29:26 -0800

    Don't use annotations in comments in CF tests

--- checker/tests/upperbound/IntroAdd.java
+++ checker/tests/upperbound/IntroAdd.java
@@ -3,13 +3,13 @@ import org.checkerframework.checker.upperbound.qual.*;
 public class IntroAdd {
     void test() {
         //:: error: (assignment.type.incompatible)
-        /*@LTLengthOf({"banana"})*/ int a = 3;
+        @LTLengthOf({"banana"}) int a = 3;
         //:: error: (assignment.type.incompatible)
-        /*@LTLengthOf({"banana"})*/ int c = a + 1;
-        /*@LTEqLengthOf({"banana"})*/ int c1 = a + 1;
-        /*@LTLengthOf({"banana"})*/ int d = a + 0;
-        /*@LTLengthOf({"banana"})*/ int e = a + (-7);
+        @LTLengthOf({"banana"}) int c = a + 1;
+        @LTEqLengthOf({"banana"}) int c1 = a + 1;
+        @LTLengthOf({"banana"}) int d = a + 0;
+        @LTLengthOf({"banana"}) int e = a + (-7);
         //:: error: (assignment.type.incompatible)
-        /*@LTLengthOf({"banana"})*/ int f = a + 7;
+        @LTLengthOf({"banana"}) int f = a + 7;
     }
 }
