d22735e14390f180ab050f85498b8afb7ba9dccd
Martin Kellogg <kelloggm@cs.washington.edu>
2016-12-09 17:39:50 -0800

    new tests based on plume lib issues

---
+++ checker/tests/upperbound/OneLTL.java
@@ -0,0 +1,12 @@
+// @skip-test until this bug is fixed.
+
+class OneLTL {
+    public static boolean sorted(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i + 1] < a[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
