147892f40f5a19d8a1dc1173268845d3f27ea2b6
Martin Kellogg <kelloggm@cs.washington.edu>
2016-12-13 18:52:13 -0800

    something is wrong with the reformatter on my machine, but this should conform

--- checker/tests/upperbound/EnumValues.java
+++ checker/tests/upperbound/EnumValues.java
@@ -15,7 +15,7 @@ class EnumValues {
     public static void enumValues() {
         Direction @ArrayLen(4) [] arr4 = Direction.values();
         Direction[] arr = Direction.values();
-        //	Direction a = arr[0]; Doesn't work for some reason? FIXME
+        Direction a = arr[0];
         Direction b = arr[1];
         Direction c = arr[2];
         Direction d = arr[3];
