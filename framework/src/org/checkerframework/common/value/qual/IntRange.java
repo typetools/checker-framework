989ce09864a21b1b318265bc1900736e5f8ed76a
JasonMrX <jiasenxu@gmail.com>
2016-12-20 22:50:25 -0800

    add javadoc for @IntRange interface

--- framework/src/org/checkerframework/common/value/qual/IntRange.java
+++ framework/src/org/checkerframework/common/value/qual/IntRange.java
@@ -6,6 +6,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.checkerframework.framework.qual.SubtypeOf;
 
+/**
+ * An annotation indicating that the annotated target should be a byte, short, char, int, or long in
+ * the given range.
+ *
+ * @checker_framework.manual #constant-value-checker Constant Value Checker
+ */
 @SubtypeOf(UnknownVal.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_PARAMETER, ElementType.TYPE_USE})
