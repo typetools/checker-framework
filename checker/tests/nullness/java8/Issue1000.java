2ca84aab4bc20634e47ccaef14e188a67454cb55
Michael Ernst <mernst@cs.washington.edu>
2016-12-27 12:32:11 -0800

    Improve method names

--- checker/tests/nullness/java8/Issue1000.java
+++ checker/tests/nullness/java8/Issue1000.java
@@ -5,11 +5,11 @@ class Issue1000 {
     //:: error: (type.argument.type.incompatible
     void illegalInstantiation(Optional<@Nullable String> arg) {}
 
-    String foo1(Optional<String> opt) {
+    String orElseAppliedToNonNull(Optional<String> opt) {
         return opt.orElse("");
     }
 
-    String foo2(Optional<String> opt) {
+    String orElseAppliedToNullable(Optional<String> opt) {
         //:: error: (return.type.incompatible)
         return opt.orElse(null);
     }
