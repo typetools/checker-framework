7682da4e0f0904690216c17b8ad8a4bd5a62fe51
Michael Ernst <mernst@cs.washington.edu>
2016-12-27 12:31:12 -0800

    Add test case

--- checker/tests/nullness/java8/Issue1000.java
+++ checker/tests/nullness/java8/Issue1000.java
@@ -2,7 +2,7 @@ import java.util.Optional;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 class Issue1000 {
-    //:: error: illegal.instantiation
+    //:: error: (type.argument.type.incompatible
     void illegalInstantiation(Optional<@Nullable String> arg) {}
 
     String foo1(Optional<String> opt) {
@@ -10,7 +10,7 @@ class Issue1000 {
     }
 
     String foo2(Optional<String> opt) {
-        //:: error: invalid.return.type
+        //:: error: (return.type.incompatible)
         return opt.orElse(null);
     }
 }
