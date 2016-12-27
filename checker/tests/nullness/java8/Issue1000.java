8e4353a6ad29c4fb885f6b5c67564731a6fe63eb
Michael Ernst <mernst@cs.washington.edu>
2016-12-27 13:17:58 -0800

    Test should only run under JDK 8

--- checker/tests/nullness/java8/Issue1000.java
+++ checker/tests/nullness/java8/Issue1000.java
@@ -1,3 +1,7 @@
+// Test case for issue #1000:
+// https://github.com/typetools/checker-framework/issues/1000
+// @below-java8-jdk-skip-test
+
 import java.util.Optional;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
