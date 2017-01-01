ae2c9211c5d5dea8943821f576a55822894cb8bc
Werner Dietl <wdietl@gmail.com>
2017-01-01 18:39:16 -0500

    Properly format the Eclipse plug-in sources. (#991)
    Properly format the Eclipse plug-in sources and make list of excluded files more specific.

--- eclipse/checker-framework-eclipse-plugin/src/org/checkerframework/eclipse/actions/CheckerManager.java
+++ eclipse/checker-framework-eclipse-plugin/src/org/checkerframework/eclipse/actions/CheckerManager.java
@@ -4,44 +4,38 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-
+import org.checkerframework.eclipse.CheckerPlugin;
 import org.checkerframework.eclipse.prefs.CheckerPreferences;
 import org.checkerframework.eclipse.util.PluginUtil;
 import org.eclipse.jface.preference.IPreferenceStore;
 
-import org.checkerframework.eclipse.CheckerPlugin;
-
 /**
- * This class manages the current checkers that can be run. Also keeps track of
- * custom checkers that are made by the user.
+ * This class manages the current checkers that can be run. Also keeps track of custom checkers that
+ * are made by the user.
  *
  * @author asumu
- *
  */
 public class CheckerManager {
 
-    /**
-     * Singleton constructor, should only be called once for the instance
-     */
-    private CheckerManager() {
-    }
+    /** Singleton constructor, should only be called once for the instance */
+    private CheckerManager() {}
 
     public static List<CheckerInfo> getCheckerInfos() {
         return CheckerInfo.getCheckers();
     }
 
     /**
-     * For each processor in processors, check to see if we recognize that processor and
-     * add its implicit imports to the "selected" list.  Return selected.
+     * For each processor in processors, check to see if we recognize that processor and add its
+     * implicit imports to the "selected" list. Return selected.
      *
      * @return a list of quals paths to use as imports
      */
-    public static List<String> getSelectedQuals(final String [] processors) {
+    public static List<String> getSelectedQuals(final String[] processors) {
         List<String> selected = new ArrayList<String>();
 
         final Map<String, CheckerInfo> pathToChecker = CheckerInfo.getPathToCheckerInfo();
 
-        for (String processor : processors ) {
+        for (String processor : processors) {
             final String trimmedProc = processor.trim();
             if (pathToChecker.containsKey(trimmedProc)) {
                 final String qualsPath = pathToChecker.get(trimmedProc).getQualsPath();
@@ -58,7 +52,7 @@ public class CheckerManager {
         return CheckerPlugin.getDefault().getPreferenceStore();
     }
 
-    public static String [] getStoredCustomClasses() {
+    public static String[] getStoredCustomClasses() {
         final IPreferenceStore store = getPrefStore();
         final String storedItems = store.getString(CheckerPreferences.PREF_CHECKER_CUSTOM_CLASSES);
 
@@ -66,17 +60,15 @@ public class CheckerManager {
             return storedItems.split(",");
         }
 
-        return new String[]{};
+        return new String[] {};
     }
 
-
-    public static void storeCustomClasses(final String [] customClasses) {
+    public static void storeCustomClasses(final String[] customClasses) {
         final IPreferenceStore store = getPrefStore();
         final String classString = PluginUtil.join(",", customClasses);
 
         store.setValue(CheckerPreferences.PREF_CHECKER_CUSTOM_CLASSES, classString);
     }
-
 
     public static void storeSelectedClasses(final List<String> selectedClasses) {
         final IPreferenceStore store = getPrefStore();
@@ -98,5 +90,4 @@ public class CheckerManager {
 
         return Arrays.asList(selectedStr.split(";"));
     }
-
 }
--- eclipse/checker-framework-eclipse-plugin/src/org/checkerframework/eclipse/util/Paths.java
+++ eclipse/checker-framework-eclipse-plugin/src/org/checkerframework/eclipse/util/Paths.java
@@ -4,14 +4,12 @@ import java.io.*;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.jdt.core.IClasspathEntry;
 
-public class Paths
-{
-    private Paths()  {
+public class Paths {
+    private Paths() {
         throw new AssertionError("Cannot be instantiated");
     }
 
@@ -21,28 +19,26 @@ public class Paths
         IPath path = (location != null) ? location : jarFile.getFullPath();
         String osString = path.toOSString();
 
-        if ( PluginUtil.isWindows() && !hasDriveLetter(osString)) {
+        if (PluginUtil.isWindows() && !hasDriveLetter(osString)) {
             osString = kludgeFixAddDriveLetterToFilePath(osString);
         }
         return osString;
     }
 
-
     private static boolean equalsIgnoreCase(char c1, char c2) {
         return String.valueOf(c1).toLowerCase().equals(String.valueOf(c2).toLowerCase());
     }
-
 
     private static char driveLetter(final File file) {
         return file.getAbsolutePath().charAt(0);
     }
 
     /**
-     * Ordered by preference.  Use listRoots to get drive letters for windows and
-     * put C (if available) up front and (A:) in the back.
+     * Ordered by preference. Use listRoots to get drive letters for windows and put C (if
+     * available) up front and (A:) in the back.
      */
     private static List<File> determineOrderedDriveRoots() {
-        final File [] files = File.listRoots();
+        final File[] files = File.listRoots();
         final List<File> driveRoots = new ArrayList<File>();
 
         final Pattern pattern = Pattern.compile("^([A-Z]):\\\\$");
@@ -55,29 +51,34 @@ public class Paths
         }
 
         // C up front, a in the back, everything else is equal
-        Collections.sort(driveRoots, new Comparator<File>() {
-            @Override
-            public int compare(File o1, File o2) {
-                if (equalsIgnoreCase(driveLetter(o1), driveLetter(o2))) {
-                    return 0;
-                }
-                if (equalsIgnoreCase(driveLetter(o1), 'c') || equalsIgnoreCase(driveLetter(o2), 'a')) {
-                    return -1;
-                }
-                if (equalsIgnoreCase(driveLetter(o1), 'a') || equalsIgnoreCase(driveLetter(o2), 'c')) {
-                    return 1;
-                }
+        Collections.sort(
+                driveRoots,
+                new Comparator<File>() {
+                    @Override
+                    public int compare(File o1, File o2) {
+                        if (equalsIgnoreCase(driveLetter(o1), driveLetter(o2))) {
+                            return 0;
+                        }
+                        if (equalsIgnoreCase(driveLetter(o1), 'c')
+                                || equalsIgnoreCase(driveLetter(o2), 'a')) {
+                            return -1;
+                        }
+                        if (equalsIgnoreCase(driveLetter(o1), 'a')
+                                || equalsIgnoreCase(driveLetter(o2), 'c')) {
+                            return 1;
+                        }
 
-                return 0;
-            }
-        });
+                        return 0;
+                    }
+                });
 
         return driveRoots;
     }
 
-    private static List<File> orderedDriveRoots = Collections.unmodifiableList(determineOrderedDriveRoots());
+    private static List<File> orderedDriveRoots =
+            Collections.unmodifiableList(determineOrderedDriveRoots());
 
-    private static boolean hasDriveLetter(String filePath ) {
+    private static boolean hasDriveLetter(String filePath) {
         for (File file : orderedDriveRoots) {
             if (filePath.startsWith(file.getAbsolutePath())) {
                 return true;
@@ -85,7 +86,6 @@ public class Paths
         }
         return false;
     }
-
 
     private static String toDriveLetter(final char c) {
         return c + File.pathSeparator + File.separator;
@@ -97,19 +97,21 @@ public class Paths
     }
 
     /**
-     * I am having a heck of a time trying to get the drive letter to appear on the filepath
-     * for jars added via Eclipse classpath entries.  For now, use the potentially error prone
-     * technique of listing all drive letters and testing to see if the path exists on that drive,
-     * prefer C:.  This is error prone because a path can exist on multiple drives.  In the case of
-     * mirroring it could potentially use an archived version rather than the latest version of the jar.
-     * This is not a huge risk but still a kludge.
+     * I am having a heck of a time trying to get the drive letter to appear on the filepath for
+     * jars added via Eclipse classpath entries. For now, use the potentially error prone technique
+     * of listing all drive letters and testing to see if the path exists on that drive, prefer C:.
+     * This is error prone because a path can exist on multiple drives. In the case of mirroring it
+     * could potentially use an archived version rather than the latest version of the jar. This is
+     * not a huge risk but still a kludge.
+     *
      * @param pathStr A file path that does not start with a drive letter
      * @return pathStr
      */
     public static String kludgeFixAddDriveLetterToFilePath(final String pathStr) {
         for (final File root : orderedDriveRoots) {
 
-            final File cPath = new File(root, (pathStr.startsWith("\\") ? pathStr.substring(1) : pathStr));
+            final File cPath =
+                    new File(root, (pathStr.startsWith("\\") ? pathStr.substring(1) : pathStr));
             if (cPath.exists()) {
                 return cPath.getAbsolutePath();
             }
