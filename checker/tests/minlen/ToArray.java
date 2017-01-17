cfb1b9ac8bc426db460a427c30852f6fa862080b
Michael Ernst <mernst@cs.washington.edu>
2017-01-17 15:19:54 -0800

    Test for List.toArray, skipped until list support is reinstated

---
+++ checker/tests/minlen/ToArray.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import org.checkerframework.checker.index.qual.MinLen;
+
+// @skip-test until we bring list support back
+
+public class ToArray {
+
+    public String @MinLen(1) [] m(@MinLen(1) ArrayList<String> compiler) {
+        return compiler.toArray(new String[0]);
+    }
+}
