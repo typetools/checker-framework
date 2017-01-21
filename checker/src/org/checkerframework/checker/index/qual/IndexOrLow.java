3c2f65403bbf4f2dc368e2b11d748249941ae601
Michael Ernst <mernst@cs.washington.edu>
2017-01-20 22:09:50 -0800

    Add examples of @IndexFor, @IndexOrHigh, @IndexOrLow.

--- checker/src/org/checkerframework/checker/index/qual/IndexOrLow.java
+++ checker/src/org/checkerframework/checker/index/qual/IndexOrLow.java
@@ -3,6 +3,16 @@ package org.checkerframework.checker.index.qual;
 /**
  * An integer that is either -1 or is a valid index for each of the given sequences.
  *
+ * <p>The <a
+ * href="https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#indexOf-java.lang.String-">
+ * <code>String.indexOf(String)</code></a> method is declared as
+ *
+ * <pre><code>
+ *   class String {
+ *    {@literal @}IndexOrLow("this") int indexOf(String str) { ... }
+ *   }
+ * </code></pre>
+ *
  * <p>Writing {@code @IndexOrLow("arr")} is equivalent to writing {@link
  * GTENegativeOne @GTENegativeOne} {@link LTLengthOf @LTLengthOf("arr")}, and that is how it is
  * treated internally by the checker. Thus, if you write an {@code @IndexOrLow("arr")} annotation,
