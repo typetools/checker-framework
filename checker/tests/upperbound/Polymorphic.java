335a0a1a75fbc7a828afce0371448b7d156b1d65
Martin Kellogg <kelloggm@cs.washington.edu>
2017-01-31 11:33:22 -0800

    changed test to match new qualifiers

--- checker/tests/upperbound/Polymorphic.java
+++ checker/tests/upperbound/Polymorphic.java
@@ -9,41 +9,48 @@ class Polymorphic {
 
     //Identity functions
 
-    int @PolyIndex [] array_identity(int @PolyIndex [] a) {
+    int @PolyMinLen [] minlen_identity(int @PolyMinLen [] a) {
         return a;
     }
 
-    @PolyIndex
-    int int_identity(@PolyIndex int a) {
+    int @PolySameLen [] samelen_identity(int @PolySameLen [] a) {
+        return a;
+    }
+
+    @PolyLowerBound int lbc_identity(@PolyLowerBound int a) {
+        return a;
+    }
+
+    @PolyUpperBound int ubc_identity(@PolyUpperBound int a) {
         return a;
     }
 
     // MinLen tests
     void minlen_id(int @MinLen(5) [] a) {
-        int @MinLen(5) [] b = array_identity(a);
+        int @MinLen(5) [] b = minlen_identity(a);
         //:: error: (assignment.type.incompatible)
-        int @MinLen(6) [] c = array_identity(b);
+        int @MinLen(6) [] c = minlen_identity(b);
     }
 
     // SameLen tests
     void samelen_id(int @SameLen("#2") [] a, int[] a2) {
         int[] banana;
-        int @SameLen("a2") [] b = array_identity(a);
+        int @SameLen("a2") [] b = samelen_identity(a);
         //:: error: (assignment.type.incompatible)
-        int @SameLen("banana") [] c = array_identity(b);
+        int @SameLen("banana") [] c = samelen_identity(b);
     }
 
     // LowerBound tests
     void lbc_id(@NonNegative int n, @Positive int p, @GTENegativeOne int g) {
-        @NonNegative int an = int_identity(n);
+        @NonNegative int an = lbc_identity(n);
         //:: error: (assignment.type.incompatible)
-        @Positive int bn = int_identity(n);
+        @Positive int bn = lbc_identity(n);
 
-        @GTENegativeOne int ag = int_identity(g);
+        @GTENegativeOne int ag = lbc_identity(g);
         //:: error: (assignment.type.incompatible)
-        @NonNegative int bg = int_identity(g);
+        @NonNegative int bg = lbc_identity(g);
 
-        @Positive int ap = int_identity(p);
+        @Positive int ap = lbc_identity(p);
     }
 
     // UpperBound tests
@@ -56,20 +63,20 @@ class Polymorphic {
             @LTEqLengthOf({"#1", "#2"}) int abl) {
         int[] c;
 
-        @LTLengthOf("a") int ai1 = int_identity(ai);
+        @LTLengthOf("a") int ai1 = ubc_identity(ai);
         //:: error: (assignment.type.incompatible)
-        @LTLengthOf("b") int ai2 = int_identity(ai);
+        @LTLengthOf("b") int ai2 = ubc_identity(ai);
 
-        @LTEqLengthOf("a") int al1 = int_identity(al);
+        @LTEqLengthOf("a") int al1 = ubc_identity(al);
         //:: error: (assignment.type.incompatible)
-        @LTLengthOf("a") int al2 = int_identity(al);
+        @LTLengthOf("a") int al2 = ubc_identity(al);
 
-        @LTLengthOf({"a", "b"}) int abi1 = int_identity(abi);
+        @LTLengthOf({"a", "b"}) int abi1 = ubc_identity(abi);
         //:: error: (assignment.type.incompatible)
-        @LTLengthOf({"a", "b", "c"}) int abi2 = int_identity(abi);
+        @LTLengthOf({"a", "b", "c"}) int abi2 = ubc_identity(abi);
 
-        @LTEqLengthOf({"a", "b"}) int abl1 = int_identity(abl);
+        @LTEqLengthOf({"a", "b"}) int abl1 = ubc_identity(abl);
         //:: error: (assignment.type.incompatible)
-        @LTEqLengthOf({"a", "b", "c"}) int abl2 = int_identity(abl);
+        @LTEqLengthOf({"a", "b", "c"}) int abl2 = ubc_identity(abl);
     }
 }
