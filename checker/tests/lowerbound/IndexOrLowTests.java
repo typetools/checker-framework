b5158150db18625b01fd09bb68fd8232c5acffce
Martin Kellogg <kelloggm@cs.washington.edu>
2017-02-02 15:41:25 -0800

    modified index or low tests to both: not do evil things, and test more behavior

--- checker/tests/lowerbound/IndexOrLowTests.java
+++ checker/tests/lowerbound/IndexOrLowTests.java
@@ -1,4 +1,5 @@
 import org.checkerframework.checker.index.qual.GTENegativeOne;
+import org.checkerframework.checker.index.qual.IndexOrHigh;
 import org.checkerframework.checker.index.qual.IndexOrLow;
 import org.checkerframework.checker.index.qual.LTLengthOf;
 
@@ -12,8 +13,11 @@ public class IndexOrLowTests {
         //:: error: (array.access.unsafe.low)
         array[index] = 1;
 
-        int y = index + 1;
-        array[y] = 1;
+        if (index > -1) {
+            array[index] = 1;
+        }
+
+        @IndexOrHigh("array") int y = index + 1;
         if (y < array.length) {
             array[y] = 1;
         }
@@ -25,5 +29,11 @@ public class IndexOrLowTests {
         index = array.length - 1;
         @LTLengthOf("array") @GTENegativeOne int x = index;
         index = param;
+    }
+
+    void test3(int[] a, int b) {
+        //:: error: (assignment.type.incompatible)
+        @IndexOrLow("a")
+        int k = a.length - b;
     }
 }
