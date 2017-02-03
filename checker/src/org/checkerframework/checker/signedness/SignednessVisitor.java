eb7c8d1b67f865a8b4d0d909d22750d023c658cb
Michael Ernst <mernst@alum.mit.edu>
2017-02-03 06:58:24 -0800

    Add checking of masked shifts

--- checker/src/org/checkerframework/checker/signedness/SignednessVisitor.java
+++ checker/src/org/checkerframework/checker/signedness/SignednessVisitor.java
@@ -1,9 +1,8 @@
 package org.checkerframework.checker.signedness;
 
-import com.sun.source.tree.BinaryTree;
-import com.sun.source.tree.CompoundAssignmentTree;
-import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.*;
 import com.sun.source.tree.Tree.Kind;
+import com.sun.source.util.TreePath;
 import org.checkerframework.checker.signedness.qual.Signed;
 import org.checkerframework.checker.signedness.qual.Unsigned;
 import org.checkerframework.common.basetype.BaseTypeChecker;
@@ -21,6 +20,128 @@ public class SignednessVisitor extends BaseTypeVisitor<SignednessAnnotatedTypeFa
 
     public SignednessVisitor(BaseTypeChecker checker) {
         super(checker);
+    }
+
+    /** @return true iff node is a mask operation (&amp; or |) */
+    private boolean isMask(Tree node) {
+        Kind kind = node.getKind();
+
+        return kind == Kind.AND || kind == Kind.OR;
+    }
+
+    /** @return true iff expr is a literal */
+    private boolean isLiteral(ExpressionTree expr) {
+        return expr instanceof LiteralTree;
+    }
+
+    /**
+     * @param obj either an Integer or a Long
+     * @return the long value of obj
+     */
+    private long getLong(Object obj) {
+        if (obj instanceof Integer) {
+            Integer intObj = (Integer) obj;
+            return intObj.longValue();
+        } else {
+            return (long) obj;
+        }
+    }
+
+    /**
+     * Given a masking operation of the form {@code expr & maskLit} or {@code expr | maskLit},
+     * return true iff the masking operation results in the same output regardless of the value of
+     * the numBits most significant bits of expr. This is if the numBits most significant bits of
+     * mask are 0 for AND, and 1 for OR. For example, assuming that numBits is 4, the following is
+     * true about AND and OR masks:
+     *
+     * <p>{@code expr & 0x0... == 0x0... ;}
+     *
+     * <p>{@code expr | 0xF... == 0xF... ;}
+     *
+     * @param maskKind the kind of mask (AND or OR)
+     * @param numBitsLit the LiteralTree whose value is numBits
+     * @param maskLit the LiteralTree whose value is mask
+     * @return true iff the numBits most significant bits of mask are 0 for AND, and 1 for OR
+     */
+    private boolean maskIgnoresMSB(Kind maskKind, LiteralTree numBitsLit, LiteralTree maskLit) {
+        long numBits = getLong(numBitsLit.getValue());
+        long mask = getLong(maskLit.getValue());
+
+        // Shift the numBits most significant bits to become the numBits least significant bits, zeroing out the rest.
+        if (maskLit.getKind() != Kind.LONG_LITERAL) {
+            mask <<= 32;
+        }
+        mask >>>= (64 - numBits);
+
+        if (maskKind == Kind.AND) {
+            // Check that the numBits most significant bits of the mask were 0.
+            return mask == 0;
+        } else if (maskKind == Kind.OR) {
+            // Check that the numBits most significant bits of the mask were 1.
+            return mask == (1 << numBits) - 1;
+        } else {
+            // This shouldn't be possible.
+            throw new RuntimeException("Invalid Masking Operation");
+        }
+    }
+
+    /**
+     * Determines if a right shift operation, {@code >>} or {@code >>>}, is masked with a masking
+     * operation of the form {@code shiftExpr & maskLit} or {@code shiftExpr | maskLit} such that
+     * the mask renders the shift signedness ({@code >>} vs {@code >>>}) irrelevent by destroying
+     * the bits duplicated into the shift result. For example, the following pairs of right shifts
+     * on {@code byte b} both produce the same results under any input, because of their masks:
+     *
+     * <p>{@code (b >> 4) & 0x0F == (b >>> 4) & 0x0F;}
+     *
+     * <p>{@code (b >> 4) | 0xF0 == (b >>> 4) | 0xF0;}
+     *
+     * @param shiftExpr a right shift expression: {@code expr1 >> expr2} or {@code expr1 >>> expr2}
+     * @return true iff the right shift is masked such that a signed or unsigned right shift has the
+     *     same effect
+     */
+    private boolean isMaskedShift(BinaryTree shiftExpr) {
+        // enclosing is the operation or statement that immediately contains shiftExpr
+        Tree enclosing;
+        // enclosingChild is the top node in the chain of nodes from shiftExpr to parent
+        Tree enclosingChild;
+        {
+            TreePath parentPath = visitorState.getPath().getParentPath();
+            enclosing = parentPath.getLeaf();
+            enclosingChild = enclosing;
+            // Strip away all parentheses from the shift operation
+            while (enclosing.getKind() == Kind.PARENTHESIZED) {
+                parentPath = parentPath.getParentPath();
+                enclosingChild = enclosing;
+                enclosing = parentPath.getLeaf();
+            }
+        }
+
+        if (!isMask(enclosing)) {
+            return false;
+        }
+
+        BinaryTree maskExpr = (BinaryTree) enclosing;
+        ExpressionTree shift = shiftExpr.getRightOperand();
+
+        // Determine which child of maskExpr leads to shiftExpr. The other one is the mask.
+        ExpressionTree mask =
+                maskExpr.getRightOperand() == enclosingChild
+                        ? maskExpr.getLeftOperand()
+                        : maskExpr.getRightOperand();
+
+        // Strip away the parentheses from the mask if any exist
+        while (mask.getKind() == Kind.PARENTHESIZED)
+            mask = ((ParenthesizedTree) mask).getExpression();
+
+        if (!isLiteral(shift) || !isLiteral(mask)) {
+            return false;
+        }
+
+        LiteralTree shiftLit = (LiteralTree) shift;
+        LiteralTree maskLit = (LiteralTree) mask;
+
+        return maskIgnoresMSB(maskExpr.getKind(), shiftLit, maskLit);
     }
 
     /**
@@ -56,13 +177,13 @@ public class SignednessVisitor extends BaseTypeVisitor<SignednessAnnotatedTypeFa
                 break;
 
             case RIGHT_SHIFT:
-                if (leftOpType.hasAnnotation(Unsigned.class)) {
+                if (leftOpType.hasAnnotation(Unsigned.class) && !isMaskedShift(node)) {
                     checker.report(Result.failure("shift.signed", kind), leftOp);
                 }
                 break;
 
             case UNSIGNED_RIGHT_SHIFT:
-                if (leftOpType.hasAnnotation(Signed.class)) {
+                if (leftOpType.hasAnnotation(Signed.class) && !isMaskedShift(node)) {
                     checker.report(Result.failure("shift.unsigned", kind), leftOp);
                 }
                 break;
