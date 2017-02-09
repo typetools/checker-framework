188fd10921fba6ec5bbb8f662ae56a3086c6642a
Michael Ernst <mernst@cs.washington.edu>
2017-02-09 08:58:41 -0800

    Add failing test case, disable for now

--- checker/tests/nullness/java8/StreamMap.java
+++ checker/tests/nullness/java8/StreamMap.java
@@ -1,3 +1,7 @@
+// Test case for issue #1032:
+// https://github.com/typetools/checker-framework/issues/1032
+// @skip-test until the issue is fixed.
+
 // @below-java8-jdk-skip-test
 
 import java.util.stream.Stream;
@@ -6,11 +10,24 @@ import org.checkerframework.checker.nullness.qual.*;
 class StreamMap {
 
     @SuppressWarnings("nullness")
-    static @NonNull String castToNonNull(@Nullable String arg) {
+    static @NonNull String castStringToNonNull(@Nullable String arg) {
         return (@NonNull String) arg;
     }
 
-    Stream<@NonNull String> mapCast(Stream<@Nullable String> arg) {
-        return arg.map(StreamMap::castToNonNull);
+    Stream<@NonNull String> mapStringCast1(Stream<@Nullable String> arg) {
+        return arg.map(StreamMap::castStringToNonNull);
+    }
+
+    Stream<@NonNull String> mapStringCast2(Stream<@Nullable String> arg) {
+        return arg.map(StreamMap::<String>castStringToNonNull);
+    }
+
+    @SuppressWarnings("nullness")
+    static <T> @NonNull T castTToNonNull(@Nullable T arg) {
+        return (@NonNull T) arg;
+    }
+
+    <T> Stream<@NonNull T> mapTCast(Stream<@Nullable T> arg) {
+        return arg.map(StreamMap::<T>castTToNonNull);
     }
 }
