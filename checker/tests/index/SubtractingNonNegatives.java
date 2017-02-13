f85833ef0095a71e0c3babb6c60eedfb12421eab
Martin Kellogg <kelloggm@cs.washington.edu>
2017-02-13 13:46:50 -0800

    added comment describing test case

--- checker/tests/index/SubtractingNonNegatives.java
+++ checker/tests/index/SubtractingNonNegatives.java
@@ -1,9 +1,11 @@
+// Test case for issue 98: https://github.com/kelloggm/checker-framework/issues/98
+
 import org.checkerframework.checker.index.qual.*;
 
 public class SubtractingNonNegatives {
     public static void m4(int[] a, @IndexFor("#1") int i, @IndexFor("#1") int j) {
         int k = i;
-        // The checker correctly issues now warning on the if block below
+        // The checker correctly issues no warning on the if block below
         if (k >= j) {
             @IndexFor("a") int y = k;
         }
