4a308080b4d95b2a758e2b64d7048bda79d2a1db
Suzanne Millstein <smillst@cs.washington.edu>
2017-02-22 12:57:30 -0800

    Correct regex

--- framework/tests/flowexpression/ValueLiterals.java
+++ framework/tests/flowexpression/ValueLiterals.java
@@ -2,4 +2,10 @@ import testlib.flowexpression.qual.FlowExp;
 
 public class ValueLiterals {
     void test(@FlowExp("0") Object a, @FlowExp("0L") Object b) {}
+
+    void test2(@FlowExp("1000") Object a, @FlowExp("100L") Object b) {}
+    //:: error: (expression.unparsable.type.invalid)
+    void test3(@FlowExp("01000") Object a) {}
+    //:: error: (expression.unparsable.type.invalid)
+    void test4(@FlowExp("0100L") Object b) {}
 }
