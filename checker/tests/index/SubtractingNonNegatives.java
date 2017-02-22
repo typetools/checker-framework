79bdd88c7c4a4ef61412d4c116e9fb55387f566c
Martin Kellogg <kelloggm@cs.washington.edu>
2017-02-22 11:29:24 -0800

    fix comments on test case

--- checker/tests/index/SubtractingNonNegatives.java
+++ checker/tests/index/SubtractingNonNegatives.java
@@ -5,12 +5,11 @@ import org.checkerframework.checker.index.qual.*;
 public class SubtractingNonNegatives {
     public static void m4(int[] a, @IndexFor("#1") int i, @IndexFor("#1") int j) {
         int k = i;
-        // The checker correctly issues no warning on the if block below
         if (k >= j) {
             @IndexFor("a") int y = k;
         }
         for (k = i; k >= j; k -= j) {
-            @IndexFor("a") int x = k; // index checker warning
+            @IndexFor("a") int x = k;
         }
     }
 }
