e03a3880020aabed4f4037cceba9a408dc439ee1
Suzanne Millstein <smillst@cs.washington.edu>
2017-02-23 09:51:37 -0800

    Document offset.
    Still needs to be documented in the manual.

--- checker/src/org/checkerframework/checker/index/qual/LTLengthOf.java
+++ checker/src/org/checkerframework/checker/index/qual/LTLengthOf.java
@@ -8,12 +8,22 @@ import org.checkerframework.framework.qual.JavaExpression;
 import org.checkerframework.framework.qual.SubtypeOf;
 
 /**
- * The annotated expression evaluates to an integer whose value is less than the lengths of all the
- * given sequences.
+ * If no offset is specified, then the annotated expression evaluates to an integer whose value is
+ * less than the lengths of all the given sequences.
  *
  * <p>For example, an expression with type {@code @LTLengthOf({"a", "b"})} is less than both {@code
  * a.length} and {@code b.length}. The sequences {@code a} and {@code b} might have different
  * lengths.
+ *
+ * <p>If the offset element is nonempty, then the annotated expression plus the expression in
+ * offset[i] element is less than the length of the sequence specified by value[i].
+ *
+ * <p>For example, an expression with type {@code @LTLengthOf(value = {"a", "b"}, offset = {"-1",
+ * "x"})} is less than {@code a.length} if "-1" is added to it and is less than {@code b.length} if
+ * "x" is added to it.
+ *
+ * <p>The expressions in offset by be addition/subtraction of any number of Java expressions. For
+ * example, {@code @LessThanLengthOf(value = "a", offset = "x + y + 2"}}.
  *
  * @checker_framework.manual #index-checker Index Checker
  */
@@ -25,6 +35,10 @@ public @interface LTLengthOf {
     @JavaExpression
     public String[] value();
 
+    /**
+     * This expression plus the annotated expression is less than the length of the sequence. This
+     * element must ether be empty or the same size as value.
+     */
     @JavaExpression
     String[] offset() default {};
 }
