bd83ca952127e049e853cba9cb2b4f050f0b44a3
Suzanne Millstein <smillst@cs.washington.edu>
2017-03-08 12:13:06 -0800

    Aliasing Checker fixes
    1. New arrays are @Unique.
    2. Since type arguments may not be annotated with @Unique, All bounds should be @MaybeAliased by default.
    3. Run all-system tests.

--- framework/src/org/checkerframework/common/aliasing/qual/MaybeAliased.java
+++ framework/src/org/checkerframework/common/aliasing/qual/MaybeAliased.java
@@ -5,10 +5,12 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.checkerframework.framework.qual.DefaultFor;
 import org.checkerframework.framework.qual.DefaultQualifierInHierarchy;
 import org.checkerframework.framework.qual.ImplicitFor;
 import org.checkerframework.framework.qual.LiteralKind;
 import org.checkerframework.framework.qual.SubtypeOf;
+import org.checkerframework.framework.qual.TypeUseLocation;
 
 /**
  * An expression with this type might have an alias. In other words, some other expression,
@@ -19,6 +21,7 @@ import org.checkerframework.framework.qual.SubtypeOf;
  */
 @Documented
 @DefaultQualifierInHierarchy
+@DefaultFor({TypeUseLocation.UPPER_BOUND, TypeUseLocation.LOWER_BOUND})
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_PARAMETER, ElementType.TYPE_USE})
 @ImplicitFor(
