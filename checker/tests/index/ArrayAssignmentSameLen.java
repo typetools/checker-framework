ef81ebe713ebeb3a7b0a226136880db80bbc37cf
Suzanne Millstein <smillst@cs.washington.edu>
2017-03-14 10:21:28 -0700

    Add test cases

--- checker/tests/index/ArrayAssignmentSameLen.java
+++ checker/tests/index/ArrayAssignmentSameLen.java
@@ -9,4 +9,20 @@ public class ArrayAssignmentSameLen {
         i_array = array;
         i_index = index;
     }
+
+    void test1(int[] a, int[] b, @LTEqLengthOf("#1") int index) {
+        int[] array = a;
+        @LTLengthOf(
+            value = {"array", "b"},
+            offset = {"0", "-3"}
+        )
+        int i = index;
+    }
+
+    void test2(int[] a, int[] b, @LTLengthOf("#1") int i) {
+        int[] c = a;
+        //:: error: (assignment.type.incompatible)
+        @LTLengthOf(value = {"c", "b"}) int x = i;
+        @LTLengthOf("c") int y = i;
+    }
 }
