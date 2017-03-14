8d57c6dbfe03a0ac3b6ed48ec7e729a92191935b
Michael Ernst <mernst@cs.washington.edu>
2017-03-14 09:48:03 -0700

    Add expected error

--- checker/tests/index/IntroAnd.java
+++ checker/tests/index/IntroAnd.java
@@ -15,7 +15,7 @@ class IntroAnd {
             @IndexFor("#2") int i, int[] a, @LTLengthOf("#2") int j, int k, @NonNegative int m) {
         int x = a[i & k];
         int x1 = a[k & i];
-        //:: error: (array.access.unsafe.low)
+        //:: error: (array.access.unsafe.low) :: error: (array.access.unsafe.high)
         int y = a[j & k];
         if (j > -1) {
             int z = a[j & k];
