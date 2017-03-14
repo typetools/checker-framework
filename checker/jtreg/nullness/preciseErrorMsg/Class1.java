ff05517d0577fc73788d93e036d7e3f4a3a66962
Suzanne Millstein <smillst@cs.washington.edu>
2017-03-14 11:44:18 -0700

    Only report error in Pre/Post condition expression once. (#1106)
    Fixes #1051

---
+++ checker/jtreg/nullness/preciseErrorMsg/Class1.java
@@ -0,0 +1,15 @@
+/*
+ * @test
+ * @summary
+ * Test case for Issue 1051
+ * https://github.com/typetools/checker-framework/issues/1051
+ *
+ * @compile/fail/ref=Class1.out -XDrawDiagnostics -Xlint:unchecked -processor org.checkerframework.checker.nullness.NullnessChecker -AprintErrorStack -Anomsgtext Class1.java
+ *
+ */
+import org.checkerframework.checker.nullness.qual.RequiresNonNull;
+
+public class Class1 {
+    @RequiresNonNull("instanceField")
+    public static void foo() {}
+}
