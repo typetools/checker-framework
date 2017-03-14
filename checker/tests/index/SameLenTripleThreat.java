754e0b26efa2348bc7fca1c7f224356a298c8d64
Suzanne Millstein <smillst@cs.washington.edu>
2017-03-14 11:39:50 -0700

    Add test case from issue.

--- checker/tests/index/SameLenTripleThreat.java
+++ checker/tests/index/SameLenTripleThreat.java
@@ -7,4 +7,26 @@ public class SameLenTripleThreat {
         String[] indices = new String[vars.length];
         String @SameLen("qrets") [] x = indices;
     }
+
+    String[] indices;
+
+    public void foo2(String... vars) {
+        String[] qrets = new String[vars.length];
+        indices = new String[vars.length];
+        String[] indicesLocal = new String[vars.length];
+        for (int i = 0; i < qrets.length; i++) {
+            indices[i] = "hello";
+            indicesLocal[i] = "hello";
+        }
+    }
+
+    public void foo3(String... vars) {
+        String[] qrets = new String[vars.length];
+        String[] indicesLocal = new String[vars.length];
+        indices = new String[vars.length];
+        for (int i = 0; i < qrets.length; i++) {
+            indices[i] = "hello";
+            indicesLocal[i] = "hello";
+        }
+    }
 }
