f355ea07bb386c0346359db7cbd570a250d7e8bd
kanha95 <kanha95@gmail.com>
2017-03-17 23:23:14 +0530

    modified ConcurrentHashMapTest - fixes #1142

--- checker/tests/nullness/ConcurrentHashMapTest.java
+++ checker/tests/nullness/ConcurrentHashMapTest.java
@@ -1,36 +1,19 @@
-
 // Test case for issue 1142: https://github.com/typetools/checker-framework/issues/1142
 
-
-import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
-class Test{
+class issue1142{
 
-public static void main(String[] args){
-
+void foo(){
+	
 Map<Integer,@Nullable Integer> myMap=new ConcurrentHashMap<>();
-
 myMap.put(1, 1);
 myMap.put(3, 3);
-
 myMap.put(3, null);
 
 }
 }
 
 
-
-/*
-Output without annotated ConcurrentHashMap.java: Nullness Checker passed!
-
-Output at runtime: NullPointerException at line 16 
-
-As per java docs ,
-https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/ConcurrentHashMap.html
-The concurrent-hashmap raises NullpointerException if any of the key or value is null. 
-So here the Nullness Checker fails in catching the NullPointerException generated at the runtime.
-If bymistake any programmer annotates the value parameter to be @Nullable then the program will crash at the runtime.
-*/
