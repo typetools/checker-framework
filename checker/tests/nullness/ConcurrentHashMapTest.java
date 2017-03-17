184388b53ba188262f0ea21cb62f0300cb4e8f49
kanha95 <kanha95@gmail.com>
2017-03-18 03:27:16 +0530

    formatted ConcurrentHashMapTest - fixes #1142

--- checker/tests/nullness/ConcurrentHashMapTest.java
+++ checker/tests/nullness/ConcurrentHashMapTest.java
@@ -1,17 +1,16 @@
+
 import java.util.concurrent.ConcurrentHashMap;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
-class Issue1142{
+class Issue1142 {
 
-void foo(){
-	
-ConcurrentHashMap<Integer,@Nullable Integer> chm = new ConcurrentHashMap<>();
-chm.put(1, 1);
-chm.put(3, 3);
-chm.put(4, null);
+    void foo() {
 
+        ConcurrentHashMap<Integer, @Nullable Integer> chm = new ConcurrentHashMap<Integer, @Nullable Integer>();
+        chm.put(1, 1);
+        chm.put(3, 3);
+        chm.put(4, null);
+
+    }
 }
-}
-
-
