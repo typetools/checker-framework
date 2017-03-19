50c09c2634228f601405e04055c0ee25debc9d2a
kanha95 <kanha95@gmail.com>
2017-03-20 02:17:59 +0530

    new files added to fix Issue #1142

---
+++ checker/tests/nullness/Issue1142.java
@@ -0,0 +1,17 @@
+// Issue 1142 https://github.com/typetools/checker-framework/issues/67
+import java.util.concurrent.ConcurrentHashMap;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+class Issue1142 {
+
+  void foo() {
+
+    ConcurrentHashMap<Integer, @Nullable Integer> chm =
+        new ConcurrentHashMap<>();
+    chm.put(1, 1);
+    chm.put(3, 3);
+    chm.put(4, null);
+
+  }
+}
