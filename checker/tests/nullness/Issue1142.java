b99e499643d0dbd45ae5fce15326492f31b271f1
kanha95 <kanha95@gmail.com>
2017-03-20 02:19:29 +0530

    ant reformat fixes #1142

--- checker/tests/nullness/Issue1142.java
+++ checker/tests/nullness/Issue1142.java
@@ -1,17 +1,14 @@
 // Issue 1142 https://github.com/typetools/checker-framework/issues/67
 import java.util.concurrent.ConcurrentHashMap;
 import org.checkerframework.checker.nullness.qual.Nullable;
-import org.checkerframework.checker.nullness.qual.NonNull;
 
 class Issue1142 {
 
-  void foo() {
+    void foo() {
 
-    ConcurrentHashMap<Integer, @Nullable Integer> chm =
-        new ConcurrentHashMap<>();
-    chm.put(1, 1);
-    chm.put(3, 3);
-    chm.put(4, null);
-
-  }
+        ConcurrentHashMap<Integer, @Nullable Integer> chm = new ConcurrentHashMap<>();
+        chm.put(1, 1);
+        chm.put(3, 3);
+        chm.put(4, null);
+    }
 }
