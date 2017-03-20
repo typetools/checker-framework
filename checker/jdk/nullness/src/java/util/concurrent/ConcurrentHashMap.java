61453ac04b1b06ec8d9a5161c4459a3fd40bb7ac
kanha95 <kanha95@gmail.com>
2017-03-20 18:29:53 +0530

    minor fixes #1142

--- checker/jdk/nullness/src/java/util/concurrent/ConcurrentHashMap.java
+++ checker/jdk/nullness/src/java/util/concurrent/ConcurrentHashMap.java
@@ -17,17 +17,17 @@ public class ConcurrentHashMap<K extends @NonNull Object, V extends @NonNull Obj
      public ConcurrentHashMap() {throw new RuntimeException("skeleton method");}
      @Pure public boolean isEmpty() {throw new RuntimeException("skeleton method");}
      @Pure public int size() {throw new RuntimeException("skeleton method");}
-     @Pure public @Nullable V get(Object key){throw new RuntimeException("skeleton method");}
-     @Pure public boolean containsKey(Object key) {throw new RuntimeException("skeleton method");}
-     @Pure public boolean containsValue(Object value) {throw new RuntimeException("skeleton method");}
-     @Pure public boolean contains(Object value) {throw new RuntimeException("skeleton method");}
-     public @Nullable V put(K key, V value) {throw new RuntimeException("skeleton method");}
-     public @Nullable V putIfAbsent(K key, V value) {throw new RuntimeException("skeleton method");}
+     @Pure public @Nullable V get(@NonNull Object key){throw new RuntimeException("skeleton method");}
+     @Pure public boolean containsKey(@NonNull Object key) {throw new RuntimeException("skeleton method");}
+     @Pure public boolean containsValue(@NonNull Object value) {throw new RuntimeException("skeleton method");}
+     @Pure public boolean contains(@NonNull Object value) {throw new RuntimeException("skeleton method");}
+     public @Nullable V put(@NonNull K key,@NonNull V value) {throw new RuntimeException("skeleton method");}
+     public @Nullable V putIfAbsent(@NonNull K key, @NonNull V value) {throw new RuntimeException("skeleton method");}
      public void putAll(Map<? extends K, ? extends V> m) {throw new RuntimeException("skeleton method");}
-     public @Nullable V remove(Object key) {throw new RuntimeException("skeleton method");}
-     public boolean remove(Object key, Object value) {throw new RuntimeException("skeleton method");}
-     public boolean replace(K key, V oldValue, V newValue) {throw new RuntimeException("skeleton method");}
-     public @Nullable V replace(K key, V value) {throw new RuntimeException("skeleton method");}
+     public @Nullable V remove(@NonNull Object key) {throw new RuntimeException("skeleton method");}
+     public boolean remove(@NonNull Object key, @NonNull Object value) {throw new RuntimeException("skeleton method");}
+     public boolean replace(@NonNull K key, @NonNull V oldValue,@NonNull V newValue) {throw new RuntimeException("skeleton method");}
+     public @Nullable V replace(@NonNull K key, @NonNull V value) {throw new RuntimeException("skeleton method");}
      public void clear() {throw new RuntimeException("skeleton method");}
      @SideEffectFree public Set<@KeyFor("this") K> keySet() {throw new RuntimeException("skeleton method");}
      @SideEffectFree public Collection<V> values() {throw new RuntimeException("skeleton method");}
--- checker/tests/nullness/Issue1142.java
+++ checker/tests/nullness/Issue1142.java
@@ -1,14 +1,15 @@
-// Issue 1142 https://github.com/typetools/checker-framework/issues/67
+// Issue 1142 https://github.com/typetools/checker-framework/issues/1142
 import java.util.concurrent.ConcurrentHashMap;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 class Issue1142 {
 
     void foo() {
-
+        //:: error: (assignment.type.incompatible)
         ConcurrentHashMap<Integer, @Nullable Integer> chm = new ConcurrentHashMap<>();
         chm.put(1, 1);
         chm.put(3, 3);
+        //:: error: (assignment.type.incompatible)
         chm.put(4, null);
     }
 }
