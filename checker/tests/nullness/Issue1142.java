f6b23738218ae1588c5457a331459c6e0242a5e8
kanha95 <kanha95@gmail.com>
2017-03-20 23:25:14 +0530

    fixes #1142

--- checker/tests/nullness/Issue1142.java
+++ checker/tests/nullness/Issue1142.java
@@ -5,10 +5,10 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 class Issue1142 {
 
     void foo() {
-
+        //error expected on assignment compatibility
         ConcurrentHashMap<Integer, @Nullable Integer> chm1 = new ConcurrentHashMap<>();
 
-        //:: error: (assignment.type.incompatible)
+        //error expected on assignment compatibility
         chm1.put(1, null);
     }
 }
