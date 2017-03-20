5e5d5b97c5639bcec45d1575f250cb9519ad60ef
kanha95 <kanha95@gmail.com>
2017-03-20 22:53:30 +0530

    fixes #1142

--- checker/tests/nullness/Issue1142.java
+++ checker/tests/nullness/Issue1142.java
@@ -6,11 +6,9 @@ class Issue1142 {
 
     void foo() {
 
-        ConcurrentHashMap<Integer, Integer> chm1 = new ConcurrentHashMap<>();
+        ConcurrentHashMap<Integer, @Nullable Integer> chm1 = new ConcurrentHashMap<>();
 
-        ConcurrentHashMap<Integer, @Nullable Integer> chm2 = new ConcurrentHashMap<>();
-
+        //:: error: (assignment.type.incompatible)
         chm1.put(1, null);
-        chm2.put(1, null);
     }
 }
