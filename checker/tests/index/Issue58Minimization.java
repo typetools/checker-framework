518c4e6abd2d3b4aafd85b3412496a1dd7092f7f
Martin Kellogg <kelloggm@cs.washington.edu>
2017-03-21 11:40:09 -0700

    improve test case

--- checker/tests/index/Issue58Minimization.java
+++ checker/tests/index/Issue58Minimization.java
@@ -4,9 +4,25 @@ class Issue58Minimization {
 
     void test(@GTENegativeOne int x) {
         int z;
-        int w = (z = x);
         if ((z = x) != -1) {
             @NonNegative int y = z;
+        }
+        if ((z = x) != 1) {
+            //:: error: (assignment.type.incompatible)
+            @NonNegative int y = z;
+        }
+    }
+
+    void test2(@NonNegative int x) {
+        int z;
+        if ((z = x) != 0) {
+            @Positive int y = z;
+        }
+        if ((z = x) == 0) {
+            // do nothing
+            int y = 5;
+        } else {
+            @Positive int y = x;
         }
     }
 }
