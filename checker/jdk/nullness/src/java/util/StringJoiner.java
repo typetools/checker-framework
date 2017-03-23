9c3251e38061262167d4b64ff7b3f2ef2b0513f9
kanha95 <kanha95@gmail.com>
2017-03-23 23:56:50 +0530

    method bodies removed from source file

--- checker/jdk/nullness/src/java/util/StringJoiner.java
+++ checker/jdk/nullness/src/java/util/StringJoiner.java
@@ -15,77 +15,41 @@ public final class StringJoiner {
     private String emptyValue;
 
     public StringJoiner(@NonNull CharSequence delimiter) {
-        this(delimiter, "", "");
+        throw new RuntimeException("skeleton method");
     }
 
     public StringJoiner(@NonNull CharSequence delimiter,
                         @NonNull CharSequence prefix,
                         @NonNull CharSequence suffix) {
-        Objects.requireNonNull(prefix, "The prefix must not be null");
-        Objects.requireNonNull(delimiter, "The delimiter must not be null");
-        Objects.requireNonNull(suffix, "The suffix must not be null");
-        // make defensive copies of arguments
-        this.prefix = prefix.toString();
-        this.delimiter = delimiter.toString();
-        this.suffix = suffix.toString();
-        this.emptyValue = this.prefix + this.suffix;
+        throw new RuntimeException("skeleton method");
     }
 
     public StringJoiner setEmptyValue(@NonNull CharSequence emptyValue) {
-        this.emptyValue = Objects.requireNonNull(emptyValue,
-            "The empty value must not be null").toString();
-        return this;
+       throw new RuntimeException("skeleton method");
     }
 
    
     @Override
     public String toString() {
-        if (value == null) {
-            return emptyValue;
-        } else {
-            if (suffix.equals("")) {
-                return value.toString();
-            } else {
-                int initialLength = value.length();
-                String result = value.append(suffix).toString();
-                // reset value to pre-append initialLength
-                value.setLength(initialLength);
-                return result;
-            }
-        }
+      throw new RuntimeException("skeleton method");
     }
 
     
     public StringJoiner add(@Nullable CharSequence newElement) {
-        prepareBuilder().append(newElement);
-        return this;
+        throw new RuntimeException("skeleton method");
     }
 
 
     public StringJoiner merge(@NonNull StringJoiner other) {
-        Objects.requireNonNull(other);
-        if (other.value != null) {
-            final int length = other.value.length();
-        
-            StringBuilder builder = prepareBuilder();
-            builder.append(other.value, other.prefix.length(), length);
-        }
-        return this;
+        throw new RuntimeException("skeleton method");
     }
 
     private StringBuilder prepareBuilder() {
-        if (value != null) {
-            value.append(delimiter);
-        } else {
-            value = new StringBuilder().append(prefix);
-        }
-        return value;
+        throw new RuntimeException("skeleton method");
     }
 
    
     public int length() {
-       
-        return (value != null ? value.length() + suffix.length() :
-                emptyValue.length());
+       throw new RuntimeException("skeleton method");
     }
 }
