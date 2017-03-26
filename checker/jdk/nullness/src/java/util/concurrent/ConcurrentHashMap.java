786409a6dea4a49ba59711ec6faad8c43802c2e7
kanha95 <kanha95@gmail.com>
2017-03-26 08:20:49 +0530

    updated ConcurrentMap.java & omitted the extra @NonNull statements

--- checker/jdk/nullness/src/java/util/concurrent/ConcurrentHashMap.java
+++ checker/jdk/nullness/src/java/util/concurrent/ConcurrentHashMap.java
@@ -7,9 +7,8 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 public class ConcurrentHashMap<K extends @NonNull Object, V extends @NonNull Object> extends AbstractMap<K, V>
-        implements ConcurrentMap<K, V>, java.io.Serializable {
-
-    private static final long serialVersionUID = 0;
+        implements ConcurrentMap<K, V>, java.io.Serializable 
+{
 
      public ConcurrentHashMap(int initialCapacity,float loadFactor, int concurrencyLevel) {throw new RuntimeException("skeleton method");}
      public ConcurrentHashMap(int initialCapacity, float loadFactor) {throw new RuntimeException("skeleton method");}
@@ -21,13 +20,13 @@ public class ConcurrentHashMap<K extends @NonNull Object, V extends @NonNull Obj
      @Pure public boolean containsKey(@NonNull Object key) {throw new RuntimeException("skeleton method");}
      @Pure public boolean containsValue(@NonNull Object value) {throw new RuntimeException("skeleton method");}
      @Pure public boolean contains(@NonNull Object value) {throw new RuntimeException("skeleton method");}
-     public @Nullable V put(@NonNull K key,@NonNull V value) {throw new RuntimeException("skeleton method");}
-     public @Nullable V putIfAbsent(@NonNull K key, @NonNull V value) {throw new RuntimeException("skeleton method");}
+     public @Nullable V put(K key,V value) {throw new RuntimeException("skeleton method");}
+     public @Nullable V putIfAbsent(K key,V value) {throw new RuntimeException("skeleton method");}
      public void putAll(Map<? extends K, ? extends V> m) {throw new RuntimeException("skeleton method");}
-     public @Nullable V remove(@NonNull Object key) {throw new RuntimeException("skeleton method");}
+     public @Nullable V remove(Object key) {throw new RuntimeException("skeleton method");}
      public boolean remove(@NonNull Object key, @NonNull Object value) {throw new RuntimeException("skeleton method");}
-     public boolean replace(@NonNull K key, @NonNull V oldValue,@NonNull V newValue) {throw new RuntimeException("skeleton method");}
-     public @Nullable V replace(@NonNull K key, @NonNull V value) {throw new RuntimeException("skeleton method");}
+     public boolean replace(K key, V oldValue,V newValue) {throw new RuntimeException("skeleton method");}
+     public @Nullable V replace(K key, V value) {throw new RuntimeException("skeleton method");}
      public void clear() {throw new RuntimeException("skeleton method");}
      @SideEffectFree public Set<@KeyFor("this") K> keySet() {throw new RuntimeException("skeleton method");}
      @SideEffectFree public Collection<V> values() {throw new RuntimeException("skeleton method");}
@@ -36,7 +35,7 @@ public class ConcurrentHashMap<K extends @NonNull Object, V extends @NonNull Obj
      @SideEffectFree public Enumeration<V> elements() {throw new RuntimeException("skeleton method");}
      @SideEffectFree public Object clone() { throw new RuntimeException("skeleton method"); }
 
-	 }
+}
 
 
 
