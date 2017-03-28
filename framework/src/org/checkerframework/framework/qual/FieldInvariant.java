db641ca197d76d66e5318aa51fb5427aa4866f15
Suzanne Millstein <smillst@cs.washington.edu>
2017-03-28 15:56:27 -0700

    Improve documentation.

--- framework/src/org/checkerframework/framework/qual/FieldInvariant.java
+++ framework/src/org/checkerframework/framework/qual/FieldInvariant.java
@@ -6,7 +6,8 @@ import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+import javax.lang.model.element.TypeElement;
+import org.checkerframework.framework.type.AnnotatedTypeFactory;
 /**
  * In the class in which the field invariant holds, then field[i] has annotation[i].
  *
@@ -14,6 +15,12 @@ import java.lang.annotation.Target;
  * subclasses have the same annotation. If subclass has it's own @FieldInvariant, then it must
  * include the fields in the superclass annotation and those fields' annotations must be a subtype
  * (or equal) to the annotations for those fields in the the superclass @FieldInvariant.
+ *
+ * <p>If a type system includes qualifiers with attributes such as {@code @MinLen(1)} then, the type
+ * system can implement its own field invariant annotation and override {@link
+ * AnnotatedTypeFactory#getFieldInvariantDeclarationAnnotations()} and {@link
+ * AnnotatedTypeFactory#getFieldInvariants(TypeElement)}. See {@link
+ * org.checkerframework.checker.index.qual.MinLenFieldInvariant} for example.
  */
 @Target({ElementType.TYPE})
 @Inherited
