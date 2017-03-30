a2f5ba7bb7648ab1ac9de57b80dee97715ab331b
Martin Kellogg <kelloggm@cs.washington.edu>
2017-03-30 13:56:33 -0700

    new test from issue 132 that now passes due to resolution of 131

---
+++ checker/tests/index/Index132.java
@@ -0,0 +1,10 @@
+
+public class Index132 {
+    public static String /*@ArrayLen({3,4})*/[] esc_quantify(String /*@ArrayLen({1,2})*/... vars) {
+        if (vars.length == 1) {
+            return new String[] {"hello", vars[0], ")"};
+        } else {
+            return new String[] {"hello", vars[0], vars[1], ")"};
+        }
+    }
+}
