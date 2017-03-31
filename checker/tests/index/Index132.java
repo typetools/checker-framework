846d1627c971e08755a44d09e96fa366d16682b4
Martin Kellogg <kelloggm@cs.washington.edu>
2017-03-30 21:07:25 -0700

    fix typo in test

--- checker/tests/index/Index132.java
+++ checker/tests/index/Index132.java
@@ -1,6 +1,7 @@
+import org.checkerframework.common.value.qual.*;
 
 public class Index132 {
-    public static String /*@ArrayLen({3,4})*/[] esc_quantify(String /*@ArrayLen({1,2})*/... vars) {
+    public static String @ArrayLen({3, 4}) [] esc_quantify(String @ArrayLen({1, 2}) ... vars) {
         if (vars.length == 1) {
             return new String[] {"hello", vars[0], ")"};
         } else {
