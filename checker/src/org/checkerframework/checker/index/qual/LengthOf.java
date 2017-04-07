626259b596548c25979a304385c50c3c34a7bb4b
Martin Kellogg <kelloggm@cs.washington.edu>
2017-04-07 16:57:59 -0700

    adds something to manual mentioning lengthOf, updates javadoc

--- checker/src/org/checkerframework/checker/index/qual/LengthOf.java
+++ checker/src/org/checkerframework/checker/index/qual/LengthOf.java
@@ -1,10 +1,12 @@
 package org.checkerframework.checker.index.qual;
 
 /**
- * An integer that, for each of the given sequences, is equal to the sequence's length. This is
- * treated as an {@link IndexOrHigh} annotation internally.
+ * An integer that, for each of the given sequences, is equal to the sequence's length.
  *
- * @see IndexOrHigh
+ * <p>This is treated as an {@link IndexOrHigh} annotation internally. This is an implementation
+ * detail that may change in the future, when this type may be used to implement more precise
+ * refinements.
+ *
  * @checker_framework.manual #index-checker Index Checker
  */
 public @interface LengthOf {
