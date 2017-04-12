6141ebbcd2d9e37ec08d07a9d6f7773c31bc3eae
Martin Kellogg <kelloggm@cs.washington.edu>
2017-04-11 20:07:48 -0700

    tests for value checker with no overflow

---
+++ framework/tests/value-no-overflow/ValueNoOverflow.java
@@ -0,0 +1,31 @@
+import org.checkerframework.common.value.qual.*;
+
+class ValueNoOverflow {
+    void test_plus(@IntRange(from = 0) int x, @IntRange(from = -1) int z) {
+        @IntRange(from = 1)
+        int y = x + 1; // NonNegative to Positive
+        @IntRange(from = 0)
+        int w = z + 1; // GTEN1 to NN
+    }
+
+    void test_minus(@IntRange(to = 0) int x, @IntRange(to = 1) int z) {
+        @IntRange(to = -1)
+        int y = x - 1; // NonNegative to GTEN1
+        @IntRange(to = 0)
+        int w = z - 1; // Pos to NN
+    }
+
+    void test_mult(@IntRange(from = 0) int x, @IntRange(from = 1) int z) {
+        @IntRange(from = 0)
+        int y = x * z;
+        @IntRange(from = 1)
+        int w = z * z;
+    }
+
+    // Include ArrayLenRange tests once ArrayLenRange is merged.
+    /*
+     void arraylenrange_test(int @ArrayLenRange(from = 5) [] a) {
+         int @ArrayLenRange(from = 7) [] b = new int[a.length + 2];
+     }
+    */
+}
