91005c812d86d9433d4b04aa63c0a4c3e89fbbee
Suzanne Millstein <smillst@cs.washington.edu>
2017-04-13 09:42:16 -0700

    Fix formatting

--- framework/tests/value/RefineUnknownToIntRange.java
+++ framework/tests/value/RefineUnknownToIntRange.java
@@ -4,38 +4,31 @@ import org.checkerframework.common.value.qual.IntRange;
 public class RefineUnknownToIntRange {
     void test1(int x) {
         if (x > 1) {
-            @IntRange(from = 2)
-            int z = x;
+            @IntRange(from = 2) int z = x;
         }
 
         if (x < 1) {
-            @IntRange(to = 0)
-            int z = x;
+            @IntRange(to = 0) int z = x;
         }
 
         if (1 < x) {
-            @IntRange(from = 2)
-            int z = x;
+            @IntRange(from = 2) int z = x;
         }
 
         if (1 > x) {
-            @IntRange(to = 0)
-            int z = x;
+            @IntRange(to = 0) int z = x;
         }
 
         if (x >= 1) {
-            @IntRange(from = 1)
-            int z = x;
+            @IntRange(from = 1) int z = x;
         }
 
         if (x <= 1) {
-            @IntRange(to = 1)
-            int z = x;
+            @IntRange(to = 1) int z = x;
         }
 
         if (x < 100 && x > 2) {
-            @IntRange(from = 3, to = 99)
-            int z = x;
+            @IntRange(from = 3, to = 99) int z = x;
         }
     }
 
