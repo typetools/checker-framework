3086e0c51f6e9ba8b4fd0c5db545614b393934cf
Michael Ernst <mernst@cs.washington.edu>
2017-04-13 08:00:29 -0700

    Add passing test

---
+++ checker/tests/nullness/generics/BoundedWildcardTest.java
@@ -0,0 +1,43 @@
+// Test case from http://stackoverflow.com/questions/38339332/in-a-bounded-wildcard-where-does-the-annotation-belong
+
+import java.util.List;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+class Styleable {}
+
+class BoundedWildcardTest {
+
+    private void locChildren(Styleable c) {
+        // ...
+    }
+
+    public void initLoc(List<? extends Styleable> s) {
+        for (Styleable c : s) {
+            locChildren(c);
+        }
+    }
+
+    //:: error: (bound.type.incompatible)
+    public void initLoc1(@Nullable List<@Nullable ? extends Styleable> s) {
+        //:: error: (iterating.over.nullable)
+        for (Styleable c : s) {
+            locChildren(c);
+        }
+    }
+
+    public void initLoc2(@Nullable List<@Nullable ? extends @Nullable Styleable> s) {
+        //:: error: (iterating.over.nullable)
+        for (Styleable c : s) {
+            //:: error: argument.type.incompatible
+            locChildren(c);
+        }
+    }
+
+    public void initLoc3(@Nullable List<? extends @Nullable Styleable> s) {
+        //:: error: (iterating.over.nullable)
+        for (Styleable c : s) {
+            //:: error: argument.type.incompatible
+            locChildren(c);
+        }
+    }
+}
