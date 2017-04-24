cb5a4ab3356b96df56f7d48b67bf1ffd8c0d3b74
Suzanne Millstein <smillst@cs.washington.edu>
2017-04-24 16:01:18 -0700

    Add a few more test (which pass).

--- checker/tests/index/MinLenFromPositive.java
+++ checker/tests/index/MinLenFromPositive.java
@@ -46,8 +46,18 @@ class MinLenFromPositive {
         return x;
     }
 
-    void test_id() {
+    void test_id(int param) {
         @Positive int x = id(5);
         @IntRange(from = 1) int y = id(5);
+
+        int @MinLen(1) [] a = new int[id(100)];
+        //:: error: (assignment.type.incompatible)
+        int @MinLen(10) [] c = new int[id(100)];
+
+        int q = id(10);
+
+        if (param == q) {
+            int @MinLen(1) [] d = new int[param];
+        }
     }
 }
--- checker/tests/value-index-interaction/MinLenFromPositive.java
+++ checker/tests/value-index-interaction/MinLenFromPositive.java
@@ -1,16 +1,43 @@
 import org.checkerframework.checker.index.qual.Positive;
-import org.checkerframework.common.value.qual.*;
+import org.checkerframework.common.value.qual.IntRange;
+import org.checkerframework.common.value.qual.MinLen;
 
 class MinLenFromPositive {
 
-    void test(@Positive int x) {
+    public @Positive int x = 0;
+
+    void testField() {
+        this.x = -1;
+        @IntRange(from = 1) int f = this.x;
         int @MinLen(1) [] y = new int[x];
+    }
+
+    void testArray(@Positive int[] x) {
+        int @MinLen(1) [] array = new int[x[0]];
+    }
+
+    void test(@Positive int x) {
         @IntRange(from = 1) int z = x;
         @Positive int q = x;
+        @Positive int a = -1;
+        int @MinLen(1) [] array = new int[a];
     }
 
     // Ensure that just running the value checker doesn't result in an LHS warning.
     void foo2(int x) {
         test(x);
+    }
+
+    @Positive int id(@Positive int x) {
+        return -1;
+    }
+
+    @Positive int plus(@Positive int x, @Positive int y) {
+        //:: error: (assignment.type.incompatible)
+        @IntRange(from = 0) int z = x + y;
+        //:: error: (assignment.type.incompatible)
+        @IntRange(from = 1) int q = x + y;
+
+        return x + y;
     }
 }
