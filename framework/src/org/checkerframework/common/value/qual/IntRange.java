b5ec607fae8a010ef9239643dd37e0f5ed368715
Martin Kellogg <kelloggm@cs.washington.edu>
2017-05-24 10:26:20 -0400

    readd default fields so javac won't complain

--- framework/src/org/checkerframework/common/value/qual/IntRange.java
+++ framework/src/org/checkerframework/common/value/qual/IntRange.java
@@ -12,7 +12,9 @@ import org.checkerframework.framework.qual.SubtypeOf;
  * represents the four values 6, 7, 8, and 9.
  *
  * <p>If only one of the {@code to} and {@code from} fields is set, then the other will default to
- * the max/min value of the underlying type of the variable that is annotated.
+ * the max/min value of the underlying type of the variable that is annotated. Note that there must
+ * be "default" values (Long.MIN_VALUE and Long.MAX_VALUE below) even though they will be replaced
+ * in order to prevent javac from issuing warnings when only one of the values is set by a user.
  *
  * @checker_framework.manual #constant-value-checker Constant Value Checker
  */
@@ -21,7 +23,7 @@ import org.checkerframework.framework.qual.SubtypeOf;
 @Target({ElementType.TYPE_PARAMETER, ElementType.TYPE_USE})
 public @interface IntRange {
     /** Smallest value in the range, inclusive */
-    long from();
+    long from() default Long.MIN_VALUE;
     /** Largest value in the range, inclusive */
-    long to();
+    long to() default Long.MAX_VALUE;
 }
