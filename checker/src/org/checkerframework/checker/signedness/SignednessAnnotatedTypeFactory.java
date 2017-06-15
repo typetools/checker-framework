1a3de21b0d5b71b8d7cda0e4f9e0d3c11473c27a
Chris Mackie <mackic@cs.washington.edu>
2017-06-15 15:52:51 -0700

    Fixed format issues

--- checker/src/org/checkerframework/checker/signedness/SignednessAnnotatedTypeFactory.java
+++ checker/src/org/checkerframework/checker/signedness/SignednessAnnotatedTypeFactory.java
@@ -1,8 +1,8 @@
 package org.checkerframework.checker.signedness;
 
-import com.sun.source.tree.Tree;
 import com.sun.source.tree.BinaryTree;
 import com.sun.source.tree.CompoundAssignmentTree;
+import com.sun.source.tree.Tree;
 import java.lang.annotation.Annotation;
 import java.util.Set;
 import javax.lang.model.element.AnnotationMirror;
