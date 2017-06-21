9b682904c756b76a9f899d8cd44793dbc04b47fb
Michael Ernst <mernst@cs.washington.edu>
2017-06-21 07:49:20 -0700

    Add missing annotation

--- checker/jdk/optional/src/java/util/Optional.java
+++ checker/jdk/optional/src/java/util/Optional.java
@@ -31,6 +31,7 @@ import java.util.function.Supplier;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
+import org.checkerframework.checker.nullness.qual.PolyNull;
 import org.checkerframework.checker.optional.qual.Present;
 import org.checkerframework.framework.qual.EnsuresQualifierIf;
 
