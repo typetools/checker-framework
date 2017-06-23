c32a7b60b8c764e41d1d2e15f9210dd61e020034
Michael Ernst <mernst@cs.washington.edu>
2017-06-23 16:59:38 -0700

    Remove unneeded test file

--- checker/tests/optional/Marks2.java
+++ checker/tests/optional/Marks2.java
@@ -7,7 +7,7 @@ import java.util.Optional;
  * Test case for rule #2: "Never use Optional.get() unless you can prove that the Optional is
  * present."
  */
-public class Marks2b {
+public class Marks2 {
 
     class Customer {
         int getID() {
@@ -21,7 +21,7 @@ public class Marks2b {
 
     String customerNameByID(List<Customer> custList, int custID) {
         Optional<Customer> opt = custList.stream().filter(c -> c.getID() == custID).findFirst();
-        //:: warning: (prefer.map.and.orelse)
-        return opt.isPresent() ? opt.get().getName() : "UNKNOWN";
+        //:: error: (method.invocation.invalid)
+        return opt.get().getName();
     }
 }
