8b8b8848d6673789789a00937fee4386b17488e5
Vlastimil Dort <dort@d3s.mff.cuni.cz>
2017-06-26 14:47:33 +0200

    Comment test case

--- framework/tests/value/CharacterToString.java
+++ framework/tests/value/CharacterToString.java
@@ -1,3 +1,6 @@
+// Annotation on the return value of Character.toString caused
+// two annotations from the same hierarchy.
+// https://github.com/typetools/checker-framework/issues/1356
 public class CharacterToString {
     void m() {
         String s = Character.toString('a');
