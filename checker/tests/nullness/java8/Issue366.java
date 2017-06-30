618ce83c8c6e2954d159646ae4a97ec840c531d0
Michael Ernst <mernst@cs.washington.edu>
2017-06-29 19:17:21 -0700

    Make class name consistent with file name

--- checker/tests/nullness/java8/Issue366.java
+++ checker/tests/nullness/java8/Issue366.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
-class Test {
+class Issue366 {
     static Optional<@NonNull String> getPossiblyEmptyString() {
         return Optional.ofNullable(null);
     }
