06ad1d18ca8dc2e1cab9584b1f0420c4d85a2e97
Michael Ernst <mernst@cs.washington.edu>
2017-07-05 13:50:23 -0700

    Simplify test case

--- checker/tests/formatter/ConversionNull2.java
+++ checker/tests/formatter/ConversionNull2.java
@@ -1,14 +1,10 @@
 import java.util.Formatter;
-import java.util.Locale;
 import org.checkerframework.checker.formatter.qual.FormatMethod;
 
 public class ConversionNull2 {
     static void main(String... p) {
         Formatter f1 = new Formatter();
         MyFormatter f2 = new MyFormatter();
-
-        f1.format("%d %s", 0, null);
-        f2.format("%d %s", 0, null);
 
         //:: error: (argument.type.incompatible)
         f1.format("%d %c", 0, null);
@@ -20,11 +16,6 @@ public class ConversionNull2 {
 class MyFormatter {
     @FormatMethod
     public MyFormatter format(String format, Object... args) {
-        return null;
-    }
-
-    @FormatMethod
-    public MyFormatter format(Locale l, String format, Object... args) {
         return null;
     }
 }
