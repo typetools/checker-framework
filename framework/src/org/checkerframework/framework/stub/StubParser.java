ec72c7ca8a3ccc2178df46a5478d4e7b867aa36f
Bohdan Sharipov <bohdansharipov@gmail.com>
2017-07-24 16:59:04 -0400

    Revert "- Added skipping receiver parameter when comparing methods. A receiver could be added as first parameter to the method to annotate receiver."
    This reverts commit ff34e76cf514c60f04ef9276f91b98c61e0d46f8.

--- framework/src/org/checkerframework/framework/stub/StubParser.java
+++ framework/src/org/checkerframework/framework/stub/StubParser.java
@@ -965,8 +965,9 @@ public class StubParser {
 
     private ExecutableElement findElement(TypeElement typeElt, MethodDeclaration methodDecl) {
         final String wantedMethodName = methodDecl.getNameAsString();
-        final int wantedMethodParams = getWantedMethodParamsAmount(methodDecl);
-
+        final int wantedMethodParams =
+                (methodDecl.getParameters() == null) ? 0 : methodDecl.getParameters().size();
+        // TODO Make sure assignment was changed right
         final String wantedMethodString = StubUtil.toString(methodDecl);
         for (ExecutableElement method : ElementUtils.getAllMethodsIn(elements, typeElt)) {
             // do heuristics first
@@ -985,20 +986,6 @@ public class StubParser {
             }
         }
         return null;
-    }
-
-    /**
-     * Returns an amount of parameters in a method excluding receiver parameter which could be
-     * present to annotate receiver
-     */
-    private static int getWantedMethodParamsAmount(MethodDeclaration methodDecl) {
-        int result = (methodDecl.getParameters() == null) ? 0 : methodDecl.getParameters().size();
-        // This is required because first parameter in annotated declaration could be added to annotate
-        // the receiver which is not a parameter in the regular method declaration.
-        if (result > 0 && methodDecl.getParameter(0).getNameAsString().equals("this")) {
-            result--;
-        }
-        return result;
     }
 
     private ExecutableElement findElement(TypeElement typeElt, ConstructorDeclaration methodDecl) {
