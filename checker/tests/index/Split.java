4d3f66db55a1e8e974e5600459418a20ea90d5e6
Bohdan Sharipov <bohdansharipov@gmail.com>
2017-07-27 22:33:21 -0400

    - Reverted all the changes done in index/Split.java

--- checker/tests/index/Split.java
+++ checker/tests/index/Split.java
@@ -1,4 +1,3 @@
-
 import java.util.regex.Pattern;
 import org.checkerframework.common.value.qual.MinLen;
 
@@ -6,6 +5,6 @@ public class Split {
     Pattern p = Pattern.compile(".*");
 
     void test() {
-        @MinLen(1) String[] s = p.split("sdf");
+        String @MinLen(1) [] s = p.split("sdf");
     }
 }
