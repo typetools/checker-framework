15650052a20638945f1f61d3113863a1187032b1
Vlastimil Dort <dort@d3s.mff.cuni.cz>
2017-08-03 16:32:55 +0200

    Fix formatting

--- checker/tests/index/Index165StringLiteral.java
+++ checker/tests/index/Index165StringLiteral.java
@@ -4,12 +4,12 @@
 import org.checkerframework.checker.index.qual.IndexFor;
 
 public class Index165StringLiteral {
-	
+
     public void testMethodInvocation() {
-    	requiresIndex("012345", 5);
-    	//:: error: (argument.type.incompatible)
-    	requiresIndex("012345", 6);
+        requiresIndex("012345", 5);
+        //:: error: (argument.type.incompatible)
+        requiresIndex("012345", 6);
     }
-    
-    public void requiresIndex(String str, @IndexFor("#1") int index) {    }
+
+    public void requiresIndex(String str, @IndexFor("#1") int index) {}
 }
