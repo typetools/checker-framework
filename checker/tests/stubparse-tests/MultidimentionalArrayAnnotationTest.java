33987772f3e61f55cde3fa53199ab7958a110722
Bohdan Sharipov <bohdansharipov@gmail.com>
2017-08-07 20:28:20 -0400

    - Moved variables declaration to one place. Assignments will happen in the same places.

--- checker/tests/stubparse-tests/MultidimentionalArrayAnnotationTest.java
+++ checker/tests/stubparse-tests/MultidimentionalArrayAnnotationTest.java
@@ -6,137 +6,147 @@ public class MultidimentionalArrayAnnotationTest {
 
     int numb = 1;
 
+    String @Nullable [] @Nullable [] @Nullable [] obj1 = new String[numb][numb][numb];
+    String @NonNull [] @Nullable [] @Nullable [] obj2 = new String[numb][numb][numb];
+    String @Nullable [] @NonNull [] @Nullable [] obj3 = new String[numb][numb][numb];
+    String @Nullable [] @Nullable [] @NonNull [] obj4 = new String[numb][numb][numb];
+    String @NonNull [] @NonNull [] @Nullable [] obj5 = new String[numb][numb][numb];
+    String @NonNull [] @Nullable [] @NonNull [] obj6 = new String[numb][numb][numb];
+    String @Nullable [] @NonNull [] @NonNull [] obj7 = new String[numb][numb][numb];
+    String @NonNull [] @NonNull [] @NonNull [] obj8 = new String[numb][numb][numb];
+
     // Call to method 1
     void callTomethod1() {
-        String @Nullable [] @Nullable [] @Nullable [] obj1 = method1();
-        String @NonNull [] @Nullable [] @Nullable [] obj2 = method1();
-        String @Nullable [] @NonNull [] @Nullable [] obj3 = method1();
-        String @Nullable [] @Nullable [] @NonNull [] obj4 = method1();
-        String @NonNull [] @NonNull [] @Nullable [] obj5 = method1();
-        String @NonNull [] @Nullable [] @NonNull [] obj6 = method1();
-        String @Nullable [] @NonNull [] @NonNull [] obj7 = method1();
-        String @NonNull [] @NonNull [] @NonNull [] obj8 = method1();
+        obj1 = method1();
+        obj2 = method1();
+        obj3 = method1();
+        obj4 = method1();
+        obj5 = method1();
+        obj6 = method1();
+        obj7 = method1();
+        obj8 = method1();
+        ;
     }
 
     // Call to method 2
     void callTomethod2() {
-        String @Nullable [] @Nullable [] @Nullable [] obj1 = method2();
+        obj1 = method2();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @Nullable [] @Nullable [] obj2 = method2();
-        String @Nullable [] @NonNull [] @Nullable [] obj3 = method2();
-        String @Nullable [] @Nullable [] @NonNull [] obj4 = method2();
+        obj2 = method2();
+        obj3 = method2();
+        obj4 = method2();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @NonNull [] @Nullable [] obj5 = method2();
+        obj5 = method2();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @Nullable [] @NonNull [] obj6 = method2();
-        String @Nullable [] @NonNull [] @NonNull [] obj7 = method2();
+        obj6 = method2();
+        obj7 = method2();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @NonNull [] @NonNull [] obj8 = method2();
+        obj8 = method2();
     }
 
     // Call to method 3
     void callTomethod3() {
-        String @Nullable [] @Nullable [] @Nullable [] obj1 = method3();
-        String @NonNull [] @Nullable [] @Nullable [] obj2 = method3();
+        obj1 = method3();
+        obj2 = method3();
         //:: error: (assignment.type.incompatible)
-        String @Nullable [] @NonNull [] @Nullable [] obj3 = method3();
-        String @Nullable [] @Nullable [] @NonNull [] obj4 = method3();
+        obj3 = method3();
+        obj4 = method3();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @NonNull [] @Nullable [] obj5 = method3();
-        String @NonNull [] @Nullable [] @NonNull [] obj6 = method3();
+        obj5 = method3();
+        obj6 = method3();
         //:: error: (assignment.type.incompatible)
-        String @Nullable [] @NonNull [] @NonNull [] obj7 = method3();
+        obj7 = method3();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @NonNull [] @NonNull [] obj8 = method3();
+        obj8 = method3();
     }
 
     // Call to method 4
     void callTomethod4() {
-        String @Nullable [] @Nullable [] @Nullable [] obj1 = method4();
-        String @NonNull [] @Nullable [] @Nullable [] obj2 = method4();
-        String @Nullable [] @NonNull [] @Nullable [] obj3 = method4();
+        obj1 = method4();
+        obj2 = method4();
+        obj3 = method4();
         //:: error: (assignment.type.incompatible)
-        String @Nullable [] @Nullable [] @NonNull [] obj4 = method4();
-        String @NonNull [] @NonNull [] @Nullable [] obj5 = method4();
+        obj4 = method4();
+        obj5 = method4();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @Nullable [] @NonNull [] obj6 = method4();
+        obj6 = method4();
         //:: error: (assignment.type.incompatible)
-        String @Nullable [] @NonNull [] @NonNull [] obj7 = method4();
+        obj7 = method4();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @NonNull [] @NonNull [] obj8 = method4();
+        obj8 = method4();
     }
 
     // Call to method 5
     void callTomethod5() {
-        String @Nullable [] @Nullable [] @Nullable [] obj1 = method5();
+        obj1 = method5();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @Nullable [] @Nullable [] obj2 = method5();
+        obj2 = method5();
         //:: error: (assignment.type.incompatible)
-        String @Nullable [] @NonNull [] @Nullable [] obj3 = method5();
-        String @Nullable [] @Nullable [] @NonNull [] obj4 = method5();
+        obj3 = method5();
+        obj4 = method5();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @NonNull [] @Nullable [] obj5 = method5();
+        obj5 = method5();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @Nullable [] @NonNull [] obj6 = method5();
+        obj6 = method5();
         //:: error: (assignment.type.incompatible)
-        String @Nullable [] @NonNull [] @NonNull [] obj7 = method5();
+        obj7 = method5();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @NonNull [] @NonNull [] obj8 = method5();
+        obj8 = method5();
     }
 
     // Call to method 6
     void callTomethod6() {
-        String @Nullable [] @Nullable [] @Nullable [] obj1 = method6();
+        obj1 = method6();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @Nullable [] @Nullable [] obj2 = method6();
-        String @Nullable [] @NonNull [] @Nullable [] obj3 = method6();
+        obj2 = method6();
+        obj3 = method6();
         //:: error: (assignment.type.incompatible)
-        String @Nullable [] @Nullable [] @NonNull [] obj4 = method6();
+        obj4 = method6();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @NonNull [] @Nullable [] obj5 = method6();
+        obj5 = method6();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @Nullable [] @NonNull [] obj6 = method6();
+        obj6 = method6();
         //:: error: (assignment.type.incompatible)
-        String @Nullable [] @NonNull [] @NonNull [] obj7 = method6();
+        obj7 = method6();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @NonNull [] @NonNull [] obj8 = method6();
+        obj8 = method6();
     }
 
     // Call to method 7
     void callTomethod7() {
-        String @Nullable [] @Nullable [] @Nullable [] obj1 = method7();
-        String @NonNull [] @Nullable [] @Nullable [] obj2 = method7();
+        obj1 = method7();
+        obj2 = method7();
         //:: error: (assignment.type.incompatible)
-        String @Nullable [] @NonNull [] @Nullable [] obj3 = method7();
+        obj3 = method7();
         //:: error: (assignment.type.incompatible)
-        String @Nullable [] @Nullable [] @NonNull [] obj4 = method7();
+        obj4 = method7();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @NonNull [] @Nullable [] obj5 = method7();
+        obj5 = method7();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @Nullable [] @NonNull [] obj6 = method7();
+        obj6 = method7();
         //:: error: (assignment.type.incompatible)
-        String @Nullable [] @NonNull [] @NonNull [] obj7 = method7();
+        obj7 = method7();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @NonNull [] @NonNull [] obj8 = method7();
+        obj8 = method7();
     }
 
     // Call to method 8
     void callTomethod8() {
-        String @Nullable [] @Nullable [] @Nullable [] obj1 = method8();
+        obj1 = method8();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @Nullable [] @Nullable [] obj2 = method8();
+        obj2 = method8();
         //:: error: (assignment.type.incompatible)
-        String @Nullable [] @NonNull [] @Nullable [] obj3 = method8();
+        obj3 = method8();
         //:: error: (assignment.type.incompatible)
-        String @Nullable [] @Nullable [] @NonNull [] obj4 = method8();
+        obj4 = method8();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @NonNull [] @Nullable [] obj5 = method8();
+        obj5 = method8();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @Nullable [] @NonNull [] obj6 = method8();
+        obj6 = method8();
         //:: error: (assignment.type.incompatible)
-        String @Nullable [] @NonNull [] @NonNull [] obj7 = method8();
+        obj7 = method8();
         //:: error: (assignment.type.incompatible)
-        String @NonNull [] @NonNull [] @NonNull [] obj8 = method8();
+        obj8 = method8();
     }
 
     String[][][] method1() {
