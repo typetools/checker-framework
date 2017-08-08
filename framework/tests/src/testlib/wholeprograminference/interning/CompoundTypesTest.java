3c652169b04b13725a560e9a6d0e37eb439ecbe0
Felipe R. Monteiro <rms.felipe@gmail.com>
2017-08-08 18:08:45 -0400

    Toy program for testing inference of compound types
    Signed-off-by: Felipe R. Monteiro <rms.felipe@gmail.com>

---
+++ framework/tests/src/testlib/wholeprograminference/interning/CompoundTypesTest.java
@@ -0,0 +1,11 @@
+package testlib.wholeprograminference.interning;
+
+/**
+ * Toy program for testing inference of compound types.
+ *
+ * @author feliperodri
+ */
+class CompoundTypesTest {
+    private static int[] foo = new int[10];
+    private static String[] bar = new String[10];
+}
