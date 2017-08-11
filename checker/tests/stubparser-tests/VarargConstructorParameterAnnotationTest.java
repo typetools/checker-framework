d7c6191c89669f25fd08614fcf5cc12c8eb1957e
Bohdan Sharipov <bohdansharipov@gmail.com>
2017-08-11 18:31:30 -0400

    - Fixed the test varargConstructorParameterAnnotaion

--- checker/tests/stubparser-tests/VarargConstructorParameterAnnotationTest.java
+++ checker/tests/stubparser-tests/VarargConstructorParameterAnnotationTest.java
@@ -6,20 +6,20 @@ import org.checkerframework.checker.nullness.qual.*;
 class ProcessBuilding2 {
 
     public void strArraysNonNull(@NonNull String[] parameter) {
-        new ProcessBuilder(untrustedArr);
+        new ProcessBuilder(parameter);
     }
 
     public void strArraysNullable(@Nullable String[] parameter) {
         //:: error: (argument.type.incompatible)
-        new ProcessBuilder(untrustedArr);
+        new ProcessBuilder(parameter);
     }
 
     public void strVarargNonNull(@NonNull String... parameter) {
-        new ProcessBuilder(untrustedArr);
+        new ProcessBuilder(parameter);
     }
 
     public void strVarargNullable(@Nullable String... parameter) {
         //:: error: (argument.type.incompatible)
-        new ProcessBuilder(untrustedArr);
+        new ProcessBuilder(parameter);
     }
 }
