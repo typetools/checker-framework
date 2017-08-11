3365fc891ab96a5d6d3b9b944b5eac438fcca7b1
Vlastimil Dort <dort@d3s.mff.cuni.cz>
2017-08-11 16:33:03 +0200

    Fix test

--- checker/tests/index/RegexMatcher.java
+++ checker/tests/index/RegexMatcher.java
@@ -7,9 +7,18 @@ import java.util.regex.Pattern;
 public class RegexMatcher {
     static void m(String p, String s) {
         Matcher matcher = Pattern.compile(p).matcher(s);
+        // The following line cannot be used as a test,
+        // because the relation of matcher to p is not tracked,
+        // so the upper bound is not known.
 
-        s.substring(matcher.start(), matcher.end());
-        //:: error: (argument.type.incompatible)
-        s.substring(matcher.start(1), matcher.end(1));
+        // s.substring(matcher.start(), matcher.end());
+
+        @NonNegative int i;
+        i = matcher.start();
+        i = matcher.end();
+        //:: error: (assignment.type.incompatible)
+        i = matcher.start(1);
+        //:: error: (assignment.type.incompatible)
+        i = matcher.end(1);
     }
 }
