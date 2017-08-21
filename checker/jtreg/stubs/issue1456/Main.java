d6c6162987dc41bc84f2d66dfda64d3b6cd48331
Bohdan Sharipov <bohdansharipov@gmail.com>
2017-08-21 14:03:40 -0400

    - Added a test case for the #1456

---
+++ checker/jtreg/stubs/issue1456/Main.java
@@ -0,0 +1,8 @@
+/*
+ * @test
+ * @summary Test case for Issue 1456.
+ * https://github.com/typetools/checker-framework/issues/1456
+ * @compile -processor nullness -Astubs=MyClass.astub Main.java
+ */
+
+class Main {}
