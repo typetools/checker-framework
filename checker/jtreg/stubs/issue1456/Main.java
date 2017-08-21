4a228da01851615588d718efad2e002a47871981
Bohdan Sharipov <bohdansharipov@gmail.com>
2017-08-21 17:22:23 -0400

    Fixed the test for issue #1456. Renamed the stub file.

--- checker/jtreg/stubs/issue1456/Main.java
+++ checker/jtreg/stubs/issue1456/Main.java
@@ -2,7 +2,7 @@
  * @test
  * @summary Test case for Issue 1456.
  * https://github.com/typetools/checker-framework/issues/1456
- * @compile -processor nullness -Astubs=MyClass.astub Main.java
+ * @compile -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker -Astubs=StubfileWithField.astub Main.java
  */
 
 class Main {}
