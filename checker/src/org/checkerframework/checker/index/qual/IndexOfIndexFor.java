4061ead72cefe5041ba2512b04846c06d26df1e0
Vlastimil Dort <dort@d3s.mff.cuni.cz>
2017-08-26 16:13:45 +0200

    Extend javadoc

--- checker/src/org/checkerframework/checker/index/qual/IndexOfIndexFor.java
+++ checker/src/org/checkerframework/checker/index/qual/IndexOfIndexFor.java
@@ -8,11 +8,19 @@ import org.checkerframework.framework.qual.JavaExpression;
 import org.checkerframework.framework.qual.SubtypeOf;
 
 /**
- * The annotated expression evaluates to either -1 or a value compatible with a {@link LTLengthOf}
- * annotation with the same parameters.
+ * The annotated expression evaluates to either -1 or a less than the lengths of all the given
+ * sequences. The restrictions that this annotation poses on the set of possible values of the
+ * annotated expression are similar to the {@link LTLengthOf} annotation with the same parameters,
+ * except that this annotation additionally allows the value to be -1.
  *
  * <p>This is the return type of {@link java.lang.String#indexOf(String) String.indexOf} and {@link
- * java.lang.String#lastIndexOf(String) String.lastIndexOf} in the JDK.
+ * java.lang.String#lastIndexOf(String) String.lastIndexOf} in the JDK. The return type of these
+ * methods is annotated {@code @IndexOfIndexFor(value="this",offset="#1.length()-1")}, meaning that
+ * the returned value is either -1 or it is less than or equal to the length of the receiver
+ * sequence minus the length of the sequence passed as the first argument.
+ *
+ * <p>The name of this annotation, "indexOf index for", is to mean that the annotated expression is
+ * a index returned by {@code indexOf} for the specified sequence.
  *
  * @checker_framework.manual #index-checker Index Checker
  */
@@ -20,14 +28,18 @@ import org.checkerframework.framework.qual.SubtypeOf;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 public @interface IndexOfIndexFor {
-    /** Sequences, each of which is longer than the annotated expression's value. */
+    /**
+     * Sequences, each of which is longer than the annotated expression's value with the
+     * corresponding offset added, except when the annotated expression's value is -1.
+     */
     @JavaExpression
     public String[] value();
 
     /**
-     * If the annotated expression is not -1, then this expression plus the annotated expression is
-     * less than the length of the sequence. The {@code offset} element must ether be empty or the
-     * same length as {@code value}.
+     * Array of offset expressions. For each offset expression, if the annotated expression's value
+     * is not -1, then the the annotated expression's value plus the offset expression's value is
+     * less than the length of the corresponding sequence in the {@code value} array. The {@code
+     * offset} array must either be empty or the same length as {@code value}.
      *
      * <p>The expressions in {@code offset} may be addition/subtraction of any number of Java
      * expressions. For example, {@code @IndexOfIndexFor(value = "a", offset = "b.length() - 1")}.
