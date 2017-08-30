c4717037d1e6f43cdec44916257b5d13e3245428
bitterfox <bitterfoxc@gmail.com>
2017-08-31 03:04:00 +0900

    Fix #1214: CFG construction does not have branches to catch blocks for unchecked exceptions (#1418)
    The cause of issue #1214 is that there is no ArithmeticException path in CFG for division operation tough it could occur.
    By adding such path at every operation which it could occur, this issue is fixed.
    Note that OOME which is shown as an example in the issue is an Error and there is no path to catch (Exception e) {...}even if OOME occurred.
    And I believe that it's not necessary to consider Error path for all tree because Error is usually not recoverable and applications shouldn't catch it.

--- dataflow/src/org/checkerframework/dataflow/cfg/CFGBuilder.java
+++ dataflow/src/org/checkerframework/dataflow/cfg/CFGBuilder.java
@@ -2689,6 +2689,10 @@ public class CFGBuilder {
                         } else if (kind == Tree.Kind.DIVIDE_ASSIGNMENT) {
                             if (TypesUtils.isIntegral(exprType)) {
                                 operNode = new IntegerDivisionNode(operTree, targetRHS, value);
+
+                                TypeElement throwableElement =
+                                        elements.getTypeElement("java.lang.ArithmeticException");
+                                extendWithNodeWithException(operNode, throwableElement.asType());
                             } else {
                                 operNode = new FloatingDivisionNode(operTree, targetRHS, value);
                             }
@@ -2696,6 +2700,10 @@ public class CFGBuilder {
                             assert kind == Kind.REMAINDER_ASSIGNMENT;
                             if (TypesUtils.isIntegral(exprType)) {
                                 operNode = new IntegerRemainderNode(operTree, targetRHS, value);
+
+                                TypeElement throwableElement =
+                                        elements.getTypeElement("java.lang.ArithmeticException");
+                                extendWithNodeWithException(operNode, throwableElement.asType());
                             } else {
                                 operNode = new FloatingRemainderNode(operTree, targetRHS, value);
                             }
@@ -2899,6 +2907,10 @@ public class CFGBuilder {
                         } else if (kind == Tree.Kind.DIVIDE) {
                             if (TypesUtils.isIntegral(exprType)) {
                                 r = new IntegerDivisionNode(tree, left, right);
+
+                                TypeElement throwableElement =
+                                        elements.getTypeElement("java.lang.ArithmeticException");
+                                extendWithNodeWithException(r, throwableElement.asType());
                             } else {
                                 r = new FloatingDivisionNode(tree, left, right);
                             }
@@ -2906,6 +2918,10 @@ public class CFGBuilder {
                             assert kind == Kind.REMAINDER;
                             if (TypesUtils.isIntegral(exprType)) {
                                 r = new IntegerRemainderNode(tree, left, right);
+
+                                TypeElement throwableElement =
+                                        elements.getTypeElement("java.lang.ArithmeticException");
+                                extendWithNodeWithException(r, throwableElement.asType());
                             } else {
                                 r = new FloatingRemainderNode(tree, left, right);
                             }
@@ -3610,6 +3626,8 @@ public class CFGBuilder {
                 arrayAccessNode.setInSource(false);
                 extendWithNode(arrayAccessNode);
                 translateAssignment(variable, new LocalVariableNode(variable), arrayAccessNode);
+                Element npeElement = elements.getTypeElement("java.lang.NullPointerException");
+                extendWithNodeWithException(arrayAccessNode, npeElement.asType());
 
                 if (statement != null) {
                     scan(statement, p);
@@ -3827,7 +3845,13 @@ public class CFGBuilder {
         public Node visitArrayAccess(ArrayAccessTree tree, Void p) {
             Node array = scan(tree.getExpression(), p);
             Node index = unaryNumericPromotion(scan(tree.getIndex(), p));
-            return extendWithNode(new ArrayAccessNode(tree, array, index));
+            Node arrayAccess = extendWithNode(new ArrayAccessNode(tree, array, index));
+            Element aioobeElement =
+                    elements.getTypeElement("java.lang.ArrayIndexOutOfBoundsException");
+            extendWithNodeWithException(arrayAccess, aioobeElement.asType());
+            Element npeElement = elements.getTypeElement("java.lang.NullPointerException");
+            extendWithNodeWithException(arrayAccess, npeElement.asType());
+            return arrayAccess;
         }
 
         @Override
