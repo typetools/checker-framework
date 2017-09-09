d5a1864179cc09dc566ff93f8189e38b1bc2789e
Michael Ernst <mernst@alum.mit.edu>
2017-09-08 17:22:08 -0700

    Enable one @FormatMethod method to forward its arguments to another one; fixes #1507

---
+++ checker/tests/formatter/FormatMethodAnnotation.java
@@ -0,0 +1,27 @@
+// Test case for Issue 1507:
+// https://github.com/typetools/checker-framework/issues/1507
+
+import java.util.Locale;
+import org.checkerframework.checker.formatter.qual.FormatMethod;
+
+public class FormatMethodAnnotation {
+
+    public void example() {
+        String ex1 = String.format(Locale.ENGLISH, "%s %d", "cost", 12);
+        log("%d", 0);
+        log2(Locale.ENGLISH, "%d", 0);
+    }
+
+    @FormatMethod
+    static void log(String format, Object... args) {
+        String ex1 = String.format(format, args);
+        String ex2 = String.format(Locale.ENGLISH, format, args);
+    }
+
+    @FormatMethod
+    static void log2(Locale locale, String format, Object... args) {
+        String ex1 = String.format(format, args);
+        String ex2 = String.format(locale, format, args);
+        String ex3 = String.format(Locale.FRENCH, format, args);
+    }
+}
