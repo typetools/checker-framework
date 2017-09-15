083cfa43a8af0fc7ce19a550cf4a5897b52ed15f
Bohdan Sharipov <bohdansharipov@gmail.com>
2017-09-15 09:09:28 -0400

    Addressed some requested changes.

--- checker/jtreg/stubs/issue1496/Main.java
+++ checker/jtreg/stubs/issue1496/Main.java
@@ -2,8 +2,8 @@
  * @test
  * @summary Test case for Issue 1496 https://github.com/typetools/checker-framework/issues/1496
  * @compile -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker -Astubs=ClassAnnotation.astub Main.java
- * */
+ */
 
 package issue1496;
 
-public class Main extends Object {}
+public class Main {}
