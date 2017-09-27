654ef407da8ad73dd51be5c659f7ccab132d6dd0
Michael Ernst <mernst@cs.washington.edu>
2017-09-27 09:09:54 -0700

    Spelling fixes

--- checker/jtreg/index/ValueStubDriver.java
+++ checker/jtreg/index/ValueStubDriver.java
@@ -1,6 +1,6 @@
 /*
  * @test
- * @summary Test problem with alaising LengthOf -> NonNegative and NonNegative -> IntRangeFromNonNegative
+ * @summary Test problem with aliasing LengthOf -> NonNegative and NonNegative -> IntRangeFromNonNegative
  *
  * @compile -XDrawDiagnostics -processor org.checkerframework.checker.index.IndexChecker -AprintErrorStack valuestub/Test.java -Astubs=valuestub/Test.astub
  * @compile -XDrawDiagnostics -processor org.checkerframework.checker.index.IndexChecker -AprintErrorStack valuestub/UseTest.java
