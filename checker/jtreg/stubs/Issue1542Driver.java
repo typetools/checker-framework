cfba539a5c0573920dd431b36557fd348078d9ab
Suzanne Millstein <smillst@cs.washington.edu>
2017-09-29 06:06:52 -0700

    Correct Stubparser handling of expressions in annotations (#1547)
    This fixes both #1542 and #1496. This uses code written by @Bohdankm22 in #1524 to parse class literals.

---
+++ checker/jtreg/stubs/Issue1542Driver.java
@@ -0,0 +1,10 @@
+/*
+ * @test
+ * @summary Test case for Issue 1542 https://github.com/typetools/checker-framework/issues/1542
+ * @compile -XDrawDiagnostics issue1542/NeedsIntRange.java issue1542/UsesIntRange.java
+ * @compile -XDrawDiagnostics -Xlint:unchecked -processor org.checkerframework.checker.tainting.TaintingChecker -AprintErrorStack -Anomsgtext Issue1542Driver.java -Astubs=issue1542/ -AstubWarnIfNotFound
+ */
+
+package issue1542;
+
+public class Issue1542Driver {}
