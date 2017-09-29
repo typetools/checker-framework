bc1b7c9c4247ec5576719b7a2937c91801272add
Werner Dietl <wdietl@gmail.com>
2017-09-28 22:36:47 -0400

    Change to `-Astubs=issue1542/` to make more obvious that whole directory is passed.

--- checker/jtreg/stubs/Issue1542Driver.java
+++ checker/jtreg/stubs/Issue1542Driver.java
@@ -2,7 +2,7 @@
  * @test
  * @summary Test case for Issue 1542 https://github.com/typetools/checker-framework/issues/1542
  * @compile -XDrawDiagnostics issue1542/*.java
- * @compile -XDrawDiagnostics -Xlint:unchecked -processor org.checkerframework.checker.tainting.TaintingChecker -AprintErrorStack -Anomsgtext Issue1542Driver.java -Astubs=issue1542 -AstubWarnIfNotFound
+ * @compile -XDrawDiagnostics -Xlint:unchecked -processor org.checkerframework.checker.tainting.TaintingChecker -AprintErrorStack -Anomsgtext Issue1542Driver.java -Astubs=issue1542/ -AstubWarnIfNotFound
  */
 
 package issue1542;
