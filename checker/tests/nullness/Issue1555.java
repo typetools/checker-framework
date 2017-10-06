f92261c3b2b38438297cc8739be93bed033a1c9a
Suzanne Millstein <smillst@cs.washington.edu>
2017-10-06 09:02:30 -0700

    Fixes.

--- checker/tests/nullness/Issue1555.java
+++ checker/tests/nullness/Issue1555.java
@@ -1,9 +1,11 @@
+// Test case for Issue 1555
+// https://github.com/typetools/checker-framework/issues/1555
 import org.checkerframework.checker.nullness.NullnessUtil;
 import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 
 class Issue1555 {
 
-    @MonotonicNonNull private String x;
+    private @MonotonicNonNull String x;
 
     String test() {
         return NullnessUtil.castNonNull(x);
