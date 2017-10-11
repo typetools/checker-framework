17aa6474d175377cf1d406849a14619030a5576b
Martin Kellogg <kelloggm@cs.washington.edu>
2017-10-11 11:59:20 -0700

    formatting. I'm not sure how this wasn't caught by the commit hooks

--- framework/src/org/checkerframework/common/basetype/BaseTypeVisitor.java
+++ framework/src/org/checkerframework/common/basetype/BaseTypeVisitor.java
@@ -2401,8 +2401,9 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
      * Type checks that a method may override another method. Uses an OverrideChecker subclass as
      * created by createOverrideChecker(). This version of the method uses the annotated type
      * factory to get the annotated type of the overriding method, and does NOT expose that type.
-     * @see #checkOverride(MethodTree, AnnotatedExecutableType, AnnotatedDeclaredType, AnnotatedExecutableType, AnnotatedDeclaredType, Void)
      *
+     * @see #checkOverride(MethodTree, AnnotatedExecutableType, AnnotatedDeclaredType,
+     *     AnnotatedExecutableType, AnnotatedDeclaredType, Void)
      * @param overriderTree declaration tree of overriding method
      * @param overridingType type of overriding class
      * @param overridden type of overridden method
@@ -2420,17 +2421,19 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
         AnnotatedExecutableType overrider = atypeFactory.getAnnotatedType(overriderTree);
 
         // Call the other version of the method, which takes overrider. Both versions
-	// exist to allow checkers to override one or the other depending on their needs.
+        // exist to allow checkers to override one or the other depending on their needs.
         return checkOverride(
                 overriderTree, overrider, overridingType, overridden, overriddenType, p);
     }
 
     /**
      * Type checks that a method may override another method. Uses an OverrideChecker subclass as
-     * created by createOverrideChecker(). This version of the method exposes AnnotatedExecutableType
-     * of the overriding method. Override this version of the method if you need to access that type.
-     * @see #checkOverride(MethodTree, AnnotatedDeclaredType, AnnotatedExecutableType, AnnotatedDeclaredType, Void)
+     * created by createOverrideChecker(). This version of the method exposes
+     * AnnotatedExecutableType of the overriding method. Override this version of the method if you
+     * need to access that type.
      *
+     * @see #checkOverride(MethodTree, AnnotatedDeclaredType, AnnotatedExecutableType,
+     *     AnnotatedDeclaredType, Void)
      * @param overriderTree declaration tree of overriding method
      * @param overrider type of the overriding method
      * @param overridingType type of overriding class
