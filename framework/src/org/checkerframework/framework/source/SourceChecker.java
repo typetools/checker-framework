3a2a0ba8ecfd7f2b144ba504fbd1e4a0676f75c7
Werner Dietl <wdietl@gmail.com>
2017-10-15 18:22:50 -0400

    Comment on another use of trees.getPath.

--- framework/src/org/checkerframework/framework/source/SourceChecker.java
+++ framework/src/org/checkerframework/framework/source/SourceChecker.java
@@ -1329,6 +1329,9 @@ public abstract class SourceChecker extends AbstractTypeProcessor
             return false;
         }
 
+        // trees.getPath might be slow, but this is only used in error reporting
+        // TODO: #1586 this might return null within a cloned finally block and
+        // then a warning that should be suppressed isn't. Fix this when fixing #1586.
         /*@Nullable*/ TreePath path = trees.getPath(this.currentRoot, tree);
         if (path == null) {
             return false;
