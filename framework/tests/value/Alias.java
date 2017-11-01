6eed5f07e64708bc8f55df10b287acede3fa17e5
Jiasen (Jason) Xu <jiasenxu@gmail.com>
2017-11-01 10:28:55 -0700

    Fix #1386: copy values when getting alias annotation from canonical annotation (#1393)
    * Fix #1386: copy values when building canonical annotation from alias
    
    * fix lower bound checker crash
    
    * update deprecated usage
    
    * add ignorable element list to add alises methods
    
    * fix type issue
    
    * fix javadoc
    
    * Javadoc fix
    
    * address Werner's comments
    
    * javadoc tweak
    
    * Further revise the documentations for alias related helper functions
    
    * Further explain in javadoc the usage of the add alias with ignorable elements feature
    
    * fix value test afer merge

---
+++ framework/tests/value/Alias.java
@@ -0,0 +1,10 @@
+import android.support.annotation.IntRange;
+
+class Alias {
+    public void androidIntRange() {
+        // :: error: (assignment.type.incompatible)
+        @IntRange(from = 0, to = 10) int j = 13;
+        // :: error: (assignment.type.incompatible)
+        @IntRange(from = 0) int k = -1;
+    }
+}
