368ee5c74bc8222a089faf1b09faf667cfe15a9f
Michael Ernst <mernst@cs.washington.edu>
2017-11-17 12:10:45 -0800

    Optimization

--- javacutil/src/org/checkerframework/javacutil/TypesUtils.java
+++ javacutil/src/org/checkerframework/javacutil/TypesUtils.java
@@ -172,7 +172,7 @@ public final class TypesUtils {
      */
     public static boolean areSameDeclaredTypes(Type.ClassType t1, Type.ClassType t2) {
         // Do a cheaper test first
-        if (!t1.tsym.name.toString().equals(t2.tsym.name.toString())) {
+        if (t1.tsym.name != t2.tsym.name) {
             return false;
         }
         return t1.toString().equals(t1.toString());
