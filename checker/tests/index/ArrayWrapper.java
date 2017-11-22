e781557bf6025f51e08dd55e7dc25cd99bcec932
Michael Ernst <mernst@cs.washington.edu>
2017-11-21 20:58:10 -0800

    Simplify code and text

--- checker/tests/index/ArrayWrapper.java
+++ checker/tests/index/ArrayWrapper.java
@@ -1,41 +1,45 @@
+// Test case for https://github.com/kelloggm/checker-framework/issues/154
+// This class wraps an array, but doesn't expose the array in its public interface. This test
+// ensures that indexes for this new collection can be annotated as if the collection were an array.
+
 import org.checkerframework.checker.index.qual.IndexFor;
 import org.checkerframework.checker.index.qual.LengthOf;
 import org.checkerframework.checker.index.qual.NonNegative;
 import org.checkerframework.checker.index.qual.SameLen;
 
-/**
- * A test for https://github.com/kelloggm/checker-framework/issues/154
- *
- * <p>This class wraps an array, but doesn't expose the array in its public interface. This test
- * ensures that indexes for this new collection can be annotated as if the collection were an array.
- *
- * <p>ArrayWrapper is a fixed-size, weakly typed generic collection.
- */
-@SuppressWarnings("unchecked")
+/** ArrayWrapper is a fixed-size, weakly-typed generic collection. */
 public class ArrayWrapper<T> {
-    private final Object @SameLen("this") [] internalRep;
+    private final Object @SameLen("this") [] delegate;
 
+    @SuppressWarnings("index") // constructor creates object of size @SameLen(this) by definition
     ArrayWrapper(@NonNegative int size) {
-        @SuppressWarnings(
-                "index") // use the SameLen checker to ensure that anything that's an index for "this" is an
-        // index for the internal array representation. This suppress warnings asserts this fact.
-        final Object @SameLen("this") [] tmp = new Object[size];
-        internalRep = tmp;
+        delegate = new Object[size];
     }
 
     public @LengthOf("this") int size() {
-        return internalRep.length;
+        return delegate.length;
     }
 
     public void set(@IndexFor("this") int index, T obj) {
-        internalRep[index] = obj;
+        delegate[index] = obj;
     }
 
+    @SuppressWarnings("unchecked") // required for normal Java compilation due to unchecked cast
     public T get(@IndexFor("this") int index) {
-        return (T) internalRep[index];
+        return (T) delegate[index];
     }
 
-    public static void clearIndex(ArrayWrapper a, @NonNegative int i) {
+    public static void clearIndex1(ArrayWrapper<? extends Object> a, @IndexFor("#1") int i) {
+        a.set(i, null);
+    }
+
+    public static void clearIndex2(ArrayWrapper<? extends Object> a, int i) {
+        if (0 <= i && i < a.size()) {
+            a.set(i, null);
+        }
+    }
+
+    public static void clearIndex3(ArrayWrapper<? extends Object> a, @NonNegative int i) {
         if (i < a.size()) {
             a.set(i, null);
         }
