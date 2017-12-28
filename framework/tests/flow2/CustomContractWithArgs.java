da0ec4209aad64433f97e640cc4426f227f1bb88
Vlastimil Dort <dort@d3s.mff.cuni.cz>
2017-12-28 20:06:43 +0100

    Add test to framework

---
+++ framework/tests/flow2/CustomContractWithArgs.java
@@ -0,0 +1,92 @@
+import org.checkerframework.framework.qual.ConditionalPostconditionAnnotation;
+import org.checkerframework.framework.qual.PostconditionAnnotation;
+import org.checkerframework.framework.qual.PreconditionAnnotation;
+import testlib.util.Value;
+
+public class CustomContractWithArgs {
+    // Postcondition for Value
+    @PostconditionAnnotation(
+        qualifier = Value.class,
+        sourceArguments = "targetValue",
+        targetArguments = "value"
+    )
+    @interface EnsuresValue {
+        public String[] value();
+
+        public int targetValue();
+    }
+
+    // Conditional postcondition for LTLengthOf
+    @ConditionalPostconditionAnnotation(
+        qualifier = Value.class,
+        sourceArguments = "targetValue",
+        targetArguments = "value"
+    )
+    @interface EnsuresValueIf {
+        public boolean result();
+
+        public String[] expression();
+
+        public int targetValue();
+    }
+
+    //Precondition for LTLengthOf
+    @PreconditionAnnotation(
+        qualifier = Value.class,
+        sourceArguments = "targetValue",
+        targetArguments = "value"
+    )
+    @interface RequiresValue {
+        public String[] value();
+
+        public int targetValue();
+    }
+
+    class Base {
+        Object o;
+
+        @Value(10) Object o10;
+
+        @EnsuresValue(value = "o", targetValue = 10)
+        void ensures() {
+            o = o10;
+        }
+
+        @EnsuresValue(value = "o", targetValue = 9)
+        // :: error: (contracts.postcondition.not.satisfied)
+        void ensuresWrong() {
+            o = o10;
+        }
+
+        void ensuresUse() {
+            ensures();
+            o10 = o;
+        }
+
+        @EnsuresValueIf(expression = "o", targetValue = 10, result = true)
+        boolean ensuresIf(boolean b) {
+            if (b) {
+                o = o10;
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        @RequiresValue(value = "o", targetValue = 10)
+        void requires() {
+            o10 = o;
+        }
+
+        void use(boolean b) {
+            if (ensuresIf(b)) {
+                o10 = o;
+                requires();
+            }
+            // :: error: (assignment.type.incompatible)
+            o10 = o;
+            // :: error: (contracts.precondition.not.satisfied)
+            requires();
+        }
+    }
+}
