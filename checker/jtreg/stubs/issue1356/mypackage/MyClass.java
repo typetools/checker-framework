5c6dae72ccc7dbaa7ef4d550f5de84438710231f
Suzanne Millstein <smillst@cs.washington.edu>
2018-01-05 01:19:39 -0800

    Fix StubParser two dimensional array bug.  (#1745)
    Fix two dimensional array bug.

--- checker/jtreg/stubs/issue1356/mypackage/MyClass.java
+++ checker/jtreg/stubs/issue1356/mypackage/MyClass.java
@@ -4,4 +4,12 @@ public class MyClass {
     static String toString(char c) {
         return c + "";
     }
+
+    void method1(Object[] param) {}
+
+    void method2(Object[][] param) {}
+
+    void method3(Object[][][] param) {}
+
+    void method4(Object[][][][] param) {}
 }
