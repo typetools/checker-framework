69166e7d5847cdf003764f252de946d69c91ac33
Vlastimil Dort <dort@d3s.mff.cuni.cz>
2018-01-09 17:01:45 +0100

    Improve javadoc

--- javacutil/src/org/checkerframework/javacutil/AnnotationBuilder.java
+++ javacutil/src/org/checkerframework/javacutil/AnnotationBuilder.java
@@ -181,22 +181,22 @@ public class AnnotationBuilder {
     }
 
     /**
-     * Copies the specified element values from the given annotation, using the specified name
-     * mapping. If an element specified as a target in the mapping doesn't exist in the annotation
-     * to be built, an error is raised. If an element from the given annotation is not in the
-     * mapping, it is ignored.
+     * Copies the specified element values from the given annotation, using the specified renaming
+     * map. Each value in the map must be an element name in the annotation being built. If an
+     * element from the given annotation is not a key in the map, it is ignored.
      *
      * @param valueHolder the annotation that holds the values to be copied
-     * @param mapping mapping from source to target names
+     * @param elementNameRenaming a map from element names in {@code valueHolder} to element names
+     *     of the annotation being built
      */
     public void copyRenameElementValuesFromAnnotation(
-            AnnotationMirror valueHolder, Map<String, String> mapping) {
+            AnnotationMirror valueHolder, Map<String, String> elementNameRenaming) {
 
         for (Map.Entry<? extends ExecutableElement, ? extends AnnotationValue> eltValToCopy :
                 valueHolder.getElementValues().entrySet()) {
 
             String sourceName = eltValToCopy.getKey().getSimpleName().toString();
-            String targetName = mapping.get(sourceName);
+            String targetName = elementNameRenaming.get(sourceName);
             if (targetName == null) {
                 continue;
             }
