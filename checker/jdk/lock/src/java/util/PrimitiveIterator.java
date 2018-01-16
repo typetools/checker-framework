86be593df61679ea6cd9ff1fff979af3c6308bee
Michael Ernst <mernst@cs.washington.edu>
2018-01-16 12:54:38 -0800

    Add import statement

--- checker/jdk/lock/src/java/util/PrimitiveIterator.java
+++ checker/jdk/lock/src/java/util/PrimitiveIterator.java
@@ -1,5 +1,7 @@
 package java.util;
 
+import org.checkerframework.checker.lock.qual.*;
+
 import java.util.function.Consumer;
 import java.util.function.DoubleConsumer;
 import java.util.function.IntConsumer;
