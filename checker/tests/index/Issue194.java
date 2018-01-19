c8d77d7eeb6c941bd92ea1343b2b0c674734fa0d
Suzanne Millstein <smillst@cs.washington.edu>
2018-01-19 13:34:59 -0800

    Expand support for customn collections.
    Treat a method annotated with @Length("this") the same as String#length.
    
    This fixes kelloggm 194.

---
+++ checker/tests/index/Issue194.java
@@ -0,0 +1,26 @@
+// Test case for kelloggm 194
+// https://github.com/kelloggm/checker-framework/issues/194
+import org.checkerframework.checker.index.qual.IndexFor;
+import org.checkerframework.checker.index.qual.LengthOf;
+
+public class Issue194 {
+    class Custom {
+        public @LengthOf("this") int length() {
+            throw new RuntimeException();
+        }
+
+        public Object get(@IndexFor("this") int i) {
+            return null;
+        }
+    }
+
+    public boolean m(Custom a, Custom b) {
+        if (a.length() != b.length()) {
+            return false;
+        }
+        for (int i = 0; i < a.length(); ++i) {
+            if (a.get(i) != b.get(i)) return false;
+        }
+        return true;
+    }
+}
