abc3025b5e9b77bcd8a92b69efed7aa7c0d44346
Suzanne Millstein <smillst@cs.washington.edu>
2018-01-23 10:26:55 -0800

    Add counts for type casts
    Also, adds more places for generics.

--- framework/src/org/checkerframework/common/util/count/TypeStatistics.java
+++ framework/src/org/checkerframework/common/util/count/TypeStatistics.java
@@ -2,13 +2,18 @@ package org.checkerframework.common.util.count;
 
 import com.sun.source.tree.ArrayAccessTree;
 import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.MethodTree;
 import com.sun.source.tree.NewArrayTree;
+import com.sun.source.tree.NewClassTree;
 import com.sun.source.tree.ParameterizedTypeTree;
+import com.sun.source.tree.TypeCastTree;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import org.checkerframework.framework.source.SourceChecker;
 import org.checkerframework.framework.source.SourceVisitor;
 import org.checkerframework.javacutil.AnnotationProvider;
+import org.checkerframework.javacutil.TreeUtils;
 
 /**
  * An annotation processor for listing the facts about types and array accesses locations of
@@ -23,11 +28,13 @@ public class TypeStatistics extends SourceChecker {
 
     int generics = 0;
     int arrayAccesses = 0;
+    int typecasts = 0;
 
     @Override
     public void typeProcessingOver() {
         System.out.printf("Found %d generic type uses.\n", generics);
         System.out.printf("Found %d array accesses and creations.\n", arrayAccesses);
+        System.out.printf("Found %d typecasts.\n", typecasts);
     }
 
     @Override
@@ -49,18 +56,42 @@ public class TypeStatistics extends SourceChecker {
                 // class entirely.
                 return null;
             }
+            generics += tree.getTypeParameters().size();
             return super.visitClass(tree, p);
         }
 
         @Override
         public Void visitNewArray(NewArrayTree node, Void aVoid) {
             arrayAccesses += node.getDimensions().size();
+
             return super.visitNewArray(node, aVoid);
         }
 
         @Override
+        public Void visitNewClass(NewClassTree node, Void aVoid) {
+            // TODO: diamonds
+            if (TreeUtils.isDiamondTree(node)) {
+                generics++;
+            }
+            generics += node.getTypeArguments().size();
+            return super.visitNewClass(node, aVoid);
+        }
+
+        @Override
+        public Void visitMethodInvocation(MethodInvocationTree node, Void aVoid) {
+            generics += node.getTypeArguments().size();
+            return super.visitMethodInvocation(node, aVoid);
+        }
+
+        @Override
+        public Void visitMethod(MethodTree node, Void aVoid) {
+            generics += node.getTypeParameters().size();
+            return super.visitMethod(node, aVoid);
+        }
+
+        @Override
         public Void visitParameterizedType(ParameterizedTypeTree tree, Void p) {
-            generics++;
+            generics += tree.getTypeArguments().size();
             return super.visitParameterizedType(tree, p);
         }
 
@@ -68,6 +99,12 @@ public class TypeStatistics extends SourceChecker {
         public Void visitArrayAccess(ArrayAccessTree node, Void aVoid) {
             arrayAccesses++;
             return super.visitArrayAccess(node, aVoid);
+        }
+
+        @Override
+        public Void visitTypeCast(TypeCastTree node, Void aVoid) {
+            typecasts++;
+            return super.visitTypeCast(node, aVoid);
         }
     }
 
