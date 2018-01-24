58073619f28060cff0a7704c638289015dc26e48
Suzanne Millstein <smillst@cs.washington.edu>
2018-01-24 08:47:36 -0800

    First pass add LessThan Checker

---
+++ checker/src/org/checkerframework/checker/index/inequality/InequalityChecker.java
@@ -0,0 +1,5 @@
+package org.checkerframework.checker.index.inequality;
+
+// TODO: This is proabaly a generally useful checker and should therefor be in the
+// org.checkerframework.common package, but we don't have time to properly review it for public release.
+public class InequalityChecker {}
