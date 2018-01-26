06c059b808d58eef411254c33e82290008721584
Vlastimil Dort <dort@d3s.mff.cuni.cz>
2018-01-26 22:07:48 +0100

    Test case with Positive

--- checker/tests/index/Modulo.java
+++ checker/tests/index/Modulo.java
@@ -2,10 +2,16 @@
 // https://github.com/kelloggm/checker-framework/issues/218
 import org.checkerframework.checker.index.qual.IndexFor;
 import org.checkerframework.checker.index.qual.IndexOrHigh;
+import org.checkerframework.checker.index.qual.LTEqLengthOf;
 import org.checkerframework.checker.index.qual.NonNegative;
+import org.checkerframework.checker.index.qual.Positive;
 
 public class Modulo {
     void m1(Object[] a, @IndexOrHigh("#1") int i, @NonNegative int j) {
+        @IndexFor("a") int k = j % i;
+    }
+
+    void m1p(Object[] a, @Positive @LTEqLengthOf("#1") int i, @Positive int j) {
         @IndexFor("a") int k = j % i;
     }
 
