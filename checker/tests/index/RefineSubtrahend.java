1a094465d43d8c78526540c260ba6d2b993810ed
Vlastimil Dort <dort@d3s.mff.cuni.cz>
2018-01-28 23:46:14 +0100

    Add test for switch

--- checker/tests/index/RefineSubtrahend.java
+++ checker/tests/index/RefineSubtrahend.java
@@ -37,4 +37,15 @@ public class RefineSubtrahend {
             }
         }
     }
+
+    void cases(int[] a, @NonNegative int l) {
+        switch (a.length - l) {
+            case 1:
+                int x = a[l];
+                break;
+            case 2:
+                int y = a[l + 1];
+                break;
+        }
+    }
 }
