f77b3f9b5ea916f688db86c617495871cb0dd1d5
Werner Dietl <wdietl@gmail.com>
2018-02-08 00:49:37 -0500

    No more messing with `finally` trees.

--- framework/src/org/checkerframework/common/basetype/BaseTypeVisitor.java
+++ framework/src/org/checkerframework/common/basetype/BaseTypeVisitor.java
@@ -24,7 +24,6 @@ import com.sun.source.tree.ParameterizedTypeTree;
 import com.sun.source.tree.ReturnTree;
 import com.sun.source.tree.ThrowTree;
 import com.sun.source.tree.Tree;
-import com.sun.source.tree.TryTree;
 import com.sun.source.tree.TypeCastTree;
 import com.sun.source.tree.TypeParameterTree;
 import com.sun.source.tree.UnaryTree;
@@ -33,10 +32,8 @@ import com.sun.source.util.SourcePositions;
 import com.sun.source.util.TreePath;
 import com.sun.source.util.TreeScanner;
 import com.sun.tools.javac.tree.JCTree;
-import com.sun.tools.javac.tree.JCTree.JCBlock;
 import com.sun.tools.javac.tree.JCTree.JCMemberReference;
 import com.sun.tools.javac.tree.JCTree.JCMemberReference.ReferenceKind;
-import com.sun.tools.javac.tree.JCTree.JCTry;
 import com.sun.tools.javac.tree.TreeInfo;
 import java.lang.SuppressWarnings;
 import java.lang.annotation.Annotation;
@@ -897,23 +894,6 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
                     var, iteratedType, node.getExpression(), "enhancedfor.type.incompatible");
         }
         return super.visitEnhancedForLoop(node, p);
-    }
-
-    @Override
-    public Void visitTry(TryTree node, Void aVoid) {
-        Void ret = super.visitTry(node, aVoid);
-        JCTry tree = (JCTry) node;
-        JCBlock oldFinalizer = tree.finalizer;
-        if (oldFinalizer != null) {
-            // Run type-checking for all generated trees. Set a generated tree to 'tree.finalizer'
-            // to emulate a path for generated tree during type-checking.
-            for (Tree generatedFinalizer : atypeFactory.getGeneratedTrees(oldFinalizer)) {
-                tree.finalizer = (JCBlock) generatedFinalizer;
-                this.scan(generatedFinalizer, aVoid);
-            }
-            tree.finalizer = oldFinalizer;
-        }
-        return ret;
     }
 
     /**
