f7bdbb5a71fe9ccbfe04073dfc20d077495b9ad3
Ashish Rana <ashishrana160796@gmail.com>
2018-02-10 23:34:54 +0530

    Changes made as requested

--- checker/jdk/nullness/src/javax/script/SimpleScriptContext.java
+++ checker/jdk/nullness/src/javax/script/SimpleScriptContext.java
@@ -1,26 +1,26 @@
 /*
  * Copyright (c) 2005, 2013, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
  *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
  */
 
 package javax.script;
@@ -141,7 +141,6 @@ public class SimpleScriptContext  implements ScriptContext {
      * @throws NullPointerException if the name is null.
      * @throws IllegalArgumentException if the name is empty.
      */
-    @SuppressWarnings("nullness")
     public @Nullable Object getAttribute(String name) {
         checkName(name);
         if (engineScope.containsKey(name)) {
@@ -165,7 +164,6 @@ public class SimpleScriptContext  implements ScriptContext {
      *         if the name is empty or if the value of scope is invalid.
      * @throws NullPointerException if the name is null.
      */
-    @SuppressWarnings("nullness")
     public @Nullable Object getAttribute(String name, int scope) {
         checkName(name);
         switch (scope) {
@@ -195,8 +193,8 @@ public class SimpleScriptContext  implements ScriptContext {
      *         if the name is empty or if the scope is invalid.
      * @throws NullPointerException if the name is null.
      */
-    @SuppressWarnings("nullness")
-    public Object removeAttribute(String name, int scope) {
+    @SuppressWarnings("nullness") // getBindings(ENGINE_SCOPE), getBindings(GLOBAL_SCOPE) will not be causing null pointer exception  
+    public @Nullable Object removeAttribute(String name, int scope) {
         checkName(name);
         switch (scope) {
 
@@ -306,8 +304,7 @@ public class SimpleScriptContext  implements ScriptContext {
      * @return The value of either the  <code>engineScope</code> or <code>globalScope</code> field.
      * @throws IllegalArgumentException if the value of scope is invalid.
      */
-    @SuppressWarnings("nullness")
-    public Bindings getBindings(int scope) {
+    public @Nullable Bindings getBindings(int scope) {
         if (scope == ENGINE_SCOPE) {
             return engineScope;
         } else if (scope == GLOBAL_SCOPE) {
