517374e70308d06f51da75025044c2c2e94bb658
Ashish Rana <ashishrana160796@gmail.com>
2018-02-14 19:08:55 +0530

    AbstractProcessor issue suppressed

--- checker/jdk/nullness/src/javax/annotation/processing/FilerException.java
+++ checker/jdk/nullness/src/javax/annotation/processing/FilerException.java
@@ -1,33 +1,33 @@
 /*
  * Copyright (c) 2005, 2012, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
  *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
  *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  */
 
 package javax.annotation.processing;
 
 import java.io.IOException;
 
-import org.checkerframework.checker.nullness.qual.*;
+import org.checkerframework.checker.nullness.qual.Nullable;
 /**
  * Indicates a {@link Filer} detected an attempt to open a file that
  * would violate the guarantees provided by the {@code Filer}.  Those
@@ -47,7 +47,8 @@ public class FilerException extends IOException {
      * @param s the detail message, which should include the name of
      * the file attempting to be opened; may be {@code null}
      */
-    public FilerException(@Nullable String s) {
+    @SuppressWarnings("initialization")    // Main aim is to call super() constructor for IOException class, dereference of a null is not happening in this context.
+    public @Nullable FilerException(String s) {
         super(s);
     }
 }
