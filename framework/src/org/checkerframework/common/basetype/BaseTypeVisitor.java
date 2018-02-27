188e8d24a1273ed37b10838ee8a753fa3edf219e
Suzanne Millstein <smillst@cs.washington.edu>
2018-02-27 12:29:20 -0800

    Fix.

--- framework/src/org/checkerframework/common/basetype/BaseTypeVisitor.java
+++ framework/src/org/checkerframework/common/basetype/BaseTypeVisitor.java
@@ -1993,13 +1993,39 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
 
         // Use an error key only if it's overridden by a checker.
         if (!success) {
-            boolean verbose = shouldPrintVerbose(varType, valueType);
-            String valueTypeString = valueType.toString(verbose);
-            String varTypeString = varType.toString(verbose);
+            FoundRequired pair = FoundRequired.of(valueType, varType);
+            String valueTypeString = pair.found;
+            String varTypeString = pair.required;
             checker.report(Result.failure(errorKey, valueTypeString, varTypeString), valueTree);
         }
     }
 
+    /**
+     * Class that creates string representations of {@link AnnotatedTypeMirror}s which are only
+     * verbose if required to differentiate the two types.
+     */
+    private static class FoundRequired {
+        public final String found;
+        public final String required;
+
+        private FoundRequired(AnnotatedTypeMirror found, AnnotatedTypeMirror required) {
+            if (shouldPrintVerbose(found, required)) {
+                this.found = found.toString(true);
+                this.required = required.toString(true);
+            } else {
+                this.found = found.toString();
+                this.required = required.toString();
+            }
+        }
+
+        /**
+         * Creates string representations of {@link AnnotatedTypeMirror}s which are only verbose if
+         * required to differentiate the two types.
+         */
+        static FoundRequired of(AnnotatedTypeMirror found, AnnotatedTypeMirror required) {
+            return new FoundRequired(found, required);
+        }
+    }
     /**
      * Return whether or not the verbose toString should be used when printing the two annotated
      * types.
@@ -2009,7 +2035,7 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
      * @return true iff there are two annotated types (in either ATM) such that their toStrings are
      *     the same but their verbose toStrings differ
      */
-    private boolean shouldPrintVerbose(AnnotatedTypeMirror atm1, AnnotatedTypeMirror atm2) {
+    private static boolean shouldPrintVerbose(AnnotatedTypeMirror atm1, AnnotatedTypeMirror atm2) {
         String atm1ToString = atm1.toString();
         String atm2ToString = atm2.toString();
         // If both types as strings are the same, use verbose toString.
@@ -2986,9 +3012,8 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
             if (!atypeFactory
                     .getTypeHierarchy()
                     .isSubtype(overriddenReceiver, overrider.getReceiverType().getErased())) {
-                boolean verbose =
-                        shouldPrintVerbose(
-                                overrider.getReceiverType(), overridden.getReceiverType());
+                FoundRequired pair =
+                        FoundRequired.of(overrider.getReceiverType(), overridden.getReceiverType());
                 checker.report(
                         Result.failure(
                                 "override.receiver.invalid",
@@ -2996,8 +3021,8 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
                                 overriderTyp,
                                 overriddenMeth,
                                 overriddenTyp,
-                                overrider.getReceiverType().toString(verbose),
-                                overridden.getReceiverType().toString(verbose)),
+                                pair.found,
+                                pair.required),
                         overriderTree);
                 return false;
             }
@@ -3077,8 +3102,8 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
                         overriddenParams.get(index).toString());
             }
             if (!success) {
-                boolean verbose =
-                        shouldPrintVerbose(overriderParams.get(index), overriddenParams.get(index));
+                FoundRequired pair =
+                        FoundRequired.of(overriderParams.get(index), overriddenParams.get(index));
                 checker.report(
                         Result.failure(
                                 msgKey,
@@ -3086,8 +3111,8 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
                                 overriderTyp,
                                 overriddenMeth,
                                 overriddenTyp,
-                                overriderParams.get(index).toString(verbose),
-                                overriddenParams.get(index).toString(verbose)),
+                                pair.found,
+                                pair.required),
                         posTree);
             }
         }
@@ -3177,7 +3202,7 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
                         overridden.getReturnType().toString());
             }
             if (!success) {
-                boolean verbose = shouldPrintVerbose(overridingReturnType, overriddenReturnType);
+                FoundRequired pair = FoundRequired.of(overridingReturnType, overriddenReturnType);
                 checker.report(
                         Result.failure(
                                 msgKey,
@@ -3185,8 +3210,8 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
                                 overriderTyp,
                                 overriddenMeth,
                                 overriddenTyp,
-                                overridingReturnType.toString(verbose),
-                                overriddenReturnType.toString(verbose)),
+                                pair.found,
+                                pair.required),
                         posTree);
             }
         }
