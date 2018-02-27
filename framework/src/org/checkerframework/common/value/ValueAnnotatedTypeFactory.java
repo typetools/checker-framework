170d278f306526c52e5a1317c8050d1e6b8925d0
Werner Dietl <wdietl@gmail.com>
2018-02-27 00:27:56 -0500

    Fix typo in comment.

--- framework/src/org/checkerframework/common/value/ValueAnnotatedTypeFactory.java
+++ framework/src/org/checkerframework/common/value/ValueAnnotatedTypeFactory.java
@@ -239,7 +239,7 @@ public class ValueAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
 
     @Override
     protected TypeHierarchy createTypeHierarchy() {
-        // This is a lot of code to replace annotations so that annotations that are equivalent qualifers
+        // This is a lot of code to replace annotations so that annotations that are equivalent qualifiers
         // are the same annotation.
         return new DefaultTypeHierarchy(
                 checker,
