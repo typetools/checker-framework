00e87d42f80436ab1a40571e882718cd524f9d93
Werner Dietl <wdietl@gmail.com>
2018-02-27 00:26:53 -0500

    Suppress a different warning.

--- framework/tests/all-systems/MissingBoundAnnotations.java
+++ framework/tests/all-systems/MissingBoundAnnotations.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 import java.util.Map;
 
 public final class MissingBoundAnnotations {
-    @SuppressWarnings("nullness:type.argument.type.incompatible")
+    @SuppressWarnings("keyfor:argument.type.incompatible")
     public static <K extends Comparable<? super K>, V> Collection<K> sortedKeySet(Map<K, V> m) {
         ArrayList<K> theKeys = new ArrayList<K>(m.keySet());
         Collections.sort(theKeys);
