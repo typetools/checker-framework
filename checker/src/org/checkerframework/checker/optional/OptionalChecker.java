817d3d5e73c6c2aade6ce9bcb9a7c93bcb14d24d
Michael Ernst <mernst@cs.washington.edu>
2018-02-27 16:06:30 -0800

    More code review feedback.

--- checker/src/org/checkerframework/checker/optional/OptionalChecker.java
+++ checker/src/org/checkerframework/checker/optional/OptionalChecker.java
@@ -7,9 +7,7 @@ import org.checkerframework.common.basetype.BaseTypeChecker;
  *
  * @checker_framework.manual #optional-checker Optional Checker
  */
-/* TODO:
- * For a call to ofNullable, if the argument has type @NonNull, make the return type have type @Present.
- * Make Optional Checker a subchecker of the Nullness Checker, or vice versa.
- */
-// Uncomment when the Checker Framework assumes Java 8: @RelevantJavaTypes(Optional.class)
+// TODO: For a call to ofNullable, if the argument has type @NonNull, make the return type have type
+// @Present.  Make Optional Checker a subchecker of the Nullness Checker.
+@RelevantJavaTypes(Optional.class)
 public class OptionalChecker extends BaseTypeChecker {}
