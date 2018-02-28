794b0b5f13e03826f50cc0deba1ad39e060158ab
Werner Dietl <wdietl@gmail.com>
2018-02-27 22:29:34 -0500

    Include necessary imports.

--- checker/jdk/optional/src/java/util/Optional.java
+++ checker/jdk/optional/src/java/util/Optional.java
@@ -24,12 +24,10 @@
  */
 package java.util;
 
-/*
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
-*/
 
 import org.checkerframework.checker.optional.qual.Present;
 import org.checkerframework.framework.qual.EnsuresQualifierIf;
