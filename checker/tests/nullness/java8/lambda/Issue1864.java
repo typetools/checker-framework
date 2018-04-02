3dd5824457d8c13d6c34a0aa8d04b2d6d6b32404
Werner Dietl <wdietl@gmail.com>
2018-04-02 10:11:40 -0400

    Fix #1864 and #1897 by passing through LambdaResultExpressionNodes. (#1925)

---
+++ checker/tests/nullness/java8/lambda/Issue1864.java
@@ -0,0 +1,18 @@
+// Test case for Issue 1864:
+// https://github.com/typetools/checker-framework/issues/1864
+
+import java.util.List;
+import java.util.function.Supplier;
+
+abstract class Issue1864 {
+    interface A {}
+
+    abstract <T extends A> List<T> g();
+
+    abstract void h(Supplier<Iterable<A>> s);
+
+    void f() {
+        Iterable<A> xs = g();
+        h(() -> xs);
+    }
+}
