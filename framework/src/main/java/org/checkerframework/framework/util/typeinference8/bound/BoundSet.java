4856d04b808c0bada3540968e2ef37a1e0072bfe
Suzanne Millstein <smillst@cs.washington.edu>
2018-04-13 11:29:07 -0700

    Fix.

--- framework/src/main/java/org/checkerframework/framework/util/typeinference8/bound/BoundSet.java
+++ framework/src/main/java/org/checkerframework/framework/util/typeinference8/bound/BoundSet.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
-import org.checkerframework.framework.util.PluginUtil;
 import org.checkerframework.framework.util.typeinference8.Resolution;
 import org.checkerframework.framework.util.typeinference8.constraint.Constraint;
 import org.checkerframework.framework.util.typeinference8.constraint.ReductionResult;
@@ -15,6 +14,7 @@ import org.checkerframework.framework.util.typeinference8.types.Dependencies;
 import org.checkerframework.framework.util.typeinference8.types.Theta;
 import org.checkerframework.framework.util.typeinference8.types.Variable;
 import org.checkerframework.framework.util.typeinference8.util.Java8InferenceContext;
+import org.checkerframework.javacutil.PluginUtil;
 import org.checkerframework.javacutil.TypesUtils;
 
 /**
