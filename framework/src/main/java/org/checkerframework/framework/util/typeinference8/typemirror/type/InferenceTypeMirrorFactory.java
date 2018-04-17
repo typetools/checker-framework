a1c53f7423efed7a9aacbce9a68927ad547f2d14
Suzanne Millstein <smillst@cs.washington.edu>
2018-04-17 14:55:55 -0700

    Move methods around.

--- framework/src/main/java/org/checkerframework/framework/util/typeinference8/typemirror/type/InferenceTypeMirrorFactory.java
+++ framework/src/main/java/org/checkerframework/framework/util/typeinference8/typemirror/type/InferenceTypeMirrorFactory.java
@@ -1,20 +1,35 @@
 package org.checkerframework.framework.util.typeinference8.typemirror.type;
 
+import com.sun.source.tree.AssignmentTree;
+import com.sun.source.tree.CompoundAssignmentTree;
+import com.sun.source.tree.ConditionalExpressionTree;
 import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.LambdaExpressionTree;
 import com.sun.source.tree.MemberReferenceTree;
+import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.NewArrayTree;
 import com.sun.source.tree.NewClassTree;
 import com.sun.source.tree.Tree;
 import com.sun.source.tree.VariableTree;
+import com.sun.source.util.TreePath;
+import com.sun.tools.javac.code.Symbol;
 import com.sun.tools.javac.code.Type;
+import com.sun.tools.javac.code.Types;
+import com.sun.tools.javac.processing.JavacProcessingEnvironment;
+import com.sun.tools.javac.tree.JCTree.JCNewClass;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.TypeParameterElement;
+import javax.lang.model.type.ArrayType;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.ExecutableType;
 import javax.lang.model.type.TypeKind;
@@ -32,13 +47,12 @@ import org.checkerframework.framework.util.typeinference8.types.ProperType;
 import org.checkerframework.framework.util.typeinference8.types.Theta;
 import org.checkerframework.framework.util.typeinference8.types.Variable;
 import org.checkerframework.framework.util.typeinference8.util.CheckedExceptionsUtil;
-import org.checkerframework.framework.util.typeinference8.util.InferenceUtils;
-import org.checkerframework.framework.util.typeinference8.util.InternalInferenceUtils;
 import org.checkerframework.framework.util.typeinference8.util.Java8InferenceContext;
 import org.checkerframework.javacutil.ElementUtils;
 import org.checkerframework.javacutil.ErrorReporter;
 import org.checkerframework.javacutil.Pair;
 import org.checkerframework.javacutil.TreeUtils;
+import org.checkerframework.javacutil.TypeAnnotationUtils;
 import org.checkerframework.javacutil.TypesUtils;
 
 public class InferenceTypeMirrorFactory implements InferenceFactory {
@@ -126,15 +140,173 @@ public class InferenceTypeMirrorFactory implements InferenceFactory {
     @Override
     public InvocationType getTypeOfMethodAdaptedToUse(ExpressionTree invocation) {
         return new InvocationTypeMirror(
-                InternalInferenceUtils.getTypeOfMethodAdaptedToUse(invocation, context),
-                invocation,
-                context);
+                getTypeOfMethodAdaptedToUse(invocation, context), invocation, context);
+    }
+    /**
+     * Returns the type that the leaf of path is assigned to, if it is within an assignment context.
+     * Returns the type that the method invocation at the leaf is assigned to. If the result is a
+     * primitive, return the boxed version.
+     *
+     * @return type that path leaf is assigned to
+     */
+    public static TypeMirror getTargetType(TreePath path, Java8InferenceContext context) {
+        Tree assignmentContext = TreeUtils.getAssignmentContext(path);
+        if (assignmentContext == null) {
+            return null;
+        }
+
+        switch (assignmentContext.getKind()) {
+            case ASSIGNMENT:
+                ExpressionTree variable = ((AssignmentTree) assignmentContext).getVariable();
+                return TreeUtils.typeOf(variable);
+            case VARIABLE:
+                VariableTree variableTree = (VariableTree) assignmentContext;
+                return TreeUtils.typeOf(variableTree.getType());
+            case METHOD_INVOCATION:
+                MethodInvocationTree methodInvocation = (MethodInvocationTree) assignmentContext;
+                return assignedToExecutable(
+                        path, methodInvocation, methodInvocation.getArguments(), context);
+            case NEW_CLASS:
+                NewClassTree newClassTree = (NewClassTree) assignmentContext;
+                return assignedToExecutable(
+                        path, newClassTree, newClassTree.getArguments(), context);
+            case NEW_ARRAY:
+                NewArrayTree newArrayTree = (NewArrayTree) assignmentContext;
+                ArrayType arrayType = (ArrayType) TreeUtils.typeOf(newArrayTree);
+                return arrayType.getComponentType();
+            case RETURN:
+                HashSet<Tree.Kind> kinds =
+                        new HashSet<>(Arrays.asList(Tree.Kind.LAMBDA_EXPRESSION, Tree.Kind.METHOD));
+                Tree enclosing = TreeUtils.enclosingOfKind(path, kinds);
+                if (enclosing.getKind() == Tree.Kind.METHOD) {
+                    MethodTree methodTree = (MethodTree) enclosing;
+                    return TreeUtils.typeOf(methodTree.getReturnType());
+                } else {
+                    // TODO: I don't think this should happen. during inference
+                    LambdaExpressionTree lambdaTree = (LambdaExpressionTree) enclosing;
+                    return TreeUtils.typeOf(lambdaTree);
+                }
+            default:
+                if (assignmentContext
+                        .getKind()
+                        .asInterface()
+                        .equals(CompoundAssignmentTree.class)) {
+                    // 11 Tree kinds are compound assignments, so don't use it in the switch
+                    ExpressionTree var = ((CompoundAssignmentTree) assignmentContext).getVariable();
+                    return TreeUtils.typeOf(var);
+                } else {
+                    ErrorReporter.errorAbort(
+                            "Unexpected assignment context.\nKind: %s\nTree: %s",
+                            assignmentContext.getKind(), assignmentContext);
+                    return null;
+                }
+        }
+    }
+
+    private static TypeMirror assignedToExecutable(
+            TreePath path,
+            ExpressionTree methodInvocation,
+            List<? extends ExpressionTree> arguments,
+            Java8InferenceContext context) {
+        int treeIndex = -1;
+        for (int i = 0; i < arguments.size(); ++i) {
+            ExpressionTree argumentTree = arguments.get(i);
+            if (isArgument(path, argumentTree)) {
+                treeIndex = i;
+                break;
+            }
+        }
+
+        ExecutableType methodType = getTypeOfMethodAdaptedToUse(methodInvocation, context);
+        if (treeIndex >= methodType.getParameterTypes().size() - 1
+                && TreeUtils.isVarArgMethodCall(methodInvocation)) {
+            treeIndex = methodType.getParameterTypes().size() - 1;
+            TypeMirror typeMirror = methodType.getParameterTypes().get(treeIndex);
+            return ((ArrayType) typeMirror).getComponentType();
+        }
+
+        return methodType.getParameterTypes().get(treeIndex);
+    }
+
+    /**
+     * Returns whether argumentTree is the tree at the leaf of path. if tree is a conditional
+     * expression, isArgument is called recursively on the true and false expressions.
+     */
+    private static boolean isArgument(TreePath path, ExpressionTree argumentTree) {
+        argumentTree = TreeUtils.skipParens(argumentTree);
+        if (argumentTree == path.getLeaf()) {
+            return true;
+        } else if (argumentTree.getKind() == Tree.Kind.CONDITIONAL_EXPRESSION) {
+            ConditionalExpressionTree conditionalExpressionTree =
+                    (ConditionalExpressionTree) argumentTree;
+            return isArgument(path, conditionalExpressionTree.getTrueExpression())
+                    || isArgument(path, conditionalExpressionTree.getFalseExpression());
+        }
+        return false;
+    }
+
+    private static DeclaredType getReceiverType(ExpressionTree tree) {
+        Tree receiverTree;
+        if (tree.getKind() == Tree.Kind.NEW_CLASS) {
+            receiverTree = ((NewClassTree) tree).getEnclosingExpression();
+        } else {
+            receiverTree = TreeUtils.getReceiverTree(tree);
+        }
+
+        if (receiverTree == null) {
+            return null;
+        }
+        TypeMirror type = TreeUtils.typeOf(receiverTree);
+        if (type.getKind() == TypeKind.TYPEVAR) {
+            return (DeclaredType) ((TypeVariable) type).getUpperBound();
+        }
+        return type.getKind() == TypeKind.DECLARED ? (DeclaredType) type : null;
+    }
+
+    /**
+     * @return ExecutableType of the method invocation or new class tree adapted to the call site.
+     */
+    public static ExecutableType getTypeOfMethodAdaptedToUse(
+            ExpressionTree expressionTree, Java8InferenceContext context) {
+        if (expressionTree.getKind() == Tree.Kind.NEW_CLASS) {
+            if (!TreeUtils.isDiamondTree(expressionTree)) {
+                return (ExecutableType) ((JCNewClass) expressionTree).constructorType;
+            }
+        } else if (expressionTree.getKind() != Tree.Kind.METHOD_INVOCATION) {
+            return null;
+        }
+        ExecutableElement ele = (ExecutableElement) TreeUtils.elementFromUse(expressionTree);
+
+        if (ElementUtils.isStatic(ele)) {
+            return (ExecutableType) ele.asType();
+        }
+        DeclaredType receiverType = getReceiverType(expressionTree);
+
+        if (receiverType == null) {
+            receiverType = context.enclosingType;
+        }
+
+        while (context.types.asSuper((Type) receiverType, (Symbol) ele.getEnclosingElement())
+                == null) {
+            TypeMirror enclosing = receiverType.getEnclosingType();
+            if (enclosing == null || enclosing.getKind() != TypeKind.DECLARED) {
+                if (expressionTree.getKind() == Tree.Kind.NEW_CLASS) {
+                    // No receiver for the constructor.
+                    return (ExecutableType) ele.asType();
+                } else {
+                    ErrorReporter.errorAbort("Method not found");
+                }
+            }
+            receiverType = (DeclaredType) enclosing;
+        }
+        javax.lang.model.util.Types types = context.env.getTypeUtils();
+        return (ExecutableType) types.asMemberOf(receiverType, ele);
     }
 
     @Override
     public ProperType getTargetType() {
         ProperType targetType = null;
-        TypeMirror assignmentType = InferenceUtils.getTargetType(context.pathToExpression, context);
+        TypeMirror assignmentType = getTargetType(context.pathToExpression, context);
 
         if (assignmentType != null) {
             targetType = new ProperTypeMirror(assignmentType, context);
@@ -172,7 +344,7 @@ public class InferenceTypeMirrorFactory implements InferenceFactory {
         TypeMirror aTypeMirror = a.getJavaType();
         TypeMirror bTypeMirror = b.getJavaType();
         // com.sun.tools.javac.comp.Infer#getParameterizedSupers
-        TypeMirror lubResult = InternalInferenceUtils.lub(context.env, aTypeMirror, bTypeMirror);
+        TypeMirror lubResult = lub(context.env, aTypeMirror, bTypeMirror);
         if (!TypesUtils.isParameterizedType(lubResult)) {
             return null;
         }
@@ -225,7 +397,7 @@ public class InferenceTypeMirrorFactory implements InferenceFactory {
             if (ti == null) {
                 ti = li;
             } else {
-                ti = InternalInferenceUtils.lub(context.env, ti, li);
+                ti = lub(context.env, ti, li);
             }
         }
         return new ProperTypeMirror(ti, context);
@@ -361,5 +533,25 @@ public class InferenceTypeMirrorFactory implements InferenceFactory {
             subsTypeArg.add(new ProperTypeMirror(subs, context));
         }
         return subsTypeArg;
+    }
+
+    public static TypeMirror lub(
+            ProcessingEnvironment processingEnv, TypeMirror tm1, TypeMirror tm2) {
+        Type t1 = TypeAnnotationUtils.unannotatedType(tm1);
+        Type t2 = TypeAnnotationUtils.unannotatedType(tm2);
+        JavacProcessingEnvironment javacEnv = (JavacProcessingEnvironment) processingEnv;
+        Types types = Types.instance(javacEnv.getContext());
+
+        return types.lub(t1, t2);
+    }
+
+    public static TypeMirror glb(
+            ProcessingEnvironment processingEnv, TypeMirror tm1, TypeMirror tm2) {
+        Type t1 = TypeAnnotationUtils.unannotatedType(tm1);
+        Type t2 = TypeAnnotationUtils.unannotatedType(tm2);
+        JavacProcessingEnvironment javacEnv = (JavacProcessingEnvironment) processingEnv;
+        Types types = Types.instance(javacEnv.getContext());
+
+        return types.glb(t1, t2);
     }
 }
--- framework/src/main/java/org/checkerframework/framework/util/typeinference8/util/InferenceUtils.java
+++ framework/src/main/java/org/checkerframework/framework/util/typeinference8/util/InferenceUtils.java
@@ -1,24 +1,10 @@
 package org.checkerframework.framework.util.typeinference8.util;
 
-import com.sun.source.tree.AssignmentTree;
-import com.sun.source.tree.CompoundAssignmentTree;
-import com.sun.source.tree.ConditionalExpressionTree;
 import com.sun.source.tree.ExpressionTree;
-import com.sun.source.tree.LambdaExpressionTree;
-import com.sun.source.tree.MethodInvocationTree;
-import com.sun.source.tree.MethodTree;
-import com.sun.source.tree.NewArrayTree;
-import com.sun.source.tree.NewClassTree;
-import com.sun.source.tree.Tree;
-import com.sun.source.tree.Tree.Kind;
-import com.sun.source.tree.VariableTree;
-import com.sun.source.util.TreePath;
 import com.sun.tools.javac.code.Type;
 import com.sun.tools.javac.code.Types;
 import com.sun.tools.javac.processing.JavacProcessingEnvironment;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.processing.ProcessingEnvironment;
@@ -36,114 +22,9 @@ import javax.lang.model.type.TypeVariable;
 import javax.lang.model.type.TypeVisitor;
 import javax.lang.model.type.UnionType;
 import javax.lang.model.type.WildcardType;
-import org.checkerframework.javacutil.ErrorReporter;
 import org.checkerframework.javacutil.TreeUtils;
 
 public class InferenceUtils {
-
-    /**
-     * Returns the type that the leaf of path is assigned to, if it is within an assignment context.
-     * Returns the type that the method invocation at the leaf is assigned to. If the result is a
-     * primitive, return the boxed version.
-     *
-     * @return type that path leaf is assigned to
-     */
-    public static TypeMirror getTargetType(TreePath path, Java8InferenceContext context) {
-        Tree assignmentContext = TreeUtils.getAssignmentContext(path);
-        if (assignmentContext == null) {
-            return null;
-        }
-
-        switch (assignmentContext.getKind()) {
-            case ASSIGNMENT:
-                ExpressionTree variable = ((AssignmentTree) assignmentContext).getVariable();
-                return TreeUtils.typeOf(variable);
-            case VARIABLE:
-                VariableTree variableTree = (VariableTree) assignmentContext;
-                return TreeUtils.typeOf(variableTree.getType());
-            case METHOD_INVOCATION:
-                MethodInvocationTree methodInvocation = (MethodInvocationTree) assignmentContext;
-                return assignedToExecutable(
-                        path, methodInvocation, methodInvocation.getArguments(), context);
-            case NEW_CLASS:
-                NewClassTree newClassTree = (NewClassTree) assignmentContext;
-                return assignedToExecutable(
-                        path, newClassTree, newClassTree.getArguments(), context);
-            case NEW_ARRAY:
-                NewArrayTree newArrayTree = (NewArrayTree) assignmentContext;
-                ArrayType arrayType = (ArrayType) TreeUtils.typeOf(newArrayTree);
-                return arrayType.getComponentType();
-            case RETURN:
-                HashSet<Kind> kinds =
-                        new HashSet<>(Arrays.asList(Kind.LAMBDA_EXPRESSION, Kind.METHOD));
-                Tree enclosing = TreeUtils.enclosingOfKind(path, kinds);
-                if (enclosing.getKind() == Kind.METHOD) {
-                    MethodTree methodTree = (MethodTree) enclosing;
-                    return TreeUtils.typeOf(methodTree.getReturnType());
-                } else {
-                    // TODO: I don't think this should happen. during inference
-                    LambdaExpressionTree lambdaTree = (LambdaExpressionTree) enclosing;
-                    return TreeUtils.typeOf(lambdaTree);
-                }
-            default:
-                if (assignmentContext
-                        .getKind()
-                        .asInterface()
-                        .equals(CompoundAssignmentTree.class)) {
-                    // 11 Tree kinds are compound assignments, so don't use it in the switch
-                    ExpressionTree var = ((CompoundAssignmentTree) assignmentContext).getVariable();
-                    return TreeUtils.typeOf(var);
-                } else {
-                    ErrorReporter.errorAbort(
-                            "Unexpected assignment context.\nKind: %s\nTree: %s",
-                            assignmentContext.getKind(), assignmentContext);
-                    return null;
-                }
-        }
-    }
-
-    private static TypeMirror assignedToExecutable(
-            TreePath path,
-            ExpressionTree methodInvocation,
-            List<? extends ExpressionTree> arguments,
-            Java8InferenceContext context) {
-        int treeIndex = -1;
-        for (int i = 0; i < arguments.size(); ++i) {
-            ExpressionTree argumentTree = arguments.get(i);
-            if (isArgument(path, argumentTree)) {
-                treeIndex = i;
-                break;
-            }
-        }
-
-        ExecutableType methodType =
-                InternalInferenceUtils.getTypeOfMethodAdaptedToUse(methodInvocation, context);
-        if (treeIndex >= methodType.getParameterTypes().size() - 1
-                && TreeUtils.isVarArgMethodCall(methodInvocation)) {
-            treeIndex = methodType.getParameterTypes().size() - 1;
-            TypeMirror typeMirror = methodType.getParameterTypes().get(treeIndex);
-            return ((ArrayType) typeMirror).getComponentType();
-        }
-
-        return methodType.getParameterTypes().get(treeIndex);
-    }
-
-    /**
-     * Returns whether argumentTree is the tree at the leaf of path. if tree is a conditional
-     * expression, isArgument is called recursively on the true and false expressions.
-     */
-    private static boolean isArgument(TreePath path, ExpressionTree argumentTree) {
-        argumentTree = TreeUtils.skipParens(argumentTree);
-        if (argumentTree == path.getLeaf()) {
-            return true;
-        } else if (argumentTree.getKind() == Kind.CONDITIONAL_EXPRESSION) {
-            ConditionalExpressionTree conditionalExpressionTree =
-                    (ConditionalExpressionTree) argumentTree;
-            return isArgument(path, conditionalExpressionTree.getTrueExpression())
-                    || isArgument(path, conditionalExpressionTree.getFalseExpression());
-        }
-        return false;
-    }
 
     /**
      * Returns a mapping of type variable to type argument computed using the type of {@code
