512e9856942e148c68e5b707b0e6e58bf398f40b
Suzanne Millstein <smillst@cs.washington.edu>
2018-04-18 13:29:13 -0700

    Add CaptureVariableAnnotatedType

---
+++ framework/src/main/java/org/checkerframework/framework/util/typeinference8/CaptureVariableAnnotatedType.java
@@ -0,0 +1,36 @@
+package org.checkerframework.framework.util.typeinference8;
+
+import com.sun.source.tree.ExpressionTree;
+import org.checkerframework.framework.type.AnnotatedTypeMirror.AnnotatedTypeVariable;
+import org.checkerframework.javacutil.typeinference8.constraint.ConstraintSet;
+import org.checkerframework.javacutil.typeinference8.types.AbstractType;
+import org.checkerframework.javacutil.typeinference8.types.CaptureVariable;
+import org.checkerframework.javacutil.typeinference8.util.Java8InferenceContext;
+
+public class CaptureVariableAnnotatedType extends VariableAnnotatedType implements CaptureVariable {
+
+    CaptureVariableAnnotatedType(
+            AnnotatedTypeVariable type, ExpressionTree invocation, Java8InferenceContext context) {
+        super(type, invocation, context, context.getNextCaptureVariableId());
+    }
+
+    @Override
+    public String toString() {
+        // Use "b" instead of "a" like super so it is apparent that this is a capture variable.
+        if (variableBounds.hasInstantiation()) {
+            return "b" + id + " := " + variableBounds.getInstantiation();
+        }
+        return "b" + id;
+    }
+
+    /** These are constraints generated when incorporating a capture bound. See JLS 18.3.2. */
+    @Override
+    public ConstraintSet getWildcardConstraints(AbstractType Ai, AbstractType Bi) {
+        return variableBounds.getWildcardConstraints(Ai, Bi);
+    }
+
+    @Override
+    public boolean isCaptureVariable() {
+        return true;
+    }
+}
