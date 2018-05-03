61e6f5dcea042eee04f7b3683c177d976cff73fd
Suzanne Millstein <smillst@cs.washington.edu>
2018-05-03 13:26:55 -0700

    Use unique type var names.

--- framework/tests/all-systems/java8inference/MemRefInfere.java
+++ framework/tests/all-systems/java8inference/MemRefInfere.java
@@ -9,14 +9,14 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public abstract class MemRefInfere<K, V> implements Map<K, V>, Serializable {
-    public static <K, V> MemRefInfere<K, V> copyOf(Map<? extends K, ? extends V> map) {
+    public static <K1, V1> MemRefInfere<K1, V1> copyOf(Map<? extends K1, ? extends V1> map) {
         throw new RuntimeException();
     }
 
-    public static <T, K, V> Collector<T, ?, MemRefInfere<K, V>> toImmutableMap(
-            Function<? super T, ? extends K> keyFunction,
-            Function<? super T, ? extends V> valueFunction,
-            BinaryOperator<V> mergeFunction) {
+    public static <T, K2, V2> Collector<T, ?, MemRefInfere<K2, V2>> toImmutableMap(
+            Function<? super T, ? extends K2> keyFunction,
+            Function<? super T, ? extends V2> valueFunction,
+            BinaryOperator<V2> mergeFunction) {
 
         return Collectors.collectingAndThen(
                 Collectors.toMap(keyFunction, valueFunction, mergeFunction, LinkedHashMap::new),
