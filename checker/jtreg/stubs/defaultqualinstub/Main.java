a6bbc589a853488e328c68908dfc7d84ac34db90
Werner Dietl <wdietl@gmail.com>
2018-05-21 22:07:20 -0400

    Fix jtreg header.

--- checker/jtreg/stubs/defaultqualinstub/Main.java
+++ checker/jtreg/stubs/defaultqualinstub/Main.java
@@ -1,6 +1,6 @@
 /*
  * @test
- * @summary Test that the @DefaultQualifier in an astub works.
+ * @summary Test that the DefaultQualifier in an astub works.
  * @library .
  * @compile -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker -Astubs=defaults.astub pck/Defaults.java -AstubWarnIfNotFound -Werror
  */
