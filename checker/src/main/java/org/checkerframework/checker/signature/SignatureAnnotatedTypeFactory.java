efdb21a2a183b3c73a82d8842dad82c3a3216542
Sagar Tewari <sagartewariym@yahoo.com>
2018-05-29 10:19:34 +0530

    Fixed imports, Refactored code, added documentation

--- checker/src/main/java/org/checkerframework/checker/signature/SignatureAnnotatedTypeFactory.java
+++ checker/src/main/java/org/checkerframework/checker/signature/SignatureAnnotatedTypeFactory.java
@@ -10,7 +10,14 @@ import java.lang.annotation.Annotation;
 import java.util.Set;
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.ExecutableElement;
-import org.checkerframework.checker.signature.qual.*;
+import org.checkerframework.checker.signature.qual.BinaryName;
+import org.checkerframework.checker.signature.qual.BinaryNameForNonArray;
+import org.checkerframework.checker.signature.qual.ClassGetName;
+import org.checkerframework.checker.signature.qual.FullyQualifiedName;
+import org.checkerframework.checker.signature.qual.InternalForm;
+import org.checkerframework.checker.signature.qual.InternalFormForNonArray;
+import org.checkerframework.checker.signature.qual.SignatureBottom;
+import org.checkerframework.checker.signature.qual.SignatureUnknown;
 import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.framework.type.AnnotatedTypeFactory;
@@ -112,52 +119,63 @@ public class SignatureAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
          */
         @Override
         public Void visitMethodInvocation(MethodInvocationTree tree, AnnotatedTypeMirror type) {
-            char c1 = ' ';
-            char c2 = ' ';
-            if (TreeUtils.isMethodInvocation(tree, replaceCharChar, processingEnv)) {
-                ExpressionTree arg0 = tree.getArguments().get(0);
-                ExpressionTree arg1 = tree.getArguments().get(1);
-                if (arg0.getKind() == Tree.Kind.CHAR_LITERAL
-                        && arg1.getKind() == Tree.Kind.CHAR_LITERAL) {
-                    c1 = (char) ((LiteralTree) arg0).getValue();
-                    c2 = (char) ((LiteralTree) arg1).getValue();
-                }
-            } else if (TreeUtils.isMethodInvocation(
-                    tree, replaceCharSequenceCharSequence, processingEnv)) {
-                ExpressionTree arg0 = tree.getArguments().get(0);
-                ExpressionTree arg1 = tree.getArguments().get(1);
-                if (arg0.getKind() == Tree.Kind.STRING_LITERAL
-                        && arg1.getKind() == Tree.Kind.STRING_LITERAL) {
-                    String const0 = (String) ((LiteralTree) arg0).getValue();
-                    String const1 = (String) ((LiteralTree) arg1).getValue();
-                    if (const0.length() == 1 && const1.length() == 1) {
-                        c1 = const0.charAt(0);
-                        c2 = const1.charAt(0);
+            if (TreeUtils.isMethodInvocation(tree, replaceCharChar, processingEnv)
+                    || TreeUtils.isMethodInvocation(
+                            tree, replaceCharSequenceCharSequence, processingEnv)) {
+                char c1 = ' ';
+                char c2 = ' ';
+                if (TreeUtils.isMethodInvocation(tree, replaceCharChar, processingEnv)) {
+                    ExpressionTree arg0 = tree.getArguments().get(0);
+                    ExpressionTree arg1 = tree.getArguments().get(1);
+                    if (arg0.getKind() == Tree.Kind.CHAR_LITERAL
+                            && arg1.getKind() == Tree.Kind.CHAR_LITERAL) {
+                        c1 = (char) ((LiteralTree) arg0).getValue();
+                        c2 = (char) ((LiteralTree) arg1).getValue();
+                    }
+                } else {
+                    ExpressionTree arg0 = tree.getArguments().get(0);
+                    ExpressionTree arg1 = tree.getArguments().get(1);
+                    if (arg0.getKind() == Tree.Kind.STRING_LITERAL
+                            && arg1.getKind() == Tree.Kind.STRING_LITERAL) {
+                        String const0 = (String) ((LiteralTree) arg0).getValue();
+                        String const1 = (String) ((LiteralTree) arg1).getValue();
+                        if (const0.length() == 1 && const1.length() == 1) {
+                            c1 = const0.charAt(0);
+                            c2 = const1.charAt(0);
+                        }
                     }
                 }
-            } else {
-                return super.visitMethodInvocation(tree, type);
-            }
-            ExpressionTree receiver = TreeUtils.getReceiverTree(tree);
-            final AnnotatedTypeMirror receiverType = getAnnotatedType(receiver);
-            if (receiverType.getAnnotation(BinaryName.class) != null) {
-                handleReplaceOnBinaryName(c1, c2, type);
-            } else if (receiverType.getAnnotation(InternalForm.class) != null) {
-                handleReplaceOnInternalForm(c1, c2, type);
-            } else if (receiverType.getAnnotation(InternalFormForNonArray.class) != null) {
-                handleReplaceOnInternalFormForNonArray(c1, c2, type);
-            } else if (receiverType.getAnnotation(BinaryNameForNonArray.class) != null) {
-                handleReplaceOnBinaryNameForNonArray(c1, c2, type);
+                if (!(c1 == ' ' || c2 == ' ')) {
+                    ExpressionTree receiver = TreeUtils.getReceiverTree(tree);
+                    final AnnotatedTypeMirror receiverType = getAnnotatedType(receiver);
+                    if (receiverType.getAnnotation(BinaryName.class) != null) {
+                        handleReplaceOnBinaryName(c1, c2, type);
+                    } else if (receiverType.getAnnotation(InternalForm.class) != null) {
+                        handleReplaceOnInternalForm(c1, c2, type);
+                    } else if (receiverType.getAnnotation(InternalFormForNonArray.class) != null) {
+                        handleReplaceOnInternalFormForNonArray(c1, c2, type);
+                    } else if (receiverType.getAnnotation(BinaryNameForNonArray.class) != null) {
+                        handleReplaceOnBinaryNameForNonArray(c1, c2, type);
+                    }
+                }
             }
             return super.visitMethodInvocation(tree, type);
         }
 
+        /**
+         * Handles String.replace calls when the receiver is a {@link
+         * org.checkerframework.checker.signature.qual.BinaryName}
+         */
         private void handleReplaceOnBinaryName(char c1, char c2, AnnotatedTypeMirror type) {
             if (c1 == '.' && c2 == '/') {
                 type.replaceAnnotation(INTERNAL_FORM);
             }
         }
 
+        /**
+         * Handles String.replace calls when the receiver is a {@link
+         * org.checkerframework.checker.signature.qual.BinaryNameForNonArray}
+         */
         private void handleReplaceOnBinaryNameForNonArray(
                 char c1, char c2, AnnotatedTypeMirror type) {
             if (c1 == '.' && c2 == '/') {
@@ -165,6 +183,10 @@ public class SignatureAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
             }
         }
 
+        /**
+         * Handles String.replace calls when the receiver is a {@link
+         * org.checkerframework.checker.signature.qual.InternalFormForNonArray}
+         */
         private void handleReplaceOnInternalFormForNonArray(
                 char c1, char c2, AnnotatedTypeMirror type) {
             if (c1 == '/' && c2 == '.') {
@@ -172,6 +194,10 @@ public class SignatureAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
             }
         }
 
+        /**
+         * Handles String.replace calls when the receiver is a {@link
+         * org.checkerframework.checker.signature.qual.InternalForm}
+         */
         private void handleReplaceOnInternalForm(char c1, char c2, AnnotatedTypeMirror type) {
             if (c1 == '/' && c2 == '.') {
                 type.replaceAnnotation(BINARY_NAME);
