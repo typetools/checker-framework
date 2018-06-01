427f0d3f9a8f02ef4538fdf8fe2cf0178210ff06
Suzanne Millstein <smillst@cs.washington.edu>
2018-06-01 15:28:43 -0700

    Tweak Javadoc.

--- checker/src/main/java/org/checkerframework/checker/index/qual/HasSubsequence.java
+++ checker/src/main/java/org/checkerframework/checker/index/qual/HasSubsequence.java
@@ -15,7 +15,7 @@ import org.checkerframework.framework.qual.JavaExpression;
  *
  * <pre><code>
  *   class IntSubArray {
- *    {@literal @}HasSubsequence(value = "this", from = "this.start", to = "this.end") int [] array;
+ *   {@literal @}HasSubsequence(value = "this", from = "this.start", to = "this.end") int [] array;
  *    int {@literal @}IndexFor("array") int start;
  *    int {@literal @}IndexOrHigh("array") int end;
  *   }
@@ -24,24 +24,24 @@ import org.checkerframework.framework.qual.JavaExpression;
  * These annotations allow two kinds of indexing operations that would otherwise be forbidden:
  *
  * <ul>
- *   <li>If <code>i</code> is <code>{@literal @}IndexFor("this")</code>, then <code>start + i</code>
- *       is <code>{@literal @}IndexFor("array")</code>.
- *   <li>If <code>j</code> is <code>{@literal @}IndexFor("array")</code>, then <code>j - start
- *       </code> is <code>{@literal @}IndexFor("this")</code>.
+ *   <li>If {@code i} is {@code @IndexFor("this")}, then {@code start + i} is
+ *       {@code @IndexFor("array")}.
+ *   <li>If {@code j} is {@code @IndexFor("array")}, then {@code j - start } is
+ *       {@code @IndexFor("this")}.
  * </ul>
  *
- * When assigning an array <code>a</code> to <code>array</code>, 3 facts need to be proven:
+ * When assigning an array {@code a} to {@code array}, 3 facts need to be proven:
  *
  * <ul>
- *   <li><code>start</code> is <code>{literal @}NonNegative</code>.
- *   <li><code>end</code> is <code>{literal @}LTEqLengthOf("a")</code>.
- *   <li><code>start</code> is <code>{literal @}LessThan("end + 1")</code>.
+ *   <li>{@code start} is {@code @NonNegative}.
+ *   <li>{@code end} is {@code @LTEqLengthOf("a")}.
+ *   <li>{@code start} is {@code @LessThan("end + 1")}.
  * </ul>
  *
  * A warning will still be issued at this assignment, because the Index Checker cannot prove that
- * <code>"this"</code> is the name of the subsequence, only that such a subsequence exists. You
- * should manually verify that the named sequence is, in fact, the subsequence described in the
- * annotation and then suppress the warning.
+ * {@code "this"} is the name of the subsequence, only that such a subsequence exists. You should
+ * manually verify that the named sequence is, in fact, the subsequence described in the annotation
+ * and then suppress the warning.
  *
  * <p>For an example of how this annotation is used in practice, see the test GuavaPrimitives.java
  * in /checker/tests/index/.
