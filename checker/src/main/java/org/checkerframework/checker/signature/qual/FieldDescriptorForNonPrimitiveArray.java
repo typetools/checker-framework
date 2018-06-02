75606a24e3dc839ba0f2cbf894d857a488b6790c
Sagar Tewari <sagartewariym@yahoo.com>
2018-06-02 15:11:17 +0530

    added a new annotation

---
+++ checker/src/main/java/org/checkerframework/checker/signature/qual/FieldDescriptorForNonPrimitiveArray.java
@@ -0,0 +1,19 @@
+package org.checkerframework.checker.signature.qual;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+import org.checkerframework.framework.qual.ImplicitFor;
+import org.checkerframework.framework.qual.SubtypeOf;
+
+/**
+ * Represents a {@link FieldDescriptor field descriptor} (JVM type format) as defined in the <a
+ * href="https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-4.html#jvms-4.3.2">Java Virtual
+ * Machine Specification, section 4.3.2</a>, but <b>not</b> for all array types: only for an array
+ * type whose base type is in the unnamed package.
+ *
+ * @checker_framework.manual #signature-checker Signature Checker
+ */
+@SubtypeOf({InternalForm.class, FieldDescriptorForArray.class})
+@ImplicitFor(stringPatterns = "^\\[+([BCDFIJSZ]|L[A-Za-z_][A-Za-z_0-9]*;)$")
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+public @interface FieldDescriptorForNonPrimitiveArray {}
