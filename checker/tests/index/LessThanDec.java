b89ed5ed72199e4160294fc05d3db3d2ea93e458
Suzanne Millstein <smillst@cs.washington.edu>
2018-06-05 08:15:53 -0700

    Annotation isn't needed.

--- checker/tests/index/LessThanDec.java
+++ checker/tests/index/LessThanDec.java
@@ -5,7 +5,7 @@ import org.checkerframework.checker.index.qual.LessThan;
 public class LessThanDec {
     private static @IndexOrLow("#1") @LessThan("#4") int lastIndexOf(
             short[] array, short target, @IndexOrHigh("#1") int start, @IndexOrHigh("#1") int end) {
-        for (@LessThan("end") int i = end - 1; i >= start; i--) {
+        for (int i = end - 1; i >= start; i--) {
             if (array[i] == target) {
                 return i;
             }
