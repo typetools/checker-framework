58d49cfcc3c7e599e3fb121605b9265bc3affcc7
Rashmi <rashmi4@cs.washington.edu>
2018-06-12 19:27:01 -0700

    test - minor change

--- checker/tests/determinism/TestAbstractSequentialList.java
+++ checker/tests/determinism/TestAbstractSequentialList.java
@@ -3,6 +3,7 @@ import org.checkerframework.checker.determinism.qual.*;
 
 public class TestAbstractSequentialList {
     void test(@OrderNonDet AbstractSequentialList<@Det String> aList, @NonDet String a) {
+        // :: error: (argument.type.incompatible)
         aList.add(a);
     }
 }
