547808ebf7a1d1a290b97e21d8c318d293b808e3
Michael Ernst <mernst@cs.washington.edu>
2018-06-12 17:16:58 -0700

    Use source for WeakHashMap; update AbstractMap

--- checker/jdk/nullness/src/java/util/AbstractMap.java
+++ checker/jdk/nullness/src/java/util/AbstractMap.java
@@ -1,26 +1,26 @@
 /*
  * Copyright (c) 1997, 2013, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
  *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
  */
 
 package java.util;
@@ -356,9 +356,8 @@ public abstract class AbstractMap<K extends @Nullable Object, V extends @Nullabl
      */
     @SideEffectFree
     public Set<@KeyFor("this") K> keySet() {
-        Set<K> ks = keySet;
-        if (ks == null) {
-            ks = new AbstractSet<K>() {
+        if (keySet == null) {
+            keySet = new AbstractSet<K>() {
                 public Iterator<K> iterator() {
                     return new Iterator<K>() {
                         private Iterator<Entry<K,V>> i = entrySet().iterator();
@@ -393,9 +392,8 @@ public abstract class AbstractMap<K extends @Nullable Object, V extends @Nullabl
                     return AbstractMap.this.containsKey(k);
                 }
             };
-            keySet = ks;
         }
-        return ks;
+        return keySet;
     }
 
     /**
@@ -416,9 +414,8 @@ public abstract class AbstractMap<K extends @Nullable Object, V extends @Nullabl
      */
     @SideEffectFree
     public Collection<V> values() {
-        Collection<V> vals = values;
-        if (vals == null) {
-            vals = new AbstractCollection<V>() {
+        if (values == null) {
+            values = new AbstractCollection<V>() {
                 public Iterator<V> iterator() {
                     return new Iterator<V>() {
                         private Iterator<Entry<K,V>> i = entrySet().iterator();
@@ -453,9 +450,8 @@ public abstract class AbstractMap<K extends @Nullable Object, V extends @Nullabl
                     return AbstractMap.this.containsValue(v);
                 }
             };
-            values = vals;
         }
-        return vals;
+        return values;
     }
 
     @SideEffectFree
