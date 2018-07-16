4903cc0da281bcd2bc093b1f290115714f2eeafc
Werner Dietl <wdietl@gmail.com>
2018-07-15 20:54:36 -0400

    Fix #2059 by outputting a warning for unknown annotations.

---
+++ checker/jtreg/stubs/issue2059/Main.java
@@ -0,0 +1,10 @@
+/*
+ * @test
+ * @summary Test case for Issue 2059 https://github.com/typetools/checker-framework/issues/2059
+ *
+ * @compile/ref=AnnoNotFound.out -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker -AstubWarnIfNotFound -Astubs=AnnoNotFound.astub Main.java
+ */
+
+package issue2059;
+
+public class Main {}
