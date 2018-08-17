c274c064bd3ebe1daeaa5f908b031f5b8f84a6cb
Suzanne Millstein <smillst@cs.washington.edu>
2018-08-17 15:48:57 -0700

    Remove confusing comments.

--- checker/tests/tainting/Issue2107.java
+++ checker/tests/tainting/Issue2107.java
@@ -5,12 +5,10 @@ public abstract class Issue2107 {
     abstract @PolyTainted int method(@PolyTainted Issue2107 this);
 
     @PolyTainted int method2(@PolyTainted Issue2107 this) {
-        // Error
         return this.method();
     }
 
     @PolyTainted int method3(@PolyTainted Issue2107 this) {
-        // No error
         return method();
     }
 }
