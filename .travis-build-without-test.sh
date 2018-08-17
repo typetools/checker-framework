9f8804323acb49b6b67d92cba4ca78e92aebfa40
Michael Ernst <mernst@cs.washington.edu>
2018-08-17 15:52:27 -0700

    Add debugging output [ci skip]

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -32,6 +32,7 @@ else
     [ -d /tmp/plume-scripts ] || (cd /tmp && git clone --depth 1 https://github.com/plume-lib/plume-scripts.git)
     REPO=`/tmp/plume-scripts/git-find-fork ${SLUGOWNER} typetools annotation-tools`
     BRANCH=`/tmp/plume-scripts/git-find-branch ${REPO} ${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}`
+    echo "About to execute: git clone -b ${BRANCH} --single-branch --depth 1 ${REPO}"
     (cd .. && git clone -b ${BRANCH} --single-branch --depth 1 ${REPO}) || (cd .. && git clone -b ${BRANCH} --single-branch --depth 1 ${REPO})
 fi
 
