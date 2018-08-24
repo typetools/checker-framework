d4bfa3d5cc3f54cdbfdbb8e406d367918d8235f2
Jason Waataja <jwaataja@cs.washington.edu>
2018-08-24 15:43:32 -0700

    Added test for issue #22

---
+++ checker/tests/determinism/Issue22.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import org.checkerframework.checker.determinism.qual.*;
+
+public class Issue22<T extends Comparable<T>> {
+    public int compare(T[] a1, T[] a2) {
+        return a1[0].compareTo(a2[0]);
+    }
+}
