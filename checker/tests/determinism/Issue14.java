966e5b35cd20413edb4cc790ff43fadb2159c063
Rashmi <rashmi4@cs.washington.edu>
2018-08-29 15:31:30 -0700

    test case changes

--- checker/tests/determinism/Issue14.java
+++ checker/tests/determinism/Issue14.java
@@ -5,21 +5,22 @@ import org.checkerframework.checker.determinism.qual.*;
 
 // @skip-test
 class Issue14 {
-    public static <T> Set<T> genericsBug(List<T> list) {
-        Set<T> result = new HashSet<T>();
-        for (T element : list) {
-            result.add(element);
-        }
-        return result;
-    }
-
-    public static <T extends @PolyDet("use") Object> @PolyDet Set<T> toSet(@PolyDet List<T> list) {
-        @PolyDet Set<T> result = new @PolyDet HashSet<T>();
-        for (T element : list) {
-            result.add(element);
-        }
-        return result;
-    }
+    //    public static <T> Set<T> genericsBug(List<T> list) {
+    //        Set<T> result = new HashSet<T>();
+    //        for (T element : list) {
+    //            result.add(element);
+    //        }
+    //        return result;
+    //    }
+    //
+    //    public static <T extends @PolyDet("use") Object> @PolyDet Set<T> toSet(@PolyDet List<T>
+    // list) {
+    //        @PolyDet Set<T> result = new @PolyDet HashSet<T>();
+    //        for (T element : list) {
+    //            result.add(element);
+    //        }
+    //        return result;
+    //    }
 
     public static <T extends @PolyDet Object> @PolyDet Set<T> toSet1(
             @PolyDet List<T> list, @PolyDet Set<T> result) {
@@ -29,11 +30,11 @@ class Issue14 {
         return result;
     }
 
-    public static <@PolyDet T extends @PolyDet Object> @PolyDet Set<T> toSet2(
-            @PolyDet List<T> list, @PolyDet Set<T> result) {
-        for (T element : list) {
-            result.add(element);
-        }
-        return result;
-    }
+    //    public static <@PolyDet T extends @PolyDet Object> @PolyDet Set<T> toSet2(
+    //            @PolyDet List<T> list, @PolyDet Set<T> result) {
+    //        for (T element : list) {
+    //            result.add(element);
+    //        }
+    //        return result;
+    //    }
 }
--- checker/tests/determinism/Issue14PolyNull.java
+++ checker/tests/determinism/Issue14PolyNull.java
@@ -5,13 +5,13 @@ import org.checkerframework.checker.nullness.qual.*;
 
 // @skip-test
 class Issue14PolyNUll {
-    public static <T extends @Nullable Object> void toSet(List<T> list) {
-        @NonNull T a = list.get(0);
-    }
+    //    public static <T extends @Nullable Object> void toSet(List<T> list) {
+    //        @NonNull T a = list.get(0);
+    //    }
 
-    public static <T> @PolyNull Set<T> toSet1(@PolyNull List<T> list) {
-        Set<T> result = new HashSet<T>();
-        for (@NonNull T element : list) {
+    public static <T extends @PolyNull Object> @PolyNull Set<T> toSet1(
+            @PolyNull List<T> list, @PolyNull Set<T> result) {
+        for (T element : list) {
             result.add(element);
         }
         return result;
