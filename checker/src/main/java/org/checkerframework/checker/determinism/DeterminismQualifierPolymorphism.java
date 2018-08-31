87669be115e17f1903294a07e0457bc7127bdc71
Rashmi <rashmi4@cs.washington.edu>
2018-08-30 19:37:07 -0700

    recursive function for @OrderNonDet collections with nested type arguments

--- checker/src/main/java/org/checkerframework/checker/determinism/DeterminismQualifierPolymorphism.java
+++ checker/src/main/java/org/checkerframework/checker/determinism/DeterminismQualifierPolymorphism.java
@@ -91,58 +91,32 @@ public class DeterminismQualifierPolymorphism extends DefaultQualifierPolymorphi
             return;
         }
 
+        // TODO-rashmi: Handle Maps
+        recursiveReplaceOrderNonDet(type, replaceType);
+    }
+
+    /**
+     * Iterates over all the nested type parameters and does the replacement. Example:
+     * In @OrderNonDet Set<@OrderNonDet Set<@Det Integer>>, this while loop iterates twice for the
+     * two @OrderNonDet Sets.
+     *
+     * @param type
+     * @param replaceType
+     */
+    void recursiveReplaceOrderNonDet(AnnotatedTypeMirror type, AnnotationMirror replaceType) {
         TypeMirror underlyingTypeOfReceiver =
                 TypesUtils.getTypeElement(type.getUnderlyingType()).asType();
-        // TODO: The following comment is incorrect, because isCollectionOrIterator is reassigned
-        // without `type` being reassigned.  Or maybe "the type" doesn't refer to `type` but some
-        // other variable such as declaredTypeOuter that the reader is supposed to guess from
-        // context.  Clarify, and be specific.
-        // This flag is true if the type is a collection or an iterator
-        boolean isCollectionOrIterator =
-                (factory.isCollection(underlyingTypeOfReceiver)
-                        || factory.isIterator(underlyingTypeOfReceiver));
-        // Outer declaration type
-        AnnotatedTypeMirror.AnnotatedDeclaredType declaredTypeOuter =
-                (isCollectionOrIterator ? (AnnotatedTypeMirror.AnnotatedDeclaredType) type : null);
-
-        // TODO: I am still confused by this loop.
-        // Why not make a recursive call to this method for each type parameter?
-        // That will make it easy to accommodate multiple type parameters, too (just a for loop over
-        // each one), whereas I don't see how to handle multiple type parameters with the current
-        // design.
-        // Iterates over all the nested type parameters and does the replacement.
-        // Example: In @OrderNonDet Set<@OrderNonDet Set<@Det Integer>>,
-        // this while loop iterates twice for the two @OrderNonDet Sets.
-        while (isCollectionOrIterator) {
-            // Iterates over all the type parameters of this collection and
-            // replaces all @OrderNonDet type parameters with 'replaceType'.
-            // Example: @OrderNonDet MyMap<@Det Integer, @Det Integer>
-            // This loop executes twice for the two type parameters.
-            for (AnnotatedTypeMirror argType : declaredTypeOuter.getTypeArguments()) {
-                if (argType.hasAnnotation(factory.ORDERNONDET)) {
-                    argType.replaceAnnotation(replaceType);
-                }
-            }
-
-            // Assuming a single type parameter (will not work for HashMaps)
-            // TODO-rashmi: Handle all type parameters
-
-            // Example: @OrderNonDet Set<@OrderNonDet List<@Det Integer>>
-            // In the first iteration of this loop, declaredTypeOuter would be @OrderNonDet Set
-            // and declaredTypeInner would be @OrderNonDet List.
-            // In the second iteration, declaredTypeOuter would be @OrderNonDet List
-            // and declaredTypeInner would be @Det Integer.
-            TypeMirror declaredTypeInner =
-                    TypesUtils.getTypeElement(
-                                    declaredTypeOuter.getTypeArguments().get(0).getUnderlyingType())
-                            .asType();
-            if (factory.isCollection(declaredTypeInner) || factory.isIterator(declaredTypeInner)) {
-                declaredTypeOuter =
-                        (AnnotatedTypeMirror.AnnotatedDeclaredType)
-                                declaredTypeOuter.getTypeArguments().get(0);
-            } else {
-                isCollectionOrIterator = false;
-            }
+        if (!(factory.isCollection(underlyingTypeOfReceiver)
+                || factory.isIterator(underlyingTypeOfReceiver))) {
+            return;
         }
+
+        AnnotatedTypeMirror.AnnotatedDeclaredType declaredTypeOuter =
+                (AnnotatedTypeMirror.AnnotatedDeclaredType) type;
+        AnnotatedTypeMirror argType = declaredTypeOuter.getTypeArguments().get(0);
+        if (argType.hasAnnotation(factory.ORDERNONDET)) {
+            argType.replaceAnnotation(replaceType);
+        }
+        recursiveReplaceOrderNonDet(argType, replaceType);
     }
 }
