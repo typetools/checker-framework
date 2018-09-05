c68d86ca93341b2abe5b22dae919561284a93297
Michael Ernst <mernst@cs.washington.edu>
2018-09-05 10:28:44 -0700

    Add an example

--- checker/src/main/java/org/checkerframework/checker/signature/qual/ClassTypeSignature.java
+++ checker/src/main/java/org/checkerframework/checker/signature/qual/ClassTypeSignature.java
@@ -5,8 +5,11 @@ import java.lang.annotation.Target;
 import org.checkerframework.framework.qual.SubtypeOf;
 
 /**
- * Represents signature of a class declaration as defined in <a
- * href=https://docs.oracle.com/javase/specs/jvms/se9/html/jvms-4.html#jvms-4.7.9.1>JVM specs</a>.
+ * Represents a class declaration as defined in the <a
+ * href="https://docs.oracle.com/javase/specs/jvms/se10/html/jvms-4.html#jvms-4.7.9.1">JVM
+ * specification</a>.
+ *
+ * <p>An example is {@code Lfoo/bar/Baz<Lp1.Quux;>.22;}.
  *
  * @see GenericSignature
  */
