3631663135622645a02d15ccdea2e9e922e00e16
Jeff Luo <j36luo@uwaterloo.ca>
2018-09-08 16:57:22 -0400

    Disable modules in javac for the AnnotationBuilderTest (#16)

--- framework/src/test/java/tests/AnnotationBuilderTest.java
+++ framework/src/test/java/tests/AnnotationBuilderTest.java
@@ -4,8 +4,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import com.sun.tools.javac.main.JavaCompiler;
+import com.sun.tools.javac.main.Option;
 import com.sun.tools.javac.processing.JavacProcessingEnvironment;
 import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.util.List;
+import com.sun.tools.javac.util.Options;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.type.TypeMirror;
@@ -24,8 +27,16 @@ public class AnnotationBuilderTest {
 
     public AnnotationBuilderTest() {
         Context context = new Context();
+        // Set source and target to 8
+        Options options = Options.instance(context);
+        options.put(Option.SOURCE, "8");
+        options.put(Option.TARGET, "8");
+
         env = JavacProcessingEnvironment.instance(context);
         JavaCompiler javac = JavaCompiler.instance(context);
+        // Even though source/target are set to 8, the modules in the JavaCompiler
+        // need to be initialized by setting the list of modules to nil.
+        javac.initModules(List.nil());
         javac.enterDone();
         ErrorReporter.setHandler(new TestChecker());
     }
