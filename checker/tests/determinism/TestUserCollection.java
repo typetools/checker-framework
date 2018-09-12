dd6ae193452a31f1bd3dd75516c5aed3a75bc583
Rashmi <rashmi4@cs.washington.edu>
2018-09-12 13:54:55 -0700

    document edits

---
+++ checker/tests/determinism/TestUserCollection.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import org.checkerframework.checker.determinism.qual.*;
+
+public class TestUserCollection<E> extends ArrayList<E> {
+    public @PolyDet("down") boolean contains(@PolyDet Object o) {
+        return true;
+    }
+
+    void callContains(@OrderNonDet TestUserCollection<@Det Integer> list, @Det Object o) {
+        @Det boolean result = list.contains(o);
+    }
+}
