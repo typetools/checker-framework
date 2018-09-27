8b2ce609a97571c3ee7c3119474d3b49d7b674e8
Rashmi <rashmi4@cs.washington.edu>
2018-09-27 13:41:29 -0700

    fixes failing tests

--- checker/tests/determinism/TestArrayUpperBounds.java
+++ checker/tests/determinism/TestArrayUpperBounds.java
@@ -5,11 +5,13 @@ import org.checkerframework.checker.determinism.qual.*;
 
 class TestArrayUpperBounds {
     public static <@Det T extends @PolyDet Object> T[] newArray() {
+        // :: warning: [unchecked] unchecked cast
         T[] arr = (T[]) new Object[0];
         return arr;
     }
 
     public static <T> T[] newArray1() {
+        // :: warning: [unchecked] unchecked cast
         T[] arr = (T[]) new Integer[10];
         return arr;
     }
