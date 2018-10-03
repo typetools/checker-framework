f7a69358138e0ff50e64b079b310aee522869859
Rashmi <rashmi4@cs.washington.edu>
2018-10-02 17:24:37 -0700

    issue #24 fixed

--- checker/tests/determinism/Issue24.java
+++ checker/tests/determinism/Issue24.java
@@ -1,6 +1,5 @@
 import org.checkerframework.checker.determinism.qual.*;
 
-// @skip-test
 public class Issue24 {
     public static String formatInts(@Det int i, @PolyDet int j) {
         return String.format("%i %i", i, j);
