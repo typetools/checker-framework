6ca904b25eb2146d0c802c7f4b83764a386178ed
Rashmi <rashmi4@cs.washington.edu>
2018-10-05 10:45:10 -0700

    issue 53 test

--- checker/tests/determinism/Issue53.java
+++ checker/tests/determinism/Issue53.java
@@ -9,6 +9,11 @@ public class Issue53<T extends @NonDet Object> {
     }
 
     public static <U extends @PolyDet("use") Object> U @PolyDet [] g(U @PolyDet [] a) {
-        return f(new Issue53<U>());
+        return f((@PolyDet Issue53<U>) new @PolyDet Issue53<U>());
+    }
+
+    public static <U extends @PolyDet("use") Object> U @PolyDet [] g1(
+            U @PolyDet [] a, @PolyDet Issue53<U> inp) {
+        return f(inp);
     }
 }
