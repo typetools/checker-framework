d7a08275edb8e46aaeacb3f09998d174d3d66bdf
Rashmi <rashmi4@cs.washington.edu>
2018-10-18 11:27:50 -0700

    test cases

--- checker/tests/determinism/Issue57.java
+++ checker/tests/determinism/Issue57.java
@@ -1,9 +1,7 @@
 import java.util.*;
 import org.checkerframework.checker.determinism.qual.*;
 
-public class Issue57<T extends @PolyDet("use") Object> implements @NonDet Iterator<T> {
-    public Issue57(@PolyDet List<T> a) {}
-
+public class Issue57<T> implements @NonDet Iterator<T> {
     public Issue57() {}
 
     public @PolyDet("down") boolean hasNext() {
