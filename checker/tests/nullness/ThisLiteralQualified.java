828c821d5536f4e39545de3cbb6a8f1eb50fd7af
Michael Ernst <mernst@cs.washington.edu>
2018-11-14 19:50:40 -0800

    Skip test, and refer to issue in issue tracker

--- checker/tests/nullness/ThisLiteralQualified.java
+++ checker/tests/nullness/ThisLiteralQualified.java
@@ -1,10 +1,15 @@
-import org.checkerframework.checker.initialization.qual.*;
-import org.checkerframework.checker.nullness.qual.*;
+// Test case for Issue #2208:
+// https://github.com/typetools/checker-framework/issues/2208
+
+// @skip-test until the issue is fixed
+
+import org.checkerframework.checker.initialization.qual.UnderInitialization;
+import org.checkerframework.checker.nullness.qual.Raw;
 
 public class ThisLiteralQualified {
     public ThisLiteralQualified() {
         super();
-        @UnderInitialization @Raw ThisLiteralQualified b = this;
-        @UnderInitialization @Raw ThisLiteralQualified a = ThisLiteralQualified.this;
+        @UnderInitialization @Raw ThisLiteralQualified a = this;
+        @UnderInitialization @Raw ThisLiteralQualified b = ThisLiteralQualified.this;
     }
 }
