9b454439482a34030701b41f4991ddc98a2522da
Rashmi <rashmi4@cs.washington.edu>
2019-01-09 00:46:55 -0800

    Fixes #2229

--- checker/tests/lock/Issue2229.java
+++ checker/tests/lock/Issue2229.java
@@ -1,5 +1,6 @@
 import org.checkerframework.checker.lock.qual.*;
 
 @GuardedBy("lock") class Issue2229 {
+    // :: error: (expression.unparsable.type.invalid)
     @GuardedBy("lock") Issue2229() {}
 }
