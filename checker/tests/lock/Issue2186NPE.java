321f8cee029b1625a71d92baec3de617102c344b
Jason Waataja <jwaataja@cs.washington.edu>
2019-01-17 16:05:09 -0800

    Removed test in lock

--- checker/tests/lock/Issue2186NPE.java
+++
@@ -1,7 +0,0 @@
-// @skip-test until https://github.com/t-rasmud/checker-framework/issues/73
-
-import org.checkerframework.checker.lock.qual.*;
-
-@GuardedBy("lock") class Issue2186NPE {
-    @GuardedBy("lock") Issue2186NPE() {}
-}
