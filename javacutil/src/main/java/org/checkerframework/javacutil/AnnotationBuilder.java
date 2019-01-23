c11f7adc15e24a62086e302f4a4c79684ceb7c14
Michael Ernst <mernst@cs.washington.edu>
2019-01-23 10:13:31 -0800

    Document peculiarities of annotation creation

--- javacutil/src/main/java/org/checkerframework/javacutil/AnnotationBuilder.java
+++ javacutil/src/main/java/org/checkerframework/javacutil/AnnotationBuilder.java
@@ -60,7 +60,11 @@ public class AnnotationBuilder {
     private final DeclaredType annotationType;
     private final Map<ExecutableElement, AnnotationValue> elementValues;
 
-    /** Caching for annotation creation. */
+    /**
+     * Caching for annotation creation. Each annotation has no values; that is, getElementValues
+     * returns an empty map. This may be in conflict with the annotation's definition, which might
+     * contain elements (annotation fields).
+     */
     private static final Map<CharSequence, AnnotationMirror> annotationsFromNames =
             Collections.synchronizedMap(new HashMap<>());
 
@@ -106,7 +110,8 @@ public class AnnotationBuilder {
 
     /**
      * Creates an {@link AnnotationMirror} given by a particular fully-qualified name.
-     * getElementValues on the result returns an empty map.
+     * getElementValues on the result returns an empty map. This may be in conflict with the
+     * annotation's definition, which might contain elements (annotation fields).
      *
      * @param elements the element utilities to use
      * @param name the name of the annotation to create
