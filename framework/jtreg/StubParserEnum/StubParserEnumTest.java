1442f4e798624125112c1183772e00603ea79d79
Jeff Luo <j36luo@uwaterloo.ca>
2019-01-29 20:39:06 -0500

    format code

--- framework/jtreg/StubParserEnum/StubParserEnumTest.java
+++ framework/jtreg/StubParserEnum/StubParserEnumTest.java
@@ -22,7 +22,7 @@ class StubParserEnumTest {
         MILLISECONDS,
         @ReportReadWrite
         SECONDS;
-        
+
         @ReportCall
         long toMicros(long d) {
             return d;
@@ -30,39 +30,22 @@ class StubParserEnumTest {
     }
 
     void readFromEnumInSource() {
-        // :: error: (fieldreadwrite)
         MyTimeUnit u1 = MyTimeUnit.SECONDS;
-        // :: error: (fieldreadwrite)
         MyTimeUnit u2 = MyTimeUnit.MILLISECONDS;
-
-        // these 2 uses should not have any reports
         MyTimeUnit u3 = MyTimeUnit.MICROSECONDS;
         MyTimeUnit u4 = MyTimeUnit.NANOSECONDS;
-        
-        // :: error: (fieldreadwrite) :: error: (methodcall)
         long sUS = MyTimeUnit.SECONDS.toMicros(10);
     }
 
     void readFromEnumInStub() {
-        // :: error: (fieldreadwrite)
         TimeUnit u1 = TimeUnit.SECONDS;
-        // :: error: (fieldreadwrite)
         TimeUnit u2 = MILLISECONDS;
-
-        // these 2 uses should not have any reports
         TimeUnit u3 = TimeUnit.MICROSECONDS;
         TimeUnit u4 = NANOSECONDS;
-
-        // :: error: (fieldreadwrite) :: error: (methodcall)
         long sUS = TimeUnit.SECONDS.toMicros(10);
-        // :: error: (fieldreadwrite)
         long sNS = SECONDS.toNanos(10);
-
-        // :: error: (fieldreadwrite)
         long msMS = TimeUnit.MILLISECONDS.toMillis(10);
-        // :: error: (fieldreadwrite) :: error: (methodcall)
         long msUS = TimeUnit.MILLISECONDS.toMicros(10);
-        // :: error: (fieldreadwrite)
         long msNS = MILLISECONDS.toNanos(10);
     }
 }
