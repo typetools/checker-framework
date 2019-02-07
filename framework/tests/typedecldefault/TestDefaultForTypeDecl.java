5bb5a064b9427408a96a340adbe9df8b8b0ed96b
Rashmi <rashmi4@cs.washington.edu>
2019-02-07 15:19:49 -0800

    test with unannotated and other qual usetypes

--- framework/tests/typedecldefault/TestDefaultForTypeDecl.java
+++ framework/tests/typedecldefault/TestDefaultForTypeDecl.java
@@ -2,4 +2,11 @@ import typedecldefault.quals.*;
 
 public class TestDefaultForTypeDecl {
     void test(@TypeDeclDefaultTop TestDefaultForTypeDecl arg) {}
+
+    void testUnannotated(TestDefaultForTypeDecl arg) {}
+
+    void testOtherQual(
+            @TypeDeclDefaultBottom TestDefaultForTypeDecl arg, TestDefaultForTypeDecl arg1) {
+        arg = arg1;
+    }
 }
