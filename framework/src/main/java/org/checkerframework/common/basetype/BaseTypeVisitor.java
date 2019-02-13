105521bb6fd74c31a5acd1475bc90f7b8f9c89c2
Werner Dietl <wdietl@gmail.com>
2019-02-13 11:20:32 -0500

    Remove duplication between constructors

--- framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
+++ framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
@@ -192,28 +192,14 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
      *     TypeHierarchy#isSubtype})
      */
     public BaseTypeVisitor(BaseTypeChecker checker) {
-        super(checker);
-
-        this.checker = checker;
-        this.atypeFactory = createTypeFactory();
-        this.contractsUtils = ContractsUtils.getInstance(atypeFactory);
-        this.positions = trees.getSourcePositions();
-        this.visitorState = atypeFactory.getVisitorState();
-        this.typeValidator = createTypeValidator();
-        ProcessingEnvironment env = checker.getProcessingEnvironment();
-        this.objectEquals = TreeUtils.getMethod("java.lang.Object", "equals", 1, env);
-        this.vectorCopyInto = TreeUtils.getMethod("java.util.Vector", "copyInto", 1, env);
-        this.functionApply = TreeUtils.getMethod("java.util.function.Function", "apply", 1, env);
-        this.vectorType = atypeFactory.fromElement(elements.getTypeElement("java.util.Vector"));
-
-        checkForAnnotatedJdk();
+        this(checker, null);
     }
 
     protected BaseTypeVisitor(BaseTypeChecker checker, Factory typeFactory) {
         super(checker);
 
         this.checker = checker;
-        this.atypeFactory = typeFactory;
+        this.atypeFactory = typeFactory == null ? createTypeFactory() : typeFactory;
         this.contractsUtils = ContractsUtils.getInstance(atypeFactory);
         this.positions = trees.getSourcePositions();
         this.visitorState = atypeFactory.getVisitorState();
