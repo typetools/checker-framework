7b87d10d95ee686ae60acc4763091ffd0c244c47
Werner Dietl <wdietl@gmail.com>
2019-02-18 23:10:18 -0500

    Clean up test.

--- checker/tests/signedness/AnnoBeforeModifier.java
+++ checker/tests/signedness/AnnoBeforeModifier.java
@@ -36,55 +36,55 @@ public class AnnoBeforeModifier {
     // :: warning: (type.anno.before.modifier)
     @Unsigned public int iMethod() {
         return 0;
-    };
+    }
 
     public @Unsigned int jMethod() {
         return 0;
-    };
+    }
 
     // :: warning: (type.anno.before.modifier)
     public @Unsigned final int kMethod() {
         return 0;
-    };
+    }
 
     @SuppressWarnings("foobar")
     @Unsigned public int lMethod() {
         return 0;
-    };
+    }
 
     public @SuppressWarnings("foobar") @Unsigned int mMethod() {
         return 0;
-    };
+    }
 
     @SuppressWarnings("foobar")
     @Unsigned public int nMethod() {
         return 0;
-    };
+    }
 
     // TODO: :: warning: (type.anno.before.modifier)
     public @SuppressWarnings("foobar") @Unsigned final int oMethod() {
         return 0;
-    };
+    }
 
     // :: warning: (type.anno.before.decl.anno) :: warning: (type.anno.before.modifier)
     public @Unsigned @SuppressWarnings("foobar") final int pMethod() {
         return 0;
-    };
+    }
 
     public @SuppressWarnings("foobar") final @Unsigned int qMethod() {
         return 0;
-    };
+    }
 
     @SuppressWarnings("foobar")
     public int rMethod() {
         return 0;
-    };
+    }
 
     public @SuppressWarnings("foobar") int sMethod() {
         return 0;
-    };
+    }
 
     public @SuppressWarnings("foobar") final int tMethod() {
         return 0;
-    };
+    }
 }
