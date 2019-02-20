96297f5a36143f53cbfed5200da20332e12a19bb
Rashmi <rashmi4@cs.washington.edu>
2019-02-20 09:46:57 -0800

    changed to i18ntest

--- checker/tests/i18n/ConstructorIssue.java
+++ checker/tests/i18n/ConstructorIssue.java
@@ -1,16 +1,15 @@
 // Test case for issue #2186
 // https://github.com/typetools/checker-framework/issues/2186
 
-import org.checkerframework.checker.propkey.qual.PropertyKey;
-import org.checkerframework.checker.propkey.qual.PropertyKeyBottom;
+import org.checkerframework.checker.i18n.qual.*;
 
-@PropertyKey class ConstructorIssue {
+@LocalizableKey class ConstructorIssue {
     ConstructorIssue() {}
 
-    @PropertyKeyBottom ConstructorIssue(int x) {}
+    @LocalizableKeyBottom ConstructorIssue(int x) {}
 
     void test() {
-        @PropertyKey ConstructorIssue obj = new ConstructorIssue();
-        @PropertyKeyBottom ConstructorIssue obj1 = new ConstructorIssue(9);
+        @LocalizableKey ConstructorIssue obj = new ConstructorIssue();
+        @LocalizableKeyBottom ConstructorIssue obj1 = new ConstructorIssue(9);
     }
 }
