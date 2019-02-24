65c24ebb6b764b1099ed0e9b47e2876634cfd932
Michael Ernst <mernst@cs.washington.edu>
2019-02-24 07:30:31 -0800

    Undo a change

--- checker/tests/regex/TestRegex.java
+++ checker/tests/regex/TestRegex.java
@@ -1,8 +1,17 @@
-// test-case for issue 148
-
 import org.checkerframework.checker.regex.qual.*;
 
-class Issue148 {
+// test-case for issue 128
+public class TestRegex {
+
+    public void Concatenation2() {
+        @Regex String a = "a";
+        // :: error: (compound.assignment.type.incompatible)
+        a += "(";
+    }
+}
+
+// test-case for issue 148
+class Search {
     public static void main(String[] args) {
         if (!org.checkerframework.checker.regex.RegexUtil.isRegex(args[0], 4)) {
             return;
