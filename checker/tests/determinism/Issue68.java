2750a07ddafd265dc5c88b83648a1913f4543ea0
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2019-03-06 14:59:32 -0800

    adding expecte warning

--- checker/tests/determinism/Issue68.java
+++ checker/tests/determinism/Issue68.java
@@ -4,6 +4,7 @@ public class Issue68 {
     @PolyDet Issue68(@PolyDet int i) {}
 
     static void f(@NonDet int i) {
+        // :: warning: (cast.unsafe.constructor.invocation)
         @Det Issue68 p = new @Det Issue68(i);
     }
 }
