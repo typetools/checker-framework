f5b955d42452f695584fe015a98a56ef912ed509
Kartikeya Goswami <kartikeyagoswami20@gmail.com>
2019-03-09 15:11:27 +0530

    Update EnsuresQualifierIf.java

--- framework/src/main/java/org/checkerframework/framework/qual/EnsuresQualifierIf.java
+++ framework/src/main/java/org/checkerframework/framework/qual/EnsuresQualifierIf.java
@@ -3,10 +3,10 @@ package org.checkerframework.framework.qual;
 import java.lang.annotation.Annotation;
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.lang.annotation.Repeatable;
 
 /**
  * A conditional postcondition annotation to indicate that a method ensures that certain expressions
