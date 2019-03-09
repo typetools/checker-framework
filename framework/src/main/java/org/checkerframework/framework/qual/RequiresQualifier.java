e49e9a97f99487fd7ff45421865852d3905a2356
Kartikeya Goswami <kartikeyagoswami20@gmail.com>
2019-03-09 14:55:22 +0530

    Update RequiresQualifier.java

--- framework/src/main/java/org/checkerframework/framework/qual/RequiresQualifier.java
+++ framework/src/main/java/org/checkerframework/framework/qual/RequiresQualifier.java
@@ -6,6 +6,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.annotation.Repeatable;
 
 /**
  * A precondition annotation to indicate that a method requires certain expressions to have a
@@ -18,6 +19,7 @@ import java.lang.annotation.Target;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.CONSTRUCTOR})
+@Repeatable(RequiresQualifiers.class)
 public @interface RequiresQualifier {
     /**
      * The Java expressions for which the annotation need to be present.
