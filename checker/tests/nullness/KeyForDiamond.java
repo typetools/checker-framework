c5f2cc7d00bc3ca4a8082fd3d5f6674fcced76fd
Suzanne Millstein <smillst@cs.washington.edu>
2019-03-14 15:07:59 -0700

    Fix test case.

--- checker/tests/nullness/KeyForDiamond.java
+++ checker/tests/nullness/KeyForDiamond.java
@@ -5,10 +5,9 @@ public class KeyForDiamond {
     private final Map<Integer, Double> map = new HashMap<>();
 
     public void method() {
-        Map<@KeyFor("map") Integer, Double> paths1; // does not compile
-        Map<@KeyFor("map") Integer, Double> paths = new HashMap<>(); // does not compile
-        Set<@KeyFor("map") Integer> set = new HashSet<>(); // compiles
-        List<@KeyFor("map") Integer> list = new ArrayList<>(); // does not compile
+        Map<@KeyFor("map") Integer, Double> paths = new HashMap<>();
+        Set<@KeyFor("map") Integer> set = new HashSet<>();
+        List<@KeyFor("map") Integer> list = new ArrayList<>();
     }
 
     public static void main(String[] args) {
