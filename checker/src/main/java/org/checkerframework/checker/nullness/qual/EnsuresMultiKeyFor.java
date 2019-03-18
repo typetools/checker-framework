4e4ad5963a6261c1af9f1a3c97447ff7cac02f3d
Kartikeya Goswami <kartikeyagoswami20@gmail.com>
2019-03-19 02:54:21 +0530

    Create EnsuresMultiKeyFor.java

---
+++ checker/src/main/java/org/checkerframework/checker/nullness/qual/EnsuresMultiKeyFor.java
@@ -0,0 +1,32 @@
+package org.checkerframework.checker.nullness.qual;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.checkerframework.framework.qual.InheritedAnnotation;
+import org.checkerframework.framework.qual.PostconditionAnnotation;
+
+/**
+ * An annotation wrapper to allow multiple {@code @EnsuresKeyFor} annotation at the same location.
+ *
+ * <p>Programmers generally do not need to use this; it is created by Java when a programmer writes
+ * more than one {@code @EnsuresEnsuresKeyFor} annotation at the same location.
+ *
+ * @see EnsuresKeyFor
+ * @checker_framework.manual #java-expressions-as-arguments Syntax of Java expressions
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.CONSTRUCTOR})
+@PostconditionAnnotation(qualifier = KeyFor.class)
+@InheritedAnnotation
+public @interface EnsuresMultiKeyFor {
+    /**
+     * Constitutes the Java expressions that are keys in the given maps on successful method termination.
+     *
+     * @checker_framework.manual #java-expressions-as-arguments Syntax of Java expressions
+     */
+    EnsuresKeyFor[] value();
+}
