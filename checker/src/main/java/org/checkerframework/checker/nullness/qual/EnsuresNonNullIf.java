85e5f871313842ac50a37969a0ffc80afb9506a4
Kartikeya Goswami <kartikeyagoswami20@gmail.com>
2019-03-19 02:52:50 +0530

    Update EnsuresNonNullIf.java

--- checker/src/main/java/org/checkerframework/checker/nullness/qual/EnsuresNonNullIf.java
+++ checker/src/main/java/org/checkerframework/checker/nullness/qual/EnsuresNonNullIf.java
@@ -2,6 +2,7 @@ package org.checkerframework.checker.nullness.qual;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -67,6 +68,7 @@ import org.checkerframework.framework.qual.InheritedAnnotation;
 @Target({ElementType.METHOD, ElementType.CONSTRUCTOR})
 @ConditionalPostconditionAnnotation(qualifier = NonNull.class)
 @InheritedAnnotation
+@Repeatable(EnsuresMultiNonNullIf.class)
 public @interface EnsuresNonNullIf {
     /**
      * Java expression(s) that are non-null after the method returns the given result.
