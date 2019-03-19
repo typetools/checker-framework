eff4c24a060ff1f8a0e86eb4e4c49e99187252be
Michael Ernst <mernst@alum.mit.edu>
2019-03-19 08:34:22 -0700

    Reduce code duplication in getSuppressWarningsKeys() (#2364)

--- checker/src/main/java/org/checkerframework/checker/fenum/FenumChecker.java
+++ checker/src/main/java/org/checkerframework/checker/fenum/FenumChecker.java
@@ -1,12 +1,8 @@
 package org.checkerframework.checker.fenum;
 
-import java.lang.annotation.Annotation;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 import javax.annotation.processing.SupportedOptions;
 import org.checkerframework.common.basetype.BaseTypeChecker;
-import org.checkerframework.common.basetype.BaseTypeVisitor;
 import org.checkerframework.common.subtyping.SubtypingChecker;
 import org.checkerframework.framework.qual.StubFiles;
 
@@ -36,25 +32,9 @@ public class FenumChecker extends BaseTypeChecker {
     }
     */
 
-    /**
-     * Copied from SubtypingChecker; cannot reuse it, because SubtypingChecker is final.
-     *
-     * @see SubtypingChecker#getSuppressWarningsKeys()
-     */
     @Override
     public Collection<String> getSuppressWarningsKeys() {
-        Set<Class<? extends Annotation>> annos =
-                ((BaseTypeVisitor<?>) visitor).getTypeFactory().getSupportedTypeQualifiers();
-        if (annos.isEmpty()) {
-            return super.getSuppressWarningsKeys();
-        }
-
-        Set<String> swKeys = new HashSet<>();
-        swKeys.add(SUPPRESS_ALL_KEY);
-        for (Class<? extends Annotation> anno : annos) {
-            swKeys.add(anno.getSimpleName().toLowerCase());
-        }
-
-        return swKeys;
+        return SubtypingChecker.getSuppressWarningsKeys(
+                this.visitor, super.getSuppressWarningsKeys());
     }
 }
