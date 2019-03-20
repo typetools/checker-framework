380cb12477f5e519b00140ea922271499a793883
Kartikeya Goswami <kartikeyagoswami20@gmail.com>
2019-03-20 22:06:46 +0530

    Create EnsuresMultiMinLenIf.java

---
+++ framework/src/main/java/org/checkerframework/common/value/qual/EnsuresMultiMinLenIf.java
@@ -0,0 +1,28 @@
+package org.checkerframework.common.value.qual;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.checkerframework.framework.qual.ConditionalPostconditionAnnotation;
+import org.checkerframework.framework.qual.InheritedAnnotation;
+
+/**
+ * An annotation wrapper to allow multiple postcondition annotations.
+ *
+ * <p>Programmers generally do not need to use this; it is created by Java when a programmer writes
+ * more than one {@code @EnsuresMultiMinLenIf} annotation at the same location.
+ */
+@ConditionalPostconditionAnnotation(qualifier = MinLen.class)
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.CONSTRUCTOR})
+@InheritedAnnotation
+public @interface EnsuresMultiMinLenIf {
+    /**
+     * Constitutes the java expression(s) that are a sequence with the given minimum length after
+     * the method returns the given result.
+     *
+     * @checker_framework.manual #java-expressions-as-arguments Syntax of Java expressions
+     */
+    EnsuresMinLenIf[] value();
+}
