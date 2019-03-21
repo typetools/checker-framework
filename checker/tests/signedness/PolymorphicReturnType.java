b0e5ddb25196bc302d3525e70c8497471eae2a13
Michael Ernst <mernst@cs.washington.edu>
2019-03-21 06:19:34 -0700

    Issue #1209 is already closed, so don't skip test [ci skip]

--- checker/tests/signedness/PolymorphicReturnType.java
+++ checker/tests/signedness/PolymorphicReturnType.java
@@ -1,13 +1,11 @@
 // Test case for Issue #1209
 // https://github.com/typetools/checker-framework/issues/1209
 
-// @skip-test until the issue is fixed
-
 import org.checkerframework.checker.signedness.qual.PolySigned;
 
 public class PolymorphicReturnType {
 
-    // :: error: (some.error.goes.here)
+    // :: error: (return.type.incompatible)
     public @PolySigned byte get() {
         return 0;
     }
