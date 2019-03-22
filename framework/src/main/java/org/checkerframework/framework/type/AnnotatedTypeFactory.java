af4d48c26c0c7b23ee8f1a4f9ab6a4d9c911f0d1
Baorui Zhou <b42zhou@uwaterloo.ca>
2019-03-22 16:38:39 -0400

    fix ATF so that viewpointAdaptMember wouldn't get called on executables

--- framework/src/main/java/org/checkerframework/framework/type/AnnotatedTypeFactory.java
+++ framework/src/main/java/org/checkerframework/framework/type/AnnotatedTypeFactory.java
@@ -1376,7 +1376,7 @@ public class AnnotatedTypeFactory implements AnnotationProvider {
             addAnnotationFromFieldInvariant(type, owner, (VariableElement) element);
         }
         addComputedTypeAnnotations(element, type);
-        if (viewpointAdapter != null) {
+        if (viewpointAdapter != null && type.getKind() != TypeKind.EXECUTABLE) {
             viewpointAdapter.viewpointAdaptMember(owner, element, type);
         }
     }
