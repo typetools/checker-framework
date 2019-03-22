0dc341576526b876d3452c806846493361ebbe58
Michael Ernst <mernst@cs.washington.edu>
2019-03-22 15:16:03 -0700

    Test case for issue #2367

---
+++ framework/tests/value/Issue2367.java
@@ -0,0 +1,10 @@
+// Test case for issue #2367: http://tinyurl.com/cfissue/2367
+
+// @skip-test until the issue is fixed
+
+public class Issue2367 {
+    byte b1 = 0x4B;
+    byte b2 = 0x8B;
+    byte b3 = (byte) 0x4B;
+    byte b4 = (byte) 0x8B;
+}
