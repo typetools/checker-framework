5083af666e2c039b230e115e9c871b482b892730
Jeff Luo <j36luo@uwaterloo.ca>
2019-04-11 16:17:33 -0400

    Travis downstream echo (#85)
    * add downstream CFI branch printout for travis log
    
    * update format

--- .travis-build.sh
+++ .travis-build.sh
@@ -120,6 +120,7 @@ if [[ "${GROUP}" == "checker-framework-inference" || "${GROUP}" == "all" ]]; the
   [ -d /tmp/plume-scripts ] || (cd /tmp && git clone --depth 1 https://github.com/plume-lib/plume-scripts.git)
   REPO=`/tmp/plume-scripts/git-find-fork ${SLUGOWNER} typetools checker-framework-inference`
   BRANCH=`/tmp/plume-scripts/git-find-branch ${REPO} ${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}`
+  echo "About to execute: (cd .. && git clone -b $BRANCH --single-branch --depth 1 $REPO)"
   (cd .. && git clone -b ${BRANCH} --single-branch --depth 1 ${REPO}) || (cd .. && git clone -b ${BRANCH} --single-branch --depth 1 ${REPO})
 
   export AFU=`readlink -f ${AFU:-../annotation-tools/annotation-file-utilities}`
