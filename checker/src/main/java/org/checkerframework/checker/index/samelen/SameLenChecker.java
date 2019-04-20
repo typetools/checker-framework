c62705e3ba5669841c35c23f60819b8481997343
Michael Ernst <mernst@cs.washington.edu>
2019-04-20 10:42:50 -0700

    Remove method that just calls super

--- checker/src/main/java/org/checkerframework/checker/index/samelen/SameLenChecker.java
+++ checker/src/main/java/org/checkerframework/checker/index/samelen/SameLenChecker.java
@@ -1,6 +1,5 @@
 package org.checkerframework.checker.index.samelen;
 
-import java.util.LinkedHashSet;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.framework.source.SuppressWarningsKeys;
 
@@ -11,12 +10,4 @@ import org.checkerframework.framework.source.SuppressWarningsKeys;
  * @checker_framework.manual #index-checker Index Checker
  */
 @SuppressWarningsKeys({"index", "samelen"})
-public class SameLenChecker extends BaseTypeChecker {
-
-    @Override
-    protected LinkedHashSet<Class<? extends BaseTypeChecker>> getImmediateSubcheckerClasses() {
-        LinkedHashSet<Class<? extends BaseTypeChecker>> checkers =
-                super.getImmediateSubcheckerClasses();
-        return checkers;
-    }
-}
+public class SameLenChecker extends BaseTypeChecker {}
