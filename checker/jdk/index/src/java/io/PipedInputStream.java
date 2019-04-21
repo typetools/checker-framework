c2dcd7ea22cd53be8926c835e7567a30d71312b9
Maximilian Gama <maxi.gama@yahoo.com>
2019-04-21 13:23:03 +0300

    Fixed annotations

--- checker/jdk/index/src/java/io/PipedInputStream.java
+++ checker/jdk/index/src/java/io/PipedInputStream.java
@@ -155,7 +155,7 @@ public class PipedInputStream extends InputStream {
         initPipe(pipeSize);
     }
 
-    private void initPipe(@NonNegative int pipeSize) {
+    private void initPipe(@Positive int pipeSize) {
          if (pipeSize <= 0) {
             throw new IllegalArgumentException("Pipe Size <= 0");
          }
@@ -366,7 +366,7 @@ public class PipedInputStream extends InputStream {
      *           {@link #connect(java.io.PipedOutputStream) unconnected},
      *           closed, or if an I/O error occurs.
      */
-    public synchronized @GTENegativeOne @LTEqLengthOf("#1") int read(byte b[], @IndexOrHigh("#1") int off, @IndexOrHigh("#1") int len)  throws IOException {
+    public synchronized @GTENegativeOne @LTEqLengthOf("#1") int read(byte b[], @IndexOrHigh("#1") int off, @NonNegative @LTLengthOf(value = "#1", offset = "#2 - 1") int len)  throws IOException {
         if (b == null) {
             throw new NullPointerException();
         } else if (off < 0 || len < 0 || len > b.length - off) {
