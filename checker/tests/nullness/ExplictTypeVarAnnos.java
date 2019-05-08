203bbe77020076179ec2deb425cccf603b99204c
Suzanne Millstein <smillst@cs.washington.edu>
2019-05-08 12:11:28 -0700

    Add more tests.

--- checker/tests/nullness/ExplictTypeVarAnnos.java
+++ checker/tests/nullness/ExplictTypeVarAnnos.java
@@ -1,6 +1,6 @@
 import org.checkerframework.checker.nullness.qual.Nullable;
 
-public class ExplictTypeVarAnnos<E extends @Nullable Object> {
+public class ExplictTypeVarAnnos<E extends @Nullable Object, @Nullable F> {
     interface Consumer<A extends @Nullable Object> {}
 
     public static <B extends @Nullable Object> Consumer<B> cast(
@@ -8,15 +8,27 @@ public class ExplictTypeVarAnnos<E extends @Nullable Object> {
         throw new RuntimeException();
     }
 
-    public static <C extends @Nullable Object> Consumer<C> getConsumer() {
+    public static <C extends @Nullable Object> Consumer<C> getConsumer0() {
         Consumer<@Nullable Object> nullConsumer = null;
         Consumer<C> result = ExplictTypeVarAnnos.<C>cast(nullConsumer);
+        return result;
+    }
+
+    public static <@Nullable D> Consumer<D> getConsumer1() {
+        Consumer<@Nullable Object> nullConsumer = null;
+        Consumer<D> result = ExplictTypeVarAnnos.<D>cast(nullConsumer);
         return result;
     }
 
     public Consumer<E> getConsumer2() {
         Consumer<@Nullable Object> nullConsumer = null;
         Consumer<E> result = ExplictTypeVarAnnos.<E>cast(nullConsumer);
+        return result;
+    }
+
+    public Consumer<F> getConsumer3() {
+        Consumer<@Nullable Object> nullConsumer = null;
+        Consumer<F> result = ExplictTypeVarAnnos.<F>cast(nullConsumer);
         return result;
     }
 }
