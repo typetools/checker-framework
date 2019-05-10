c91cfb0d1d72dbdbc8dac91633cac5e6893e965e
Shubham Raj <shubhamraj1in@gmail.com>
2019-05-10 15:15:44 +0530

    Change indentation
    Signed-off-by: Shubham Raj <shubhamraj1in@gmail.com>

--- checker/tests/nullness/LogRecordTest.java
+++ checker/tests/nullness/LogRecordTest.java
@@ -1,21 +1,33 @@
-import java.util.logging.LogRecord;
+// Test Case for adding nullness annotations to java.util.logging.LogRecord
+// @skip-test
+
 import java.util.logging.Level;
+import java.util.logging.LogRecord;
 
 public class LogRecordTest {
 
-    @SuppressWarnings("initialization.fields.uninitialized") // Consturctor for java.util.logging.Level is protected
+    @SuppressWarnings(
+            "initialization.fields.uninitialized") // Consturctor for java.util.logging.Level is
+    // protected
     Level level;
 
     void test() {
-        
+
         // :: error: (argument.type.incompatible)
-        LogRecord logRecord=new LogRecord(level,null);
+        LogRecord logRecord = new LogRecord(level, null);
+
         logRecord.setLoggerName(null);
+
         logRecord.setResourceBundle(null);
+
         logRecord.setSourceClassName(null);
+
         logRecord.setMessage(null);
+
         logRecord.setSourceMethodName(null);
+
         logRecord.setParameters(null);
+
         logRecord.setThrown(null);
     }
 }
