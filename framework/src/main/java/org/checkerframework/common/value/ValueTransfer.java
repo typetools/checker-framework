be4c58efb801715fdf4b80f7de62871574f647fb
Michael Ernst <mernst@cs.washington.edu>
2019-05-20 08:56:11 -0700

    Remove TODO comment

--- framework/src/main/java/org/checkerframework/common/value/ValueTransfer.java
+++ framework/src/main/java/org/checkerframework/common/value/ValueTransfer.java
@@ -1217,7 +1217,6 @@ public class ValueTransfer extends CFTransfer {
         for (Node internal : splitAssignments(node)) {
             Receiver rec = FlowExpressions.internalReprOf(analysis.getTypeFactory(), internal);
             if (!store.canInsertReceiver(rec)) {
-                // TODO: This skips the calls to refineArray... and refineString... .  Is that OK?
                 continue;
             }
             CFValue currentValueFromStore = store.getValue(rec);
