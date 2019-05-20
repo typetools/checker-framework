9119703ce52759e54215d71116858a19577afb58
Michael Ernst <mernst@cs.washington.edu>
2019-05-19 19:17:56 -0700

    Fix expected errors

--- checker/tests/index/BinomialTest.java
+++ checker/tests/index/BinomialTest.java
@@ -24,7 +24,7 @@ class BinomialTest {
 
     public static void binomial1Error(
             @LTLengthOf("this.factorials") int n, @LessThan("#1 + 1") int k) {
-        // :: error
+        // :: error: (assignment.type.incompatible)
         @LTLengthOf(value = "factorials", offset = "1") int i = k;
     }
 
@@ -34,7 +34,7 @@ class BinomialTest {
 
     public static void binomial2Error(
             @LTLengthOf("this.factorials") int n, @LessThan("#1 + 2") int k) {
-        // :: error
+        // :: error: (assignment.type.incompatible)
         @LTLengthOf(value = "factorials", offset = "0") int i = k;
     }
 
@@ -44,7 +44,7 @@ class BinomialTest {
 
     public static void binomial_1Error(
             @LTLengthOf("this.factorials") int n, @LessThan("#1 - 1") int k) {
-        // :: error
+        // :: error: (assignment.type.incompatible)
         @LTLengthOf(value = "factorials", offset = "3") int i = k;
     }
 
@@ -54,7 +54,7 @@ class BinomialTest {
 
     public static void binomial_2Error(
             @LTLengthOf("this.factorials") int n, @LessThan("#1 - 2") int k) {
-        // :: error
+        // :: error: (assignment.type.incompatible)
         @LTLengthOf(value = "factorials", offset = "4") int i = k;
     }
 }
