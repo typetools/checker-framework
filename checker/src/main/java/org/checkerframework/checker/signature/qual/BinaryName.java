0e7ffeb98a01b679b71ad590f63508482be4f9c6
Michael Ernst <mernst@cs.washington.edu>
2019-05-25 17:05:00 -0700

    Add `@FqBinaryName` type for Signature Checker

--- checker/src/main/java/org/checkerframework/checker/signature/qual/BinaryName.java
+++ checker/src/main/java/org/checkerframework/checker/signature/qual/BinaryName.java
@@ -30,7 +30,7 @@ import org.checkerframework.framework.qual.SubtypeOf;
  *
  * @checker_framework.manual #signature-checker Signature Checker
  */
-@SubtypeOf(ClassGetName.class)
+@SubtypeOf({ClassGetName.class, FqBinaryName.class})
 @ImplicitFor(
         stringPatterns = "^[A-Za-z_][A-Za-z_0-9]*(\\.[A-Za-z_][A-Za-z_0-9]*)*(\\$[A-Za-z_0-9]+)*$")
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
