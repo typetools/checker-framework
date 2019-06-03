698c593585ec19ecd43943122fb52e3b4e4ee08f
Suzanne Millstein <smillst@cs.washington.edu>
2019-06-03 12:55:20 -0700

    Skip for now.

--- checker/src/main/java/org/checkerframework/checker/tainting/TaintingVisitor.java
+++ checker/src/main/java/org/checkerframework/checker/tainting/TaintingVisitor.java
@@ -1,8 +1,10 @@
 package org.checkerframework.checker.tainting;
 
+import com.sun.source.tree.Tree;
 import javax.lang.model.element.ExecutableElement;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.common.basetype.BaseTypeVisitor;
+import org.checkerframework.framework.type.AnnotatedTypeMirror.AnnotatedDeclaredType;
 import org.checkerframework.framework.type.AnnotatedTypeMirror.AnnotatedExecutableType;
 
 public class TaintingVisitor extends BaseTypeVisitor<TaintingAnnotatedTypeFactory> {
@@ -15,5 +17,12 @@ public class TaintingVisitor extends BaseTypeVisitor<TaintingAnnotatedTypeFactor
     protected void checkConstructorResult(
             AnnotatedExecutableType constructorType, ExecutableElement constructorElement) {
         // skip
+    }
+
+    @Override
+    public boolean isValidUse(
+            AnnotatedDeclaredType declarationType, AnnotatedDeclaredType useType, Tree tree) {
+        // skip!
+        return true;
     }
 }
