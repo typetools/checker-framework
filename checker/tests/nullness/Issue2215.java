5ba2f478f929381410d37ede11b2fbdb5811bcba
Shubham Raj <shubhamraj1in@gmail.com>
2019-06-25 04:29:40 +0530

    Remove blank lines
    Signed-off-by: Shubham Raj <shubhamraj1in@gmail.com>

--- checker/tests/nullness/Issue2215.java
+++ checker/tests/nullness/Issue2215.java
@@ -17,27 +17,18 @@ class ParentClass {
 public class Issue2215 extends ParentClass {
 
     @MonotonicNonNull Object f;
-
     @MonotonicNonNull Object g = null;
-
     @MonotonicNonNull Object h = new Object();
 
     Issue2215(@Nullable Object o) {
 
         super(o); // should be allowed
-
         this.parentClassField = o; // should be allowed
-
         this.f = o; // should be allowed
-
         this.f = null; // should be allowed
-
         this.g = o; // should be allowed
-
         this.g = null; // should be allowed
-
         this.h = o; // should not be allowed
-
         this.h = null; // should not be allowed
     }
 }
