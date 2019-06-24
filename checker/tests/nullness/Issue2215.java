05cdb5a703b4769e607913756241daec221a635a
Shubham Raj <shubhamraj1in@gmail.com>
2019-06-25 03:26:35 +0530

    Add some more test cases
    Signed-off-by: Shubham Raj <shubhamraj1in@gmail.com>

--- checker/tests/nullness/Issue2215.java
+++ checker/tests/nullness/Issue2215.java
@@ -5,14 +5,39 @@
 import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
-public class Issue2215 {
+class ParentClass {
+
+    @MonotonicNonNull Object parentClassField;
+
+    ParentClass(Object o) {
+        this.parentClassField = o; // should be allowed
+    }
+}
+
+public class Issue2215 extends ParentClass {
 
     @MonotonicNonNull Object f;
 
+    @MonotonicNonNull Object g = null;
+
+    @MonotonicNonNull Object h = new Object();
+
     Issue2215(@Nullable Object o) {
+
+        super(o); // should be allowed
+
+        this.parentClassField = o; // should be allowed
 
         this.f = o; // should be allowed
 
         this.f = null; // should be allowed
+
+        this.g = o; // should be allowed
+
+        this.g = null; // should be allowed
+
+        this.h = o; // should not be allowed
+
+        this.h = null; // should not be allowed
     }
 }
