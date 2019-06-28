de2eb58642fde5e3381db5969cecedc658503392
Maxi17 <maxi.gama@yahoo.com>
2019-06-28 16:24:10 +0300

    Fix `@LessThan` false warning on OffsetEquation parameters (#2580)
    Before checking for valid casting, @LessThan arguments are simplified if possible to avoid [cast.unsafe] false warnings.
    
    Fixes #2541

--- checker/tests/index/LessThanConstantAddition.java
+++ checker/tests/index/LessThanConstantAddition.java
@@ -1,7 +1,5 @@
 // Test case for issue #2541: https://github.com/typetools/checker-framework/issues/2541
 
-// @skip-test until the issue is fixed
-
 public class LessThanConstantAddition {
 
     public static void checkedPow(int b) {
