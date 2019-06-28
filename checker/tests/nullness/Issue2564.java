78d4f255c92d95b539d312907d5f722878631ccc
Suzanne Millstein <smillst@cs.washington.edu>
2019-06-28 10:47:08 -0700

    Fix enum constants.

--- checker/tests/nullness/Issue2564.java
+++ checker/tests/nullness/Issue2564.java
@@ -2,15 +2,11 @@ import java.util.HashMap;
 import java.util.Map;
 import org.checkerframework.checker.nullness.qual.KeyFor;
 
-// https://github.com/typetools/checker-framework/issues/2587
-// Issue 2587 tracks the missing errors.
-@SuppressWarnings("") // Just check for crashes.
 public abstract class Issue2564 {
     public enum EnumType {
-        // TODO:: error: (expression.unparsable.type.invalid) :: error:
-        // (assignment.type.incompatible)
+        // :: error: (assignment.type.incompatible)
         @KeyFor("myMap") MY_KEY,
-        // TODO:: error: (assignment.type.incompatible)
+        // :: error: (assignment.type.incompatible)
         @KeyFor("enumMap") ENUM_KEY;
         private static final Map<String, Integer> enumMap = new HashMap<>();
 
@@ -20,10 +16,5 @@ public abstract class Issue2564 {
         }
     }
 
-    public static class Inner {
-        // TODO:: error: (expression.unparsable.type.invalid)
-        @KeyFor("myMap") String MY_KEY = "";
-    }
-
-    private final Map<String, Integer> myMap = new HashMap<>();
+    private static final Map<String, Integer> myMap = new HashMap<>();
 }
