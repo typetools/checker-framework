a54c41358d7778a8d69fbf003d5d33badc1d7856
Suzanne Millstein <smillst@cs.washington.edu>
2019-06-28 10:24:37 -0700

    Update test case.

--- checker/tests/nullness/Issue2564.java
+++ checker/tests/nullness/Issue2564.java
@@ -2,10 +2,15 @@ import java.util.HashMap;
 import java.util.Map;
 import org.checkerframework.checker.nullness.qual.KeyFor;
 
+// https://github.com/typetools/checker-framework/issues/2587
+// Issue 2587 tracks the missing errors.
+@SuppressWarnings("") // Just check for crashes.
 public abstract class Issue2564 {
     public enum EnumType {
-        // :: error: (expression.unparsable.type.invalid)
+        // TODO:: error: (expression.unparsable.type.invalid) :: error:
+        // (assignment.type.incompatible)
         @KeyFor("myMap") MY_KEY,
+        // TODO:: error: (assignment.type.incompatible)
         @KeyFor("enumMap") ENUM_KEY;
         private static final Map<String, Integer> enumMap = new HashMap<>();
 
@@ -16,7 +21,7 @@ public abstract class Issue2564 {
     }
 
     public static class Inner {
-
+        // TODO:: error: (expression.unparsable.type.invalid)
         @KeyFor("myMap") String MY_KEY = "";
     }
 
