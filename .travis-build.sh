c8368f3e74bad64942ab005f5dd0acbdb604420a
Suzanne Millstein <smillst@cs.washington.edu>
2019-07-08 10:30:36 -0700

    Add task to print jdk8.jar manifest

--- .travis-build.sh
+++ .travis-build.sh
@@ -70,7 +70,7 @@ fi
 
 if [[ "${GROUP}" == "jdk.jar" || "${GROUP}" == "all" ]]; then
   ## Run the tests for the type systems that use the annotated JDK
-  ./gradlew IndexTest LockTest NullnessFbcTest OptionalTest -PuseLocalJdk --console=plain --warning-mode=all --no-daemon
+  ./gradlew IndexTest LockTest NullnessFbcTest OptionalTest printJdkJarManifest -PuseLocalJdk --console=plain --warning-mode=all --no-daemon
 fi
 
 if [[ "${GROUP}" == "misc" || "${GROUP}" == "all" ]]; then
