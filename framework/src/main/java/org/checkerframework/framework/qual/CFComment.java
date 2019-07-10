4506f7996c9ea8db5cc30c2e56db2168cb6e6193
Shubham Raj <shubhamraj1in@gmail.com>
2019-07-10 21:15:53 +0530

    Reformat
    Signed-off-by: Shubham Raj <shubhamraj1in@gmail.com>

--- framework/src/main/java/org/checkerframework/framework/qual/CFComment.java
+++ framework/src/main/java/org/checkerframework/framework/qual/CFComment.java
@@ -1,21 +1,22 @@
 package org.checkerframework.framework.qual;
 
-import java.lang.annotation.Documented;
 import static java.lang.annotation.ElementType.*;
+
+import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * <p>This annotation is to be used to add comments related to
- * Checker-Framework. This annotation can be used on packages, classes, interfaces and
- * any elements inside these.
- * 
- * <p>Each comment should first start with the type-system to which comment is related to followed by ':' and then actual comment.
- * Provide single string if comment is for only one type-system and  multiple string for separate type-system.
- * 
+ * This annotation is to be used to add comments related to Checker-Framework. This annotation can
+ * be used on packages, classes, interfaces and any elements inside these.
+ *
+ * <p>Each comment should first start with the type-system to which comment is related to followed
+ * by ':' and then actual comment. Provide single string if comment is for only one type-system and
+ * multiple string for separate type-system.
+ *
  * <p>Here is an example:
- * 
+ *
  * <pre><code>
  * {@literal @}CFComment("type-system:some comment")
  * public class {
@@ -25,19 +26,16 @@ import java.lang.annotation.Target;
  *     }
  * }
  * </code></pre>
- * 
- * <p>As a matter of style, programmers should always use this annotation
- * on the most deeply nested element where the comment related to Checker-Framework
- * is to be provided.
- * 
+ *
+ * <p>As a matter of style, programmers should always use this annotation on the most deeply nested
+ * element where the comment related to Checker-Framework is to be provided.
+ *
  * <p>These annotations are {@link Documented} and hence will appear in Javadocs.
  */
 @Documented
 @Target({TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, LOCAL_VARIABLE, PACKAGE})
 @Retention(RetentionPolicy.SOURCE)
 public @interface CFComment {
-    /**
-     * @return array of {@link String} containing comments
-     */
+    /** @return array of {@link String} containing comments */
     String[] value();
 }
