2f50bf7ed76e497ea55daea12d7efed1f1a099fd
Michael Ernst <mernst@cs.washington.edu>
2019-07-10 05:53:11 -0700

    Add test case

---
+++ checker/tests/signedness/Issue2534.java
@@ -0,0 +1,23 @@
+import org.checkerframework.checker.signedness.qual.Unsigned;
+import org.checkerframework.common.value.qual.IntRange;
+
+class Issue2534 {
+
+    @IntRange(from = 0, to = Integer.MAX_VALUE) int field = 3;
+
+    @IntRange(from = 0, to = Integer.MAX_VALUE) int qwe() {
+        return 3;
+    }
+
+    void m1() {
+        @Unsigned int c = qwe();
+    }
+
+    void m2() {
+        @Unsigned int c = field;
+    }
+
+    void m3() {
+        @Unsigned int c = this.field;
+    }
+}
