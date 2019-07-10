9593e35b2c8b65ea1f9d82263460060532696703
Michael Ernst <mernst@cs.washington.edu>
2019-07-10 05:56:25 -0700

    Add Javadoc

--- checker/src/main/java/org/checkerframework/checker/signedness/SignednessAnnotatedTypeFactory.java
+++ checker/src/main/java/org/checkerframework/checker/signedness/SignednessAnnotatedTypeFactory.java
@@ -79,6 +79,10 @@ public class SignednessAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
         super.addComputedTypeAnnotations(tree, type, iUseFlow);
     }
 
+    /**
+     * If the tree is a local variable and the type is byte, short, int, or long, then add the
+     * UnknownSignedness annotation so that dataflow can refine it.
+     */
     private void addSignednessAnnotation(Tree tree, AnnotatedTypeMirror type) {
         TypeMirror javaType = type.getUnderlyingType();
         TypeKind javaTypeKind = javaType.getKind();
