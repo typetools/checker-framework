8a0798defcd6f3af1c9ed7fff3862efe1d58c379
Michael Ernst <mernst@cs.washington.edu>
2019-07-11 12:03:17 -0700

    Improve comments

--- framework/src/main/java/org/checkerframework/framework/qual/CFComment.java
+++ framework/src/main/java/org/checkerframework/framework/qual/CFComment.java
@@ -5,38 +5,32 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * This annotation is to be used to add comments related to the Checker Framework.
+ * This annotation is for comments related to the Checker Framework.
  *
- * <p>Purpose of {@code @CFComment} annotation is:
- *
- * <ul>
- *   <li>To make it easy to find Checker-Framework-related comments in a large codebase.
- *   <li>To make it easy to copy those comments from one codebase to another, using the Annotation
- *       File Utilities.
- * </ul>
- *
- * <p>While using {@code @CFComment} it is recommended that each comment should first start with the
- * type-system to which comment is related to followed by ':' and then actual comment. A user could
- * also break a long comment across lines, by putting it in multiple strings.
+ * <p>Use of {@code @CFComment} makes it easy to find Checker-Framework-related comments, and to
+ * copy those comments from one version of a codebase to another, using the Annotation File
+ * Utilities.
  *
  * <p>Here is an example:
  *
  * <pre><code>
- * {@literal @}CFComment("type-system:some comment")
- * public class {
- *     {@literal @}CFComment({"nullness:Return type should be nullable",
- *      "signedness:Comment related to Signedness type-system"})
- *     public String test{
- *         return "test";
- *     }
+ * {@literal @}CFComment("interning: factory methods guarantee that all elements are interned")
+ *  public class MyClass {
+ *   {@literal @}CFComment({"nullness: non-null return type is more specific than in superclass",
+ *                "signedness: comment related to Signedness type system"})
+ *    public String myMethod(){ ... }
  * }
  * </code></pre>
  *
- * <p>As a matter of style, programmers should always use this annotation on the most deeply nested
- * element where the comment related to Checker-Framework is to be provided.
+ * <p>As a matter of style, programmers should use this annotation on the most deeply nested element
+ * to which the comment applies.
+ *
+ * <p>It is opitional to prefixing each comment by the name of the type system. Doing so makes the
+ * comment is about makes the comments easier to understand and search for.
  */
 @Documented
 @Retention(RetentionPolicy.SOURCE)
 public @interface CFComment {
+    /** Comments about Checker Framework annotations. */
     String[] value();
 }
