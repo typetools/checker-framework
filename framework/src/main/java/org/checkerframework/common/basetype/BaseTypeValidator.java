25419fb0895089fbf0da943dc50d67cc62bfa11f
xingweitian <13183370+xingweitian@users.noreply.github.com>
2019-07-11 16:21:36 -0400

    Get the erased type.

--- framework/src/main/java/org/checkerframework/common/basetype/BaseTypeValidator.java
+++ framework/src/main/java/org/checkerframework/common/basetype/BaseTypeValidator.java
@@ -183,9 +183,8 @@ public class BaseTypeValidator extends AnnotatedTypeScanner<Void, Tree> implemen
             final @CompilerMessageKey String errorType,
             final AnnotatedTypeMirror type,
             final Tree p) {
-        // TODO: if underlying is a compound type such as List<@Palindrome String>, then it would be
-        // nice to print it as "List" instead of as "List<@Palindrome String>".
-        TypeMirror unannotated = TypeAnnotationUtils.unannotatedType(type.getUnderlyingType());
+        TypeMirror unannotated =
+                TypeAnnotationUtils.unannotatedType(type.getErased().getUnderlyingType());
         checker.report(Result.failure(errorType, type.getAnnotations(), unannotated.toString()), p);
         isValid = false;
     }
