91aa91cba87447328d2b55897606014695fe2953
Suzanne Millstein <smillst@cs.washington.edu>
2019-07-15 13:43:17 -0700

    More tests.

--- checker/tests/nullness/Issue2619.java
+++ checker/tests/nullness/Issue2619.java
@@ -5,10 +5,23 @@ import org.checkerframework.checker.nullness.qual.KeyFor;
 import org.checkerframework.dataflow.qual.Pure;
 
 public class Issue2619 {
+    public Map<String, String> map = new HashMap<>();
 
     void m01(Aux aux1, Aux aux2) {
         if (aux1.hasValue(Aux.MINIMUM_VALUE) && aux2.hasValue(Aux.MINIMUM_VALUE)) {
             @KeyFor({"aux1.map", "aux2.map"}) String s1 = Aux.MINIMUM_VALUE;
+        }
+    }
+
+    void m02(Aux aux1, Aux aux2) {
+        if (aux1.hasValue(Aux.MINIMUM_VALUE) && map.containsKey(Aux.MINIMUM_VALUE)) {
+            @KeyFor({"aux1.map", "map"}) String s1 = Aux.MINIMUM_VALUE;
+        }
+    }
+
+    void m03(Aux aux1, Aux aux2) {
+        if (map.containsKey(Aux.MINIMUM_VALUE) && aux1.hasValue(Aux.MINIMUM_VALUE)) {
+            @KeyFor({"aux1.map", "map"}) String s1 = Aux.MINIMUM_VALUE;
         }
     }
 
