75033cc4ed92e0971b0740e24debf585d31396d7
Maximilian Gama <maxi.gama@yahoo.com>
2019-07-16 20:33:19 +0300

    Moved code to correct place

--- framework/src/main/java/org/checkerframework/framework/util/dependenttypes/DependentTypesHelper.java
+++ framework/src/main/java/org/checkerframework/framework/util/dependenttypes/DependentTypesHelper.java
@@ -480,14 +480,6 @@ public class DependentTypesHelper {
             if (result == null) {
                 return new DependentTypesError(expression, " ").toString();
             }
-            if (result instanceof FlowExpressions.FieldAccess
-                    && ((FlowExpressions.FieldAccess) result).isFinal()) {
-                Object constant =
-                        ((FlowExpressions.FieldAccess) result).getField().getConstantValue();
-                if (constant != null && !(constant instanceof String)) {
-                    return constant.toString();
-                }
-            }
             return result.toString();
         } catch (FlowExpressionParseUtil.FlowExpressionParseException e) {
             return new DependentTypesError(expression, e).toString();
