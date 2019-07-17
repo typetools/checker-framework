f98b613c2d59056edac593f3d337ddeb7fc3944e
xingweitian <13183370+xingweitian@users.noreply.github.com>
2019-07-17 16:16:51 -0400

    Tweaks.

--- framework/src/main/java/org/checkerframework/common/basetype/BaseTypeValidator.java
+++ framework/src/main/java/org/checkerframework/common/basetype/BaseTypeValidator.java
@@ -177,9 +177,9 @@ public class BaseTypeValidator extends AnnotatedTypeScanner<Void, Tree> implemen
      * Like {@link #reportValidityResult}, but the type is printed in the error message without
      * annotations. This method would print "annotation @NonNull is not permitted on type int",
      * whereas {@link #reportValidityResult} would print "annotation @NonNull is not permitted on
-     * type @NonNull int". In addition, when underlying is a compound type such as @Bad
-     * List&lt;String&gt;, the erased type will be used, i.e., "List" will be print instead of "@Bad
-     * List&lt;String&gt;".
+     * type @NonNull int". In addition, when the underlying type is a compound type such as
+     * {@code @Bad List<String>}, the erased type will be used, i.e., "{@code List}" will be print
+     * instead of "{@code @Bad List<String>}".
      */
     protected void reportValidityResultOnUnannotatedType(
             final @CompilerMessageKey String errorType,
