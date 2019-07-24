b9eb1795b124c4f8283b574e76180a04b1fd713d
Maximilian Gama <maxi.gama@yahoo.com>
2019-07-24 22:17:35 +0300

    Added char test case

--- framework/tests/flowexpression/DoubleParsing.java
+++
@@ -1,9 +0,0 @@
-package flowexpression;
-
-import testlib.flowexpression.qual.FlowExp;
-
-public class DoubleParsing {
-    void method(@FlowExp("1.0") Object doubleValue) {
-        @FlowExp("1.0") Object value = doubleValue;
-    }
-}
