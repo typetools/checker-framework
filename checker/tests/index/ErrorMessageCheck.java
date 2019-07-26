7ef563ebd0db76e68bd4177239281ca3414dc99c
Maximilian Gama <maxi.gama@yahoo.com>
2019-07-26 15:01:52 +0300

    Improved error messages

--- checker/tests/index/ErrorMessageCheck.java
+++ checker/tests/index/ErrorMessageCheck.java
@@ -7,7 +7,7 @@ public class ErrorMessageCheck {
     void method3(@NonNegative int size, @NonNegative int value) {
         this.size = size;
         this.vDown = new int[this.size];
-        // :: error : (array.access.unsafe.high)
+        // :: error: (array.access.unsafe.high)
         vDown[1 + value] = 10;
     }
 }
