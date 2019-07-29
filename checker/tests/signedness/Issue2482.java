1ae4cbaa6e0d8c248bd3c7712e6fd68df192721d
Kartikeya Goswami <kartikeyagoswami20@gmail.com>
2019-07-29 22:12:24 +0530

    Add test case

---
+++ checker/tests/signedness/Issue2482.java
@@ -0,0 +1,40 @@
+import org.checkerframework.checker.signedness.qual.*;
+
+class Issue2482 {
+
+    void m1(String s, int a) {
+
+        int len = s.length();
+
+        len += a;
+        len -= a;
+        len /= a;
+        len *= a;
+    }
+
+    void m2(byte[] b, int a) {
+
+        int len = b.length;
+
+        len += a;
+        len -= a;
+        len /= a;
+        len *= a;
+    }
+
+    void m2(int a) {
+
+        int len = -1; // Negative
+        int len2 = 1; // Positive
+
+        len += a;
+        len -= a;
+        len /= a;
+        len *= a;
+
+        len2 += a;
+        len2 -= a;
+        len2 /= a;
+        len2 *= a;
+    }
+}
