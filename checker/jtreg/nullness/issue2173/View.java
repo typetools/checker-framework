fae0c1a320642d07822a05a7b6380452d5aaedc5
Suzanne Millstein <smillst@cs.washington.edu>
2019-08-01 14:52:15 -0700

    Update jtreg test.

--- checker/jtreg/nullness/issue2173/View.java
+++ checker/jtreg/nullness/issue2173/View.java
@@ -3,7 +3,8 @@
  * @summary Test case for issue #2173: https://github.com/typetools/checker-framework/issues/2173
  * See README in this directory.
  *
- * @compile -processor org.checkerframework.checker.nullness.NullnessChecker View.java
+ * @compile/ref=View.out -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker View.java -Anomsgtext
+ * @compile -processor org.checkerframework.checker.nullness.NullnessChecker View.java -AignoreInvalidAnnotationLocations -Werror
  */
 
 public class View {
