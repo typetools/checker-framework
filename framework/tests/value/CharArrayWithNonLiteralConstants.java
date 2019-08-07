2c7d47ebb342e108c724d008268a6aee2bcb62de
Maximilian Gama <maxi.gama@yahoo.com>
2019-08-07 14:38:14 +0300

    Renamed test case, simplified code

--- framework/tests/value/CharArrayWithNonLiteralConstants.java
+++ framework/tests/value/CharArrayWithNonLiteralConstants.java
@@ -1,7 +1,7 @@
 import org.checkerframework.common.value.qual.IntVal;
 import org.checkerframework.common.value.qual.StringVal;
 
-public class ByteArrayWithNonLiteralConstants {
+public class CharArrayWithNonLiteralConstants {
 
     public static void main(String[] args) {
         char @StringVal("hello") [] greeting1 = {'h', 'e', 'l', 'l', 'o'};
