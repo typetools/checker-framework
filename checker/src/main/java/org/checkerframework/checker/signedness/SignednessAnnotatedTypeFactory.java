6211012257563fba3ec001f1a10b519691758a15
Kartikeya Goswami <kartikeyagoswami20@gmail.com>
2019-08-08 20:26:56 +0530

    Reformat

--- checker/src/main/java/org/checkerframework/checker/signedness/SignednessAnnotatedTypeFactory.java
+++ checker/src/main/java/org/checkerframework/checker/signedness/SignednessAnnotatedTypeFactory.java
@@ -115,12 +115,11 @@ public class SignednessAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
                         ExpressionTree expr = t.getExpression();
                         AnnotatedTypeMirror varType = getAnnotatedType(var);
                         AnnotatedTypeMirror exprType = getAnnotatedType(expr);
-                        AnnotatedTypeMirror lht = getAnnotatedType(var);
                         if (!(varType.hasAnnotation(Unsigned.class)
                                         && exprType.hasAnnotation(Signed.class))
                                 && !(varType.hasAnnotation(Signed.class)
                                         && exprType.hasAnnotation(Unsigned.class)))
-                            type.replaceAnnotations(lht.getAnnotations());
+                            type.replaceAnnotations(varType.getAnnotations());
                     } else {
                         if (treeRange != null) {
                             switch (javaType.getKind()) {
