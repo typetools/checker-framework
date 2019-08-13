98cb4c6b105764a89ba2301f2443a088bcf61a47
Maximilian Gama <maxi.gama@yahoo.com>
2019-08-14 01:53:45 +0300

    Requested change

--- framework/src/main/java/org/checkerframework/framework/util/FlowExpressionParseUtil.java
+++ framework/src/main/java/org/checkerframework/framework/util/FlowExpressionParseUtil.java
@@ -38,11 +38,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.annotation.processing.ProcessingEnvironment;
-import javax.lang.model.element.Element;
-import javax.lang.model.element.ElementKind;
-import javax.lang.model.element.ExecutableElement;
-import javax.lang.model.element.Modifier;
-import javax.lang.model.element.VariableElement;
+import javax.lang.model.element.*;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
@@ -66,10 +62,7 @@ import org.checkerframework.dataflow.cfg.node.ObjectCreationNode;
 import org.checkerframework.framework.source.Result;
 import org.checkerframework.framework.type.AnnotatedTypeFactory;
 import org.checkerframework.framework.util.dependenttypes.DependentTypesError;
-import org.checkerframework.javacutil.ElementUtils;
-import org.checkerframework.javacutil.Resolver;
-import org.checkerframework.javacutil.TreeUtils;
-import org.checkerframework.javacutil.TypesUtils;
+import org.checkerframework.javacutil.*;
 import org.checkerframework.javacutil.trees.TreeBuilder;
 
 /**
@@ -309,10 +302,8 @@ public class FlowExpressionParseUtil {
             if (fieldElem != null && fieldElem.getKind() == ElementKind.FIELD) {
                 FieldAccess fieldAccess =
                         (FieldAccess) getReceiverField(s, context, originalReceiver, fieldElem);
-                String className = fieldAccess.getReceiver().getType().toString();
-                Element scopeClassElement =
-                        resolver.findClass(
-                                className.substring(className.lastIndexOf(".") + 1), path);
+                TypeElement scopeClassElement =
+                        TypesUtils.getTypeElement(fieldAccess.getReceiver().getType());
                 if (!originalReceiver
                         && !ElementUtils.isStatic(fieldElem)
                         && ElementUtils.isStatic(scopeClassElement)) {
