58bd31519521568b3429cee621fa04ba185638cc
Suzanne Millstein <smillst@cs.washington.edu>
2019-08-14 14:37:17 -0700

    Revert support of .java files as stub files. (#41)

--- checker/jtreg/stubs/Issue1542Driver.java
+++ checker/jtreg/stubs/Issue1542Driver.java
@@ -3,7 +3,7 @@
  * @summary Test case for Issue 1542 https://github.com/typetools/checker-framework/issues/1542
  *
  * @compile -XDrawDiagnostics issue1542/NeedsIntRange.java issue1542/Stub.java issue1542/ExampleAnno.java
- * @compile -XDrawDiagnostics -processor org.checkerframework.common.value.ValueChecker issue1542/UsesIntRange.java -Astubs=issue1542/NeedsIntRange.astub:issue1542/Stub.astub -AstubWarnIfNotFound -Werror
+ * @compile -XDrawDiagnostics -processor org.checkerframework.common.value.ValueChecker issue1542/UsesIntRange.java -Astubs=issue1542 -AstubWarnIfNotFound -Werror
  */
 
 public class Issue1542Driver {}
