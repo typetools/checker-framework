9f1f84923737f4e9b4907e373c34b31fbd6a89cd
Suzanne Millstein <smillst@cs.washington.edu>
2019-08-21 10:11:51 -0700

    Just check for crashes.

--- checker/tests/nullness/Issue2721.java
+++ checker/tests/nullness/Issue2721.java
@@ -2,6 +2,7 @@ import static java.util.Arrays.asList;
 
 import java.util.List;
 
+@SuppressWarnings("") // Just check for crashed.
 class Issue2721 {
     void foo() {
         passThrough(asList(asList(1))).get(0).get(0).intValue();
