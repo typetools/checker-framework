093aa724dffdcd809b336d2530377a0d4e025a0e
Suzanne Millstein <smillst@cs.washington.edu>
2019-08-21 15:26:13 -0700

    Don't skip tests that hide a regression.

--- checker/tests/index/PlumeFailMin.java
+++ checker/tests/index/PlumeFailMin.java
@@ -1,8 +1,6 @@
 // Test case for eisop Issue 22:
 // https://github.com/eisop/checker-framework/issues/22
 
-// @skip-test until the issue is fixed
-
 import org.checkerframework.checker.index.qual.IndexOrHigh;
 import org.checkerframework.common.value.qual.MinLen;
 
