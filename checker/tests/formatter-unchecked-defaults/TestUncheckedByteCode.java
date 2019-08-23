d7dddd35f5ae660bc4127c03f1444a04543dd4ba
Kartikeya Goswami <kartikeyagoswami20@gmail.com>
2019-08-24 00:40:09 +0530

    Correct tests

--- checker/tests/formatter-unchecked-defaults/TestUncheckedByteCode.java
+++ checker/tests/formatter-unchecked-defaults/TestUncheckedByteCode.java
@@ -8,6 +8,8 @@ public class TestUncheckedByteCode {
         field = param.getInt(1);
         field = param.getInteger(i);
         field = param.getObject(new Object());
+        // Strings are relevant and must be annotated in bytecode
+        // ::error: (argument.type.incompatible)
         field = param.getString("hello");
         field = param.identity("hello");
     }
--- checker/tests/signedness-unchecked-defaults/TestUncheckedByteCode.java
+++ checker/tests/signedness-unchecked-defaults/TestUncheckedByteCode.java
@@ -6,9 +6,6 @@ public class TestUncheckedByteCode {
     void test(UncheckedByteCode<Object> param, Integer i) {
         field = param.getCT();
         field = param.getInt(1);
-        // Signedness Checker doesn't default boxed primitives correctly.
-        // https://github.com/typetools/checker-framework/issues/797
-        // :: error: (argument.type.incompatible)
         field = param.getInteger(i);
         field = param.getObject(new Object());
         field = param.getString("hello");
