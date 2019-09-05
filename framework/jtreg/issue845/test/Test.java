7c079e77ec098fcd949052579b84cce9896cc663
Werner Dietl <wdietl@gmail.com>
2019-09-05 17:48:52 -0400

    Undo experiment.

--- framework/jtreg/issue845/test/Test.java
+++ framework/jtreg/issue845/test/Test.java
@@ -3,6 +3,6 @@
  * @summary Testing that a checker isn't required to be in a package. (Issue 845, https://github.com/typetools/checker-framework/issues/845)  The checker java files have to be in a separate directory otherwise $CHECKERFRAMEWORK/framework/jtreg/checker/qual directory is on the classpath before the qual directory with the compiled classes.
  *
  * @compile -XDrawDiagnostics -Xlint:unchecked ../checker/NotInPackageChecker.java ../checker/qual/NotInPackageTop.java
- * @compile -XDrawDiagnostics -Xlint:unchecked -classpath $work -processor NotInPackageChecker Test.java -proc:only
+ * @compile -XDrawDiagnostics -Xlint:unchecked -processor NotInPackageChecker Test.java -proc:only
  */
 class Test {}
