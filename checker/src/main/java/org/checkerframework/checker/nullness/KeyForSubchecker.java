0fed2cd1a360d3e9c2e2f2230389cef936943361
Suzanne Millstein <smillst@cs.washington.edu>
2019-09-06 12:21:33 -0700

    Hacks to use jdk as stubfiles.

--- checker/src/main/java/org/checkerframework/checker/nullness/KeyForSubchecker.java
+++ checker/src/main/java/org/checkerframework/checker/nullness/KeyForSubchecker.java
@@ -1,6 +1,7 @@
 package org.checkerframework.checker.nullness;
 
 import org.checkerframework.common.basetype.BaseTypeChecker;
+import org.checkerframework.framework.qual.StubFiles;
 
 /**
  * A type-checker for determining which values are keys for which maps. Typically used as part of
@@ -9,4 +10,5 @@ import org.checkerframework.common.basetype.BaseTypeChecker;
  * @checker_framework.manual #map-key-checker Map Key Checker
  * @checker_framework.manual #nullness-checker Nullness Checker
  */
+@StubFiles("/Users/smillst/jsr308/checker-framework/checker/jdk/nullness/src")
 public class KeyForSubchecker extends BaseTypeChecker {}
--- checker/src/main/java/org/checkerframework/checker/nullness/NullnessChecker.java
+++ checker/src/main/java/org/checkerframework/checker/nullness/NullnessChecker.java
@@ -7,6 +7,7 @@ import org.checkerframework.checker.initialization.InitializationChecker;
 import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.common.basetype.BaseTypeVisitor;
+import org.checkerframework.framework.qual.StubFiles;
 import org.checkerframework.framework.source.SupportedLintOptions;
 
 /**
@@ -29,6 +30,7 @@ import org.checkerframework.framework.source.SupportedLintOptions;
     // See issue #986: https://github.com/typetools/checker-framework/issues/986
     "forbidnonnullarraycomponents"
 })
+@StubFiles("/Users/smillst/jsr308/checker-framework/checker/jdk/nullness/src")
 public class NullnessChecker extends InitializationChecker {
 
     /** Should we be strict about initialization of {@link MonotonicNonNull} variables. */
