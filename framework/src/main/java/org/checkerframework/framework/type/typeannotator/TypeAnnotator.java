bb064dc26f1f729e2d3d87c262f79ed66c55873a
Suzanne Millstein <smillst@cs.washington.edu>
2019-09-16 10:23:30 -0700

    Document how to change the type of parameters.

--- framework/src/main/java/org/checkerframework/framework/type/typeannotator/TypeAnnotator.java
+++ framework/src/main/java/org/checkerframework/framework/type/typeannotator/TypeAnnotator.java
@@ -1,11 +1,17 @@
 package org.checkerframework.framework.type.typeannotator;
 
+import javax.lang.model.element.Element;
 import org.checkerframework.framework.type.AnnotatedTypeFactory;
+import org.checkerframework.framework.type.AnnotatedTypeMirror;
+import org.checkerframework.framework.type.AnnotatedTypeMirror.AnnotatedExecutableType;
 import org.checkerframework.framework.type.visitor.AnnotatedTypeScanner;
 
 /**
  * {@link TypeAnnotator} is an abstract AnnotatedTypeScanner to be used with {@link
  * ListTypeAnnotator}.
+ *
+ * <p>When implementing this class, each method should return {@code null} instead of calling super,
+ * because the super implementation does nothing.
  *
  * @see org.checkerframework.framework.type.typeannotator.ListTypeAnnotator
  * @see org.checkerframework.framework.type.typeannotator.PropagationTypeAnnotator
@@ -17,5 +23,16 @@ public abstract class TypeAnnotator extends AnnotatedTypeScanner<Void, Void> {
 
     public TypeAnnotator(AnnotatedTypeFactory typeFactory) {
         this.typeFactory = typeFactory;
+    }
+
+    /**
+     * If this method adds annotations to the type of method parameters, then {@link
+     * org.checkerframework.framework.type.GenericAnnotatedTypeFactory#addComputedTypeAnnotations(Element,
+     * AnnotatedTypeMirror)} should be overriden and the same annotations added to the type of
+     * elements with kind {@link javax.lang.model.element.ElementKind#PARAMETER}.
+     */
+    @Override
+    public Void visitExecutable(AnnotatedExecutableType method, Void aVoid) {
+        return super.visitExecutable(method, aVoid);
     }
 }
