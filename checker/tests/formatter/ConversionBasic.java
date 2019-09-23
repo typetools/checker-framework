ebb1d4462c7637ec294c4d566e63f016cfe86271
Nhat Nguyen <hoangnhat2911@gmail.com>
2019-09-23 17:17:45 -0400

    Fix tests using APIs that are deprecated in JDK11 (#2771)

--- checker/tests/formatter/ConversionBasic.java
+++ checker/tests/formatter/ConversionBasic.java
@@ -17,7 +17,7 @@ public class ConversionBasic {
         f.format("%b", 4);
         f.format("%B", 7.5);
         f.format("%h", new Date());
-        f.format("%H", new Integer(4));
+        f.format("%H", Integer.valueOf(4));
         f.format("%s", new ArrayList<Integer>());
 
         // test CHAR
@@ -25,10 +25,10 @@ public class ConversionBasic {
         f.format("%c", 'c');
         f.format("%c", (byte) 67);
         f.format("%c", (int) 67);
-        f.format("%c", new Character('c'));
-        f.format("%c", new Byte((byte) 67));
-        f.format("%c", new Short((short) 67));
-        f.format("%C", new Integer(67));
+        f.format("%c", Character.valueOf('c'));
+        f.format("%c", Byte.valueOf((byte) 67));
+        f.format("%c", Short.valueOf((short) 67));
+        f.format("%C", Integer.valueOf(67));
         // :: error: (argument.type.incompatible)
         f.format("%c", 7.5);
         // :: error: (argument.type.incompatible)
@@ -40,7 +40,7 @@ public class ConversionBasic {
         f.format("%o", (short) 67);
         f.format("%x", (int) 67);
         f.format("%X", (long) 67);
-        f.format("%d", new Long(67));
+        f.format("%d", Long.valueOf(67));
         f.format("%d", BigInteger.ONE);
         // :: error: (argument.type.incompatible)
         f.format("%d", 'c');
@@ -51,8 +51,8 @@ public class ConversionBasic {
         @Format({ConversionCategory.FLOAT}) String d = "%f";
         f.format("%e", (float) 67.1);
         f.format("%E", (double) 67.3);
-        f.format("%f", new Float(42.5));
-        f.format("%g", new Double(42.5));
+        f.format("%f", Float.valueOf(42.5f));
+        f.format("%g", Double.valueOf(42.5));
         f.format("%G", 67.87);
         f.format("%a", BigDecimal.ONE);
         // :: error: (argument.type.incompatible)
