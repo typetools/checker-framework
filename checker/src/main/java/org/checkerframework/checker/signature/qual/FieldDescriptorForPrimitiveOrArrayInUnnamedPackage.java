c971fc4166426560c4c6196651a6adccb01c909f
Michael Ernst <mernst@cs.washington.edu>
2019-10-27 13:40:04 -0700

    Fix regular expression

--- checker/src/main/java/org/checkerframework/checker/signature/qual/FieldDescriptorForPrimitiveOrArrayInUnnamedPackage.java
+++ checker/src/main/java/org/checkerframework/checker/signature/qual/FieldDescriptorForPrimitiveOrArrayInUnnamedPackage.java
@@ -18,6 +18,8 @@ import org.checkerframework.framework.qual.SubtypeOf;
  * @checker_framework.manual #signature-checker Signature Checker
  */
 @SubtypeOf({ClassGetName.class, FieldDescriptor.class})
-@QualifierForLiterals(stringPatterns = "^([BCDFIJSZ]|\\[+[BCDFIJSZ]|\\[L[A-Za-z_][A-Za-z_0-9]*;)$")
+@QualifierForLiterals(
+        stringPatterns =
+                "^([BCDFIJSZ]|\\[+[BCDFIJSZ]|\\[L[A-Za-z_][A-Za-z_0-9]*(\\$[A-Za-z_0-9]+)*;)$")
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 public @interface FieldDescriptorForPrimitiveOrArrayInUnnamedPackage {}
