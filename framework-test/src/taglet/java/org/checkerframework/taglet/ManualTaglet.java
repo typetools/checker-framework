ddeeecbada16df736549628b38f7f0066ccbb05f
Michael Ernst <mernst@cs.washington.edu>
2019-10-29 12:03:34 -0700

    Reduce diffs with tagletJdk11

--- framework-test/src/taglet/java/org/checkerframework/taglet/ManualTaglet.java
+++ framework-test/src/taglet/java/org/checkerframework/taglet/ManualTaglet.java
@@ -101,12 +101,9 @@ public class ManualTaglet implements Taglet {
         if (tags.length == 0) {
             return "";
         }
-        StringBuilder sb = new StringBuilder();
+        StringJoiner sb = new StringJoiner(", ");
         for (Tag t : tags) {
             String[] split = t.text().split(" ", 2);
-            if (t != tags[0]) {
-                sb.append(", ");
-            }
             sb.append(formatLink(split));
         }
         return formatHeader(sb.toString());
