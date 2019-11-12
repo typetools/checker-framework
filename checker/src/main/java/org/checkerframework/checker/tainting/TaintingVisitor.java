c18b719446812977a5cfe2a79168a78478c1e5d3
Suzanne Millstein <smillst@cs.washington.edu>
2019-11-12 09:59:18 -0800

    Add comments.

--- checker/src/main/java/org/checkerframework/checker/tainting/TaintingVisitor.java
+++ checker/src/main/java/org/checkerframework/checker/tainting/TaintingVisitor.java
@@ -1,19 +1,24 @@
 package org.checkerframework.checker.tainting;
 
 import javax.lang.model.element.ExecutableElement;
+import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.common.basetype.BaseTypeVisitor;
 import org.checkerframework.framework.type.AnnotatedTypeMirror.AnnotatedExecutableType;
 
-public class TaintingVisitor extends BaseTypeVisitor<TaintingAnnotatedTypeFactory> {
+/** Visitor for the {@link TaintingChecker}. */
+public class TaintingVisitor extends BaseTypeVisitor<BaseAnnotatedTypeFactory> {
 
+    /** Creates a {@link TaintingVisitor}. */
     public TaintingVisitor(BaseTypeChecker checker) {
         super(checker);
     }
 
+    /**
+     * Don't check the constructor result. Checking that the super() or this() call is a subtype of
+     * the constructor result is sufficient.
+     */
     @Override
     protected void checkConstructorResult(
-            AnnotatedExecutableType constructorType, ExecutableElement constructorElement) {
-        // skip
-    }
+            AnnotatedExecutableType constructorType, ExecutableElement constructorElement) {}
 }
