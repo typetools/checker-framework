a0791963698f6b7dc8c6f67f881f5693bb26dc9e
Maxi17 <maxi.gama@yahoo.com>
2019-11-18 23:36:04 +0000

    Requested change

--- framework/src/main/java/org/checkerframework/common/value/ValueTransfer.java
+++ framework/src/main/java/org/checkerframework/common/value/ValueTransfer.java
@@ -573,14 +573,14 @@ public class ValueTransfer extends CFTransfer {
         List<String> leftValues = getStringValues(leftOperand, p);
         List<String> rightValues = getStringValues(rightOperand, p);
 
-        boolean nullStringConcat =
+        boolean nonNullStringConcat =
                 atypefactory.getContext().getChecker().hasOption("nonNullStringsConcatenation");
 
         if (leftValues != null && rightValues != null) {
             // Both operands have known string values, compute set of results
             List<String> concatValues = new ArrayList<>();
 
-            if (!nullStringConcat) {
+            if (!nonNullStringConcat) {
                 if (isNullable(leftOperand)) {
                     leftValues.add("null");
                 }
@@ -622,7 +622,7 @@ public class ValueTransfer extends CFTransfer {
 
         if (leftLengths != null && rightLengths != null) {
             // Both operands have known lengths, compute set of result lengths
-            if (!nullStringConcat) {
+            if (!nonNullStringConcat) {
                 if (isNullable(leftOperand)) {
                     leftLengths.add(4); // "null"
                 }
@@ -646,7 +646,7 @@ public class ValueTransfer extends CFTransfer {
 
         if (leftLengthRange != null && rightLengthRange != null) {
             // Both operands have a length from a known range, compute a range of result lengths
-            if (!nullStringConcat) {
+            if (!nonNullStringConcat) {
                 if (isNullable(leftOperand)) {
                     leftLengthRange.union(new Range(4, 4)); // "null"
                 }
