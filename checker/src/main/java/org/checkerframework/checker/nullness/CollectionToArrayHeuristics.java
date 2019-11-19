f34a3e01314bb0fc277105858adc24f1d1e7f333
Werner Dietl <wdietl@gmail.com>
2019-11-18 20:37:27 -0500

    Always enforce a non-null receiver type.

--- checker/src/main/java/org/checkerframework/checker/nullness/CollectionToArrayHeuristics.java
+++ checker/src/main/java/org/checkerframework/checker/nullness/CollectionToArrayHeuristics.java
@@ -79,11 +79,9 @@ public class CollectionToArrayHeuristics {
             ExpressionTree argument = tree.getArguments().get(0);
             boolean receiverIsNonNull = isNonNullReceiver(tree);
             boolean argIsHandled =
-                    receiverIsNonNull
-                            && isHandledArrayCreation(
-                                    argument, receiverName(tree.getMethodSelect()));
+                    isHandledArrayCreation(argument, receiverName(tree.getMethodSelect()));
             argIsHandled = argIsHandled || isArrayLenZeroFieldAccess(argument);
-            setComponentNullness(argIsHandled, method.getReturnType());
+            setComponentNullness(receiverIsNonNull && argIsHandled, method.getReturnType());
 
             // TODO: We need a mechanism to prevent nullable collections
             // from inserting null elements into a nonnull arrays.
