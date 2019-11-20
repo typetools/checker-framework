6da6e2092ab91b21afc757ac572b048fed6b8b15
xingweitian <13183370+xingweitian@users.noreply.github.com>
2019-11-20 00:50:21 -0500

    Pass Travis.

--- javacutil/src/main/java/org/checkerframework/javacutil/TreeUtils.java
+++ javacutil/src/main/java/org/checkerframework/javacutil/TreeUtils.java
@@ -417,18 +417,20 @@ public final class TreeUtils {
             case RETURN:
             case VARIABLE:
                 return parent;
-            case MEMBER_SELECT:
-                // Also check case when treepath's leaf tree is used as methohd
-                // invocation's actual receiver
-                // If so, return that method invocation tree too as the assignment
-                // context tree rather than null as we did before
-                TreePath grandParentPath = parentPath.getParentPath();
-                if (grandParentPath != null
-                        && grandParentPath.getLeaf() instanceof MethodInvocationTree) {
-                    return grandParentPath.getLeaf();
-                } else {
-                    return null;
-                }
+                // Comment it for now. It will cause false negative in MyTree.java.
+                //            case MEMBER_SELECT:
+                //                // Also check case when treepath's leaf tree is used as methohd
+                //                // invocation's actual receiver
+                //                // If so, return that method invocation tree too as the assignment
+                //                // context tree rather than null as we did before
+                //                TreePath grandParentPath = parentPath.getParentPath();
+                //                if (grandParentPath != null
+                //                        && grandParentPath.getLeaf() instanceof
+                // MethodInvocationTree) {
+                //                    return grandParentPath.getLeaf();
+                //                } else {
+                //                    return null;
+                //                }
 
             default:
                 // 11 Tree.Kinds are CompoundAssignmentTrees,
