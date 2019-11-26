dda21fdf651b2507c8fb6c518115fe90f92c0b48
abslamp <abslamp@users.noreply.github.com>
2019-11-26 11:22:50 -0500

    revert 58dc4fb7ba03d694d9142a9b2f5bcb3323f278d9

--- framework/src/main/java/org/checkerframework/framework/type/AsSuperVisitor.java
+++ framework/src/main/java/org/checkerframework/framework/type/AsSuperVisitor.java
@@ -368,11 +368,7 @@ public class AsSuperVisitor extends AbstractAtmComboVisitor<AnnotatedTypeMirror,
     @Override
     public AnnotatedTypeMirror visitDeclared_Typevar(
             AnnotatedDeclaredType type, AnnotatedTypeVariable superType, Void p) {
-        // setUpperBound() may have a side effect on parameter "type" when the upper bound of
-        // "superType" equals to "type" (referencing the same object: changes will be shared)
-        // copy before visiting to avoid
-        AnnotatedTypeMirror typeCopy = type.deepCopy();
-        AnnotatedTypeMirror upperBound = visit(typeCopy, superType.getUpperBound(), p).asUse();
+        AnnotatedTypeMirror upperBound = visit(type, superType.getUpperBound(), p).asUse();
         superType.setUpperBound(upperBound);
 
         AnnotatedTypeMirror lowerBound = asSuperTypevarLowerBound(type, superType, p).asUse();
