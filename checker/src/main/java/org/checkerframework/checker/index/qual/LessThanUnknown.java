f851fe35624ed370ce3010ebc8664ba2c1892bed
Michael Ernst <mernst@cs.washington.edu>
2019-12-17 08:19:38 -0800

    Documentation goes before meta-annotations

--- checker/src/main/java/org/checkerframework/checker/index/qual/LessThanUnknown.java
+++ checker/src/main/java/org/checkerframework/checker/index/qual/LessThanUnknown.java
@@ -8,15 +8,15 @@ import java.lang.annotation.Target;
 import org.checkerframework.framework.qual.DefaultQualifierInHierarchy;
 import org.checkerframework.framework.qual.SubtypeOf;
 
-@Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE_PARAMETER, ElementType.TYPE_USE})
-@SubtypeOf({})
-@DefaultQualifierInHierarchy
 /**
  * The top qualifier for the LessThan type hierarchy. It indicates that no other expression is known
  * to be larger than the annotated one.
  *
  * @checker_framework.manual #index-inequalities Index Chceker Inequalities
  */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE_PARAMETER, ElementType.TYPE_USE})
+@SubtypeOf({})
+@DefaultQualifierInHierarchy
 public @interface LessThanUnknown {}
