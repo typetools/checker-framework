378e06c779a98f76c7e76ba30cc50ce7eedc3ed3
Suzanne Millstein <smillst@cs.washington.edu>
2019-12-18 15:33:46 -0800

    Must be allowed to write top on bounds.

--- checker/src/main/java/org/checkerframework/checker/formatter/qual/UnknownFormat.java
+++ checker/src/main/java/org/checkerframework/checker/formatter/qual/UnknownFormat.java
@@ -1,12 +1,15 @@
 package org.checkerframework.checker.formatter.qual;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.checkerframework.framework.qual.DefaultQualifierInHierarchy;
 import org.checkerframework.framework.qual.InvisibleQualifier;
 import org.checkerframework.framework.qual.SubtypeOf;
+import org.checkerframework.framework.qual.TargetLocations;
+import org.checkerframework.framework.qual.TypeUseLocation;
 
 /**
  * The top qualifier.
@@ -21,8 +24,9 @@ import org.checkerframework.framework.qual.SubtypeOf;
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({})
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 @InvisibleQualifier
 @SubtypeOf({})
 @DefaultQualifierInHierarchy
+@TargetLocations({TypeUseLocation.EXPLICIT_LOWER_BOUND, TypeUseLocation.EXPLICIT_UPPER_BOUND})
 public @interface UnknownFormat {}
