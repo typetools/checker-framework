81fc54c4683da513744f8863bd0f49585323b8f2
Sidney Monteiro <sidney_f_monteiro@hotmail.com>
2019-12-21 13:30:28 -0500

    per review

--- checker/bin-devel/test-jdk-jar.sh
+++ checker/bin-devel/test-jdk-jar.sh
@@ -5,10 +5,12 @@ set -o verbose
 set -o xtrace
 export SHELLOPTS
 echo "SHELLOPTS=${SHELLOPTS}"
+
 export CHECKERFRAMEWORK="${CHECKERFRAMEWORK:-$(pwd -P)}"
 echo "CHECKERFRAMEWORK=$CHECKERFRAMEWORK"
-echo "BUILDJDK=${BUILDJDK}"
+
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "BUILDJDK=${BUILDJDK}"
 source $SCRIPTDIR/build.sh ${BUILDJDK}
 
 
