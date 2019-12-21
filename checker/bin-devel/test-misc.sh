f3d9f155516ffe99ba7b85b201935cf85fb9b3d3
Sidney Monteiro <sidney_f_monteiro@hotmail.com>
2019-12-21 13:28:33 -0500

    per review

--- checker/bin-devel/test-misc.sh
+++ checker/bin-devel/test-misc.sh
@@ -11,8 +11,8 @@ git -C /tmp/plume-scripts pull > /dev/null 2>&1 \
 
 export CHECKERFRAMEWORK="${CHECKERFRAMEWORK:-$(pwd -P)}"
 echo "CHECKERFRAMEWORK=$CHECKERFRAMEWORK"
-echo "BUILDJDK=${BUILDJDK}"
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "BUILDJDK=${BUILDJDK}"
 source $SCRIPTDIR/build.sh ${BUILDJDK}
 
 
