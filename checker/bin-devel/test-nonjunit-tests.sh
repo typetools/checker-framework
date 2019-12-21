bee2676436b7ff9dcb9a914110eed2ee80856d2e
Sidney Monteiro <sidney_f_monteiro@hotmail.com>
2019-12-21 13:27:57 -0500

    moved echo lines per request

--- checker/bin-devel/test-nonjunit-tests.sh
+++ checker/bin-devel/test-nonjunit-tests.sh
@@ -8,8 +8,8 @@ echo "SHELLOPTS=${SHELLOPTS}"
 
 export CHECKERFRAMEWORK="${CHECKERFRAMEWORK:-$(pwd -P)}"
 echo "CHECKERFRAMEWORK=$CHECKERFRAMEWORK"
-echo "BUILDJDK=${BUILDJDK}"
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "BUILDJDK=${BUILDJDK}"
 source $SCRIPTDIR/build.sh ${BUILDJDK}
 
 
