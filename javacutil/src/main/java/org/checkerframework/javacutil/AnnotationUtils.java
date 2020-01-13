0224233d377feb915c9a90270db34f3874d39215
Michael Hixson <mhixson@techempower.com>
2020-01-13 12:33:46 -0800

    Resolve TODO in AnnotationUtils to return ElementKind.MODULE (#3019)
    Co-authored-by: Suzanne Millstein <smillst@cs.washington.edu>

--- javacutil/src/main/java/org/checkerframework/javacutil/AnnotationUtils.java
+++ javacutil/src/main/java/org/checkerframework/javacutil/AnnotationUtils.java
@@ -497,9 +497,6 @@ public class AnnotationUtils {
      * type is TYPE_USE, then ElementKinds returned should be the same as those returned for TYPE
      * and TYPE_PARAMETER, but this method returns the empty set instead.
      *
-     * <p>If the Element is MODULE, the empty set is returned. This is so that this method can
-     * compile with Java 8.
-     *
      * @param elementType the elementType to find ElementKinds for
      * @return the set of {@link ElementKind}s corresponding to {@code elementType}
      */
@@ -529,10 +526,10 @@ public class AnnotationUtils {
             case TYPE_USE:
                 return EnumSet.noneOf(ElementKind.class);
             default:
-                // TODO: Add actual case to check for the enum constant and return Set containing
-                // ElementKind.MODULE.  (Java 11)
-                if (elementType.name().contentEquals("MODULE")) {
-                    return EnumSet.noneOf(ElementKind.class);
+                // TODO: Use MODULE enum constants directly instead of looking them up by name.
+                // (Java 11)
+                if (elementType.name().equals("MODULE")) {
+                    return EnumSet.of(ElementKind.valueOf("MODULE"));
                 }
                 if (elementType.name().equals("RECORD_COMPONENT")) {
                     return EnumSet.of(ElementKind.valueOf("RECORD_COMPONENT"));
