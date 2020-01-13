213da36355b96015f8ca78265f652bfac8599ed8
Michael Ernst <mernst@cs.washington.edu>
2020-01-13 13:26:26 -0800

    Reinstate space

--- checker/bin-devel/test-misc.sh
+++ checker/bin-devel/test-misc.sh
@@ -11,6 +11,7 @@ git -C /tmp/plume-scripts pull > /dev/null 2>&1 \
 
 export CHECKERFRAMEWORK="${CHECKERFRAMEWORK:-$(pwd -P)}"
 echo "CHECKERFRAMEWORK=$CHECKERFRAMEWORK"
+
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "BUILDJDK=${BUILDJDK}"
 source $SCRIPTDIR/build.sh ${BUILDJDK}
--- checker/bin-devel/test-nonjunit-tests.sh
+++ checker/bin-devel/test-nonjunit-tests.sh
@@ -8,6 +8,7 @@ echo "SHELLOPTS=${SHELLOPTS}"
 
 export CHECKERFRAMEWORK="${CHECKERFRAMEWORK:-$(pwd -P)}"
 echo "CHECKERFRAMEWORK=$CHECKERFRAMEWORK"
+
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "BUILDJDK=${BUILDJDK}"
 source $SCRIPTDIR/build.sh ${BUILDJDK}
