0e9f8ad45ec0742fa63ad3c244edfc7651cf130c
Michael Ernst <mernst@cs.washington.edu>
2020-01-18 22:36:46 -0800

    Type-check Guava in continuous integration

---
+++ checker/bin-devel/test-guava.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+set -o verbose
+set -o xtrace
+export SHELLOPTS
+echo "SHELLOPTS=${SHELLOPTS}"
+
+if [ -d "/tmp/plume-scripts" ] ; then
+  (cd /tmp/plume-scripts && git pull -q)
+else
+  (cd /tmp && git clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git)
+fi
+
+export CHECKERFRAMEWORK="${CHECKERFRAMEWORK:-$(pwd -P)}"
+echo "CHECKERFRAMEWORK=$CHECKERFRAMEWORK"
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "BUILDJDK=${BUILDJDK}"
+source $SCRIPTDIR/build.sh ${BUILDJDK}
+
+
+# daikon-typecheck: 15 minutes
+/tmp/plume-scripts/git-clone-related typetools guava
+
+cd ../guava
+cd guava && time mvn package -P checkerframework-local -Dmaven.test.skip=true -Danimal.sniffer.skip=true
