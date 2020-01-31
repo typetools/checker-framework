4668e49e072bc92edff8230de86523b090c9a4d1
Suzanne Millstein <smillst@cs.washington.edu>
2020-01-31 10:45:12 -0800

    Account for recursive type variables in enclosing types (Fixes #3055) (#3072)
    Fixes #3055

---
+++ framework/tests/all-systems/Issue3055.java
@@ -0,0 +1,14 @@
+class Issue3055 {
+
+    class C1<T extends C1<T>.Bound> {
+        class Bound {}
+    }
+
+    class C2<T extends C2.Bound> {
+        class Bound {}
+    }
+
+    class C3<T extends C3<?>.Bound> {
+        class Bound {}
+    }
+}
