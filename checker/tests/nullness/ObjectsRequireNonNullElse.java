f9ff29067e68f161efbf1132f9d26307d57f7246
Michael Ernst <mernst@cs.washington.edu>
2020-03-24 09:47:08 -0700

    Unskip test that is fixed by this pull request

--- checker/tests/nullness/ObjectsRequireNonNullElse.java
+++ checker/tests/nullness/ObjectsRequireNonNullElse.java
@@ -1,5 +1,4 @@
-// Test case for https://tinyurl.com/cfissue/3149 .
-// @skip-test jdk annotations are wrong.
+// Test case for https://tinyurl.com/cfissue/3056 and https://tinyurl.com/cfissue/3149 .
 // @below-java11-jdk-skip-test
 
 import static java.util.Objects.requireNonNullElse;
