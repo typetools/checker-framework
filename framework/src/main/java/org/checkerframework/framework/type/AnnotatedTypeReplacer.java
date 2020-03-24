124d609a66d9522c75b44d4155acd22e1ac0b515
Martin Kellogg <kelloggm@cs.washington.edu>
2020-03-24 13:36:21 -0700

    okay, too overzealous the other way

--- framework/src/main/java/org/checkerframework/framework/type/AnnotatedTypeReplacer.java
+++ framework/src/main/java/org/checkerframework/framework/type/AnnotatedTypeReplacer.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import org.checkerframework.framework.type.AnnotatedTypeMirror.AnnotatedTypeVariable;
 
 /** Duplicates annotated types and replaces components according to a replacement map. */
-public class AnnotatedTypeMerger {
+public class AnnotatedTypeReplacer {
 
     /**
      * Return a copy of type after making the specified replacements.
