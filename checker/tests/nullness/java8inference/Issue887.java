2c6e1108de5ad05a16639d124ca05a23f6d572c5
Suzanne Millstein <smillst@cs.washington.edu>
2020-04-01 07:59:26 -0700

    Correct method type argument inference with null; fixes #3056; fixes #3150. (#3173)

--- checker/tests/nullness/java8inference/Issue887.java
+++ checker/tests/nullness/java8inference/Issue887.java
@@ -7,8 +7,7 @@ import org.checkerframework.checker.nullness.qual.*;
 
 public abstract class Issue887 {
     void test() {
-        // TODO: false negative
-        //// :: error: (argument.type.incompatible) :: error: (type.argument.type.incompatible)
+        // :: error: (argument.type.incompatible) :: error: (type.argument.type.incompatible)
         method(foo(null).get(0));
         methodNullable(fooNullable(null).get(0));
     }
