bde4bf14cf0d1860a0c1d6483dd74dc29856fcab
Martin Kellogg <kelloggm@cs.washington.edu>
2020-04-13 09:15:32 -0700

    fix crash on lambdas

--- framework/src/main/java/org/checkerframework/framework/flow/CFAbstractTransfer.java
+++ framework/src/main/java/org/checkerframework/framework/flow/CFAbstractTransfer.java
@@ -825,6 +825,10 @@ public abstract class CFAbstractTransfer<
         if (shouldPerformWholeProgramInference(n.getTree())) {
             // Retrieves class containing the method
             ClassTree classTree = analysis.getContainingClass(n.getTree());
+            // classTree is null e.g. if this is a return statement in a lambda.
+            if (classTree == null) {
+                return super.visitReturn(n, p);
+            }
             ClassSymbol classSymbol = (ClassSymbol) TreeUtils.elementFromTree(classTree);
 
             ExecutableElement methodElem =
