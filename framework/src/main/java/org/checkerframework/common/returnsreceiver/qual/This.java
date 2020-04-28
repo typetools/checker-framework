0eaefbb86c25a70ba001d5d9e3ddfcdb5017af34
Manu Sridharan <msridhar@gmail.com>
2020-04-28 09:16:11 -0700

    add @TargetLocations annotation

--- framework/src/main/java/org/checkerframework/common/returnsreceiver/qual/This.java
+++ framework/src/main/java/org/checkerframework/common/returnsreceiver/qual/This.java
@@ -5,6 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.checkerframework.framework.qual.PolymorphicQualifier;
+import org.checkerframework.framework.qual.TargetLocations;
+import org.checkerframework.framework.qual.TypeUseLocation;
 
 /**
  * Write {@code @This} on the return type of a method that always returns its receiver ({@code
@@ -41,4 +43,5 @@ import org.checkerframework.framework.qual.PolymorphicQualifier;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 @PolymorphicQualifier
+@TargetLocations({TypeUseLocation.RECEIVER, TypeUseLocation.RETURN})
 public @interface This {}
