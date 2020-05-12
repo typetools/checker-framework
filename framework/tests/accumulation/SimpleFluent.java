943996c9d0efb55aaeb35b1a86adb96b64ae1232
Martin Kellogg <kelloggm@cs.washington.edu>
2020-05-11 17:02:53 -0700

    add another test case that checks that RR support works as intended

---
+++ framework/tests/accumulation/SimpleFluent.java
@@ -0,0 +1,44 @@
+// A simple test that the fluent API logic in the Accumulation Checker works.
+// Based on a similar test in the Object Construction Checker.
+
+import testaccumulation.qual.*;
+import org.checkerframework.common.returnsreceiver.qual.*;
+
+/* Simple inference of a fluent builder */
+class SimpleFluent {
+    SimpleFluent build(@TestAccumulation({"a", "b"}) SimpleFluent this) { return this; }
+
+    @This SimpleFluent a() { return this; }
+
+    @This SimpleFluent b() { return this; }
+
+    // intentionally does not have an @This annotation
+    SimpleFluent c() { return new SimpleFluent(); }
+
+    static void doStuffCorrect(@TestAccumulation({"a", "b"}) SimpleFluent s) {
+        s.a().b().build();
+    }
+
+    static void doStuffWrong(@TestAccumulation({"a"}) SimpleFluent s) {
+        s.a()
+            // :: error: method.invocation.invalid
+            .build();
+    }
+
+    static void noReturnsReceiverAnno(@TestAccumulation({"a", "b"}) SimpleFluent s) {
+        s.a().b().c()
+             // :: error: method.invocation.invalid
+             .build();
+    }
+
+    static void fluentLoop(@TestAccumulation({"a", "b"}) SimpleFluent t) {
+        SimpleFluent s = t.a();
+        int i = 10;
+        while (i > 0) {
+            // :: error: method.invocation.invalid
+            s.b().build();
+            i--;
+            s = new SimpleFluent();
+        }
+    }
+}
--- framework/tests/accumulation/Subtyping.java
+++ framework/tests/accumulation/Subtyping.java
@@ -7,28 +7,22 @@ class Subtyping {
     void top(@TestAccumulationTop Object o1) {
         @TestAccumulationTop Object o2 = o1;
         // :: error: assignment.type.incompatible
-        @TestAccumulation("foo")
-        Object o3 = o1;
+        @TestAccumulation("foo") Object o3 = o1;
         // :: error: assignment.type.incompatible
-        @TestAccumulation("bar")
-        Object o4 = o1;
+        @TestAccumulation("bar") Object o4 = o1;
         // :: error: assignment.type.incompatible
-        @TestAccumulation({"foo", "bar"})
-        Object o5 = o1;
+        @TestAccumulation({"foo", "bar"}) Object o5 = o1;
         // :: error: assignment.type.incompatible
         @TestAccumulationBottom Object o6 = o1;
     }
 
     void foo(@TestAccumulation("foo") Object o1) {
         @TestAccumulationTop Object o2 = o1;
-        @TestAccumulation("foo")
-        Object o3 = o1;
+        @TestAccumulation("foo") Object o3 = o1;
         // :: error: assignment.type.incompatible
-        @TestAccumulation("bar")
-        Object o4 = o1;
+        @TestAccumulation("bar") Object o4 = o1;
         // :: error: assignment.type.incompatible
-        @TestAccumulation({"foo", "bar"})
-        Object o5 = o1;
+        @TestAccumulation({"foo", "bar"}) Object o5 = o1;
         // :: error: assignment.type.incompatible
         @TestAccumulationBottom Object o6 = o1;
     }
@@ -36,49 +30,37 @@ class Subtyping {
     void bar(@TestAccumulation("bar") Object o1) {
         @TestAccumulationTop Object o2 = o1;
         // :: error: assignment.type.incompatible
-        @TestAccumulation("foo")
-        Object o3 = o1;
-        @TestAccumulation("bar")
-        Object o4 = o1;
+        @TestAccumulation("foo") Object o3 = o1;
+        @TestAccumulation("bar") Object o4 = o1;
         // :: error: assignment.type.incompatible
-        @TestAccumulation({"foo", "bar"})
-        Object o5 = o1;
+        @TestAccumulation({"foo", "bar"}) Object o5 = o1;
         // :: error: assignment.type.incompatible
         @TestAccumulationBottom Object o6 = o1;
     }
 
     void foobar(@TestAccumulation({"foo", "bar"}) Object o1) {
         @TestAccumulationTop Object o2 = o1;
-        @TestAccumulation("foo")
-        Object o3 = o1;
-        @TestAccumulation("bar")
-        Object o4 = o1;
-        @TestAccumulation({"foo", "bar"})
-        Object o5 = o1;
+        @TestAccumulation("foo") Object o3 = o1;
+        @TestAccumulation("bar") Object o4 = o1;
+        @TestAccumulation({"foo", "bar"}) Object o5 = o1;
         // :: error: assignment.type.incompatible
         @TestAccumulationBottom Object o6 = o1;
     }
 
     void barfoo(@TestAccumulation({"bar", "foo"}) Object o1) {
         @TestAccumulationTop Object o2 = o1;
-        @TestAccumulation("foo")
-        Object o3 = o1;
-        @TestAccumulation("bar")
-        Object o4 = o1;
-        @TestAccumulation({"foo", "bar"})
-        Object o5 = o1;
+        @TestAccumulation("foo") Object o3 = o1;
+        @TestAccumulation("bar") Object o4 = o1;
+        @TestAccumulation({"foo", "bar"}) Object o5 = o1;
         // :: error: assignment.type.incompatible
         @TestAccumulationBottom Object o6 = o1;
     }
 
     void bot(@TestAccumulationBottom Object o1) {
         @TestAccumulationTop Object o2 = o1;
-        @TestAccumulation("foo")
-        Object o3 = o1;
-        @TestAccumulation("bar")
-        Object o4 = o1;
-        @TestAccumulation({"foo", "bar"})
-        Object o5 = o1;
+        @TestAccumulation("foo") Object o3 = o1;
+        @TestAccumulation("bar") Object o4 = o1;
+        @TestAccumulation({"foo", "bar"}) Object o5 = o1;
         @TestAccumulationBottom Object o6 = o1;
     }
 }
