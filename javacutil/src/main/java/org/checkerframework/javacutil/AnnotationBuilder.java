7ffefd368d87090c0b91f3719dd9e3cd4cb61119
Michael Ernst <mernst@cs.washington.edu>
2020-05-24 20:35:48 -0700

    Add import statement

--- javacutil/src/main/java/org/checkerframework/javacutil/AnnotationBuilder.java
+++ javacutil/src/main/java/org/checkerframework/javacutil/AnnotationBuilder.java
@@ -4,6 +4,7 @@ import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
