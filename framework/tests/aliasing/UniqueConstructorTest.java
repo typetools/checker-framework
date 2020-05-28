579d38555ffc4880dba9c18d537b255b1bb23306
aditya3434 <aditya390402@gmail.com>
2020-05-29 00:00:40 +0530

    Adding new line at end of file

--- framework/tests/aliasing/UniqueConstructorTest.java
+++ framework/tests/aliasing/UniqueConstructorTest.java
@@ -1,16 +1,16 @@
 import org.checkerframework.common.aliasing.qual.Unique;
 
-class ParentUniqueClass{
+class ParentUniqueClass {
 
     // No need for @SuppressWarnings()
-    @Unique ParentUniqueClass(){
+    @Unique ParentUniqueClass() {
         // Doesn't raise unique.leaked error since it's a parent class (top of heirarchy)
     }
 }
 
 class ChildUniqueClass extends ParentUniqueClass {
 
-    @Unique ChildUniqueClass(){
+    @Unique ChildUniqueClass() {
         // Doesn't raise unique.leaked error since its parent is @Unique
     }
 }
