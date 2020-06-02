8f94fd08d98c987fe5929fe940f486fb5fd4e7ab
Michael Ernst <mernst@cs.washington.edu>
2020-06-01 17:27:18 -0700

    Documentation tweaks from code review

--- framework/src/main/java/org/checkerframework/common/accumulation/AccumulationChecker.java
+++ framework/src/main/java/org/checkerframework/common/accumulation/AccumulationChecker.java
@@ -4,14 +4,15 @@ import java.util.LinkedHashSet;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.common.returnsreceiver.ReturnsReceiverChecker;
 
-// TODO: This Javadoc comment should reference the Checker Framework manual.
+// TODO: This Javadoc comment should reference the Checker Framework manual, once the Accumulation
+// Checker chapter is uncommented in the manual's LaTeX source.
 /**
  * An accumulation checker is one that accumulates some property: method calls, map keys, etc.
  *
- * <p>This class provides a basic accumulation analysis that can be extended to implement a
- * particular accumulation type system. This accumulation analysis represents all facts as Strings.
- * It automatically includes returns-receiver aliasing to precisely handle fluent APIs, but
- * otherwise uses no alias analysis.
+ * <p>This class provides a basic accumulation analysis that can be extended to implement an
+ * accumulation type system. This accumulation analysis represents all facts as Strings. It
+ * automatically includes returns-receiver aliasing to precisely handle fluent APIs, but otherwise
+ * uses no alias analysis.
  *
  * <p>The primary extension point is the constructor of {@link AccumulationAnnotatedTypeFactory},
  * which every subclass should override to provide custom annotations.
