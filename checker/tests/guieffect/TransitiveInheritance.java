1c36335ce1bd3039a89d628516f036ffe4b32448
Colin Stebbins Gordon <colin.s.gordon@gmail.com>
2020-06-02 21:17:41 -0400

    add test reproducing typetools/checker-framework#3287 and some variants

---
+++ checker/tests/guieffect/TransitiveInheritance.java
@@ -0,0 +1,40 @@
+import org.checkerframework.checker.guieffect.qual.UIEffect;
+
+public class TransitiveInheritance {
+
+    public static class TopLevel {
+        // Implicitly safe
+        public void foo() {}
+    }
+
+    public static interface ITop {
+        public void bar();
+    }
+
+    // Mid-level class and interface that do not redeclare or override the default safe methods
+    public static class MidLevel extends TopLevel {}
+
+    public static interface IMid extends ITop {}
+
+    // Issue #3287 is that if foo or bar is overridden with a @UIEffect implementation here, the
+    // "skip" in declarations causes the override warning to not be issued
+    // We check both classes and interfaces
+    public static class Base extends MidLevel implements IMid {
+        @Override
+        @UIEffect
+        // :: warning: (override.effect.invalid)
+        public void foo() {}
+
+        @Override
+        @UIEffect
+        // :: warning: (override.effect.invalid)
+        public void bar() {}
+    }
+
+    public static interface IBase extends IMid {
+        @Override
+        @UIEffect
+        // :: warning: (override.effect.invalid)
+        public void bar();
+    }
+}
