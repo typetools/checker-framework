0105a965bbf50d2b6c22cd5884c38315a22395eb
Weitian Xing <xingweitian@gmail.com>
2020-06-09 11:56:47 -0400

    Make type parameter names in dataflow framework consistent. (#3346)

--- dataflow/src/main/java/org/checkerframework/dataflow/cfg/AbstractCFGVisualizer.java
+++ dataflow/src/main/java/org/checkerframework/dataflow/cfg/AbstractCFGVisualizer.java
@@ -31,12 +31,15 @@ import org.checkerframework.dataflow.cfg.node.Node;
  * {@link CFGVisualizer} are already implemented in this abstract class, but can be overridden if
  * necessary.
  *
+ * @param <V> the abstract value type to be tracked by the analysis
+ * @param <S> the store type used in the analysis
+ * @param <T> the transfer function type that is used to approximate runtime behavior
  * @see DOTCFGVisualizer
  * @see StringCFGVisualizer
  */
 public abstract class AbstractCFGVisualizer<
-                A extends AbstractValue<A>, S extends Store<S>, T extends TransferFunction<A, S>>
-        implements CFGVisualizer<A, S, T> {
+                V extends AbstractValue<V>, S extends Store<S>, T extends TransferFunction<V, S>>
+        implements CFGVisualizer<V, S, T> {
 
     /**
      * Initialized in {@link #init(Map)}. If its value is {@code true}, {@link CFGVisualizer}
@@ -69,7 +72,7 @@ public abstract class AbstractCFGVisualizer<
      * @return the representation of the control flow graph
      */
     protected String visualizeGraph(
-            ControlFlowGraph cfg, Block entry, @Nullable Analysis<A, S, T> analysis) {
+            ControlFlowGraph cfg, Block entry, @Nullable Analysis<V, S, T> analysis) {
         return visualizeGraphHeader()
                 + visualizeGraphWithoutHeaderAndFooter(cfg, entry, analysis)
                 + visualizeGraphFooter();
@@ -84,7 +87,7 @@ public abstract class AbstractCFGVisualizer<
      * @return the String representation of the control flow graph
      */
     protected String visualizeGraphWithoutHeaderAndFooter(
-            ControlFlowGraph cfg, Block entry, @Nullable Analysis<A, S, T> analysis) {
+            ControlFlowGraph cfg, Block entry, @Nullable Analysis<V, S, T> analysis) {
         Set<Block> visited = new HashSet<>();
         StringBuilder sbGraph = new StringBuilder();
         Queue<Block> workList = new ArrayDeque<>();
@@ -174,7 +177,7 @@ public abstract class AbstractCFGVisualizer<
      * @return the String representation of the block
      */
     protected String visualizeBlockHelper(
-            Block bb, @Nullable Analysis<A, S, T> analysis, String escapeString) {
+            Block bb, @Nullable Analysis<V, S, T> analysis, String escapeString) {
         StringBuilder sbBlock = new StringBuilder();
         sbBlock.append(loopOverBlockContents(bb, analysis, escapeString));
 
@@ -227,7 +230,7 @@ public abstract class AbstractCFGVisualizer<
      * @return the String representation of the contents of the block
      */
     protected String loopOverBlockContents(
-            Block bb, @Nullable Analysis<A, S, T> analysis, String separator) {
+            Block bb, @Nullable Analysis<V, S, T> analysis, String separator) {
 
         List<Node> contents = addBlockContent(bb);
         StringJoiner sjBlockContents = new StringJoiner(separator);
@@ -269,11 +272,11 @@ public abstract class AbstractCFGVisualizer<
      * @return the String representation of the transfer input of the block
      */
     protected String visualizeBlockTransferInputHelper(
-            Block bb, Analysis<A, S, T> analysis, String escapeString) {
+            Block bb, Analysis<V, S, T> analysis, String escapeString) {
         assert analysis != null
                 : "analysis should be non-null when visualizing the transfer input of a block.";
 
-        TransferInput<A, S> input = analysis.getInput(bb);
+        TransferInput<V, S> input = analysis.getInput(bb);
         assert input != null : "@AssumeAssertion(nullness): well-behaved analysis";
 
         StringBuilder sbStore = new StringBuilder();
@@ -372,7 +375,7 @@ public abstract class AbstractCFGVisualizer<
      * @return the String representation of the nodes
      */
     protected abstract String visualizeNodes(
-            Set<Block> blocks, ControlFlowGraph cfg, @Nullable Analysis<A, S, T> analysis);
+            Set<Block> blocks, ControlFlowGraph cfg, @Nullable Analysis<V, S, T> analysis);
 
     /**
      * Generate the String representation of an edge.
