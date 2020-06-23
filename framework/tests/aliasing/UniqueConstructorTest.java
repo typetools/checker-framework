21f91a837a3205595898903782b9973fcbbf82dd
Werner Dietl <wdietl@gmail.com>
2020-06-23 14:21:57 -0400

    Fix comments

--- framework/tests/aliasing/UniqueConstructorTest.java
+++ framework/tests/aliasing/UniqueConstructorTest.java
@@ -3,15 +3,13 @@ import org.checkerframework.common.aliasing.qual.Unique;
 public class UniqueConstructorTest {
 
     @Unique UniqueConstructorTest() {
-        // No need for @SuppressWarnings()
-        // Doesn't raise unique.leaked error since it's a top parent class (@Unique Object subclass
-        // by default)
+        // Does not raise unique.leaked error since the parent constructor (Object) is unique
     }
 
     class ParentClass extends UniqueConstructorTest {
 
         ParentClass() {
-            // Doesn't raise unique.leaked error since it's a non-unique class with a unique parent
+            // Does not raise unique.leaked error since the parent constructor is unique
         }
     }
 
@@ -19,7 +17,7 @@ public class UniqueConstructorTest {
 
         // ::error: (unique.leaked)
         @Unique ChildUniqueClass() {
-            // Raises unique.leaked error since its parent is not unique
+            // Raises unique.leaked error since the parent constructor is not unique
         }
     }
 }
