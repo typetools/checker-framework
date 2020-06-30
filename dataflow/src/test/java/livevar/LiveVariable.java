a178adedbcb32f33af570ace45e78f7df76e5f02
Suzanne Millstein <smillst@cs.washington.edu>
2020-06-30 13:18:08 -0700

    Add comments.

--- dataflow/src/test/java/livevar/LiveVariable.java
+++ dataflow/src/test/java/livevar/LiveVariable.java
@@ -10,8 +10,14 @@ import org.checkerframework.dataflow.livevariable.LiveVar;
 import org.checkerframework.dataflow.livevariable.LiveVarStore;
 import org.checkerframework.dataflow.livevariable.LiveVarTransfer;
 
+/** Used in liveVariableTest Gradle task to test the LiveVariable analysis. */
 public class LiveVariable {
 
+    /**
+     * The main method expects to be run in dataflow/tests/live-variable directory.
+     *
+     * @param args not used
+     */
     public static void main(String[] args) {
 
         String inputFile = "Test.java";
