89c33bd737788d21cc06b6dc097476bf89e77cca
Michael Ernst <mernst@cs.washington.edu>
2020-07-03 22:47:21 -0700

    Suppress Interning Checker errors

--- javacutil/src/main/java/org/checkerframework/javacutil/TypesUtils.java
+++ javacutil/src/main/java/org/checkerframework/javacutil/TypesUtils.java
@@ -186,6 +186,7 @@ public final class TypesUtils {
      *
      * @return whether the arguments are the same primitive types
      */
+    @SuppressWarnings("interning:not.interned") // equality test optimization
     public static boolean areSameDeclaredTypes(Type.ClassType t1, Type.ClassType t2) {
         // Do a cheaper test first
         if (t1.tsym.name != t2.tsym.name) {
