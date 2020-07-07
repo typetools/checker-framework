210f1093a8cce9b88e0813836356a3e915e4375c
Suzanne Millstein <smillst@cs.washington.edu>
2020-07-07 11:00:34 -0700

    Rework test.

--- checker/tests/interning/SymbolCompleterUsesObjectEquals.java
+++
@@ -1,7 +0,0 @@
-import com.sun.tools.javac.code.Symbol;
-
-public class SymbolCompleterUsesObjectEquals {
-    boolean p(Symbol.Completer c) {
-        return c == Symbol.Completer.NULL_COMPLETER;
-    }
-}
