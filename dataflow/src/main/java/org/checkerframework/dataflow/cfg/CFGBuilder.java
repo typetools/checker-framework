10af10a7f351062a8b07d57695c68ff9c26824c7
Michael Ernst <mernst@cs.washington.edu>
2020-07-06 19:24:18 -0700

    Correct a warning suppression

--- dataflow/src/main/java/org/checkerframework/dataflow/cfg/CFGBuilder.java
+++ dataflow/src/main/java/org/checkerframework/dataflow/cfg/CFGBuilder.java
@@ -209,7 +209,7 @@ import org.checkerframework.javacutil.trees.TreeBuilder;
  *       preserving the control flow structure.
  * </ol>
  */
-@SuppressWarnings("interning") // TODO
+@SuppressWarnings("nullness") // TODO
 public class CFGBuilder {
 
     /** This class should never be instantiated. Protected to still allow subclasses. */
