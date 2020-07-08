f53db6c3681979d7e900658173a39df36ec903a6
Michael Ernst <mernst@cs.washington.edu>
2020-07-08 15:53:38 -0700

    Move comment

--- framework/src/main/java/org/checkerframework/framework/source/DiagMessage.java
+++ framework/src/main/java/org/checkerframework/framework/source/DiagMessage.java
@@ -34,9 +34,9 @@ public class DiagMessage {
      * @param args the arguments that will be interpolated into the localized message
      */
     @SuppressWarnings({
-        "nullness:assignment.type.incompatible",
+        "nullness:assignment.type.incompatible", // this call to Arrays.copyOf is polymorphic
         "nullness:argument.type.incompatible" // https://tinyurl.com/cfissue/3448
-    }) // Arrays.copyOf is polymorphic
+    })
     public DiagMessage(Kind kind, @CompilerMessageKey String messageKey, Object... args) {
         this.kind = kind;
         this.messageKey = messageKey;
