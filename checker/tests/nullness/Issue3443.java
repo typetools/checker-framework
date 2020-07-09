7adce64b2bc2b795b1b45f979815fdd61e929076
Suzanne Millstein <smillst@cs.washington.edu>
2020-07-09 10:19:35 -0700

    Do complete subtype check.

---
+++ checker/tests/nullness/Issue3443.java
@@ -0,0 +1,18 @@
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+class Issue3443 {
+    static <T extends Supplier3443<@Nullable String>> Supplier3443<String> passThrough(T t) {
+        // :: error: (return.type.incompatible)
+        return t;
+    }
+
+    public static void main(String[] args) {
+        Supplier3443<@Nullable String> s1 = () -> null;
+        Supplier3443<String> s2 = passThrough(s1);
+        s2.get().toString();
+    }
+}
+
+interface Supplier3443<T> {
+    T get();
+}
