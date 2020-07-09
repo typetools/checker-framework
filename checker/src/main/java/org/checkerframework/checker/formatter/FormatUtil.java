615bbe6ad3f8115a6fb110cd7f618f1b9f5eebfe
Michael Ernst <mernst@cs.washington.edu>
2020-07-09 12:45:47 -0700

    Add Javadoc

--- checker/src/main/java/org/checkerframework/checker/formatter/FormatUtil.java
+++ checker/src/main/java/org/checkerframework/checker/formatter/FormatUtil.java
@@ -150,6 +150,12 @@ public class FormatUtil {
         return conversionCharFromFormat(m);
     }
 
+    /**
+     * Parse the given format string, return information about its format specifiers.
+     *
+     * @param format a format string
+     * @return the list of Conversions from the format specifiers in the format string
+     */
     private static Conversion[] parse(String format) {
         ArrayList<Conversion> cs = new ArrayList<>();
         Matcher m = fsPattern.matcher(format);
