8aeae0fb0dc386b525e3d5be6b8186c3404433b4
Michael Ernst <mernst@cs.washington.edu>
2020-07-21 21:24:19 -0700

    Improve Dataflow Framework manual

--- dataflow/manual/examples/CFGConditionalOr.java
+++ dataflow/manual/examples/CFGConditionalOr.java
@@ -1,8 +1,8 @@
 class Test {
-    void test(boolean b1, boolean b2, boolean b3) {
-        int x = 0;
-        if (b1 || (b2 || b3)) {
-            x = 1;
-        }
+  void test(boolean b1, boolean b2, boolean b3) {
+    int x = 0;
+    if (b1 || (b2 || b3)) {
+      x = 1;
     }
+  }
 }
--- dataflow/manual/examples/CFGSwitch.java
+++ dataflow/manual/examples/CFGSwitch.java
@@ -1,14 +1,14 @@
 class Test {
-    void test(int x) {
-        switch (x) {
-            case 1:
-                int a = x;
-                break;
-            case 2:
-                int b = x;
-            default:
-                int c = x;
-                break;
-        }
+  void test(int x) {
+    switch (x) {
+      case 1:
+        int a = x;
+        break;
+      case 2:
+        int b = x;
+      default:
+        int c = x;
+        break;
     }
+  }
 }
