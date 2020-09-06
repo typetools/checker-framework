9f278b811d9583e2e444b0b8e2cda3e7b2f15063
Michael Ernst <mernst@cs.washington.edu>
2020-09-06 14:09:37 -0700

    Add more test cases from bug report

--- checker/tests/nullness/Issue3614.java
+++ checker/tests/nullness/Issue3614.java
@@ -11,10 +11,38 @@ public class Issue3614 {
     }
 
     public static @PolyNull Boolean not2(@PolyNull Boolean b) {
+        return (b == null) ? b : !b;
+    }
+
+    public static @PolyNull Boolean not3(@PolyNull Boolean b) {
         if (b == null) {
             return null;
         } else {
             return !b;
+        }
+    }
+
+    public static <T> T of1(T a) {
+        return a == null ? null : a;
+    }
+
+    public static <T> T of2(T a) {
+        if (a == null) {
+            return null;
+        } else {
+            return a;
+        }
+    }
+
+    public static @PolyNull Integer plus1(@PolyNull Integer b0, @PolyNull Integer b1) {
+        return (b0 == null || b1 == null) ? null : (b0 + b1);
+    }
+
+    public static @PolyNull Integer plus2(@PolyNull Integer b0, @PolyNull Integer b1) {
+        if (b0 == null || b1 == null) {
+            return null;
+        } else {
+            return b0 + b1;
         }
     }
 }
