1bb4f4bbc4cad47a858f47703eab355ba989520c
Michael Ernst <mernst@cs.washington.edu>
2020-09-09 21:29:44 -0700

    Add more extensive, but disabled, test

--- framework/tests/value/Issue3105.java
+++ framework/tests/value/Issue3105.java
@@ -4,12 +4,6 @@ import org.checkerframework.common.value.qual.StringVal;
 
 public class Issue3105 {
     public static final String FIELD1 = "foo";
-
-    public static final String FIELD2;
-
-    static {
-        FIELD2 = "bar";
-    }
 }
 
 class Demo1 {
@@ -21,17 +15,5 @@ class Demo1 {
 class Demo2 extends Issue3105 {
     @StringVal("foo") String m() {
         return FIELD1;
-    }
-}
-
-class Demo3 {
-    @StringVal("bar") String m() {
-        return Issue3105.FIELD2;
-    }
-}
-
-class Demo4 extends Issue3105 {
-    @StringVal("bar") String m() {
-        return FIELD2;
     }
 }
