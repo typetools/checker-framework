9d50a4ee0ee447a501c7a55fe16f8b26be57b169
Suzanne Millstein <smillst@cs.washington.edu>
2020-09-14 13:17:47 -0700

    Fix.

--- checker/tests/nullness/init/Issue3407.java
+++ checker/tests/nullness/init/Issue3407.java
@@ -1,16 +1,17 @@
-class Issue3407 {
+// @below-java9-jdk-skip-test
+class Issue3408 {
     final String foo;
 
     String getFoo() {
         return foo;
     }
 
-    Issue3407() {
-        Object anon =
+    Issue3408() {
+        var anon =
                 new Object() {
                     String bar() {
                         // :: error: (method.invocation.invalid)
-                        return Issue3407.this.getFoo().substring(1);
+                        return Issue3408.this.getFoo().substring(1);
                     }
                 };
         anon.bar(); // / WHOOPS... NPE, `getFoo()` returns `foo` which is still null
