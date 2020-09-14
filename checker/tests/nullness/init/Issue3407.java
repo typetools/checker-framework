b5c137ddacd6182929aa7c72d5b9bc75aa8070f6
Suzanne Millstein <smillst@cs.washington.edu>
2020-09-14 10:31:59 -0700

    Remove var.

--- checker/tests/nullness/init/Issue3407.java
+++ checker/tests/nullness/init/Issue3407.java
@@ -1,16 +1,16 @@
-class Issue3408 {
+class Issue3407 {
     final String foo;
 
     String getFoo() {
         return foo;
     }
 
-    Issue3408() {
-        var anon =
+    Issue3407() {
+        Object anon =
                 new Object() {
                     String bar() {
                         // :: error: (method.invocation.invalid)
-                        return Issue3408.this.getFoo().substring(1);
+                        return Issue3407.this.getFoo().substring(1);
                     }
                 };
         anon.bar(); // / WHOOPS... NPE, `getFoo()` returns `foo` which is still null
