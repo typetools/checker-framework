baef0d97e37d8f6b576cb80e8c6b5d17b54216f2
Michael Ernst <mernst@cs.washington.edu>
2020-09-22 09:48:40 -0700

    Add tests

--- checker/tests/signedness/WideningConversion3.java
+++ checker/tests/signedness/WideningConversion3.java
@@ -3,10 +3,14 @@ import org.checkerframework.checker.signedness.qual.*;
 public class WideningConversion3 {
 
     char c1;
+    char c2;
 
     void plus() {
         @Signed int si;
         si = c1;
         si = (int) c1;
+
+        si = (int) (c1 + c2);
+        si = c1 + c2;
     }
 }
