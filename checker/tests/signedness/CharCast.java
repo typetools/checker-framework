881cb09519e1d059b0ca25416c130c10c8c6d6f5
Michael Ernst <mernst@cs.washington.edu>
2020-10-01 11:26:00 -0700

    Simplify CharCast test case

--- checker/tests/signedness/CharCast.java
+++ checker/tests/signedness/CharCast.java
@@ -8,54 +8,54 @@ public class CharCast {
 
     void clientReturnedCastedShort2(int x) {
         // :: warning: (cast.unsafe)
-        String s = Character.toString((char) x);
+        String str = Character.toString((char) x);
     }
 
-    void clientReturnedCastedShort() {
-        int x = returnCastedShort();
+    void clientReturnedCastedShort(short s) {
+        int x = s;
         // :: warning: (cast.unsafe)
-        String s = Character.toString((char) x);
+        String str = Character.toString((char) x);
+    }
+
+    void clientCastShort() {
+        int x = (short) i;
+        // :: warning: (cast.unsafe)
+        String str = Character.toString((char) x);
+    }
+
+    void clientReturnedCastedShortLiteral(short s) {
+        int x = s;
+        // :: warning: (cast.unsafe)
+        String str = Character.toString((char) x);
+    }
+
+    void clientCastShortLiteral() {
+        int x = (short) 1;
+        String str = Character.toString((char) x);
+    }
+
+    void clientShortUpcast() {
+        short x = 1;
+        int y = x;
+        String str = Character.toString((char) y);
+    }
+
+    void clientReturnedShortUpcast(short s) {
+        int y = s;
+        // :: warning: (cast.unsafe)
+        String str = Character.toString((char) y);
+    }
+
+    short returnShort() {
+        short x = 1;
+        return x;
     }
 
     short returnCastedShort() {
         return (short) i;
     }
 
-    void clientCastShort() {
-        int x = (short) i;
-        // :: warning: (cast.unsafe)
-        String s = Character.toString((char) x);
-    }
-
-    void clientReturnedCastedShortLiteral() {
-        int x = returnCastedShortLiteral();
-        // :: warning: (cast.unsafe)
-        String s = Character.toString((char) x);
-    }
-
     short returnCastedShortLiteral() {
         return (short) 1;
-    }
-
-    void clientCastShortLiteral() {
-        int x = (short) 1;
-        String s = Character.toString((char) x);
-    }
-
-    void clientShortUpcast() {
-        short x = 1;
-        int y = x;
-        String s = Character.toString((char) y);
-    }
-
-    void clientReturnedShortUpcast() {
-        int y = returnShort();
-        // :: warning: (cast.unsafe)
-        String s = Character.toString((char) y);
-    }
-
-    short returnShort() {
-        short x = 1;
-        return x;
     }
 }
