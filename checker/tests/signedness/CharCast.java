4493e59f3433889951fd97ce81b2dfff99b77fc8
Michael Ernst <mernst@cs.washington.edu>
2020-10-01 18:53:13 -0700

    Simplify CharCast test case

--- checker/tests/signedness/CharCast.java
+++ checker/tests/signedness/CharCast.java
@@ -1,61 +1,25 @@
 public class CharCast {
-    int i;
 
-    void clientReturnedCastedShort1(int x) {
+    void m1(short s) {
+        int x = s;
         // :: warning: (cast.unsafe)
         char c = (char) x;
     }
 
-    void clientReturnedCastedShort2(int x) {
-        // :: warning: (cast.unsafe)
-        String s = Character.toString((char) x);
-    }
-
-    void clientReturnedCastedShort() {
-        int x = returnCastedShort();
-        // :: warning: (cast.unsafe)
-        String s = Character.toString((char) x);
-    }
-
-    short returnCastedShort() {
-        return (short) i;
-    }
-
-    void clientCastShort() {
+    void m2(int i) {
         int x = (short) i;
         // :: warning: (cast.unsafe)
-        String s = Character.toString((char) x);
+        char c = (char) x;
     }
 
-    void clientReturnedCastedShortLiteral() {
-        int x = returnCastedShortLiteral();
-        // :: warning: (cast.unsafe)
-        String s = Character.toString((char) x);
-    }
-
-    short returnCastedShortLiteral() {
-        return (short) 1;
-    }
-
-    void clientCastShortLiteral() {
+    void m3() {
         int x = (short) 1;
-        String s = Character.toString((char) x);
+        char c = (char) x;
     }
 
-    void clientShortUpcast() {
+    void m4() {
         short x = 1;
         int y = x;
-        String s = Character.toString((char) y);
-    }
-
-    void clientReturnedShortUpcast() {
-        int y = returnShort();
-        // :: warning: (cast.unsafe)
-        String s = Character.toString((char) y);
-    }
-
-    short returnShort() {
-        short x = 1;
-        return x;
+        char c = (char) y;
     }
 }
