b007d382c6b17f98e6fdfc91979e938638d22a20
Michael Ernst <mernst@cs.washington.edu>
2020-10-03 09:36:52 -0700

    Use Signatures.isBinaryName method rather than re-implementing it

--- framework/src/main/java/org/checkerframework/common/reflection/ClassValVisitor.java
+++ framework/src/main/java/org/checkerframework/common/reflection/ClassValVisitor.java
@@ -10,6 +10,7 @@ import org.checkerframework.common.reflection.qual.ClassBound;
 import org.checkerframework.common.reflection.qual.ClassVal;
 import org.checkerframework.framework.type.AnnotatedTypeFactory;
 import org.checkerframework.framework.type.AnnotatedTypeMirror;
+import org.plumelib.reflection.Signatures;
 
 public class ClassValVisitor extends BaseTypeVisitor<ClassValAnnotatedTypeFactory> {
     public ClassValVisitor(BaseTypeChecker checker) {
@@ -49,51 +50,11 @@ class ClassNameValidator extends BaseTypeValidator {
                     ((ClassValAnnotatedTypeFactory) atypeFactory)
                             .getClassNamesFromAnnotation(classVal);
             for (String className : classNames) {
-                if (!isLegalClassName(className)) {
+                if (!Signatures.isBinaryName(className)) {
                     checker.reportError(tree, "illegal.classname", className, type);
                 }
             }
         }
         return super.isValid(type, tree);
-    }
-
-    /**
-     * A string is a <a
-     * href="https://docs.oracle.com/javase/specs/jls/se11/html/jls-13.html#jls-13.1">binary
-     * name</a> if it has the following form: ((Java identifier)\.)*(Java identifier)([])*
-     *
-     * @param className string to check
-     * @return true if className is a legal class name
-     */
-    private boolean isLegalClassName(String className) {
-        int lastBracket = className.lastIndexOf("]");
-        if (lastBracket != -1 && lastBracket != className.length() - 1) {
-            return false;
-        }
-        className = className.replaceAll("\\[\\]", "");
-        String[] identifiers = className.split("(\\.)");
-        for (String identifier : identifiers) {
-            if (!isJavaIdentifier(identifier)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /**
-     * Whether the given string is a Java Identifier. (This method returns true if the Identifier is
-     * a keyword, boolean literal, null literal.
-     */
-    private boolean isJavaIdentifier(String identifier) {
-        char[] identifierChars = identifier.toCharArray();
-        if (!(identifierChars.length > 0 && Character.isJavaIdentifierStart(identifierChars[0]))) {
-            return false;
-        }
-        for (int i = 1; i < identifierChars.length; i++) {
-            if (!Character.isJavaIdentifierPart(identifierChars[i])) {
-                return false;
-            }
-        }
-        return true;
     }
 }
