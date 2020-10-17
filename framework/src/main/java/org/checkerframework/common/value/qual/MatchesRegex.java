c7c69d0babf7a25c06162deadf90dad0d5b01555
Michael Ernst <mernst@cs.washington.edu>
2020-10-16 22:05:01 -0700

    Code review changes

--- framework/src/main/java/org/checkerframework/common/value/qual/MatchesRegex.java
+++ framework/src/main/java/org/checkerframework/common/value/qual/MatchesRegex.java
@@ -8,10 +8,13 @@ import java.lang.annotation.Target;
 import org.checkerframework.framework.qual.SubtypeOf;
 
 /**
- * An annotation indicating the possible values for a String type. If an expression's type has this
- * annotation, then at run time, the expression evaluates to a string that matches (via the <a
+ * An annotation indicating the possible values for a String type. The annotation's arguments are
+ * Java regular expressions. If an expression's type has this annotation, then at run time, the
+ * expression evaluates to a string that matches at least one of the regular expressions. Matching
+ * is via the <a
  * href="https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#matches-java.lang.String-">java.lang.String#matches</a>
- * method) at least one of the annotation's arguments, which are Java regular expressions.
+ * method, which matches against the entire string (it does not look for a match against a
+ * substring).
  *
  * @checker_framework.manual #constant-value-checker Constant Value Checker
  */
--- framework/tests/value/RegexPatternSyntaxException.java
+++ framework/tests/value/RegexPatternSyntaxException.java
@@ -4,7 +4,5 @@ import org.checkerframework.common.value.qual.*;
 
 class RegexPatternSyntaxException {
     // :: warning: invalid.matches.regex
-    void stringConstants(@MatchesRegex("(a*") String a) {
-        // TODO: Add more tests like the above.
-    }
+    void stringConstants(@MatchesRegex("(a*") String a) {}
 }
