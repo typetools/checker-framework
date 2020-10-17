1623e61b5cdf55a4de62f2083126c547bbc20a2a
Michael Ernst <mernst@cs.washington.edu>
2020-10-17 12:38:42 -0700

    Issue error if cannot find java

--- checker/bin-devel/build.sh
+++ checker/bin-devel/build.sh
@@ -57,14 +57,15 @@ if type -p java; then
   _java=java
 elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
   _java="$JAVA_HOME/bin/java"
+else
+  echo "Can't find java"
+  exit 1
 fi
-if [[ "$_java" ]]; then
-  version=$("$_java" -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
-  if [[ "$version" -ge 9 ]]; then
-    echo "Running:  (cd ../jspecify/ && ./gradlew build)"
-    (cd ../jspecify/ && ./gradlew build)
-    echo "... done: (cd ../jspecify/ && ./gradlew build)"
-  fi
+version=$("$_java" -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
+if [[ "$version" -ge 9 ]]; then
+  echo "Running:  (cd ../jspecify/ && ./gradlew build)"
+  (cd ../jspecify/ && ./gradlew build)
+  echo "... done: (cd ../jspecify/ && ./gradlew build)"
 fi
 
 
