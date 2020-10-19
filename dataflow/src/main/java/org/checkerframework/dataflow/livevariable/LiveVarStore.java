067a6bcc32efc905e65421b41706342cd79d66f4
Michael Ernst <mernst@cs.washington.edu>
2020-10-19 15:11:53 -0700

    Add import statement

--- dataflow/src/main/java/org/checkerframework/dataflow/livevariable/LiveVarStore.java
+++ dataflow/src/main/java/org/checkerframework/dataflow/livevariable/LiveVarStore.java
@@ -1,6 +1,7 @@
 package org.checkerframework.dataflow.livevariable;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.StringJoiner;
 import org.checkerframework.checker.nullness.qual.Nullable;
