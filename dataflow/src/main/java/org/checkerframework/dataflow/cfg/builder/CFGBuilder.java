0ffe4902378112f0224d6134b64ff187f2ab95a7
Michael Ernst <mernst@cs.washington.edu>
2020-10-19 00:16:41 -0700

    Fix 24 determinism bugs: 5 in CFGBuilder, 19 in CFGTranslationPhaseOne

--- dataflow/src/main/java/org/checkerframework/dataflow/cfg/builder/CFGBuilder.java
+++ dataflow/src/main/java/org/checkerframework/dataflow/cfg/builder/CFGBuilder.java
@@ -3,6 +3,7 @@ package org.checkerframework.dataflow.cfg.builder;
 import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.CompilationUnitTree;
 import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.Tree;
 import com.sun.source.util.TreePath;
 import java.util.Collection;
 import java.util.Map;
@@ -21,6 +22,7 @@ import org.checkerframework.dataflow.cfg.node.Node;
 import org.checkerframework.javacutil.AnnotationProvider;
 import org.checkerframework.javacutil.BasicAnnotationProvider;
 import org.checkerframework.javacutil.trees.TreeBuilder;
+import org.plumelib.util.UniqueIdMap;
 
 /**
  * Builds the control flow graph of some Java code (either a method, or an arbitrary statement).
@@ -49,7 +51,19 @@ public class CFGBuilder {
     /** This class should never be instantiated. Protected to still allow subclasses. */
     protected CFGBuilder() {}
 
-    /** Build the control flow graph of some code. */
+    /** Unique IDs for trees. */
+    static UniqueIdMap<Tree> treeUids = new UniqueIdMap<>();
+
+    /**
+     * Build the control flow graph of some code.
+     *
+     * @param root the compilation unit
+     * @param underlyingAST the AST that underlies the control frow graph
+     * @param assumeAssertionsDisabled can assertions be assumed to be disabled?
+     * @param assumeAssertionsEnabled can assertions be assumed to be enabled?
+     * @param env annotation processing environment containing type utilities
+     * @return a control flow graph
+     */
     public static ControlFlowGraph build(
             CompilationUnitTree root,
             UnderlyingAST underlyingAST,
@@ -144,22 +158,24 @@ public class CFGBuilder {
     /**
      * Print a set of {@link Block}s and the edges between them. This is useful for examining the
      * results of phase two.
+     *
+     * @param blocks the blocks to print
      */
     protected static void printBlocks(Set<Block> blocks) {
         for (Block b : blocks) {
-            System.out.print(b.hashCode() + ": " + b);
+            System.out.print(b.getUid() + ": " + b);
             switch (b.getType()) {
                 case REGULAR_BLOCK:
                 case SPECIAL_BLOCK:
                     {
                         Block succ = ((SingleSuccessorBlockImpl) b).getSuccessor();
-                        System.out.println(" -> " + (succ != null ? succ.hashCode() : "||"));
+                        System.out.println(" -> " + (succ != null ? succ.getUid() : "||"));
                         break;
                     }
                 case EXCEPTION_BLOCK:
                     {
                         Block succ = ((SingleSuccessorBlockImpl) b).getSuccessor();
-                        System.out.print(" -> " + (succ != null ? succ.hashCode() : "||") + " {");
+                        System.out.print(" -> " + (succ != null ? succ.getUid() : "||") + " {");
                         for (Map.Entry<TypeMirror, Set<Block>> entry :
                                 ((ExceptionBlockImpl) b).getExceptionalSuccessors().entrySet()) {
                             System.out.print(entry.getKey() + " : " + entry.getValue() + ", ");
@@ -173,9 +189,9 @@ public class CFGBuilder {
                         Block eSucc = ((ConditionalBlockImpl) b).getElseSuccessor();
                         System.out.println(
                                 " -> T "
-                                        + (tSucc != null ? tSucc.hashCode() : "||")
+                                        + (tSucc != null ? tSucc.getUid() : "||")
                                         + " F "
-                                        + (eSucc != null ? eSucc.hashCode() : "||"));
+                                        + (eSucc != null ? eSucc.getUid() : "||"));
                         break;
                     }
             }
