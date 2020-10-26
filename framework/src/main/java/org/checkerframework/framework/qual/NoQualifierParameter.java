21ee892b69a56228cd8f060d118f616a73f5c2c5
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2020-10-26 14:54:53 -0700

    address review

--- framework/src/main/java/org/checkerframework/framework/qual/NoQualifierParameter.java
+++ framework/src/main/java/org/checkerframework/framework/qual/NoQualifierParameter.java
@@ -3,6 +3,7 @@ package org.checkerframework.framework.qual;
 import java.lang.annotation.Annotation;
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -25,6 +26,7 @@ import java.lang.annotation.Target;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Inherited
 public @interface NoQualifierParameter {
 
     /**
