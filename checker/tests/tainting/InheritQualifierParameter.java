cb1c7137c0454080f15dfe00ad61896315a3f8ee
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2020-10-27 11:22:54 -0700

    test case modifications

--- checker/tests/tainting/InheritQualifierParameter.java
+++ checker/tests/tainting/InheritQualifierParameter.java
@@ -1,11 +1,17 @@
 import org.checkerframework.checker.tainting.qual.Tainted;
+import org.checkerframework.checker.tainting.qual.Untainted;
 import org.checkerframework.framework.qual.HasQualifierParameter;
 import org.checkerframework.framework.qual.NoQualifierParameter;
 
 @HasQualifierParameter(Tainted.class)
 public class InheritQualifierParameter {}
 
-class SubHasQualifierParameter extends InheritQualifierParameter {}
+class SubHasQualifierParameter extends InheritQualifierParameter {
+    void test(@Untainted SubHasQualifierParameter arg) {
+        // :: error: (assignment.type.incompatible)
+        @Tainted SubHasQualifierParameter local = arg;
+    }
+}
 
 @NoQualifierParameter(Tainted.class)
 // :: error: (conflicting.qual.param)
@@ -14,7 +20,11 @@ class SubHasQualifierParameter1 extends InheritQualifierParameter {}
 @NoQualifierParameter(Tainted.class)
 class InheritNoQualifierParameter {}
 
-class SubNoQualifierParameter extends InheritNoQualifierParameter {}
+class SubNoQualifierParameter extends InheritNoQualifierParameter {
+    void test(@Untainted SubNoQualifierParameter arg) {
+        @Tainted SubNoQualifierParameter local = arg;
+    }
+}
 
 @HasQualifierParameter(Tainted.class)
 // :: error: (conflicting.qual.param)
