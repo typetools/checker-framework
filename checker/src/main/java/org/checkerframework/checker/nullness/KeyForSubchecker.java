6282116472bc0ca963249296a56707140137c6fd
Michael Ernst <mernst@cs.washington.edu>
2020-10-29 15:31:19 -0700

    Adapt to moved field

--- checker/src/main/java/org/checkerframework/checker/nullness/KeyForSubchecker.java
+++ checker/src/main/java/org/checkerframework/checker/nullness/KeyForSubchecker.java
@@ -9,11 +9,4 @@ import org.checkerframework.common.basetype.BaseTypeChecker;
  * @checker_framework.manual #map-key-checker Map Key Checker
  * @checker_framework.manual #nullness-checker Nullness Checker
  */
-public class KeyForSubchecker extends BaseTypeChecker {
-    {
-        // While strictly required for soundness, this leads to too many false positives.  Printing
-        // a key or putting it in a map erases all knowledge of what maps it was a key for.
-        // TODO: Revisit when side effect annotations are more precise.
-        // this.sideEffectsUnrefineAliases = true;
-    }
-}
+public class KeyForSubchecker extends BaseTypeChecker {}
