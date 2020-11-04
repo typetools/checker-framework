3d087d079072c09f41775b93be851d4df7be0712
Michael Ernst <mernst@cs.washington.edu>
2020-11-03 20:04:28 -0800

    Fix cut-and-paste typo

--- checker/src/main/java/org/checkerframework/checker/initialization/InitializationAnnotatedTypeFactory.java
+++ checker/src/main/java/org/checkerframework/checker/initialization/InitializationAnnotatedTypeFactory.java
@@ -882,7 +882,7 @@ public abstract class InitializationAnnotatedTypeFactory<
          * Returns the least upper bound of two types.
          *
          * @param a the first argument
-         * @param a the second argument
+         * @param b the second argument
          * @return the lub of the two arguments
          */
         protected TypeMirror lubTypeFrame(TypeMirror a, TypeMirror b) {
