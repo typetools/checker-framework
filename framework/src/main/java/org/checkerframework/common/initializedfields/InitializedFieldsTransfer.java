46c05971ed14f7666c9bc8261c125c579c54c22c
Michael Ernst <mernst@cs.washington.edu>
2020-11-07 09:03:05 -0800

    Comment cleanups

--- framework/src/main/java/org/checkerframework/common/initializedfields/InitializedFieldsTransfer.java
+++ framework/src/main/java/org/checkerframework/common/initializedfields/InitializedFieldsTransfer.java
@@ -10,33 +10,11 @@ import org.checkerframework.framework.flow.CFAnalysis;
 import org.checkerframework.framework.flow.CFStore;
 import org.checkerframework.framework.flow.CFValue;
 
-/*
-import com.sun.source.util.TreePath;
-import java.util.List;
-import javax.lang.model.element.AnnotationMirror;
-import javax.lang.model.element.ExecutableElement;
-import org.checkerframework.common.accumulation.AccumulationTransfer;
-import org.checkerframework.common.initializedfields.qual.InitializesFields;
-import org.checkerframework.dataflow.analysis.TransferInput;
-import org.checkerframework.dataflow.analysis.TransferResult;
-import org.checkerframework.dataflow.cfg.node.AssignmentNode;
-import org.checkerframework.dataflow.cfg.node.FieldAccessNode;
-import org.checkerframework.dataflow.cfg.node.MethodInvocationNode;
-import org.checkerframework.dataflow.cfg.node.Node;
-import org.checkerframework.dataflow.expression.Receiver;
-import org.checkerframework.framework.flow.CFAnalysis;
-import org.checkerframework.framework.flow.CFStore;
-import org.checkerframework.framework.flow.CFValue;
-import org.checkerframework.framework.util.FlowExpressionParseUtil;
-import org.checkerframework.javacutil.AnnotationUtils;
-import org.checkerframework.javacutil.BugInCF;
-*/
-
 /** Accumulates the names of fields that are initialized. */
 public class InitializedFieldsTransfer extends AccumulationTransfer {
 
     /**
-     * default constructor
+     * Create an InitializedFieldsTransfer.
      *
      * @param analysis the analysis
      */
