9eb0de4f5ba3b4a4b1bdd3ee97f489278733dbaf
Michael Ernst <mernst@cs.washington.edu>
2020-11-15 11:21:33 -0800

    Increase output from -AstubDebug

--- framework/src/main/java/org/checkerframework/framework/type/AnnotatedTypeFactory.java
+++ framework/src/main/java/org/checkerframework/framework/type/AnnotatedTypeFactory.java
@@ -147,6 +147,9 @@ import org.checkerframework.javacutil.trees.DetachedVarSymbol;
  */
 public class AnnotatedTypeFactory implements AnnotationProvider {
 
+    /** Whether to print verbose debugging messages about stub files. */
+    private final boolean debugStubParser;
+
     /** The {@link Trees} instance to use for tree node path finding. */
     protected final Trees trees;
 
@@ -485,6 +488,8 @@ public class AnnotatedTypeFactory implements AnnotationProvider {
         ignoreUninferredTypeArguments = !checker.hasOption("conservativeUninferredTypeArguments");
 
         objectGetClass = TreeUtils.getMethod("java.lang.Object", "getClass", 0, processingEnv);
+
+        this.debugStubParser = checker.hasOption("stubDebug");
     }
 
     /**
@@ -1194,7 +1199,13 @@ public class AnnotatedTypeFactory implements AnnotationProvider {
         }
 
         if (checker.hasOption("mergeStubsWithSource")) {
+            if (debugStubParser) {
+                System.out.printf("fromElement: mergeStubsIntoType(%s, %s)", type, elt);
+            }
             type = mergeStubsIntoType(type, elt);
+            if (debugStubParser) {
+                System.out.printf(" => %s%n", type);
+            }
         }
         // Caching is disabled if stub files are being parsed, because calls to this
         // method before the stub files are fully read can return incorrect results.
@@ -1236,7 +1247,13 @@ public class AnnotatedTypeFactory implements AnnotationProvider {
         AnnotatedTypeMirror result = TypeFromTree.fromMember(this, tree);
 
         if (checker.hasOption("mergeStubsWithSource")) {
+            if (debugStubParser) {
+                System.out.printf("fromClass: mergeStubsIntoType(%s, %s)", result, tree);
+            }
             result = mergeStubsIntoType(result, tree);
+            if (debugStubParser) {
+                System.out.printf(" => %s%n", result);
+            }
         }
 
         if (shouldCache) {
