941a119208c1ae3e5bb640f4d25707b77ddb9b43
Michael Ernst <mernst@cs.washington.edu>
2020-11-19 07:19:10 -0800

    Put stdout with other files

--- checker/bin/wpi.sh
+++ checker/bin/wpi.sh
@@ -133,8 +133,7 @@ function configure_and_exec_dljc {
   # ensure the project is clean before invoking DLJC
   eval "${CLEAN_CMD}" < /dev/null > /dev/null 2>&1
 
-  tmpfile=$(mktemp /tmp/dljc-output.XXXXXX)
-  echo "About to run dljc with stdout to $tmpfile"
+  tmpfile=$(mktemp "${DIR}/dljc-output/dljc-stdout.XXXXXX")
 
   # This command also includes "clean"; I'm not sure why it is necessary.
   { echo "JAVA_HOME: ${JAVA_HOME}"; \
