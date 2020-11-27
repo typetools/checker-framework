22ec7acbf96b8a4d7ef7d5d0299b9d6566195ce9
Michael Ernst <mernst@cs.washington.edu>
2020-11-26 20:45:12 -0800

    Fix mktemp call

--- checker/bin/wpi.sh
+++ checker/bin/wpi.sh
@@ -138,7 +138,7 @@ function configure_and_exec_dljc {
   # ensure the project is clean before invoking DLJC
   eval "${CLEAN_CMD}" < /dev/null > /dev/null 2>&1
 
-  mkdir -p "${DIR}/dljc-out/dljc-stdout.XXXXXX"
+  mkdir -p "${DIR}/dljc-out/"
   dljc_stdout=$(mktemp "${DIR}/dljc-out/dljc-stdout.XXXXXX")
 
   # This command also includes "clean"; I'm not sure why it is necessary.
