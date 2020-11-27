1b6dffdf38c056ee117c91db8d411a0271d353c2
Michael Ernst <mernst@cs.washington.edu>
2020-11-27 11:31:04 -0800

    Add method getUninitializedFields()

--- checker/src/main/java/org/checkerframework/checker/nullness/NullnessAnnotatedTypeFactory.java
+++ checker/src/main/java/org/checkerframework/checker/nullness/NullnessAnnotatedTypeFactory.java
@@ -291,16 +291,19 @@ public class NullnessAnnotatedTypeFactory
     }
 
     @Override
-    public List<VariableTree> getUninitializedInvariantFields(
+    public Pair<List<VariableTree>, List<VariableTree>> getUninitializedFields(
             NullnessStore store,
             TreePath path,
             boolean isStatic,
             List<? extends AnnotationMirror> receiverAnnotations) {
-        List<VariableTree> result =
-                super.getUninitializedInvariantFields(store, path, isStatic, receiverAnnotations);
+        Pair<List<VariableTree>, List<VariableTree>> result =
+                super.getUninitializedFields(store, path, isStatic, receiverAnnotations);
         // Filter out primitives.  They have the @NonNull annotation, but this checker issues no
         // warning when they are not initialized.
-        result.removeIf(vt -> TypesUtils.isPrimitive(getAnnotatedType(vt).getUnderlyingType()));
+        result.first.removeIf(
+                vt -> TypesUtils.isPrimitive(getAnnotatedType(vt).getUnderlyingType()));
+        result.second.removeIf(
+                vt -> TypesUtils.isPrimitive(getAnnotatedType(vt).getUnderlyingType()));
         return result;
     }
 
