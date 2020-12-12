8991cb2c2e400e5cb4bb355cfcf800c00a6807f6
Michael Ernst <mernst@cs.washington.edu>
2020-12-12 09:48:28 -0800

    Declare an alias for `@FormatMethod`

--- checker/src/main/java/org/checkerframework/checker/formatter/FormatterAnnotatedTypeFactory.java
+++ checker/src/main/java/org/checkerframework/checker/formatter/FormatterAnnotatedTypeFactory.java
@@ -56,6 +56,11 @@ public class FormatterAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
     public FormatterAnnotatedTypeFactory(BaseTypeChecker checker) {
         super(checker);
 
+        addAliasedDeclAnnotation(
+                com.google.errorprone.annotations.FormatMethod.class,
+                FormatMethod.class,
+                FORMATMETHOD);
+
         this.postInit();
     }
 
