fba1122a17dc41839d98182bfa2f923201a26ce1
Michael Ernst <mernst@cs.washington.edu>
2020-12-12 18:58:00 -0800

    Add documentation

--- framework/src/main/java/com/google/errorprone/annotations/FormatMethod.java
+++ framework/src/main/java/com/google/errorprone/annotations/FormatMethod.java
@@ -6,6 +6,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation for a method that takes a printf-style format string as an argument followed by
+ * arguments for that format string.
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
