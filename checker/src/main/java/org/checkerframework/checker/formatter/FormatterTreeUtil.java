c1596f04ed5ea30b94c0894ab078bb47f6587d38
Michael Ernst <mernst@cs.washington.edu>
2020-12-13 09:29:37 -0800

    Comment tweak

--- checker/src/main/java/org/checkerframework/checker/formatter/FormatterTreeUtil.java
+++ checker/src/main/java/org/checkerframework/checker/formatter/FormatterTreeUtil.java
@@ -338,7 +338,7 @@ public class FormatterTreeUtil {
          * @return true if the argument is a {@code null} expression
          */
         public final boolean isArgumentNull(TypeMirror type) {
-            // TODO: Just check whether it is the VOID TypeMirrr.
+            // TODO: Just check whether it is the VOID TypeMirror.
 
             // is it the null literal
             return type.accept(
