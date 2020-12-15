e0cec71fb0636efadf749051c17d5a04d39ca4b1
Michael Ernst <mernst@cs.washington.edu>
2020-12-15 12:15:20 -0800

    Improve AnnotationFileParser diagnostics (#4017)

--- checker/jtreg/stubs/issue2059/Main.java
+++ checker/jtreg/stubs/issue2059/Main.java
@@ -2,7 +2,7 @@
  * @test
  * @summary Test case for Issue 2059 https://github.com/typetools/checker-framework/issues/2059
  *
- * @compile/ref=AnnoNotFound.out -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker -AstubWarnIfNotFound -Astubs=AnnoNotFound.astub Main.java
+ * @compile/ref=AnnoNotFound.out -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker -AstubWarnIfNotFound -Astubs=AnnoNotFound.astub -Anomsgtext Main.java
  */
 
 package issue2059;
