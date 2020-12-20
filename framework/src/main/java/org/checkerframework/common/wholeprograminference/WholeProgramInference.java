a56201b7d7405ba5b4ea27de94525c8f05db0053
Michael Ernst <mernst@cs.washington.edu>
2020-12-20 10:20:07 -0800

    Refactorings related to annotation files

--- framework/src/main/java/org/checkerframework/common/wholeprograminference/WholeProgramInference.java
+++ framework/src/main/java/org/checkerframework/common/wholeprograminference/WholeProgramInference.java
@@ -195,6 +195,6 @@ public interface WholeProgramInference {
          * Output the results of whole-program inference as a Java annotation index file. The
          * Annotation File Utilities project contains code for reading and writing .jaif files.
          */
-        JAIF()
+        JAIF(),
     }
 }
