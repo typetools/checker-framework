44fdf391263643bae53d6ba89b047e2b3c02d1b6
Suzanne Millstein <smillst@cs.washington.edu>
2021-01-04 11:53:01 -0800

    Fix broken links.

--- javacutil/src/main/java/org/checkerframework/javacutil/ElementUtils.java
+++ javacutil/src/main/java/org/checkerframework/javacutil/ElementUtils.java
@@ -585,7 +585,7 @@ public class ElementUtils {
      * Return the set of kinds that represent classes.
      *
      * @return the set of kinds that represent classes
-     * @deprecated use {@link #typeElementKinds}
+     * @deprecated use {@link #typeElementKinds()}
      */
     @Deprecated // use typeElementKinds
     public static Set<ElementKind> classElementKinds() {
