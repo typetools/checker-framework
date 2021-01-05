cbe6e050733e84ebc9485fcc568d40466bc6e4c5
Michael Ernst <mernst@cs.washington.edu>
2021-01-05 09:25:40 -0800

    Improve documentation of `UnitsRelations` classes (#4091)

--- checker/src/main/java/org/checkerframework/checker/units/qual/UnitsRelations.java
+++ checker/src/main/java/org/checkerframework/checker/units/qual/UnitsRelations.java
@@ -6,7 +6,8 @@ import java.lang.annotation.RetentionPolicy;
 
 /**
  * Specify the class that knows how to handle the meta-annotated unit when put in relation (plus,
- * multiply, ...) with another unit.
+ * multiply, ...) with another unit. That class is a subtype of interface {@link
+ * org.checkerframework.checker.units.UnitsRelations}.
  *
  * @see org.checkerframework.checker.units.UnitsRelations
  * @checker_framework.manual #units-checker Units Checker
@@ -15,13 +16,14 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface UnitsRelations {
     /**
-     * Returns the UnitsRelations subclass to use.
+     * Returns the subclass of {@link org.checkerframework.checker.units.UnitsRelations} to use.
      *
-     * @return the UnitsRelations subclass to use
+     * @return the subclass of {@link org.checkerframework.checker.units.UnitsRelations} to use
      */
     // The more precise type is Class<? extends org.checkerframework.checker.units.UnitsRelations>,
     // but org.checkerframework.checker.units.UnitsRelations is not in checker-qual.jar, nor can
-    // it be since it uses AnnotatedTypeMirrors.  So use a less precise type and check that it
-    // is a subclass in UnitsAnnotatedTypeFactory.
+    // it be since it uses AnnotatedTypeMirrors.  So this declaration uses a less precise type, and
+    // UnitsAnnotatedTypeFactory checks that the argument implements
+    // org.checkerframework.checker.units.UnitsRelations.
     Class<?> value();
 }
