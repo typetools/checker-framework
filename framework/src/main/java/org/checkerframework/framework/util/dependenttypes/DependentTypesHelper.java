5f51fd5c1b17f5fa7cb46ba39da839d8f2eb578f
Michael Ernst <mernst@cs.washington.edu>
2021-01-10 12:42:50 -0800

    A class encloses each expression

--- framework/src/main/java/org/checkerframework/framework/util/dependenttypes/DependentTypesHelper.java
+++ framework/src/main/java/org/checkerframework/framework/util/dependenttypes/DependentTypesHelper.java
@@ -308,9 +308,6 @@ public class DependentTypesHelper {
 
         TreePath path = factory.getPath(tree);
         Tree enclosingClass = TreePathUtil.enclosingClass(path);
-        if (enclosingClass == null) {
-            return;
-        }
         TypeMirror enclosingType = TreeUtils.typeOf(enclosingClass);
         JavaExpression r = JavaExpression.getPseudoReceiver(path, enclosingType);
         JavaExpressionContext context =
@@ -489,9 +486,6 @@ public class DependentTypesHelper {
             return;
         }
         Tree enclosingClass = TreePathUtil.enclosingClass(path);
-        if (enclosingClass == null) {
-            return;
-        }
         TypeMirror enclosingType = TreeUtils.typeOf(enclosingClass);
 
         JavaExpression receiver = JavaExpression.getPseudoReceiver(path, enclosingType);
