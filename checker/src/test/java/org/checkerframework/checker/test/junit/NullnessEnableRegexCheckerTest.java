8d31d1bbec32374e38b44d48a7820191860962ec
Utkarsh Rai <utkarshrai491@gmail.com>
2021-01-17 12:36:25 +0530

    Added a test.

---
+++ checker/src/test/java/org/checkerframework/checker/test/junit/NullnessEnableRegexCheckerTest.java
@@ -0,0 +1,25 @@
+package org.checkerframework.checker.test.junit;
+
+import java.io.File;
+import java.util.List;
+import org.checkerframework.checker.nullness.NullnessChecker;
+import org.checkerframework.framework.test.CheckerFrameworkPerDirectoryTest;
+import org.junit.runners.Parameterized.Parameters;
+
+/** JUnit tests for the Nullness Checker. */
+public class NullnessEnableRegexCheckerTest extends CheckerFrameworkPerDirectoryTest {
+
+    /**
+     * Create a NullnessEnableRegexCheckerTest.
+     *
+     * @param testFiles the files containing test code, which will be type-checked
+     */
+    public NullnessEnableRegexCheckerTest(List<File> testFiles) {
+        super(testFiles, NullnessChecker.class, "nullness", "-AenableRegexChecker", "-Anomsgtext");
+    }
+
+    @Parameters
+    public static String[] getTestDirs() {
+        return new String[] {"nullness-enableregexchecker"};
+    }
+}
---
+++ checker/tests/nullness-enableregexchecker/NullnessEnableRegexExampleTest.java
@@ -0,0 +1,24 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+class NullnessEnableRegexExampleTest {
+    private static final Pattern PATTERN = Pattern.compile("^.(.*).$");
+
+    String doSomethingOdd(String input) {
+        Matcher matcher = PATTERN.matcher(input);
+        if (!matcher.matches()) {
+            return "";
+        }
+
+        // The regex checker concludes that this call is legal, therefore it's return type
+        // can be @NonNull.
+        @NonNull String a = matcher.group(1);
+
+        // This call is not legal because the number of groups in the pattern is 1, therefore
+        // it's return type cannot be @NonNull.
+        // :: error: (assignment.type.incompatible) :: error: (group.count.invalid)
+        @NonNull String b = matcher.group(2);
+        return a + b;
+    }
+}
