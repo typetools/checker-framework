67db8ed5326444232597800cab4f244f2a4870d5
Michael Ernst <mernst@cs.washington.edu>
2021-01-17 20:13:48 -0800

    Undo unrelated changes

--- checker/bin/wpi.sh
+++ checker/bin/wpi.sh
@@ -216,7 +216,7 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 (cd "${SCRIPTDIR}"/../.. && ./gradlew getPlumeScripts -q)
 (cd "${SCRIPTDIR}" && ../bin-devel/.plume-scripts/git-clone-related kelloggm do-like-javac .do-like-javac)
 if [ ! -d "${SCRIPTDIR}/.do-like-javac" ]; then
-    echo "Cannot clone do-like-javac"
+    echo "Failed to clone do-like-javac"
     exit 1
 fi
 
