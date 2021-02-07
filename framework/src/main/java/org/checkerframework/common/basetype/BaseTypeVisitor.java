f755bedc3a5c13c2501b2b83f662618d971a72bc
Michael Ernst <mernst@cs.washington.edu>
2021-02-07 14:50:49 -0800

    Document `annoTrees` formal parameter

--- framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
+++ framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
@@ -2381,7 +2381,8 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
      * the annotations are attached to the VariableTree or MethodTree, respectively.
      *
      * @param annoTrees annotations written before a variable/method declaration, if this type is
-     *     from one; null otherwise
+     *     from one; null otherwise. This might contain type annotations that the Java parser
+     *     attached to the declaration rather than to the type.
      * @param typeTree the type that any type annotations in annoTrees apply to
      */
     public void visitAnnotatedType(
@@ -2394,7 +2395,8 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
      * the @RelevantJavaTypes annotation.
      *
      * @param annoTrees annotations written before a variable/method declaration, if this type is
-     *     from one; null otherwise
+     *     from one; null otherwise. This might contain type annotations that the Java parser
+     *     attached to the declaration rather than to the type.
      * @param typeTree the type that any type annotations in annoTrees apply to
      */
     public void warnAboutIrrelevantJavaTypes(
