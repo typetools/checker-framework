75d3dc532e5fb9d678f3f20f6785561f0d9cddf6
Michael Ernst <mernst@cs.washington.edu>
2021-02-08 13:20:08 -0800

    Augment tests

--- checker/tests/nullness/EnsuresKeyForOverriding.java
+++ checker/tests/nullness/EnsuresKeyForOverriding.java
@@ -6,18 +6,18 @@ public class EnsuresKeyForOverriding {
         Object field = new Object();
     }
 
+    MyClass o = new MyClass();
+
+    @EnsuresKeyFor(value = "#1.field", map = "#2")
+    void method(MyClass o, Map<Object, Object> map) {
+        map.put(o.field, "Hello");
+    }
+
     static class SubEnsuresKeyForOverriding extends EnsuresKeyForOverriding {
         @Override
         @EnsuresKeyFor(value = "#1.field", map = "#2")
         void method(MyClass q, Map<Object, Object> subMap) {
             super.method(q, subMap);
         }
-    }
-
-    MyClass o = new MyClass();
-
-    @EnsuresKeyFor(value = "#1.field", map = "#2")
-    void method(MyClass o, Map<Object, Object> map) {
-        map.put(o.field, "Hello");
     }
 }
