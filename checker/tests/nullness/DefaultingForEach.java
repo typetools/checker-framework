03d3188a3a186f4d218a8def5000fe253f5aa2e7
Martin Kellogg <kelloggm@cs.washington.edu>
2021-02-08 14:49:23 -0800

    better test case from Suzanne

--- checker/tests/nullness/DefaultingForEach.java
+++ checker/tests/nullness/DefaultingForEach.java
@@ -2,6 +2,8 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.checkerframework.framework.qual.DefaultQualifier;
 
+import java.util.List;
+
 class DefaultForEach {
     @DefaultQualifier(Nullable.class)
     Object @NonNull [] foo() {
@@ -11,6 +13,18 @@ class DefaultForEach {
     void bar() {
         for (Object p : foo()) {
             // :: error: dereference.of.nullable
+            p.toString();
+        }
+    }
+
+    @DefaultQualifier(Nullable.class)
+    @NonNull List<Object> foo2() {
+        throw new RuntimeException();
+    }
+
+    void bar2() {
+        for (Object p : foo2()) {
+            // :: error: (dereference.of.nullable)
             p.toString();
         }
     }
