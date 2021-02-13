321c53603cf843400db90b920979d87eb5ca299e
Michael Ernst <mernst@cs.washington.edu>
2021-02-13 13:17:52 -0800

    Add expected error

--- checker/tests/nullness/ReadyReadLine.java
+++ checker/tests/nullness/ReadyReadLine.java
@@ -7,6 +7,7 @@ public class ReadyReadLine {
     void m(MyBufferedReader buf) throws Exception {
         if (buf.ready()) {
             String line = buf.readLine();
+            // :: error: (dereference.of.nullable)
             line.toString();
         }
 
@@ -17,7 +18,7 @@ public class ReadyReadLine {
     }
 }
 
-// this is a replication of the JDK BufferedReader (with only the relevant methods)
+// This is a replication of the JDK BufferedReader, with only the relevant methods.
 class MyBufferedReader {
     public @Nullable String readLine() throws Exception {
         return null;
