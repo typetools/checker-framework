379482d3ba5a9049608b12fcd09f4fe54fcb0973
Suzanne Millstein <smillst@cs.washington.edu>
2021-02-23 09:35:34 -0800

    Add Javadoc.

--- dataflow/src/main/java/org/checkerframework/dataflow/expression/JavaExpressionVisitor.java
+++ dataflow/src/main/java/org/checkerframework/dataflow/expression/JavaExpressionVisitor.java
@@ -1,32 +1,128 @@
 package org.checkerframework.dataflow.expression;
 
+/**
+ * A simple visitor for {@link JavaExpression}.
+ *
+ * @param <R> the return type of the visit methods
+ * @param <P> the parameter passed to the visit methods
+ */
 public abstract class JavaExpressionVisitor<R, P> {
 
-    protected R visit(JavaExpression javaExpr, P p) {
+    /**
+     * Visits the given {@code javaExpr}.
+     *
+     * @param javaExpr the expression to visit
+     * @param p the parameter to pass to the visit method
+     * @return the result of visiting the expression
+     */
+    public R visit(JavaExpression javaExpr, P p) {
         return javaExpr.accept(this, p);
     }
 
-    protected abstract R visitArrayAccess(ArrayAccess expression, P p);
+    /**
+     * Visit an {@link ArrayAccess}.
+     *
+     * @param javaExpr the JavaExpression to visit
+     * @param p the parameter to pass to the visit method
+     * @return the result of visiting the {@code javaExpr}
+     */
+    protected abstract R visitArrayAccess(ArrayAccess javaExpr, P p);
+    /**
+     * Visit an {@link ArrayCreation}.
+     *
+     * @param javaExpr the JavaExpression to visit
+     * @param p the parameter to pass to the visit method
+     * @return the result of visiting the {@code javaExpr}
+     */
+    protected abstract R visitArrayCreation(ArrayCreation javaExpr, P p);
 
-    protected abstract R visitArrayCreation(ArrayCreation expression, P p);
+    /**
+     * Visit a {@link BinaryOperation}.
+     *
+     * @param javaExpr the JavaExpression to visit
+     * @param p the parameter to pass to the visit method
+     * @return the result of visiting the {@code javaExpr}
+     */
+    protected abstract R visitBinaryOperation(BinaryOperation javaExpr, P p);
 
-    protected abstract R visitBinaryOperation(BinaryOperation expression, P p);
+    /**
+     * Visit a {@link ClassName}.
+     *
+     * @param javaExpr the JavaExpression to visit
+     * @param p the parameter to pass to the visit method
+     * @return the result of visiting the {@code javaExpr}
+     */
+    protected abstract R visitClassName(ClassName javaExpr, P p);
 
-    protected abstract R visitClassName(ClassName expression, P p);
+    /**
+     * Visit a {@link FieldAccess}.
+     *
+     * @param javaExpr the JavaExpression to visit
+     * @param p the parameter to pass to the visit method
+     * @return the result of visiting the {@code javaExpr}
+     */
+    protected abstract R visitFieldAccess(FieldAccess javaExpr, P p);
 
-    protected abstract R visitFieldAccess(FieldAccess expression, P p);
+    /**
+     * Visit a {@link FormalParameter}.
+     *
+     * @param javaExpr the JavaExpression to visit
+     * @param p the parameter to pass to the visit method
+     * @return the result of visiting the {@code javaExpr}
+     */
+    protected abstract R visitFormalParameter(FormalParameter javaExpr, P p);
 
-    protected abstract R visitFormalParameter(FormalParameter expression, P p);
+    /**
+     * Visit a {@link LocalVariable}.
+     *
+     * @param javaExpr the JavaExpression to visit
+     * @param p the parameter to pass to the visit method
+     * @return the result of visiting the {@code javaExpr}
+     */
+    protected abstract R visitLocalVariable(LocalVariable javaExpr, P p);
 
-    protected abstract R visitLocalVariable(LocalVariable expression, P p);
+    /**
+     * Visit a {@link MethodCall}.
+     *
+     * @param javaExpr the JavaExpression to visit
+     * @param p the parameter to pass to the visit method
+     * @return the result of visiting the {@code javaExpr}
+     */
+    protected abstract R visitMethodCall(MethodCall javaExpr, P p);
 
-    protected abstract R visitMethodCall(MethodCall expression, P p);
+    /**
+     * Visit a {@link ThisReference}.
+     *
+     * @param javaExpr the JavaExpression to visit
+     * @param p the parameter to pass to the visit method
+     * @return the result of visiting the {@code javaExpr}
+     */
+    protected abstract R visitThisReference(ThisReference javaExpr, P p);
 
-    protected abstract R visitThisReference(ThisReference expression, P p);
+    /**
+     * Visit an {@link UnaryOperation}.
+     *
+     * @param javaExpr the JavaExpression to visit
+     * @param p the parameter to pass to the visit method
+     * @return the result of visiting the {@code javaExpr}
+     */
+    protected abstract R visitUnaryOperation(UnaryOperation javaExpr, P p);
 
-    protected abstract R visitUnaryOperation(UnaryOperation expression, P p);
+    /**
+     * Visit an {@link Unknown}.
+     *
+     * @param javaExpr the JavaExpression to visit
+     * @param p the parameter to pass to the visit method
+     * @return the result of visiting the {@code javaExpr}
+     */
+    protected abstract R visitUnknown(Unknown javaExpr, P p);
 
-    protected abstract R visitUnknown(Unknown expression, P p);
-
-    protected abstract R visitValueLiteral(ValueLiteral expression, P p);
+    /**
+     * Visit a {@link ValueLiteral}.
+     *
+     * @param javaExpr the JavaExpression to visit
+     * @param p the parameter to pass to the visit method
+     * @return the result of visiting the {@code javaExpr}
+     */
+    protected abstract R visitValueLiteral(ValueLiteral javaExpr, P p);
 }
