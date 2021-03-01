5e889514919b67dd40e38e9e45465064d44d5f24
Suzanne Millstein <smillst@cs.washington.edu>
2021-03-01 09:09:28 -0800

    Simplify code. (#4331)

--- framework/src/main/java/org/checkerframework/framework/util/dependenttypes/DependentTypesHelper.java
+++ framework/src/main/java/org/checkerframework/framework/util/dependenttypes/DependentTypesHelper.java
@@ -3,7 +3,6 @@ package org.checkerframework.framework.util.dependenttypes;
 import com.sun.source.tree.AnnotationTree;
 import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.ExpressionTree;
-import com.sun.source.tree.IdentifierTree;
 import com.sun.source.tree.LambdaExpressionTree;
 import com.sun.source.tree.MemberSelectTree;
 import com.sun.source.tree.MethodInvocationTree;
@@ -32,7 +31,6 @@ import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.checkerframework.dataflow.expression.ArrayCreation;
-import org.checkerframework.dataflow.expression.FieldAccess;
 import org.checkerframework.dataflow.expression.JavaExpression;
 import org.checkerframework.framework.source.SourceChecker;
 import org.checkerframework.framework.type.AnnotatedTypeFactory;
@@ -511,17 +509,7 @@ public class DependentTypesHelper {
 
             case FIELD:
             case ENUM_CONSTANT:
-                JavaExpression receiverJe;
-                if (declarationTree.getKind() == Tree.Kind.IDENTIFIER) {
-                    JavaExpression nodeJe =
-                            JavaExpression.fromTree((IdentifierTree) declarationTree);
-                    receiverJe =
-                            nodeJe instanceof FieldAccess
-                                    ? ((FieldAccess) nodeJe).getReceiver()
-                                    : nodeJe;
-                } else {
-                    receiverJe = JavaExpression.getImplicitReceiver(variableElt);
-                }
+                JavaExpression receiverJe = JavaExpression.getImplicitReceiver(variableElt);
                 JavaExpressionContext fieldContext =
                         new JavaExpressionContext(receiverJe, factory.getChecker());
                 viewpointAdaptToContext(fieldContext, type);
