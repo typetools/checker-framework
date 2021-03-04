5df48d73189b1d59ea5a852f93f1269e435d916e
Suzanne Millstein <smillst@cs.washington.edu>
2021-03-04 13:07:36 -0800

    Revert changes.

--- checker/jtreg/SymbolNotFoundErrors.java
+++ checker/jtreg/SymbolNotFoundErrors.java
@@ -2,8 +2,11 @@
  * @test
  * @summary Test for expected number of error messages.
  * @compile/fail/ref=SymbolNotFoundErrors.out -XDrawDiagnostics SymbolNotFoundErrors.java
- * @compile/fail/ref=SymbolNotFoundErrors2.out -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker SymbolNotFoundErrors.java
+ * @compile/fail/ref=SymbolNotFoundErrors.out -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker SymbolNotFoundErrors.java
  */
 public class SymbolNotFoundErrors {
+    // We only expect one error message for the unknown symbol.
+    // However, we receive three.
+    // https://github.com/typetools/checker-framework/issues/94
     CCC f;
 }
