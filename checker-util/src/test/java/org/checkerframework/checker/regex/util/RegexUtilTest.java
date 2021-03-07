0f3683c371b6232e1f4825313f8010d088c1b41a
Michael Ernst <mernst@cs.washington.edu>
2021-03-06 19:41:43 -0800

    Don't use static assert

--- checker-util/src/test/java/org/checkerframework/checker/regex/util/RegexUtilTest.java
+++ checker-util/src/test/java/org/checkerframework/checker/regex/util/RegexUtilTest.java
@@ -2,10 +2,7 @@
 
 package org.checkerframework.checker.regex.util;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public final class RegexUtilTest {
@@ -18,40 +15,40 @@ public final class RegexUtilTest {
         String s3 = "colou?r";
         String s4 = "1) first point";
 
-        assertTrue(RegexUtil.isRegex(s1));
+        Assert.assertTrue(RegexUtil.isRegex(s1));
         RegexUtil.asRegex(s1);
-        assertTrue(RegexUtil.isRegex(s1, 0));
+        Assert.assertTrue(RegexUtil.isRegex(s1, 0));
         RegexUtil.asRegex(s1, 0);
-        assertTrue(RegexUtil.isRegex(s1, 1));
+        Assert.assertTrue(RegexUtil.isRegex(s1, 1));
         RegexUtil.asRegex(s1, 1);
-        assertFalse(RegexUtil.isRegex(s1, 2));
-        assertThrows(Error.class, () -> RegexUtil.asRegex(s1, 2));
+        Assert.assertFalse(RegexUtil.isRegex(s1, 2));
+        Assert.assertThrows(Error.class, () -> RegexUtil.asRegex(s1, 2));
 
-        assertTrue(RegexUtil.isRegex(s2));
+        Assert.assertTrue(RegexUtil.isRegex(s2));
         RegexUtil.asRegex(s2);
-        assertTrue(RegexUtil.isRegex(s2, 0));
+        Assert.assertTrue(RegexUtil.isRegex(s2, 0));
         RegexUtil.asRegex(s2, 0);
-        assertTrue(RegexUtil.isRegex(s2, 1));
+        Assert.assertTrue(RegexUtil.isRegex(s2, 1));
         RegexUtil.asRegex(s2, 1);
-        assertFalse(RegexUtil.isRegex(s2, 2));
-        assertThrows(Error.class, () -> RegexUtil.asRegex(s2, 2));
+        Assert.assertFalse(RegexUtil.isRegex(s2, 2));
+        Assert.assertThrows(Error.class, () -> RegexUtil.asRegex(s2, 2));
 
-        assertTrue(RegexUtil.isRegex(s3));
+        Assert.assertTrue(RegexUtil.isRegex(s3));
         RegexUtil.asRegex(s3);
-        assertTrue(RegexUtil.isRegex(s3, 0));
+        Assert.assertTrue(RegexUtil.isRegex(s3, 0));
         RegexUtil.asRegex(s3, 0);
-        assertFalse(RegexUtil.isRegex(s3, 1));
-        assertThrows(Error.class, () -> RegexUtil.asRegex(s3, 1));
-        assertFalse(RegexUtil.isRegex(s3, 2));
-        assertThrows(Error.class, () -> RegexUtil.asRegex(s3, 2));
+        Assert.assertFalse(RegexUtil.isRegex(s3, 1));
+        Assert.assertThrows(Error.class, () -> RegexUtil.asRegex(s3, 1));
+        Assert.assertFalse(RegexUtil.isRegex(s3, 2));
+        Assert.assertThrows(Error.class, () -> RegexUtil.asRegex(s3, 2));
 
-        assertFalse(RegexUtil.isRegex(s4));
-        assertThrows(Error.class, () -> RegexUtil.asRegex(s4));
-        assertFalse(RegexUtil.isRegex(s4, 0));
-        assertThrows(Error.class, () -> RegexUtil.asRegex(s4, 0));
-        assertFalse(RegexUtil.isRegex(s4, 1));
-        assertThrows(Error.class, () -> RegexUtil.asRegex(s4, 1));
-        assertFalse(RegexUtil.isRegex(s4, 2));
-        assertThrows(Error.class, () -> RegexUtil.asRegex(s4, 2));
+        Assert.assertFalse(RegexUtil.isRegex(s4));
+        Assert.assertThrows(Error.class, () -> RegexUtil.asRegex(s4));
+        Assert.assertFalse(RegexUtil.isRegex(s4, 0));
+        Assert.assertThrows(Error.class, () -> RegexUtil.asRegex(s4, 0));
+        Assert.assertFalse(RegexUtil.isRegex(s4, 1));
+        Assert.assertThrows(Error.class, () -> RegexUtil.asRegex(s4, 1));
+        Assert.assertFalse(RegexUtil.isRegex(s4, 2));
+        Assert.assertThrows(Error.class, () -> RegexUtil.asRegex(s4, 2));
     }
 }
