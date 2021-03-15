5e2807d21c9dbe477cc330818fa7271b2544a45b
Michael Ernst <mernst@cs.washington.edu>
2021-03-15 10:33:32 -0700

    Tweaks

--- checker/src/main/java/org/checkerframework/checker/index/upperbound/UpperBoundVisitor.java
+++ checker/src/main/java/org/checkerframework/checker/index/upperbound/UpperBoundVisitor.java
@@ -101,7 +101,7 @@ public class UpperBoundVisitor extends BaseTypeVisitor<UpperBoundAnnotatedTypeFa
             String from = atypeFactory.hasSubsequenceFromValue(anno);
             String to = atypeFactory.hasSubsequenceToValue(anno);
 
-            // check that each expression is parseable at the declaration of this class
+            // check that each expression is parsable at the declaration of this class
             ClassTree enclosingClass = TreePathUtil.enclosingClass(getCurrentPath());
             checkEffectivelyFinalAndParsable(seq, enclosingClass, node);
             checkEffectivelyFinalAndParsable(from, enclosingClass, node);
