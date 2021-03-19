518c6ddf301874c2c30462e9cc7af96d5bbcd384
Jason Waataja <jwaataja@cs.washington.edu>
2021-03-19 01:08:48 -0700

    Remove wildcard imports

--- framework/src/main/java/org/checkerframework/framework/type/AnnotatedTypeFactory.java
+++ framework/src/main/java/org/checkerframework/framework/type/AnnotatedTypeFactory.java
@@ -3,10 +3,6 @@ package org.checkerframework.framework.type;
 // The imports from com.sun are all @jdk.Exported and therefore somewhat safe to use.
 // Try to avoid using non-@jdk.Exported classes.
 
-import com.github.javaparser.ast.*;
-import com.github.javaparser.ast.body.*;
-import com.github.javaparser.ast.expr.*;
-import com.github.javaparser.ast.stmt.*;
 import com.sun.source.tree.AnnotationTree;
 import com.sun.source.tree.AssignmentTree;
 import com.sun.source.tree.BinaryTree;
