9317f58d815c3bb3a939953dd897f7b1a6260b87
Michael Ernst <mernst@cs.washington.edu>
2021-03-28 08:42:38 -0700

    Run WPI on Options project

--- checker/bin-devel/wpi-plumelib/test-wpi-plumelib.sh
+++ checker/bin-devel/wpi-plumelib/test-wpi-plumelib.sh
@@ -85,5 +85,6 @@ test_wpi_plume_lib bibtex-clean      "formatter,index,interning,lock,nullness,re
 test_wpi_plume_lib html-pretty-print "formatter,index,interning,lock,nullness,regex,signature"
 test_wpi_plume_lib icalavailable     "formatter,index,interning,lock,nullness,regex,signature,initializedfields"
 test_wpi_plume_lib lookup            "formatter,index,interning,lock,nullness,regex,signature"
+test_wpi_plume_lib options           "formatter,index,interning,lock,nullness,regex,signature,initializedfields"
 
 echo "exiting test-wpi-plumelib.sh"
