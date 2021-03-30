04737af45043fb642a93236ed3c8f3f7d81d5a1a
Michael Ernst <mernst@cs.washington.edu>
2021-03-30 16:21:15 -0700

    Undo a change

--- checker/src/main/java/org/checkerframework/checker/formatter/FormatterVisitor.java
+++ checker/src/main/java/org/checkerframework/checker/formatter/FormatterVisitor.java
@@ -131,9 +131,10 @@ public class FormatterVisitor extends BaseTypeVisitor<FormatterAnnotatedTypeFact
             if (!isWrappedFormatCall(fc, enclosingMethod)) {
               ftu.warning(invc, "format.indirect.arguments");
             }
-            // TODO:  If it is explict array construction, such as "new Object[] { ... }", then we
-            // could treat it like the VARARGS case, analyzing each argument.  "new array" is
-            // probably rare, in the varargs position.  fall through
+            // TODO:  If it is explict array construction, such as "new Object[] {
+            // ... }", then we could treat it like the VARARGS case, analyzing each
+            // argument.  "new array" is probably rare, in the varargs position.
+            // fall through
           case NULLARRAY:
             for (ConversionCategory cat : formatCats) {
               if (cat == ConversionCategory.NULL) {
