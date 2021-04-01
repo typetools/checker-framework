0fd7a54bf687bc924de313b25259a695bbcdf185
Michael Ernst <mernst@cs.washington.edu>
2021-04-01 15:41:33 -0700

    Add import

--- checker/src/main/java/org/checkerframework/checker/index/upperbound/OffsetEquation.java
+++ checker/src/main/java/org/checkerframework/checker/index/upperbound/OffsetEquation.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+import java.util.regex.Pattern;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.checkerframework.common.value.ValueAnnotatedTypeFactory;
 import org.checkerframework.common.value.ValueCheckerUtils;
