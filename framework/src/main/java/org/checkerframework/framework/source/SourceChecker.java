e80cf714d1c6d687c0f60832ae68ffef05008f26
Michael Ernst <mernst@cs.washington.edu>
2021-04-01 20:53:32 -0700

    Show stack trace always

--- framework/src/main/java/org/checkerframework/framework/source/SourceChecker.java
+++ framework/src/main/java/org/checkerframework/framework/source/SourceChecker.java
@@ -2528,22 +2528,22 @@ public abstract class SourceChecker extends AbstractTypeProcessor implements Opt
             msg.add(line);
           }
         }
+      }
+    }
 
-        msg.add("Exception: " + ce.getCause() + "; " + UtilPlume.stackTraceToString(ce.getCause()));
-        boolean printClasspath = ce.getCause() instanceof NoClassDefFoundError;
-        Throwable cause = ce.getCause().getCause();
-        while (cause != null) {
-          msg.add("Underlying Exception: " + cause + "; " + UtilPlume.stackTraceToString(cause));
-          printClasspath |= cause instanceof NoClassDefFoundError;
-          cause = cause.getCause();
-        }
+    msg.add("Exception: " + ce.getCause() + "; " + UtilPlume.stackTraceToString(ce.getCause()));
+    boolean printClasspath = ce.getCause() instanceof NoClassDefFoundError;
+    Throwable cause = ce.getCause().getCause();
+    while (cause != null) {
+      msg.add("Underlying Exception: " + cause + "; " + UtilPlume.stackTraceToString(cause));
+      printClasspath |= cause instanceof NoClassDefFoundError;
+      cause = cause.getCause();
+    }
 
-        if (printClasspath) {
-          msg.add("Classpath:");
-          for (URI uri : new ClassGraph().getClasspathURIs()) {
-            msg.add(uri.toString());
-          }
-        }
+    if (printClasspath) {
+      msg.add("Classpath:");
+      for (URI uri : new ClassGraph().getClasspathURIs()) {
+        msg.add(uri.toString());
       }
     }
 
