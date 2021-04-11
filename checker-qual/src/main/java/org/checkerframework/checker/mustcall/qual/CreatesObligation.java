b50e986f8618dca96693f7d1b4066b865b2348db
Michael Ernst <mernst@cs.washington.edu>
2021-04-11 15:55:28 -0700

    Javadoc improvements

--- checker-qual/src/main/java/org/checkerframework/checker/mustcall/qual/CreatesObligation.java
+++ checker-qual/src/main/java/org/checkerframework/checker/mustcall/qual/CreatesObligation.java
@@ -6,6 +6,7 @@ import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.checkerframework.checker.calledmethods.qual.CalledMethods;
 import org.checkerframework.framework.qual.InheritedAnnotation;
 import org.checkerframework.framework.qual.JavaExpression;
 
@@ -14,18 +15,18 @@ import org.checkerframework.framework.qual.JavaExpression;
  * greatest lower bound of its current must-call type and its declared must-call type. This
  * effectively undoes flow-sensitive type refinement.
  *
- * <p>It is an error to call a method annotated by this annotation if the target's declared
- * CalledMethods type is non-empty (i.e. its type is not top in the CalledMethods hierarchy).
+ * <p>When calling a method annotated as {@code @CreatesObligation("}<em>target</em>{@code ")}, the
+ * expression {@code target} must not have type {@code @}{@link CalledMethods}{@code ({})}. That is,
+ * {@code target}'s CalledMethods type must be non-empty.
  *
- * <p>It is an error to fail to write this annotation on any method that (re-)assigns a non-final,
- * owning field whose declared type has a must-call obligation. It is an error if the target of the
- * {@literal @}CreatesObligation annotation on the re-assigning method is not the string "this".
+ * <p>{@code @CreatesObligation("this")} must be written on any method that assigns a non-final,
+ * owning field whose declared type has a must-call obligation.
  *
  * <p>This annotation is trusted, not checked. (Because this annotation can only add obligations,
  * the analysis remains sound.)
  *
- * <p>For example, consider the following code, which uses an {@literal @}CreatesObligation
- * annotation to indicate that the {@code reset()} method re-assigns the {@code socket} field:
+ * <p>For example, consider the following code, which uses a {@code @CreatesObligation} annotation
+ * to indicate that the {@code reset()} method re-assigns the {@code socket} field:
  *
  * <pre>
  * &#64;MustCall("stop")
@@ -46,13 +47,13 @@ import org.checkerframework.framework.qual.JavaExpression;
  * }
  * </pre>
  *
- * A client of {@code SocketContainer} is permitted to call {@code reset()} arbitrarily-many times.
+ * A client of {@code SocketContainer} is permitted to call {@code reset()} arbitrarily many times.
  * Each time it does so, a new {@code Socket} might be created, so only after the last call to
- * reset() may a {@code SocketContainer}'s must-call obligation of "stop" be fulfilled. The
- * {@literal @}CreatesObligation annotation on reset's declaration enforces this requirement: at any
- * call to reset(), all called-methods information about the receiver is removed from the stores of
- * the Must Call and Called Methods Checkers, so the client has to "start over" as if a fresh {@code
- * SocketContainer} object had been created.
+ * {@code reset()} may a {@code SocketContainer}'s must-call obligation of "stop" be fulfilled. The
+ * {@code @CreatesObligation} annotation on reset's declaration enforces this requirement: at any
+ * call to {@code reset()}, all called-methods information about the receiver is removed from the
+ * store of the Must Call Checker and the store of the Called Methods Checker, so the client has to
+ * "start over" as if a fresh {@code SocketContainer} object had been created.
  *
  * @checker_framework.manual #must-call-checker Must Call Checker
  */
