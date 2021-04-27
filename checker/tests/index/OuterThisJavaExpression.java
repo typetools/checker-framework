500c9c0d2c5097d9dc70b709b221a043bc715671
Michael Ernst <mernst@cs.washington.edu>
2021-04-27 12:41:02 -0700

    Use newer StubParser version

--- checker/tests/index/OuterThisJavaExpression.java
+++ checker/tests/index/OuterThisJavaExpression.java
@@ -1,7 +1,5 @@
 // Test case for issue #4558: https://tinyurl.com/cfissue/4558
 
-// @skip-test until the issue is fixed
-
 import org.checkerframework.checker.index.qual.SameLen;
 
 public abstract class OuterThisJavaExpression {
