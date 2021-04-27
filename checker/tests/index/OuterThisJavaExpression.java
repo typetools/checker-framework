243ab53d54babacdc35bc6891c8b43fc4bfc1e85
Michael Ernst <mernst@cs.washington.edu>
2021-04-27 14:36:45 -0700

    Expand test case (which now fails for a different reason)

--- checker/tests/index/OuterThisJavaExpression.java
+++ checker/tests/index/OuterThisJavaExpression.java
@@ -1,5 +1,7 @@
 // Test case for issue #4558: https://tinyurl.com/cfissue/4558
 
+// @skip-test until the issue is fixed
+
 import org.checkerframework.checker.index.qual.SameLen;
 
 public abstract class OuterThisJavaExpression {
@@ -12,11 +14,45 @@ public abstract class OuterThisJavaExpression {
 
   final class Inner {
 
+    String s = "different from " + OuterThisJavaExpression.this.s;
+
     @SameLen("s") String f1() {
       return s;
     }
 
-    @SameLen("OuterThisJavaExpression.this.s") String f2() {
+    @SameLen("s") String f2() {
+      return this.s;
+    }
+
+    @SameLen("s") String f3() {
+      // :: error: (return.type.incompatible)
+      return OuterThisJavaExpression.this.s;
+    }
+
+    @SameLen("this.s") String f4() {
+      return s;
+    }
+
+    @SameLen("this.s") String f5() {
+      return this.s;
+    }
+
+    @SameLen("this.s") String f6() {
+      // :: error: (return.type.incompatible)
+      return OuterThisJavaExpression.this.s;
+    }
+
+    @SameLen("OuterThisJavaExpression.this.s") String f7() {
+      // :: error: (return.type.incompatible)
+      return s;
+    }
+
+    @SameLen("OuterThisJavaExpression.this.s") String f8() {
+      // :: error: (return.type.incompatible)
+      return this.s;
+    }
+
+    @SameLen("OuterThisJavaExpression.this.s") String f9() {
       return OuterThisJavaExpression.this.s;
     }
   }
