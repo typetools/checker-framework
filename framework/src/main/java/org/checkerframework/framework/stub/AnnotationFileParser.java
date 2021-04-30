4899d066ee760811608a9459ba3dcadca943ad00
Michael Ernst <mernst@cs.washington.edu>
2021-04-30 13:18:35 -0700

    Efficiency tweak

--- framework/src/main/java/org/checkerframework/framework/stub/AnnotationFileParser.java
+++ framework/src/main/java/org/checkerframework/framework/stub/AnnotationFileParser.java
@@ -2795,7 +2795,7 @@ public class AnnotationFileParser {
    * @param warning warning to print
    */
   private void stubDebug(String warning) {
-    if (warnings.add(warning) && debugAnnotationFileParser) {
+    if (debugAnnotationFileParser && warnings.add(warning)) {
       processingEnv
           .getMessager()
           .printMessage(javax.tools.Diagnostic.Kind.NOTE, "AnnotationFileParser: " + warning);
