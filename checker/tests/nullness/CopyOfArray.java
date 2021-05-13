aedecdc77b2b6a20207e28f0e0c1d9913736d737
Michael Ernst <mernst@cs.washington.edu>
2021-05-13 09:22:44 -0700

    Make nullness handling of `Arrays.copyOf` more precise

--- checker/tests/nullness/CopyOfArray.java
+++ checker/tests/nullness/CopyOfArray.java
@@ -2,12 +2,12 @@ import java.util.Arrays;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 public class CopyOfArray {
-  protected void makeCopy(Object[] args) {
-    // :: error: (assignment)
-    Object[] copy = Arrays.copyOf(args, args.length);
-  }
+  protected void makeCopy(Object[] args, int i) {
+    Object[] copyExact1 = Arrays.copyOf(args, args.length);
+    @Nullable Object[] copyExact2 = Arrays.copyOf(args, args.length);
 
-  protected void makeCopyGood(Object[] args) {
-    @Nullable Object @Nullable [] copy = Arrays.copyOf(args, args.length);
+    // :: error: (assignment)
+    Object[] copyInexact1 = Arrays.copyOf(args, i);
+    @Nullable Object[] copyInexact2 = Arrays.copyOf(args, i);
   }
 }
