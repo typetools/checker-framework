3ef69c9795b20d85073355ba0b6b0cdcb2a01ecd
Michael Ernst <mernst@cs.washington.edu>
2021-05-23 11:25:04 -0700

    Use simple name

--- checker/src/main/java/org/checkerframework/checker/calledmethods/CalledMethodsAnnotatedTypeFactory.java
+++ checker/src/main/java/org/checkerframework/checker/calledmethods/CalledMethodsAnnotatedTypeFactory.java
@@ -57,7 +57,7 @@ public class CalledMethodsAnnotatedTypeFactory extends AccumulationAnnotatedType
   private final ExecutableElement collectionsSingletonList =
       TreeUtils.getMethod("java.util.Collections", "singletonList", 1, getProcessingEnv());
 
-  /** The value argument to {@link org.checkerframework.checker.calledmethods.qual.CalledMethods} */
+  /** The value argument to {@link CalledMethods}. */
   /* package-private */ final ExecutableElement calledMethodsValueElement =
       TreeUtils.getMethod(CalledMethods.class, "value", 0, processingEnv);
 
