848ab99b1ca6c8bbe8a394fa07b2cbf66047f619
Martin Kellogg <kelloggm@cs.washington.edu>
2021-06-09 16:31:43 -0700

    merge

--- checker/tests/calledmethods/EnsuresCalledMethodsVarArgsSimple.java
+++ checker/tests/calledmethods/EnsuresCalledMethodsVarArgsSimple.java
@@ -18,6 +18,7 @@ class EnsuresCalledMethodsVarArgsSimple {
     }
   }
 
+  // :: error: ensuresvarargs.unverified
   @EnsuresCalledMethodsVarArgs("close")
   // :: error: ensuresvarargs.invalid
   void closeAllNotVA(List<Socket> sockets) {
