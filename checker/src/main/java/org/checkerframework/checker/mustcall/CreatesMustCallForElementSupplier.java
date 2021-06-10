1d5381a0527372b3b215eed99fabb82cdd3be8dd
Martin Kellogg <kelloggm@cs.washington.edu>
2021-06-10 15:30:56 +0000

    rename CreatesObligation to CreatesMustCallFor

--- checker/src/main/java/org/checkerframework/checker/mustcall/CreatesMustCallForElementSupplier.java
+++ checker/src/main/java/org/checkerframework/checker/mustcall/CreatesMustCallForElementSupplier.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.ExecutableElement;
-import org.checkerframework.checker.mustcall.qual.CreatesObligation;
+import org.checkerframework.checker.mustcall.qual.CreatesMustCallFor;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.checkerframework.dataflow.cfg.node.MethodInvocationNode;
 import org.checkerframework.dataflow.expression.JavaExpression;
@@ -17,69 +17,70 @@ import org.checkerframework.javacutil.AnnotationUtils;
 /**
  * This interface should be implemented by all type factories that can provide the ExecutableElement
  * needed to call {@link AnnotationUtils#getElementValueArray} when {@link
- * #getCreatesObligationExpressions(MethodInvocationNode, GenericAnnotatedTypeFactory,
- * CreatesObligationElementSupplier)} is called. This interface is needed so any type factory with
+ * #getCreatesMustCallForExpressions(MethodInvocationNode, GenericAnnotatedTypeFactory,
+ * CreatesMustCallForElementSupplier)} is called. This interface is needed so any type factory with
  * these elements can be used to call that method, not just the MustCallAnnotatedTypeFactory (in
  * particular, the consistency checker needs to be able to call that method with both the
- * ObjectConstruction/CalledMethods type factory and the MustCall type factory).
+ * CalledMethods type factory and the MustCall type factory).
  */
-public interface CreatesObligationElementSupplier {
+public interface CreatesMustCallForElementSupplier {
 
   /**
-   * Returns the CreatesObligation.value field/element.
+   * Returns the CreatesMustCallFor.value field/element.
    *
-   * @return the CreatesObligation.value field/element
+   * @return the CreatesMustCallFor.value field/element
    */
-  ExecutableElement getCreatesObligationValueElement();
+  ExecutableElement getCreatesMustCallForValueElement();
 
   /**
-   * Returns the CreatesObligation.List.value field/element.
+   * Returns the CreatesMustCallFor.List.value field/element.
    *
-   * @return the CreatesObligation.List.value field/element
+   * @return the CreatesMustCallFor.List.value field/element
    */
-  ExecutableElement getCreatesObligationListValueElement();
+  ExecutableElement getCreatesMustCallForListValueElement();
 
   /**
-   * Returns the arguments of the @CreatesObligation annotation on the invoked method, as
-   * JavaExpressions. Returns the empty set if the given method has no @CreatesObligation
+   * Returns the arguments of the @CreatesMustCallFor annotation on the invoked method, as
+   * JavaExpressions. Returns the empty set if the given method has no @CreatesMustCallFor
    * annotation.
    *
    * <p>If any expression is unparseable, this method reports an error and returns the empty set.
    *
    * @param n a method invocation
    * @param atypeFactory the type factory to report errors and parse the expression string
-   * @param supplier a type factory that can supply the executable elements for CreatesObligation
-   *     and CreatesObligation.List's value elements. Usually, you should just pass atypeFactory
+   * @param supplier a type factory that can supply the executable elements for CreatesMustCallFor
+   *     and CreatesMustCallFor.List's value elements. Usually, you should just pass atypeFactory
    *     again. The arguments are different so that the given type factory's adherence to both
    *     protocols are checked by the type system.
-   * @return the arguments of the method's @CreatesObligation annotation, or an empty list
+   * @return the arguments of the method's @CreatesMustCallFor annotation, or an empty list
    */
-  static List<JavaExpression> getCreatesObligationExpressions(
+  static List<JavaExpression> getCreatesMustCallForExpressions(
       MethodInvocationNode n,
       GenericAnnotatedTypeFactory<?, ?, ?, ?> atypeFactory,
-      CreatesObligationElementSupplier supplier) {
-    AnnotationMirror createsObligationList =
-        atypeFactory.getDeclAnnotation(n.getTarget().getMethod(), CreatesObligation.List.class);
+      CreatesMustCallForElementSupplier supplier) {
+    AnnotationMirror createsMustCallForList =
+        atypeFactory.getDeclAnnotation(n.getTarget().getMethod(), CreatesMustCallFor.List.class);
     List<JavaExpression> results = new ArrayList<>(1);
-    if (createsObligationList != null) {
-      // Handle a set of CreatesObligation annotations.
-      List<AnnotationMirror> createsObligations =
+    if (createsMustCallForList != null) {
+      // Handle a set of CreatesMustCallFor annotations.
+      List<AnnotationMirror> createMustCallForAnnos =
           AnnotationUtils.getElementValueArray(
-              createsObligationList,
-              supplier.getCreatesObligationListValueElement(),
+              createsMustCallForList,
+              supplier.getCreatesMustCallForListValueElement(),
               AnnotationMirror.class);
-      for (AnnotationMirror co : createsObligations) {
-        JavaExpression expr = getCreatesObligationExpression(co, n, atypeFactory, supplier);
+      for (AnnotationMirror createsMustCallFor : createMustCallForAnnos) {
+        JavaExpression expr =
+            getCreatesMustCallForExpression(createsMustCallFor, n, atypeFactory, supplier);
         if (expr != null && !results.contains(expr)) {
           results.add(expr);
         }
       }
     }
-    AnnotationMirror createsObligation =
-        atypeFactory.getDeclAnnotation(n.getTarget().getMethod(), CreatesObligation.class);
-    if (createsObligation != null) {
+    AnnotationMirror createsMustCallFor =
+        atypeFactory.getDeclAnnotation(n.getTarget().getMethod(), CreatesMustCallFor.class);
+    if (createsMustCallFor != null) {
       JavaExpression expr =
-          getCreatesObligationExpression(createsObligation, n, atypeFactory, supplier);
+          getCreatesMustCallForExpression(createsMustCallFor, n, atypeFactory, supplier);
       if (expr != null && !results.contains(expr)) {
         results.add(expr);
       }
@@ -88,31 +89,31 @@ public interface CreatesObligationElementSupplier {
   }
 
   /**
-   * Parses a single CreatesObligation annotation. Clients should use {@link
-   * #getCreatesObligationExpressions(MethodInvocationNode, GenericAnnotatedTypeFactory,
-   * CreatesObligationElementSupplier)}, which handles the possibility of multiple such annotations,
-   * instead.
+   * Parses a single CreatesMustCallFor annotation. Clients should use {@link
+   * #getCreatesMustCallForExpressions(MethodInvocationNode, GenericAnnotatedTypeFactory,
+   * CreatesMustCallForElementSupplier)}, which handles the possibility of multiple such
+   * annotations, instead.
    *
-   * @param createsObligation a @CreatesObligation annotation
+   * @param createsMustCallFor a @CreatesMustCallFor annotation
    * @param n the invocation of a reset method
    * @param atypeFactory the type factory
-   * @param supplier a type factory that can supply the executable elements for CreatesObligation
-   *     and CreatesObligation.List's value elements. Usually, you should just pass atypeFactory
+   * @param supplier a type factory that can supply the executable elements for CreatesMustCallFor
+   *     and CreatesMustCallFor.List's value elements. Usually, you should just pass atypeFactory
    *     again. The arguments are different so that the given type factory's adherence to both
    *     protocols are checked by the type system.
    * @return the Java expression representing the target, or null if the target is unparseable
    */
-  static @Nullable JavaExpression getCreatesObligationExpression(
-      AnnotationMirror createsObligation,
+  static @Nullable JavaExpression getCreatesMustCallForExpression(
+      AnnotationMirror createsMustCallFor,
       MethodInvocationNode n,
       GenericAnnotatedTypeFactory<?, ?, ?, ?> atypeFactory,
-      CreatesObligationElementSupplier supplier) {
+      CreatesMustCallForElementSupplier supplier) {
     // Unfortunately, there is no way to avoid passing the default string "this" here. The default
     // must be hard-coded into the client, such as here. That is the price for the efficiency of not
     // having to query the annotation definition (such queries are expensive).
     String targetStrWithoutAdaptation =
         AnnotationUtils.getElementValue(
-            createsObligation, supplier.getCreatesObligationValueElement(), String.class, "this");
+            createsMustCallFor, supplier.getCreatesMustCallForValueElement(), String.class, "this");
     // TODO: find a way to also check if the target is a known tempvar, and if so return that. That
     // should improve the quality of the error messages we give.
     JavaExpression targetExpr;
@@ -132,7 +133,7 @@ public interface CreatesObligationElementSupplier {
   }
 
   /**
-   * Issues a createsobligation.target.unparseable error.
+   * Issues a createsmustcallfor.target.unparseable error.
    *
    * @param n the node
    * @param atypeFactory the type factory to use to issue the error
@@ -146,7 +147,7 @@ public interface CreatesObligationElementSupplier {
         .getChecker()
         .reportError(
             n.getTree(),
-            "createsobligation.target.unparseable",
+            "createsmustcallfor.target.unparseable",
             n.getTarget().getMethod().getSimpleName(),
             unparseable);
   }
