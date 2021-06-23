168d8521510e6fc5a7db392cd314d05ef2069753
Suzanne Millstein <smillst@cs.washington.edu>
2021-06-23 10:12:38 -0700

    Use better api.

--- javacutil/src/main/java/org/checkerframework/javacutil/TreeUtils.java
+++ javacutil/src/main/java/org/checkerframework/javacutil/TreeUtils.java
@@ -67,6 +67,7 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Name;
+import javax.lang.model.element.NestingKind;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.TypeKind;
@@ -1197,8 +1198,8 @@ public final class TreeUtils {
     if (e == null || e.getKind() != ElementKind.CONSTRUCTOR) {
       return false;
     }
-
-    return e.getEnclosingElement().getSimpleName().length() == 0;
+    TypeElement typeElement = (TypeElement) e.getEnclosingElement();
+    return typeElement.getNestingKind() == NestingKind.ANONYMOUS;
   }
 
   /**
