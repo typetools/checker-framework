2a7bf3878ac2341cf21d53c43c5e5ea9161dba13
Martin Kellogg <kelloggm@cs.washington.edu>
2021-07-15 16:59:51 -0700

    add failing test

---
+++ checker/tests/calledmethods/EnsuresCalledMethodsThisLub.java
@@ -0,0 +1,32 @@
+import org.checkerframework.checker.calledmethods.qual.CalledMethods;
+import org.checkerframework.checker.calledmethods.qual.EnsuresCalledMethods;
+
+class EnsuresCalledMethodsThisLub {
+
+  @EnsuresCalledMethods(
+      value = "#1",
+      methods = {"toString", "equals"})
+  void call1(Object obj) {
+    obj.toString();
+    obj.equals(null);
+  }
+
+  @EnsuresCalledMethods(
+      value = "#1",
+      methods = {"toString", "hashCode"})
+  void call2(Object obj) {
+    obj.toString();
+    obj.hashCode();
+  }
+
+  void test(boolean b) {
+    if (b) {
+      call1(this);
+    } else {
+      call2(this);
+    }
+    @CalledMethods("toString") Object obj1 = this;
+    // :: error: (assignment)
+    @CalledMethods({"toString", "equals"}) Object obj2 = this;
+  }
+}
