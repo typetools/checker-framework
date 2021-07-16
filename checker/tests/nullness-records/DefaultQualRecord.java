bf5c45545fb96af9f15a03eae3e87fccb777aa0c
Neil Brown <neil@twistedsquare.com>
2021-07-16 23:03:20 +0100

    Expanded the new test to include static fields in interfaces and enums, which shows up that enums already weren't quite working correctly.

--- checker/tests/nullness-records/DefaultQualRecord.java
+++ checker/tests/nullness-records/DefaultQualRecord.java
@@ -4,16 +4,46 @@ import org.checkerframework.framework.qual.DefaultQualifier;
 class StandardQualClass {
   // :: error: assignment
   public static String s = null;
+  // :: error: initialization.static.field.uninitialized
+  public static String u;
 }
 
 @DefaultQualifier(Nullable.class)
 class DefaultQualClass {
   public static String s = null;
+  public static String u;
+}
+
+interface StandardQualInterface {
+  // :: error: assignment
+  public static String s = null;
+}
+
+@DefaultQualifier(Nullable.class)
+interface DefaultQualInterface {
+  public static String s = null;
+}
+
+enum StandardQualEnum {
+  DUMMY;
+  // :: error: assignment
+  public static String s = null;
+  // :: error: initialization.static.field.uninitialized
+  public static String u;
+}
+
+@DefaultQualifier(Nullable.class)
+enum DefaultQualEnum {
+  DUMMY;
+  public static String s = null;
+  public static String u;
 }
 
 record StandardQualRecord(String m) {
   // :: error: assignment
   public static String s = null;
+  // :: error: initialization.static.field.uninitialized
+  public static String u;
 
   StandardQualRecord {
     // :: error: assignment
@@ -24,6 +54,7 @@ record StandardQualRecord(String m) {
 @DefaultQualifier(Nullable.class)
 record DefaultQualRecord(String m) {
   public static String s = null;
+  public static String u;
 
   DefaultQualRecord {
     m = null;
