1fedede8d84405461c016cf1da97e8bcbbed3b63
Neil Brown <neil@twistedsquare.com>
2021-07-19 07:34:31 +0100

    Added a new test for annotations on records via stub files.

---
+++ checker/tests/stubparser-records/PairUsage.java
@@ -0,0 +1,11 @@
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+class PairUsage {
+  public void makePairs() {
+    PairRecord a = new PairRecord("key", "value");
+    PairRecord b = new PairRecord(null);
+    // :: error : assignment
+    @NonNull Object o = a.value();
+    PairRecord p = new PairRecord("key", null);
+  }
+}
