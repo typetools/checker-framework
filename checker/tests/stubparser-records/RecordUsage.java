e0bd593b5d784177a25cf288516b936971179f0d
Neil Brown <neil@twistedsquare.com>
2021-07-22 21:55:08 +0100

    Added test to reveal next problem with records: specific constructor/accessor in stubs that should override the record component.

---
+++ checker/tests/stubparser-records/RecordUsage.java
@@ -0,0 +1,23 @@
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+class PairUsage {
+  public void makePairs() {
+    PairRecord a = new PairRecord("key", "value");
+    PairRecord b = new PairRecord(null);
+    // :: error: (assignment)
+    @NonNull Object o = a.value();
+    PairRecord p = new PairRecord("key", null);
+  }
+
+  public void makeStubbed() {
+    RecordStubbed r = new RecordStubbed("a", "b", null);
+    // :: error: (argument)
+    RecordStubbed r2 = new RecordStubbed((String) null, "b", null);
+    // :: error: (argument)
+    RecordStubbed r3 = new RecordStubbed("a", null, null);
+    @NonNull Object o = r.nxx();
+    @NonNull Object o2 = r.nsxx();
+    // :: error: (assignment)
+    @NonNull Object o3 = r.xnn();
+  }
+}
