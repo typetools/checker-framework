d0feaf65b0a7cdb5e020d9b1d3979ca386e9fe94
Narges Shadab <n.shadab94@gmail.com>
2021-07-28 15:50:51 -0700

    open an issue

--- checker/tests/resourceleak/MultipleMethodParamsMustCallAliasTest.java
+++ checker/tests/resourceleak/MultipleMethodParamsMustCallAliasTest.java
@@ -27,7 +27,7 @@ class MultipleMethodParamsMustCallAliasTest {
     }
   }
 
-  // Is this a bug in checker framework?
+  // It's a FP, see: https://github.com/typetools/checker-framework/issues/4843
   // :: error: required.method.not.called
   void testMultiMethodParamsCorrect3(@Owning InputStream in1, @Owning InputStream in2)
       throws IOException {
