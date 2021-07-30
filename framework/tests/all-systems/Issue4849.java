ef4acca38636d3a9bfacc68c570a55f149b4dd8e
Suzanne Millstein <smillst@cs.washington.edu>
2021-07-30 09:58:14 -0700

    Fix crash.

---
+++ framework/tests/all-systems/Issue4849.java
@@ -0,0 +1,21 @@
+// The error is reproducable only if the classes are not inner classes.
+final class Issue4849F<T> {}
+
+abstract class C<F1> extends Issue4849G<C<F1>> {}
+
+abstract class Issue4849G<M extends Issue4849G<M>> {
+  abstract M e();
+}
+
+@SuppressWarnings("all") // Just check for crashes.
+abstract class Issue4849 {
+  enum MyEnum {}
+
+  abstract <F1> C<F1> n(Issue4849F<F1> field1);
+
+  abstract <E extends Enum<E>> Issue4849F<E> of(Class<E> e);
+
+  void method() {
+    C<MyEnum> c = this.n(this.of(MyEnum.class)).e();
+  }
+}
