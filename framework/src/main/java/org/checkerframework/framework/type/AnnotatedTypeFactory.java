0e4f659b52381141340660a5a1daae068540e13b
Suzanne Millstein <smillst@cs.washington.edu>
2021-08-11 12:22:28 -0700

    Don't use assignment context.

--- framework/src/main/java/org/checkerframework/framework/type/AnnotatedTypeFactory.java
+++ framework/src/main/java/org/checkerframework/framework/type/AnnotatedTypeFactory.java
@@ -2637,19 +2637,13 @@ public class AnnotatedTypeFactory implements AnnotationProvider {
           .tsym
           .getTypeParameters()
           .nonEmpty()) {
-        Pair<Tree, AnnotatedTypeMirror> ctx = this.visitorState.getAssignmentContext();
-        if (ctx != null) {
-          AnnotatedTypeMirror ctxtype = ctx.second;
+        TreePath p = getPath(newClassTree);
+        AnnotatedTypeMirror ctxtype = TypeArgInferenceUtil.assignedTo(this, p);
+        if (ctxtype != null) {
           fromNewClassContextHelper(type, ctxtype);
         } else {
-          TreePath p = getPath(newClassTree);
-          AnnotatedTypeMirror ctxtype = TypeArgInferenceUtil.assignedTo(this, p);
-          if (ctxtype != null) {
-            fromNewClassContextHelper(type, ctxtype);
-          } else {
-            // give up trying and set to raw.
-            type.setIsUnderlyingTypeRaw();
-          }
+          // give up trying and set to raw.
+          type.setIsUnderlyingTypeRaw();
         }
       }
       AnnotatedDeclaredType fromTypeTree =
