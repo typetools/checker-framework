b62b06fccfa1e42e522e76a3c05abf62b44163cc
Michael Ernst <mernst@cs.washington.edu>
2021-08-12 10:48:10 -0700

    Add comment

--- checker/src/test/java/org/checkerframework/checker/testchecker/disbaruse/DisbarUseChecker.java
+++ checker/src/test/java/org/checkerframework/checker/testchecker/disbaruse/DisbarUseChecker.java
@@ -2,4 +2,8 @@ package org.checkerframework.checker.testchecker.disbaruse;
 
 import org.checkerframework.common.basetype.BaseTypeChecker;
 
+/**
+ * A checker that issues a "disbar.use" error at any use of an expression whose type is
+ * {@code @DisbarUse}.
+ */
 public class DisbarUseChecker extends BaseTypeChecker {}
