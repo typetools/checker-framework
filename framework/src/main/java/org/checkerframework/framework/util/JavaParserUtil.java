a9ec9a88028f49cb2b253ae9a2ac028fdd78891c
Werner Dietl <wdietl@gmail.com>
2021-09-04 20:13:46 -0400

    Use Java 17 as highest supported version

--- framework/src/main/java/org/checkerframework/framework/util/JavaParserUtil.java
+++ framework/src/main/java/org/checkerframework/framework/util/JavaParserUtil.java
@@ -35,9 +35,9 @@ public class JavaParserUtil {
 
     /**
      * The Language Level to use when parsing if a specific level isn't applied. This should be the
-     * highest version of Java that the Checker Framework can process. Currently, Java 16.
+     * highest version of Java that the Checker Framework can process. Currently, Java 17.
      */
-    public static LanguageLevel DEFAULT_LANGUAGE_LEVEL = LanguageLevel.JAVA_16;
+    public static LanguageLevel DEFAULT_LANGUAGE_LEVEL = LanguageLevel.JAVA_17;
 
     ///
     /// Replacements for StaticJavaParser
