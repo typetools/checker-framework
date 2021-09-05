83fab85dfedba63e10e82a6fe47298cdee01407b
Werner Dietl <wdietl@gmail.com>
2021-09-04 20:18:16 -0400

    Expand expected error keys

--- checker/tests/stubparser-records/RecordUsage.java
+++ checker/tests/stubparser-records/RecordUsage.java
@@ -4,7 +4,7 @@ class PairUsage {
     public void makePairs() {
         PairRecord a = new PairRecord("key", "value");
         PairRecord b = new PairRecord(null);
-        // :: error: (assignment)
+        // :: error: (assignment.type.incompatible)
         @NonNull Object o = a.value();
         PairRecord p = new PairRecord("key", null);
     }
@@ -12,13 +12,13 @@ class PairUsage {
     public void makeStubbed() {
         RecordStubbed r = new RecordStubbed("a", "b", 7);
         RecordStubbed r1 = new RecordStubbed("a", "b", null);
-        // :: error: (argument)
+        // :: error: (argument.type.incompatible)
         RecordStubbed r2 = new RecordStubbed((String) null, "b", null);
-        // :: error: (argument)
+        // :: error: (argument.type.incompatible)
         RecordStubbed r3 = new RecordStubbed("a", null, null);
         @NonNull Object o = r.nxx();
         @NonNull Object o2 = r.nsxx();
-        // :: error: (assignment)
+        // :: error: (assignment.type.incompatible)
         @NonNull Object o3 = r.xnn();
     }
 }
