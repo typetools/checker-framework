8c5201208f0b278350a9c8cd865edcc228212c4c
Werner Dietl <wdietl@gmail.com>
2021-11-02 21:46:08 -0400

    Move comments together

--- checker/tests/mustcall/OwningParams.java
+++ checker/tests/mustcall/OwningParams.java
@@ -1,10 +1,8 @@
 // @above-java11-skip-test until this is fixed: https://tinyurl.com/cfissue/4934
-
-// Tests that parameters (including receiver parameters) marked as @Owning are still checked.
-
 // Use of @Owning on receiver parameter doesn't work in Java 17+.
 // The TODO comment below might indicate that it also doesn't work before.
-// @skip-test
+
+// Tests that parameters (including receiver parameters) marked as @Owning are still checked.
 
 import org.checkerframework.checker.mustcall.qual.*;
 
