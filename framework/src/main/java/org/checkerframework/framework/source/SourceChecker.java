70b8cd8351f7c5c13b5c57b5dcb03faf29638538
Werner Dietl <wdietl@gmail.com>
2021-11-02 21:47:25 -0400

    Add back support for the noJreVersionCheck option

--- framework/src/main/java/org/checkerframework/framework/source/SourceChecker.java
+++ framework/src/main/java/org/checkerframework/framework/source/SourceChecker.java
@@ -543,7 +543,10 @@ public abstract class SourceChecker extends AbstractTypeProcessor implements Opt
         // Keep in sync with check in checker-framework/build.gradle and text in installation
         // section of manual.
         int jreVersion = SystemUtil.getJreVersion();
-        if (jreVersion != 8 && jreVersion != 11 && jreVersion != 17) {
+        if (!hasOption("noJreVersionCheck")
+                && jreVersion != 8
+                && jreVersion != 11
+                && jreVersion != 17) {
             message(
                     (hasOption("permitUnsupportedJdkVersion") ? Kind.NOTE : Kind.WARNING),
                     "Use JDK 8, 11, or 17 to run the Checker Framework.  You are using version %d.",
