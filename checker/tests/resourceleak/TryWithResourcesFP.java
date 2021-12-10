85d6d44952e954aed80920a85fd64f7c6916cf42
Werner Dietl <wdietl@gmail.com>
2021-12-09 21:38:11 -0500

    Fix code comment formatting

--- checker/tests/resourceleak/TryWithResourcesFP.java
+++ checker/tests/resourceleak/TryWithResourcesFP.java
@@ -15,10 +15,8 @@ public final class TryWithResourcesFP {
             File outFile = new File(inFile.getName()); // in current directory
             // Delete the file to work around a bug.  Files.newBufferedWriter (which is called by
             // UtilPlume.bufferedFileWriter) seems to have a bug where it does not correctly
-            // truncate the
-            // file first.  If the target file already exists, then characters beyond what is
-            // written
-            // remain in the file.
+            // truncate the file first.  If the target file already exists, then characters beyond
+            // what is written remain in the file.
             outFile.delete();
             try (PrintWriter out =
                             new PrintWriter(UtilPlume.bufferedFileWriter(outFile.toString()));
