77db7754fc2adfb0fb5d2ee68e6a00cf2aaef465
Suzanne Millstein <smillst@cs.washington.edu>
2021-12-13 10:44:32 -0800

    Don't fall through for switch rules.

---
+++ checker/tests/nullness/java17/NullnessSwitchStatementRules.java
@@ -0,0 +1,42 @@
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+public class NullnessSwitchStatementRules {
+  @Nullable Object field = null;
+
+  void method(int selector) {
+    field = new Object();
+    switch (selector) {
+      case 1 -> field = null;
+      case 2 -> field.toString();
+    }
+
+    field = new Object();
+    switch (selector) {
+      case 1 -> {
+        field = null;
+      }
+      case 2 -> {
+        field.toString();
+      }
+    }
+
+    field = new Object();
+    switch (selector) {
+      case 1 -> {
+        field = null;
+      }
+      case 2 -> {
+        field.toString();
+      }
+    }
+
+    field = new Object();
+    switch (selector) {
+      case 1:
+        field = null;
+      case 2:
+        // :: error: (dereference.of.nullable)
+        field.toString();
+    }
+  }
+}
