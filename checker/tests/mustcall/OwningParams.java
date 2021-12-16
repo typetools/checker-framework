946fc030888be9d81dcd2fc3e01e741edeb36890
Martin Kellogg <kelloggm@cs.washington.edu>
2021-12-16 13:28:04 -0800

    add note to documentation of Owning about why it is a declaration annotation, and fix and reenable a test for java 17 (#4989)

--- checker/tests/mustcall/OwningParams.java
+++ checker/tests/mustcall/OwningParams.java
@@ -1,21 +1,13 @@
-// @above-java11-skip-test until this is fixed: https://tinyurl.com/cfissue/4934
-
-// Tests that parameters (including receiver parameters) marked as @Owning are still checked.
+// Tests that parameters marked as @Owning are still checked.
 
 import org.checkerframework.checker.mustcall.qual.*;
 
 class OwningParams {
   static void o1(@Owning OwningParams o) {}
 
-  void o2(@Owning OwningParams this) {}
-
   void test(@Owning @MustCall({"a"}) OwningParams o, @Owning OwningParams p) {
     // :: error: argument
     o1(o);
-    // TODO: this error doesn't show up! See MustCallVisitor#skipReceiverSubtypeCheck
-    //  error: method.invocation
-    o.o2();
     o1(p);
-    p.o2();
   }
 }
