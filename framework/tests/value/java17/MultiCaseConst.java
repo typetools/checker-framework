2ba8d351016552685e11a04194cf6dcda19c4cf1
Suzanne Millstein <smillst@cs.washington.edu>
2021-12-16 10:31:50 -0800

    Add test case.

--- framework/tests/value/java17/MultiCaseConst.java
+++ framework/tests/value/java17/MultiCaseConst.java
@@ -28,4 +28,18 @@ public class MultiCaseConst {
         @IntVal({1, 2, 3, 4, 5}) int tmp3 = selector;
     }
   }
+
+  void method3(int selector) {
+    switch (selector) {
+      case 1 -> {
+        // :: error: (assignment)
+        @IntVal(0) int o = selector;
+        @IntVal({1, 2, 3}) int tmp = selector;
+      }
+      case 4, 5 -> {
+        @IntVal({4, 5}) int tmp2 = selector;
+        @IntVal({1, 2, 3, 4, 5}) int tmp3 = selector;
+      }
+    }
+  }
 }
