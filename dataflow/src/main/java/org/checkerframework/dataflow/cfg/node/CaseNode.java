dcc3db193a6116e5c771f2b6ea1c797f84386639
Werner Dietl <wdietl@gmail.com>
2021-12-19 19:05:03 -0500

    Fix javadoc

--- dataflow/src/main/java/org/checkerframework/dataflow/cfg/node/CaseNode.java
+++ dataflow/src/main/java/org/checkerframework/dataflow/cfg/node/CaseNode.java
@@ -25,10 +25,12 @@ public class CaseNode extends Node {
 
     /** The tree for this node. */
     protected final CaseTree tree;
+
     /**
      * The Node for the assignment of the switch selector expression to a synthetic local variable.
      */
     protected final AssignmentNode selectorExprAssignment;
+
     /**
      * The case expressions to match the switch expression against: the operands of (possibly
      * multiple) case labels.
@@ -39,7 +41,8 @@ public class CaseNode extends Node {
      * Create a new CaseNode.
      *
      * @param tree the tree for this node
-     * @param selectorExprAssignment the switch expression
+     * @param selectorExprAssignment the Node for the assignment of the switch selector expression
+     *     to a synthetic local variable
      * @param caseExprs the case expression(s) to match the switch expression against
      * @param types a factory of utility methods for operating on types
      */
