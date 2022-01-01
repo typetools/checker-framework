a924dde67f3c775c7a44a761e766a5cd6cf30bf0
Manu Sridharan <msridhar@gmail.com>
2022-01-01 15:13:51 -0800

    add javadoc

--- dataflow/src/main/java/org/checkerframework/dataflow/cfg/node/TernaryExpressionNode.java
+++ dataflow/src/main/java/org/checkerframework/dataflow/cfg/node/TernaryExpressionNode.java
@@ -17,9 +17,16 @@ import org.checkerframework.javacutil.TreeUtils;
  */
 public class TernaryExpressionNode extends Node {
 
+  /** The {@code ConditionalExpressionTree} corresponding to this node */
   protected final ConditionalExpressionTree tree;
+
+  /** Node representing the condition checked by the expression */
   protected final Node condition;
+
+  /** Node representing the "then" case of the expression */
   protected final Node thenOperand;
+
+  /** Node representing the "else" case of the expression */
   protected final Node elseOperand;
 
   /**
@@ -28,6 +35,17 @@ public class TernaryExpressionNode extends Node {
    */
   private final LocalVariableNode ternaryExpressionVar;
 
+  /**
+   * Creates a new TernaryExpressionNode.
+   *
+   * @param tree the {@code ConditionalExpressionTree} for the node
+   * @param condition node representing the condition checked by the expression
+   * @param thenOperand node representing the "then" case of the expression
+   * @param elseOperand node representing the "else" case of the expression
+   * @param ternaryExpressionVar a variable created by dataflow to which each case expression of the
+   *     ternary expression is assigned. Its value should be used for the value of the switch
+   *     expression.
+   */
   public TernaryExpressionNode(
       ConditionalExpressionTree tree,
       Node condition,
@@ -43,14 +61,29 @@ public class TernaryExpressionNode extends Node {
     this.ternaryExpressionVar = ternaryExpressionVar;
   }
 
+  /**
+   * Gets the node representing the conditional operand for this node
+   *
+   * @return the condition operand node
+   */
   public Node getConditionOperand() {
     return condition;
   }
 
+  /**
+   * Gets the node representing the "then" operand for this node
+   *
+   * @return the "then" operand node
+   */
   public Node getThenOperand() {
     return thenOperand;
   }
 
+  /**
+   * Gets the node representing the "else" operand for this node
+   *
+   * @return the "else" operand node
+   */
   public Node getElseOperand() {
     return elseOperand;
   }
