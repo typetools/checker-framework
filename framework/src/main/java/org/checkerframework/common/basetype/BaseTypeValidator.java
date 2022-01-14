6cd34fe96e30ae518abfbd4231ac5eee4402aa4b
Suzanne Millstein <smillst@cs.washington.edu>
2022-01-14 10:10:48 -0800

    Add comment.

--- framework/src/main/java/org/checkerframework/common/basetype/BaseTypeValidator.java
+++ framework/src/main/java/org/checkerframework/common/basetype/BaseTypeValidator.java
@@ -603,6 +603,12 @@ public class BaseTypeValidator extends AnnotatedTypeScanner<Void, Tree> implemen
                   capturedTypeVar.getUpperBound());
             }
           } else if (AnnotatedTypes.isExplicitlySuperBounded(wildcard)) {
+            // If the super bound of the wildcard is the same as the upper bound of the type
+            // parameter, then javac uses the bound rather than creating a fresh type variable.
+            // In this case, the Checker Framework uses the annotations on the super bound of the
+            // wildcard and ignores the annotations on the extends bound. So, issue a warning if
+            // the annotations on the extends bound are not the same as the annotations on the super
+            // bound.
             if (!(atypeFactory
                     .getQualifierHierarchy()
                     .isSubtype(
