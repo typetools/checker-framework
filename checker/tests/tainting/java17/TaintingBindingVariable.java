261c0cc6aaa16002ed7b1ecced5a4a96c0f2b9d4
Suzanne Millstein <smillst@cs.washington.edu>
2022-01-28 13:47:08 -0800

    Add more tests.

--- checker/tests/tainting/java17/TaintingBindingVariable.java
+++ checker/tests/tainting/java17/TaintingBindingVariable.java
@@ -4,14 +4,21 @@ import org.checkerframework.checker.tainting.qual.Untainted;
 public class TaintingBindingVariable {
 
   void bar(@Untainted Object o) {
-    if (o instanceof String s) {
+    if (o instanceof @Untainted String s) {
       @Untainted String f = s;
+    }
+    if (o instanceof String s) {
+      @Untainted String f2 = s;
     }
   }
   void bar2( Object o) {
     // :: warning: (instanceof.pattern.unsafe)
     if (o instanceof @Untainted String s) {
       @Untainted String f = s;
+    }
+    if (o instanceof  String s) {
+      // :: error: (assignment)
+      @Untainted String f2 = s;
     }
   }
 }
