8fad51b4880390a453e9ca99c208a69e0cd4c7c0
Suzanne Millstein <smillst@cs.washington.edu>
2022-01-28 09:19:31 -0800

    Handle binding patterns in dataflow.

--- dataflow/src/main/java/org/checkerframework/dataflow/cfg/node/InstanceOfNode.java
+++ dataflow/src/main/java/org/checkerframework/dataflow/cfg/node/InstanceOfNode.java
@@ -27,21 +27,35 @@ public class InstanceOfNode extends Node {
   /** The tree associated with this node. */
   protected final InstanceOfTree tree;
 
+  /** The node of the binding variable is one exists. */
+  protected final @Nullable LocalVariableNode bindingVariable;
+
   /** For Types.isSameType. */
   protected final Types types;
 
   /** Create an InstanceOfNode. */
   public InstanceOfNode(Tree tree, Node operand, TypeMirror refType, Types types) {
+    this(tree, operand, null, refType, types);
+  }
+
+  /** Create an InstanceOfNode. */
+  public InstanceOfNode(
+      Tree tree, Node operand, LocalVariableNode bindingVariable, TypeMirror refType, Types types) {
     super(types.getPrimitiveType(TypeKind.BOOLEAN));
     assert tree.getKind() == Tree.Kind.INSTANCE_OF;
     this.tree = (InstanceOfTree) tree;
     this.operand = operand;
     this.refType = refType;
     this.types = types;
+    this.bindingVariable = bindingVariable;
   }
 
   public Node getOperand() {
     return operand;
+  }
+
+  public @Nullable LocalVariableNode getBindingVariable() {
+    return bindingVariable;
   }
 
   /** The reference type being tested against. */
