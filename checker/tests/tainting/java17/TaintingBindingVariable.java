97cc4f9a2505886108995d460f35ea3ed73699cf
Suzanne Millstein <smillst@cs.washington.edu>
2022-01-28 15:33:12 -0800

    Reformat with Java 17.

--- checker/tests/tainting/java17/TaintingBindingVariable.java
+++ checker/tests/tainting/java17/TaintingBindingVariable.java
@@ -11,12 +11,13 @@ public class TaintingBindingVariable {
       @Untainted String f2 = s;
     }
   }
-  void bar2( Object o) {
+
+  void bar2(Object o) {
     // :: warning: (instanceof.pattern.unsafe)
     if (o instanceof @Untainted String s) {
       @Untainted String f = s;
     }
-    if (o instanceof  String s) {
+    if (o instanceof String s) {
       // :: error: (assignment)
       @Untainted String f2 = s;
     }
