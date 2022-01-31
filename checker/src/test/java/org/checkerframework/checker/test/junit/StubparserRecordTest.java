f7e98788e047c53af3f625f694979298dd92b143
Michael Ernst <mernst@cs.washington.edu>
2022-01-31 08:37:37 -0800

    Use curly braces for `if` statements (#5034)

--- checker/src/test/java/org/checkerframework/checker/test/junit/StubparserRecordTest.java
+++ checker/src/test/java/org/checkerframework/checker/test/junit/StubparserRecordTest.java
@@ -27,8 +27,10 @@ public class StubparserRecordTest extends CheckerFrameworkPerDirectoryTest {
   public static String[] getTestDirs() {
     // Check for JDK 16+ without using a library:
     // There is no decimal point in the JDK 17 version number.
-    if (System.getProperty("java.version").matches("^(1[6-9]|[2-9][0-9])"))
+    if (System.getProperty("java.version").matches("^(1[6-9]|[2-9][0-9])")) {
       return new String[] {"stubparser-records"};
-    else return new String[] {};
+    } else {
+      return new String[] {};
+    }
   }
 }
