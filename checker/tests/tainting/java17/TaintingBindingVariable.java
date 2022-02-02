4409ad75a9efc3bd978ea4f463630b087261d204
Werner Dietl <wdietl@gmail.com>
2022-02-01 20:34:32 -0500

    Fix error key and expand test

--- checker/tests/tainting/java17/TaintingBindingVariable.java
+++ checker/tests/tainting/java17/TaintingBindingVariable.java
@@ -1,4 +1,4 @@
-// @below-java17-jdk-skip-test
+// @below-java16-jdk-skip-test
 import org.checkerframework.checker.tainting.qual.Untainted;
 
 public class TaintingBindingVariable {
@@ -18,8 +18,15 @@ public class TaintingBindingVariable {
             @Untainted String f = s;
         }
         if (o instanceof String s) {
-            // :: error: (assignment)
+            // :: error: (assignment.type.incompatible)
             @Untainted String f2 = s;
+        }
+    }
+
+    void bar3(Object o, boolean b) {
+        // :: warning: (instanceof.pattern.unsafe)
+        if (b && o instanceof @Untainted String s) {
+            @Untainted String f = s;
         }
     }
 }
