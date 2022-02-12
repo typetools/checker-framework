670a0f71fb09560c11530587dbe4b167f6bc3990
Suzanne Millstein <smillst@cs.washington.edu>
2022-02-12 11:02:10 -0800

    Widening from an integral type to an integral type does not change the annotation
    Co-authored-by: Mark Roberts <markro@cs.washington.edu>

--- checker/tests/signedness/CompareChars.java
+++ checker/tests/signedness/CompareChars.java
@@ -4,9 +4,13 @@
 public class CompareChars {
   void compareUnsignedChars(char c2) {
     char c1 = 'a';
+    // :: error: (comparison.unsignedrhs)
     boolean res = c1 > c2;
+    // :: error: (comparison.unsignedrhs)
     res = c1 >= c2;
+    // :: error: (comparison.unsignedrhs)
     res = c1 < c2;
+    // :: error: (comparison.unsignedrhs)
     res = c1 <= c2;
   }
 }
