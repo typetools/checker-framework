b600862b2a939b0b325a970bf30218089d98f6af
Martin Kellogg <kelloggm@cs.washington.edu>
2022-03-02 14:03:51 -0800

    rather than disabling the test, disable jaif inference on varargs params

--- framework/src/main/java/org/checkerframework/common/wholeprograminference/WholeProgramInferenceImplementation.java
+++ framework/src/main/java/org/checkerframework/common/wholeprograminference/WholeProgramInferenceImplementation.java
@@ -214,6 +214,14 @@ public class WholeProgramInferenceImplementation<T> implements WholeProgramInfer
 
       VariableElement ve;
       boolean varargsParam = i >= methodElt.getParameters().size() - 1 && methodElt.isVarArgs();
+      if (varargsParam && this.atypeFactory.wpiOutputFormat == OutputFormat.JAIF) {
+        // The AFU's annotator.Main produces a non-compilable source file when JAIF-based WPI
+        // tries to output an annotated varargs parameter, such as when running the test
+        // checker/tests/ainfer-testchecker/non-annotated/AnonymousAndInnerClass.java.
+        // Until that bug is fixed, do not attempt to infer information about varargs parameters
+        // in JAIF mode.
+        return;
+      }
       if (varargsParam) {
         ve = methodElt.getParameters().get(methodElt.getParameters().size() - 1);
       } else {
