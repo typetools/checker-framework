4f7f65a2bce9c47359e16fe8afc43534796ccd53
Michael Ernst <mernst@cs.washington.edu>
2022-03-17 14:20:46 -0700

    Gradle maintainers no longer recommend `--no-daemon` argument in CI (#5084)

--- checker/bin-devel/test-misc.sh
+++ checker/bin-devel/test-misc.sh
@@ -14,12 +14,12 @@ PLUME_SCRIPTS="$SCRIPTDIR/.plume-scripts"
 
 ## Checker Framework demos
 "$PLUME_SCRIPTS/git-clone-related" typetools checker-framework.demos
-./gradlew :checker:demosTests --console=plain --warning-mode=all --no-daemon
+./gradlew :checker:demosTests --console=plain --warning-mode=all
 
 status=0
 
 ## Code style and formatting
-./gradlew checkBasicStyle checkFormat --console=plain --warning-mode=all --no-daemon
+./gradlew checkBasicStyle checkFormat --console=plain --warning-mode=all
 if grep -n -r --exclude-dir=build --exclude-dir=examples --exclude-dir=jtreg --exclude-dir=tests --exclude="*.astub" --exclude="*.tex" '^\(import static \|import .*\*;$\)'; then
   echo "Don't use static import or wildcard import"
   exit 1
@@ -29,18 +29,18 @@ make -C checker/bin-devel
 make -C docs/developer/release
 
 ## HTML legality
-./gradlew htmlValidate --console=plain --warning-mode=all --no-daemon
+./gradlew htmlValidate --console=plain --warning-mode=all
 
 ## Javadoc documentation
 # Try twice in case of network lossage.
-(./gradlew javadoc --console=plain --warning-mode=all --no-daemon || (sleep 60 && ./gradlew javadoc --console=plain --warning-mode=all --no-daemon)) || status=1
-./gradlew javadocPrivate --console=plain --warning-mode=all --no-daemon || status=1
+(./gradlew javadoc --console=plain --warning-mode=all || (sleep 60 && ./gradlew javadoc --console=plain --warning-mode=all)) || status=1
+./gradlew javadocPrivate --console=plain --warning-mode=all || status=1
 # For refactorings that touch a lot of code that you don't understand, create
 # top-level file SKIP-REQUIRE-JAVADOC.  Delete it after the pull request is merged.
 if [ ! -f SKIP-REQUIRE-JAVADOC ]; then
-  (./gradlew requireJavadoc --console=plain --warning-mode=all --no-daemon > /tmp/warnings-rjp.txt 2>&1) || true
+  (./gradlew requireJavadoc --console=plain --warning-mode=all > /tmp/warnings-rjp.txt 2>&1) || true
   "$PLUME_SCRIPTS"/ci-lint-diff /tmp/warnings-rjp.txt || status=1
-  (./gradlew javadocDoclintAll --console=plain --warning-mode=all --no-daemon > /tmp/warnings-jda.txt 2>&1) || true
+  (./gradlew javadocDoclintAll --console=plain --warning-mode=all > /tmp/warnings-jda.txt 2>&1) || true
   "$PLUME_SCRIPTS"/ci-lint-diff /tmp/warnings-jda.txt || status=1
 fi
 if [ $status -ne 0 ]; then exit $status; fi
