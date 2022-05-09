116fe61537b009f43c585837f183b4d1d8de89d4
Michael Ernst <mernst@cs.washington.edu>
2022-05-09 10:35:55 -0700

    Issue #58 is fixed

--- checker/tests/regex_poly/StringBuilderToStringPolyRegex.java
+++ checker/tests/regex_poly/StringBuilderToStringPolyRegex.java
@@ -1,7 +1,5 @@
 // Test case for issue #58: https://tinyurl.com/cfissue/58
 
-// @skip-test until the issue is fixed
-
 import org.checkerframework.checker.regex.qual.Regex;
 
 class StringBuilderToStringPolyRegex {
