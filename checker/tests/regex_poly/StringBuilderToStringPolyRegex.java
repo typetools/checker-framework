c669f5b3fe5a1bf02ee6dd504e4247598c48a098
Werner Dietl <wdietl@gmail.com>
2022-06-02 11:41:21 -0400

    Add link to follow-up issue

--- checker/tests/regex_poly/StringBuilderToStringPolyRegex.java
+++ checker/tests/regex_poly/StringBuilderToStringPolyRegex.java
@@ -1,4 +1,7 @@
 // Test case for issue #58: https://tinyurl.com/cfissue/58
+// This does not test what #58 wanted. See
+// https://github.com/eisop/checker-framework/issues/242
+// for follow-up discussions.
 
 import org.checkerframework.checker.regex.qual.Regex;
 
