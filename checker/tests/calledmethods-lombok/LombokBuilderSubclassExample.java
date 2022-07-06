5c4559a3b1de3dcff4d8359d18a7571e53c6b105
Jeroen Meijer <jeroen.meijer@eijsink.nl>
2022-07-06 10:59:22 +0200

    Add failing test case for subclassing lombok builders

---
+++ checker/tests/calledmethods-lombok/LombokBuilderSubclassExample.java
@@ -0,0 +1,29 @@
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.Value;
+
+@Builder(builderClassName = "LBSEBuilder")
+@Value public class LombokBuilderSubclassExample {
+
+  @NonNull Integer attribute;
+
+  public static LombokBuilderSubclassExample builder() {
+    return new LombokBuilderSubclassExampleBuilder();
+  }
+
+  public static class LombokBuilderSubclassExampleBuilder extends LBSEBuilder {
+
+    @Override
+    public LombokBuilderSubclassExample build() {
+      final LombokBuilderSubclassExample result = super.build();
+      // here result.getAttribute() is guaranteed to be non null, so we do not have to check this
+      // ourselves
+
+      if (result.getAttribute() < 0) {
+        throw new IllegalArgumentException("attribute must be >= 0");
+      }
+
+      return result;
+    }
+  }
+}
