9aee63670fc1f0c1f22139f91cd36cbc1a5eb2eb
Martin Kellogg <kelloggm@cs.washington.edu>
2022-07-06 20:59:05 -0400

    Ajava-based WPI should not print invisible qualifiers (#5188)

--- checker-qual/src/main/java/org/checkerframework/common/returnsreceiver/qual/UnknownThis.java
+++ checker-qual/src/main/java/org/checkerframework/common/returnsreceiver/qual/UnknownThis.java
@@ -6,6 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.checkerframework.framework.qual.DefaultFor;
 import org.checkerframework.framework.qual.DefaultQualifierInHierarchy;
+import org.checkerframework.framework.qual.InvisibleQualifier;
 import org.checkerframework.framework.qual.LiteralKind;
 import org.checkerframework.framework.qual.QualifierForLiterals;
 import org.checkerframework.framework.qual.SubtypeOf;
@@ -23,4 +24,5 @@ import org.checkerframework.framework.qual.TypeUseLocation;
 @SubtypeOf({})
 @QualifierForLiterals(LiteralKind.NULL)
 @DefaultFor(value = TypeUseLocation.LOWER_BOUND)
+@InvisibleQualifier
 public @interface UnknownThis {}
