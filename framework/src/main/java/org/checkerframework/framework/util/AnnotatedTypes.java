22b6d5a8c3f412fa496dc228c2a6b13b664f6002
Michael Ernst <mernst@cs.washington.edu>
2022-07-09 17:53:44 -0700

    Handle passing no arguments to a varargs method

--- framework/src/main/java/org/checkerframework/framework/util/AnnotatedTypes.java
+++ framework/src/main/java/org/checkerframework/framework/util/AnnotatedTypes.java
@@ -987,6 +987,9 @@ public class AnnotatedTypes {
     if (!method.getElement().isVarArgs()) {
       return parameters;
     }
+    if (parameters.size() == 0) {
+      return parameters;
+    }
 
     AnnotatedArrayType varargs = (AnnotatedArrayType) parameters.get(parameters.size() - 1);
 
