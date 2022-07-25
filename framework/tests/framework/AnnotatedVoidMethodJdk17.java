38f70e426a3f724dc3d1edd5afd9dade7ee5c3d5
Michael Ernst <mernst@cs.washington.edu>
2022-07-24 20:01:47 -0700

    Correct "skip-test" notation

--- framework/tests/framework/AnnotatedVoidMethodJdk17.java
+++ framework/tests/framework/AnnotatedVoidMethodJdk17.java
@@ -1,6 +1,6 @@
 import org.checkerframework.framework.testchecker.util.Odd;
 
-// @above-java17-skip-test the error message changed in JDK 18
+// @above-java17-jdk-skip-test the error message changed in JDK 18
 
 public class AnnotatedVoidMethodJdk17 {
   // :: error: annotation type not applicable to this kind of declaration
--- framework/tests/framework/AnnotatedVoidMethodJdk18.java
+++ framework/tests/framework/AnnotatedVoidMethodJdk18.java
@@ -1,6 +1,6 @@
 import org.checkerframework.framework.testchecker.util.Odd;
 
-// @below-java18-skip-test the error message changed in JDK 18
+// @below-java18-jdk-skip-test the error message changed in JDK 18
 
 public class AnnotatedVoidMethodJdk18 {
   // :: error: annotation interface not applicable to this kind of declaration
