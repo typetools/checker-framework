803bc76c71944cf53956771b669c570dc2ea3517
Michael Ernst <mernst@cs.washington.edu>
2022-08-09 14:52:22 -0700

    Don't compile JSpecify (compiling it fails under JDK 17)

--- checker/bin-devel/build.sh
+++ checker/bin-devel/build.sh
@@ -55,23 +55,25 @@ echo "Running:  (cd ../stubparser/ && ./.build-without-test.sh)"
 echo "... done: (cd ../stubparser/ && ./.build-without-test.sh)"
 
 
-## Build JSpecify, only for the purpose of using its tests.
-"$PLUME_SCRIPTS/git-clone-related" jspecify jspecify
-if type -p java; then
-  _java=java
-elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
-  _java="$JAVA_HOME/bin/java"
-else
-  echo "Can't find java"
-  exit 1
-fi
-version=$("$_java" -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
-if [[ "$version" -ge 9 ]]; then
-  echo "Running:  (cd ../jspecify/ && ./gradlew build)"
-  # If failure, retry in case the failure was due to network lossage.
-  (cd ../jspecify/ && export JDK_JAVA_OPTIONS='--add-opens jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED' && (./gradlew build || (sleep 60 && ./gradlew build)))
-  echo "... done: (cd ../jspecify/ && ./gradlew build)"
-fi
+### Commented temporarily because JSpecify build is failing.
+### (I guess they don't use continuous integration.)
+# ## Build JSpecify, only for the purpose of using its tests.
+# "$PLUME_SCRIPTS/git-clone-related" jspecify jspecify
+# if type -p java; then
+#   _java=java
+# elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+#   _java="$JAVA_HOME/bin/java"
+# else
+#   echo "Can't find java"
+#   exit 1
+# fi
+# version=$("$_java" -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
+# if [[ "$version" -ge 9 ]]; then
+#   echo "Running:  (cd ../jspecify/ && ./gradlew build)"
+#   # If failure, retry in case the failure was due to network lossage.
+#   (cd ../jspecify/ && export JDK_JAVA_OPTIONS='--add-opens jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED' && (./gradlew build || (sleep 60 && ./gradlew build)))
+#   echo "... done: (cd ../jspecify/ && ./gradlew build)"
+# fi
 
 
 ## Compile
