b3ee22bbfa3b22609d2bfe0438e06fb5b577e167
Michael Ernst <mernst@cs.washington.edu>
2022-08-09 12:34:17 -0700

    Improve variable naming and comments

--- checker/tests/mustcall/NotInheritableMustCallOnClassError.java
+++ checker/tests/mustcall/NotInheritableMustCallOnClassError.java
@@ -5,7 +5,7 @@
 import org.checkerframework.checker.mustcall.qual.*;
 
 @MustCall("foo")
-// :: error: mustcall.not.inheritable
+// :: warning: mustcall.not.inheritable
 public class NotInheritableMustCallOnClassError {
   public void foo() {}
 }
