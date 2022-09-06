5ba8dff40b8577852f7101ba921827dc71e762f5
Michael Ernst <mernst@cs.washington.edu>
2022-09-06 08:11:39 -0700

    Use JDK 11+ formatting

--- checker/src/test/java/org/checkerframework/checker/test/junit/NullnessJavadocTest.java
+++ checker/src/test/java/org/checkerframework/checker/test/junit/NullnessJavadocTest.java
@@ -12,7 +12,9 @@ import org.junit.runners.Parameterized.Parameters;
  */
 public class NullnessJavadocTest extends CheckerFrameworkPerDirectoryTest {
 
-  /** @param testFiles the files containing test code, which will be type-checked */
+  /**
+   * @param testFiles the files containing test code, which will be type-checked
+   */
   public NullnessJavadocTest(List<File> testFiles) {
     super(
         testFiles,
