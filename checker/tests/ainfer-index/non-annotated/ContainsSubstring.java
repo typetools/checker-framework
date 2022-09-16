b6629927c05118ca21c51814991b4e283cbdf1c8
Martin Kellogg <martin.kellogg@njit.edu>
2022-09-16 10:20:32 -0400

    failing test

---
+++ checker/tests/ainfer-index/non-annotated/ContainsSubstring.java
@@ -0,0 +1,17 @@
+// Based on a snippet of code that caused a malformed ajava file to be
+// produced by WPI. The offending ajava file contained an @SameLen annotation
+// whose argument was a constant String, e.g., @SameLen({ ""Hamburg"", "word1" })
+
+public class ContainsSubstring {
+
+  public static void run() {
+    String word1 = "Hamburg";
+    String word2 = "burg";
+    System.out.println(compute(word1, word2));
+  }
+
+  public static boolean compute(String word1, String word2) {
+    // content doesn't matter
+    return false;
+  }
+}
