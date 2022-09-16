aabe71dad35579471fa4a5e1ed573fb67ba42c93
Michael Ernst <mernst@cs.washington.edu>
2022-09-15 17:19:16 -0700

    Temporarily support JDK 19

--- framework/src/main/java/org/checkerframework/framework/source/SourceChecker.java
+++ framework/src/main/java/org/checkerframework/framework/source/SourceChecker.java
@@ -545,10 +545,15 @@ public abstract class SourceChecker extends AbstractTypeProcessor implements Opt
     // Keep in sync with check in checker-framework/build.gradle and text in installation
     // section of manual.
     int jreVersion = SystemUtil.jreVersion;
-    if (jreVersion != 8 && jreVersion != 11 && jreVersion != 17 && jreVersion != 18) {
+    if (jreVersion != 8
+        && jreVersion != 11
+        && jreVersion != 17
+        && jreVersion != 18
+        && jreVersion != 19) {
       message(
           (hasOption("permitUnsupportedJdkVersion") ? Kind.NOTE : Kind.WARNING),
-          "The Checker Framework is tested with JDK 8, 11, 17, and 18.  You are using version %d.",
+          "The Checker Framework is tested with JDK 8, 11, 17, 18, and 19.  You are using version"
+              + " %d.",
           jreVersion);
     }
 
