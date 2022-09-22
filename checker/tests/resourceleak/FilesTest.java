1df189db79372ad8c81fac06707ef01c1243462f
Michael Ernst <mernst@cs.washington.edu>
2022-09-22 13:48:31 -0700

    Add test of `java.nio.file.Files` method annotations

---
+++ checker/tests/resourceleak/FilesTest.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+import org.checkerframework.checker.mustcall.qual.MustCall;
+
+public class FilesTest {
+
+  void bad(Path p) throws IOException {
+    // :: error: (required.method.not.called)
+    Stream<Path> s = Files.list(p);
+  }
+
+  void good(Path p) throws IOException {
+    // TODO: Programmers should not be required to write this `@MustCall("close")`.
+    try (@MustCall("close") Stream<Path> s = Files.list(p)) {
+      // empty body
+    }
+  }
+}
