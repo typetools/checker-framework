77c2021c1a02e395d9ec0e46813228ba5c17d2d6
Martin Kellogg <martin.kellogg@njit.edu>
2022-09-26 15:34:46 -0400

    actually comment out the tests correctly

--- checker/tests/ainfer-index/non-annotated/DependentTypesViewpointAdaptationTest.java
+++ checker/tests/ainfer-index/non-annotated/DependentTypesViewpointAdaptationTest.java
@@ -61,13 +61,13 @@ public class DependentTypesViewpointAdaptationTest {
   //
   public void compute5(DependentTypesViewpointAdaptationTest this,
       DependentTypesViewpointAdaptationTest other) {
-    // warning: assignment
+    // should be :: warning: assignment when the next line is uncommented
     // @SameLen("this") DependentTypesViewpointAdaptationTest myOther = other;
   }
 
   // Same as compute5, but without an explicit this parameter.
   public void compute6(DependentTypesViewpointAdaptationTest other) {
-    // warning: assignment
+    // should be :: warning: assignment when the next line is uncommented
     // @SameLen("this") DependentTypesViewpointAdaptationTest myOther = other;
   }
 }
