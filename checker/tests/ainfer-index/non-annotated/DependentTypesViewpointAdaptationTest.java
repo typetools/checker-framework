27e5fc19bec861ac94909475c503e01376c1810e
Martin Kellogg <martin.kellogg@njit.edu>
2022-09-26 14:47:28 -0400

    test that Suzanne was actually talking about

--- checker/tests/ainfer-index/non-annotated/DependentTypesViewpointAdaptationTest.java
+++ checker/tests/ainfer-index/non-annotated/DependentTypesViewpointAdaptationTest.java
@@ -14,12 +14,11 @@ public class DependentTypesViewpointAdaptationTest {
     // to compute(), to test that such local variables are viewpoint-adapted
     // correctly.
     String word3 = word1;
-    System.out.println(compute(word1, word2));
+    compute(word1, word2);
   }
 
-  public static boolean compute(String word1, String otherWord) {
+  public static void compute(String word1, String otherWord) {
     // content doesn't matter
-    return false;
   }
 
   public static void receiverTest(@SameLen("#2") DependentTypesViewpointAdaptationTest t1,
@@ -27,29 +26,44 @@ public class DependentTypesViewpointAdaptationTest {
     t1.compute2(t2);
   }
 
-  public boolean compute2(DependentTypesViewpointAdaptationTest this,
+  public void compute2(DependentTypesViewpointAdaptationTest this,
       DependentTypesViewpointAdaptationTest other) {
     // content doesn't matter
-    return false;
   }
 
   public void thisTest(@SameLen("this") DependentTypesViewpointAdaptationTest t1) {
     compute3(this, t1);
   }
 
-  public static boolean compute3(DependentTypesViewpointAdaptationTest t1,
+  public static void compute3(DependentTypesViewpointAdaptationTest t1,
       DependentTypesViewpointAdaptationTest t2) {
     // content doesn't matter
-    return false;
   }
 
   public void thisTestNoUse(@SameLen("this") DependentTypesViewpointAdaptationTest t1) {
     compute4(t1, t1);
   }
 
-  public static boolean compute4(DependentTypesViewpointAdaptationTest t1,
+  public static void compute4(DependentTypesViewpointAdaptationTest t1,
       DependentTypesViewpointAdaptationTest t2) {
     // content doesn't matter
-    return false;
+  }
+
+  public static void testThisInference(DependentTypesViewpointAdaptationTest t1,
+      @SameLen("#1") DependentTypesViewpointAdaptationTest t2) {
+    t1.compute5(t2);
+    t1.compute6(t2);
+  }
+
+  public void compute5(DependentTypesViewpointAdaptationTest this,
+      DependentTypesViewpointAdaptationTest other) {
+    // :: warning: assignment
+    @SameLen("this") DependentTypesViewpointAdaptationTest myOther = other;
+  }
+
+  // Same as compute5, but without an explicit this parameter.
+  public void compute6(DependentTypesViewpointAdaptationTest other) {
+    // :: warning: assignment
+    @SameLen("this") DependentTypesViewpointAdaptationTest myOther = other;
   }
 }
