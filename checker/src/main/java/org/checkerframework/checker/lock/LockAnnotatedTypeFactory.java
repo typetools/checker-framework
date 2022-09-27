95ecff574e1769db195c60f415df331d3b8f5e38
Michael Ernst <mernst@cs.washington.edu>
2022-09-27 13:21:35 -0700

    Checkpoint

--- checker/src/main/java/org/checkerframework/checker/lock/LockAnnotatedTypeFactory.java
+++ checker/src/main/java/org/checkerframework/checker/lock/LockAnnotatedTypeFactory.java
@@ -150,13 +150,13 @@ public class LockAnnotatedTypeFactory
 
     // This alias is only true for the Lock Checker. All other checkers must
     // ignore the @LockingFree annotation.
-    addAliasedDeclAnnotation(LockingFree.class, SideEffectFree.class, SIDEEFFECTFREE);
+    // addAliasedDeclAnnotation(LockingFree.class, SideEffectFree.class, SIDEEFFECTFREE);
 
     // This alias is only true for the Lock Checker. All other checkers must
     // ignore the @ReleasesNoLocks annotation.  Note that ReleasesNoLocks is
     // not truly side-effect-free even as far as the Lock Checker is concerned,
     // so there is additional handling of this annotation in the Lock Checker.
-    addAliasedDeclAnnotation(ReleasesNoLocks.class, SideEffectFree.class, SIDEEFFECTFREE);
+    // addAliasedDeclAnnotation(ReleasesNoLocks.class, SideEffectFree.class, SIDEEFFECTFREE);
 
     jcipGuardedBy = classForNameOrNull("net.jcip.annotations.GuardedBy");
 
