0205a576dfcb4d63f5f4401c007101eec2b26af6
Michael Ernst <mernst@cs.washington.edu>
2022-09-27 07:43:18 -0700

    Cleanup "infer purity" commit

--- checker/src/main/java/org/checkerframework/checker/lock/LockVisitor.java
+++ checker/src/main/java/org/checkerframework/checker/lock/LockVisitor.java
@@ -86,13 +86,6 @@ public class LockVisitor extends BaseTypeVisitor<LockAnnotatedTypeFactory> {
    */
   public LockVisitor(BaseTypeChecker checker) {
     super(checker);
-    for (String checkerName : atypeFactory.getCheckerNames()) {
-      if (!(checkerName.equals("lock")
-          || checkerName.equals("LockChecker")
-          || checkerName.equals("org.checkerframework.checker.lock.LockChecker"))) {
-        break;
-      }
-    }
   }
 
   @Override
