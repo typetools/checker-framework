485ea823c6e80a7335eddad6039a25dfb62e8ce2
Michael Ernst <mernst@cs.washington.edu>
2022-09-27 12:34:27 -0700

    Check purity annotations

--- framework/src/test/java/org/checkerframework/framework/test/junit/FlowTest.java
+++ framework/src/test/java/org/checkerframework/framework/test/junit/FlowTest.java
@@ -13,7 +13,7 @@ public class FlowTest extends CheckerFrameworkPerDirectoryTest {
    * @param testFiles the files containing test code, which will be type-checked
    */
   public FlowTest(List<File> testFiles) {
-    super(testFiles, FlowTestChecker.class, "flow", "-Anomsgtext");
+    super(testFiles, FlowTestChecker.class, "flow", "-Anomsgtext", "-AcheckPurityAnnotations");
   }
 
   @Parameters
