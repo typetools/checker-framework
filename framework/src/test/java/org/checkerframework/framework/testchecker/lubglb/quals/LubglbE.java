4f03301a6e086e665e6ed720b873eec2f949c9b7
Michael Ernst <mernst@cs.washington.edu>
2022-11-29 09:13:28 -0800

    Rename lubglb qualifiers

--- framework/src/test/java/org/checkerframework/framework/testchecker/lubglb/quals/LubglbE.java
+++ framework/src/test/java/org/checkerframework/framework/testchecker/lubglb/quals/LubglbE.java
@@ -5,12 +5,10 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import org.checkerframework.framework.qual.DefaultQualifierInHierarchy;
 import org.checkerframework.framework.qual.SubtypeOf;
 
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
-@DefaultQualifierInHierarchy
-@SubtypeOf({})
-public @interface A {}
+@SubtypeOf({LubglbC.class})
+public @interface LubglbE {}
