81f8ce68000a041eb305c4baa3654f77f65b5adc
Michael Ernst <mernst@cs.washington.edu>
2022-12-29 10:12:20 -0800

    `@LessThanOrEqual` does not exist

--- checker-qual/src/main/java/org/checkerframework/checker/index/qual/LessThan.java
+++ checker-qual/src/main/java/org/checkerframework/checker/index/qual/LessThan.java
@@ -15,8 +15,6 @@ import org.checkerframework.framework.qual.SubtypeOf;
  * <p>If an expression's type has this annotation, then at run time, the expression evaluates to a
  * value that is less than the value of the expression in the annotation.
  *
- * <p>{@code @LessThan("end + 1")} is equivalent to {@code @LessThanOrEqual("end")}.
- *
  * <p>Subtyping:
  *
  * <ul>
