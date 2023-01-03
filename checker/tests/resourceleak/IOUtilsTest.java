394d927b6914c6edbba1da183563df0a8478bd64
Manu Sridharan <msridhar@gmail.com>
2023-01-03 11:59:13 -0800

    stub fixes

--- checker/tests/resourceleak/IOUtilsTest.java
+++ checker/tests/resourceleak/IOUtilsTest.java
@@ -1,7 +1,16 @@
 import org.checkerframework.checker.mustcall.qual.*;
+import java.io.*;
 
 class IOUtilsTest {
-  static void foo(@Owning java.io.InputStream inputStream) {
+  static void test1(@Owning InputStream inputStream) {
     org.apache.commons.io.IOUtils.closeQuietly(inputStream);
+  }
+
+  static void test2(@Owning InputStream inputStream) throws IOException {
+    try {
+      InputStream other = org.apache.commons.io.IOUtils.toBufferedInputStream(inputStream);
+    } finally {
+      inputStream.close();
+    }
   }
 }
