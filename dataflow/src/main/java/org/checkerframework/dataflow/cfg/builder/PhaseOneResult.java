062027ab113b9a5b70f46f813f2c389d2153899c
Michael Ernst <mernst@cs.washington.edu>
2023-01-06 16:52:19 -0800

    Improve naming for lookup maps

--- dataflow/src/main/java/org/checkerframework/dataflow/cfg/builder/PhaseOneResult.java
+++ dataflow/src/main/java/org/checkerframework/dataflow/cfg/builder/PhaseOneResult.java
@@ -30,19 +30,19 @@ public class PhaseOneResult {
    * Maps from AST {@link Tree}s to sets of {@link Node}s. Every Tree that produces a value will
    * have at least one corresponding Node. Trees that undergo conversions, such as boxing or
    * unboxing, can map to two distinct Nodes. The Node for the pre-conversion value is stored in the
-   * treeLookupMap, while the Node for the post-conversion value is stored in the
-   * convertedTreeLookupMap.
+   * treeToCfgNodes, while the Node for the post-conversion value is stored in the
+   * treeToConvertedCfgNodes.
    */
-  final IdentityHashMap<Tree, Set<Node>> treeLookupMap;
+  final IdentityHashMap<Tree, Set<Node>> treeToCfgNodes;
 
   /** Map from AST {@link Tree}s to post-conversion sets of {@link Node}s. */
-  final IdentityHashMap<Tree, Set<Node>> convertedTreeLookupMap;
+  final IdentityHashMap<Tree, Set<Node>> treeToConvertedCfgNodes;
 
   /**
    * Map from postfix increment or decrement trees that are AST {@link UnaryTree}s to the synthetic
    * tree that is {@code v + 1} or {@code v - 1}.
    */
-  final IdentityHashMap<UnaryTree, BinaryTree> postfixLookupMap;
+  final IdentityHashMap<UnaryTree, BinaryTree> postfixTreeToCfgNodes;
 
   /** The list of extended nodes. */
   final ArrayList<ExtendedNode> nodeList;
@@ -79,9 +79,9 @@ public class PhaseOneResult {
 
   public PhaseOneResult(
       UnderlyingAST underlyingAST,
-      IdentityHashMap<Tree, Set<Node>> treeLookupMap,
-      IdentityHashMap<Tree, Set<Node>> convertedTreeLookupMap,
-      IdentityHashMap<UnaryTree, BinaryTree> postfixLookupMap,
+      IdentityHashMap<Tree, Set<Node>> treeToCfgNodes,
+      IdentityHashMap<Tree, Set<Node>> treeToConvertedCfgNodes,
+      IdentityHashMap<UnaryTree, BinaryTree> postfixTreeToCfgNodes,
       ArrayList<ExtendedNode> nodeList,
       Map<Label, Integer> bindings,
       Set<Integer> leaders,
@@ -91,9 +91,9 @@ public class PhaseOneResult {
       List<ClassTree> declaredClasses,
       List<LambdaExpressionTree> declaredLambdas) {
     this.underlyingAST = underlyingAST;
-    this.treeLookupMap = treeLookupMap;
-    this.convertedTreeLookupMap = convertedTreeLookupMap;
-    this.postfixLookupMap = postfixLookupMap;
+    this.treeToCfgNodes = treeToCfgNodes;
+    this.treeToConvertedCfgNodes = treeToConvertedCfgNodes;
+    this.postfixTreeToCfgNodes = postfixTreeToCfgNodes;
     this.nodeList = nodeList;
     this.bindings = bindings;
     this.leaders = leaders;
@@ -166,9 +166,9 @@ public class PhaseOneResult {
     StringJoiner result =
         new StringJoiner(
             String.format("%n  "), String.format("PhaseOneResult{%n  "), String.format("%n  }"));
-    result.add("treeLookupMap=" + mapToString(treeLookupMap));
-    result.add("convertedTreeLookupMap=" + mapToString(convertedTreeLookupMap));
-    result.add("postfixLookupMap=" + mapToString(postfixLookupMap));
+    result.add("treeToCfgNodes=" + mapToString(treeToCfgNodes));
+    result.add("treeToConvertedCfgNodes=" + mapToString(treeToConvertedCfgNodes));
+    result.add("postfixTreeToCfgNodes=" + mapToString(postfixTreeToCfgNodes));
     result.add("underlyingAST=" + underlyingAST);
     result.add("bindings=" + bindings);
     result.add("nodeList=" + CFGBuilder.extendedNodeCollectionToStringDebug(nodeList));
