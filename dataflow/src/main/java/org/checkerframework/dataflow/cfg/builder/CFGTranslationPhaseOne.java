653703826c648bf24a0dca26c892c520a97d2cd3
Michael Ernst <mernst@cs.washington.edu>
2023-01-08 10:07:58 -0800

    Set collection sizes in `dataflow`

--- dataflow/src/main/java/org/checkerframework/dataflow/cfg/builder/CFGTranslationPhaseOne.java
+++ dataflow/src/main/java/org/checkerframework/dataflow/cfg/builder/CFGTranslationPhaseOne.java
@@ -63,7 +63,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.IdentityHashMap;
-import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
@@ -163,6 +162,7 @@ import org.checkerframework.dataflow.qual.TerminatesExecution;
 import org.checkerframework.dataflow.util.IdentityMostlySingleton;
 import org.checkerframework.javacutil.AnnotationProvider;
 import org.checkerframework.javacutil.BugInCF;
+import org.checkerframework.javacutil.CollectionUtils;
 import org.checkerframework.javacutil.ElementUtils;
 import org.checkerframework.javacutil.Pair;
 import org.checkerframework.javacutil.TreePathUtil;
@@ -170,6 +170,8 @@ import org.checkerframework.javacutil.TreeUtils;
 import org.checkerframework.javacutil.TypeKindUtils;
 import org.checkerframework.javacutil.TypesUtils;
 import org.checkerframework.javacutil.trees.TreeBuilder;
+import org.plumelib.util.ArrayMap;
+import org.plumelib.util.ArraySet;
 import org.plumelib.util.CollectionsPlume;
 
 /**
@@ -419,10 +421,10 @@ public class CFGTranslationPhaseOne extends TreeScanner<Node, Void> {
     noClassDefFoundErrorType = maybeGetTypeMirror(NoClassDefFoundError.class);
     stringType = getTypeMirror(String.class);
     throwableType = getTypeMirror(Throwable.class);
-    uncheckedExceptionTypes = new LinkedHashSet<>(2);
+    uncheckedExceptionTypes = new ArraySet<>(2);
     uncheckedExceptionTypes.add(getTypeMirror(RuntimeException.class));
     uncheckedExceptionTypes.add(getTypeMirror(Error.class));
-    newArrayExceptionTypes = new LinkedHashSet<>(2);
+    newArrayExceptionTypes = new ArraySet<>(2);
     newArrayExceptionTypes.add(negativeArraySizeExceptionType);
     if (outOfMemoryErrorType != null) {
       newArrayExceptionTypes.add(outOfMemoryErrorType);
@@ -694,7 +696,7 @@ public class CFGTranslationPhaseOne extends TreeScanner<Node, Void> {
   protected NodeWithExceptionsHolder extendWithNodeWithExceptions(
       Node node, Set<TypeMirror> causes) {
     addToLookupMap(node);
-    Map<TypeMirror, Set<Label>> exceptions = new LinkedHashMap<>(causes.size());
+    Map<TypeMirror, Set<Label>> exceptions = new ArrayMap<>(causes.size());
     for (TypeMirror cause : causes) {
       exceptions.put(cause, tryStack.possibleLabels(cause));
     }
@@ -713,7 +715,7 @@ public class CFGTranslationPhaseOne extends TreeScanner<Node, Void> {
    * @return the node holder
    */
   protected NodeWithExceptionsHolder extendWithClassNameNode(ClassNameNode node) {
-    Set<TypeMirror> thrownSet = new LinkedHashSet<>(4);
+    Set<TypeMirror> thrownSet = new ArraySet<>(4);
     if (classCircularityErrorType != null) {
       thrownSet.add(classCircularityErrorType);
     }
@@ -756,7 +758,7 @@ public class CFGTranslationPhaseOne extends TreeScanner<Node, Void> {
   protected NodeWithExceptionsHolder insertNodeWithExceptionsAfter(
       Node node, Set<TypeMirror> causes, Node pred) {
     addToLookupMap(node);
-    Map<TypeMirror, Set<Label>> exceptions = new LinkedHashMap<>(causes.size());
+    Map<TypeMirror, Set<Label>> exceptions = new ArrayMap<>(causes.size());
     for (TypeMirror cause : causes) {
       exceptions.put(cause, tryStack.possibleLabels(cause));
     }
@@ -3349,7 +3351,8 @@ public class CFGTranslationPhaseOne extends TreeScanner<Node, Void> {
 
     List<? extends TypeMirror> thrownTypes = constructor.getThrownTypes();
     Set<TypeMirror> thrownSet =
-        new LinkedHashSet<>(thrownTypes.size() + uncheckedExceptionTypes.size());
+        CollectionUtils.newArrayOrLinkedHashSet(
+            thrownTypes.size() + uncheckedExceptionTypes.size());
     // Add exceptions explicitly mentioned in the throws clause.
     thrownSet.addAll(thrownTypes);
     // Add types to account for unchecked exceptions
--- javacutil/src/main/java/org/checkerframework/javacutil/CollectionUtils.java
+++ javacutil/src/main/java/org/checkerframework/javacutil/CollectionUtils.java
@@ -1,7 +1,14 @@
 package org.checkerframework.javacutil;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Set;
+import org.plumelib.util.ArrayMap;
+import org.plumelib.util.ArraySet;
+import org.plumelib.util.CollectionsPlume;
 
 /** Utility methods related to Java Collections. */
 public class CollectionUtils {
@@ -22,5 +29,129 @@ public class CollectionUtils {
         return size() > size;
       }
     };
+  }
+
+  // TODO: move into ArrayMap.java.
+
+  /**
+   * Returns a new ArrayMap or HashMap with the given capacity. Uses an ArrayMap if the capacity is
+   * small, and a HashMap otherwise.
+   *
+   * @param capacity the expected maximum number of elements in the set
+   * @return a new ArrayMap or HashMap with the given capacity
+   */
+  public static <K, V> Map<K, V> newArrayOrHashMap(int capacity) {
+    if (capacity <= 4) {
+      return new ArrayMap<>(capacity);
+    } else {
+      return new HashMap<>(CollectionsPlume.mapCapacity(capacity));
+    }
+  }
+
+  /**
+   * Returns a new ArrayMap or HashMap with the given elements. Uses an ArrayMap if the capacity is
+   * small, and a HashMap otherwise.
+   *
+   * @param m the elements to put in the returned set
+   * @return a new ArrayMap or HashMap with the given elements
+   */
+  public static <K, V> Map<K, V> newArrayOrHashMap(Map<K, V> m) {
+    if (m.size() <= 4) {
+      return new ArrayMap<>(m);
+    } else {
+      return new HashMap<>(m);
+    }
+  }
+
+  /**
+   * Returns a new ArrayMap or LinkedHashMap with the given capacity. Uses an ArrayMap if the
+   * capacity is small, and a LinkedHashMap otherwise.
+   *
+   * @param capacity the expected maximum number of elements in the set
+   * @return a new ArrayMap or LinkedHashMap with the given capacity
+   */
+  public static <K, V> Map<K, V> newArrayOrLinkedHashMap(int capacity) {
+    if (capacity <= 4) {
+      return new ArrayMap<>(capacity);
+    } else {
+      return new LinkedHashMap<>(CollectionsPlume.mapCapacity(capacity));
+    }
+  }
+
+  /**
+   * Returns a new ArrayMap or LinkedHashMap with the given elements. Uses an ArrayMap if the
+   * capacity is small, and a LinkedHashMap otherwise.
+   *
+   * @param m the elements to put in the returned set
+   * @return a new ArrayMap or LinkedHashMap with the given elements
+   */
+  public static <K, V> Map<K, V> newArrayOrLinkedHashMap(Map<K, V> m) {
+    if (m.size() <= 4) {
+      return new ArrayMap<>(m);
+    } else {
+      return new LinkedHashMap<>(m);
+    }
+  }
+
+  // TODO: move into ArraySet.java.
+
+  /**
+   * Returns a new ArraySet or HashSet with the given capacity. Uses an ArraySet if the capacity is
+   * small, and a HashSet otherwise.
+   *
+   * @param capacity the expected maximum number of elements in the set
+   * @return a new ArraySet or HashSet with the given capacity
+   */
+  public static <E> Set<E> newArrayOrHashSet(int capacity) {
+    if (capacity <= 4) {
+      return new ArraySet<>(capacity);
+    } else {
+      return new HashSet<>(CollectionsPlume.mapCapacity(capacity));
+    }
+  }
+
+  /**
+   * Returns a new ArraySet or HashSet with the given elements. Uses an ArraySet if the capacity is
+   * small, and a HashSet otherwise.
+   *
+   * @param s the elements to put in the returned set
+   * @return a new ArraySet or HashSet with the given elements
+   */
+  public static <E> Set<E> newArrayOrHashSet(Set<E> s) {
+    if (s.size() <= 4) {
+      return new ArraySet<>(s);
+    } else {
+      return new HashSet<>(s);
+    }
+  }
+
+  /**
+   * Returns a new ArraySet or LinkedHashSet with the given capacity. Uses an ArraySet if the
+   * capacity is small, and a LinkedHashSet otherwise.
+   *
+   * @param capacity the expected maximum number of elements in the set
+   * @return a new ArraySet or LinkedHashSet with the given capacity
+   */
+  public static <E> Set<E> newArrayOrLinkedHashSet(int capacity) {
+    if (capacity <= 4) {
+      return new ArraySet<>(capacity);
+    } else {
+      return new LinkedHashSet<>(CollectionsPlume.mapCapacity(capacity));
+    }
+  }
+
+  /**
+   * Returns a new ArraySet or LinkedHashSet with the given elements. Uses an ArraySet if the
+   * capacity is small, and a LinkedHashSet otherwise.
+   *
+   * @param s the elements to put in the returned set
+   * @return a new ArraySet or LinkedHashSet with the given elements
+   */
+  public static <E> Set<E> newArrayOrLinkedHashSet(Set<E> s) {
+    if (s.size() <= 4) {
+      return new ArraySet<>(s);
+    } else {
+      return new LinkedHashSet<>(s);
+    }
   }
 }
