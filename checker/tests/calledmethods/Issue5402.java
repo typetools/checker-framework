c7f14ade0e73cbbe19e9b2ec8a37ee2a1deaae38
Suzanne Millstein <smillst@cs.washington.edu>
2023-01-10 10:02:03 -0800

    Revert test case.

--- checker/tests/calledmethods/Issue5402.java
+++ checker/tests/calledmethods/Issue5402.java
@@ -1,26 +1,70 @@
+// Test case for https://github.com/typetools/checker-framework/issues/5402
+
+import org.checkerframework.checker.calledmethods.qual.CalledMethods;
 import org.checkerframework.checker.calledmethods.qual.RequiresCalledMethods;
 import org.checkerframework.common.returnsreceiver.qual.This;
-import org.checkerframework.checker.calledmethods.qual.CalledMethods;
+import org.checkerframework.dataflow.qual.Deterministic;
 
-public class Issue5402 {
+public class Issue5402 {}
 
-  public @This Issue5402 bar() {
+class Issue5402_Ok1 {
+
+  public @This Issue5402_Ok1 bar() {
     return this;
   }
-  public @This Issue5402 bar(String s) {
-    return this;
-  }
 
-  @RequiresCalledMethods( value = "this", methods = "bar" )
-  public void baz() {}
-
+  public void baz(@CalledMethods("bar") Issue5402_Ok1 this) {}
 
   public static void test() {
-    final Issue5402 foo = new Issue5402();
-    foo.bar().baz();
-    new Issue5402().bar().baz();
-     final Issue5402 foo3 = new Issue5402();
-        foo3.bar(String.format("")).baz();
+    final Issue5402_Ok1 foo = new Issue5402_Ok1();
+    foo.bar().baz(); // No error
+  }
+}
+
+class Issue5402_Ok2 {
+
+  public @This Issue5402_Ok2 bar() {
+    return this;
   }
 
+  @RequiresCalledMethods(value = "this", methods = "bar")
+  public void baz() {}
+
+  public static void test() {
+    final Issue5402_Ok2 foo = new Issue5402_Ok2();
+    final Issue5402_Ok2 foo1 = foo.bar();
+    foo1.baz(); // No error
+  }
+}
+
+class Issue5402_Ok3 {
+
+  @Deterministic
+  public @This Issue5402_Ok3 bar() {
+    return this;
+  }
+
+  @RequiresCalledMethods(value = "this", methods = "bar")
+  public void baz() {}
+
+  public static void test() {
+    final Issue5402_Ok2 foo = new Issue5402_Ok2();
+    final Issue5402_Ok2 foo1 = foo.bar();
+    foo1.baz(); // No error
+  }
+}
+
+class Issue5402_Bad {
+
+  public @This Issue5402_Bad bar() {
+    return this;
+  }
+
+  @RequiresCalledMethods(value = "this", methods = "bar")
+  public void baz() {}
+
+  public static void test() {
+    final Issue5402_Bad foo = new Issue5402_Bad();
+    foo.bar().baz(); // Error
+  }
 }
