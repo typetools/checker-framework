ad42ba09af65402aee80360184af11791977a5c1
Nargeshdb <nshad001@ucr.edu>
2023-01-12 13:58:11 -0800

    add test infrastructure for the RLC and WPI

---
+++ checker/src/test/java/org/checkerframework/checker/test/junit/ainferrunners/AinferResourceLeakAjavaValidationTest.java
@@ -0,0 +1,37 @@
+package org.checkerframework.checker.test.junit.ainferrunners;
+
+import java.io.File;
+import java.util.List;
+import org.checkerframework.checker.resourceleak.ResourceLeakChecker;
+import org.checkerframework.framework.test.AinferValidatePerDirectoryTest;
+import org.junit.experimental.categories.Category;
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ * Tests whole-program type inference with ajava files. This test is the second pass, which ensures
+ * that with the ajava files in place, the errors that those annotations remove are no longer
+ * issued.
+ */
+@Category(AinferResourceLeakAjavaTest.class)
+public class AinferResourceLeakAjavaValidationTest extends AinferValidatePerDirectoryTest {
+
+  /**
+   * @param testFiles the files containing test code, which will be type-checked
+   */
+  public AinferResourceLeakAjavaValidationTest(List<File> testFiles) {
+    super(
+        testFiles,
+        ResourceLeakChecker.class,
+        "resourceleak",
+        "ainfer-resourceleak/annotated",
+        AinferResourceLeakAjavaTest.class,
+        "-Anomsgtext",
+        ajavaArgFromFiles(testFiles, "resourceleak"),
+        "-Awarns");
+  }
+
+  @Parameters
+  public static String[] getTestDirs() {
+    return new String[] {"ainfer-resourceleak/annotated/"};
+  }
+}
