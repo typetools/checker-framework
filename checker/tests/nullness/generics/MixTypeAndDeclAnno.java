5bbdd6c6530f2e47fc669806f685ea106d0535ee
Michael Ernst <mernst@cs.washington.edu>
2023-01-25 16:00:12 -0800

    generics tests

---
+++ checker/tests/nullness/generics/MixTypeAndDeclAnno.java
@@ -0,0 +1,38 @@
+// Test case for eisop issue #204:
+// https://github.com/typetools/checker-framework/issues/204
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+class MixTypeAndDeclAnno<T extends @Nullable Object> {
+  @NonNull T t;
+  @android.annotation.NonNull T tdecl;
+  // :: error: (type.conflicting.annos)
+  @android.annotation.NonNull @Nullable T tdecl2;
+  // :: error: (type.conflicting.annos)
+  @android.annotation.NonNull @android.annotation.Nullable Object f1;
+  // :: error: (type.conflicting.annos)
+  @android.annotation.NonNull @Nullable Object f2;
+  // Nullable applies to the array itself, while NonNull apply to components of the array.
+  @android.annotation.Nullable @NonNull Object[] g;
+  // :: error: (type.conflicting.annos)
+  @android.annotation.Nullable Object @NonNull [] k;
+
+  MixTypeAndDeclAnno(
+      @NonNull T t,
+      @android.annotation.NonNull T tdecl,
+      // :: error: (type.conflicting.annos)
+      @android.annotation.NonNull @android.annotation.Nullable Object f1,
+      // :: error: (type.conflicting.annos)
+      @android.annotation.NonNull @Nullable Object f2,
+      // :: error: (type.conflicting.annos)
+      @android.annotation.Nullable Object @NonNull [] k) {
+    this.t = t;
+    this.tdecl = tdecl;
+    // :: error: (type.conflicting.annos)
+    this.f1 = f1;
+    // :: error: (type.conflicting.annos)
+    this.f2 = f2;
+    // :: error: (type.conflicting.annos)
+    this.k = k;
+  }
+}
