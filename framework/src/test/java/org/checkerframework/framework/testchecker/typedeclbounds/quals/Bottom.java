9edc3721ee4d0f967857a397414e61a182414983
Michael Ernst <mernst@cs.washington.edu>
2023-01-31 09:40:20 -0800

    Add missing files

---
+++ framework/src/test/java/org/checkerframework/framework/testchecker/typedeclbounds/quals/Bottom.java
@@ -0,0 +1,20 @@
+package org.checkerframework.framework.testchecker.typedeclbounds.quals;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.checkerframework.framework.qual.DefaultFor;
+import org.checkerframework.framework.qual.SubtypeOf;
+import org.checkerframework.framework.qual.TypeUseLocation;
+
+/**
+ * Toy type system for testing impact of implicit java type conversion.
+ *
+ * @see Top
+ */
+@SubtypeOf({S1.class, S2.class})
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+@DefaultFor(value = {TypeUseLocation.LOWER_BOUND})
+public @interface Bottom {}
