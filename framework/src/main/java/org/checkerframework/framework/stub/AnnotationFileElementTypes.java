a95be50f4166c2e189e7589b07c08cc1af6a238d
Michael Ernst <mernst@cs.washington.edu>
2023-02-04 13:02:46 -0800

    Add import statement

--- framework/src/main/java/org/checkerframework/framework/stub/AnnotationFileElementTypes.java
+++ framework/src/main/java/org/checkerframework/framework/stub/AnnotationFileElementTypes.java
@@ -21,6 +21,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.StringJoiner;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
