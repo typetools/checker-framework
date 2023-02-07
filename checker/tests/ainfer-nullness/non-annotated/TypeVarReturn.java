c7b337e3d6bbe18fbbe57f4e3ad53798adbfe846
Martin Kellogg <martin.kellogg@njit.edu>
2023-02-07 15:54:07 -0500

    failing test

---
+++ checker/tests/ainfer-nullness/non-annotated/TypeVarReturn.java
@@ -0,0 +1,11 @@
+// Code like this caused WPI to loop infinitely, because the annotation on the return type
+// was only sometimes inferred. Based on an example from
+// https://github.com/dd482IT/cache2k-wpi/blob/0eaa156bdecd617b2aa4c745d0f8844a32609697/cache2k-api/src/main/java/org/cache2k/config/ToggleFeature.java#L73
+
+public class TypeVarReturn {
+
+  public static <T extends TypeVarReturn> T extract() {
+    // :: warning: return
+    return null;
+  }
+}
