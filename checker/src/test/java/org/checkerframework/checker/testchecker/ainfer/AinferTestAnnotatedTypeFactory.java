7a59c0646c0ab3a04dfc4fa2f11c1a1c572784f2
Martin Kellogg <martin.kellogg@njit.edu>
2023-02-07 15:10:08 -0500

    place declaration annotations on fields correctly

--- checker/src/test/java/org/checkerframework/checker/testchecker/ainfer/AinferTestAnnotatedTypeFactory.java
+++ checker/src/test/java/org/checkerframework/checker/testchecker/ainfer/AinferTestAnnotatedTypeFactory.java
@@ -11,6 +11,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
@@ -71,8 +72,8 @@ public class AinferTestAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
 
   public AinferTestAnnotatedTypeFactory(BaseTypeChecker checker) {
     super(checker);
-    // Support a declaration annotation that can be written on parameters, to test that the
-    // WPI feature allowing inference of declaration annotations on parameters works as intended.
+    // Support a declaration annotation that has the same meaning as @Sibling1, to test that the
+    // WPI feature allowing inference of declaration annotations works as intended.
     addAliasedTypeAnnotation(AinferTreatAsSibling1.class, SIBLING1);
     postInit();
   }
@@ -101,6 +102,19 @@ public class AinferTestAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
         new PropagationTreeAnnotator(this),
         literalTreeAnnotator,
         new AinferTestTreeAnnotator(this));
+  }
+
+  @Override
+  public AnnotatedTypeMirror getAnnotatedType(Element elt) {
+    // By default, the CF does not look for declaration annotations
+    // that are aliases of type annotations in annotation files.
+    // For the test that the WPI places declaration annotations properly,
+    // adjust those rules for fields here. TODO: is that a bug in the CF or expected behavior?
+    AnnotatedTypeMirror result = super.getAnnotatedType(elt);
+    if (getDeclAnnotation(elt, AinferTreatAsSibling1.class) != null) {
+      result.replaceAnnotation(SIBLING1);
+    }
+    return result;
   }
 
   protected class AinferTestTreeAnnotator extends TreeAnnotator {
