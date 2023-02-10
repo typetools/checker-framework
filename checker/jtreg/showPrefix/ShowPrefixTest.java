beb7b53af6b9bc86f202cb2e26e2840e59e869e8
Michael Ernst <mernst@cs.washington.edu>
2023-02-10 09:31:09 -0800

    Remove cut-and-paste error

--- checker/jtreg/showPrefix/ShowPrefixTest.java
+++ checker/jtreg/showPrefix/ShowPrefixTest.java
@@ -1,7 +1,5 @@
 /*
  * @test
- * @summary Test that Issue 469 is fixed. Thanks to user pSub for the test case.
- * @library .
  *
  * @compile/fail/ref=ShowPrefixTest.out -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker ShowPrefixTest.java -AshowPrefixInWarningMessages
  */
