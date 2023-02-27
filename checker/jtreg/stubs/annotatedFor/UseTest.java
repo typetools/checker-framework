89a167a84e4ba49fea273bc1ce430c3a155ffefa
Suzanne Millstein <smillst@cs.washington.edu>
2023-02-27 13:11:07 -0800

    Don't cache annotatedfor while parsing annotation files

--- checker/jtreg/stubs/annotatedFor/UseTest.java
+++ checker/jtreg/stubs/annotatedFor/UseTest.java
@@ -3,16 +3,20 @@
  * @summary Test AnnotatedFor in stub files
  * @compile -XDrawDiagnostics -Xlint:unchecked ../annotatedForLib/Test.java
  * @compile/fail/ref=WithStub.out -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker -Anomsgtext UseTest.java -Astubs=Test.astub -Werror
+ * @compile/fail/ref=WithStub.out -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker -Anomsgtext UseTest.java -Astubs=Test.astub -Werror -AuseConservativeDefaultsForUncheckedCode=bytecode
  * @compile/fail/ref=WithoutStub.out -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker -Anomsgtext UseTest.java -Werror
+ * @compile/fail/ref=WithoutStubConservative.out -XDrawDiagnostics -processor org.checkerframework.checker.nullness.NullnessChecker -Anomsgtext UseTest.java -Werror -AuseConservativeDefaultsForUncheckedCode=bytecode
  */
 
 package annotatedfor;
 
 import annotatedforlib.Test;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 public class UseTest {
   void test(Test<String> test) {
     test.method1(null);
     test.method2(null);
+    @NonNull Object o = test.method3();
   }
 }
