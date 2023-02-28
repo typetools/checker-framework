603bc8d064619113599fa83e2bb5d9fc27e472f5
Michael Ernst <mernst@cs.washington.edu>
2023-02-28 09:53:53 -0800

    Adjust changes

--- javacutil/src/main/java/org/checkerframework/javacutil/ElementUtils.java
+++ javacutil/src/main/java/org/checkerframework/javacutil/ElementUtils.java
@@ -900,7 +900,7 @@ public class ElementUtils {
 
     // Generated constructors seem to get GENERATEDCONSTR even though the documentation
     // seems to imply they would get GENERATED_MEMBER like the fields do.
-    return (((Symbol) e).flags() & (Flags_GENERATED_MEMBER | Flags.GENERATEDCONSTR)) != 0;
+    return (((Symbol) e).flags() & (TreeUtils.Flags_GENERATED_MEMBER | Flags.GENERATEDCONSTR)) != 0;
   }
 
   /**
