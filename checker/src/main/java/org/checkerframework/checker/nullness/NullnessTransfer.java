11c7363b0ab0bd6a990b56755b5f720c41828e1b
Haifeng Shi <shihaifeng1998@gmail.com>
2023-02-28 10:17:06 -0800

    New `-AinvocationPreservesArgumentNullness` command-line option
    Co-authored-by: Michael Ernst <mernst@cs.washington.edu>

--- checker/src/main/java/org/checkerframework/checker/nullness/NullnessTransfer.java
+++ checker/src/main/java/org/checkerframework/checker/nullness/NullnessTransfer.java
@@ -30,6 +30,7 @@ import org.checkerframework.dataflow.cfg.node.ReturnNode;
 import org.checkerframework.dataflow.cfg.node.ThrowNode;
 import org.checkerframework.dataflow.expression.JavaExpression;
 import org.checkerframework.dataflow.expression.LocalVariable;
+import org.checkerframework.dataflow.util.PurityUtils;
 import org.checkerframework.framework.flow.CFAbstractStore;
 import org.checkerframework.framework.type.AnnotatedTypeMirror;
 import org.checkerframework.framework.type.AnnotatedTypeMirror.AnnotatedDeclaredType;
@@ -86,6 +87,13 @@ public class NullnessTransfer
   private final boolean assumeKeyFor;
 
   /**
+   * True if invocationPreservesArgumentNullness flag is turned on, meaning that after a method call
+   * or constructor invocation, non-null arguments of the invocation (including the receiver) are
+   * assumed to still be non-null.
+   */
+  private final boolean nonNullAssumptionAfterInvocation;
+
+  /**
    * Create a new NullnessTransfer for the given analysis.
    *
    * @param analysis nullness analysis
@@ -115,6 +123,12 @@ public class NullnessTransfer
                 TypesUtils.typeFromClass(Map.class, analysis.getTypes(), elements),
                 nullnessTypeFactory,
                 false);
+
+    nonNullAssumptionAfterInvocation =
+        !analysis
+            .getTypeFactory()
+            .getChecker()
+            .getBooleanOption("invocationPreservesArgumentNullness", true);
   }
 
   /**
@@ -322,6 +336,8 @@ public class NullnessTransfer
   public TransferResult<NullnessValue, NullnessStore> visitMethodAccess(
       MethodAccessNode n, TransferInput<NullnessValue, NullnessStore> p) {
     TransferResult<NullnessValue, NullnessStore> result = super.visitMethodAccess(n, p);
+    // In contrast to the conditional makeNonNull in visitMethodInvocation, this
+    // makeNonNull is unconditional, as the receiver is definitely non-null after the access.
     makeNonNull(result, n.getReceiver());
     return result;
   }
@@ -342,11 +358,22 @@ public class NullnessTransfer
     return result;
   }
 
-  /*
-   * Provided that m is of a type that implements interface java.util.Map:
+  /**
+   * {@inheritDoc}
+   *
+   * <p>When the invocationPreservesArgumentNullness flag is turned on, the receiver and arguments
+   * that are passed to non-null parameters in a method call or constructor invocation are unsoundly
+   * assumed to be non-null after the invocation.
+   *
+   * <p>When the flag is turned off, the analysis is sound: it checks that the method is
+   * SideEffectFree or the receiver is unassignable. Only if either one of the two is true, is the
+   * receiver made non-null. Similar logic is applied to the arguments of the invocation.
+   *
+   * <p>Provided that m is of a type that implements interface java.util.Map:
+   *
    * <ul>
-   * <li>Given a call m.get(k), if k is @KeyFor("m") and m's value type is @NonNull,
-   *     then the result is @NonNull in the thenStore and elseStore of the transfer result.
+   *   <li>Given a call m.get(k), if k is @KeyFor("m") and m's value type is @NonNull, then the
+   *       result is @NonNull in the thenStore and elseStore of the transfer result.
    * </ul>
    */
   @Override
@@ -354,21 +381,30 @@ public class NullnessTransfer
       MethodInvocationNode n, TransferInput<NullnessValue, NullnessStore> in) {
     TransferResult<NullnessValue, NullnessStore> result = super.visitMethodInvocation(n, in);
 
-    // Make receiver non-null.
+    MethodInvocationTree tree = n.getTree();
+    ExecutableElement method = TreeUtils.elementFromUse(tree);
+
+    boolean isMethodSideEffectFree = PurityUtils.isSideEffectFree(atypeFactory, method);
     Node receiver = n.getTarget().getReceiver();
-    makeNonNull(result, receiver);
+    if (nonNullAssumptionAfterInvocation
+        || isMethodSideEffectFree
+        || JavaExpression.fromNode(receiver).isUnassignableByOtherCode()) {
+      // Make receiver non-null.
+      makeNonNull(result, receiver);
+    }
 
     // For all formal parameters with a non-null annotation, make the actual argument non-null.
     // The point of this is to prevent cascaded errors -- the Nullness Checker will issue a
     // warning for the method invocation, but not for subsequent uses of the argument.  See test
     // case FlowNullness.java.
-    MethodInvocationTree tree = n.getTree();
-    ExecutableElement method = TreeUtils.elementFromUse(tree);
     AnnotatedExecutableType methodType = nullnessTypeFactory.getAnnotatedType(method);
     List<AnnotatedTypeMirror> methodParams = methodType.getParameterTypes();
     List<? extends ExpressionTree> methodArgs = tree.getArguments();
     for (int i = 0; i < methodParams.size() && i < methodArgs.size(); ++i) {
-      if (methodParams.get(i).hasAnnotation(NONNULL)) {
+      if (methodParams.get(i).hasAnnotation(NONNULL)
+          && (nonNullAssumptionAfterInvocation
+              || isMethodSideEffectFree
+              || JavaExpression.fromTree(methodArgs.get(i)).isUnassignableByOtherCode())) {
         makeNonNull(result, n.getArgument(i));
       }
     }
