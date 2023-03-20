54e18a2aa3657319d90ef9b0493bb8f5acaf52ae
Michael Ernst <mernst@cs.washington.edu>
2023-03-20 08:43:19 -0700

    Catch another type of exception (#5723)

--- javacutil/src/main/java/org/checkerframework/javacutil/TypesUtils.java
+++ javacutil/src/main/java/org/checkerframework/javacutil/TypesUtils.java
@@ -142,7 +142,7 @@ public final class TypesUtils {
 
         try {
           return Class.forName(typeString);
-        } catch (ClassNotFoundException | UnsupportedClassVersionError e) {
+        } catch (ClassNotFoundException | NoClassDefFoundError | UnsupportedClassVersionError e) {
           return Object.class;
         }
 
