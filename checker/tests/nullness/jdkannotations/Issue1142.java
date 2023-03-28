82d2f54727e728c0df83e1941c12145fb481eee7
Suzanne Millstein <smillst@cs.washington.edu>
2023-03-28 08:24:52 -0700

    Fix more errors.

--- checker/tests/nullness/jdkannotations/Issue1142.java
+++ checker/tests/nullness/jdkannotations/Issue1142.java
@@ -6,7 +6,7 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 public class Issue1142 {
 
   void foo() {
-    // :: error: (type.argument)
+    // :: error: (type.argument) :: error: (type.arguments.not.inferred)
     ConcurrentHashMap<Integer, @Nullable Integer> chm1 = new ConcurrentHashMap<>();
     chm1.put(1, null);
   }
