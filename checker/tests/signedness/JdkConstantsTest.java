ac961714ff4b12d11e4d696081cbd18c78e0bffd
Michael Ernst <mernst@cs.washington.edu>
2023-04-13 14:37:44 -0700

    Fix expected errors

--- checker/tests/signedness/JdkConstantsTest.java
+++ checker/tests/signedness/JdkConstantsTest.java
@@ -3,12 +3,12 @@ import org.checkerframework.checker.signedness.qual.PolySigned;
 public class JdkConstantsTest {
 
   static @PolySigned int integerMinValue(@PolySigned int value) {
-    // error: (return)
+    // :: error: (return)
     return Integer.MIN_VALUE;
   }
 
   static @PolySigned int flip(@PolySigned int value) {
-    // error: (return)
+    // :: error: (return)
     return value ^ Integer.MIN_VALUE;
   }
 }
