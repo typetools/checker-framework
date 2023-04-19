10a62bcb4a094d59daade0b2b0236c96814960d3
Martin Kellogg <martin.kellogg@njit.edu>
2023-04-19 15:50:08 -0400

    Revert "finish incomplete refactor"
    This reverts commit 283d19af448bb9c47d248eb3382fcac51a3611f4.

--- checker/tests/resourceleak/InitializationAfterSuperTest.java
+++ checker/tests/resourceleak/InitializationAfterSuperTest.java
@@ -1,5 +1,7 @@
 // Test case for https://github.com/typetools/checker-framework/issues/5762
 
+// @skip-test until the bug is fixed
+
 import java.io.IOException;
 import java.net.Socket;
 import org.checkerframework.checker.calledmethods.qual.EnsuresCalledMethods;
@@ -11,13 +13,9 @@ public class InitializationAfterSuperTest implements AutoCloseable {
 
   @Owning Socket mySocket;
 
-  public InitializationAfterSuperTest(@Owning Socket mySocket) throws IOException {
+  public InitializationAfterSuperTest(@Owning Socket mySocket) {
     super();
-    if (this.mySocket == null) {
-      this.mySocket = mySocket;
-    } else {
-      mySocket.close();
-    }
+    this.mySocket = mySocket;
   }
 
   @EnsuresCalledMethods(value = "mySocket", methods = "close")
