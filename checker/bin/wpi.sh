09c4ba3bb301556b022c6beeac9326d758c289a1
Michael Ernst <mernst@cs.washington.edu>
2023-04-22 16:10:31 -0700

    Retry `getPlumeScripts` if it fails

--- checker/bin/wpi.sh
+++ checker/bin/wpi.sh
@@ -309,7 +309,7 @@ function configure_and_exec_dljc {
 # Clone or update DLJC
 if [ "${DLJC}" = "" ]; then
   # The user did not set the DLJC environment variable.
-  (cd "${SCRIPTDIR}"/../.. && ./gradlew --stacktrace getPlumeScripts)
+  (cd "${SCRIPTDIR}"/../.. && (./gradlew --stacktrace getPlumeScripts || (sleep 60 && ./gradlew --stacktrace getPlumeScripts)))
   "${SCRIPTDIR}"/../bin-devel/.plume-scripts/git-clone-related kelloggm do-like-javac "${SCRIPTDIR}"/.do-like-javac
   if [ ! -d "${SCRIPTDIR}/.do-like-javac" ]; then
       echo "Failed to clone do-like-javac"
