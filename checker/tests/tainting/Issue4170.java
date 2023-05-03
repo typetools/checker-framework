edf188526a084ae7b73e5ee21915624dd141d312
Kanak Das <kdas006@ucr.edu>
2023-05-03 13:44:23 -0700

    few test cases

--- checker/tests/tainting/Issue4170.java
+++ checker/tests/tainting/Issue4170.java
@@ -7,6 +7,8 @@ public class Issue4170 {
   public void method1() {
     var list = new ArrayList<@Untainted String>();
     ArrayList<@Untainted String> list2 = list;
+    ArrayList<@Untainted String> list3 = new ArrayList<@Untainted String>();
+    ArrayList<@Tainted String> list4 = list3;
     var stream = list.stream();
   }
 
@@ -19,4 +21,27 @@ public class Issue4170 {
     var list = new ArrayList<@Tainted String>();
     var stream = list.stream();
   }
+
+  public ArrayList<@Untainted String> method4() {
+    var list = new ArrayList<@Untainted String>();
+    return list;
+  }
+
+  public ArrayList<@Tainted String> method5() {
+    var list = new ArrayList<@Untainted String>();
+    return list;
+  }
+
+  public ArrayList<@Untainted String> method6() {
+    var list = new ArrayList<String>();
+    // :: error: (return)
+    return list;
+  }
+
+  public void method7() {
+    var list = new ArrayList<@Untainted String>();
+    method8(list);
+  }
+
+  public void method8(ArrayList<@Untainted String> data) {}
 }
