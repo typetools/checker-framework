623a88bd602e90ba018bd8d6b13d50765144945a
Martin Kellogg <martin.kellogg@njit.edu>
2023-05-10 10:45:07 -0400

    enforce behavioral subtyping for ownership

--- checker/tests/resourceleak/OwningOverride2.java
+++ checker/tests/resourceleak/OwningOverride2.java
@@ -1,14 +1,16 @@
-// More tests for the problem described at https://github.com/typetools/checker-framework/issues/5722
+// More tests for the problem described at
+// https://github.com/typetools/checker-framework/issues/5722
 // Test that an owning return cannot override a non-owning return
 
-import java.net.*;
 import java.io.*;
+import java.net.*;
 import org.checkerframework.checker.mustcall.qual.*;
 
 public class OwningOverride2 {
   abstract static class A {
     public abstract @NotOwning Socket get();
   }
+
   static class B extends A {
     @Override
     // :: error: owning.override.return
