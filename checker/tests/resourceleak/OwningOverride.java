7993583c84a247c03c7fbd376aef8607ee925b1a
Martin Kellogg <martin.kellogg@njit.edu>
2023-05-10 10:06:37 -0400

    failing tests

---
+++ checker/tests/resourceleak/OwningOverride.java
@@ -0,0 +1,22 @@
+// Test case for https://github.com/typetools/checker-framework/issues/5722
+
+import java.net.*;
+import java.io.*;
+import org.checkerframework.checker.mustcall.qual.*;
+
+public class OwningOverride {
+  abstract static class A {
+    public abstract void closeStream(@Owning InputStream s) throws IOException;
+  }
+  static class B extends A {
+    @Override
+    // :: error: owning.override.param
+    public void closeStream(InputStream s) { }
+  }
+  static void main(String[] args) throws IOException {
+    // no resource leak reported for x
+    InputStream x = new FileInputStream("foo.txt");
+    A a = new B();
+    a.closeStream(x);
+  }
+}
---
+++ checker/tests/resourceleak/OwningOverride2.java
@@ -0,0 +1,19 @@
+// More tests for the problem described at https://github.com/typetools/checker-framework/issues/5722
+// Test that an owning return cannot override a non-owning return
+
+import java.net.*;
+import java.io.*;
+import org.checkerframework.checker.mustcall.qual.*;
+
+public class OwningOverride2 {
+  abstract static class A {
+    public abstract @NotOwning Socket get();
+  }
+  static class B extends A {
+    @Override
+    // :: error: owning.override.return
+    public Socket get() {
+      return null;
+    }
+  }
+}
