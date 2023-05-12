942d63b075c6c0fc8a7a42ccdf24294535e44c87
Michael Ernst <mernst@cs.washington.edu>
2023-05-12 16:33:52 -0700

    Narrow suppression

--- checker/src/main/java/org/checkerframework/checker/initialization/InitializationStore.java
+++ checker/src/main/java/org/checkerframework/checker/initialization/InitializationStore.java
@@ -186,7 +186,10 @@ public class InitializationStore<V extends CFAbstractValue<V>, S extends Initial
         removedOtherFieldValues.put(invariantField, v);
       }
 
-      @SuppressWarnings("allcheckers:argument") // not sure what is happening here
+      @SuppressWarnings({
+        "formatter:argument",
+        "compilermessages:argument"
+      }) // not sure what is happening here, generic types are complex
       boolean result = super.supersetOf(other);
       return result;
     } finally {
