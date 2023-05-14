a0db7cb6677a29e84180e83628057fded9d819dc
Michael Ernst <mernst@cs.washington.edu>
2023-05-14 10:08:31 -0700

    Move `has*` predicates after `is*` predicates

--- javacutil/src/main/java/org/checkerframework/javacutil/TypesUtils.java
+++ javacutil/src/main/java/org/checkerframework/javacutil/TypesUtils.java
@@ -621,17 +621,6 @@ public final class TypesUtils {
   }
 
   /**
-   * Returns true if {@code type} has an enclosing type.
-   *
-   * @param type type to checker
-   * @return true if {@code type} has an enclosing type
-   */
-  public static boolean hasEnclosingType(TypeMirror type) {
-    Type e = ((Type) type).getEnclosingType();
-    return e.getKind() != TypeKind.NONE;
-  }
-
-  /**
    * Returns whether or not {@code type} is a functional interface type (as defined in JLS 9.8).
    *
    * @param type possible functional interface type
@@ -642,6 +631,17 @@ public final class TypesUtils {
     Context ctx = ((JavacProcessingEnvironment) env).getContext();
     com.sun.tools.javac.code.Types javacTypes = com.sun.tools.javac.code.Types.instance(ctx);
     return javacTypes.isFunctionalInterface((Type) type);
+  }
+
+  /**
+   * Returns true if {@code type} has an enclosing type.
+   *
+   * @param type type to checker
+   * @return true if {@code type} has an enclosing type
+   */
+  public static boolean hasEnclosingType(TypeMirror type) {
+    Type e = ((Type) type).getEnclosingType();
+    return e.getKind() != TypeKind.NONE;
   }
 
   /// Type variables and wildcards
