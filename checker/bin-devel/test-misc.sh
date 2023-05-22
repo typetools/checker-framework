e40309ab24940518cf43882f7f2e5c2bb4b324f6
Michael Ernst <mernst@cs.washington.edu>
2023-05-22 02:13:35 -0700

    Add diagnostics

--- checker/bin-devel/test-misc.sh
+++ checker/bin-devel/test-misc.sh
@@ -10,6 +10,9 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # shellcheck disable=SC1090 # In newer shellcheck than 0.6.0, pass: "-P SCRIPTDIR" (literally)
 source "$SCRIPTDIR"/build.sh
 
+which black
+make -C docs/developer/release check-python-style
+
 PLUME_SCRIPTS="$SCRIPTDIR/.plume-scripts"
 
 status=0
