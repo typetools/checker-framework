056e72c5df81a9a1085db78363a7a9df76b7621d
Michael Ernst <mernst@cs.washington.edu>
2023-05-22 02:46:31 -0700

    Remove diagnostics

--- checker/bin-devel/test-misc.sh
+++ checker/bin-devel/test-misc.sh
@@ -10,11 +10,6 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # shellcheck disable=SC1090 # In newer shellcheck than 0.6.0, pass: "-P SCRIPTDIR" (literally)
 source "$SCRIPTDIR"/build.sh
 
-which black
-ls -al /root/.local/bin/
-whoami
-make -C docs/developer/release check-python-style
-
 PLUME_SCRIPTS="$SCRIPTDIR/.plume-scripts"
 
 status=0
