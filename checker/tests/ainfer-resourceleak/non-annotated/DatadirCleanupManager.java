f2e707705285778560a4f695986e1f451293c160
Nargeshdb <nshad001@ucr.edu>
2023-05-23 16:25:35 -0700

    update the test case

--- checker/tests/ainfer-resourceleak/non-annotated/DatadirCleanupManager.java
+++ checker/tests/ainfer-resourceleak/non-annotated/DatadirCleanupManager.java
@@ -2,27 +2,12 @@ import java.io.*;
 import java.util.*;
 
 public class DatadirCleanupManager {
-
-  void start(File dataDir, File snapDir, int count) {
-    TimerTask task = new PurgeTask(dataDir, snapDir, count);
-  }
-
   static class PurgeTask extends TimerTask {
-
-    private File logsDir;
-    private File snapsDir;
-    private int snapRetainCount;
-
-    public PurgeTask(File dataDir, File snapDir, int count) {
-      logsDir = dataDir;
-      snapsDir = snapDir;
-      snapRetainCount = count;
-    }
 
     @Override
     public void run() {
       try {
-        PurgeTxnLog.purge(logsDir, snapsDir, snapRetainCount);
+        PurgeTxnLog.purge();
       } catch (Exception e) {
 
       }
--- checker/tests/ainfer-resourceleak/non-annotated/PurgeTxnLog.java
+++ checker/tests/ainfer-resourceleak/non-annotated/PurgeTxnLog.java
@@ -3,15 +3,13 @@ import java.util.*;
 
 public class PurgeTxnLog {
 
-  public static void purge(File dataDir, File snapDir, int num) throws IOException {
+  public static void purge() throws IOException {
     staticMethod();
   }
 
   static void staticMethod() {
 
     class MyFileFilter implements FileFilter {
-
-      //      private final String prefix;
       MyFileFilter() {}
 
       public boolean accept(File f) {
