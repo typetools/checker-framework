b4b9ccb61cd5e3fa89a30ee5fd99f615e590c0b4
Michael Ernst <mernst@cs.washington.edu>
2023-05-24 09:17:43 -0700

    Don't call `erasure()` on illegal arguments (#5916)

--- framework/src/main/java/org/checkerframework/framework/type/GenericAnnotatedTypeFactory.java
+++ framework/src/main/java/org/checkerframework/framework/type/GenericAnnotatedTypeFactory.java
@@ -2363,7 +2363,9 @@ public abstract class GenericAnnotatedTypeFactory<
    *     Java type
    */
   public final boolean isRelevant(TypeMirror tm) {
-    tm = types.erasure(tm);
+    if (tm.getKind() != TypeKind.PACKAGE && tm.getKind() != TypeKind.MODULE) {
+      tm = types.erasure(tm);
+    }
     Boolean cachedResult = isRelevantCache.get(tm);
     if (cachedResult != null) {
       return cachedResult;
