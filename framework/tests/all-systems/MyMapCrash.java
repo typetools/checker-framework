f8d9938ae7a56d2041ea187cef4e32f6e9a0f9a5
Suzanne Millstein <smillst@cs.washington.edu>
2023-05-25 08:01:35 -0700

    Complete renaming.

--- framework/tests/all-systems/MyMapCrash.java
+++ framework/tests/all-systems/MyMapCrash.java
@@ -3,10 +3,10 @@ import java.util.Set;
 @SuppressWarnings("all") // Just check for crashes
 public class MyMapCrash<K, V> {
   public static class ImmutableSetMultimap<S, T> {
-    public ImmutableSetMultimap(Map<S, Set<T>> map) {}
+    public ImmutableSetMultimap(MyMapCrash<S, Set<T>> map) {}
   }
 
-  private final class MapSet extends Map<K, Set<V>> {}
+  private final class MapSet extends MyMapCrash<K, Set<V>> {}
 
   public void asMultimap() {
     new ImmutableSetMultimap<K, V>(new MapSet());
