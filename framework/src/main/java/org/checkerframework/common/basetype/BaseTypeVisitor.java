54f1da328a784fe94dd86269f90903609a21c275
Michael Ernst <mernst@cs.washington.edu>
2023-05-25 18:02:02 -0700

    Diagnostics

--- framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
+++ framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
@@ -4740,8 +4740,9 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
     for (AnnotationMirror top : tops) {
       AnnotationMirror upperBound = qualHierarchy.findAnnotationInHierarchy(upperBounds, top);
       AnnotationMirror qualifier = useType.getAnnotationInHierarchy(top);
-      if (!qualHierarchy.isSubtypeShallow(
-          qualifier, useType.getUnderlyingType(), upperBound, declarationTM)) {
+      boolean isSubtype = qualHierarchy.isSubtypeQualifiers(qualifier, upperBound);
+      System.out.printf("issubType(%s, %s) => %s; top=%s%n", qualifier, upperBound, isSubtype, top);
+      if (!isSubtype) {
         return false;
       }
     }
