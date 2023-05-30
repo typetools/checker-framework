ec5c088c4f0f90431b99669ad2be26a5af83ce86
Martin Kellogg <martin.kellogg@njit.edu>
2023-05-30 16:59:37 -0400

    fix tests

--- checker/tests/resourceleak/AccumulationValueTest.java
+++ checker/tests/resourceleak/AccumulationValueTest.java
@@ -12,27 +12,27 @@ public class AccumulationValueTest {
     void b() {}
   }
 
-  <T extends MCAB> void simple1(@MustCall({"a", "b"}) T mcab) {
+  <T extends MCAB> void simple1(@Owning @MustCall({"a", "b"}) T mcab) {
     // test that an accumulation value can accumulate more than one item
     mcab.a();
     mcab.b();
   }
 
   // :: error: required.method.not.called
-  <T extends MCAB> void simple2(@MustCall({"a", "b"}) T mcab) {
+  <T extends MCAB> void simple2(@Owning @MustCall({"a", "b"}) T mcab) {
     // test that the RLC handles missing a in the must call type
     mcab.b();
   }
 
   // :: error: required.method.not.called
-  <T extends MCAB> void simple3(@MustCall({"a", "b"}) T mcab, boolean b) {
+  <T extends MCAB> void simple3(@Owning @MustCall({"a", "b"}) T mcab, boolean b) {
     // tests lubbing two AccumulationValue at a join
     if (b) mcab.a();
     mcab.b();
   }
 
   // :: error: ? but there definitely should be one
-  <T extends MCAB> void simple4(@MustCall({"a"}) T mcab) {
+  <T extends MCAB> void simple4(@Owning @MustCall({"a"}) T mcab) {
     // test that incompatible bounds leads to a warning
     mcab.a();
   }
