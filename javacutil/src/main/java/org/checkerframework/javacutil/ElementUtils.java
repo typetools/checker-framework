f567a87ad538fce152b3fcd7337837782cafec70
Michael Ernst <mernst@cs.washington.edu>
2023-06-01 10:20:29 -0700

    Tweak Javadoc

--- javacutil/src/main/java/org/checkerframework/javacutil/ElementUtils.java
+++ javacutil/src/main/java/org/checkerframework/javacutil/ElementUtils.java
@@ -347,8 +347,9 @@ public class ElementUtils {
   }
 
   /**
-   * Returns a user-friendly name for the given method. Does not return {@code "<init>"} or {@code
-   * "<clinit>"} as ExecutableElement.getSimpleName() does.
+   * Returns a user-friendly name for the given method, which includes the name of the enclosing
+   * type. Does not return {@code "<init>"} or {@code "<clinit>"} as
+   * ExecutableElement.getSimpleName() does.
    *
    * @param element a method declaration
    * @return a user-friendly name for the method
