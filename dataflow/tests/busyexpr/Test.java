c0e6a364bab4a3ef40cf9d6473135026b45f3200
Haifeng Shi <44760135+AndrewShf@users.noreply.github.com>
2023-06-06 17:58:19 -0700

    Fix deprecation warning in busy expression analysis test

--- dataflow/tests/busyexpr/Test.java
+++ dataflow/tests/busyexpr/Test.java
@@ -1,15 +1,16 @@
 class Test {
+  Test(int x) {}
+
   public int test(int m) {
-    int a = 2, b = 3;
-    int x = 1;
-    Integer y;
+    int a = 2, b = 3, x = 1, y;
     if (a != b) {
       x = b >> a;
-      y = new Integer(a - b);
+      new Test(a - b); // test object creation
+      y = a + b;
     } else {
       y = b >> a;
       a = 0;
-      test(a - b);
+      test(a - b); // test method invocation
     }
 
     // test exceptional exit block
