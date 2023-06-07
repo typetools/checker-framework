f67b454e7deeff80b2daec564b5d5e3d4cb27d4d
Martin Kellogg <martin.kellogg@njit.edu>
2023-06-07 05:57:04 -0400

    Enable accumulation checkers to accumulate facts on type variables (and use this new facility in the RLC) (#5943)

--- framework/src/test/java/org/checkerframework/framework/testchecker/testaccumulation/TestAccumulationTransfer.java
+++ framework/src/test/java/org/checkerframework/framework/testchecker/testaccumulation/TestAccumulationTransfer.java
@@ -1,13 +1,13 @@
 package org.checkerframework.framework.testchecker.testaccumulation;
 
+import org.checkerframework.common.accumulation.AccumulationAnalysis;
+import org.checkerframework.common.accumulation.AccumulationStore;
 import org.checkerframework.common.accumulation.AccumulationTransfer;
+import org.checkerframework.common.accumulation.AccumulationValue;
 import org.checkerframework.dataflow.analysis.TransferInput;
 import org.checkerframework.dataflow.analysis.TransferResult;
 import org.checkerframework.dataflow.cfg.node.MethodInvocationNode;
 import org.checkerframework.dataflow.cfg.node.Node;
-import org.checkerframework.framework.flow.CFAnalysis;
-import org.checkerframework.framework.flow.CFStore;
-import org.checkerframework.framework.flow.CFValue;
 
 /** A basic transfer function that accumulates the names of methods called. */
 public class TestAccumulationTransfer extends AccumulationTransfer {
@@ -17,14 +17,15 @@ public class TestAccumulationTransfer extends AccumulationTransfer {
    *
    * @param analysis the analysis
    */
-  public TestAccumulationTransfer(CFAnalysis analysis) {
+  public TestAccumulationTransfer(AccumulationAnalysis analysis) {
     super(analysis);
   }
 
   @Override
-  public TransferResult<CFValue, CFStore> visitMethodInvocation(
-      MethodInvocationNode node, TransferInput<CFValue, CFStore> input) {
-    TransferResult<CFValue, CFStore> result = super.visitMethodInvocation(node, input);
+  public TransferResult<AccumulationValue, AccumulationStore> visitMethodInvocation(
+      MethodInvocationNode node, TransferInput<AccumulationValue, AccumulationStore> input) {
+    TransferResult<AccumulationValue, AccumulationStore> result =
+        super.visitMethodInvocation(node, input);
     Node receiver = node.getTarget().getReceiver();
     if (receiver != null) {
       String methodName = node.getTarget().getMethod().getSimpleName().toString();
