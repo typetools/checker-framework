29075c2571559998cb0299bcd7254c824f17abfd
Michael Ernst <mernst@cs.washington.edu>
2023-06-16 09:55:44 -0700

    Don't return null from leastUpperBoundInitialization

--- checker/src/main/java/org/checkerframework/checker/initialization/InitializationAnnotatedTypeFactory.java
+++ checker/src/main/java/org/checkerframework/checker/initialization/InitializationAnnotatedTypeFactory.java
@@ -892,9 +892,8 @@ public abstract class InitializationAnnotatedTypeFactory<
      */
     protected AnnotationMirror leastUpperBoundInitialization(
         AnnotationMirror anno1, QualifierKind qual1, AnnotationMirror anno2, QualifierKind qual2) {
-      if (!isInitializationAnnotation(anno1) || !isInitializationAnnotation(anno2)) {
-        return null;
-      }
+      assert isInitializationAnnotation(anno1);
+      assert isInitializationAnnotation(anno2);
 
       // Handle the case where one is a subtype of the other.
       if (isSubtypeInitialization(anno1, qual1, anno2, qual2)) {
