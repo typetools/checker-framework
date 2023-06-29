41436653cf74000aa7c41ad4e619ea90dc8b6b39
Suzanne Millstein <smillst@cs.washington.edu>
2023-06-29 08:44:09 -0700

    Add expected errors.

--- checker/tests/tainting/TaintingIssue6060.java
+++ checker/tests/tainting/TaintingIssue6060.java
@@ -9,13 +9,16 @@ public interface TaintingIssue6060<R> extends Iterable<@Untainted R> {
   }
 
   default Spliterator<R> spliterator2() {
+    // :: error: (return)
     return Iterable.super.spliterator();
   }
 
   default Spliterator<R> spliterator3() {
+    // :: error: (return)
     return this.spliterator();
   }
 
+  // :: error: (override.param)
   default void forEach(Consumer<? super R> action) {
     Iterable.super.forEach(action);
   }
