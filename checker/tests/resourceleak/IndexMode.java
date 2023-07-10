6484bc3cd7b7b3eacd7b79a6d9f260d75168cc43
Martin Kellogg <martin.kellogg@njit.edu>
2023-07-10 10:58:50 -0400

    new test suggested by Manu (it fails)

--- checker/tests/resourceleak/IndexMode.java
+++ checker/tests/resourceleak/IndexMode.java
@@ -2,6 +2,7 @@
 // Reported as part of https://github.com/typetools/checker-framework/issues/6077.
 
 import java.util.Map;
+import org.checkerframework.checker.mustcall.qual.MustCall;
 
 public class IndexMode {
   public static Object getMode(Map<String, String> indexOptions) {
@@ -13,5 +14,31 @@ public class IndexMode {
 
     // Actual return type rather than void is needed, otherwise no FP.
     return null;
+  }
+
+  // This copy of getMode() adds an explicit `@MustCall` annotation to the String.
+  public static Object getMode2(Map<String, @MustCall("hashCode") String> indexOptions) {
+    try {
+      // :: error: required.method.not.called
+      String literalOption = indexOptions.get("is_literal");
+    } catch (Exception e) {
+    }
+
+    return null;
+  }
+
+  // This copy of getMode() adds an explicit `@MustCall` annotation to the String and removes
+  // the try-catch.
+  public static Object getMode3(Map<String, @MustCall("hashCode") String> indexOptions) {
+    // :: error: required.method.not.called
+    String literalOption = indexOptions.get("is_literal");
+    return null;
+  }
+
+  // This copy of getMode() adds an explicit `@MustCall` annotation to the String, removes
+  // the try-catch, and makes the return type void.
+  public static void getMode4(Map<String, @MustCall("hashCode") String> indexOptions) {
+    // :: error: required.method.not.called
+    String literalOption = indexOptions.get("is_literal");
   }
 }
