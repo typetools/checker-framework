2146f3c56fbae74ca0908e6fc7bd27550b8157f4
Michael Ernst <mernst@cs.washington.edu>
2023-07-10 08:08:33 -0700

    Remove support for `-AuseDefaultsForUncheckedCode`

--- framework/src/main/java/org/checkerframework/framework/source/SourceChecker.java
+++ framework/src/main/java/org/checkerframework/framework/source/SourceChecker.java
@@ -165,8 +165,6 @@ import org.plumelib.util.UtilPlume;
   // sections in the manual for more details
   // org.checkerframework.framework.source.SourceChecker.useConservativeDefault
   "useConservativeDefaultsForUncheckedCode",
-  // Temporary, for backward compatibility
-  "useDefaultsForUncheckedCode",
 
   // Whether to assume sound concurrent semantics or
   // simplified sequential semantics
