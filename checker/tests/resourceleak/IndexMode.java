e38dbc01803c5329b763b3e0b1d6ae4a86180237
Martin Kellogg <martin.kellogg@njit.edu>
2023-07-10 15:18:59 -0400

    additional test, plus a comment explaining why we're accepting a false negative

--- checker/tests/resourceleak/IndexMode.java
+++ checker/tests/resourceleak/IndexMode.java
@@ -1,6 +1,7 @@
 // A test for a new false positive issued in release 3.36.0 but not 3.35.0.
 // Reported as part of https://github.com/typetools/checker-framework/issues/6077.
 
+import java.io.InputStream;
 import java.util.Map;
 import org.checkerframework.checker.mustcall.qual.MustCall;
 
@@ -19,7 +20,15 @@ public class IndexMode {
   // This copy of getMode() adds an explicit `@MustCall` annotation to the String.
   public static Object getMode2(Map<String, @MustCall("hashCode") String> indexOptions) {
     try {
-      // :: error: required.method.not.called
+      // TODO: a required.method.not.called error should be issued on this line, but currently
+      // it is not. The reason is an interaction between type variable defaulting,
+      // local dataflow, and the rules that the RLC uses for choosing a variable's must-call
+      // obligations: local inference defaults literalOption to @MustCallUnknown (i.e., the
+      // top MustCall type) even though the RHS expression's type is @MustCall("hashCode").
+      // Then, the rule for obligations says that if a variable has the top must-call type,
+      // use the type's default must-call type instead. For String, this is @MustCall({}),
+      // so no error is issued. This rule is important to avoid false positives in realistic
+      // code (such as the first getMode() method in this class).
       String literalOption = indexOptions.get("is_literal");
     } catch (Exception e) {
     }
@@ -52,5 +61,23 @@ public class IndexMode {
   public static void getMode4(Map<String, @MustCall("hashCode") String> indexOptions) {
     // :: error: required.method.not.called
     String literalOption = indexOptions.get("is_literal");
+  }
+
+  // This copy of getMode() removes the try-catch and makes the return type void.
+  public static void getMode5(Map<String, String> indexOptions) {
+    String literalOption = indexOptions.get("is_literal");
+  }
+
+  // This variant uses an InputStream (which has a MustCall type by default) as the
+  // value type in the map.
+  // :: error: type.argument
+  public static Object getModeIS(Map<String, InputStream> indexOptions) {
+    try {
+      // :: error: required.method.not.called
+      InputStream literalOption = indexOptions.get("is_literal");
+    } catch (Exception e) {
+    }
+
+    return null;
   }
 }
