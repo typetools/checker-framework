b6182b99bb3c0f331d09a28f6351623eb9d86af7
Suzanne Millstein <smillst@cs.washington.edu>
2023-07-18 12:58:08 -0700

    Add an easier way to avoid running WPI on tests

--- checker/src/test/java/org/checkerframework/checker/test/junit/ainferrunners/AinferTestCheckerJaifsValidationTest.java
+++ checker/src/test/java/org/checkerframework/checker/test/junit/ainferrunners/AinferTestCheckerJaifsValidationTest.java
@@ -24,8 +24,7 @@ public class AinferTestCheckerJaifsValidationTest extends AinferValidatePerDirec
         "ainfer-testchecker/non-annotated",
         AinferTestCheckerJaifsGenerationTest.class,
         "-Awarns",
-        // The AFU's JAIF reading/writing libraries don't support records.
-        "-AskipDefs=TestPure|SimpleRecord|MyRecord");
+        "-AskipDefs=TestPure");
   }
 
   @Parameters
