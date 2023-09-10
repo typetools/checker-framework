74d17178a598815746d0472c7c01d2dece4e2b8c
Michael Ernst <mernst@cs.washington.edu>
2023-09-10 08:09:36 -0700

    Add quoting

--- checker/bin-devel/wpi-plumelib/test-wpi-plumelib.sh
+++ checker/bin-devel/wpi-plumelib/test-wpi-plumelib.sh
@@ -47,7 +47,7 @@ clean_compile_output() {
     sed -i '/^warning: \[path\] bad path element /d' "$out"
     sed -i '/^.*warning: Option --illegal-access is deprecated and will be removed in a future release./d' "$out"
     sed -i '/^warning: \[options\] bootstrap class path not set/d' "$out"
-    sed -i '/^warning: [options] system modules path not set in conjunction with -source 11/d' "$out"
+    sed -i '/^warning: \[options\] system modules path not set in conjunction with -source 11/d' "$out"
 
     # Remove warning count because it can differ between JDK 8 and later JDKs due to the bootstrap warning:
     sed -i '/^[0-9]* warning/d' "$out"
