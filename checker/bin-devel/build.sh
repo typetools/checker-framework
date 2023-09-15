40ef3b31d5ce7101356eae05573245d697cbc3f5
Suzanne Millstein <smillst@cs.washington.edu>
2023-09-15 12:34:57 -0700

    Opt in to using JDK 17 to build.

--- checker/bin-devel/build.sh
+++ checker/bin-devel/build.sh
@@ -94,6 +94,6 @@ echo "... done: (cd ../stubparser/ && ./.build-without-test.sh)"
      (sleep 1m && ./gradlew --write-verification-metadata sha256 help --dry-run))
 
 echo "running \"./gradlew assemble\" for checker-framework"
-./gradlew assemble --console=plain --warning-mode=all -s -Dorg.gradle.internal.http.socketTimeout=60000 -Dorg.gradle.internal.http.connectionTimeout=60000
+./gradlew assemble --console=plain --warning-mode=all -s -Dorg.gradle.internal.http.socketTimeout=60000 -Dorg.gradle.internal.http.connectionTimeout=60000 -Porg.checkerframework.useJdk17Compiler
 
 echo Exiting checker/bin-devel/build.sh in "$(pwd)"
