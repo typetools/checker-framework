f51868408202faa3623f7841accfc5f5ce9941fc
Michael Ernst <mernst@cs.washington.edu>
2023-09-17 08:40:38 -0700

    Suppress "this-escape" warning

--- javacutil/src/main/java/org/checkerframework/javacutil/AnnotationMirrorMap.java
+++ javacutil/src/main/java/org/checkerframework/javacutil/AnnotationMirrorMap.java
@@ -38,7 +38,10 @@ public class AnnotationMirrorMap<V> implements Map<@KeyFor("this") AnnotationMir
    *
    * @param copy a map whose contents should be copied to the newly created map
    */
-  @SuppressWarnings("nullness:method.invocation") // initialization in constructor
+  @SuppressWarnings({
+    "nullness:method.invocation", // initialization in constructor
+    "this-escape"
+  })
   public AnnotationMirrorMap(Map<AnnotationMirror, ? extends V> copy) {
     this();
     this.putAll(copy);
