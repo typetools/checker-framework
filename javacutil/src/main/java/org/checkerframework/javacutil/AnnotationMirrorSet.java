37da7e6d6feabb9060d29d46c66ca1d12e6402d5
Michael Ernst <mernst@cs.washington.edu>
2023-09-17 09:04:17 -0700

    Suppress "this-escape" warning

--- javacutil/src/main/java/org/checkerframework/javacutil/AnnotationMirrorSet.java
+++ javacutil/src/main/java/org/checkerframework/javacutil/AnnotationMirrorSet.java
@@ -52,6 +52,7 @@ public class AnnotationMirrorSet
    *
    * @param value the AnnotationMirror to put in the set
    */
+  @SuppressWarnings("this-escape")
   public AnnotationMirrorSet(AnnotationMirror value) {
     this.add(value);
   }
@@ -61,6 +62,7 @@ public class AnnotationMirrorSet
    *
    * @param annos the AnnotationMirrors to put in the set
    */
+  @SuppressWarnings("this-escape")
   public AnnotationMirrorSet(Collection<? extends AnnotationMirror> annos) {
     this.addAll(annos);
   }
