15f16036c97b2e9988e22fb7bed8d2f5e2e08986
Suzanne Millstein <smillst@cs.washington.edu>
2023-09-27 10:20:27 -0700

    Move ORG_GRADLE_PROJECT_useJdk17Compiler=true to test scripts.

--- checker/bin-devel/build.sh
+++ checker/bin-devel/build.sh
@@ -85,7 +85,6 @@ echo "... done: (cd ${AT} && ./.build-without-test.sh)"
 # Download dependencies, trying a second time if there is a failure.
 (TERM=dumb timeout 300 ./gradlew --write-verification-metadata sha256 help --dry-run || \
      (sleep 1m && ./gradlew --write-verification-metadata sha256 help --dry-run))
-export ORG_GRADLE_PROJECT_useJdk17Compiler=true
 echo "running \"./gradlew assemble\" for checker-framework"
 ./gradlew assemble --console=plain --warning-mode=all -s -Dorg.gradle.internal.http.socketTimeout=60000 -Dorg.gradle.internal.http.connectionTimeout=60000
 
--- checker/bin-devel/test-cftests-all.sh
+++ checker/bin-devel/test-cftests-all.sh
@@ -9,6 +9,7 @@ set -o xtrace
 export SHELLOPTS
 echo "SHELLOPTS=${SHELLOPTS}"
 
+ORG_GRADLE_PROJECT_useJdk17Compiler=true
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # shellcheck disable=SC1090 # In newer shellcheck than 0.6.0, pass: "-P SCRIPTDIR" (literally)
 source "$SCRIPTDIR"/build.sh
