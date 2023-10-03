3e8563eebad67ea6ce27d1e66bc25b38b1dc735d
Suzanne Millstein <smillst@cs.washington.edu>
2023-10-03 14:27:10 -0700

    Deprecate `checker/bin-devel/build.sh`; add `clone-related.sh`

--- checker/bin-devel/test-plume-lib.sh
+++ checker/bin-devel/test-plume-lib.sh
@@ -34,7 +34,8 @@ echo "PACKAGES=" "${PACKAGES[@]}"
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # shellcheck disable=SC1090 # In newer shellcheck than 0.6.0, pass: "-P SCRIPTDIR" (literally)
 export ORG_GRADLE_PROJECT_useJdk17Compiler=true
-source "$SCRIPTDIR"/build.sh
+source "$SCRIPTDIR"/clone-related.sh
+./gradlew assembleForJavac --console=plain -Dorg.gradle.internal.http.socketTimeout=60000 -Dorg.gradle.internal.http.connectionTimeout=60000
 
 
 failing_packages=""
