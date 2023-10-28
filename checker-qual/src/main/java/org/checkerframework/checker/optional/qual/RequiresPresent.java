7548b7c77c0e021c0e791411d2593810a8263c41
James Yoo <jyoo980@gmail.com>
2023-10-28 12:42:23 -0700

    Update with formatting changes

--- checker-qual/src/main/java/org/checkerframework/checker/optional/qual/RequiresPresent.java
+++ checker-qual/src/main/java/org/checkerframework/checker/optional/qual/RequiresPresent.java
@@ -1,18 +1,18 @@
 package org.checkerframework.checker.optional.qual;
 
-import org.checkerframework.framework.qual.PreconditionAnnotation;
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.checkerframework.framework.qual.PreconditionAnnotation;
 
 /**
- * Indicates a method precondition: the method expects the specified
- * expressions of type Optional to be present (i.e., non-empty) when
- * the annotated method is invoked.
+ * Indicates a method precondition: the method expects the specified expressions of type Optional to
+ * be present (i.e., non-empty) when the annotated method is invoked.
  *
  * <p>For example:
+ *
  * <pre>
  * import java.util.Optional;
  *
@@ -50,9 +50,9 @@ import java.lang.annotation.Target;
  * }
  * </pre>
  *
- * Do not use this annotation for formal parameters (instead, give them
- * a {@code @Present} type). The {@code @RequiresNonNull} annotation is
- * intended for other expressions, such as field accesses or method calls.
+ * Do not use this annotation for formal parameters (instead, give them a {@code @Present} type).
+ * The {@code @RequiresNonNull} annotation is intended for other expressions, such as field accesses
+ * or method calls.
  *
  * @checker_framework.manual #optional-checker Optional Checker
  */
@@ -66,20 +66,16 @@ public @interface RequiresPresent {
    * org.checkerframework.checker.optional.qual.Present}.
    *
    * @return the Java expressions that need to be {@link
-   * org.checkerframework.checker.optional.qual.Present}.
-   *
-   * @checker_framework.manual #java-expressions-as-arguments
-   * Syntax of Java expressions
+   *     org.checkerframework.checker.optional.qual.Present}.
+   * @checker_framework.manual #java-expressions-as-arguments Syntax of Java expressions
    */
   String[] value();
 
   /**
-   * A wrapper annotation that makes the {@link RequiresPresent} annotation
-   * repeatable.
+   * A wrapper annotation that makes the {@link RequiresPresent} annotation repeatable.
    *
-   * <p>Programmers generally do not need to write this. It is created by Java
-   * when a programmer writes more than one {@link RequiresPresent} annotation
-   * at the same location.</p>
+   * <p>Programmers generally do not need to write this. It is created by Java when a programmer
+   * writes more than one {@link RequiresPresent} annotation at the same location.
    */
   @Documented
   @Retention(RetentionPolicy.RUNTIME)
@@ -88,6 +84,7 @@ public @interface RequiresPresent {
   public static @interface List {
     /**
      * Returns the repeatable annotations.
+     *
      * @return the repeatable annotations.
      */
     RequiresPresent[] value();
