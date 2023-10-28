35489bc0b828ba9c24892b5eda4508b96eededc9
Michael Ernst <mernst@cs.washington.edu>
2023-10-28 09:14:45 -0700

    Improve formatting & text

--- checker-qual/src/main/java/org/checkerframework/checker/optional/qual/EnsuresPresentIf.java
+++ checker-qual/src/main/java/org/checkerframework/checker/optional/qual/EnsuresPresentIf.java
@@ -10,47 +10,42 @@ import org.checkerframework.framework.qual.InheritedAnnotation;
 
 /**
  * Indicates that the given expressions of type Optional&lt;T&gt; are present, if the method returns
- * the given result (either true or false).
+ * the given result (either true or false).FOO
  *
  * <p>Here are ways this conditional postcondition annotation can be used:
  *
- * <p><b>Method parameters: </b>A method that has two arguments of type Optional&lt;T&gt; and
- * returns true if they are both equal <i>and</i> present might be annotated as follows:
+ * <p><b>Method parameters:</b> Suppose that a method has two arguments of type Optional&lt;T&gt;
+ * and returns true if they are both equal <i>and</i> present. You could annotate the method as
+ * follows:
  *
- * <pre>{@code @EnsuresPresentIf(expression="#1", result=true)
- * public <T> boolean isPresentAndEqual(Optional<T> optA, Optional<T> optB) { ... }}</pre>
+ * <pre><code> &nbsp;@EnsuresPresentIf(expression="#1", result=true)
+ * &nbsp;@EnsuresPresentIf(expression="#2", result=true)
+ * &nbsp;public &lt;T&gt; boolean isPresentAndEqual(Optional&lt;T&gt; optA, Optional&lt;T&gt; optB) { ... }</code>
+ * </pre>
  *
  * because, if {@code isPresentAndEqual} returns true, then the first (#1) argument to {@code
- * isPresentAndEqual} was present.
+ * isPresentAndEqual} was present, and so was the second (#2) argument. Note that you can write two
+ * {@code @EnsurePresentIf} annotations on a single method.
  *
  * <p><b>Fields:</b> The value expressions can refer to fields, even private ones. For example:
  *
- * <pre>{@code @EnsuresPresentIf(expression="this.optShape", result=true)
- * public boolean isShape() {
- *   return (this.optShape != null && this.optShape.isPresent());
- * }}</pre>
+ * <pre><code> &nbsp;@EnsuresPresentIf(expression="this.optShape", result=true)
+ *  public boolean isShape() {
+ *    return (this.optShape != null &amp;&amp; this.optShape.isPresent());
+ * }</code></pre>
  *
- * An {@code EnsuresPresentIf} annotation that refers to a private field is useful for verifying
+ * An {@code @EnsuresPresentIf} annotation that refers to a private field is useful for verifying
  * that client code performs needed checks in the right order, even if the client code cannot
  * directly affect the field.
  *
- * <p><b>Method calls:</b> If a method {@code isRectangle()} returns true, then {@code
- * getRectangle()} will return a present Optional. You an express this relationship as:
+ * <p><b>Method postconditions:</b> Suppose that if a method {@code isRectangle()} returns true,
+ * then {@code getRectangle()} will return a present Optional. You an express this relationship as:
  *
- * <pre>{@code @EnsuresPresentIf(expression="getRectangle()", result=true)
+ * <pre>{@code @EnsuresPresentIf(result=true, expression="getRectangle()")
  * public @Pure isRectangle() { ... }}</pre>
- *
- * You can also write two {@code @EnsurePresentIf} annotations on a single method:
- *
- * <pre><code>
- * &nbsp;   @EnsuresNonNullIf(expression="#1", result=true)
- * &nbsp;   @EnsuresNonNullIf(expression="#2", result=false)
- *     public &lt;T&gt; boolean isPresentAndEqual(Optional&lt;T&gt; optA, Optional&lt;T&gt; optB) { ... }
- * </code></pre>
  *
  * @see Present
  * @see EnsuresPresent
- * @see org.checkerframework.checker.optional.OptionalChecker
  * @checker_framework.manual #optional-checker Optional Checker
  */
 @Documented
