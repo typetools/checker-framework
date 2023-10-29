5f1129ad017a2b3480e0aea7369498de23ed22f7
James Yoo <jyoo980@gmail.com>
2023-10-28 21:10:44 -0700

    Add additional test cases

--- checker/tests/optional/RequiresPresentTest.java
+++ checker/tests/optional/RequiresPresentTest.java
@@ -4,9 +4,9 @@ import org.checkerframework.checker.optional.qual.*;
 public class RequiresPresentTest {
 
   // :: warning: (optional.field)
-  Optional<String> field1;
+  Optional<String> field1 = Optional.of("abc");
   // :: warning: (optional.field)
-  Optional<String> field2;
+  Optional<String> field2 = Optional.empty();
 
   @RequiresPresent("field1")
   void method1() {
@@ -16,5 +16,38 @@ public class RequiresPresentTest {
     field2.get().length(); // error, might throw NoSuchElementException
   }
 
-  // TODO: write additional test cases.
+  @RequiresPresent("field1")
+  void method2() {
+    // OK, an indirect call to method1.
+    method1();
+  }
+
+  void method3() {
+    field1 = Optional.of("abc");
+    method1(); // OK, satisfied method precondition.
+    field1 = Optional.empty();
+    // :: error: (contracts.precondition)
+    method1(); // error, does not satisfy method precondition.
+  }
+
+  // :: warning: (optional.field)
+  protected Optional<String> field;
+
+  @RequiresPresent("field")
+  public void requiresPresentField() {}
+
+  public void clientFail(RequiresPresentTest arg1) {
+    // :: error: (contracts.precondition)
+    arg1.requiresPresentField();
+  }
+
+  public void clientOK(RequiresPresentTest arg2) {
+    arg2.field = Optional.of("def");
+
+    // this is legal.
+    @Present Optional<String> optField = arg2.field;
+
+    // OK, field is known to be present.
+    arg2.requiresPresentField();
+  }
 }
