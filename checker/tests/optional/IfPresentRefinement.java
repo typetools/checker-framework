c921149eec67e6033ccc8c5770264774ff79576b
Suzanne Millstein <smillst@cs.washington.edu>
2023-12-04 11:06:43 -0800

    Add test.

--- checker/tests/optional/IfPresentRefinement.java
+++ checker/tests/optional/IfPresentRefinement.java
@@ -11,4 +11,10 @@ public class IfPresentRefinement {
   void m2(Optional<String> o) {
     o.ifPresentOrElse(s -> o.get(), () -> {});
   }
+
+  @SuppressWarnings("optional.parameter")
+  void m3(Optional<String> o) {
+    // :: error: (method.invocation)
+    o.ifPresentOrElse(s -> o.get(), () -> o.get());
+  }
 }
