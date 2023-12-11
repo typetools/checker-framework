a5ce240778809f4d6007268f10375cf455e8da65
Michael Ernst <mernst@cs.washington.edu>
2023-12-11 08:08:16 -0800

    Add import statement

--- framework/src/main/java/org/checkerframework/common/util/debug/DoNothingProcessor.java
+++ framework/src/main/java/org/checkerframework/common/util/debug/DoNothingProcessor.java
@@ -4,6 +4,7 @@ import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 
 /**
