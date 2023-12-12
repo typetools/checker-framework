ea3a37ebf3b4492af4dcd01592637d23519aea72
Michael Ernst <mernst@cs.washington.edu>
2023-12-12 08:05:14 -0800

    Define `isDeclaredOfName(TypeMirror, Collection<String>)` (#6356)

--- javacutil/src/main/java/org/checkerframework/javacutil/TypesUtils.java
+++ javacutil/src/main/java/org/checkerframework/javacutil/TypesUtils.java
@@ -357,12 +357,40 @@ public final class TypesUtils {
    * Check if the type represents a declared type of the given qualified name.
    *
    * @param type the type
-   * @return type iff type represents a declared type of the qualified name
+   * @param qualifiedName the name to check {@code type} against
+   * @return true iff type represents a declared type of the qualified name
    */
   public static boolean isDeclaredOfName(TypeMirror type, CharSequence qualifiedName) {
     return type.getKind() == TypeKind.DECLARED
         && getQualifiedName((DeclaredType) type).contentEquals(qualifiedName);
   }
+
+  /**
+   * Check if the type represents a declared type whose fully-qualified name is any of the given
+   * names.
+   *
+   * @param type the type
+   * @param qualifiedNames fully-qualified type names to check for
+   * @return type iff type represents a declared type whose fully-qualified name is one of the given
+   *     names
+   */
+  public static boolean isDeclaredOfName(TypeMirror type, Collection<String> qualifiedNames) {
+    return type.getKind() == TypeKind.DECLARED
+        && qualifiedNames.contains(getQualifiedName((DeclaredType) type));
+  }
+
+  /** The fully-qualified names of the boxed types. */
+  private static Set<String> fqBoxedTypes =
+      new HashSet<>(
+          Arrays.asList(
+              "java.lang.Boolean",
+              "java.lang.Byte",
+              "java.lang.Character",
+              "java.lang.Short",
+              "java.lang.Integer",
+              "java.lang.Long",
+              "java.lang.Double",
+              "java.lang.Float"));
 
   /**
    * Check if the {@code type} represents a boxed primitive type.
@@ -371,20 +399,7 @@ public final class TypesUtils {
    * @return true iff type represents a boxed primitive type
    */
   public static boolean isBoxedPrimitive(TypeMirror type) {
-    if (type.getKind() != TypeKind.DECLARED) {
-      return false;
-    }
-
-    String qualifiedName = getQualifiedName((DeclaredType) type).toString();
-
-    return (qualifiedName.equals("java.lang.Boolean")
-        || qualifiedName.equals("java.lang.Byte")
-        || qualifiedName.equals("java.lang.Character")
-        || qualifiedName.equals("java.lang.Short")
-        || qualifiedName.equals("java.lang.Integer")
-        || qualifiedName.equals("java.lang.Long")
-        || qualifiedName.equals("java.lang.Double")
-        || qualifiedName.equals("java.lang.Float"));
+    return isDeclaredOfName(type, fqBoxedTypes);
   }
 
   /**
