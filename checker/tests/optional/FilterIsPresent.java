091552cdbffc8cf7996fde8adc7b3c0bb59cfd6e
Suzanne Millstein <smillst@cs.washington.edu>
2023-12-12 10:35:19 -0800

    There's already a test like this.

--- checker/tests/optional/FilterIsPresent.java
+++
@@ -1,9 +0,0 @@
-import java.util.Optional;
-import java.util.stream.Stream;
-
-public class FilterIsPresent {
-
-  <E> Stream<E> filterPresent(Stream<Optional<E>> stream) {
-    return stream.filter(Optional::isPresent).map(Optional::get);
-  }
-}
