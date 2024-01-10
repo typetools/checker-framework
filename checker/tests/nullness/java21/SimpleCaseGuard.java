e4993a1c9970ac40b8789801ae25454c4015b684
Suzanne Millstein <smillst@cs.washington.edu>
2024-01-10 08:01:31 -0800

    The latest version of Google Java Format formats Java 21 features. (#6386)

--- checker/tests/nullness/java21/SimpleCaseGuard.java
+++ checker/tests/nullness/java21/SimpleCaseGuard.java
@@ -1,6 +1,7 @@
 // @below-java21-jdk-skip-test
 
-// None of the WPI formats support the new Java 21 languages features, so skip inference until they do.
+// None of the WPI formats supports the new Java 21 languages features, so skip inference until they
+// do.
 // @infer-jaifs-skip-test
 // @infer-ajava-skip-test
 // @infer-stubs-skip-test
@@ -27,5 +28,4 @@ public class SimpleCaseGuard {
       }
     }
   }
-
 }
