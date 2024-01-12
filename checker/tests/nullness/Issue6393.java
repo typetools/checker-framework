c01d97b03d76b2506d133587b1ff2c299358cc49
Suzanne Millstein <smillst@cs.washington.edu>
2024-01-12 08:11:31 -0800

    Add testcase for Issue 6393

---
+++ checker/tests/nullness/Issue6393.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.Serializable;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+class Issue6393 {
+
+  public static class AClass implements Serializable {}
+
+  static class GClass {
+    public <T extends Serializable> @Nullable T g(Class<? super T> e) throws IOException {
+      throw new AssertionError();
+    }
+  }
+
+  // :: warning: (cast.unsafe)
+  static <T extends AClass> T f(Class<? super T> x, GClass y) {
+    AClass z;
+    try {
+      z = y.g(x);
+    } catch (IOException e) {
+      throw new IllegalStateException(e);
+    }
+    return (T) z;
+  }
+}
