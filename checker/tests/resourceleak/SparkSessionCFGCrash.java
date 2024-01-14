b0d2abed0121c5f789987db21b37f869ac8329f0
Manu Sridharan <msridhar@gmail.com>
2024-01-13 16:10:51 -0800

    simplify testcase

--- checker/tests/resourceleak/SparkSessionCFGCrash.java
+++ checker/tests/resourceleak/SparkSessionCFGCrash.java
@@ -2,14 +2,7 @@ import org.apache.spark.sql.SparkSession;
 
 public class SparkSessionCFGCrash {
 
-  private X run() {
-    try (SparkSession session = SparkSession.builder().getOrCreate()) {
-      X x = new X(session);
-      return x;
-    }
-  }
-
-  static class X {
-    X(SparkSession session) {}
+  private void run() {
+    try (SparkSession session = SparkSession.builder().getOrCreate()) {}
   }
 }
