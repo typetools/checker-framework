a6df84208091f2c50a60bb6be7c4f75a5fd8d974
Michael Ernst <mernst@cs.washington.edu>
2024-01-21 17:04:35 -0800

    Miscellaneous tweaks

--- framework/src/main/java/org/checkerframework/framework/source/SourceChecker.java
+++ framework/src/main/java/org/checkerframework/framework/source/SourceChecker.java
@@ -2097,7 +2097,7 @@ public abstract class SourceChecker extends AbstractTypeProcessor implements Opt
     } else if (src == null) {
       return false;
     } else {
-      throw new BugInCF("Unexpected source " + src);
+      throw new BugInCF("Unexpected source [" + src.getClass() + "] " + src);
     }
   }
 
