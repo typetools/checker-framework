70242720238857ae809a12001c532b21345b0e71
Michael Ernst <mernst@cs.washington.edu>
2024-02-17 04:19:38 -0800

    Add Javadoc

--- dataflow/src/main/java/org/checkerframework/dataflow/cfg/node/ReturnNode.java
+++ dataflow/src/main/java/org/checkerframework/dataflow/cfg/node/ReturnNode.java
@@ -40,7 +40,11 @@ public class ReturnNode extends Node {
     this.returnTree = returnTree;
   }
 
-  /** The result of the return node, {@code null} otherwise. */
+  /**
+   * The result of the return node, {@code null} otherwise.
+   *
+   * @return the result of the return node, {@code null} otherwise
+   */
   public @Nullable Node getResult() {
     return result;
   }
