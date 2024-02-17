f62fa5efc501bf785d86f849f10a504a0198917d
Michael Ernst <mernst@cs.washington.edu>
2024-02-17 04:21:15 -0800

    Fix typos
    Co-authored-by: Aosen Xiong <82676488+Ao-senXiong@users.noreply.github.com>
    Co-authored-by: Werner Dietl <wdietl@gmail.com>

--- framework/src/main/java/org/checkerframework/framework/type/visitor/AtmComboVisitor.java
+++ framework/src/main/java/org/checkerframework/framework/type/visitor/AtmComboVisitor.java
@@ -51,9 +51,9 @@ public interface AtmComboVisitor<RETURN_TYPE, PARAM> {
   }
 
   /**
-   * Called by the default implementation of every AbstractAtmComboVisitor visit method. This
-   * methodnS issues a runtime exception by default. In general, it should handle the case where a
-   * visit method has been called with a pair of type mirrors that should never be passed to this
+   * Called by the default implementation of every AbstractAtmComboVisitor visit method. This method
+   * issues a runtime exception by default. In general, it should handle the case where a visit
+   * method has been called with a pair of type mirrors that should never be passed to this
    * particular visitor.
    *
    * @param type1 the first AnnotatedTypeMirror parameter to the visit method called
