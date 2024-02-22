82ec19e622ee952fec86ebc1d7d59e36b92badbe
James Yoo <jyoo980@gmail.com>
2024-02-21 20:46:52 -0800

    Add test case for invalid delegation

---
+++ checker/tests/nonempty/delegation/InvalidDelegateTest.java
@@ -0,0 +1,30 @@
+import java.util.IdentityHashMap;
+import org.checkerframework.checker.nonempty.qual.Delegate;
+
+public class InvalidDelegateTest<K, V> extends IdentityHashMap<K, V> {
+
+  @Delegate public IdentityHashMap<K, V> map;
+
+  @Override
+  public boolean isEmpty() {
+    return this.map.isEmpty(); // OK
+  }
+
+  @Override
+  public int size() {
+    return map.size(); // OK
+  }
+
+  @Override
+  // :: warning: (invalid.delegate)
+  public boolean containsKey(Object key) {
+    return true;
+  }
+
+  @Override
+  // :: warning: (invalid.delegate)
+  public boolean containsValue(Object value) {
+    int x = 3;
+    return map.containsValue(value);
+  }
+}
