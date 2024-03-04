7743e5dcccb0ceafc06aff2a3cbcb4360fd288a1
Sanjay Malakar <iamsanjaymalakar@gmail.com>
2024-03-04 10:05:51 -0800

    Fix incorrect handling of temporary variable enclosing element in RL inference; fixes #6473

--- checker/src/main/java/org/checkerframework/checker/mustcall/MustCallTransfer.java
+++ checker/src/main/java/org/checkerframework/checker/mustcall/MustCallTransfer.java
@@ -1,6 +1,5 @@
 package org.checkerframework.checker.mustcall;
 
-import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.IdentifierTree;
 import com.sun.source.tree.Tree;
@@ -289,8 +288,10 @@ public class MustCallTransfer extends CFTransfer {
     if (path == null) {
       enclosingElement = TreeUtils.elementFromUse(tree).getEnclosingElement();
     } else {
-      ClassTree classTree = TreePathUtil.enclosingClass(path);
-      enclosingElement = TreeUtils.elementFromDeclaration(classTree);
+      // Issue 6473
+      // Adjusts handling of nearest enclosing element for temporary variables.
+      // This approach ensures the correct enclosing element (method or class) is determined.
+      enclosingElement = TreePathUtil.findNearestEnclosingElement(path);
     }
     if (enclosingElement == null) {
       return null;
