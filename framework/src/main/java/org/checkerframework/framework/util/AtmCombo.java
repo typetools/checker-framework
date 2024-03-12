321170fef2efabd796e3bdb8e68eba2e5cbf31f5
Michael Ernst <mernst@cs.washington.edu>
2024-03-12 09:14:24 -0700

    Use Error Prone 2.26.0

--- framework/src/main/java/org/checkerframework/framework/util/AtmCombo.java
+++ framework/src/main/java/org/checkerframework/framework/util/AtmCombo.java
@@ -79,6 +79,7 @@ enum AtmKind {
  *
  * @see AtmCombo#accept
  */
+@SuppressWarnings("EnumOrdinal") // Uses arrays instead of maps for access
 public enum AtmCombo {
   ARRAY_ARRAY(AtmKind.ARRAY, AtmKind.ARRAY),
   ARRAY_DECLARED(AtmKind.ARRAY, AtmKind.DECLARED),
