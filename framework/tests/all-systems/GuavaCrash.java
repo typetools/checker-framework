4c4e7578835ac5115c4385974bd3463e88416fd4
Suzanne Millstein <smillst@cs.washington.edu>
2024-04-19 12:20:08 -0700

    Not parsable.

--- framework/tests/all-systems/GuavaCrash.java
+++ framework/tests/all-systems/GuavaCrash.java
@@ -1,5 +1,6 @@
 import java.io.Serializable;
 
+// @infer-ajava-skip-test
 @SuppressWarnings("all") // Just check for crashes.
 public class GuavaCrash<C extends Comparable<?>> {
   void method(Range<C> restriction, Range<Cut<C>> lowerBoundWindow) {
