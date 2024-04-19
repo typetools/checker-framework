15e474f9eea78f0c41b799037c8d1b2911ab535d
Suzanne Millstein <smillst@cs.washington.edu>
2024-04-19 10:50:29 -0700

    Fix another crash.

---
+++ framework/tests/all-systems/GuavaCrash.java
@@ -0,0 +1,34 @@
+import java.io.Serializable;
+
+public class GuavaCrash<C extends Comparable<?>> {
+  void method(Range<C> restriction, Range<Cut<C>> lowerBoundWindow) {
+    Cut<Cut<C>> upperBoundOnLowerBounds =
+        Ordering.natural().min(lowerBoundWindow.upperBound, Cut.belowValue(restriction.upperBound));
+  }
+
+  abstract static class Cut<C extends Comparable> implements Comparable<Cut<C>>, Serializable {
+    static <C extends Comparable> Cut<C> belowValue(C endpoint) {
+      throw new RuntimeException();
+    }
+  }
+
+  static class Ordering<T> {
+    public static <C extends Comparable> Ordering<C> natural() {
+      throw new RuntimeException();
+    }
+
+    public <E extends T> E min(E a, E b) {
+      throw new RuntimeException();
+    }
+  }
+
+  public static final class Range<C extends Comparable> {
+    final Cut<C> upperBound =
+        new Cut<C>() {
+          @Override
+          public int compareTo(Cut<C> o) {
+            return 0;
+          }
+        };
+  }
+}
