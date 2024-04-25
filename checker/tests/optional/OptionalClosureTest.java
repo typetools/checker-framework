ab791caf59776d262a69e6f0c9b8c593cb6b4d58
James Yoo <jyoo980@gmail.com>
2024-04-24 20:38:39 -0700

    Add test case for closing over an `Optional` value

---
+++ checker/tests/optional/OptionalClosureTest.java
@@ -0,0 +1,21 @@
+import java.util.Optional;
+import java.util.function.Function;
+
+class OptionalClosureTest {
+
+  @SuppressWarnings("optional:field") // We don't care, not the point of this test
+  public Optional<String> opt;
+
+  public Function<String, String> test() {
+    if (opt.isPresent()) {
+      // We *should* issue an error here. It's no good that opt is @Present here; it might be
+      // @MaybePresent at the time of invocation.
+      // :: error: (method.invocation)
+      return str -> opt.get();
+    }
+    return str -> "Hello";
+  }
+
+  @SuppressWarnings("optional:parameter") // We don't care, not the point of this test
+  public void setOpt(Optional<String> opt) {}
+}
