74dd23d33234ba21d1c32a95ce9d1b87727650c0
James Yoo <jyoo980@gmail.com>
2024-05-08 10:30:53 -0700

    Implement right-hand side of a method reference expresssion (`MethodReferenceTarget`)

---
+++ dataflow/src/main/java/org/checkerframework/dataflow/expression/MethodReferenceTarget.java
@@ -0,0 +1,74 @@
+package org.checkerframework.dataflow.expression;
+
+import org.checkerframework.checker.nullness.qual.EnsuresNonNullIf;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+/**
+ * The right-hand side of a <a
+ * href="https://docs.oracle.com/javase/specs/jls/se8/html/jls-15.html#jls-15.13">Java Method
+ * Reference expression</a>
+ *
+ * <p>A Java method Reference expression can have the following forms:
+ *
+ * <ul>
+ *   <li>{@literal [TypeArguments] Identifier}, which may be represented by a standard {@link
+ *       JavaExpression}
+ *   <li>{@literal [TypeArguments] new}, which is a constructor call and cannot be represented by an
+ *       arbitrary {@link JavaExpression}
+ * </ul>
+ */
+public class MethodReferenceTarget {
+
+  /** The type arguments for this method reference target. */
+  private final @Nullable JavaExpression typeArguments;
+
+  /** The identifier for this method reference target. */
+  private final @Nullable JavaExpression identifier;
+
+  /** Whether this method reference target is a constructor call. */
+  private final boolean isConstructorCall;
+
+  /**
+   * Creates a new method reference target.
+   *
+   * @param typeArguments the type arguments
+   * @param identifier the identifier
+   * @param isConstructorCall whether a method reference target is a constructor call
+   */
+  public MethodReferenceTarget(
+      @Nullable JavaExpression typeArguments,
+      @Nullable JavaExpression identifier,
+      boolean isConstructorCall) {
+    this.typeArguments = typeArguments;
+    this.identifier = identifier;
+    this.isConstructorCall = isConstructorCall;
+  }
+
+  /**
+   * Return the type arguments for this method reference target.
+   *
+   * @return the type arguments for this method reference target
+   */
+  public @Nullable JavaExpression getTypeArguments() {
+    return this.typeArguments;
+  }
+
+  /**
+   * Return the identifier for this method reference target.
+   *
+   * @return the identifier for this method reference target
+   */
+  public @Nullable JavaExpression getIdentifier() {
+    return this.identifier;
+  }
+
+  /**
+   * Return true if this method reference target is a constructor call (i.e., "new").
+   *
+   * @return true if this method reference target is a constructor call
+   */
+  @EnsuresNonNullIf(result = false, expression = "this.identifier")
+  public boolean isConstructorCall() {
+    return this.isConstructorCall;
+  }
+}
