06d74b12717b3dcdeb0e17b3532fca1115234f67
James Yoo <jyoo980@gmail.com>
2024-05-19 16:47:38 -0700

    Extracting delegation checking logic into `DelegationVisitor`

--- checker-qual/src/main/java/org/checkerframework/common/delegation/qual/Delegate.java
+++ checker-qual/src/main/java/org/checkerframework/common/delegation/qual/Delegate.java
@@ -1,8 +1,10 @@
 package org.checkerframework.common.delegation.qual;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * This is an annotation that indicates a field is a delegate, fields are not delegates by default.
@@ -27,4 +29,5 @@ import java.lang.annotation.RetentionPolicy;
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD})
 public @interface Delegate {}
