63ae9e898e7a28ec29a850da43508b21d1c25bc6
Sascha Kehrli <skehrli@ethz.ch>
2024-05-22 12:52:04 +0200

    add tests for resourceleak-arrays

--- checker/tests/resourceleak-arrays/owningArrayField.java
+++ checker/tests/resourceleak-arrays/owningArrayField.java
@@ -3,7 +3,6 @@ import org.checkerframework.checker.calledmethodsonelements.qual.*;
 import org.checkerframework.checker.calledmethodsonelements.qual.EnsuresCalledMethodsOnElements;
 import org.checkerframework.checker.mustcall.qual.*;
 import org.checkerframework.checker.mustcallonelements.qual.*;
-import org.checkerframework.framework.qual.*;
 
 @InheritableMustCall({"close", "foo"})
 class Resource {
@@ -12,17 +11,112 @@ class Resource {
   public void close() {}
 }
 
-@InheritableMustCall({"destruct", "close"})
-class OwningArrayField {
+class illegalOwningArrayField {
+  // non-final owningarray field is illegal
+  // :: error: owningarray.field.not.final
+  @OwningArray Resource[] arr;
+  // static owningarray field is illegal
+  // :: error: owningarray.field.static
+  // :: error: unfulfilled.mustcallonelements.obligations
+  static final @OwningArray Resource[] arr2 = new Resource[1];
+}
+
+@InheritableMustCall("close")
+class multipleOwningArrayFieldAssignment {
   final @OwningArray Resource[] arr;
 
-  public OwningArrayField(@OwningArray Resource[] arr) {
+  @EnsuresCalledMethodsOnElements(
+      value = "arr",
+      methods = {"close", "foo"})
+  public void close() {
+    // :: error: owningarray.field.assigned.outside.constructor
+    arr[0] = null;
+    for (int i = 0; i < arr.length; i++) {
+      arr[i].close();
+    }
+    for (int i = 0; i < arr.length; i++) {
+      arr[i].foo();
+    }
+  }
+
+  public multipleOwningArrayFieldAssignment(int n) {
+    arr = new Resource[n];
+    // illegal assignment - only in pattern-matched loop allowed
+    // :: error: illegal.owningarray.field.elements.assignment
+    arr[0] = null;
+    for (int i = 0; i < n; i++) {
+      arr[i] = new Resource();
+    }
+    for (int i = 0; i < n; i++) {
+      arr[i].close();
+    }
+    for (int i = 0; i < n; i++) {
+      arr[i].foo();
+    }
+    for (int i = 0; i < n; i++) {
+      // :: error: owningarray.field.elements.assigned.multiple.times
+      arr[i] = new Resource();
+    }
+  }
+}
+
+class NoDestructorMethodForOwningArrayField {
+  // no mustcall annotation on class (for destructor method)
+  // :: error: unfulfilled.mustcallonelements.obligations
+  final @OwningArray Resource[] arr = new Resource[10];
+}
+
+@InheritableMustCall("close")
+class DestructorMethodWithoutEnsuresCmoeForOwningArrayField {
+  // mustcall annotation on class doesn't have a EnsuresCmoe annotation
+  // :: error: unfulfilled.mustcallonelements.obligations
+  final @OwningArray Resource[] arr = new Resource[10];
+
+  public void close() {}
+}
+
+@InheritableMustCall("close")
+class DestructorMethodWithInsufficientEnsuresCmoeForOwningArrayField {
+  // destructor method doesn't cover all calling obligations
+  // :: error: unfulfilled.mustcallonelements.obligations
+  final @OwningArray Resource[] arr = new Resource[10];
+
+  @EnsuresCalledMethodsOnElements(value = "arr", methods = "close")
+  public void close() {
+    for (int i = 0; i < arr.length; i++) {
+      arr[i].close();
+    }
+  }
+}
+
+@InheritableMustCall("close")
+class DestructorMethodWithInvalidEnsuresCmoeForOwningArrayField {
+  final @OwningArray Resource[] arr = new Resource[10];
+
+  @EnsuresCalledMethodsOnElements(
+      value = "arr",
+      methods = {"foo", "close"})
+  // destructor method doesn't fulfill post-condition
+  // :: error: contracts.postcondition
+  public void close() {}
+}
+
+@InheritableMustCall({"destruct", "close"})
+class ValidOwningArrayField {
+  final @OwningArray Resource[] arr;
+
+  public Resource[] getField() {
+    // :: error: return.owningarray
+    return arr;
+  }
+
+  public ValidOwningArrayField(@OwningArray Resource[] arr) {
     this.arr = arr;
   }
 
-  public OwningArrayField(Resource[] arr, int k) {
+  public ValidOwningArrayField(Resource[] arr, int k) {
     // k is here simply because without it, we would have a duplicate constructor.
-    // it's illegal since the arr parameter is not @OwningArray
+    // assignment is illegal since the arr parameter is not @OwningArray
     // :: error: illegal.owningarray.field.assignment
     this.arr = arr;
   }
@@ -46,6 +140,81 @@ class OwningArrayField {
   }
 }
 
+class EvilOwningArrayWrapperClient {
+  public EvilOwningArrayWrapperClient() {
+    int n = 10;
+    @OwningArray Resource[] localarr = new Resource[n];
+    for (int i = 0; i < n; i++) {
+      localarr[i] = new Resource();
+    }
+    // give up ownership to constructor
+    ValidOwningArrayField d = new ValidOwningArrayField(localarr);
+    // this loop doesn't pattern-match, simply because d.arr is not accepted
+    // as an identifier (it is a MemberSelectTree). A warning is issued to inform
+    // the programmer that d.arr is an unexpected array expression.
+    for (int i = 0; i < d.arr.length; i++) {
+      // :: error: required.method.not.called
+      // :: error: illegal.owningarray.field.elements.assignment
+      // :: warning: unexpected.array.expression
+      d.arr[i] = new Resource();
+    }
+    // fulfill the obligations of 'd'
+    d.destruct();
+    d.close();
+  }
+
+  public void tryCapturingOwningArrayField() {
+    int n = 10;
+    @OwningArray Resource[] localarr = new Resource[n];
+    for (int i = 0; i < n; i++) {
+      localarr[i] = new Resource();
+    }
+    // give up ownership to constructor
+    ValidOwningArrayField d = new ValidOwningArrayField(localarr);
+    // try reassigning the elements of array, despite lost ownership
+    for (int i = 0; i < n; i++) {
+      // :: error: assignment.without.ownership
+      localarr[i] = new Resource();
+    }
+    // this method call is not allowed either, due to the missing ownership over localarr
+    // :: error: argument
+    // :: error: nonowning.argument.passed
+    methodWithOwningArrayParameter(localarr);
+
+    // reassign localarr to a new array, which is legal.
+    // However, after its elements have been assigned, they
+    // are never closed, which throws an error.
+    // :: error: unfulfilled.mustcallonelements.obligations
+    localarr = new Resource[n];
+    // assigning its elements is now allowed
+    for (int i = 0; i < n; i++) {
+      localarr[i] = new Resource();
+    }
+
+    // try to capture the @OwningArray field of 'd'
+    // since the RHS of the assignment is @OwningArray, that is illegal aliasing.
+    // :: error: illegal.aliasing
+    Resource[] capture = d.arr;
+    // this illegal assignment counts as a field assignment
+    // and since the field is @OwningArray, it is forbidden
+    // :: error: owningarray.field.assigned.outside.constructor
+    d.arr[0] = null;
+
+    // fulfill the obligations of 'd'
+    d.destruct();
+    d.close();
+  }
+
+  private void methodWithOwningArrayParameter(@OwningArray Resource[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      arr[i].close();
+    }
+    for (int i = 0; i < arr.length; i++) {
+      arr[i].foo();
+    }
+  }
+}
+
 class OwningArrayFieldClient {
   public void m() {
     int n = 10;
@@ -55,7 +224,7 @@ class OwningArrayFieldClient {
     }
     // d.close() would also have to be called
     // :: error: required.method.not.called
-    OwningArrayField d = new OwningArrayField(localarr);
+    ValidOwningArrayField d = new ValidOwningArrayField(localarr);
     d.destruct();
   }
 }
---
+++ checker/tests/resourceleak-arrays/patternMatchOwningArrayLoops.java
@@ -0,0 +1,236 @@
+import java.net.Socket;
+import org.checkerframework.checker.mustcall.qual.Owning;
+import org.checkerframework.checker.mustcallonelements.qual.OwningArray;
+
+class PatternMatchOwningArrayLoops {
+  private final int n = 10;
+  private final String myHost = "";
+  private final int myPort = 1;
+  // :: error: owning.array
+  @Owning Socket[] s;
+
+  // @OwningArray non-(array/collection) is not allowed
+  public void owningArrayNonArray() {
+    // :: error: owningarray.nonarray
+    @OwningArray Socket s;
+  }
+
+  public void illegalOwningArrayAssignment() {
+    Socket[] s = new Socket[n];
+    // this is a false positive, but we only allow assignment
+    // of an @OwningArray to a new [].
+    // :: error: illegal.owningarray.assignment
+    @OwningArray Socket[] arr = s;
+  }
+
+  // test that aliasing is not allowed.
+  public void illegalAliasing() {
+    @OwningArray Socket[] arr = new Socket[n];
+    // :: error: illegal.owningarray.assignment
+    @OwningArray Socket[] arr2 = arr;
+    // :: error: illegal.aliasing
+    Socket[] arr3 = arr;
+  }
+
+  // test that declaring an @OwningArray is alright
+  public void illegalOwningArrayElementAssignment() {
+    @OwningArray Socket[] arr = new Socket[n];
+    try {
+      // :: error: illegal.owningarray.element.assignment
+      // :: error: required.method.not.called
+      arr[0] = new Socket(myHost, myPort);
+    } catch (Exception e) {
+    }
+    // :: error: illegal.owningarray.element.assignment
+    arr[0] = null;
+  }
+
+  public void unfulfilledAllocationLoop() {
+    // :: error: unfulfilled.mustcallonelements.obligations
+    @OwningArray Socket[] arr = new Socket[n];
+    for (int i = 0; i < n; i++) {
+      try {
+        arr[i] = new Socket(myHost, myPort);
+      } catch (Exception e) {
+      }
+    }
+  }
+
+  // test that opening and subsequent closing is alright
+  public void validClosing() {
+    @OwningArray Socket[] arr = new Socket[n];
+    for (int i = 0; i < n; i++) {
+      try {
+        arr[i] = new Socket(myHost, myPort);
+      } catch (Exception e) {
+      }
+    }
+    for (int i = 0; i < n; i++) {
+      try {
+        arr[i].close();
+      } catch (Exception e) {
+      }
+    }
+  }
+
+  public void validDeallocationLoop() {
+    @OwningArray Socket[] arr = new Socket[n];
+    for (int i = 0; i < n; i++) {
+      try {
+        arr[i] = new Socket(myHost, myPort);
+      } catch (Exception e) {
+      }
+    }
+    // this deallocation loop is legal and should be pattern-matched
+    for (int i = 0; i < n; i++) {
+      try {
+        try {
+          arr[i].close();
+        } catch (Exception e) {
+        }
+      } catch (Exception e) {
+      }
+    }
+  }
+
+  public void invalidDeallocationLoop() {
+    // :: error: unfulfilled.mustcallonelements.obligations
+    @OwningArray Socket[] arr = new Socket[n];
+    for (int i = 0; i < n; i++) {
+      try {
+        arr[i] = new Socket(myHost, myPort);
+      } catch (Exception e) {
+      }
+    }
+    // this deallocation loop is illegal and is not pattern-matched
+    for (int i = 0; i < n; i++) {
+      try {
+        arr[i].close();
+        i++;
+      } catch (Exception e) {
+      }
+    }
+  }
+
+  public void invalidDeallocationLoop2() {
+    // :: error: unfulfilled.mustcallonelements.obligations
+    @OwningArray Socket[] arr = new Socket[n];
+    for (int i = 0; i < n; i++) {
+      try {
+        arr[i] = new Socket(myHost, myPort);
+      } catch (Exception e) {
+      }
+    }
+    // this deallocation loop is illegal and is not pattern-matched
+    for (int i = 0; i < n; i++) {
+      try {
+        arr[i].close();
+      } catch (Exception e) {
+      } finally {
+        break;
+      }
+    }
+  }
+
+  public void invalidDeallocationLoop3() throws Exception {
+    // :: error: unfulfilled.mustcallonelements.obligations
+    @OwningArray Socket[] arr = new Socket[n];
+    for (int i = 0; i < n; i++) {
+      try {
+        arr[i] = new Socket(myHost, myPort);
+      } catch (Exception e) {
+      }
+    }
+    // this deallocation loop is illegal and is not pattern-matched
+    for (int i = 0; i < n; i++) {
+      try {
+        arr[i].close();
+      } catch (Exception e) {
+        throw new Exception("this prevents a pattern-match");
+      }
+    }
+  }
+
+  public void invalidDeallocationLoop4() {
+    // :: error: unfulfilled.mustcallonelements.obligations
+    @OwningArray Socket[] arr = new Socket[n];
+    for (int i = 0; i < n; i++) {
+      try {
+        arr[i] = new Socket(myHost, myPort);
+      } catch (Exception e) {
+      }
+    }
+    // this deallocation loop is illegal and is not pattern-matched
+    for (int i = 0; i < n; i++) {
+      try {
+        arr[i].close();
+      } catch (Exception e) {
+      } finally {
+        i += 2;
+      }
+    }
+  }
+
+  // test that opening and subsequent closing is alright
+  public void invalidClosingAndReopening() {
+    // :: error: unfulfilled.mustcallonelements.obligations
+    @OwningArray Socket[] arr = new Socket[n];
+    for (int i = 0; i < n; i++) {
+      try {
+        arr[i] = new Socket(myHost, myPort);
+      } catch (Exception e) {
+      }
+    }
+    for (int i = 0; i < n; i++) {
+      try {
+        arr[i].close();
+      } catch (Exception e) {
+      }
+    }
+    for (int i = 0; i < n; i++) {
+      try {
+        arr[i] = new Socket(myHost, myPort);
+      } catch (Exception e) {
+      }
+    }
+  }
+
+  public void invalidReallocateElements() {
+    // :: error: unfulfilled.mustcallonelements.obligations
+    @OwningArray Socket[] arr = new Socket[n];
+    for (int i = 0; i < n; i++) {
+      try {
+        arr[i] = new Socket(myHost, myPort);
+      } catch (Exception e) {
+      }
+    }
+    for (int i = 0; i < n; i++) {
+      try {
+        // :: error: illegal.owningarray.allocation
+        arr[i] = new Socket(myHost, myPort);
+      } catch (Exception e) {
+      }
+    }
+  }
+
+  // test that passing ownership works
+  public void validClosingByEnsuresCmoeMethod() {
+    @OwningArray Socket[] arr = new Socket[n];
+    for (int i = 0; i < n; i++) {
+      try {
+        arr[i] = new Socket(myHost, myPort);
+      } catch (Exception e) {
+      }
+    }
+    close(arr);
+  }
+
+  public void close(@OwningArray Socket[] arr) {
+    for (int i = 0; i < n; i++) {
+      try {
+        arr[i].close();
+      } catch (Exception e) {
+      }
+    }
+  }
+}
