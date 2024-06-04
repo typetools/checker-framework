bbc9c9369e85f4f03fd37cf259e739362e9e3070
James Yoo <jyoo980@gmail.com>
2024-06-04 09:48:21 -0700

    Fix JavaDoc

--- dataflow/src/main/java/org/checkerframework/dataflow/expression/JavaExpressionVisitor.java
+++ dataflow/src/main/java/org/checkerframework/dataflow/expression/JavaExpressionVisitor.java
@@ -104,10 +104,8 @@ public abstract class JavaExpressionVisitor<R, P> {
    * Visit a {@link SuperReference}.
    *
    * @param superExpr the JavaExpression to visit
-   * @param p the parameter to pass to the visit method <<<<<<< HEAD
-   * @return the result of visiting {@code superExpr} =======
-   * @return the result of visiting the {@code superExpr} >>>>>>>
-   *     yoo/implement-super-javaexpresssion
+   * @param p the parameter to pass to the visit method
+   * @return the result of visiting {@code superExpr}
    */
   protected abstract R visitSuperReference(SuperReference superExpr, P p);
 
