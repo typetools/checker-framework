610a4a51e6e2ca3e4fcb967367271b4793157907
Suzanne Millstein <smillst@cs.washington.edu>
2024-06-05 08:35:18 -0700

    Add comment to test case.

--- framework/tests/all-systems/Issue6078.java
+++ framework/tests/all-systems/Issue6078.java
@@ -2,6 +2,7 @@ import java.lang.invoke.MethodHandle;
 
 public class Issue6078 {
   static void call(MethodHandle methodHandle) throws Throwable {
+    // The vararg parameter disappears for the below method. It's not clear why this happens.
     methodHandle.invoke();
   }
 
