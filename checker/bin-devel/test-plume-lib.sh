53a46a841686c1ddd64e51c3669458795b38fd85
Michael Ernst <mernst@cs.washington.edu>
2024-06-24 12:32:27 -0700

    Update version number, remove debugging

--- checker/bin-devel/test-plume-lib.sh
+++ checker/bin-devel/test-plume-lib.sh
@@ -43,7 +43,7 @@ for PACKAGE in "${PACKAGES[@]}"; do
   echo "PACKAGE=${PACKAGE}"
   PACKAGEDIR="/tmp/${PACKAGE}"
   rm -rf "${PACKAGEDIR}"
-  "$SCRIPTDIR/.git-scripts/git-clone-related" --debug plume-lib "${PACKAGE}" "${PACKAGEDIR}"
+  "$SCRIPTDIR/.git-scripts/git-clone-related" plume-lib "${PACKAGE}" "${PACKAGEDIR}"
   # Uses "compileJava" target instead of "assemble" to avoid the javadoc error "Error fetching URL:
   # https://docs.oracle.com/en/java/javase/17/docs/api/" due to network problems.
   echo "About to call ./gradlew --console=plain -PcfLocal compileJava"
