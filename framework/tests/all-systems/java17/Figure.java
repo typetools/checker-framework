7d859caf9937fff239d3a1a455ab0b8aead5a42f
Suzanne Millstein <smillst@cs.washington.edu>
2024-06-25 10:48:17 -0700

    Update to version 3.26.1 of stubparser.

--- framework/tests/all-systems/java17/Figure.java
+++ framework/tests/all-systems/java17/Figure.java
@@ -1,4 +1,5 @@
 // @below-java17-jdk-skip-test
+// @infer-jaifs-skip-test
 // from https://docs.oracle.com/en/java/javase/15/language/sealed-classes-and-interfaces.html
 
 public sealed class Figure
@@ -7,18 +8,22 @@ public sealed class Figure
 // defined in the same file.
 {}
 
+@SuppressWarnings("initializedfields:contracts.postcondition")
 final class Circle extends Figure {
   float radius;
 }
 
+@SuppressWarnings("initializedfields:contracts.postcondition")
 non-sealed class Square extends Figure {
   float side;
 }
 
+@SuppressWarnings("initializedfields:contracts.postcondition")
 sealed class Rectangle extends Figure {
   float length, width;
 }
 
+@SuppressWarnings("initializedfields:contracts.postcondition")
 final class FilledRectangle extends Rectangle {
   int red, green, blue;
 }
