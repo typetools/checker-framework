4861495f419f5ca0e52fe5eb03fed435dd34db73
iywang2016 <ivyk2016@gmail.com>
2024-06-26 14:23:29 -0700

    Revised @QualifierForLiterals annotation to automatically annotate escaped SQL values as SqlSanitized.

--- checker-qual/src/main/java/org/checkerframework/checker/sqltainting/qual/SqlSanitized.java
+++ checker-qual/src/main/java/org/checkerframework/checker/sqltainting/qual/SqlSanitized.java
@@ -6,7 +6,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.checkerframework.framework.qual.DefaultFor;
-import org.checkerframework.framework.qual.LiteralKind;
 import org.checkerframework.framework.qual.QualifierForLiterals;
 import org.checkerframework.framework.qual.SubtypeOf;
 import org.checkerframework.framework.qual.TypeUseLocation;
@@ -14,11 +13,15 @@ import org.checkerframework.framework.qual.TypeUseLocation;
 /**
  * Denotes a value to be used in a SQL query that has been sanitized (i.e. non-alphanumeric
  * characters escaped as necessary) and is thus safe for SQL query use.
+ *
+ * <p>All empty Strings and String literals consisting solely of alphanumeric characters,
+ * whitespace, and backslash-escaped sensitive characters (i.e. single/double quotes, backslashes,
+ * hyphens, percent signs, underscores) are annotated SQLSanitized automatically.
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 @SubtypeOf(SqlQueryValue.class)
-@QualifierForLiterals(LiteralKind.STRING)
+@QualifierForLiterals(stringPatterns = "^(\\w|\\s|\\\\'|\\\\\"|\\\\\\\\|\\\\-|\\\\%|\\\\_)*$")
 @DefaultFor(TypeUseLocation.LOWER_BOUND)
 public @interface SqlSanitized {}
