0df969f63a2f2383a9c038c465d9a0e21ed7af75
James Yoo <jyoo980@gmail.com>
2024-07-07 08:16:13 -0400

    Update `@skip-test` comment in `OptionalRelyGuaranteeTest`

--- checker/tests/optional/OptionalRelyGuaranteeTest.java
+++ checker/tests/optional/OptionalRelyGuaranteeTest.java
@@ -4,7 +4,8 @@ import org.checkerframework.checker.nonempty.qual.*;
 import org.checkerframework.checker.optional.qual.*;
 import org.checkerframework.dataflow.qual.Pure;
 
-// @skip-test: ignore until we have the correct checkers in-place
+// @skip-test: ignore until we have the correct checkers in-place, that is, until we run the
+// RevisedOptionalChecker by default as the Optional Checker.
 // This currently verifies via javacheck -processor revisedoptional OptionalRelyGuaranteeTest.java
 // at e97204746a33f0fc374e6e74e0bd028f49f4de53
 
