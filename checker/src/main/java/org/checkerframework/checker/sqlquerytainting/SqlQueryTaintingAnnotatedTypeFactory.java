ab3f4e44cc2bc4fea55b38f1b4a6142343f08129
iywang2016 <ivyk2016@gmail.com>
2024-07-09 12:14:00 -0700

    Created sqlquerytainting directory

---
+++ checker/src/main/java/org/checkerframework/checker/sqlquerytainting/SqlQueryTaintingAnnotatedTypeFactory.java
@@ -0,0 +1,36 @@
+package org.checkerframework.checker.sqltainting;
+
+import java.util.Set;
+import javax.lang.model.element.AnnotationMirror;
+import org.checkerframework.checker.sqltainting.qual.SqlSanitized;
+import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
+import org.checkerframework.common.basetype.BaseTypeChecker;
+import org.checkerframework.javacutil.AnnotationBuilder;
+import org.checkerframework.javacutil.AnnotationMirrorSet;
+
+/** Annotated type factory for the SQL Tainting Checker. */
+public class SqlTaintingAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
+
+  /** The {@code @}{@link SqlSanitized} annotation mirror. */
+  private final AnnotationMirror SQLSANITIZED;
+
+  /** A singleton set containing the {@code @}{@link SqlSanitized} annotation mirror. */
+  private final AnnotationMirrorSet setOfSqlSanitized;
+
+  /**
+   * Creates a {@link SqlTaintingAnnotatedTypeFactory}.
+   *
+   * @param checker the SQL tainting checker
+   */
+  public SqlTaintingAnnotatedTypeFactory(BaseTypeChecker checker) {
+    super(checker);
+    this.SQLSANITIZED = AnnotationBuilder.fromClass(getElementUtils(), SqlSanitized.class);
+    this.setOfSqlSanitized = AnnotationMirrorSet.singleton(SQLSANITIZED);
+    postInit();
+  }
+
+  @Override
+  protected Set<AnnotationMirror> getEnumConstructorQualifiers() {
+    return setOfSqlSanitized;
+  }
+}
