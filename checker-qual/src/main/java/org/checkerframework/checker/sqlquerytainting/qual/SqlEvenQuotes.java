4acc56dc4186008d88ae976c98442599174a7a13
iywang2016 <ivyk2016@gmail.com>
2024-07-11 16:51:04 -0700

    Revised qualifiers to SqlOddQuotes, SqlEvenQuotes, SqlQuoteless

--- checker-qual/src/main/java/org/checkerframework/checker/sqlquerytainting/qual/SqlEvenQuotes.java
+++ checker-qual/src/main/java/org/checkerframework/checker/sqlquerytainting/qual/SqlEvenQuotes.java
@@ -5,18 +5,19 @@ import org.checkerframework.framework.qual.QualifierForLiterals;
 import org.checkerframework.framework.qual.SubtypeOf;
 
 /**
- * Used to denote a String that comprises part of a SQL query and
- * contains an even number of unescaped single quotes – i.e., there
- * must be an even number of ‘ characters in a SqlEvenQuotes String
- * that are not preceded immediately by a / character. SQLEvenQuotes
- * Strings are safe to be passed to query execution methods.
+ * Used to denote a String that comprises part of a SQL query and contains an even number of
+ * unescaped single quotes – i.e., there must be an even number of ‘ characters in a SqlEvenQuotes
+ * String that are not preceded immediately by a / character. SQLEvenQuotes Strings are safe to be
+ * passed to query execution methods.
  *
- * Common use cases include: complete SQL queries, such as “SELECT *
- * FROM table WHERE field = ‘value’”.
+ * <p>Common use cases include: complete SQL queries, such as “SELECT * FROM table WHERE field =
+ * ‘value’”.
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 @SubtypeOf(SqlQueryUnknown.class)
-@QualifierForLiterals(stringPatterns = "^(([^\\\\\\']|\\\\.)*\\'){2}([^\\\\\\']|(\\'([^\\\\\\']|\\\\.)*\\')|\\\\.)*\\\\?$")
+@QualifierForLiterals(
+    stringPatterns =
+        "^(([^\\\\\\']|\\\\.)*\\'){2}([^\\\\\\']|(\\'([^\\\\\\']|\\\\.)*\\')|\\\\.)*\\\\?$")
 public @interface SqlEvenQuotes {}
--- checker-qual/src/main/java/org/checkerframework/checker/sqlquerytainting/qual/SqlQuoteless.java
+++ checker-qual/src/main/java/org/checkerframework/checker/sqlquerytainting/qual/SqlQuoteless.java
@@ -9,14 +9,13 @@ import org.checkerframework.framework.qual.QualifierForLiterals;
 import org.checkerframework.framework.qual.SubtypeOf;
 
 /**
- * Used to denote a String that comprises part of a SQL query and
- * contains exactly zero unescaped single quotes – i.e., all occurrences
- * of the ‘ character in such a String are preceded immediately by a /
- * character. SQLQuoteless Strings are safe to be passed to query execution
+ * Used to denote a String that comprises part of a SQL query and contains exactly zero unescaped
+ * single quotes – i.e., all occurrences of the ‘ character in such a String are preceded
+ * immediately by a / character. SQLQuoteless Strings are safe to be passed to query execution
  * methods.
  *
- * Common use cases include SQL query fragments such as “SELECT * FROM”
- * and properly sanitized user input.
+ * <p>Common use cases include SQL query fragments such as “SELECT * FROM” and properly sanitized
+ * user input.
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
