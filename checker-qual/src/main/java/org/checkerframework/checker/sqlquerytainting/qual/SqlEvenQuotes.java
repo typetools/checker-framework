2843a300b386c59ae9c29054075f0e3ce69f1a6b
iywang2016 <ivyk2016@gmail.com>
2024-07-12 15:02:29 -0700

    Revised documentation and implementation of SQL query qualifiers: combined SqlQuoteless and SqlEvenQuotes; revised documentations

--- checker-qual/src/main/java/org/checkerframework/checker/sqlquerytainting/qual/SqlEvenQuotes.java
+++ checker-qual/src/main/java/org/checkerframework/checker/sqlquerytainting/qual/SqlEvenQuotes.java
@@ -5,19 +5,18 @@ import org.checkerframework.framework.qual.QualifierForLiterals;
 import org.checkerframework.framework.qual.SubtypeOf;
 
 /**
- * Used to denote a String that comprises part of a SQL query and contains an even number of
- * unescaped single quotes – i.e., there must be an even number of ‘ characters in a SqlEvenQuotes
- * String that are not preceded immediately by a / character. SQLEvenQuotes Strings are safe to be
- * passed to query execution methods.
+ * SqlEvenQuotes: Used to denote a String that comprises part of a SQL query and contains either
+ * zero or an even number of unescaped single quotes – i.e., there must be either zero or an even
+ * number of ‘ characters in a SqlEvenQuotes String that are not preceded immediately by a \
+ * character. SqlEvenQuotes Strings are syntactical and safe to be passed to query execution
+ * methods.
  *
- * <p>Common use cases include: complete SQL queries, such as “SELECT * FROM table WHERE field =
- * ‘value’”.
+ * <p>Common use cases include: SQL query fragments, such as “SELECT * FROM”; properly sanitized
+ * user input; and complete SQL queries, such as “SELECT * FROM table WHERE field = ‘value’”.
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 @SubtypeOf(SqlQueryUnknown.class)
-@QualifierForLiterals(
-    stringPatterns =
-        "^(([^\\\\\\']|\\\\.)*\\'){2}([^\\\\\\']|(\\'([^\\\\\\']|\\\\.)*\\')|\\\\.)*\\\\?$")
+@QualifierForLiterals(stringPatterns = "^((([^\\\\']|\\\\.)*'){2})*([^\\\\']|\\\\.)*\\\\?$")
 public @interface SqlEvenQuotes {}
--- checker-qual/src/main/java/org/checkerframework/checker/sqlquerytainting/qual/SqlQueryUnknown.java
+++ checker-qual/src/main/java/org/checkerframework/checker/sqlquerytainting/qual/SqlQueryUnknown.java
@@ -4,7 +4,13 @@ import java.lang.annotation.*;
 import org.checkerframework.framework.qual.DefaultQualifierInHierarchy;
 import org.checkerframework.framework.qual.SubtypeOf;
 
-/** Represents the top of the SQL query tainting qualifier hierarchy. */
+/**
+ * Represents the top of the SQL query tainting qualifier hierarchy. Used to denote a String that
+ * comprises part of a SQL query, the quoting of which is unknown. SqlQueryUnknown Strings are SQL
+ * injection vulnerabilities and thus unsafe to be passed to query execution methods.
+ *
+ * <p>Common use cases include unsanitized user input.
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
