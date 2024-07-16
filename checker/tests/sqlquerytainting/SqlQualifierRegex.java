db6bf2a1da295a944237e9a6c5ab0c9f843378bc
iywang2016 <ivyk2016@gmail.com>
2024-07-16 02:36:03 -0700

    Added test file for qualifier regex

--- checker/tests/sqlquerytainting/SqlQualifierRegex.java
+++ checker/tests/sqlquerytainting/SqlQualifierRegex.java
@@ -3,10 +3,51 @@ import org.checkerframework.checker.sqlquerytainting.qual.SqlOddQuotes;
 
 public class SqlQualifierRegex {
 
-  void oddQuotes() {
+  void oddNoEscaped() {
     // :: error: (assignment)
-    @SqlOddQuotes String odd1 = "";
+    @SqlOddQuotes String none = "asdf";
+    @SqlOddQuotes String one = "asdf'asdf";
+    // :: error: (assignment)
+    @SqlOddQuotes String two = "'asdf'";
+    @SqlOddQuotes String three = "'asdf'asdf'";
+    // :: error: (assignment)
+    @SqlOddQuotes String manyEven = "'asdf''asdf'asdf'asdf'''";
+    @SqlOddQuotes String manyOdd = "''asdf'asdf'''asdf'asdf''";
+  }
 
-    @SqlEvenQuotes String even1 = "";
+  void oddWithBackslashes() {
+    // :: error: (assignment)
+    @SqlOddQuotes String none = "asdf\\'";
+    @SqlOddQuotes String one = "asdf\\''asdf";
+    // :: error: (assignment)
+    @SqlOddQuotes String two = "'a\\'sdf'";
+    @SqlOddQuotes String three = "\\''asdf'\\asdf'";
+    // :: error: (assignment)
+    @SqlOddQuotes String manyEven = "'as\\'df'\\''a\\sdf'asdf'as\\'\\'df'\\'''";
+    @SqlOddQuotes String manyOdd = "'\\''a\\sdf'asdf'''as\\df'as\\'df''\\'";
+  }
+
+  void evenNoEscaped() {
+    @SqlEvenQuotes String none = "";
+    // :: error: (assignment)
+    @SqlEvenQuotes String one = "'asdf";
+    @SqlEvenQuotes String two = "''asdf";
+    // :: error: (assignment)
+    @SqlEvenQuotes String three = "asdf'asdf''";
+    @SqlEvenQuotes String manyEven = "''asdf''asdf'asdf''asdf'asdf";
+    // :: error: (assignment)
+    @SqlEvenQuotes String manyOdd = "asdf''''asdf'asdf'asdf'asdf";
+  }
+
+  void evenWithBackslashes() {
+    @SqlEvenQuotes String none = "\\'\\'\\'";
+    // :: error: (assignment)
+    @SqlEvenQuotes String one = "'asdf\\'";
+    @SqlEvenQuotes String two = "'\\''a\\sdf";
+    // :: error: (assignment)
+    @SqlEvenQuotes String three = "asd\\'f'asdf''";
+    @SqlEvenQuotes String manyEven = "\\'''asdf''asd\\f'\\asdf'\\''asdf'asdf\\'";
+    // :: error: (assignment)
+    @SqlEvenQuotes String manyOdd = "asdf''\\'''\\asdf'asdf'a\\'sdf'\\'asdf";
   }
 }
