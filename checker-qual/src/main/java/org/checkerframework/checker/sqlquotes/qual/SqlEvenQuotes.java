e6212d3ea2956e71e96adeee0e2073b4c212c739
iywang2016 <ivyk2016@gmail.com>
2024-07-18 23:15:10 -0700

    spotlessApply formatting

--- checker-qual/src/main/java/org/checkerframework/checker/sqlquotes/qual/SqlEvenQuotes.java
+++ checker-qual/src/main/java/org/checkerframework/checker/sqlquotes/qual/SqlEvenQuotes.java
@@ -9,13 +9,13 @@ import org.checkerframework.framework.qual.QualifierForLiterals;
 import org.checkerframework.framework.qual.SubtypeOf;
 
 /**
- * Used to denote a String that contains either zero or an even number of unescaped
- * single quotes – i.e., there must be either zero or an even number of ‘ characters
- * in a SqlEvenQuotes String that are not preceded immediately by a SQL \ character.
- * (Thus, written in Java, every occurrence of the single quote in a SqlEvenQuotes
- * String must be preceded by a double backslash.) SqlEvenQuotes Strings are syntactical
- * to be passed to query execution methods and are guaranteed to have no impact on
- * whether subsequent concatenations are interpreted as SQL command code or SQL query values.
+     * Used to denote a String that contains either zero or an even number of unescaped single quotes –
+ * i.e., there must be either zero or an even number of ‘ characters in a SqlEvenQuotes String that
+ * are not preceded immediately by a SQL \ character. (Thus, written in Java, every occurrence of
+ * the single quote in a SqlEvenQuotes String must be preceded by a double backslash.) SqlEvenQuotes
+ * Strings are syntactical to be passed to query execution methods and are guaranteed to have no
+ * impact on whether subsequent concatenations are interpreted as SQL command code or SQL query
+ * values.
  *
  * <p>Common use cases include: SQL query fragments, such as “SELECT * FROM”; properly sanitized
  * user input; and complete SQL queries, such as “SELECT * FROM table WHERE field = ‘value’”.
