147a626e3d6874bbc608e6b8fefc9d4bd01d1d6b
Sascha Kehrli <skehrli@ethz.ch>
2024-07-24 13:56:11 +0200

    make mc subchecker of rlccm

--- checker/src/main/java/org/checkerframework/checker/rlccalledmethods/RLCCalledMethodsChecker.java
+++ checker/src/main/java/org/checkerframework/checker/rlccalledmethods/RLCCalledMethodsChecker.java
@@ -1,8 +1,12 @@
 package org.checkerframework.checker.rlccalledmethods;
 
+import java.util.Set;
 import org.checkerframework.checker.calledmethods.CalledMethodsChecker;
+import org.checkerframework.checker.mustcall.MustCallChecker;
+import org.checkerframework.checker.mustcall.MustCallNoCreatesMustCallForChecker;
 import org.checkerframework.checker.resourceleak.ResourceLeakChecker;
 import org.checkerframework.checker.resourceleak.SetOfTypes;
+import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.common.basetype.BaseTypeVisitor;
 
 /**
@@ -29,5 +33,18 @@ public class RLCCalledMethodsChecker extends CalledMethodsChecker {
     return ((RLCCalledMethodsAnnotatedTypeFactory) getTypeFactory())
         .getResourceLeakChecker()
         .getIgnoredExceptions();
+  }
+
+  @Override
+  protected Set<Class<? extends BaseTypeChecker>> getImmediateSubcheckerClasses() {
+    Set<Class<? extends BaseTypeChecker>> checkers = super.getImmediateSubcheckerClasses();
+
+    if (this.processingEnv.getOptions().containsKey(MustCallChecker.NO_CREATES_MUSTCALLFOR)) {
+      checkers.add(MustCallNoCreatesMustCallForChecker.class);
+    } else {
+      checkers.add(MustCallChecker.class);
+    }
+
+    return checkers;
   }
 }
