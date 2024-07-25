1a35b7e4be9d8e877d107185b374c06429ac5910
iywang2016 <ivyk2016@gmail.com>
2024-07-25 01:36:37 -0700

    Ran spotlessApply

--- checker-qual/src/main/java/org/checkerframework/checker/sqlquotes/qual/SqlEvenQuotes.java
+++ checker-qual/src/main/java/org/checkerframework/checker/sqlquotes/qual/SqlEvenQuotes.java
@@ -12,10 +12,9 @@ import org.checkerframework.framework.qual.SubtypeOf;
  * Used to denote a String that contains either zero or an even number of unescaped single quotes –
  * i.e., there must be either zero or an even number of ‘ characters in a SqlEvenQuotes String that
  * are not preceded immediately by another ' character. (Thus, all SqlEvenQuotes Strings ultimately
- * contain an even number of ' characters, escaped or otherwise.)
- * SqlEvenQuotes Strings are syntactical to be passed to query execution methods and are guaranteed
- * to have no impact on whether subsequent concatenations are interpreted as SQL command code or as
- * SQL query values.
+ * contain an even number of ' characters, escaped or otherwise.) SqlEvenQuotes Strings are
+ * syntactical to be passed to query execution methods and are guaranteed to have no impact on
+ * whether subsequent concatenations are interpreted as SQL command code or as SQL query values.
  *
  * <p>Common use cases include: SQL query fragments, such as “SELECT * FROM”; properly sanitized
  * user input; and complete SQL queries, such as “SELECT * FROM table WHERE field = ‘value’”.
