03ae9ee46cde8f15d064c4d4bf4f38bcfb178b52
iywang2016 <ivyk2016@gmail.com>
2024-08-23 13:02:48 -0700

    Renamed Apache logger to avoid overlapping file names

--- checker/src/main/java/org/checkerframework/checker/confidential/ConfidentialChecker.java
+++ checker/src/main/java/org/checkerframework/checker/confidential/ConfidentialChecker.java
@@ -12,6 +12,6 @@ import org.checkerframework.framework.source.SuppressWarningsPrefix;
  *
  * @checker_framework.manual #confidential-checker Confidential Checker
  */
-@StubFiles({"Logger.astub", "Log.astub"})
+@StubFiles({"Log4jLogger.astub", "Log.astub"})
 @SuppressWarningsPrefix({"confidential"})
 public class ConfidentialChecker extends BaseTypeChecker {}
