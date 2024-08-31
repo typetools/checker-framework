d1161bb1fabf8bfca2c09f4e23620433ab0f18ec
James Yoo <jyoo980@gmail.com>
2024-08-31 15:09:50 -0400

    Applying lint updates

--- checker/tests/optional/SubtypeCheck.java
+++ checker/tests/optional/SubtypeCheck.java
@@ -6,9 +6,7 @@ import org.checkerframework.checker.optional.qual.Present;
 public class SubtypeCheck {
 
   @SuppressWarnings("optional.parameter")
-  void foo(
-      @MaybePresent Optional<String> mp,
-      @Present Optional<String> p) {
+  void foo(@MaybePresent Optional<String> mp, @Present Optional<String> p) {
     @MaybePresent Optional<String> mp2 = mp;
     @MaybePresent Optional<String> mp3 = p;
     // :: error: (assignment)
