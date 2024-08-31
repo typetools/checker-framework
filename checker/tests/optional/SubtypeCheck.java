7b534bd8a793b579cdb7bc99af6d57192e8ef7a8
James Yoo <jyoo980@gmail.com>
2024-08-31 15:05:48 -0400

    Removing `OptionalBottom` from the Optional Checker

--- checker/tests/optional/SubtypeCheck.java
+++ checker/tests/optional/SubtypeCheck.java
@@ -1,6 +1,5 @@
 import java.util.Optional;
 import org.checkerframework.checker.optional.qual.MaybePresent;
-import org.checkerframework.checker.optional.qual.OptionalBottom;
 import org.checkerframework.checker.optional.qual.Present;
 
 /** Basic test of subtyping. */
@@ -9,19 +8,11 @@ public class SubtypeCheck {
   @SuppressWarnings("optional.parameter")
   void foo(
       @MaybePresent Optional<String> mp,
-      @Present Optional<String> p,
-      @OptionalBottom Optional<String> ob) {
+      @Present Optional<String> p) {
     @MaybePresent Optional<String> mp2 = mp;
     @MaybePresent Optional<String> mp3 = p;
-    @MaybePresent Optional<String> mp4 = ob;
     // :: error: (assignment)
     @Present Optional<String> p2 = mp;
     @Present Optional<String> p3 = p;
-    @Present Optional<String> p4 = ob;
-    // :: error: (assignment)
-    @OptionalBottom Optional<String> ob2 = mp;
-    // :: error: (assignment)
-    @OptionalBottom Optional<String> ob3 = p;
-    @OptionalBottom Optional<String> ob4 = ob;
   }
 }
