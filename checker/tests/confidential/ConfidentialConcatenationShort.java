3518e33b460dd5151a07fdc7757b830ce760be34
Michael Ernst <mernst@cs.washington.edu>
2024-09-03 16:57:02 -0700

    Expand test

--- checker/tests/confidential/ConfidentialConcatenationShort.java
+++ checker/tests/confidential/ConfidentialConcatenationShort.java
@@ -5,7 +5,12 @@ import org.checkerframework.checker.confidential.qual.NonConfidential;
 
 public class ConfidentialConcatenationShort {
 
-  void concatenation(@NonConfidential String s1, String s2) {
+  void concatenationInitialization(@NonConfidential String s1, String s2) {
     @Confidential String s_1 = s1 + s1;
+  }
+
+  void concatenation(@NonConfidential String s1, String s2) {
+    @Confidential String s_1;
+    s_1 = s1 + s1;
   }
 }
