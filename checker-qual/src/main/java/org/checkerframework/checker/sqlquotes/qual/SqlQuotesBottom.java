077afe8510d07a62169bd77b7ef96464821a274c
iywang2016 <146040711+iywang2016@users.noreply.github.com>
2024-09-09 15:07:17 -0700

    Add SQL Quotes checker
    Co-authored-by: Michael Ernst <mernst@cs.washington.edu>

---
+++ checker-qual/src/main/java/org/checkerframework/checker/sqlquotes/qual/SqlQuotesBottom.java
@@ -0,0 +1,27 @@
+package org.checkerframework.checker.sqlquotes.qual;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.checkerframework.framework.qual.DefaultFor;
+import org.checkerframework.framework.qual.InvisibleQualifier;
+import org.checkerframework.framework.qual.SubtypeOf;
+import org.checkerframework.framework.qual.TargetLocations;
+import org.checkerframework.framework.qual.TypeUseLocation;
+
+/**
+ * Represents the bottom of the SQL Quotes qualifier hierarchy.
+ *
+ * @checker_framework.manual #sql-quotes-checker SQL Quotes Checker
+ * @checker_framework.manual #bottom-type the bottom type
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+@TargetLocations({TypeUseLocation.EXPLICIT_LOWER_BOUND, TypeUseLocation.EXPLICIT_UPPER_BOUND})
+@InvisibleQualifier
+@SubtypeOf({SqlEvenQuotes.class, SqlOddQuotes.class})
+@DefaultFor(value = {TypeUseLocation.LOWER_BOUND})
+public @interface SqlQuotesBottom {}
--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -1,7 +1,10 @@
-Version 3.47.0 (October 1, 2024)
+Version 3.47.1 (October 1, 2024)
 --------------------------------
 
 **User-visible changes:**
+
+The new SqlQuotesChecker prevents errors in quoting in SQL queries.  It prevents
+injection attacks that exploit quoting errors.
 
 **Implementation details:**
 
