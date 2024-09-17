bab909a3d896bf86cee337faedef74945c97faad
Suzanne Millstein <smillst@cs.washington.edu>
2024-09-17 10:49:56 -0700

    Update test.

--- framework/tests/all-systems/Issue6725.java
+++ framework/tests/all-systems/Issue6725.java
@@ -3,6 +3,7 @@ package typearginfer;
 import java.util.stream.Stream;
 
 public class Issue6725 {
+  @SuppressWarnings("methodref.receiver")
   static <T> void prefix(Stream<? extends Iterable<? extends T>> iterables) {
     iterables.map(Iterable::iterator);
   }
