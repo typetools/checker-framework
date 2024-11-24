ec084f9424bccfdca813e458a2b7f0449721d078
James Yoo <jyoo980@gmail.com>
2024-11-24 11:26:14 -0800

    Making the Non-Empty test suite pass

--- checker/src/main/java/org/checkerframework/checker/nonempty/NonEmptyChecker.java
+++ checker/src/main/java/org/checkerframework/checker/nonempty/NonEmptyChecker.java
@@ -12,20 +12,23 @@ import org.checkerframework.framework.source.SupportedOptions;
  * A type-checker that prevents {@link java.util.NoSuchElementException} in the use of container
  * classes.
  *
- * <p>If "-ArunAsOptionalChecker" is passed, then the Non-Empty Checker acts like the Optional
- * Checker, but with better precision. More specifically, it:
+ * <p>Runs the {@link org.checkerframework.checker.optional.OptionalImplChecker}(as a subchecker),
+ * using explicitly-written (i.e., programmer-written) annotations from the Non-Empty type system to
+ * refine the Optional analysis. This improves analysis of operations on containers (e.g., Streams,
+ * Collections) that result in values of type Optional.
  *
- * <ol>
- *   <li>Runs the Optional Checker (as a subchecker), using explicitly-written (i.e.,
- *       programmer-written) annotations from the Non-Empty type system to refine the Optional
- *       analysis. This improves analysis of operations on containers (e.g., Streams, Collections)
- *       that result in values of type Optional.
- *   <li>Checks only explicitly-written {@code @NonEmpty} annotations.
- * </ol>
+ * <p>The Non-Empty Checker's default modality is to only verify (i.e., establish guarantees) for
+ * methods that are passed to it by the Optional Checker. Running the Non-Empty Checker in this
+ * default modality does <i>not</i> guarantee the absence of {@link
+ * java.util.NoSuchElementException} across the <i>entire</i> program.
+ *
+ * <p>If "-AcheckAllDefs" is passed, then the Non-Empty Checker will attempt to verify <i>all</i>
+ * method definitions to prevent {@link java.util.NoSuchElementException}. This modality introduces
+ * a larger number of false positives and errors, and so it is not generally supported for users.
  *
  * @checker_framework.manual #non-empty-checker Non-Empty Checker
  */
-@SupportedOptions("runAsOptionalChecker")
+@SupportedOptions("checkAllDefs")
 public class NonEmptyChecker extends BaseTypeChecker {
 
   /** Creates a NonEmptyChecker. */
@@ -47,6 +50,12 @@ public class NonEmptyChecker extends BaseTypeChecker {
 
   @Override
   public boolean shouldSkipDefs(MethodTree tree) {
+    if (hasOptionNoSubcheckers("checkAllDefs")) {
+      // The "-AcheckAllDefs" flag was passed on the command-line, indicating that the user
+      // wants to verify the entire program, not just the methods collected by the Optional Checker.
+      // Consequently, do not skip any method definitions.
+      return false;
+    }
     return !getMethodsToVerify().contains(tree);
   }
 
