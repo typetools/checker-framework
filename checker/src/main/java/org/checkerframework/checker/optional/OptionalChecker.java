43c3bdadc154e30caa63904da68360721fb14bf2
James Yoo <jyoo980@gmail.com>
2025-01-08 11:07:34 -0800

    Tweaking formatting for the class comment for `Optional Checker`

--- checker/src/main/java/org/checkerframework/checker/optional/OptionalChecker.java
+++ checker/src/main/java/org/checkerframework/checker/optional/OptionalChecker.java
@@ -6,20 +6,19 @@ import org.checkerframework.checker.nonempty.NonEmptyChecker;
 import org.checkerframework.framework.source.AggregateChecker;
 import org.checkerframework.framework.source.SourceChecker;
 
+// The Optional Checker aggregates two checkers, the Non-Empty Checker and the
+// OptionalImplChecker. The Optional Checker behaves as the ultimate parent of those two checkers
+// and has no qualifiers or transfer function class of its own.
+//
+// When the Non-Empty Checker is executed as a subchecker of the Optional Checker, it runs the
+// OptionalImplChecker. It only type-checks the methods that the OptionalImplChecker passes to it.
+//
+// The OptionalImplChecker implements a type system for Optional values (i.e., what users would
+// expect the Optional Checker to implement). It reads programmer-written annotations from the
+// Non-Empty type system, and keeps track of the methods that the Non-Empty Checker should check.
 /**
  * A type-checker that prevents {@link java.util.NoSuchElementException} in the use of the {@link
  * java.util.Optional} class.
- *
- * <p>The Optional Checker aggregates two checkers, the Non-Empty Checker and the
- * OptionalImplChecker. The Optional Checker behaves as the ultimate parent of those two checkers
- * and has no qualifiers or transfer function class of its own.
- *
- * <p>When the Non-Empty Checker is executed as a subchecker of the Optional Checker, it runs the
- * OptionalImplChecker. It only type-checks the methods that the OptionalImplChecker passes to it.
- *
- * <p>The OptionalImplChecker implements a type system for Optional values (i.e., what users would
- * expect the Optional Checker to implement). It reads programmer-written annotations from the
- * Non-Empty type system, and keeps track of the methods that the Non-Empty Checker should check.
  *
  * @checker_framework.manual #optional-checker Optional Checker
  */
