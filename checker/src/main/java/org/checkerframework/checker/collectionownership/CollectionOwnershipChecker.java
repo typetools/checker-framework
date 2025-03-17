6273d652ed71f3433fb879d5c6c144e785e9379c
Sascha Kehrli <skehrli@ethz.ch>
2025-03-17 10:46:06 +0100

    add createsourcevisitor and subcheckers to coc

--- checker/src/main/java/org/checkerframework/checker/collectionownership/CollectionOwnershipChecker.java
+++ checker/src/main/java/org/checkerframework/checker/collectionownership/CollectionOwnershipChecker.java
@@ -1,10 +1,27 @@
 package org.checkerframework.checker.collectionownership;
 
+import java.util.Set;
 import org.checkerframework.common.basetype.BaseTypeChecker;
+import org.checkerframework.common.basetype.BaseTypeVisitor;
+import org.checkerframework.framework.source.SourceChecker;
 
 /**
  * This typechecker tracks at most one owning variable per resource collection using an ownership
- * type system. The resource leak checker verifies that the determined owner fulfills the calling
- * obligations of its elements.
+ * type system. The resource leak checker verifies that (at least) the determined owner fulfills the
+ * calling obligations of its elements.
  */
-public class CollectionOwnershipChecker extends BaseTypeChecker {}
+public class CollectionOwnershipChecker extends BaseTypeChecker {
+
+  // TODO sck: not sure if this is necessary
+  @Override
+  protected BaseTypeVisitor<?> createSourceVisitor() {
+    return new CollectionOwnershipVisitor(this);
+  }
+
+  @Override
+  protected Set<Class<? extends SourceChecker>> getImmediateSubcheckerClasses() {
+    Set<Class<? extends SourceChecker>> checkers = super.getImmediateSubcheckerClasses();
+    // checkers.add(RLCCalledMethodsChecker.class);
+    return checkers;
+  }
+}
