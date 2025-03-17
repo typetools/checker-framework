2bd960f0c73b21741db10aef8e95851c985cf53d
Sascha Kehrli <skehrli@ethz.ch>
2025-03-17 11:07:15 +0100

    add subtypeof({}) to NotOwningCollection

--- checker-qual/src/main/java/org/checkerframework/checker/collectionownership/qual/NotOwningCollection.java
+++ checker-qual/src/main/java/org/checkerframework/checker/collectionownership/qual/NotOwningCollection.java
@@ -4,6 +4,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.checkerframework.framework.qual.SubtypeOf;
 
 /**
  * The top qualifier in the Collection Ownership type hierarchy.
@@ -16,4 +17,5 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+@SubtypeOf({})
 public @interface NotOwningCollection {}
