c533af1bac5a059307476be8465500f6db55e91e
Suzanne Millstein <smillst@cs.washington.edu>
2025-03-26 09:20:22 -0700

    Use Java 21 to build during testing.

--- checker/bin-devel/test-daikon-part1.sh
+++ checker/bin-devel/test-daikon-part1.sh
@@ -7,7 +7,7 @@ export SHELLOPTS
 echo "SHELLOPTS=${SHELLOPTS}"
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export ORG_GRADLE_PROJECT_useJdk17Compiler=true
+export ORG_GRADLE_PROJECT_useJdk21Compiler=true
 source "$SCRIPTDIR"/clone-related.sh
 
 # Run assembleForJavac because it does not build the javadoc, so it is faster than assemble.
