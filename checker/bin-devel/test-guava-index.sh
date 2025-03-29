118075e88a185107bbfe782adf27ee31068f43ef
Suzanne Millstein <smillst@cs.washington.edu>
2025-03-29 09:18:29 -0700

    In CI, build with Java 21
    Co-authored-by: Michael Ernst <mernst@cs.washington.edu>

--- checker/bin-devel/test-guava-index.sh
+++ checker/bin-devel/test-guava-index.sh
@@ -8,7 +8,7 @@ export SHELLOPTS
 echo "SHELLOPTS=${SHELLOPTS}"
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export ORG_GRADLE_PROJECT_useJdk17Compiler=true
+export ORG_GRADLE_PROJECT_useJdk21Compiler=true
 source "$SCRIPTDIR"/clone-related.sh
 
 
