3b788853573b17c25334cee83825e4fcb1a83e87
Michael Ernst <mernst@cs.washington.edu>
2025-05-11 12:50:40 -0700

    Reformat

--- checker/bin-devel/test-cftests-all.sh
+++ checker/bin-devel/test-cftests-all.sh
@@ -10,10 +10,8 @@ export SHELLOPTS
 echo "SHELLOPTS=${SHELLOPTS}"
 
 export ORG_GRADLE_PROJECT_useJdk21Compiler=true
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 source "$SCRIPTDIR"/clone-related.sh
-
-
 
 ./gradlew allTests --console=plain --warning-mode=all
 # Moved example-tests out of all tests because it fails in
--- checker/bin-devel/test-typecheck-part1.sh
+++ checker/bin-devel/test-typecheck-part1.sh
@@ -6,10 +6,9 @@ set -o xtrace
 export SHELLOPTS
 echo "SHELLOPTS=${SHELLOPTS}"
 
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 export ORG_GRADLE_PROJECT_useJdk21Compiler=true
 source "$SCRIPTDIR"/clone-related.sh
-
 
 # Pluggable type-checking:  run the Checker Framework on itself
 ./gradlew typecheck-part1 --console=plain --warning-mode=all
