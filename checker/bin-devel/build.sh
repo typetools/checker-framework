50fc43f7a1a2fa4a79011cef202c923aaaa7b25b
Michael Ernst <mernst@cs.washington.edu>
2025-05-19 08:52:31 -0700

    Javadoc fixes

--- checker/bin-devel/build.sh
+++ checker/bin-devel/build.sh
@@ -11,8 +11,8 @@ source "$SCRIPTDIR"/clone-related.sh
 # Download dependencies, trying a second time if there is a failure.
 # echo "NO_WRITE_VERIFICATION_METADATA=$NO_WRITE_VERIFICATION_METADATA"
 if [ -z "${NO_WRITE_VERIFICATION_METADATA+x}" ]; then
-  (TERM=dumb timeout 300 ./gradlew --write-verification-metadata sha256 help --dry-run \
-    || (sleep 1m && TERM=dumb timeout 300 ./gradlew --write-verification-metadata sha256 help --dry-run))
+  (date && TERM=dumb timeout 300 ./gradlew --write-verification-metadata sha256 help --dry-run) \
+    || (sleep 1m && date && TERM=dumb timeout 300 ./gradlew --write-verification-metadata sha256 help --dry-run)
 fi
 echo "running \"./gradlew assemble\" for checker-framework"
 ./gradlew assemble --console=plain -Dorg.gradle.internal.http.socketTimeout=60000 -Dorg.gradle.internal.http.connectionTimeout=60000
