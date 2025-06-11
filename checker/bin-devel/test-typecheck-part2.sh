2dced6aa5e06d4bb0c2a3c17c23ed844d4c3f122
Michael Ernst <mernst@cs.washington.edu>
2025-06-11 07:40:52 -0700

    Don't require Java 21 to be installed

--- checker/bin-devel/test-typecheck-part2.sh
+++ checker/bin-devel/test-typecheck-part2.sh
@@ -7,7 +7,7 @@ export SHELLOPTS
 echo "SHELLOPTS=${SHELLOPTS}"
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
-export ORG_GRADLE_PROJECT_useJdk21Compiler=true
+# export ORG_GRADLE_PROJECT_useJdk21Compiler=true
 source "$SCRIPT_DIR"/clone-related.sh
 
 # Pluggable type-checking:  run the Checker Framework on itself
