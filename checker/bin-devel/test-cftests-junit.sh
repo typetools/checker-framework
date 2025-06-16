b61ca78377019d45eed2911c2b63a4109cabbdb7
Suzanne Millstein <smillst@cs.washington.edu>
2025-06-16 11:04:50 -0700

    Test.

--- checker/bin-devel/test-cftests-junit.sh
+++ checker/bin-devel/test-cftests-junit.sh
@@ -9,6 +9,12 @@ echo "SHELLOPTS=${SHELLOPTS}"
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 # Test that the CF, when built with JDK 21, works on other JDKs.
 export ORG_GRADLE_PROJECT_useJdk21Compiler=true
+mkdir ~/.gradle && echo "org.gradle.java.home=$JAVA_21_HOME" >> ~/.gradle/gradle.properties
+
 source "$SCRIPT_DIR"/clone-related.sh
 
+./gradlew printJavaVersion
+
 ./gradlew test --console=plain --warning-mode=all
+
+javap -v checker/tests/build/testclasses/AnoymousAndInnerClass.class | grep major
