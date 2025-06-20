2516ad9dfeaa4cf8535e6408b7954435191a1f46
Suzanne Millstein <smillst@cs.washington.edu>
2025-06-20 09:11:22 -0700

    Comment.

--- checker/bin-devel/test-cftests-junit.sh
+++ checker/bin-devel/test-cftests-junit.sh
@@ -8,6 +8,7 @@ echo "SHELLOPTS=${SHELLOPTS}"
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 # Test that the CF, when built with JDK 21, works on other JDKs.
+# This lines makes it so that Gradle is run using Java 21.
 mkdir ~/.gradle && echo "org.gradle.java.home=/usr/lib/jvm/java-21-openjdk-amd64" >> ~/.gradle/gradle.properties
 
 source "$SCRIPT_DIR"/clone-related.sh
