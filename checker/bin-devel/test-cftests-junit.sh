ae283830c4d1275292d5d253ffdbb8ee4679a6f4
Suzanne Millstein <smillst@cs.washington.edu>
2025-06-20 11:15:07 -0700

    Revert changes.

--- checker/bin-devel/test-cftests-junit.sh
+++ checker/bin-devel/test-cftests-junit.sh
@@ -9,16 +9,6 @@ echo "SHELLOPTS=${SHELLOPTS}"
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 # Test that the CF, when built with JDK 21, works on other JDKs.
 export ORG_GRADLE_PROJECT_useJdk21Compiler=true
-
-# This lines makes it so that Gradle is run using Java 21.
-mkdir ~/.gradle && echo "org.gradle.java.home=/usr/lib/jvm/java-21-openjdk-amd64" >> ~/.gradle/gradle.properties
-
 source "$SCRIPT_DIR"/clone-related.sh
 
-# Print the version of Java used to run Gradle. (This is for debugging.)
-./gradlew printJavaVersion
-
 ./gradlew test --console=plain --warning-mode=all
-
-# Print the version of Java used to test the Checker Framework. (This is for debugging.)
-javap -v checker/tests/build/testclasses/AnoymousAndInnerClass.class | grep major
