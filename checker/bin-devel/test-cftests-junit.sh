fdc19870c745348104664c62d8492e04babd9edb
Suzanne Millstein <smillst@cs.washington.edu>
2025-06-20 08:39:38 -0700

    Add comments.

--- checker/bin-devel/test-cftests-junit.sh
+++ checker/bin-devel/test-cftests-junit.sh
@@ -12,8 +12,10 @@ mkdir ~/.gradle && echo "org.gradle.java.home=/usr/lib/jvm/java-21-openjdk-amd64
 
 source "$SCRIPT_DIR"/clone-related.sh
 
+# Print the version of Java used to run Gradle. (This is for debugging.)
 ./gradlew printJavaVersion
 
 ./gradlew test --console=plain --warning-mode=all
 
+# Print the version of Java used to test the Checker Framework. (This is for debugging.)
 javap -v checker/tests/build/testclasses/AnoymousAndInnerClass.class | grep major
