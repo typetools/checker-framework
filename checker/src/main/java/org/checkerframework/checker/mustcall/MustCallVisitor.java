c1232cbde5bf166f793a4273a3b3c45579070487
Sascha Kehrli <skehrli@ethz.ch>
2025-06-22 22:12:38 +0200

    pattern match fulfilling for-loops on AST

--- checker/src/main/java/org/checkerframework/checker/mustcall/MustCallVisitor.java
+++ checker/src/main/java/org/checkerframework/checker/mustcall/MustCallVisitor.java
@@ -1,20 +1,39 @@
 package org.checkerframework.checker.mustcall;
 
 import com.sun.source.tree.AnnotationTree;
+import com.sun.source.tree.ArrayAccessTree;
 import com.sun.source.tree.AssignmentTree;
+import com.sun.source.tree.BinaryTree;
+import com.sun.source.tree.BlockTree;
+import com.sun.source.tree.BreakTree;
 import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.CompoundAssignmentTree;
+import com.sun.source.tree.ExpressionStatementTree;
 import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.ForLoopTree;
+import com.sun.source.tree.IdentifierTree;
+import com.sun.source.tree.LiteralTree;
+import com.sun.source.tree.MemberSelectTree;
 import com.sun.source.tree.MethodInvocationTree;
 import com.sun.source.tree.MethodTree;
 import com.sun.source.tree.ReturnTree;
+import com.sun.source.tree.StatementTree;
 import com.sun.source.tree.Tree;
+import com.sun.source.tree.UnaryTree;
+import com.sun.source.tree.VariableTree;
+import com.sun.source.util.TreeScanner;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Name;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.TypeMirror;
 import org.checkerframework.checker.mustcall.qual.InheritableMustCall;
@@ -23,8 +42,15 @@ import org.checkerframework.checker.mustcall.qual.MustCallAlias;
 import org.checkerframework.checker.mustcall.qual.NotOwning;
 import org.checkerframework.checker.mustcall.qual.Owning;
 import org.checkerframework.checker.mustcall.qual.PolyMustCall;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.checkerframework.checker.resourceleak.ResourceLeakUtils;
+import org.checkerframework.checker.rlccalledmethods.RLCCalledMethodsAnnotatedTypeFactory;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.common.basetype.BaseTypeVisitor;
+import org.checkerframework.dataflow.cfg.block.Block;
+import org.checkerframework.dataflow.cfg.block.ConditionalBlock;
+import org.checkerframework.dataflow.cfg.block.SingleSuccessorBlock;
+import org.checkerframework.dataflow.cfg.node.Node;
 import org.checkerframework.framework.type.AnnotatedTypeMirror;
 import org.checkerframework.framework.type.AnnotatedTypeMirror.AnnotatedDeclaredType;
 import org.checkerframework.framework.type.AnnotatedTypeMirror.AnnotatedExecutableType;
@@ -327,5 +353,371 @@ public class MustCallVisitor extends BaseTypeVisitor<MustCallAnnotatedTypeFactor
   @Override
   public Void visitAnnotation(AnnotationTree tree, Void p) {
     return null;
+  }
+
+  /*
+   * SECTION: syntactically match for-loops that iterate over all elements of a collection on
+   * the AST
+   */
+
+  /** Stores the size variable of the most recent array allocation per array name. */
+  private final Map<Name, Name> arrayInitializationSize = new HashMap<>();
+
+  /**
+   * Checks through pattern-matching whether the loop either:
+   *
+   * <ul>
+   *   <li>initializes entries of an {@code @OwningCollection}
+   *   <li>calls a method on entries of an {@code @OwningCollection} array
+   * </ul>
+   *
+   * If yes, this is marked in some static datastructures in the
+   * {@code @MustCallOnElementsAnnotatedTypeFactory}
+   */
+  @Override
+  public Void visitForLoop(ForLoopTree tree, Void p) {
+    boolean singleLoopVariable = tree.getUpdate().size() == 1 && tree.getInitializer().size() == 1;
+    if (singleLoopVariable) {
+      patternMatchFulfillingLoop(tree);
+    }
+    return super.visitForLoop(tree, p);
+  }
+
+  /**
+   * Checks whether a for-loop potentially fulfills collection obligations of a collection/array and
+   * marks the loop in case the check is successful.
+   *
+   * @param tree forlooptree
+   */
+  private void patternMatchFulfillingLoop(ForLoopTree tree) {
+    List<? extends StatementTree> loopBodyStatementList;
+    if (tree.getStatement() instanceof BlockTree) {
+      BlockTree blockT = (BlockTree) tree.getStatement();
+      loopBodyStatementList = blockT.getStatements();
+    } else {
+      loopBodyStatementList = Collections.singletonList(tree.getStatement());
+    }
+    StatementTree init = tree.getInitializer().get(0);
+    ExpressionTree condition = tree.getCondition();
+    ExpressionStatementTree update = tree.getUpdate().get(0);
+    Name identifierInHeader = verifyAllElementsAreCalledOn(init, (BinaryTree) condition, update);
+    Name iterator = ((VariableTree) init).getName();
+    if (identifierInHeader == null || iterator == null) {
+      return;
+    }
+    ExpressionTree collectionElementTree =
+        preMatchLoop(loopBodyStatementList, identifierInHeader, iterator);
+    if (collectionElementTree != null) {
+      // pattern match succeeded, now mark the loop in the respective datastructures
+      Set<Node> conditionNodes = atypeFactory.getNodesForTree(condition);
+      Set<Node> collectionEltNodes = atypeFactory.getNodesForTree(collectionElementTree);
+      Set<Node> updateNodes = atypeFactory.getNodesForTree(update.getExpression());
+      Block loopConditionBlock = null;
+      for (Node node : conditionNodes) {
+        Block blockOfNode = node.getBlock();
+        if (blockOfNode != null) {
+          loopConditionBlock = blockOfNode;
+          break;
+        }
+      }
+      Block loopUpdateBlock = null;
+      for (Node node : updateNodes) {
+        Block blockOfNode = node.getBlock();
+        if (blockOfNode != null) {
+          loopUpdateBlock = blockOfNode;
+          break;
+        }
+      }
+      Node nodeForCollectionElt = null;
+      if (collectionEltNodes != null) {
+        nodeForCollectionElt = collectionEltNodes.iterator().next();
+      }
+      if (loopUpdateBlock == null || loopConditionBlock == null) return;
+      // add the blocks into a static datastructure in the calledmethodsatf, such that it can
+      // analyze
+      // them (call MustCallConsistencyAnalyzer.analyzeFulfillingLoops, which in turn adds the trees
+      // to the static datastructure in McoeAtf)
+      assert (loopConditionBlock instanceof SingleSuccessorBlock);
+      Block conditionalBlock = ((SingleSuccessorBlock) loopConditionBlock).getSuccessor();
+      assert (conditionalBlock instanceof ConditionalBlock);
+      Block loopBodyEntryBlock = ((ConditionalBlock) conditionalBlock).getThenSuccessor();
+      RLCCalledMethodsAnnotatedTypeFactory.addPotentiallyFulfillingLoop(
+          collectionTreeFromExpression(collectionElementTree),
+          collectionElementTree,
+          tree.getCondition(),
+          loopBodyEntryBlock,
+          loopUpdateBlock,
+          (ConditionalBlock) conditionalBlock,
+          nodeForCollectionElt);
+    }
+  }
+
+  /**
+   * Decides for a for-loop header whether the loop iterates over all elements of some array based
+   * on a pattern-match with one-sided error with the following rules:
+   *
+   * <ul>
+   *   <li>only one loop variable
+   *   <li>initialization must be of the form i = 0
+   *   <li>condition must be of the form (i &lt; arr.length) or (i &lt; n), where n and arr are
+   *       identifiers and n is effectively final
+   *   <li>update must be prefix or postfix
+   * </ul>
+   *
+   * Returns:
+   *
+   * <ul>
+   *   <li>null if any rule is violated
+   *   <li>the name of the array if the loop condition is of the form (i &lt; arr.length)
+   *   <li>n if it is of the form (i &lt; n), where n is an identifier.
+   * </ul>
+   *
+   * @param init the initializer of the loop
+   * @param condition the loop condition
+   * @param update the loop update
+   * @return null if any rule is violated, or the name of the array if the loop condition is of the
+   *     form (i &lt; arr.length) or n if it is of the form (i &lt; n), where n is an identifier.
+   */
+  protected Name verifyAllElementsAreCalledOn(
+      StatementTree init, BinaryTree condition, ExpressionStatementTree update) {
+    Tree.Kind updateKind = update.getExpression().getKind();
+    if (updateKind == Tree.Kind.PREFIX_INCREMENT || updateKind == Tree.Kind.POSTFIX_INCREMENT) {
+      UnaryTree inc = (UnaryTree) update.getExpression();
+      // verify update is of form i++ or ++i and init is variable initializer
+      if (!(init instanceof VariableTree) || !(inc.getExpression() instanceof IdentifierTree))
+        return null;
+      VariableTree initVar = (VariableTree) init;
+      // verify that intializer is i=0
+      if (!(initVar.getInitializer() instanceof LiteralTree)
+          || !((LiteralTree) initVar.getInitializer()).getValue().equals(0)) {
+        return null;
+      }
+      // verify that condition is of the form: i < something
+      if (!(condition.getLeftOperand() instanceof IdentifierTree)) return null;
+      if (initVar.getName()
+              != ((IdentifierTree) condition.getLeftOperand()).getName() // i=0 and i<n are same "i"
+          || initVar.getName()
+              != ((IdentifierTree) inc.getExpression()).getName()) { // i=0 and i++ are same "i"
+        return null;
+      }
+      if (TreeUtils.isArrayLengthAccess(condition.getRightOperand())) {
+        return nameFromExpression(condition.getRightOperand());
+      } else if ((condition.getRightOperand() instanceof MethodInvocationTree)
+          && TreeUtils.isSizeAccess(condition.getRightOperand())) {
+        ExpressionTree methodSelect =
+            ((MethodInvocationTree) condition.getRightOperand()).getMethodSelect();
+        assert methodSelect.getKind() == Tree.Kind.MEMBER_SELECT
+            : "method selection of object.size() expected to be memberSelectTree, but is "
+                + methodSelect.getKind();
+        MemberSelectTree mst = (MemberSelectTree) methodSelect;
+        Element elt = TreeUtils.elementFromTree(mst.getExpression());
+        if (ResourceLeakUtils.isCollection(elt, atypeFactory)) {
+          return nameFromExpression(mst.getExpression());
+        }
+      } else if (condition.getRightOperand().getKind() == Tree.Kind.IDENTIFIER) {
+        return nameFromExpression(condition.getRightOperand());
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Check that the loop does not contain any writes to the loop iterator variable, or to the
+   * collection variable itself. return/break statements. Extract the collection access tree ({@code
+   * arr[i]} or {@code collection.get(i)} where {@code i} is the iterator variable and {@code
+   * collection/arr} is consistent with the loop header) and return the last encountered such tree.
+   *
+   * @param statements list of statements of the loop body
+   * @param identifierInHeader collection name if loop condition is {@code i < collection.size()} or
+   *     {@code i < arr.length} and {@code n} if loop condition is {@code i < n}
+   * @param iterator the name of the loop iterator variable
+   * @return null if any writes to loop iterator variable or return/break statements are in {@code
+   *     block}. Else, return the last encountered collection access tree consistent with the loop
+   *     heaer if it exists and else null.
+   */
+  private @Nullable ExpressionTree preMatchLoop(
+      List<? extends StatementTree> statements, Name identifierInHeader, Name iterator) {
+    AtomicBoolean blockIsIllegal = new AtomicBoolean(false);
+    final ExpressionTree[] collectionElementTree = {null};
+
+    TreeScanner<Void, Void> scanner =
+        new TreeScanner<Void, Void>() {
+          @Override
+          public Void visitUnary(UnaryTree tree, Void p) {
+            switch (tree.getKind()) {
+              case PREFIX_DECREMENT:
+              case POSTFIX_DECREMENT:
+              case PREFIX_INCREMENT:
+              case POSTFIX_INCREMENT:
+                if (nameFromExpression(tree.getExpression()) == iterator) {
+                  blockIsIllegal.set(true);
+                }
+                break;
+              default:
+                break;
+            }
+            return super.visitUnary(tree, p);
+          }
+
+          @Override
+          public Void visitCompoundAssignment(CompoundAssignmentTree tree, Void p) {
+            if (nameFromExpression(tree.getVariable()) == iterator) {
+              blockIsIllegal.set(true);
+            }
+            return super.visitCompoundAssignment(tree, p);
+          }
+
+          @Override
+          public Void visitAssignment(AssignmentTree tree, Void p) {
+            Name assignedVariable = nameFromExpression(tree.getVariable());
+            if (assignedVariable == iterator || assignedVariable == identifierInHeader) {
+              blockIsIllegal.set(true);
+            }
+
+            return super.visitAssignment(tree, p);
+          }
+
+          @Override
+          public Void visitBreak(BreakTree bt, Void p) {
+            blockIsIllegal.set(true);
+            return super.visitBreak(bt, p);
+          }
+
+          @Override
+          public Void visitReturn(ReturnTree rt, Void p) {
+            blockIsIllegal.set(true);
+            return super.visitReturn(rt, p);
+          }
+
+          // check whether corresponds to collection.get(i)
+          @Override
+          public Void visitMethodInvocation(MethodInvocationTree mit, Void p) {
+            if (isIthCollectionElement(mit, iterator)
+                && loopHeaderConsistentWithCollection(
+                    identifierInHeader, nameFromExpression(mit))) {
+              collectionElementTree[0] = mit;
+            }
+            return super.visitMethodInvocation(mit, p);
+          }
+
+          // check whether corresponds to arr[i]
+          @Override
+          public Void visitArrayAccess(ArrayAccessTree aat, Void p) {
+            boolean isIthArrayElement = nameFromExpression(aat.getIndex()) == iterator;
+            if (isIthArrayElement
+                && loopHeaderConsistentWithCollection(
+                    identifierInHeader, nameFromExpression(aat))) {
+              collectionElementTree[0] = aat;
+            }
+            return super.visitArrayAccess(aat, p);
+          }
+        };
+
+    for (StatementTree stmt : statements) {
+      scanner.scan(stmt, null);
+    }
+    if (!blockIsIllegal.get() && collectionElementTree[0] != null) {
+      return collectionElementTree[0];
+    }
+    return null;
+  }
+
+  /**
+   * Get name from an ExpressionTree
+   *
+   * @param expr ExpressionTree
+   * @return Name of the identifier the expression evaluates to or null if it doesn't
+   */
+  protected Name nameFromExpression(ExpressionTree expr) {
+    if (expr == null) return null;
+    switch (expr.getKind()) {
+      case IDENTIFIER:
+        return ((IdentifierTree) expr).getName();
+      case ARRAY_ACCESS:
+        return nameFromExpression(((ArrayAccessTree) expr).getExpression());
+      case MEMBER_SELECT:
+        Element elt = TreeUtils.elementFromUse((MemberSelectTree) expr);
+        if (elt.getKind() == ElementKind.METHOD || elt.getKind() == ElementKind.FIELD) {
+          return nameFromExpression(((MemberSelectTree) expr).getExpression());
+        } else {
+          return null;
+        }
+      case METHOD_INVOCATION:
+        return nameFromExpression(((MethodInvocationTree) expr).getMethodSelect());
+      default:
+        return null;
+    }
+  }
+
+  /**
+   * Returns the ExpressionTree of the collection/array in the given expression
+   *
+   * @param expr ExpressionTree
+   * @return the expression evaluates to or null if it doesn't
+   */
+  protected ExpressionTree collectionTreeFromExpression(ExpressionTree expr) {
+    switch (expr.getKind()) {
+      case IDENTIFIER:
+        return expr;
+      case ARRAY_ACCESS:
+        return ((ArrayAccessTree) expr).getExpression();
+      case MEMBER_SELECT:
+        Element elt = TreeUtils.elementFromUse((MemberSelectTree) expr);
+        if (elt.getKind() == ElementKind.METHOD) {
+          return ((MemberSelectTree) expr).getExpression();
+        } else {
+          return null;
+        }
+      case METHOD_INVOCATION:
+        return collectionTreeFromExpression(((MethodInvocationTree) expr).getMethodSelect());
+      default:
+        return null;
+    }
+  }
+
+  /**
+   * Returns whether the given collection name is consistent with the identifier from the loop
+   * header.
+   *
+   * <p>That is, either the names are equal, or the identifier from the header is the same variable
+   * used to initialize the given collection.
+   *
+   * <p>Returns false if any argument is null.
+   *
+   * @param idInHeader identifier from loop header
+   * @param collectionName name of collection
+   * @return whether the given collection name is consistent with the identifier from the loop
+   *     header.
+   */
+  private boolean loopHeaderConsistentWithCollection(Name idInHeader, Name collectionName) {
+    if (idInHeader == null || collectionName == null) return false;
+    boolean namesAreEqual = collectionName == idInHeader;
+    Name initSize = arrayInitializationSize.get(collectionName);
+    boolean idInHeaderIsSizeOfCollection = initSize != null && initSize == idInHeader;
+    return namesAreEqual || idInHeaderIsSizeOfCollection;
+  }
+
+  /**
+   * Returns whether the given tree is of the form collection.get(i), where i is the given index
+   * name.
+   *
+   * @param tree the tree to check
+   * @param index the index variable name
+   * @return whether the given tree is of the form collection.get(index)
+   */
+  private boolean isIthCollectionElement(Tree tree, Name index) {
+    if (tree == null || index == null) return false;
+    if (tree.getKind() == Tree.Kind.METHOD_INVOCATION
+        && index == nameFromExpression(TreeUtils.getIdxForGetCall(tree))) {
+      MethodInvocationTree mit = (MethodInvocationTree) tree;
+      ExpressionTree methodSelect = mit.getMethodSelect();
+      assert methodSelect.getKind() == Tree.Kind.MEMBER_SELECT
+          : "method selection of object.get() expected to be memberSelectTree, but is "
+              + methodSelect.getKind();
+      MemberSelectTree mst = (MemberSelectTree) methodSelect;
+      Element receiverElt = TreeUtils.elementFromTree(mst.getExpression());
+      return ResourceLeakUtils.isCollection(receiverElt, atypeFactory);
+    }
+    return false;
   }
 }
