07c7652cbceb4de5dbdc6c7caa4cf2aa4d6e7271
Suzanne Millstein <smillst@cs.washington.edu>
2025-06-25 14:59:04 -0700

    Use JDK 21 to run Gradle.

--- checker/bin-devel/test-cftests-all.sh
+++ checker/bin-devel/test-cftests-all.sh
@@ -12,6 +12,10 @@ echo "SHELLOPTS=${SHELLOPTS}"
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 # Test that the CF, when built with JDK 21, works on other JDKs.
 export ORG_GRADLE_PROJECT_useJdk21Compiler=true
+
+# This lines makes it so that Gradle is run using Java 21.
+mkdir ~/.gradle && echo "org.gradle.java.home=/usr/lib/jvm/java-21-openjdk-amd64" >> ~/.gradle/gradle.properties
+
 source "$SCRIPT_DIR"/clone-related.sh
 
 ./gradlew allTests --console=plain --warning-mode=all
