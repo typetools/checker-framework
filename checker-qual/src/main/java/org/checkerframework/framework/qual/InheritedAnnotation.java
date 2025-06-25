969ccbea511193e03a5a72f7d17f2fbc548c24e7
Suzanne Millstein <smillst@cs.washington.edu>
2025-06-25 09:47:24 -0700

    Add comment about java.lang.annotation.Inherited.

--- checker-qual/src/main/java/org/checkerframework/framework/qual/InheritedAnnotation.java
+++ checker-qual/src/main/java/org/checkerframework/framework/qual/InheritedAnnotation.java
@@ -9,6 +9,10 @@ import java.lang.annotation.Target;
 /**
  * A meta-annotation that specifies if a declaration annotation should be inherited. This should not
  * be written on type annotations.
+ *
+ * <p>{@link java.lang.annotation.Inherited} applies only to declaration annotations on a class.
+ * {@code InheritedAnnotation} applys to both classes and methods. (The Checker Framework also
+ * respects {@code Inherited}.
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
