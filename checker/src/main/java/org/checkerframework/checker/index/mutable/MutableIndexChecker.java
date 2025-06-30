54b1635d21c07d8f6d1566318ed43328008744cd
Damika-Anupama <damikaanupama@gmail.com>
2025-06-30 20:43:17 +0530

    Update MutableIndexChecker.java

--- checker/src/main/java/org/checkerframework/checker/index/mutable/MutableIndexChecker.java
+++ checker/src/main/java/org/checkerframework/checker/index/mutable/MutableIndexChecker.java
@@ -1,7 +1,7 @@
 package org.checkerframework.checker.index.mutable;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.framework.qual.RelevantJavaTypes;
 import org.checkerframework.framework.source.SuppressWarningsPrefix;
@@ -16,13 +16,11 @@ import org.checkerframework.framework.source.SuppressWarningsPrefix;
  *
  * @checker_framework.manual #index-checker-mutable-length Support for mutable-length sequences
  */
-@RelevantJavaTypes({
-  List.class,
-  ArrayList.class
-  /**  LinkedList.class, Vector.class, Stack.class, AbstractList.class, CopyOnWriteArrayList.class */
+@RelevantJavaTypes({List.class, ArrayList.class
+  /** LinkedList.class, Vector.class, Stack.class, AbstractList.class, CopyOnWriteArrayList.class */
 })
 @SuppressWarningsPrefix({"index", "mutable"})
 public class MutableIndexChecker extends BaseTypeChecker {
-    /* Creates a new MutableIndexChecker. */
-    public MutableIndexChecker() {}
+  /* Creates a new MutableIndexChecker. */
+  public MutableIndexChecker() {}
 }
