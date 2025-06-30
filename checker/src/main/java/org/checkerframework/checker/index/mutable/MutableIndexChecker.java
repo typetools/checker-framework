862da1e0e4713e4f2a56382d7fa8d185a2b3435c
Damika-Anupama <damikaanupama@gmail.com>
2025-06-30 20:21:35 +0530

    Add MutableIndexChecker.java

---
+++ checker/src/main/java/org/checkerframework/checker/index/mutable/MutableIndexChecker.java
@@ -0,0 +1,28 @@
+package org.checkerframework.checker.index.mutable;
+
+import java.util.List;
+import java.util.ArrayList;
+import org.checkerframework.common.basetype.BaseTypeChecker;
+import org.checkerframework.framework.qual.RelevantJavaTypes;
+import org.checkerframework.framework.source.SuppressWarningsPrefix;
+
+/**
+ * A type-checker that enforces rules about mutable-length sequences.
+ *
+ * <p>This checker ensures that references annotated as {@code @GrowOnly} are not used to call
+ * methods that can shrink the sequence (e.g., {@code remove()}, {@code clear()}). This allows other
+ * checkers, like the Upper Bound Checker, to reason that indices for {@code @GrowOnly} collections
+ * remain valid after mutations.
+ *
+ * @checker_framework.manual #index-checker-mutable-length Support for mutable-length sequences
+ */
+@RelevantJavaTypes({
+  List.class,
+  ArrayList.class
+  /**  LinkedList.class, Vector.class, Stack.class, AbstractList.class, CopyOnWriteArrayList.class */
+})
+@SuppressWarningsPrefix({"index", "mutable"})
+public class MutableIndexChecker extends BaseTypeChecker {
+    /* Creates a new MutableIndexChecker. */
+    public MutableIndexChecker() {}
+}
