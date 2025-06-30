b8c5e41f0dd8af6c0e04af79fa5de2f3a02875be
Damika-Anupama <damikaanupama@gmail.com>
2025-06-30 20:44:28 +0530

    Add MutableIndexAnnotatedTypeFactory.java

---
+++ checker/src/main/java/org/checkerframework/checker/index/mutable/MutableIndexAnnotatedTypeFactory.java
@@ -0,0 +1,43 @@
+package org.checkerframework.checker.index.mutable;
+
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import javax.lang.model.element.AnnotationMirror;
+import org.checkerframework.checker.index.qual.BottomGrowShrink;
+import org.checkerframework.checker.index.qual.GrowOnly;
+import org.checkerframework.checker.index.qual.Shrinkable;
+import org.checkerframework.checker.index.qual.UncheckedShrinkable;
+import org.checkerframework.checker.index.qual.UnshrinkableRef;
+import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
+import org.checkerframework.common.basetype.BaseTypeChecker;
+import org.checkerframework.javacutil.AnnotationBuilder;
+
+public class MutableIndexAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
+
+  /** The canonical @{@link GrowOnly} annotation. */
+  public final AnnotationMirror GROW_ONLY;
+
+  /**
+   * @param checker the type-checker
+   */
+  public MutableIndexAnnotatedTypeFactory(BaseTypeChecker checker) {
+    super(checker);
+
+    GROW_ONLY = AnnotationBuilder.fromClass(elements, GrowOnly.class);
+
+    this.postInit();
+  }
+
+  @Override
+  protected Set<Class<? extends Annotation>> createSupportedTypeQualifiers() {
+    return new LinkedHashSet<>(
+        Arrays.asList(
+            UnshrinkableRef.class,
+            GrowOnly.class,
+            Shrinkable.class,
+            UncheckedShrinkable.class,
+            BottomGrowShrink.class));
+  }
+}
