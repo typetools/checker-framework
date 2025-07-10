ff3aafcfef01a6d1d3590dbf70f82f1d6f3eac77
Michael Ernst <mernst@cs.washington.edu>
2025-07-09 21:19:57 -0700

    Formatting

--- checker/bin-devel/build.sh
+++ checker/bin-devel/build.sh
@@ -11,7 +11,7 @@ source "$SCRIPT_DIR"/clone-related.sh
 # Download Gradle and dependencies, retrying in case of network problems.
 # echo "NO_WRITE_VERIFICATION_METADATA=$NO_WRITE_VERIFICATION_METADATA"
 if [ -z "${NO_WRITE_VERIFICATION_METADATA+x}" ]; then
-  (date && TERM=dumb timeout 300 ./gradlew --write-verification-metadata sha256 help --dry-run </dev/null >/dev/null 2>&1) \
+  (date && TERM=dumb timeout 300 ./gradlew --write-verification-metadata sha256 help --dry-run < /dev/null > /dev/null 2>&1) \
     || (sleep 1m && date && TERM=dumb timeout 300 ./gradlew --write-verification-metadata sha256 help --dry-run)
 fi
 echo "running \"./gradlew assemble\" for checker-framework"
