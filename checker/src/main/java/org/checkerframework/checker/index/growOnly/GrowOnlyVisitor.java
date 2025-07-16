49ecb87fee68e837f47fa90affed23990fe430cc
Damika-Anupama <damikaanupama@gmail.com>
2025-07-16 09:46:44 +0530

    Update GrowOnlyVisitor

--- checker/src/main/java/org/checkerframework/checker/index/growOnly/GrowOnlyVisitor.java
+++ checker/src/main/java/org/checkerframework/checker/index/growOnly/GrowOnlyVisitor.java
@@ -2,6 +2,8 @@ package org.checkerframework.checker.index.growOnly;
 
 import com.sun.source.tree.MethodInvocationTree;
 import javax.lang.model.element.ExecutableElement;
+import org.checkerframework.checker.index.qual.GrowOnly;
+import org.checkerframework.checker.index.qual.Shrinkable;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.common.basetype.BaseTypeVisitor;
 import org.checkerframework.framework.type.AnnotatedTypeMirror;
@@ -13,9 +15,6 @@ import org.checkerframework.javacutil.TreeUtils;
  *
  * <p>Issues an error if a method that may shrink a collection (like a method annotated as
  * {@code @Shrinkable}) is called on a reference that is annotated as {@code @GrowOnly}.
- *
- * <p>It also handles the subtyping checks for assignments and overrides automatically, thanks to
- * inheriting from {@link BaseTypeVisitor}.
  */
 public class GrowOnlyVisitor extends BaseTypeVisitor<GrowOnlyAnnotatedTypeFactory> {
 
@@ -39,24 +38,31 @@ public class GrowOnlyVisitor extends BaseTypeVisitor<GrowOnlyAnnotatedTypeFactor
   public Void visitMethodInvocation(MethodInvocationTree node, Void p) {
     // Get the receiver of the method call (e.g., `myList` in `myList.remove(0)`).
     AnnotatedTypeMirror receiverType = atypeFactory.getReceiverType(node);
-    if (receiverType == null) {
-      return super.visitMethodInvocation(node, p);
+
+    // only care about method calls on receivers that are explicitly @GrowOnly.
+    if (receiverType != null && receiverType.hasPrimaryAnnotation(GrowOnly.class)) {
+
+      // Get the element for the method being called. Checks
+      // its annotations from the source or stub files.
+      ExecutableElement methodElement = TreeUtils.elementFromUse(node);
+      if (methodElement == null) {
+        return super.visitMethodInvocation(node, p);
+      }
+
+      // Get the full annotated signature of the method declaration.
+      AnnotatedExecutableType methodDeclarationType = atypeFactory.getAnnotatedType(methodElement);
+
+      // Get the receiver type that the method *declares* it requires.
+      // For `remove()`, this will be @Shrinkable because of our jdk.astub file.
+      AnnotatedTypeMirror requiredReceiverType = methodDeclarationType.getReceiverType();
+
+      // The core check: If the method requires a @Shrinkable receiver,
+      // then it's an error because we already know our receiver is @GrowOnly.
+      if (requiredReceiverType.hasPrimaryAnnotation(Shrinkable.class)) {
+        String methodName = TreeUtils.getMethodName(node).toString();
+        checker.reportError(node, "mutable.collection.shrink", methodName);
+      }
     }
-
-    // Get the method's signature from the AST.
-    ExecutableElement methodElement = TreeUtils.elementFromUse(node);
-    AnnotatedExecutableType methodType = atypeFactory.getAnnotatedType(methodElement);
-
-    // Get the type the method *requires* for its receiver.
-    // For a method like `remove()`, this will be @Shrinkable because of our jdk.astub file.
-    AnnotatedTypeMirror requiredReceiverType = methodType.getReceiverType();
-
-    // Use the visitor's built-in assignment check to see if the actual receiver
-    // (`receiverType`) is a valid subtype of what the method requires (`requiredReceiverType`).
-    // This will automatically issue an error with our custom message if the check fails.
-    // The first argument is the "variable" (what is required).
-    // The second argument is the "value" (what we have).
-    commonAssignmentCheck(requiredReceiverType, receiverType, node, "mutable.collection.shrink");
 
     return super.visitMethodInvocation(node, p);
   }
