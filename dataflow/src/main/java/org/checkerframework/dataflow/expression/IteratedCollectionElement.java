251cb9478658a4289bb61a39ddc3bf874fdcf08a
Sascha Kehrli <skehrli@ethz.ch>
2025-08-08 12:00:52 +0200

    address code review

--- dataflow/src/main/java/org/checkerframework/dataflow/expression/IteratedCollectionElement.java
+++ dataflow/src/main/java/org/checkerframework/dataflow/expression/IteratedCollectionElement.java
@@ -1,6 +1,7 @@
 package org.checkerframework.dataflow.expression;
 
 import com.sun.source.tree.Tree;
+import java.util.Objects;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.checkerframework.dataflow.cfg.node.Node;
 import org.checkerframework.javacutil.AnnotationProvider;
@@ -37,51 +38,13 @@ public class IteratedCollectionElement extends JavaExpression {
       return false;
     }
     IteratedCollectionElement other = (IteratedCollectionElement) obj;
-    return other.tree.equals(this.tree) || other.node.equals(this.node);
+    return other.tree.equals(this.tree) && other.node.equals(this.node);
   }
-
-  // /**
-  //  * Returns true if the two elements are the same.
-  //  *
-  //  * @param element1 the first element to compare
-  //  * @param element2 the second element to compare
-  //  * @return true if the two elements are the same
-  //  */
-  // protected static boolean sameElement(VariableElement element1, VariableElement element2) {
-  //   VarSymbol vs1 = (VarSymbol) element1;
-  //   VarSymbol vs2 = (VarSymbol) element2;
-  //   // If a LocalVariable is created via JavaExpressionParseUtil#parse, then `vs1.equals(vs2)`
-  //   // will not return true even if the elements represent the same local variable.
-  //   // The owner of a lambda parameter is the enclosing method, so a local variable and a lambda
-  //   // parameter might have the same name and the same owner. Use pos to differentiate this
-  //   // case.
-  //   return vs1.pos == vs2.pos && vs1.name == vs2.name && vs1.owner.equals(vs2.owner);
-  // }
-
-  // /**
-  //  * Returns the element for this variable.
-  //  *
-  //  * @return the element for this variable
-  //  */
-  // public VariableElement getElement() {
-  //   return element;
-  // }
 
   @Override
   public int hashCode() {
-    return node.hashCode();
-    // return Objects.hash(tree.hashCode());
+    return Objects.hash(tree, node);
   }
-
-  // @Override
-  // public String toString() {
-  //   return var.toString();
-  // }
-
-  // @Override
-  // public String toStringDebug() {
-  //   return super.toStringDebug() + " [owner=" + ((VarSymbol) element).owner + "]";
-  // }
 
   @SuppressWarnings("unchecked") // generic cast
   @Override
@@ -116,7 +79,6 @@ public class IteratedCollectionElement extends JavaExpression {
   @Override
   public boolean isModifiableByOtherCode() {
     return false;
-    // return !TypesUtils.isImmutableTypeInJdk(((VarSymbol) element).type);
   }
 
   @Override
