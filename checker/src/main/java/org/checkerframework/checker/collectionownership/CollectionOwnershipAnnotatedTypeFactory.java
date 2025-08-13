73d6163fc4655d20009fb5e2b5219fbddea8c207
Martin Kellogg <martin.kellogg@njit.edu>
2025-08-13 16:26:35 -0400

    explanatory comment

--- checker/src/main/java/org/checkerframework/checker/collectionownership/CollectionOwnershipAnnotatedTypeFactory.java
+++ checker/src/main/java/org/checkerframework/checker/collectionownership/CollectionOwnershipAnnotatedTypeFactory.java
@@ -216,6 +216,10 @@ public class CollectionOwnershipAnnotatedTypeFactory
         : flowResult.getStoreBefore(succBlock);
   }
 
+  // Note that this method is overridden here because the collections ownership
+  // typechecker runs last in the RLC, not because this has anything to do with
+  // collections. Whatever checker runs last in the RLC must do this. TODO: make this
+  // run last in a more sensible way.
   @Override
   public void postAnalyze(ControlFlowGraph cfg) {
     ResourceLeakChecker rlc = ResourceLeakUtils.getResourceLeakChecker(this);
