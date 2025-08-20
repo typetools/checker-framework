64058bc36dac93b316e50da0a14d09defac76991
Damika-Anupama <damikaanupama@gmail.com>
2025-08-20 14:48:51 -0500

    Activate ListSet.java

--- checker/tests/index/ListSet.java
+++ checker/tests/index/ListSet.java
@@ -1,8 +1,7 @@
+import java.util.List;
 import org.checkerframework.checker.index.qual.IndexFor;
 import org.checkerframework.checker.index.qual.LTEqLengthOf;
 import org.checkerframework.checker.index.qual.LTLengthOf;
-
-// @skip-test until we bring list support back
 
 public class ListSet {
 
