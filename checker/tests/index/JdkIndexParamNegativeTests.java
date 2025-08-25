84b2f5d70b58fb2bcc0813d5482e333fe287bf9e
Damika-Anupama <damikaanupama@gmail.com>
2025-08-25 15:44:23 -0500

    Add new tests and organize all tests into two test files

--- checker/tests/index/JdkIndexParamNegativeTests.java
+++ checker/tests/index/JdkIndexParamNegativeTests.java
@@ -1,17 +1,11 @@
 import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-public class JdkIndexParamAnnotations {
-
-  // void get_needs_upper(List<String> xs, int i) {
-  //   if (0 <= i) {
-  //     // :: error: (argument)
-  //     xs.get(i);
-  //   }
-  // }
+public class JdkIndexParamNegativeTests {
 
   void get_needs_upper(List<String> xs, int i) {
     if (0 <= i) {
+      // :: error: (argument)
       xs.get(i);
     }
   }
@@ -51,21 +45,11 @@ public class JdkIndexParamAnnotations {
     }
   }
 
-  // void subList_view_needs_high(ArrayList<String> xs, int i, int k) {
-  //   if (0 <= i && i <= xs.size()) {
-  //     List<String> view = xs.subList(0, i);
-  //     if (0 <= k && k < view.size()) {
-  //     // :: error: (argument)
-  //       view.add(k, "x");
-  //     }
-  //   }
-  // }
-
   void subList_view_needs_high(ArrayList<String> xs, int i, int k) {
     if (0 <= i && i <= xs.size()) {
-      // :: error: (argument)
       List<String> view = xs.subList(0, i);
       if (0 <= k && k < view.size()) {
+        // :: error: (argument)
         view.add(k, "x");
       }
     }
@@ -79,9 +63,9 @@ public class JdkIndexParamAnnotations {
   }
 
   void remove_needs_upper_SubList(ArrayList<String> xs, int i) {
-    // :: error: (argument)
     List<String> view = xs.subList(0, xs.size());
     if (0 <= i) {
+      // :: error: (argument)
       view.remove(i);
     }
   }
@@ -99,13 +83,6 @@ public class JdkIndexParamAnnotations {
       xs.remove(i);
     }
   }
-
-  // void subList_clear_exercises_removeRange(ArrayList<String> xs, int from, int to) {
-  //   if (0 <= from && from <= xs.size() && 0 <= to && to <= xs.size() && from <= to) {
-  //     List<String> v = xs.subList(from, to);
-  //     v.clear(); // // calls SubList.removeRange(0, v.size()), (positive)
-  //   }
-  // }
 
   void addAll_needs_high_ArrayList(ArrayList<String> xs, int i) {
     if (0 <= i && i < xs.size()) {
@@ -151,9 +128,9 @@ public class JdkIndexParamAnnotations {
 
   void listIterator_needs_high_on_COWSubList(
       java.util.concurrent.CopyOnWriteArrayList<String> xs, int i) {
-    // :: error: (argument)
     List<String> view = xs.subList(0, xs.size());
     if (0 <= i && i < view.size()) {
+      // :: error: (argument)
       view.listIterator(i);
     }
   }
@@ -189,12 +166,6 @@ public class JdkIndexParamAnnotations {
     }
   }
 
-  // void listIterator_ok_inclusive_high_ArrayList(ArrayList<String> xs, int i) {
-  //   if (0 <= i && i <= xs.size()) {
-  //     xs.listIterator(i);
-  //   }
-  // }
-
   void get_set_needs_upper_Vector(Vector<String> xs, int i) {
     if (0 <= i) {
       // :: error: (argument)
@@ -204,35 +175,11 @@ public class JdkIndexParamAnnotations {
     }
   }
 
-  // void subList_needs_bounds_LinkedList(LinkedList<String> xs, int from, int to) {
-  //   if (0 <= from && from <= to) {
-  //     // :: error: (argument)
-  //     xs.subList(from, to);
-  //   }
-  // }
-
-  // positive test of the subList_needs_bounds_LinkedList
-  void subList_ok_LinkedList(LinkedList<String> xs, int from, int to) {
-    if (0 <= from && from <= to && to <= xs.size()) {
-      xs.subList(from, to);
-    }
-  }
-
-  // void COWSubList_add_needs_high(CopyOnWriteArrayList<String> xs, int i, int k) {
-  //   if (0 <= i && i <= xs.size()) {
-  //     List<String> view = xs.subList(0, i);
-  //     if (0 <= k) {
-  //       // :: error: (argument)
-  //       view.add(k, "x");
-  //     }
-  //   }
-  // }
-
   void COWSubList_add_needs_high(CopyOnWriteArrayList<String> xs, int i, int k) {
     if (0 <= i && i <= xs.size()) {
-      // :: error: (argument)
       List<String> view = xs.subList(0, i);
-      if (0 <= k) {
+      if (0 <= k && k < view.size()) {
+        // :: error: (argument)
         view.add(k, "x");
       }
     }
