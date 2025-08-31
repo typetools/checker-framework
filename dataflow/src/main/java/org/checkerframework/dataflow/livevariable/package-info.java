df55f80d0ea6321beb23191061c5583012bfb520
Michael Ernst <mernst@cs.washington.edu>
2025-08-31 08:56:37 -0700

    Rename `*Playground` to `*Pdf`

--- dataflow/src/main/java/org/checkerframework/dataflow/livevariable/package-info.java
+++ dataflow/src/main/java/org/checkerframework/dataflow/livevariable/package-info.java
@@ -1,7 +1,8 @@
 /**
  * Classes using for live variable analysis. Live variable analysis is a backward analysis to
- * calculate the variables that are live at each point in the program. To run live variable
- * analysis, see {@link org.checkerframework.dataflow.cfg.playground.LiveVariablePlayground}.
+ * calculate the variables that are live at each point in the program. To run live variable analysis
+ * on a file and create a PDF of the CFG, run {@link java
+ * org.checkerframework.dataflow.cfg.playground.LiveVariablePdf MyFile.java}.
  *
  * @see <a
  *     href="https://en.wikipedia.org/wiki/Live_variable_analysis">https://en.wikipedia.org/wiki/Live_variable_analysis</a>
