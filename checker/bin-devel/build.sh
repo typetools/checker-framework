6b9988e67090b1bce10ea047778f5227ad32837f
Michael Ernst <mernst@cs.washington.edu>
2025-09-20 21:20:57 -0700

    Tweak

--- checker/bin-devel/build.sh
+++ checker/bin-devel/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo Deprecated: use ./gradlew assemble instead.
 echo Entering checker/bin-devel/build.sh in "$(pwd)"
+echo checker/bin-devel/build.sh is deprecated: use ./gradlew assemble instead.
 
 # Fail the whole script if any command fails
 set -e
@@ -11,4 +11,5 @@ source "$SCRIPT_DIR"/clone-related.sh
 
 echo "running \"./gradlew assemble\" for checker-framework"
 ./gradlew assemble --console=plain -Dorg.gradle.internal.http.socketTimeout=60000 -Dorg.gradle.internal.http.connectionTimeout=60000
+
 echo Exiting checker/bin-devel/build.sh in "$(pwd)"
