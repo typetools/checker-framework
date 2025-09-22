a558219364e783b28dcfb46a59fe878a5d12e8b8
Michael Ernst <mernst@cs.washington.edu>
2025-09-21 20:52:03 -0700

    Revert "Test for more CI systems"
    This reverts commit 7522623f283970ca1cf4a444c99ddfa58a3c6fb7.

--- checker/bin-devel/wpi-plumelib/test-wpi-plumelib.sh
+++ checker/bin-devel/wpi-plumelib/test-wpi-plumelib.sh
@@ -89,7 +89,7 @@ test_wpi_plume_lib() {
   if ! cmp --quiet expected.txt actual.txt; then
     echo "Comparing $EXPECTED_FILE $ACTUAL_FILE in $(pwd)"
     diff -u expected.txt actual.txt
-    if [ -n "$CI" ] || [ -n "$APPVEYOR" ] || [ -n "$AZURE_HTTP_USER_AGENT" ] || [ -n "$CIRCLECI" ] || [ -n "$GITHUB_ACTIONS" ] || [ -n "$GITLAB_CI" ] || [ -n "$TRAVIS" ]; then
+    if [ -n "$AZURE_HTTP_USER_AGENT" ] || [ -n "$CIRCLE_PR_USERNAME" ] || [ -n "$GITHUB_HEAD_REF" ] || [ "$TRAVIS" = "true" ]; then
       # Running under continuous integration.  Output files that may be useful for debugging.
       echo "TESTDIR = ${TESTDIR}"
       echo "project = ${project}"
