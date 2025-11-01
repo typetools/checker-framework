7a9a4ed09f3b454fe82fb30385c7920b1ace9fdc
Michael Ernst <mernst@cs.washington.edu>
2025-11-01 09:41:45 -0700

    Split assemble and test

--- checker/bin-devel/test-cftests-junit.sh
+++ checker/bin-devel/test-cftests-junit.sh
@@ -10,4 +10,7 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 source "$SCRIPT_DIR"/clone-related.sh
 
+# `./gradlew test` subsumes `./gradlew assemble`, but performing the
+# two steps separately seems to avoid out-of-memory errors.
+./gradlew assemble --warning-mode=all
 ./gradlew test --warning-mode=all
