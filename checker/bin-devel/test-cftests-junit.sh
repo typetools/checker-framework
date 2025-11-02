d1614021f65a2d1e193699972690b5c3d617388b
Michael Ernst <mernst@cs.washington.edu>
2025-11-02 10:58:15 -0800

    Add missing `:checker:test`

--- checker/bin-devel/test-cftests-junit.sh
+++ checker/bin-devel/test-cftests-junit.sh
@@ -16,13 +16,15 @@ source "$SCRIPT_DIR"/clone-related.sh
 ./gradlew compileTestJava testClasses --warning-mode=all
 
 ## Split "test" into its parts (up to date as of 2025-11-02).
+## As of 2025-11-02, :checker:test took 11.5m and :framework:test took 6m.
 # ./gradlew test --warning-mode=all
+./gradlew :annotation-file-utilities:test --warning-mode=all
 ./gradlew :checker-qual-android:test --warning-mode=all
 ./gradlew :checker-qual:test --warning-mode=all
-./gradlew :javacutil:test --warning-mode=all
-./gradlew :test --warning-mode=all
-./gradlew :annotation-file-utilities:test --warning-mode=all
 ./gradlew :checker-util:test --warning-mode=all
+./gradlew :checker:test --warning-mode=all
 ./gradlew :dataflow:test --warning-mode=all
 ./gradlew :framework-test:test --warning-mode=all
 ./gradlew :framework:test --warning-mode=all
+./gradlew :javacutil:test --warning-mode=all
+./gradlew :test --warning-mode=all
