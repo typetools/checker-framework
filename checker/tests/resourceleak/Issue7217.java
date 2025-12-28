bace8f2b1a047c03034b08031da74f9495f6731d
Suvrat1629 <suvrat1629@gmail.com>
2025-12-28 20:31:28 +0530

    changes

--- checker/tests/resourceleak/Issue7217.java
+++ checker/tests/resourceleak/Issue7217.java
@@ -1,12 +1,8 @@
 import java.util.Collection;
 
+// Test case to show fix for https://github.com/typetools/checker-framework/issues/7217
 class Issue7217<E extends Enum<E>> {
 
-  /**
-   * This method triggers a Least Upper Bound (LUB) merge of two collections of a captured type
-   * variable E. * Before the fix, the ternary operator causes the Resource Leak Checker to crash
-   * with a NullPointerException in CFAbstractValue.
-   */
   void test(boolean b, Collection<E> c1, Collection<E> c2) {
     Object x = b ? c1 : c2;
   }
