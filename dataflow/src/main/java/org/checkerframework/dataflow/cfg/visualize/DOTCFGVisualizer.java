15c8806d089b2bf40f01102954393251026bc1c7
vivek-0509 <viveksolanki0509@gmail.com>
2026-01-16 22:45:15 +0530

    Fix crash with empty -Acfgviz outdir option (#6620)
    Fix crash with empty -Acfgviz outdir option (#6620)
    
    Fix crash with empty -Acfgviz outdir option (#6620)

--- dataflow/src/main/java/org/checkerframework/dataflow/cfg/visualize/DOTCFGVisualizer.java
+++ dataflow/src/main/java/org/checkerframework/dataflow/cfg/visualize/DOTCFGVisualizer.java
@@ -72,6 +72,9 @@ public class DOTCFGVisualizer<
           "outDir should never be null,"
               + " provide it in args when calling DOTCFGVisualizer.init(args).");
     }
+    if (this.outDir.isEmpty()) {
+      throw new UserError("The outdir argument was empty. Provide a non-empty directory path.");
+    }
     this.checkerName = (String) args.get("checkerName");
     this.generated = new HashMap<>();
   }
