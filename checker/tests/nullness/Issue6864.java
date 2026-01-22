20559ab87479c279246dd3a1d6f6be06381ce709
Suvrat1629 <suvrat1629@gmail.com>
2026-01-22 12:55:16 +0530

    add type parameter check for overriding functions

---
+++ checker/tests/nullness/Issue6864.java
@@ -0,0 +1,24 @@
+import org.checkerframework.checker.nullness.qual.*;
+import java.util.*;
+
+class A {
+    <T extends List<Integer>> T m(T x) {
+        return x;
+    }
+}
+
+class B extends A {
+    <T extends List<@Nullable Integer>> T m(T x) {
+        x.add(null);
+        return x;
+    }
+}
+
+
+public class Issue6864 {
+    public static void main(String[] args) {
+        A x = new B();
+        List<Integer> y = new LinkedList<>();
+        x.m(y).get(0).toString();
+    }
+}
