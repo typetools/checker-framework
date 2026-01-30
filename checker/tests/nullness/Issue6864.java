0918308574e0d1b7d43495c1d4a8f634b0fa97ce
Suvrat1629 <suvrat1629@gmail.com>
2026-01-30 19:39:33 +0530

    removing unnecessary files
    Signed-off-by: Suvrat1629 <suvrat1629@gmail.com>

--- checker/tests/nullness/Issue6864.java
+++ checker/tests/nullness/Issue6864.java
@@ -1,24 +1,23 @@
-import org.checkerframework.checker.nullness.qual.*;
 import java.util.*;
+import org.checkerframework.checker.nullness.qual.*;
 
 class A {
-    <T extends List<Integer>> T m(T x) {
-        return x;
-    }
+  <T extends List<Integer>> T m(T x) {
+    return x;
+  }
 }
 
 class B extends A {
-    <T extends List<@Nullable Integer>> T m(T x) {
-        x.add(null);
-        return x;
-    }
+  <T extends List<@Nullable Integer>> T m(T x) {
+    x.add(null);
+    return x;
+  }
 }
 
-
 public class Issue6864 {
-    public static void main(String[] args) {
-        A x = new B();
-        List<Integer> y = new LinkedList<>();
-        x.m(y).get(0).toString();
-    }
+  public static void main(String[] args) {
+    A x = new B();
+    List<Integer> y = new LinkedList<>();
+    x.m(y).get(0).toString();
+  }
 }
