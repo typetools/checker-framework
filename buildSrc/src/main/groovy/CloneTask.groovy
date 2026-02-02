6bd6769fa8951bd34172be74364c331ea4122049
Suzanne Millstein <smillst@cs.washington.edu>
2026-02-02 10:31:10 -0800

    Checker point.

--- buildSrc/src/main/groovy/CloneTask.groovy
+++ buildSrc/src/main/groovy/CloneTask.groovy
@@ -1,3 +1,8 @@
+import org.gradle.internal.impldep.org.eclipse.jgit.api.Git
+import org.gradle.internal.impldep.org.eclipse.jgit.api.errors.GitAPIException
+import org.gradle.internal.impldep.org.eclipse.jgit.lib.Repository
+import org.gradle.internal.impldep.org.eclipse.jgit.storage.file.FileRepositoryBuilder
+
 import java.time.Duration
 import javax.inject.Inject
 import org.gradle.api.DefaultTask
@@ -31,22 +36,16 @@ abstract class CloneTask extends DefaultTask {
   void cloneAndUpdate(String url, File directory) {
     // Gradle creates the directory if it does not exist, so check to see if the director has a .git directory.
     if (new File(directory, ".git").exists()) {
-      execOperations.exec {
-        workingDir directory
-        executable 'git'
-        args = ['pull', '-q']
-        ignoreExitValue = true
-        timeout = Duration.ofMinutes(1)
-      }
+      update(directory)
     } else {
       try {
-        clone(url, directory.toString(), true)
+        clone(url, directory, true)
       } catch (Throwable t) {
         println "Exception while cloning ${url}"
         t.printStackTrace()
       }
       if (!new File(directory, ".git").exists()) {
-        println "Cloning failed, will try again in 1 minute: clone(${url}, ${directory}, true, ${extraArgs})"
+        println "Cloning failed, will try again in 1 minute: clone(${url}, ${directory}, true)"
         sleep(60000) // wait 1 minute, then try again
         clone(urlS, getDirectory(), false)
       }
@@ -57,22 +56,32 @@ abstract class CloneTask extends DefaultTask {
    * @param url git repository to clone
    * @param directory where to clone
    * @param ignoreError whether to fail the build if the clone command fails
-   * @param extraArgs any extra arguments to pass to git
    */
-  void clone(url, directory, ignoreError, extraArgs = []) {
-    execOperations.exec {
-      workingDir "${directory}/../"
-      executable 'git'
-      args = [
-        'clone',
-        '-q',
-        '--depth=1',
-        url,
-        new File(directory).toPath().last()
-      ]
-      args += extraArgs
-      ignoreExitValue = ignoreError
-      timeout = Duration.ofMinutes(1)
+  void clone(url, directory, ignoreError) {
+
+    try {
+      Git git = Git.cloneRepository()
+          .setURI(url)
+          .setDirectory(directory)
+      .setTimeout(60) // TODO: What units is this?
+      .setDepth(1)
+          .call();
+      System.out.println("Cloning successful.");
+      // Remember to close the Git object when finished
+      git.close();
+    } catch (GitAPIException e) {
+      System.err.println("Error cloning repository: " + e.getMessage());
+      e.printStackTrace();
+    }
+  }
+  void update( directory) {
+    try {
+    Git git = Git.open(directory);
+    git.pull().call()
+    git.close()
+    } catch (GitAPIException e) {
+      System.err.println("Error cloning repository: " + e.getMessage());
+      e.printStackTrace();
     }
   }
 }
