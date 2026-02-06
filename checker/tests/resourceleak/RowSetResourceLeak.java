68905acca8a06bf4b0a15667c5a94e6e44034fdb
PARAMESH.N <parameshdarshan1234@gmail.com>
2026-02-06 19:06:47 +0530

    Add resource leak detection tests for RowSet types
    Added tests for resource leak detection in various RowSet types, including JdbcRowSet, CachedRowSet, FilteredRowSet, WebRowSet, and JoinRowSet.

---
+++ checker/tests/resourceleak/RowSetResourceLeak.java
@@ -0,0 +1,84 @@
+// Tests for RowSet types resource leak detection
+// Test case for issue 6354:
+// https://github.com/typetools/checker-framework/issues/6354
+
+import javax.sql.rowset.*;
+import com.sun.rowset.*;
+import java.sql.*;
+
+class RowSetResourceLeak {
+
+    // ========== JdbcRowSet Tests ==========
+
+    void jdbcRowSetNotClosed(ResultSet rs) throws SQLException {
+        JdbcRowSet jrs = new JdbcRowSetImpl(rs);
+        // :: error: (required.method.not.called)
+    }
+
+    void jdbcRowSetClosed(ResultSet rs) throws SQLException {
+        JdbcRowSet jrs = new JdbcRowSetImpl(rs);
+        jrs.close();
+    }
+
+    // ========== CachedRowSet Tests ==========
+
+    void cachedRowSetNotClosed() throws SQLException {
+        CachedRowSet crs = new CachedRowSetImpl();
+        // :: error: (required.method.not.called)
+    }
+
+    void cachedRowSetClosed() throws SQLException {
+        CachedRowSet crs = new CachedRowSetImpl();
+        crs.close();
+    }
+
+    void cachedRowSetToResultSetNotClosed() throws SQLException {
+        CachedRowSet crs = new CachedRowSetImpl();
+        ResultSet rs = crs.toResultSet();
+        // :: error: (required.method.not.called)
+        crs.close();
+    }
+
+    void cachedRowSetToResultSetBothClosed() throws SQLException {
+        CachedRowSet crs = new CachedRowSetImpl();
+        ResultSet rs = crs.toResultSet();
+        rs.close();
+        crs.close();
+    }
+
+    // ========== FilteredRowSet Tests ==========
+
+    void filteredRowSetNotClosed() throws SQLException {
+        FilteredRowSet frs = new FilteredRowSetImpl();
+        // :: error: (required.method.not.called)
+    }
+
+    void filteredRowSetClosed() throws SQLException {
+        FilteredRowSet frs = new FilteredRowSetImpl();
+        frs.close();
+    }
+
+    // ========== WebRowSet Tests ==========
+
+    void webRowSetNotClosed() throws SQLException {
+        WebRowSet wrs = new WebRowSetImpl();
+        // :: error: (required.method.not.called)
+    }
+
+    void webRowSetClosed() throws SQLException {
+        WebRowSet wrs = new WebRowSetImpl();
+        wrs.close();
+    }
+
+    // ========== JoinRowSet Tests ==========
+
+    void joinRowSetNotClosed() throws SQLException {
+        JoinRowSet jrs = new JoinRowSetImpl();
+        // :: error: (required.method.not.called)
+    }
+
+    void joinRowSetClosed() throws SQLException {
+        JoinRowSet jrs = new JoinRowSetImpl();
+        jrs.close();
+    }
+}
