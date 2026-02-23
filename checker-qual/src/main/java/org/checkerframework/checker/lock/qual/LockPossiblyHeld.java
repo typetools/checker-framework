89e3e16475463994663249f6541aecb02b5d4ae2
Michael Ernst <mernst@cs.washington.edu>
2026-02-23 12:00:19 -0800

    Add imports

--- checker-qual/src/main/java/org/checkerframework/checker/lock/qual/LockPossiblyHeld.java
+++ checker-qual/src/main/java/org/checkerframework/checker/lock/qual/LockPossiblyHeld.java
@@ -1,6 +1,7 @@
 package org.checkerframework.checker.lock.qual;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
--- checker-qual/src/main/java/org/checkerframework/common/subtyping/qual/Unqualified.java
+++ checker-qual/src/main/java/org/checkerframework/common/subtyping/qual/Unqualified.java
@@ -1,6 +1,7 @@
 package org.checkerframework.common.subtyping.qual;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
