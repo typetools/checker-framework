bb6bc49821cd6521046901810c6fd6611ad12fbc
Michael Ernst <mernst@cs.washington.edu>
2026-03-20 13:21:41 -0700

    Reinstate suppression

--- annotation-file-utilities/src/main/java/org/checkerframework/afu/scenelib/el/AScene.java
+++ annotation-file-utilities/src/main/java/org/checkerframework/afu/scenelib/el/AScene.java
@@ -408,8 +408,11 @@ public class AScene implements Cloneable {
       System.out.print(args[i] + ": ");
       try {
         IndexFileParser.parseFile(args[i], s0);
-        // testing that clone() does not throw an exception
-        s0.clone();
+        @SuppressWarnings({
+          "UnusedVariable",
+          "PMD.UnusedLocalVariable"
+        }) // testing that clone() does not throw an exception
+        AScene ignore = s0.clone();
         System.out.println("ok");
       } catch (Throwable e) {
         status = 1;
