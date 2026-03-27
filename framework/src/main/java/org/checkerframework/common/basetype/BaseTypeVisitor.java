13693157aa1b200a1ca9aa3a93eb0224a37e129d
Michael Ernst <mernst@cs.washington.edu>
2026-03-27 15:50:06 -0700

    Minor changes

--- framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
+++ framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
@@ -2169,8 +2169,8 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
    * Returns true if and only if {@code inferredAnnotation} is valid for a given expression to match
    * the {@code necessaryAnnotation}.
    *
-   * <p>By default, {@code inferredAnnotation} must be a subtype of {@code necessaryAnnotation}, but
-   * subclasses might override this behavior.
+   * <p>By default, {@code inferredAnnotation} must be a subtype of (or equal to) {@code
+   * necessaryAnnotation}, but subclasses might override this behavior.
    */
   protected boolean checkContract(
       JavaExpression expr,
