609bed4fb17163d3d27c7ac775a30d3c8a230b6c
Suzanne Millstein <smillst@cs.washington.edu>
2026-04-01 10:55:30 -0700

    Use 9.

--- checker/jtreg/Jdk11Release9.java
+++ checker/jtreg/Jdk11Release9.java
@@ -1,6 +1,6 @@
 /*
  * @test
- * @summary Test that --release 8 does not cause a crash.
+ * @summary Test that --release 9 does not cause a crash.
  *
  * @compile -processor org.checkerframework.checker.interning.InterningChecker Jdk11Release8.java --release 8
  */
