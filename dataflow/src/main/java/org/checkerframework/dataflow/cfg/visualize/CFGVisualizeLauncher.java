490c16e16772209b5da44188969051fc3cbbf6ec
Suzanne Millstein <smillst@cs.washington.edu>
2026-04-02 11:20:32 -0700

    Remove suppression.

--- dataflow/src/main/java/org/checkerframework/dataflow/cfg/visualize/CFGVisualizeLauncher.java
+++ dataflow/src/main/java/org/checkerframework/dataflow/cfg/visualize/CFGVisualizeLauncher.java
@@ -255,7 +255,6 @@ public final class CFGVisualizeLauncher {
     try {
       // Redirect syserr to nothing (and prevent the compiler from issuing
       // warnings about our exception).
-      @SuppressWarnings({"builder:required.method.not.called", "mustcall:assignment"})
       @MustCall OutputStream nullOS = OutputStream.nullOutputStream();
       System.setErr(new PrintStream(nullOS));
       javac.compile(List.of(l), List.of(clas), List.of(cfgProcessor), List.nil());
