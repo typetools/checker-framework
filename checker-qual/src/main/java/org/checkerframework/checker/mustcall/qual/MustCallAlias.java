d496b704536594e2e3a0c4e04a08e1f14760fa87
Suzanne Millstein <smillst@cs.washington.edu>
2026-04-02 10:46:03 -0700

    Fix.

--- checker-qual/src/main/java/org/checkerframework/checker/mustcall/qual/MustCallAlias.java
+++ checker-qual/src/main/java/org/checkerframework/checker/mustcall/qual/MustCallAlias.java
@@ -78,5 +78,5 @@ import java.lang.annotation.Target;
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.PARAMETER, ElementType.CONSTRUCTOR, ElementType.METHOD})
+@Target({ElementType.PARAMETER, ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.TYPE_USE})
 public @interface MustCallAlias {}
