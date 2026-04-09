d72e9098c4914b7cbce9a2b41c2821ce119b4922
Sanjay Malakar <iamsanjaymalakar@gmail.com>
2026-04-08 18:27:31 -0700

    Expand RLC Log4j side-effect-free stubs and tests

--- checker/tests/resourceleak/sideeffect-free-stubs/Log4j1ObjectCalls.java
+++ checker/tests/resourceleak/sideeffect-free-stubs/Log4j1ObjectCalls.java
@@ -1,6 +1,6 @@
 // This test covers the Log4j 1.x API in package org.apache.log4j.
 // The local Logger class below is just a tiny stand-in for the real library API.
-// The RLC-specific stub marks debug/warn/error(Object) as @SideEffectFree, so
+// The RLC-specific stub marks logging methods as @SideEffectFree, so
 // logging after a resource is closed should not wipe out the close fact.
 
 package org.apache.log4j;
@@ -11,6 +11,10 @@ import org.checkerframework.checker.mustcall.qual.Owning;
 
 class Logger {
   public void debug(Object message) {}
+
+  public void debug(Object message, Throwable t) {}
+
+  public void info(Object message) {}
 
   public void warn(Object message) {}
 
@@ -31,6 +35,30 @@ class Log4j1DebugObject implements Closeable {
   public void close() {
     resource.close();
     logger.debug("after close");
+  }
+}
+
+class Log4j1DebugWithThrowable implements Closeable {
+  private final Logger logger = new Logger();
+  private @Owning CloseableResource resource = new CloseableResource();
+
+  @Override
+  @EnsuresCalledMethods(value = "this.resource", methods = "close")
+  public void close() {
+    resource.close();
+    logger.debug("after close", new RuntimeException());
+  }
+}
+
+class Log4j1InfoObject implements Closeable {
+  private final Logger logger = new Logger();
+  private @Owning CloseableResource resource = new CloseableResource();
+
+  @Override
+  @EnsuresCalledMethods(value = "this.resource", methods = "close")
+  public void close() {
+    resource.close();
+    logger.info("after close");
   }
 }
 
--- checker/tests/resourceleak/sideeffect-free-stubs/Log4j2ObjectCalls.java
+++ checker/tests/resourceleak/sideeffect-free-stubs/Log4j2ObjectCalls.java
@@ -1,8 +1,7 @@
 // This test covers the Log4j 2.x API in package org.apache.logging.log4j.
 // The local Logger and LogManager declarations below are tiny stand-ins for the
-// real library API. The RLC-specific stub marks Logger.debug/warn/error(Object)
-// as @SideEffectFree, so logging after a resource is closed should not wipe out
-// the close fact.
+// real library API. The RLC-specific stub marks logging methods as @SideEffectFree,
+// so logging after a resource is closed should not wipe out the close fact.
 
 package org.apache.logging.log4j;
 
@@ -13,6 +12,10 @@ import org.checkerframework.checker.mustcall.qual.Owning;
 interface Logger {
   void debug(Object message);
 
+  void debug(String message, Object... params);
+
+  void info(Object message);
+
   void warn(Object message);
 
   void error(Object message);
@@ -21,6 +24,12 @@ interface Logger {
 final class SimpleLogger implements Logger {
   @Override
   public void debug(Object message) {}
+
+  @Override
+  public void debug(String message, Object... params) {}
+
+  @Override
+  public void info(Object message) {}
 
   @Override
   public void warn(Object message) {}
@@ -51,6 +60,30 @@ class Log4j2DebugObject implements Closeable {
   public void close() {
     resource.close();
     logger.debug("after close");
+  }
+}
+
+class Log4j2DebugVarargs implements Closeable {
+  private static final Logger logger = LogManager.getLogger();
+  private @Owning CloseableResource resource = new CloseableResource();
+
+  @Override
+  @EnsuresCalledMethods(value = "this.resource", methods = "close")
+  public void close() {
+    resource.close();
+    logger.debug("closed resource {}", "name");
+  }
+}
+
+class Log4j2InfoObject implements Closeable {
+  private static final Logger logger = LogManager.getLogger();
+  private @Owning CloseableResource resource = new CloseableResource();
+
+  @Override
+  @EnsuresCalledMethods(value = "this.resource", methods = "close")
+  public void close() {
+    resource.close();
+    logger.info("after close");
   }
 }
 
