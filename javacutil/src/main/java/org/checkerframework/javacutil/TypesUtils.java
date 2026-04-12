5baa917215ccba5c1c6c8e1ab6b907fac622e698
Michael Ernst <mernst@cs.washington.edu>
2026-04-12 11:19:29 -0700

    Undo a conversion

--- javacutil/src/main/java/org/checkerframework/javacutil/TypesUtils.java
+++ javacutil/src/main/java/org/checkerframework/javacutil/TypesUtils.java
@@ -111,38 +111,49 @@ public final class TypesUtils {
    * @param typeMirror a TypeMirror
    * @return the class for {@code typeMirror}
    */
+  @SuppressWarnings(
+      "StatementSwitchToExpressionSwitch") // Task :javacutil:javadoc fails under JDK 17
   public static Class<?> getClassFromType(TypeMirror typeMirror) {
 
-    return switch (typeMirror.getKind()) {
-      case INT -> int.class;
-      case LONG -> long.class;
-      case SHORT -> short.class;
-      case BYTE -> byte.class;
-      case CHAR -> char.class;
-      case DOUBLE -> double.class;
-      case FLOAT -> float.class;
-      case BOOLEAN -> boolean.class;
-      case ARRAY -> {
+    switch (typeMirror.getKind()) {
+      case INT:
+        return int.class;
+      case LONG:
+        return long.class;
+      case SHORT:
+        return short.class;
+      case BYTE:
+        return byte.class;
+      case CHAR:
+        return char.class;
+      case DOUBLE:
+        return double.class;
+      case FLOAT:
+        return float.class;
+      case BOOLEAN:
+        return boolean.class;
+
+      case ARRAY:
         Class<?> componentClass = getClassFromType(((ArrayType) typeMirror).getComponentType());
-        yield componentClass.arrayType();
-      }
-      case DECLARED -> {
+        return componentClass.arrayType();
+
+      case DECLARED:
         // BUG: need to compute a @ClassGetName, but this code computes a
         // @CanonicalNameOrEmpty.  They are different for inner classes.
         @SuppressWarnings("signature") // https://tinyurl.com/cfissue/658 for Names.toString
         @DotSeparatedIdentifiers String typeString = TypesUtils.getQualifiedName((DeclaredType) typeMirror).toString();
         if (typeString.equals("<nulltype>")) {
-          yield void.class;
-        } else {
-          try {
-            yield Class.forName(typeString);
-          } catch (ClassNotFoundException | NoClassDefFoundError | UnsupportedClassVersionError e) {
-            yield Object.class;
-          }
+          return void.class;
         }
-      }
-      default -> Object.class;
-    };
+        try {
+          return Class.forName(typeString);
+        } catch (ClassNotFoundException | NoClassDefFoundError | UnsupportedClassVersionError e) {
+          return Object.class;
+        }
+
+      default:
+        return Object.class;
+    }
   }
 
   // Getters
