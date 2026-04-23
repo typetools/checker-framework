02c89a24fe9c0f5fd0dc0d587940fe9a267536e1
Suzanne Millstein <smillst@cs.washington.edu>
2026-04-23 09:08:52 -0700

    Can't test --release with --add-export.

--- checker/jtreg/Jdk11Release9.java
+++
@@ -1,9 +0,0 @@
-/*
- * @test
- * @summary Test that --release 9 does not cause a crash.
- *
- * @compile -processor org.checkerframework.checker.interning.InterningChecker Jdk11Release9.java --release 9
- * @ingore
- */
-
-public class Jdk11Release8 {}
