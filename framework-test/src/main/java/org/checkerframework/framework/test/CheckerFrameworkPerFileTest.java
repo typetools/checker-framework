1cd5d461ff3fd5903ae785d2e5294ac62cea3f30
Michael Ernst <mernst@cs.washington.edu>
2026-05-27 15:48:24 -0700

    Add import

--- framework-test/src/main/java/org/checkerframework/framework/test/CheckerFrameworkPerFileTest.java
+++ framework-test/src/main/java/org/checkerframework/framework/test/CheckerFrameworkPerFileTest.java
@@ -2,6 +2,7 @@ package org.checkerframework.framework.test;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import javax.annotation.processing.AbstractProcessor;
