1bc7cb58a123079eb37f9a91010fbebdc02cf3da
Michael Ernst <mernst@cs.washington.edu>
2026-05-28 10:18:29 -0700

    Add `@SuppressWarnings`

--- framework/tests/all-systems/Enums.java
+++ framework/tests/all-systems/Enums.java
@@ -32,6 +32,7 @@ public class Enums {
     }
 
     public <SSS extends Object> SSS firstNonNull(SSS first, SSS second) {
+      @SuppressWarnings("nullness:nulltest.redundant")
       SSS res = first != null ? first : checkNotNull(second);
       return res;
     }
