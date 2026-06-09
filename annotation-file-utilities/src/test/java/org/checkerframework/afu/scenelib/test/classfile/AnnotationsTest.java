90ce5e893aa78572d98933e3ef2eaeabd3c9d54d
Michael Ernst <mernst@cs.washington.edu>
2026-06-09 10:59:54 -0700

    Add imports

--- annotation-file-utilities/src/test/java/org/checkerframework/afu/scenelib/test/classfile/AnnotationsTest.java
+++ annotation-file-utilities/src/test/java/org/checkerframework/afu/scenelib/test/classfile/AnnotationsTest.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import junit.framework.TestResult;
 import junit.framework.TestSuite;
 import org.checkerframework.afu.scenelib.el.AScene;
