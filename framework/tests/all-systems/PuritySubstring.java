d27e2df16f79cbf43f9cc095a4db09bfe4528cdd
Michael Ernst <mernst@cs.washington.edu>
2026-07-11 15:15:15 -0700

    Suppress warnings

--- framework/tests/all-systems/PuritySubstring.java
+++ framework/tests/all-systems/PuritySubstring.java
@@ -1,12 +1,17 @@
+import org.checkerframework.checker.confidential.qual.PolyConfidential;
 import org.checkerframework.dataflow.qual.SideEffectFree;
 
 public class PuritySubstring {
 
-  @SuppressWarnings({"allcheckers:purity", "lock"}) // local StringBuilder
+  @SuppressWarnings({
+    "allcheckers:purity", // local StringBuilder
+    "lock", // local StringBuilder
+    "confidential" // JDK is not yet annotated
+  })
   @SideEffectFree
   @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
+  public @PolyConfidential String toString(@PolyConfidential PuritySubstring this) {
+    @PolyConfidential StringBuilder sb = new @PolyConfidential StringBuilder();
     sb.append("hello");
     return sb.toString();
   }
