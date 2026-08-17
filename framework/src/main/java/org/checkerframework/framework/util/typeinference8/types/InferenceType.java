24df494f137899a5f1c39b5393abb55d5b3d8c9b
Michael Ernst <mernst@cs.washington.edu>
2026-08-17 14:54:09 -0700

    Add `import`s

--- framework/src/main/java/org/checkerframework/framework/util/typeinference8/types/InferenceType.java
+++ framework/src/main/java/org/checkerframework/framework/util/typeinference8/types/InferenceType.java
@@ -7,6 +7,7 @@ import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
--- framework/src/main/java/org/checkerframework/framework/util/typeinference8/types/VariableBounds.java
+++ framework/src/main/java/org/checkerframework/framework/util/typeinference8/types/VariableBounds.java
@@ -19,6 +19,7 @@ import org.checkerframework.framework.util.typeinference8.constraint.Typing;
 import org.checkerframework.framework.util.typeinference8.util.Java8InferenceContext;
 import org.checkerframework.javacutil.BugInCF;
 import org.checkerframework.javacutil.TypesUtils;
+import org.plumelib.util.CollectionsP;
 import org.plumelib.util.IPair;
 
 /** Data structure that stores the bounds of a variable. */
