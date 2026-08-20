785dfc4e2d9e262e5a3de4afd597af70759748dd
Michael Ernst <mernst@cs.washington.edu>
2026-08-20 11:22:19 -0700

    `@SideEffectsOnly` annotations

--- checker/tests/signedness/CombinationIterator.java
+++ checker/tests/signedness/CombinationIterator.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import org.checkerframework.dataflow.qual.SideEffectsOnly;
 
 public class CombinationIterator<T> implements Iterator<List<T>> {
   public CombinationIterator(Collection<? extends Collection<T>> collectionsOfCandidates) {
@@ -15,6 +16,7 @@ public class CombinationIterator<T> implements Iterator<List<T>> {
   }
 
   @Override
+  @SideEffectsOnly("this")
   public List<T> next() {
     return null;
   }
