81b9930f6c164d761535d520997f8993bd1b1050
Michael Ernst <mernst@cs.washington.edu>
2026-09-01 10:06:14 -0400

    AFU Javadoc

--- annotation-file-utilities/src/main/java/org/checkerframework/afu/annotator/find/ConstructorInsertion.java
+++ annotation-file-utilities/src/main/java/org/checkerframework/afu/annotator/find/ConstructorInsertion.java
@@ -4,9 +4,17 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 import org.checkerframework.afu.scenelib.type.Type;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
+/**
+ * An insertion for a default constructor, which is generated when a class has no explicit
+ * constructor but a receiver or a declaration annotation must be written on one.
+ */
 public class ConstructorInsertion extends TypedInsertion {
-  private ReceiverInsertion receiverInsertion = null;
+  /** The insertion for the constructor's receiver parameter, or null if there is none. */
+  private @Nullable ReceiverInsertion receiverInsertion = null;
+
+  /** The declaration annotations to write on the generated constructor. */
   private Set<Insertion> declarationInsertions = new LinkedHashSet<>();
 
   /**
