b1c3b3326229d9e9934508945a3e699b646116c3
Michael Ernst <mernst@cs.washington.edu>
2026-09-21 14:45:40 -0700

    Improve an error message
    Both trees may be null, not just one.

--- framework/src/main/java/org/checkerframework/framework/ajava/DoubleJavacVisitor.java
+++ framework/src/main/java/org/checkerframework/framework/ajava/DoubleJavacVisitor.java
@@ -147,7 +147,7 @@ public abstract class DoubleJavacVisitor extends SimpleTreeVisitor<Void, Tree> {
   protected final void assertSameKind(@Nullable Tree tree1, @Nullable Tree tree2) {
     if (tree1 == null || tree2 == null) {
       throw new UserError(
-          "%s: one tree is null: tree1=%s [%s] tree2=%s [%s]",
+          "%s: at least one tree is null: tree1=%s [%s] tree2=%s [%s]",
           this.getClass().getCanonicalName(),
           tree1,
           (tree1 == null ? "null" : tree1.getKind()),
