98d2bfb58d1a85c51a68942ecbc4f1b18892605b
Michael Ernst <mernst@cs.washington.edu>
2026-09-21 17:17:02 -0700

    Avoid a nullness warning in AnnotationFileParser.sameTypeArguments

--- framework/src/main/java/org/checkerframework/framework/stub/AnnotationFileParser.java
+++ framework/src/main/java/org/checkerframework/framework/stub/AnnotationFileParser.java
@@ -2242,14 +2242,17 @@ public final class AnnotationFileParser {
     ClassOrInterfaceType javaParserScope = javaParserType.getScope().orElse(null);
     // If no enclosing type is written with type arguments, then the enclosing types need not be
     // compared, just as a type with no type arguments matches any type arguments.
-    boolean compareScope = javaParserScope != null && writesTypeArguments(javaParserScope);
-    if (javaParserTypeArgs == null && !compareScope) {
+    if (javaParserScope != null && !writesTypeArguments(javaParserScope)) {
+      javaParserScope = null;
+    }
+    if (javaParserTypeArgs == null && javaParserScope == null) {
       return true;
     }
     if (!(javacType instanceof DeclaredType javacDeclaredType)) {
       return false;
     }
-    if (compareScope && !sameTypeArguments(javacDeclaredType.getEnclosingType(), javaParserScope)) {
+    if (javaParserScope != null
+        && !sameTypeArguments(javacDeclaredType.getEnclosingType(), javaParserScope)) {
       return false;
     }
     if (javaParserTypeArgs == null) {
