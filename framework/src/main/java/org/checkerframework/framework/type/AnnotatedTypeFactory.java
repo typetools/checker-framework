c2f7148527dbdc975a9d417e984698c1ae454e5d
Michael Ernst <mernst@cs.washington.edu>
2026-09-21 21:03:12 -0700

    Fix nullness errors in AnnotatedTypeFactory
    * getSelfType: handle a class tree with no element
    * getDeclAnnotation: declare the return type as @Nullable
    * logGat: permit null format arguments

--- framework/src/main/java/org/checkerframework/framework/type/AnnotatedTypeFactory.java
+++ framework/src/main/java/org/checkerframework/framework/type/AnnotatedTypeFactory.java
@@ -2272,12 +2272,17 @@ public class AnnotatedTypeFactory implements AnnotationProvider {
    * nor implicitly. This method can be overridden for type-system specific behavior.
    *
    * @param tree location used to decide the type of {@code this}
-   * @return the type of {@code this} at the location of {@code tree}
+   * @return the type of {@code this} at the location of {@code tree}, or null if there is none
    */
   public @Nullable AnnotatedDeclaredType getSelfType(Tree tree) {
     logGat("getSelfType(%s) of kind %s%n", tree, tree.getKind());
     if (TreeUtils.isClassTree(tree)) {
-      return getAnnotatedType(TreeUtils.elementFromDeclaration((ClassTree) tree));
+      TypeElement classElt = TreeUtils.elementFromDeclaration((ClassTree) tree);
+      if (classElt == null) {
+        // No element exists for certain kinds of anonymous classes.
+        return null;
+      }
+      return getAnnotatedType(classElt);
     }
 
     Tree enclosingTree = getEnclosingClassOrMethod(tree);
@@ -4044,10 +4049,10 @@ public class AnnotatedTypeFactory implements AnnotationProvider {
    * @see #getDeclAnnotationNoAliases
    * @param elt the element to retrieve the declaration annotation from
    * @param annoClass annotation class
-   * @return the annotation mirror for annoClass
+   * @return the annotation mirror for annoClass, or null if none exists
    */
   @Override
-  public final AnnotationMirror getDeclAnnotation(
+  public final @Nullable AnnotationMirror getDeclAnnotation(
       Element elt, Class<? extends Annotation> annoClass) {
     logGat("entering getDeclAnnotation(%s [%s], %s)%n", elt, elt.getKind(), annoClass);
     if (debugGat) {
@@ -6179,7 +6184,7 @@ public class AnnotatedTypeFactory implements AnnotationProvider {
    * @param args arguments to the format string
    */
   @FormatMethod
-  public void logGat(String format, Object... args) {
+  public void logGat(String format, @Nullable Object... args) {
     if (debugGat) {
       SystemP.sleep(1); // logging can interleave with typechecker output
 
