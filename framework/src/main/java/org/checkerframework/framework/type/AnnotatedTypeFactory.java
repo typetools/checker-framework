df7d38cd9080fd6d85078883b1728ff65d5e8b62
Michael Ernst <mernst@cs.washington.edu>
2026-09-21 21:13:58 -0700

    Honor "allcheckers" in @DoesNotUnrefineReceiver

--- framework/src/main/java/org/checkerframework/framework/type/AnnotatedTypeFactory.java
+++ framework/src/main/java/org/checkerframework/framework/type/AnnotatedTypeFactory.java
@@ -6156,6 +6156,9 @@ public class AnnotatedTypeFactory implements AnnotationProvider {
         List<String> typeSystems =
             AnnotationUtils.getElementValueArray(
                 am, doesNotUnrefineReceiverValueElement, String.class);
+        if (typeSystems.contains("allcheckers")) {
+          return true;
+        }
         for (String prefix : checker.getSuppressWarningsPrefixes()) {
           if (typeSystems.contains(prefix)) {
             return true;
