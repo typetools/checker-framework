04a38e9dd5947f6eb37bccc341eea62e3199420e
Michael Ernst <mernst@cs.washington.edu>
2026-09-23 07:16:11 -0700

    Small independent changes
    Make BaseTypeVisitor.FoundRequired and SourceVisitor.storeSuppressWarningsAnno protected,
    drop -Werror from checker tasks that use -Awarns, suppress AFU nullness/keyfor warnings,
    and improve documentation.

--- annotation-file-utilities/src/main/java/org/checkerframework/afu/scenelib/el/AElement.java
+++ annotation-file-utilities/src/main/java/org/checkerframework/afu/scenelib/el/AElement.java
@@ -239,7 +239,11 @@ public class AElement implements Cloneable {
    * @param orig the map to copy from
    * @param copy the map to copy into
    */
-  @SuppressWarnings("unchecked")
+  @SuppressWarnings({
+    "unchecked",
+    "nullness:argument" // `orig.get(key)` passes a `K`, whose implicit upper bound is
+    // `@Nullable Object`, to `WrapperMap.get(Object)`, whose parameter is `@NonNull`.
+  })
   static <K, V extends AElement> void copyMapContents(
       VivifyingMap<K, V> orig, VivifyingMap<K, V> copy) {
     for (K key : orig.keySet()) {
--- framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
+++ framework/src/main/java/org/checkerframework/common/basetype/BaseTypeVisitor.java
@@ -3547,14 +3547,19 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
   /**
    * Class that creates string representations of {@link AnnotatedTypeMirror}s which are only
    * verbose if required to differentiate the two types.
+   *
+   * <p>It is protected so that a subclass that reports one of the message keys that take a
+   * found/required pair, such as {@code override.receiver}, renders the pair the same way that this
+   * class does.
    */
-  private static final class FoundRequired {
+  protected static final class FoundRequired {
     /** The found type. */
     public final String found;
 
     /** The required type. */
     public final String required;
 
+    /** Create a FoundRequired for two types. */
     private FoundRequired(AnnotatedTypeMirror found, AnnotatedTypeMirror required) {
       if (shouldPrintVerbose(found, required)) {
         this.found = found.toString(true);
@@ -3579,8 +3584,12 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
     /**
      * Creates string representations of {@link AnnotatedTypeMirror}s which are only verbose if
      * required to differentiate the two types.
+     *
+     * @param found the found type
+     * @param required the required type
+     * @return a FoundRequired for the two types
      */
-    static FoundRequired of(AnnotatedTypeMirror found, AnnotatedTypeMirror required) {
+    public static FoundRequired of(AnnotatedTypeMirror found, AnnotatedTypeMirror required) {
       return new FoundRequired(found, required);
     }
 
@@ -3588,8 +3597,13 @@ public class BaseTypeVisitor<Factory extends GenericAnnotatedTypeFactory<?, ?, ?
      * Creates string representations of {@link AnnotatedTypeMirror} and {@link
      * AnnotatedTypeParameterBounds}s which are only verbose if required to differentiate the two
      * types.
+     *
+     * @param found the found type
+     * @param required the required bounds
+     * @return a FoundRequired for the type and the bounds
      */
-    static FoundRequired of(AnnotatedTypeMirror found, AnnotatedTypeParameterBounds required) {
+    public static FoundRequired of(
+        AnnotatedTypeMirror found, AnnotatedTypeParameterBounds required) {
       return new FoundRequired(found, required);
     }
   }
