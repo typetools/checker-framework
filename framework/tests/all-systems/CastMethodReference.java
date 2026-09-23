014046f22aa5ddfc69956b422757896da623f26d
Suzanne Millstein <smillst@cs.washington.edu>
2026-09-23 09:33:31 -0700

    Suppress warnings.

--- framework/tests/all-systems/CastMethodReference.java
+++ framework/tests/all-systems/CastMethodReference.java
@@ -2,6 +2,7 @@ import java.util.function.Function;
 
 // JavaParser does not parse a cast of a method reference correctly:
 // https://github.com/javaparser/javaparser/issues/3855
+@SuppressWarnings("all") // check for crashes
 public class CastMethodReference {
   Object simpleType() {
     return (Runnable) this::hashCode;
