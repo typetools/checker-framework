7b8e182f9004c55e4ebf244a34a92b21c3b12eb8
Suzanne Millstein <smillst@cs.washington.edu>
2026-09-23 09:11:12 -0700

    Annotate Issue579 constructor as @SideEffectFree
    Comparator.thenComparing is @SideEffectFree in the annotated JDK, so the
    overriding method in Issue579 inherits that annotation and may only call
    side-effect-free constructors.

--- checker/tests/nullness/java8/Issue579.java
+++ checker/tests/nullness/java8/Issue579.java
@@ -2,11 +2,13 @@
 // https://github.com/typetools/checker-framework/issues/579
 
 import java.util.Comparator;
+import org.checkerframework.dataflow.qual.SideEffectFree;
 
 public class Issue579<T> implements Comparator<T> {
   private final Comparator<T> real;
 
   @SuppressWarnings("unchecked")
+  @SideEffectFree
   Issue579(Comparator<? super T> real) {
     this.real = (Comparator<T>) real;
   }
