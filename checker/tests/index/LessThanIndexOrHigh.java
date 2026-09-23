7168ef27399221a6da69b044369b2f126fbe69c9
Michael Ernst <mernst@cs.washington.edu>
2026-09-23 10:34:20 -0700

    Describe how the code works now

--- checker/tests/index/LessThanIndexOrHigh.java
+++ checker/tests/index/LessThanIndexOrHigh.java
@@ -4,9 +4,8 @@ import org.checkerframework.checker.index.qual.LessThan;
 // The type of `end` is @IndexOrHigh("array"), which implies the @IntRangeFromNonNegative alias.
 // `LessThanAnnotatedTypeFactory.getMinValueFromString` looks for @IntRange, so the Index Checker
 // can prove that -1 is less than `end` only if the abstract value for `end` holds an @IntRange
-// rather than the alias.  `ValueAnalysis` converts the alias when it creates an abstract value; the
-// conversion used to happen only when two values were merged at a control-flow join point, and this
-// method has no join point.
+// rather than the alias.  `ValueAnalysis` converts the alias whenever it creates an abstract value,
+// not only when it merges two values at a control-flow join point; this method has no join point.
 public class LessThanIndexOrHigh {
 
   private static @LessThan("#2") int f(short[] array, @IndexOrHigh("#1") int end) {
